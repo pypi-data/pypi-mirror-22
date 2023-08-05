eb72a44a58b7414a5bfa8211a740b2c20003ba06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:58 +0000

    Add package RpiNotify/2.1/RpiNotify-2.1.tar.gz

---
+++ packages/RpiNotify/RpiNotify-2.1.tar.gz/RpiNotify-2.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'RpiNotify',
+  packages = ['RpiNotify'], # this must be the same as the name above
+  version = '2.1',
+  description = 'Push notifications from IoT devices to your smarphone.',
+  author = 'Gabriele Toselli',
+  author_email = 'hello@gabrieletoselli.eu',
+  url = 'https://rpinotify.it/', # use the URL to the github repo
+  download_url = 'https://rpinotify.it/', # I'll explain this in a second
+  keywords = ['Push', 'Iot', 'Raspberry'], # arbitrary keywords
+  classifiers = [],
+  install_requires=[
+          'uuid',
+          'base64',
+          'ftplib',
+          'requests',
+          'jwt',
+          'logging'
+      ],
+)
