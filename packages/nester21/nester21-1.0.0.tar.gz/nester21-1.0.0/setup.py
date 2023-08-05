0e0cf895fdd399ccd25dd1d8a53df936c9f6d7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:35 +0000

    Add package nester21/1.0.0/nester21-1.0.0.tar.gz

---
+++ packages/nester21/nester21-1.0.0.tar.gz/nester21-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'nester21',
+    version         = '1.0.0',
+    py_modules      = ['nester21'],
+    author          = 'gigax',
+    author_email    = 'marcostolosa@hotmail.com',
+    url             = 'https://github.com/Marco-GS',
+    description     = 'A simple printer of nested lists',
+)
