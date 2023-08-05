9b1b94ee1b5b01308d7c5641e9beeb808be48455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:11 +0000

    Add package python-quickbooks/0.6.1/python-quickbooks-0.6.1.tar.gz

---
+++ packages/python-quickbooks/python-quickbooks-0.6.1.tar.gz/python-quickbooks-0.6.1/quickbooks/client.py
@@ -0,0 +1,393 @@
+try:  # Python 3
+    import http.client as httplib
+    from urllib.parse import parse_qsl
+except ImportError:  # Python 2
+    import httplib
+    from urlparse import parse_qsl
+
+import textwrap
+import json
+import os
+from .exceptions import QuickbooksException, SevereException, AuthorizationException
+import base64
+
+try:
+    from rauth import OAuth1Session, OAuth1Service
+except ImportError:
+    print("Please import Rauth:\n\n")
+    print("http://rauth.readthedocs.org/en/latest/\n")
+    raise
+
+
+class QuickBooks(object):
+    """A wrapper class around Python's Rauth module for Quickbooks the API"""
+
+    access_token = ''
+    access_token_secret = ''
+    consumer_key = ''
+    consumer_secret = ''
+    company_id = 0
+    callback_url = ''
+    session = None
+    sandbox = False
+    minorversion = None
+
+    qbService = None
+
+    sandbox_api_url_v3 = "https://sandbox-quickbooks.api.intuit.com/v3"
+    api_url_v3 = "https://quickbooks.api.intuit.com/v3"
+
+    request_token_url = "https://oauth.intuit.com/oauth/v1/get_request_token"
+    access_token_url = "https://oauth.intuit.com/oauth/v1/get_access_token"
+
+    authorize_url = "https://appcenter.intuit.com/Connect/Begin"
+
+    current_user_url = "https://appcenter.intuit.com/api/v1/user/current"
+
+    disconnect_url = "https://appcenter.intuit.com/api/v1/connection/disconnect"
+    reconnect_url = "https://appcenter.intuit.com/api/v1/connection/reconnect"
+
+    request_token = ''
+    request_token_secret = ''
+
+    _BUSINESS_OBJECTS = [
+        "Account", "Attachable", "Bill", "BillPayment",
+        "Class", "CreditMemo", "Customer",
+        "Department", "Deposit", "Employee", "Estimate", "Invoice",
+        "Item", "JournalEntry", "Payment", "PaymentMethod",
+        "Purchase", "PurchaseOrder", "RefundReceipt",
+        "SalesReceipt", "TaxCode", "TaxService/Taxcode", "TaxRate", "Term",
+        "TimeActivity", "Transfer", "Vendor", "VendorCredit"
+    ]
+
+    __instance = None
+    __use_global = False
+
+    def __new__(cls, **kwargs):
+        """
+        If global is disabled, don't set global client instance.
+        """
+        if QuickBooks.__use_global:
+            if QuickBooks.__instance is None:
+                QuickBooks.__instance = object.__new__(cls)
+            instance = QuickBooks.__instance
+        else:
+            instance = object.__new__(cls)
+
+        if 'consumer_key' in kwargs:
+            instance.consumer_key = kwargs['consumer_key']
+
+        if 'consumer_secret' in kwargs:
+            instance.consumer_secret = kwargs['consumer_secret']
+
+        if 'access_token' in kwargs:
+            instance.access_token = kwargs['access_token']
+
+        if 'access_token_secret' in kwargs:
+            instance.access_token_secret = kwargs['access_token_secret']
+
+        if 'company_id' in kwargs:
+            instance.company_id = kwargs['company_id']
+
+        if 'callback_url' in kwargs:
+            instance.callback_url = kwargs['callback_url']
+
+        if 'sandbox' in kwargs:
+            instance.sandbox = kwargs['sandbox']
+
+        if 'minorversion' in kwargs:
+            instance.minorversion = kwargs['minorversion']
+
+        return instance
+
+    @classmethod
+    def get_instance(cls):
+        return cls.__instance
+
+    @classmethod
+    def disable_global(cls):
+        """
+        Disable use of singleton pattern.
+        """
+        QuickBooks.__use_global = False
+        QuickBooks.__instance = None
+
+    @classmethod
+    def enable_global(cls):
+        """
+        Allow use of singleton pattern.
+        """
+        QuickBooks.__use_global = True
+
+    def _drop(self):
+        QuickBooks.__instance = None
+
+    @property
+    def api_url(self):
+        if self.sandbox:
+            return self.sandbox_api_url_v3
+        else:
+            return self.api_url_v3
+
+    def create_session(self):
+        if self.consumer_secret and self.consumer_key \
+                and self.access_token_secret and self.access_token:
+            session = OAuth1Session(
+                self.consumer_key,
+                self.consumer_secret,
+                self.access_token,
+                self.access_token_secret,
+            )
+            self.session = session
+        else:
+            raise QuickbooksException(
+                "Quickbooks authenication fields not set. Cannot create session.")
+
+        return self.session
+
+    def get_authorize_url(self):
+        """
+        Returns the Authorize URL as returned by QB, and specified by OAuth 1.0a.
+        :return URI:
+        """
+        self.authorize_url = self.authorize_url[:self.authorize_url.find('?')] \
+            if '?' in self.authorize_url else self.authorize_url
+        if self.qbService is None:
+            self.set_up_service()
+
+        response = self.qbService.get_raw_request_token(
+            params={'oauth_callback': self.callback_url})
+
+        oauth_resp = dict(parse_qsl(response.text))
+
+        self.request_token = oauth_resp['oauth_token']
+        self.request_token_secret = oauth_resp['oauth_token_secret']
+        return self.qbService.get_authorize_url(self.request_token)
+
+    def get_current_user(self):
+        """Get data from the current user endpoint"""
+        url = self.current_user_url
+        result = self.make_request("GET", url)
+        return result
+
+    def get_report(self, report_type, qs=None):
+        """Get data from the report endpoint"""
+        if qs is None:
+            qs = {}
+
+        url = self.api_url + "/company/{0}/reports/{1}".format(self.company_id, report_type)
+        result = self.make_request("GET", url, params=qs)
+        return result
+
+    def set_up_service(self):
+        self.qbService = OAuth1Service(
+            name=None,
+            consumer_key=self.consumer_key,
+            consumer_secret=self.consumer_secret,
+            request_token_url=self.request_token_url,
+            access_token_url=self.access_token_url,
+            authorize_url=self.authorize_url,
+            base_url=None
+        )
+
+    def get_access_tokens(self, oauth_verifier):
+        """
+        Wrapper around get_auth_session, returns session, and sets access_token and
+        access_token_secret on the QB Object.
+        :param oauth_verifier: the oauth_verifier as specified by OAuth 1.0a
+        """
+        session = self.qbService.get_auth_session(
+            self.request_token,
+            self.request_token_secret,
+            data={'oauth_verifier': oauth_verifier})
+
+        self.access_token = session.access_token
+        self.access_token_secret = session.access_token_secret
+        return session
+
+    def disconnect_account(self):
+        """
+        Disconnect current account from the application
+        :return:
+        """
+        url = self.disconnect_url
+        result = self.make_request("GET", url)
+        return result
+
+    def change_data_capture(self, entity_string, changed_since):
+        url = self.api_url + "/company/{0}/cdc".format(self.company_id)
+
+        params = {"entities": entity_string, "changedSince": changed_since}
+
+        result = self.make_request("GET", url, params=params)
+        return result
+
+    def reconnect_account(self):
+        """
+        Reconnect current account by refreshing OAuth access tokens
+        :return:
+        """
+        url = self.reconnect_url
+        result = self.make_request("GET", url)
+        return result
+
+    def make_request(self, request_type, url, request_body=None, content_type='application/json',
+                     params=None, file_path=None):
+
+        if not params:
+            params = {}
+
+        if self.minorversion:
+            params['minorversion'] = self.minorversion
+
+        if not request_body:
+            request_body = {}
+
+        if self.session is None:
+            self.create_session()
+        headers = {
+            'Content-Type': content_type,
+            'Accept': 'application/json'
+        }
+
+        if file_path:
+            attachment = open(file_path, 'rb')
+            url = url.replace('attachable', 'upload')
+            boundary = '-------------PythonMultipartPost'
+            headers.update({
+                'Content-Type': 'multipart/form-data; boundary=%s' % boundary,
+                'Accept-Encoding': 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                'User-Agent': 'OAuth gem v0.4.7',
+                'Accept': 'application/json',
+                'Connection': 'close'
+            })
+
+            binary_data = str(base64.b64encode(attachment.read()).decode('ascii'))
+
+            content_type = json.loads(request_body)['ContentType']
+
+            request_body = textwrap.dedent(
+                """
+                --%s
+                Content-Disposition: form-data; name="file_metadata_01"
+                Content-Type: application/json
+
+                %s
+
+                --%s
+                Content-Disposition: form-data; name="file_content_01"
+                Content-Type: %s
+                Content-Transfer-Encoding: base64
+
+                %s
+
+                --%s--
+                """
+            ) % (boundary, request_body, boundary, content_type, binary_data, boundary)
+
+        req = self.session.request(
+            request_type, url, True, self.company_id,
+            headers=headers, params=params, data=request_body)
+
+        if req.status_code == httplib.UNAUTHORIZED:
+            raise AuthorizationException("Application authentication failed", detail=req.text)
+
+        try:
+            result = req.json()
+        except:
+            raise QuickbooksException("Error reading json response: {0}".format(req.text), 10000)
+
+        if "Fault" in result:
+            self.handle_exceptions(result["Fault"])
+        elif not req.status_code == httplib.OK:
+            raise QuickbooksException("Error returned with status code '{0}': {1}".format(
+                req.status_code, req.text), 10000)
+        else:
+            return result
+
+    def get_single_object(self, qbbo, pk):
+        url = self.api_url + "/company/{0}/{1}/{2}/".format(self.company_id, qbbo.lower(), pk)
+        result = self.make_request("GET", url, {})
+
+        return result
+
+    def handle_exceptions(self, results):
+        # Needs to handle multiple errors
+        for error in results["Error"]:
+
+            message = error["Message"]
+
+            detail = ""
+            if "Detail" in error:
+                detail = error["Detail"]
+
+            code = ""
+            if "code" in error:
+                code = int(error["code"])
+
+            if code >= 10000:
+                raise SevereException(message, code, detail)
+            else:
+                raise QuickbooksException(message, code, detail)
+
+    def create_object(self, qbbo, request_body, _file_path=None):
+        self.isvalid_object_name(qbbo)
+
+        url = self.api_url + "/company/{0}/{1}".format(self.company_id, qbbo.lower())
+        results = self.make_request("POST", url, request_body, file_path=_file_path)
+
+        return results
+
+    def query(self, select):
+        url = self.api_url + "/company/{0}/query".format(self.company_id)
+        result = self.make_request("POST", url, select, content_type='application/text')
+
+        return result
+
+    def isvalid_object_name(self, object_name):
+        if object_name not in self._BUSINESS_OBJECTS:
+            raise Exception("{0} is not a valid QBO Business Object.".format(object_name))
+
+        return True
+
+    def update_object(self, qbbo, request_body, _file_path=None):
+        url = self.api_url + "/company/{0}/{1}".format(self.company_id, qbbo.lower())
+        result = self.make_request("POST", url, request_body, file_path=_file_path)
+
+        return result
+
+    def delete_object(self, qbbo, request_body, _file_path=None):
+        url = self.api_url + "/company/{0}/{1}".format(self.company_id, qbbo.lower())
+        result = self.make_request("POST", url, request_body, params={'operation': 'delete'}, file_path=_file_path)
+
+        return result
+
+    def batch_operation(self, request_body):
+        url = self.api_url + "/company/{0}/batch".format(self.company_id)
+        results = self.make_request("POST", url, request_body)
+
+        return results
+
+    def download_pdf(self, qbbo, item_id):
+        url = self.api_url + "/company/{0}/{1}/{2}/pdf".format(
+            self.company_id, qbbo.lower(), item_id)
+
+        if self.session is None:
+            self.create_session()
+
+        headers = {
+            'Content-Type': 'application/pdf',
+            'Accept': 'application/pdf, application/json',
+        }
+
+        response = self.session.request("GET", url, True, self.company_id, headers=headers)
+
+        if response.status_code != httplib.OK:
+            try:
+                result = response.json()
+            except:
+                raise QuickbooksException("Error reading json response: {0}".format(response.text), 10000)
+
+            self.handle_exceptions(result["Fault"])
+        else:
+            return response.content
---
+++ packages/python-quickbooks/python-quickbooks-0.6.1.tar.gz/python-quickbooks-0.6.1/tests/unit/test_client.py
@@ -0,0 +1,342 @@
+import unittest
+
+try:
+    from mock import patch
+except ImportError:
+    from unittest.mock import patch
+
+from quickbooks.exceptions import QuickbooksException, SevereException
+from quickbooks import client
+from quickbooks.objects.salesreceipt import SalesReceipt
+
+
+class ClientTest(unittest.TestCase):
+    def setUp(self):
+        """
+        Use a consistent set of defaults.
+        """
+        client.QuickBooks(
+            sandbox=True,
+            consumer_key="update_consumer_key",
+            consumer_secret="update_consumer_secret",
+            access_token="update_access_token",
+            access_token_secret="update_access_token_secret",
+            company_id="update_company_id",
+            callback_url="update_callback_url"
+        )
+
+    def tearDown(self):
+        client.QuickBooks.enable_global()
+        self.qb_client = client.QuickBooks()
+        self.qb_client._drop()
+
+    def test_client_new(self):
+        self.qb_client = client.QuickBooks(
+            sandbox=False,
+            consumer_key="consumer_key",
+            consumer_secret="consumer_secret",
+            access_token="access_token",
+            access_token_secret="access_token_secret",
+            company_id="company_id",
+            callback_url="callback_url",
+            verbose=True,
+            minorversion=4
+        )
+
+        self.assertEquals(self.qb_client.sandbox, False)
+        self.assertEquals(self.qb_client.consumer_key, "consumer_key")
+        self.assertEquals(self.qb_client.consumer_secret, "consumer_secret")
+        self.assertEquals(self.qb_client.access_token, "access_token")
+        self.assertEquals(self.qb_client.access_token_secret, "access_token_secret")
+        self.assertEquals(self.qb_client.company_id, "company_id")
+        self.assertEquals(self.qb_client.callback_url, "callback_url")
+        self.assertEquals(self.qb_client.minorversion, 4)
+
+    def test_client_updated(self):
+        self.qb_client = client.QuickBooks(
+            sandbox=False,
+            consumer_key="consumer_key",
+            consumer_secret="consumer_secret",
+            access_token="access_token",
+            access_token_secret="access_token_secret",
+            company_id="company_id",
+            callback_url="callback_url",
+        )
+
+        self.qb_client2 = client.QuickBooks(
+            sandbox=True,
+            consumer_key="update_consumer_key",
+            consumer_secret="update_consumer_secret",
+            access_token="update_access_token",
+            access_token_secret="update_access_token_secret",
+            company_id="update_company_id",
+            callback_url="update_callback_url",
+        )
+
+        self.assertEquals(self.qb_client.sandbox, True)
+        self.assertEquals(self.qb_client.consumer_key, "update_consumer_key")
+        self.assertEquals(self.qb_client.consumer_secret, "update_consumer_secret")
+        self.assertEquals(self.qb_client.access_token, "update_access_token")
+        self.assertEquals(self.qb_client.access_token_secret, "update_access_token_secret")
+        self.assertEquals(self.qb_client.company_id, "update_company_id")
+        self.assertEquals(self.qb_client.callback_url, "update_callback_url")
+
+        self.assertEquals(self.qb_client2.sandbox, True)
+        self.assertEquals(self.qb_client2.consumer_key, "update_consumer_key")
+        self.assertEquals(self.qb_client2.consumer_secret, "update_consumer_secret")
+        self.assertEquals(self.qb_client2.access_token, "update_access_token")
+        self.assertEquals(self.qb_client2.access_token_secret, "update_access_token_secret")
+        self.assertEquals(self.qb_client2.company_id, "update_company_id")
+        self.assertEquals(self.qb_client2.callback_url, "update_callback_url")
+
+    def test_disable_global(self):
+        client.QuickBooks.disable_global()
+        self.qb_client = client.QuickBooks()
+
+        self.assertFalse(self.qb_client.sandbox)
+        self.assertFalse(self.qb_client.consumer_key)
+        self.assertFalse(self.qb_client.consumer_secret)
+        self.assertFalse(self.qb_client.access_token)
+        self.assertFalse(self.qb_client.access_token_secret)
+        self.assertFalse(self.qb_client.company_id)
+        self.assertFalse(self.qb_client.callback_url)
+        self.assertFalse(self.qb_client.minorversion)
+
+    def test_api_url(self):
+        qb_client = client.QuickBooks(sandbox=False)
+        api_url = qb_client.api_url
+
+        self.assertFalse("sandbox" in api_url)
+
+    def test_api_url_sandbox(self):
+        qb_client = client.QuickBooks(sandbox=True)
+        api_url = qb_client.api_url
+
+        self.assertTrue("sandbox" in api_url)
+
+    def test_isvalid_object_name_valid(self):
+        qb_client = client.QuickBooks()
+        result = qb_client.isvalid_object_name("Customer")
+
+        self.assertEquals(result, True)
+
+    def test_isvalid_object_name_invalid(self):
+        qb_client = client.QuickBooks()
+
+        self.assertRaises(Exception, qb_client.isvalid_object_name, "invalid")
+
+    @patch('quickbooks.client.QuickBooks.make_request')
+    def test_batch_operation(self, make_req):
+        qb_client = client.QuickBooks()
+        qb_client.batch_operation("request_body")
+
+        self.assertTrue(make_req.called)
+
+    @patch('quickbooks.client.QuickBooks.make_request')
+    def test_create_object(self, make_req):
+        qb_client = client.QuickBooks()
+        qb_client.create_object("Customer", "request_body")
+
+        self.assertTrue(make_req.called)
+
+    @patch('quickbooks.client.QuickBooks.make_request')
+    def test_query(self, make_req):
+        qb_client = client.QuickBooks()
+        qb_client.query("select")
+
+        self.assertTrue(make_req.called)
+
+    @patch('quickbooks.client.QuickBooks.make_request')
+    def test_update_object(self, make_req):
+        qb_client = client.QuickBooks()
+        qb_client.update_object("Customer", "request_body")
+
+        self.assertTrue(make_req.called)
+
+    def test_get_authorize_url(self):
+        qb_client = client.QuickBooks()
+        qb_client.set_up_service()
+
+        with patch.object(qb_client.qbService, "get_raw_request_token",
+                          return_value=MockResponse()):
+
+            results = qb_client.get_authorize_url()
+
+            self.assertTrue('https://appcenter.intuit.com/Connect/Begin' in results)
+            self.assertTrue('oauth_token' in results)
+            self.assertEqual(qb_client.request_token, 'tokenvalue')
+            self.assertTrue(qb_client.request_token_secret, 'secretvalue')
+
+    @patch('quickbooks.client.QuickBooks.make_request')
+    def test_get_current_user(self, make_req):
+        qb_client = client.QuickBooks()
+        qb_client.company_id = "1234"
+
+        qb_client.get_current_user()
+        url = "https://appcenter.intuit.com/api/v1/user/current"
+        make_req.assert_called_with("GET", url)
+
+    @patch('quickbooks.client.QuickBooks.qbService')
+    def test_get_access_tokens(self, qbService):
+        qb_client = client.QuickBooks()
+        qb_client.request_token = "token"
+        qb_client.request_token_secret = "secret"
+        session = qb_client.get_access_tokens("oauth_verifier")
+
+        qbService.get_auth_session.assert_called_with('token', 'secret', data={'oauth_verifier': 'oauth_verifier'})
+        self.assertFalse(session is None)
+
+    @patch('quickbooks.client.QuickBooks.make_request')
+    def test_disconnect_account(self, make_req):
+        qb_client = client.QuickBooks()
+        qb_client.company_id = "1234"
+
+        qb_client.disconnect_account()
+        url = "https://appcenter.intuit.com/api/v1/connection/disconnect"
+        make_req.assert_called_with("GET", url)
+
+    @patch('quickbooks.client.QuickBooks.make_request')
+    def test_reconnect_account(self, make_req):
+        qb_client = client.QuickBooks()
+        qb_client.company_id = "1234"
+
+        qb_client.reconnect_account()
+        url = "https://appcenter.intuit.com/api/v1/connection/reconnect"
+        make_req.assert_called_with("GET", url)
+
+    @patch('quickbooks.client.QuickBooks.make_request')
+    def test_get_report(self, make_req):
+        qb_client = client.QuickBooks()
+        qb_client.company_id = "1234"
+
+        qb_client.get_report("profitandloss", {1: 2})
+        url = "https://sandbox-quickbooks.api.intuit.com/v3/company/1234/reports/profitandloss"
+        make_req.assert_called_with("GET", url, params={1: 2})
+
+    def test_get_instance(self):
+        qb_client = client.QuickBooks()
+
+        instance = qb_client.get_instance()
+        self.assertEquals(qb_client, instance)
+
+    @patch('quickbooks.client.OAuth1Session')
+    def test_create_session(self, auth_Session):
+        qb_client = client.QuickBooks()
+        session = qb_client.create_session()
+
+        self.assertTrue(auth_Session.called)
+        self.assertFalse(session is None)
+
+    def test_create_session_missing_auth_info_exception(self):
+        qb_client = client.QuickBooks()
+        qb_client.consumer_secret = None
+
+        self.assertRaises(QuickbooksException, qb_client.create_session)
+
+    @patch('quickbooks.client.QuickBooks.make_request')
+    def test_get_single_object(self, make_req):
+        qb_client = client.QuickBooks()
+        qb_client.company_id = "1234"
+
+        qb_client.get_single_object("test", 1)
+        url = "https://sandbox-quickbooks.api.intuit.com/v3/company/1234/test/1/"
+        make_req.assert_called_with("GET", url, {})
+
+    @patch('quickbooks.client.QuickBooks.session')
+    def test_make_request(self, qb_session):
+        qb_session.request.return_value = MockResponse()
+
+        qb_client = client.QuickBooks()
+        qb_client.company_id = "1234"
+        url = "https://sandbox-quickbooks.api.intuit.com/v3/company/1234/test/1/"
+        qb_client.make_request("GET", url, request_body=None, content_type='application/json')
+
+        qb_session.request.assert_called_with(
+                "GET", url, True, "1234", data={},
+                headers={'Content-Type': 'application/json', 'Accept': 'application/json'}, params={})
+
+    def test_make_request_create_session(self):
+        receipt = SalesReceipt()
+        receipt.Id = 1
+        self.assertRaises(QuickbooksException, receipt.save)
+
+    def test_handle_exceptions(self):
+        qb_client = client.QuickBooks()
+        error_data = {
+            "Error": [{
+                "Message": "message",
+                "Detail": "detail",
+                "code": "2030",
+                "element": "Id"}],
+            "type": "ValidationFault"
+        }
+
+        self.assertRaises(QuickbooksException, qb_client.handle_exceptions, error_data)
+
+    def test_handle_exceptions_severe(self):
+        qb_client = client.QuickBooks()
+        error_data = {
+            "Error": [{
+                "Message": "message",
+                "Detail": "detail",
+                "code": "10001",
+                "element": "Id"}],
+            "type": "ValidationFault"
+        }
+
+        self.assertRaises(SevereException, qb_client.handle_exceptions, error_data)
+
+    @patch('quickbooks.client.QuickBooks.session')
+    def test_download_pdf(self, qb_session):
+        qb_client = client.QuickBooks(sandbox=True)
+        qb_client.company_id = "1234"
+        receipt = SalesReceipt()
+        receipt.Id = 1
+
+        receipt.download_pdf(qb_client)
+
+        url = "https://sandbox-quickbooks.api.intuit.com/v3/company/1234/salesreceipt/1/pdf"
+        qb_session.request.assert_called_with(
+            "GET", url, True, "1234",
+            headers={'Content-Type': 'application/pdf', 'Accept': 'application/pdf, application/json'})
+
+        qb_session.request.return_value = MockPdfResponse()
+        response = receipt.download_pdf(qb_client)
+
+        self.assertEqual(response, 'sample pdf content')
+
+    def test_download_nonexistent_pdf(self):
+        receipt = SalesReceipt()
+        receipt.Id = 666
+        self.assertRaises(QuickbooksException, receipt.download_pdf)
+
+
+class MockResponse(object):
+    @property
+    def text(self):
+        return "oauth_token_secret=secretvalue&oauth_callback_confirmed=true&oauth_token=tokenvalue"
+
+    @property
+    def status_code(self):
+        try:
+            import httplib  # python 2
+        except ImportError:
+            import http.client as httplib  # python 3
+        return httplib.OK
+
+    def json(self):
+        return "{}"
+
+
+class MockPdfResponse(object):
+    @property
+    def status_code(self):
+        try:
+            import httplib  # python 2
+        except ImportError:
+            import http.client as httplib  # python 3
+        return httplib.OK
+
+    @property
+    def content(self):
+        return "sample pdf content"
