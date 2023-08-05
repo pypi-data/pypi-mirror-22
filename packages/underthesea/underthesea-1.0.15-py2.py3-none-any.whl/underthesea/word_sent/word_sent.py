72c5f5665676aa2040fccd8f2d7abe26e61bdf42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:27 +0000

    Add package underthesea/1.0.15/underthesea-1.0.15-py2.py3-none-any.whl

---
+++ packages/underthesea/underthesea-1.0.15-py2.py3-none-any.whl/underthesea/word_sent/word_sent.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+from underthesea.word_sent.model import CRFModel
+from tokenize import tokenize
+
+
+def word_sent(sentence, format=None):
+    """
+    :param unicode|str sentence: raw sentence
+    :return: segmented sentence
+    :rtype: unicode|str
+    """
+    sentence = tokenize(sentence)
+    crf_model = CRFModel()
+    return crf_model.predict(sentence, format)
