06278f7725f750eb8b2e8bddf8b1f389040fbdf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:47 +0000

    Add package nesterwh-py/1.0.1/nesterwh_py-1.0.1.tar.gz

---
+++ packages/nesterwh-py/nesterwh_py-1.0.1.tar.gz/nesterwh_py-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterwh_py',
+    version = '1.0.1',
+    py_modules = ['nester'],
+    author = 'ccwc1',
+    author_email = 'ccwc3@163.com',
+    url = 'http://www.headerfirsttables.com',
+    description = 'A simple printer of nested lists',
+    )
