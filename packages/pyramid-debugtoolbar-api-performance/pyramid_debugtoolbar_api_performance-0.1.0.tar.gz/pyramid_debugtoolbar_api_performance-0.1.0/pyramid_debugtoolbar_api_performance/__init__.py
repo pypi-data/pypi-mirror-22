232741b291336305dc53422509c4099fd8b2419a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:50 +0000

    Add package pyramid-debugtoolbar-api-performance/0.1.0/pyramid_debugtoolbar_api_performance-0.1.0.tar.gz

---
+++ packages/pyramid-debugtoolbar-api-performance/pyramid_debugtoolbar_api_performance-0.1.0.tar.gz/pyramid_debugtoolbar_api_performance-0.1.0/pyramid_debugtoolbar_api_performance/__init__.py
@@ -0,0 +1,16 @@
+# local
+from .panels import PerformanceCsvDebugPanel
+
+
+# ==============================================================================
+
+
+def includeme(config):
+    config.registry.settings['debugtoolbar.extra_panels'].append(PerformanceCsvDebugPanel)
+    config.add_route('debugtoolbar.api_performance_csv.function_calls', '/api-performance/function_calls-{request_id}.csv')
+    config.add_route('debugtoolbar.api_performance_csv.timing', '/api-performance/timing-{request_id}.csv')
+    config.scan('pyramid_debugtoolbar_api_performance.views')
+    config.commit()
+
+
+# ==============================================================================
