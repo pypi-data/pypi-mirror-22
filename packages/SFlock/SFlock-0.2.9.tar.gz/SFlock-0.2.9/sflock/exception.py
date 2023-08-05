c1c6addfdae2409b08f5be2f10caad27488aabad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package SFlock/0.2.9/SFlock-0.2.9.tar.gz

---
+++ packages/SFlock/SFlock-0.2.9.tar.gz/SFlock-0.2.9/sflock/exception.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2015-2016 Jurriaan Bremer.
+# This file is part of SFlock - http://www.sflock.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+class UnpackException(Exception):
+    pass
