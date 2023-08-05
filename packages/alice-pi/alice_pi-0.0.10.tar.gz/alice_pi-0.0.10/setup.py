75542a143f8103a8dae02cd29b140dfcdc31db6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package alice-pi/0.0.10/alice_pi-0.0.10.tar.gz

---
+++ packages/alice-pi/alice_pi-0.0.10.tar.gz/alice_pi-0.0.10/setup.py
@@ -0,0 +1,92 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+import os
+
+
+setup(
+    name='alice_pi',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.0.10',
+
+    description=('A package for running various home-automation' +
+                 'services on raspberry pi.'),
+    long_description=('This package is designed to run many sensor monitoring' +
+                      ' and data pushing events asynchronously.  It is' +
+                      ' inspired by and made to integrate with Home Assistant'),
+
+    # The project's main homepage.
+    url='https://github.com/jlmcgehee21/alice_pi',
+
+    # Author details
+    author='Jeff Mcgehee',
+    author_email='jlmcgehee21@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='automation sensors raspberrypi',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+
+    install_requires=['pyyaml',
+                      'requests',
+                      'rpi.gpio',
+                      'w1thermsensor'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': [],
+        'test': ['nose'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={
+        # 'sample': ['package_data.dat'],
+    # },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[(os.path.expanduser('~'),
+                ['alice_pi/data/alice_pi_config.yml.example'])],
+
+    include_package_data=True,
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'alice_pi = alice_pi.bin.run_alice:main',
+        ],
+    },
+)
