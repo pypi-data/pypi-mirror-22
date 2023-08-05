63a3392a7140314d1c912f25524d006ca0afedea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:12 +0000

    Add package yamlcf/0.3.4/yamlcf-0.3.4.tar.gz

---
+++ packages/yamlcf/yamlcf-0.3.4.tar.gz/yamlcf-0.3.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='yamlcf',      # This is the name of your PyPI-package.
+    version='0.3.4',      # Update the version number for new releases
+    scripts=['yamlcf.py'],  # The name of your script, and also the command you'll be using for calling it
+    url='https://github.com/komoot/yamlcf',
+    author='Jan Heuer',
+    author_email='jan@komoot.de',
+    install_requires=[
+        'pyyaml',
+        'botocore'
+    ]
+)
