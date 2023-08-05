53372f84f9479e4a9fd2eaeef397787ed0796abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:24 +0000

    Add package dolfyn/0.7.0/dolfyn-0.7.0.tar.gz

---
+++ packages/dolfyn/dolfyn-0.7.0.tar.gz/dolfyn-0.7.0/dolfyn/adp/api.py
@@ -0,0 +1,19 @@
+from .base import type_map, dio
+from ._readbin import adcp_loader
+from .rotate import beam2inst, inst2earth, earth2principal
+
+
+def load(fname, data_groups=None):
+    with dio.loader(fname, type_map) as ldr:
+        return ldr.load(data_groups)
+
+
+def mmload(fname, data_groups=None):
+    with dio.loader(fname, type_map) as ldr:
+        return ldr.mmload(data_groups)
+
+
+def read_rdi(fname, nens=None):
+    with adcp_loader(fname) as ldr:
+        dat = ldr.load_data(nens=nens)
+    return dat
