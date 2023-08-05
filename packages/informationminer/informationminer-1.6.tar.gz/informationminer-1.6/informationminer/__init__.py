b6dda86a4bcdc393d334141801ce0899500978a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:24 +0000

    Add package informationminer/1.6/informationminer-1.6.tar.gz

---
+++ packages/informationminer/informationminer-1.6.tar.gz/informationminer-1.6/informationminer/__init__.py
@@ -0,0 +1,8 @@
+"""
+Use InformationMiner and pass it some text. 
+The ClassifierBasedGermanTagger can be used for POS-Tagging on the German language.
+"""
+from .InformationMiner import InformationMiner
+from .ClassifierBasedGermanTagger import ClassifierBasedGermanTagger
+from .POSTagger import tag
+__version__ = '1.6'
