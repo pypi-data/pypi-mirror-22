3de647cb61d99d75d1eda3541032ca286de69efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:53 +0000

    Add package django-passbook-an/0.2.0/django-passbook-an-0.2.0.zip

---
+++ packages/django-passbook-an/django-passbook-an-0.2.0.zip/django-passbook-an-0.2.0/django_passbook/__init__.py
@@ -0,0 +1,7 @@
+VERSION = ('0', '2', '0')
+
+
+def get_version(*args, **kwargs):
+    return '.'.join(VERSION)
+
+__version__ = get_version()
---
+++ packages/django-passbook-an/django-passbook-an-0.2.0.zip/django-passbook-an-0.2.0/django_passbook/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Pass, Registration, Log
+import settings
+from apns import APNs, Payload
+
+
+def push_update(modeladmin, request, queryset):
+    for r in queryset.all():
+        # FIXME: use different certificates for different stores
+        apns = APNs(use_sandbox=False,
+                    cert_file=settings.PASSBOOK_CERT,
+                    key_file=settings.PASSBOOK_CERT_KEY)
+        apns.gateway_server.send_notification(r.push_token, Payload())
+
+push_update.short_description = "Send a push notification to update Pass"
+
+
+class RegistrationAdmin(admin.ModelAdmin):
+    actions = [push_update]
+
+admin.site.register(Pass)
+admin.site.register(Registration, RegistrationAdmin)
+admin.site.register(Log)
