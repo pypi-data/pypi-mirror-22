92d0fd5fe2eb6d8c7b97c8fb26e03c3a31adee61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:59 +0000

    Add package Missing/4.0/Missing-4.0.tar.gz

---
+++ packages/Missing/Missing-4.0.tar.gz/Missing-4.0/setup.py
@@ -0,0 +1,52 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+README = open('README.rst').read()
+CHANGES = open('CHANGES.rst').read()
+
+__version__ = '4.0'
+
+setup(
+    name='Missing',
+    version=__version__,
+    url='http://pypi.python.org/pypi/Missing',
+    license='ZPL 2.1',
+    description="Special Missing objects used in Zope2.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description='\n\n'.join([README, CHANGES]),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    install_requires=['ExtensionClass >= 4.1a1'],
+    include_package_data=True,
+    zip_safe=False,
+)
