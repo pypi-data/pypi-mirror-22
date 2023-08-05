dd6362edf4b21d53bc4e671b8e4a133abf607841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:38 +0000

    Add package wombat_foo/2.12/wombat_foo-2.12.tar.gz

---
+++ packages/wombat_foo/wombat_foo-2.12.tar.gz/wombat_foo-2.12/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'wombat_foo',
+    packages = ['wombat_foo'],
+    version = '2.12',
+
+    author = 'wombatwen',
+    author_email = 'wombatwen@gmail.com',
+    scripts = ['wombat_foo/wombat_run']
+)
