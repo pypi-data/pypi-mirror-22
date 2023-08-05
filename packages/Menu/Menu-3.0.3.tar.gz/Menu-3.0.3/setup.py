fda54166ff37d4c20648f091c5a7715774b540e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package Menu/3.0.3/Menu-3.0.3.tar.gz

---
+++ packages/Menu/Menu-3.0.3.tar.gz/Menu-3.0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Menu',
+    version='3.0.3',
+    author='Dylan Richardson',
+    author_email='dylanrichardson1996@gmail.com',
+    packages=['menu'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/Menu/',
+    license='LICENSE.txt',
+    description='Easily create command-line menus',
+    long_description=open('README.md').read(),
+)
