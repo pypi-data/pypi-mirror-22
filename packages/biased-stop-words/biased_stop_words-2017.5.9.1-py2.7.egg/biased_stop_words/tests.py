2d151a96e3ba635ffe4f511da12443348f04aeaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:51 +0000

    Add package biased-stop-words/2017.5.9.1/biased_stop_words-2017.5.9.1-py2.7.egg

---
+++ packages/biased-stop-words/biased_stop_words-2017.5.9.1-py2.7.egg/biased_stop_words/tests.py
@@ -0,0 +1,24 @@
+"""
+Tests for biased-stop-words
+"""
+import random
+import unittest
+from types import ListType
+
+from biased_stop_words import get_stop_words, BiasedStopWordError
+
+
+class TestBiasedStopWords(unittest.TestCase):
+
+    def test_get_existing_stop_words(self):
+        import ipdb; ipdb.set_trace()
+        actual = get_stop_words('en-gendered')
+        self.assertTrue(type(actual) is ListType)
+        self.assertTrue(len(actual) > 0)
+
+    def test_get_existing_stop_words(self):
+        self.assertRaises(BiasedStopWordError, get_stop_words, 'non-existant-genre')
+
+
+if __name__ == '__main__':
+    unittest.main()
