38abfbf2f480ec25042f58af05a5cd9b013417b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:20 +0000

    Add package infinispan/1.0.0b2/infinispan-1.0.0b2.tar.gz

---
+++ packages/infinispan/infinispan-1.0.0b2.tar.gz/infinispan-1.0.0b2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+version = "1.0.0b2"
+
+setup(name = 'infinispan',
+      version = version,
+      description = 'Native python client for Infinispan, over the Hot Rod wire protocol',
+      author = 'Galder Zamarreno',
+      author_email = 'galder@jboss.org',
+      url = 'http://infinispan.org',
+      py_modules = ['infinispan.remotecache', 'infinispan.unsigned'],
+      classifiers = [
+          "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+          "Programming Language :: Python",
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords = 'infinispan hotrod nosql datagrid',
+      license = 'LGPL',
+      install_requires = [
+        'setuptools',
+        'greenlet',
+      ],
+      long_description = "See `Infinispan Native Python Client <https://github.com/infinispan/python-client>`_ for more information."
+      )
