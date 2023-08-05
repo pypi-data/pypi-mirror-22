5e30cc23eea432db21bdf04bb3385060b712e8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:41 +0000

    Add package tdda/0.3.9/tdda-0.3.9-py3-none-any.whl

---
+++ packages/tdda/tdda-0.3.9-py3-none-any.whl/tdda/rexpy/__init__.py
@@ -0,0 +1,11 @@
+"""
+Rexpy infers regular expressions on a line-by-line basis from text data
+examples.
+
+To run the rexpy tool::
+
+    python -m tdda.rexpy.rexpy [inputfile]
+
+"""
+
+from tdda.rexpy.rexpy import *
