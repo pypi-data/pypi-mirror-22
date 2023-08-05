c16b910b203ad8ed639e14e8905bdc6af010fe27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:35 +0000

    Add package spectropy/0.1/spectropy-0.1.tar.gz

---
+++ packages/spectropy/spectropy-0.1.tar.gz/spectropy-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='spectropy',
+    version='0.1',
+    packages=['spectropy'],
+    url='http://github.com/nmearl/spectropy',
+    license='MIT',
+    author='Nicholas Earl',
+    author_email='contact@nicholasearl.me',
+    description='Spectroscopy tools in Python',
+    keywords=['astronomy', 'spectroscopy', 'spectroscopy reduction',
+              'spectroscopic analysis']
+)
