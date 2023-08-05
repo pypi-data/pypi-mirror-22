18e76a7c24477461240cb0a3cd446cb888d96831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:48 +0000

    Add package deep-nest/0.14/deep_nest-0.14.tar.gz

---
+++ packages/deep-nest/deep_nest-0.14.tar.gz/deep_nest-0.14/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='deep_nest',
+      version='0.14',
+      description='A sane approach to arbitrarily nested dictionaries (*cough* nested JSON *cough*)',
+      url='https://github.com/lruhlen/deep_nest',
+      author='L. Ruhlen',
+      author_email='ruhlen@gmail.com',
+      license='MIT',
+      packages=['deep_nest'],
+      install_requires=[],
+      zip_safe=False)
