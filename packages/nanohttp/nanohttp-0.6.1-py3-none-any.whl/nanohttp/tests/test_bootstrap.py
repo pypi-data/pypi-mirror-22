b683c282b8392d0b709c2b826527f5f1fef3cb74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:56 +0000

    Add package nanohttp/0.6.1/nanohttp-0.6.1-py3-none-any.whl

---
+++ packages/nanohttp/nanohttp-0.6.1-py3-none-any.whl/nanohttp/tests/test_bootstrap.py
@@ -0,0 +1,31 @@
+
+import unittest
+from os.path import join
+
+# noinspection PyProtectedMember
+from nanohttp.helpers import load_controller_from_file
+from nanohttp.tests.helpers import TEST_DIR
+
+
+class BootstrapTestCase(unittest.TestCase):
+
+    def test_load_controller_from_file(self):
+        controller = load_controller_from_file(':Static')
+        self.assertIsNotNone(controller)
+        self.assertTrue(hasattr(controller, 'load_app'))
+
+        controller = load_controller_from_file(join(TEST_DIR, 'stuff', 'package_for_test_bootstrapping'))
+        self.assertIsNotNone(controller)
+        self.assertTrue(hasattr(controller, 'load_app'))
+
+        controller = load_controller_from_file(join(TEST_DIR, 'stuff', 'module_for_test_bootstrapping.py'))
+        self.assertIsNotNone(controller)
+        self.assertTrue(hasattr(controller, 'load_app'))
+
+        controller = load_controller_from_file(join(TEST_DIR, 'stuff', 'module_for_test_bootstrapping'))
+        self.assertIsNotNone(controller)
+        self.assertTrue(hasattr(controller, 'load_app'))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
