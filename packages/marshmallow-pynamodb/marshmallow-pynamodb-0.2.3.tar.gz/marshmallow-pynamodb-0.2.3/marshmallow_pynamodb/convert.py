b986a841e60ddb4a1d18b4a2f920c8da3d95ea7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:10 +0000

    Add package marshmallow-pynamodb/0.2.3/marshmallow-pynamodb-0.2.3.tar.gz

---
+++ packages/marshmallow-pynamodb/marshmallow-pynamodb-0.2.3.tar.gz/marshmallow-pynamodb-0.2.3/marshmallow_pynamodb/convert.py
@@ -0,0 +1,29 @@
+from marshmallow import fields
+
+from pynamodb import attributes
+
+PYNAMODB_TYPE_MAPPING = {
+    attributes.NumberAttribute: fields.Decimal,
+    attributes.JSONAttribute: fields.Raw,
+    attributes.UnicodeAttribute: fields.String,
+    attributes.BooleanAttribute: fields.Boolean,
+    attributes.UTCDateTimeAttribute: fields.DateTime,
+    attributes.MapAttributeMeta: {True: fields.Nested,
+                                  False: fields.Dict},
+    attributes.ListAttribute: {True: fields.List,
+                               False: fields.Raw}
+}
+
+
+def attribute2field(attribute, validate_collections):
+    try:
+        pynamodb_type = type(attribute)
+        field = PYNAMODB_TYPE_MAPPING[pynamodb_type]
+    except KeyError:
+        pynamodb_type = type(pynamodb_type)
+        field = PYNAMODB_TYPE_MAPPING[pynamodb_type]
+
+    if isinstance(attribute, attributes.ListAttribute) or isinstance(attribute, attributes.MapAttribute):
+        return field[validate_collections]
+    else:
+        return field
