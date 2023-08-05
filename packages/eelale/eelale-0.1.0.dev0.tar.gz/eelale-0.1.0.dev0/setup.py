735f5f57edd2248780761902b3c0f6b20c34dcd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:32 +0000

    Add package eelale/0.1.0.dev0/eelale-0.1.0.dev0.tar.gz

---
+++ packages/eelale/eelale-0.1.0.dev0.tar.gz/eelale-0.1.0.dev0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    description = f.read()
+
+setup(
+    name='eelale',
+    url='http://github.com/emulbreh/eelale/',
+    version='0.1.0-dev',
+    packages=find_packages(),
+    license='BSD License',
+    author='',
+    maintainer='Johannes Dollinger',
+    maintainer_email='emulbreh@googlemail.com',
+    description='cross-compiles Python wheels',
+    long_description=description,
+    include_package_data=True,
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'eelale = eelale.cli:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
+    ]
+)
