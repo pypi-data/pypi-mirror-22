e5a20e7a4e952ce313a6001b55af210390be6ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:01 +0000

    Add package ppt-time-referee/0.7.2/ppt-time-referee-0.7.2.tar.gz

---
+++ packages/ppt-time-referee/ppt-time-referee-0.7.2.tar.gz/ppt-time-referee-0.7.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name="ppt-time-referee",
+    version='0.7.2',
+    packages=['time_referee'],
+    py_modules=['referee'],
+    description='Simple time-awareness tool for PowerPoint presentations',
+    author="Troy Larson",
+    author_email='troylar@gmail.com',
+    install_requires=[
+        'Click','arrow'
+    ],
+    entry_points='''
+        [console_scripts]
+        ppt-ref=referee:cli
+    ''',
+)
