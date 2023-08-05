d101400ac9ca094dc5ad45feef4d53e0227e1026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:05 +0000

    Add package chattie/0.0.2/chattie-0.0.2.tar.gz

---
+++ packages/chattie/chattie-0.0.2.tar.gz/chattie-0.0.2/setup.py
@@ -0,0 +1,50 @@
+import os
+import re
+from setuptools import setup
+from setuptools import find_packages
+
+
+chattiefile = os.path.join(os.path.dirname(__file__),
+                          'src', 'chattie', '__init__.py')
+
+# Thanks to SQLAlchemy:
+# https://github.com/zzzeek/sqlalchemy/blob/master/setup.py#L104
+with open(chattiefile) as stream:
+    __version__ = re.compile(
+        r".*__version__ = '(.*?)'", re.S
+    ).match(stream.read()).group(1)
+
+
+setup(
+    name='chattie',
+    description='A framework for making bots in Python. Inspired by Hubot',
+    version=__version__,
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    author="""
+Mathew Robinson <mrobinson@praelatus.io>
+    """,
+    download_url='https://github.com/chasinglogic/Chattie/releases',
+    install_requires=[
+        'python-telegram-bot',
+        'requests',
+        'click'
+    ],
+    entry_points={
+        'console_scripts': [
+            'chattie = chattie.cli:chattie'
+        ],
+        'chattie.plugins.tricks': [
+            'default_tricks = chattie.tricks',
+        ],
+        'chattie.plugins.connectors': [
+            'telegram_connector = chattie.connectors.telegram',
+            'terminal_connector = chattie.connectors.term'
+        ]
+    },
+    license='Apache2.0',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3'
+    ]
+)
