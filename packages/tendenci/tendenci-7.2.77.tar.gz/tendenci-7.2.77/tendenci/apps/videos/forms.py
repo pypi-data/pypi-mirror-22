78b2fa9d81db60c1643f5807066a8c30293a0692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:43 +0000

    Add package tendenci/7.2.77/tendenci-7.2.77.tar.gz

---
+++ packages/tendenci/tendenci-7.2.77.tar.gz/tendenci-7.2.77/tendenci/apps/videos/forms.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+from django import forms
+from django.contrib.admin import widgets
+from django.utils.translation import ugettext_lazy as _
+
+from tendenci.apps.videos.models import Video
+from tendenci.libs.tinymce.widgets import TinyMCE
+from tendenci.apps.perms.forms import TendenciBaseForm
+from embedly import Embedly
+
+# Create Embedly instance
+client = Embedly("438be524153e11e18f884040d3dc5c07")
+
+class VideoForm(TendenciBaseForm):
+    release_dt = forms.DateTimeField(label=_('Release Date/Time'),
+                                     initial=datetime.now())
+    description = forms.CharField(required=False,
+        widget=TinyMCE(attrs={'style':'width:100%'}, 
+        mce_attrs={'storme_app_label':Video._meta.app_label, 
+        'storme_model':Video._meta.model_name.lower()}))
+
+    status_detail = forms.ChoiceField(choices=(('active','Active'),('pending','Pending')))
+
+    clear_image = forms.BooleanField(required=False)
+
+    class Meta:
+        model = Video
+        fields = (
+            'title',
+            'slug',
+            'category',
+            'video_type',
+            'image',
+            'video_url',
+            'tags',
+            'description',
+            'release_dt',
+            'allow_anonymous_view',
+            'user_perms',
+            'group_perms',
+            'member_perms',
+            'status_detail',
+        )
+
+    def __init__(self, *args, **kwargs): 
+        super(VideoForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['description'].widget.mce_attrs['app_instance_id'] = self.instance.pk
+        else:
+            self.fields['description'].widget.mce_attrs['app_instance_id'] = 0
+        self.fields['release_dt'].widget = widgets.AdminSplitDateTime()
+            
+    def clean_video_url(self):
+        value = self.cleaned_data.get('video_url')
+        # Get embedded object from URL
+        obj = client.oembed(value)
+        if not value:
+            raise forms.ValidationError('You must enter a URL')
+        if obj.get('error'):
+            raise forms.ValidationError('This url is not supported by embed.ly')
+        return value
+
+    def save(self, *args, **kwargs):
+        video = super(VideoForm, self).save(*args, **kwargs)
+        if self.cleaned_data['clear_image']:
+            video.image.delete()
+        return video
