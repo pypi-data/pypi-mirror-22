6e90a2b67f126f4468ef37bd5dd78648fbb3018b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:32 +0000

    Add package wztnester/1.0/wztnester-1.0.tar.gz

---
+++ packages/wztnester/wztnester-1.0.tar.gz/wztnester-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'wztnester',
+        version = '1.0',
+        py_modules = ['wztnester'],
+        author = 'wzt',
+        author_email = 'linewu@yeah.net',
+        url = 'http://www.baidu.com',
+        description = 'wztnester distribute tester'
+    )
