b0b45f91bb8fdee9be481753155a9c04c8cec67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:40 +0000

    Add package dojo-google/0.0.12/dojo-google-0.0.12.tar.gz

---
+++ packages/dojo-google/dojo-google-0.0.12.tar.gz/dojo-google-0.0.12/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='dojo-google',
+    version='0.0.12',
+    description='Dojo transforms using Google APIs.',
+    author='Steven Normore',
+    author_email='steven@dataup.io',
+    url='https://dojo.dataup.io/',
+    packages=['dojo_google', ],
+    install_requires=[
+        'dojo',
+        'google-api-python-client',
+        'google-cloud-dataflow',
+        'retrying',
+        'html'
+    ]
+)
