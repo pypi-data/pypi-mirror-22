ab73dddaa464af6877d18c611c2305b2c1e29b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package nester_howarddeng/1.6.0/nester_howarddeng-1.6.0.zip

---
+++ packages/nester_howarddeng/nester_howarddeng-1.6.0.zip/nester_howarddeng-1.6.0/nester.py
@@ -0,0 +1,11 @@
+"""the standard way to include mutiple line comment in your code"""
+import sys
+def print_lol(mm,ident=False,level=-1,fh=sys.stdout) :
+    if isinstance(mm,list) :
+        for each_item in mm :
+            print_lol(each_item,ident,level+1,fh)
+    else :
+        if (ident==True) :
+            for num in range(level) :
+                print("\t",end='',file=fh)
+        print(mm,file=fh)
