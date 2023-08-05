f01ba1cf2cfaf1396bcdfc7e053d4bf59c82273f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:22 +0000

    Add package pytest-vcr/0.1.0/pytest_vcr-0.1.0-py2.py3-none-any.whl

---
+++ packages/pytest-vcr/pytest_vcr-0.1.0-py2.py3-none-any.whl/pytest_vcr.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import os
+import pytest
+from vcr import VCR
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('vcr')
+    group.addoption(
+        '--vcr-record-mode',
+        action='store',
+        dest='vcr_record_mode',
+        default=None,
+        choices=['once', 'new_episodes', 'none', 'all'],
+        help='Set the recording mode for VCR.py.'
+    )
+
+
+@pytest.fixture(autouse=True)
+def _vcr_marker(request):
+    marker = request.node.get_marker('vcr')
+    if marker:
+        request.getfixturevalue('vcr_cassette')
+
+
+@pytest.yield_fixture
+def vcr_cassette(request, vcr_config, vcr_cassette_path):
+    """Wrap a test in a VCR.py cassette"""
+    kwargs = dict(
+        path_transformer=VCR.ensure_suffix(".yaml"),
+    )
+    marker = request.node.get_marker('vcr')
+    record_mode = request.config.getoption('--vcr-record-mode')
+
+    kwargs.update(vcr_config)
+    if marker:
+        kwargs.update(marker.kwargs)
+    if record_mode:
+        kwargs['record_mode'] = record_mode
+
+    vcr = VCR(**kwargs)
+    with vcr.use_cassette(vcr_cassette_path) as cassette:
+        yield cassette
+
+
+@pytest.fixture
+def vcr_cassette_name(request):
+    """Name of the VCR cassette"""
+    f = request.function
+    if hasattr(f, '__self__'):
+        return f.__self__.__class__.__name__ + '.' + f.__name__
+    return f.__name__
+
+
+@pytest.fixture
+def vcr_cassette_path(request, vcr_cassette_name):
+    test_dir = request.node.fspath.dirname
+    return os.path.join(test_dir, '_cassettes', vcr_cassette_name)
+
+
+@pytest.fixture
+def vcr_config():
+    """Custom configuration for VCR.py"""
+    return {}
