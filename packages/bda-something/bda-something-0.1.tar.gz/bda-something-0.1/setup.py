04aa910b585f19a03f9a3a882b9dc4c994d7e22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:19 +0000

    Add package bda-something/0.1/bda-something-0.1.tar.gz

---
+++ packages/bda-something/bda-something-0.1.tar.gz/bda-something-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bda-something',
+      version='0.1',
+      description='Test for pypi upload',
+      url='http://some.url',
+      author='Some Author',
+      author_email='author@email.com',
+      license='MIT',
+      packages=['something'],
+      zip_safe=False)
