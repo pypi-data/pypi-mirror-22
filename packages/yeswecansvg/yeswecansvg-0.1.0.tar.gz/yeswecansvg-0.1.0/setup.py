69c89628cd222ab24f3720ab19402da232b0f2f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:00 +0000

    Add package yeswecansvg/0.1.0/yeswecansvg-0.1.0.tar.gz

---
+++ packages/yeswecansvg/yeswecansvg-0.1.0.tar.gz/yeswecansvg-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='yeswecansvg',
+    version='0.1.0',
+    description='Clean your SVGs so Illustrator won\'t tell you it CANT.',
+    author='Armand Emamdjomeh',
+    author_email='armand.emamdjomeh@washpost.com',
+    url='http://github.com/emamd/yeswecansvg',
+    download_url='http://github.com/emamd/yeswecansvg.git',
+    packages=("yeswecansvg",),
+    # cmdclass={'test': TestCommand},
+    # install_requires=('six>=1.4.1'),
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    entry_points={
+        'console_scripts': [
+            'yeswecansvg=yeswecansvg.yeswecansvg:main',
+        ],
+    },
+)
