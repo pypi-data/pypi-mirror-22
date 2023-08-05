4570fc14d9689c74652140513a045d36496d5cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:45 +0000

    Add package cdb-api/3.3.0.dev0/cdb-api-3.3.0.dev0.tar.gz

---
+++ packages/cdb-api/cdb-api-3.3.0.dev0.tar.gz/cdb-api-3.3.0.dev0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+from setuptools import setup
+import os
+
+projectRootPath = os.environ['CDB_ROOT_DIR']
+projectVersionPath = 'etc/version'
+fullVersionPath = '%s/%s' % (projectRootPath, projectVersionPath)
+
+versionFile = open(fullVersionPath)
+version = versionFile.read()
+
+setup(name='cdb-api',
+      version=version,
+      license='Copyright (c) UChicago Argonne, LLC. All rights reserved.',
+      packages=['cdb.cdb_web_service.api',
+                'cdb.common.utility',
+                'cdb.common.exceptions',
+                'cdb.common.objects'],
+      description='Python APIs used to communicate with Component Database',
+      maintainer='Dariusz Jarosz',
+      maintainer_email='djarosz@aps.anl.gov',
+      url='https://github.com/AdvancedPhotonSource/ComponentDB'
+      )
