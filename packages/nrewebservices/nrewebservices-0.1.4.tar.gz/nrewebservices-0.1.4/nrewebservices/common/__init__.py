be05875a516e2f59f449a8b9e7697a3f55925c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:56 +0000

    Add package nrewebservices/0.1.4/nrewebservices-0.1.4.tar.gz

---
+++ packages/nrewebservices/nrewebservices-0.1.4.tar.gz/nrewebservices-0.1.4/nrewebservices/common/__init__.py
@@ -0,0 +1,8 @@
+# Module init file.
+
+from .soap import SoapResponseObject
+
+from .soap import make_boolean_mapper
+from .soap import make_simple_mapper
+from .soap import make_stripped_text_mapper
+
