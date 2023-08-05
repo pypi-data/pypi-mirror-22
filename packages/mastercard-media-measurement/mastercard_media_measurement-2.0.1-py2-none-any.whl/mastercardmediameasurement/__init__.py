1654962e364c0f0f774a9c46727e51ad037a18d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:16 +0000

    Add package mastercard-media-measurement/2.0.1/mastercard_media_measurement-2.0.1-py2-none-any.whl

---
+++ packages/mastercard-media-measurement/mastercard_media_measurement-2.0.1-py2-none-any.whl/mastercardmediameasurement/__init__.py
@@ -0,0 +1,9 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardmediameasurement.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardmediameasurement.mediameasurements import *
+from mastercardmediameasurement.subscriptions import *
---
+++ packages/mastercard-media-measurement/mastercard_media_measurement-2.0.1-py2-none-any.whl/mastercardmediameasurement/mediameasurements.py
@@ -0,0 +1,72 @@
+#
+# Copyright (c) 2016 MasterCard International Incorporated
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are
+# permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list of
+# conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice, this list of
+# conditions and the following disclaimer in the documentation and/or other materials
+# provided with the distribution.
+# Neither the name of the MasterCard International Incorporated nor the names of its
+# contributors may be used to endorse or promote products derived from this software
+# without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+
+from mastercardapicore import BaseObject
+from mastercardapicore import RequestMap
+from mastercardapicore import OperationConfig
+from mastercardapicore import OperationMetadata
+from resourceconfig import ResourceConfig
+
+class MediaMeasurements(BaseObject):
+	"""
+	
+	"""
+
+	__config = {
+		
+		"ce162846-ee2f-4317-b8e6-d2b78e60b977" : OperationConfig("/mediameasurementv2/v2/mediameasurement/mediameasurements", "create", [], []),
+		
+	}
+
+	def getOperationConfig(self,operationUUID):
+		if operationUUID not in self.__config:
+			raise Exception("Invalid operationUUID: "+operationUUI)
+
+		return self.__config[operationUUID]
+
+	def getOperationMetadata(self):
+		return OperationMetadata(ResourceConfig.getInstance().getVersion(), ResourceConfig.getInstance().getHost(), ResourceConfig.getInstance().getContext())
+
+
+	@classmethod
+	def create(cls,mapObj):
+		"""
+		Creates object of type MediaMeasurements
+
+		@param Dict mapObj, containing the required parameters to create a new object
+		@return MediaMeasurements of the response of created instance.
+		@raise ApiException: raised an exception from the response status
+		"""
+		return BaseObject.execute("ce162846-ee2f-4317-b8e6-d2b78e60b977", MediaMeasurements(mapObj))
+
+
+
+
+
+
+
