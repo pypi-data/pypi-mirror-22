68aeea8fae1ad2599fa04ee369781a7e401351b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:56 +0000

    Add package trx/0.5.0/trx-0.5.0.tar.gz

---
+++ packages/trx/trx-0.5.0.tar.gz/trx-0.5.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='trx',
+      version='0.5.0',
+      description='Tools for time resolved x-ray techniques',
+      long_description=readme(),
+      url='https://github.com/marcocamma/trx',
+      author='marco cammarata',
+      author_email='marcocamma@gmail.com',
+      license='MIT',
+      packages=['trx'],
+      install_requires=[
+          'numpy',
+          'fabio',
+          'pyFAI',
+          'matplotlib',
+          'datastorage',
+          'statsmodels'
+      ],
+      zip_safe=False)
