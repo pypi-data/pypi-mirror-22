e07fa5be874fd9521371f3772c42cfaa5b5d9cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:34 +0000

    Add package mydacoclient/1.1.0/mydacoclient-1.1.0.tar.gz

---
+++ packages/mydacoclient/mydacoclient-1.1.0.tar.gz/mydacoclient-1.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'mydacoclient',
+  packages = ['mydacoclient'],
+  install_requires=[
+    'requests',
+  ],
+  version = '1.1.0',
+  description = 'python client for mydaco',
+  author = 'Benjamin Bach',
+  author_email = 'benjamin.bach@mydaco.com',
+  url = 'https://github.com/mydaco/python-client',
+  download_url = 'https://github.com/mydaco/python-client/archive/master.zip',
+  keywords = ['mydaco', 'client'],
+  classifiers = [],
+)
