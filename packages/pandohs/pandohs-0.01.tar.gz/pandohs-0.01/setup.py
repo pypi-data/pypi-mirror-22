650f309beed776fd8d705f9243008181e7eefa2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:07 +0000

    Add package pandohs/0.01/pandohs-0.01.tar.gz

---
+++ packages/pandohs/pandohs-0.01.tar.gz/pandohs-0.01/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pandohs',
+      packages=['pandohs'],
+      version='0.01',
+      description='Pandohs Data Validation',
+      author='Kallin Nagelberg',
+      author_email='kallin.nagelberg@gmail.com',
+      url='https://github.com/Kallin/pandohs',
+      )
