6331b67b0d040778aed69d54170a551794baa941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:37 +0000

    Add package cfblog/0.2.13/cfblog-0.2.13-py2-none-any.whl

---
+++ packages/cfblog/cfblog-0.2.13-py2-none-any.whl/cfblog/__init__.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+from cfblog.middleware import Middleware
+
+__all__ = ["Middleware"]
---
+++ packages/cfblog/cfblog-0.2.13-py2-none-any.whl/cfblog/managers.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+from django.db import models
+from django.db.models.query import QuerySet
+from django.utils import timezone
+
+
+class ContentMixin(object):
+    def by_author(self, user):
+        return self.filter(author=user)
+
+    def by_category(self, category):
+        return self.filter(category=category)
+
+    def published(self):
+        return self.filter(
+            status__gte=self.model.PUBLIC, publish__lte=timezone.now()
+        )
+
+    def static_pages(self):
+        return self.filter(category__is_static=True)
+
+    def blog_posts(self):
+        return self.filter(category__is_static=False)
+
+
+class ContentQuerySet(QuerySet, ContentMixin):
+    pass
+
+
+class ContentManager(models.Manager, ContentMixin):
+    def get_queryset(self):
+        return ContentQuerySet(self.model, using=self._db)
