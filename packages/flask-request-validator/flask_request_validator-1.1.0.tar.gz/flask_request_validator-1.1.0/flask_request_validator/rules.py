07ddddccc8aede24b743ed2391d6a6c84e2aff87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:22 +0000

    Add package flask-request-validator/1.1.0/flask_request_validator-1.1.0.tar.gz

---
+++ packages/flask-request-validator/flask_request_validator-1.1.0.tar.gz/flask_request_validator-1.1.0/flask_request_validator/rules.py
@@ -0,0 +1,155 @@
+import re
+
+from flask_request_validator.exceptions import NotAllowedType
+
+ALLOWED_TYPES = (str, bool, int, float, dict, list)
+
+
+class AbstractRule(object):
+
+    def validate(self, value):
+        """
+        :param mixed value:
+        :rtype: list|None
+        :return: errors
+        :rtype: list
+        """
+        raise NotImplementedError()
+
+
+class CompositeRule(AbstractRule):
+
+    def __init__(self, *rules):
+        """
+        :param tuple rules: (Rule(), Rule())
+        """
+        self.rules = rules
+
+    def validate(self, value):
+        errors = []
+        for rule in self.rules:
+            if isinstance(rule, Required) or value:
+                error = rule.validate(value)
+                if error:
+                    errors.extend(error)
+
+        return errors
+
+    def value_to_type(self, value):
+        """
+        :param mixed value:
+        :return: mixed
+        """
+        for rule in self.rules:
+            if isinstance(rule, Type):
+                return rule.value_to_type(value)
+
+        return value
+
+
+class Required(AbstractRule):
+
+    def validate(self, value):
+        if not value:
+            return ['Value is required']
+
+
+class Pattern(AbstractRule):
+
+    def __init__(self, pattern):
+        """
+        :param str pattern:
+        """
+        self.pattern = re.compile(pattern)
+
+    def validate(self, value):
+        if not self.pattern.search(value):
+            return ['Value %s does not match pattern %s' % (value, self.pattern.pattern)]
+
+
+class Enum(AbstractRule):
+
+    def __init__(self, *allowed_values):
+        """
+        :param tuple allowed_values:
+        """
+        self.allowed_values = allowed_values
+
+    def validate(self, value):
+        if value not in self.allowed_values:
+            return ['Incorrect value %s. Allowed values: %s' % (value, self.allowed_values)]
+
+
+class Type(AbstractRule):
+
+    def __init__(self, value_type):
+        """
+        :param type value_type: 
+        """
+
+        if value_type not in ALLOWED_TYPES:
+            raise NotAllowedType(
+                'Type %s is not allowed. Supported types: %s' %
+                (value_type, ALLOWED_TYPES)
+            )
+
+        self.value_type = value_type
+
+    def value_to_type(self, value):
+        """
+        :param mixed value:
+        :return: mixed
+        """
+        try:
+            if self.value_type == bool:
+                value = value.lower()
+
+                if value in ('true', '1'):
+                    value = True
+                elif value in ('false', '0'):
+                    value = False
+            elif self.value_type == list:
+                value = [item.strip() for item in value.split(',')]
+            elif self.value_type == dict:
+                value = {
+                    item.split(':')[0].strip(): item.partition(':')[-1].strip()
+                    for item in value.split(',')
+                }
+
+            value = self.value_type(value)
+        except (ValueError, TypeError):
+            pass
+
+        return value
+
+    def validate(self, value):
+        if type(value) is not self.value_type:
+            return ['Invalid type for value %s' % value]
+
+
+class MaxLength(AbstractRule):
+
+    def __init__(self, length):
+        """
+
+        :param int length:
+        """
+        self.length = length
+
+    def validate(self, value):
+        if len(value) > self.length:
+            return ['Invalid length for value %s. Max length = %s' % (value, self.length)]
+
+
+class MinLength(AbstractRule):
+
+    def __init__(self, length):
+        """
+
+        :param int length:
+        """
+        self.length = length
+
+    def validate(self, value):
+        if len(value) < self.length:
+            return ['Invalid length for value %s. Min length = %s' % (value, self.length)]
---
+++ packages/flask-request-validator/flask_request_validator-1.1.0.tar.gz/flask_request_validator-1.1.0/flask_request_validator/validator.py
@@ -0,0 +1,140 @@
+from copy import deepcopy
+from functools import wraps
+
+from werkzeug.datastructures import ImmutableDict
+
+from .exceptions import InvalidRequest, UndefinedParamType
+from .rules import Type, Required, CompositeRule
+from flask import request
+
+
+GET = 'GET'
+VIEW = 'VIEW'
+POST = 'POST'
+PARAM_TYPES = (GET, VIEW, POST)
+
+
+class Param(object):
+
+    def __init__(self, name, param_type, *rules):
+        """
+
+        :param tuple rules:
+        :param str name: name of param
+        :param str param_type: type of request param (GET OR POST)
+        """
+
+        self.name = name
+        self.param_type = param_type
+        self.rules = rules
+
+    def convert_value(self, value):
+        """
+        :param mixed value:
+        :return: mixed
+        """
+
+        for rule in self.rules:
+            if isinstance(rule, Type) and value:
+                value = rule.value_to_type(value)
+
+                break
+
+        return value
+
+
+def validate_params(*params):
+    """
+    Validate route using json schema. Example:
+
+    @app.route('/<int:level>')
+    @validate_params(
+        # POST param
+        Param('param_name', POST, Type(str), Required()),
+        # VIEW param
+        Param('level', VIEW, Type(str), Required(), Pattern(r'^[a-zA-Z0-9-_.]{5,20}$')),
+    )
+    def example_route(level):
+        ...
+
+    Also you can to set additional settings for Param(such as required, pattern(regex) etc).
+    See: rules
+
+    :param tuple params: (Param(), Param(), ...)
+    """
+
+    def validate_request(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            errors = __get_errors(params)
+            if errors[GET] or errors[POST] or errors[VIEW]:
+                raise InvalidRequest(errors)
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return validate_request
+
+
+def __get_errors(params):
+    """
+    Returns errors of validation
+    :param tuple params: (Param(), Param(), ...)
+    :rtype: dict
+    :return: {
+        'GET': {'param_name': ['error1', 'error2', ...]},
+        'VIEW': {'param_name': ['error1', 'error2', ...]},
+        'POST': {'param_name': ['error1', 'error2', ...]}
+    }
+    """
+
+    errors = {
+        GET: {},
+        VIEW: {},
+        POST: {},
+    }
+    valid_values = deepcopy(errors)
+
+    for param in params:
+        param_name = param.name
+        param_type = param.param_type
+        value = get_request_value(param_type, param_name)
+        for rule in param.rules:
+            if isinstance(rule, (Type, CompositeRule)) and value:
+                value = rule.value_to_type(value)
+
+                break
+
+        for rule in param.rules:
+            if isinstance(rule, Required) or value:
+                rule_errors = rule.validate(value)
+                if rule_errors:
+                    errors[param_type].setdefault(param_name, [])
+
+                    errors[param_type][param_name].extend(rule_errors)
+                else:
+                    valid_values[param_type][param_name] = value
+
+    request.valid_params = ImmutableDict(valid_values)
+
+    return errors
+
+
+def get_request_value(value_type, name):
+    """
+    :param str value_type: POST, GET or VIEW
+    :param str name:
+    :raise: UndefinedParamType
+    :return: mixed
+    """
+    if value_type == POST:
+        value = request.form.get(name)
+    elif value_type == GET:
+        value = request.args.get(name)
+    elif value_type == VIEW:
+        value = request.view_args.get(name)
+    else:
+        raise UndefinedParamType('Undefined param type %s' % name)
+
+    return value
