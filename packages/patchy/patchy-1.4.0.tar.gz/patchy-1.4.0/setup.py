5e79f2397181ca7abd59b22f9c40433ecc2fa83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:13 +0000

    Add package patchy/1.4.0/patchy-1.4.0.tar.gz

---
+++ packages/patchy/patchy-1.4.0.tar.gz/patchy-1.4.0/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import codecs
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('patchy')
+
+with codecs.open('README.rst', 'r', 'utf-8') as readme_file:
+    readme = readme_file.read()
+
+with codecs.open('HISTORY.rst', 'r', 'utf-8') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+
+requirements = [
+    'six>=1.9.0',
+]
+
+setup(
+    name='patchy',
+    version=version,
+    description="Patch the inner source of python functions at runtime.",
+    long_description=readme + '\n\n' + history,
+    author="Adam Johnson",
+    author_email='me@adamj.eu',
+    url='https://github.com/adamchainz/patchy',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='patchy',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
