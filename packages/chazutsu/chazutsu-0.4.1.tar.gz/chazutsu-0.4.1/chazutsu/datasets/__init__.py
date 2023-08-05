285b5dd46836c66222a5b62e64e37eb5e6f953d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:44 +0000

    Add package chazutsu/0.4.1/chazutsu-0.4.1.tar.gz

---
+++ packages/chazutsu/chazutsu-0.4.1.tar.gz/chazutsu-0.4.1/chazutsu/datasets/__init__.py
@@ -0,0 +1,7 @@
+from .movie_review import MovieReview
+from .customer_review import CustomerReview
+from .news_group20 import NewsGroup20
+from .imdb import IMDB
+from .ptb import PTB
+from .wikitext2 import WikiText2
+from .wikitext103 import WikiText103
