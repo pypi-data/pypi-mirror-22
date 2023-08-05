2663a3b13b7bccf07ec770faffc7123f83fcef85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:01 +0000

    Add package slothTw/2.2/slothTw-2.2.tar.gz

---
+++ packages/slothTw/slothTw-2.2.tar.gz/slothTw-2.2/slothTw/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+import datetime
+# 要取得會員的model要這樣寫
+from userper import Userper
+User = Userper('login.stufinite.faith')
+
+# Create your models here.
+class Course(models.Model):
+	"""docstring for Course"""
+	name = models.CharField(max_length=10)
+	ctype = models.CharField(max_length=10,default='')# 課程是通識必修還是選修
+	avatar = models.ImageField(default='') # 大頭貼照片
+	teacher = models.CharField(max_length=10)
+	school = models.CharField(max_length=10)
+	book = models.CharField(max_length=50)
+	syllabus = models.CharField(max_length=50)
+	feedback_amount = models.PositiveIntegerField(default=0)
+	feedback_freedom = models.FloatField(default=0)
+	feedback_FU = models.FloatField(default=0)
+	feedback_easy = models.FloatField(default=0)
+	feedback_GPA = models.FloatField(default=0)
+	feedback_knowledgeable = models.FloatField(default=0)
+	# attendee = models.ManyToManyField(User)
+	def __str__(self):
+		return self.name
+
+class Comment(models.Model):
+	course = models.ForeignKey(Course)
+	author = User
+	create = models.DateTimeField(default=datetime.datetime.now)
+	raw = models.CharField(max_length=500)
+	like = models.PositiveSmallIntegerField(default=0)
+	def __str__(self):
+		return self.raw
