f98afc030b82299362657ad7c47fec3f1ce0ce58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:53 +0000

    Add package bio-apricot/1.2.9/bio-APRICOT-1.2.9.tar.gz

---
+++ packages/bio-apricot/bio-APRICOT-1.2.9.tar.gz/bio-APRICOT-1.2.9/apricotlib/create_keyword_files.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Description = Stores user provided keywords in files.
+
+
+def keyword_domains_files(kw_file_path, keywords):
+    with open(kw_file_path+'/keywords_for_domain_selection.txt',
+              'w') as out_fh:
+        if ',' in keywords:
+            for each_kw in keywords.split(','):
+                if ' ' in each_kw:
+                    each_kw.replace(' ', '-')
+                out_fh.write('%s\n' % each_kw)
+        else:
+            out_fh.write(keywords)
+            print("------\nSingle keyword for domain selection "
+                  "has been given.")
+            print("Multiple keywords can be provided as comma separated "
+                  "list.\n------")
+
+
+def keyword_class_files(kw_file_path, keywords):
+    with open(kw_file_path+'/keywords_for_result_classification.txt',
+              'w') as out_fh:
+        if ',' in keywords:
+            out_fh.write('\n'.join(keywords.split(',')))
+        else:
+            out_fh.write(keywords)
+            print("------\nSingle keyword for result "
+                  "classification has been given.")
+            print("Multiple keywords can be provided as "
+                  "comma separated list.\n------")
