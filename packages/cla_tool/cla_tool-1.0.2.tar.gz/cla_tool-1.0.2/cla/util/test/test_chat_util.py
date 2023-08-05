067a6072aa1f1bf935b40dbd4cc0c716cb09f2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:42 +0000

    Add package cla_tool/1.0.2/cla_tool-1.0.2.tar.gz

---
+++ packages/cla_tool/cla_tool-1.0.2.tar.gz/cla_tool-1.0.2/cla/util/test/test_chat_util.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from cla.util import chat_util
+
+
+def test_process_qq_history():
+    test_file_path = "res/test/qq.txt"
+    result_path = chat_util.process_qq_history(test_file_path)
+
+    import os
+    assert os.path.isfile(result_path)
+    assert os.path.getsize(result_path) > 0
---
+++ packages/cla_tool/cla_tool-1.0.2.tar.gz/cla_tool-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='cla_tool',
+    version='1.0.2',
+    packages=find_packages(exclude=['res']),
+    url='https://github.com/dickrd/cla_tool',
+    license='Apache License 2.0',
+    author='DickRD',
+    author_email='dickdata7@gmail.com',
+    description='Chinese language analyze tools.',
+    scripts=['bin/cla'],
+    install_requires=['gensim', 'jieba', 'sklearn'],
+    tests_require=['pytest'],
+    test_suite="pytest"
+)
