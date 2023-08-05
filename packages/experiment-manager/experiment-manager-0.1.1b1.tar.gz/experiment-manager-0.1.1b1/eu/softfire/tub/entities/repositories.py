721b0c0c5d7c385cfbc2390ef3134141cfd0fe1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:07 +0000

    Add package experiment-manager/0.1.1b1/experiment-manager-0.1.1b1.tar.gz

---
+++ packages/experiment-manager/experiment-manager-0.1.1b1.tar.gz/experiment-manager-0.1.1b1/eu/softfire/tub/entities/repositories.py
@@ -0,0 +1,54 @@
+import logging
+import threading
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from eu.softfire.tub.entities.entities import Base
+from eu.softfire.tub.utils.utils import get_config, get_logger
+
+logger = get_logger('eu.softfire.tub.repository')
+
+lock = threading.RLock()
+
+engine = create_engine(get_config('database', 'url', "sqlite:////tmp/experiment-manager.db"))
+debug_echo = (logger.getEffectiveLevel() == logging.DEBUG) and get_config('database', 'show_sql',
+                                                                          False).lower() == 'true'
+engine.echo = debug_echo
+Base.metadata.create_all(engine)
+session_factory = sessionmaker(bind=engine)
+_session = scoped_session(session_factory)
+session = _session()
+
+
+@contextmanager
+def get_db_session():
+    with lock:
+        yield session
+
+
+def save(entity):
+    with get_db_session() as se:
+        se.add(entity)
+        se.commit()
+
+
+def delete(entity):
+    with get_db_session() as se:
+        se.delete(entity)
+        se.commit()
+
+
+def find(_clazz, _id=None):
+    with get_db_session() as se:
+        if _id is None:
+            res = se.query(_clazz).all()
+        else:
+            res = se.query(_clazz).filter(_clazz.id == _id).one()
+        se.commit()
+    return res
+
+
+def drop_tables():
+    Base.metadata.drop_all(engine)
