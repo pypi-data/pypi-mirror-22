b4d8440ef9d2093d57a19606327ab041d446deb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:41 +0000

    Add package test_yl/1.1.0/test_yl-1.1.0.zip

---
+++ packages/test_yl/test_yl-1.1.0.zip/test_yl-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'test_yl',
+        version = '1.1.0',
+        py_modules = ['test_yl'],
+        author = 'yanglian',
+        author_email = '76724223@qq.com',
+        url = 'https://www.baidu.com/',
+        description = '对复杂列表有缩进的输出',
+    )
