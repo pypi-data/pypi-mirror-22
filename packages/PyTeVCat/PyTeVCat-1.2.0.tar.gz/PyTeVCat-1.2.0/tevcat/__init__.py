c50dbff37cadaddde6432db6c89c0e5ff24a05ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:43 +0000

    Add package PyTeVCat/1.2.0/PyTeVCat-1.2.0.tar.gz

---
+++ packages/PyTeVCat/PyTeVCat-1.2.0.tar.gz/PyTeVCat-1.2.0/tevcat/__init__.py
@@ -0,0 +1,9 @@
+"""
+PyTeVCat is a python wrapper of TeVCat
+http://tevcat.uchicago.edu/
+
+It retrieves the TeV source database from the TeVCat HTML. Basic getter methods
+and an all-sky viewer are offered.
+"""
+
+from tevcat_all import *
