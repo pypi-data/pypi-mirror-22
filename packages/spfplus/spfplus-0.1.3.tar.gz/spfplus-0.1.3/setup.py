2dc5e5250b84312d828766dfa676ee58703832bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:13 +0000

    Add package spfplus/0.1.3/spfplus-0.1.3.tar.gz

---
+++ packages/spfplus/spfplus-0.1.3.tar.gz/spfplus-0.1.3/setup.py
@@ -0,0 +1,23 @@
+"""
+Setup file for pip.
+"""
+
+from distutils.core import setup
+
+setup(
+    name='spfplus',
+    packages=['spfplus'], # this must be the same as the name above
+    version='0.1.3',
+    description='Sender Policy Framework (SPF) python library.',
+    author='Hang Hu',
+    author_email='theodorehu95@gmail.com',
+    url='https://github.com/theodorehu95/spfplus', # use the URL to the github repo
+    download_url='https://github.com/theodorehu95/spfplus/archive/{tag}.tar.gz', # I'll explain this in a second
+    keywords=['spf', 'email', 'authentication'], # arbitrary keywords
+    classifiers=[],
+    platforms = 'any',
+    license = 'MIT License',
+)
+
+
+
