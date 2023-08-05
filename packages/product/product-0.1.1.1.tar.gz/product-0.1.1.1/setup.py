3ade27b6034fd9cc5fc842055e684c39c86ab265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:35 +0000

    Add package product/0.1.1.1/product-0.1.1.1.tar.gz

---
+++ packages/product/product-0.1.1.1.tar.gz/product-0.1.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='product',      # name of PyPI package
+    version='0.1.1.1',   # version number, update with new releases
+    packages=['product'] # name of script
+)
+
