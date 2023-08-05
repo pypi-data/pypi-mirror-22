b5bafc1f966535c1b0e83e001ea2e8094b1bf86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:42 +0000

    Add package stochopy/1.2.2/stochopy-1.2.2.tar.gz

---
+++ packages/stochopy/stochopy-1.2.2.tar.gz/stochopy-1.2.2/stochopy/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+StochOPy (STOCHastic OPtimization for PYthon) provides user-friendly routines
+to sample or optimize objective functions with the most popular algorithms.
+
+Author: Keurfon Luu <keurfon.luu@mines-paristech.fr>
+License: MIT
+"""
+
+from .monte_carlo import MonteCarlo
+from .evolutionary_algorithm import Evolutionary
+
+__all__ = [ "MonteCarlo", "Evolutionary" ]
+__version__ = "1.2.2"
