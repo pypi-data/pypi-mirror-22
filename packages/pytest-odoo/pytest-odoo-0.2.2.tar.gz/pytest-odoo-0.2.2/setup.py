6e9119377634a5db36ff4945d30558f34313dd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:33 +0000

    Add package pytest-odoo/0.2.2/pytest-odoo-0.2.2.tar.gz

---
+++ packages/pytest-odoo/pytest-odoo-0.2.2.tar.gz/pytest-odoo-0.2.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='pytest-odoo',
+    description='py.test plugin to run Odoo tests',
+    long_description=open("README.rst").read(),
+    version='0.2.2',
+    url='https://github.com/camptocamp/pytest-odoo',
+    license='AGPLv3',
+    author='Guewen Baconnier',
+    author_email='guewen.baconnier@camptocamp.com',
+    py_modules=['pytest_odoo'],
+    extras_require={
+        'autodiscover': ['odoo_autodiscover'],
+    },
+    entry_points={'pytest11': ['odoo = pytest_odoo']},
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['pytest>=2.9'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
