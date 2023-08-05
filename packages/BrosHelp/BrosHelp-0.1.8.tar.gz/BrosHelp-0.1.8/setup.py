8f438d1539904e147aa7d248f553a030ce9837f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:06 +0000

    Add package BrosHelp/0.1.8/BrosHelp-0.1.8.tar.gz

---
+++ packages/BrosHelp/BrosHelp-0.1.8.tar.gz/BrosHelp-0.1.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BrosHelp',
+    version='0.1.8',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    platforms = 'any',
+    include_package_data = True,
+    description='BrosHelp Bot',
+    url='https://github.com/KayShenA/PythonProjects',
+    author='Kay',
+    author_email='skyluckyrrb@126.com',
+    license='MIT',
+    install_requires=[
+        'wxpy',
+    ],)
