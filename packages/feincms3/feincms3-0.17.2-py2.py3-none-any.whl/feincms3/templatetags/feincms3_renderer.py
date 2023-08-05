a631c6a3f1d43ca940e8c365d06456008d3b9c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:56 +0000

    Add package feincms3/0.17.2/feincms3-0.17.2-py2.py3-none-any.whl

---
+++ packages/feincms3/feincms3-0.17.2-py2.py3-none-any.whl/feincms3/templatetags/feincms3_renderer.py
@@ -0,0 +1,36 @@
+from django import template
+from django.utils.html import mark_safe
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def render_plugin(context, plugin):
+    """
+    Render a single plugin. See :mod:`feincms3.renderer` for additional
+    details.
+    """
+    return context['renderer'].render_plugin_in_context(plugin, context)
+
+
+@register.simple_tag(takes_context=True)
+def render_plugins(context, plugins):
+    """
+    Render and concatenate a list of plugins. See
+    :mod:`feincms3.renderer` for additional details.
+    """
+    renderer = context['renderer']
+    return mark_safe(''.join(
+        renderer.render_plugin_in_context(plugin, context)
+        for plugin in plugins
+    ))
+
+
+@register.simple_tag(takes_context=True)
+def render_region(context, regions, region, **kwargs):
+    """
+    Render a single region. See :class:`~feincms3.renderer.Regions` for
+    additional details. Any and all keyword arguments are forwarded to the
+    ``render`` method of the ``Regions`` instance.
+    """
+    return regions.render(region, context, **kwargs)
