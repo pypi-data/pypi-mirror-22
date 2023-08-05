e6904ab3ba5f0ec750648d4272edcdaa82e3b65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:03 +0000

    Add package revived/0.1.4/revived-0.1.4.tar.gz

---
+++ packages/revived/revived-0.1.4.tar.gz/revived-0.1.4/revived/__init__.py
@@ -0,0 +1,2 @@
+"""revived - A redux-inspired predictable state container for python
+"""
---
+++ packages/revived/revived-0.1.4.tar.gz/revived-0.1.4/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+VERSION = '0.1.4'
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+with open(path.join(here, 'CHANGELOG.rst'), encoding='utf-8') as f:
+    CHANGELOG = f.read()
+
+setup(
+    name='revived',
+    version=VERSION,
+
+    description='Redux-inspired library in python',
+    long_description='\n\n'.join([
+        LONG_DESCRIPTION,
+        CHANGELOG
+    ]),
+
+    url='https://github.com/RookieGameDevs/revived',
+    author='Lorenzo Berni',
+    author_email='lorenzo@rookiegamedevs.io',
+
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='state store redux',
+
+    packages=find_packages(exclude=['tests']),
+
+    install_requires=[],
+
+    extras_require={
+        'dev': [''],
+        'test': ['pytest'],
+    },
+)
