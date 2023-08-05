3fc07d87b6352d9f69694ba7593971e368196317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:08 +0000

    Add package miniretry/0.1.1/miniretry-0.1.1.tar.gz

---
+++ packages/miniretry/miniretry-0.1.1.tar.gz/miniretry-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+# from setuptools import setup
+
+setup(
+    name="miniretry",
+    version="0.1.1",
+    description="Retry helpers",
+    author="Martin Czygan",
+    author_email="martin.czygan@gmail.com",
+    url="http://example.com",
+    py_modules=["miniretry"],
+    scripts=['bin/miniretry'],
+)
