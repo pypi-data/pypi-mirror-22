58a32ece2b947d0c1992720edbf55602d6b65417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:36 +0000

    Add package angrytest/1.3.1/angrytest-1.3.1.tar.gz

---
+++ packages/angrytest/angrytest-1.3.1.tar.gz/angrytest-1.3.1/angrytest/pypitestangry.py
@@ -0,0 +1,15 @@
+DEFAULT = 10
+DEFAULT_NAME = 'Angryziber'
+
+def callPrint(val = DEFAULT) :
+	print "Works with val ", val
+
+def sayHi(name = DEFAULT_NAME) :
+	print "Hi ",name
+
+def talklToMe(name = DEFAULT_NAME) :
+	print "What can i say?" ,name 
+
+# callPrint()
+# sayHi()
+# talklToMe()
