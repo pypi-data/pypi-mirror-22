6ea933eb834a59e6acb591540ea87cdc1bc9d517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:20 +0000

    Add package andy_hello/1.0.0/andy_hello-1.0.0.tar.gz

---
+++ packages/andy_hello/andy_hello-1.0.0.tar.gz/andy_hello-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         =   'andy_hello',
+        version      =    '1.0.0',
+        py_modules   = ['andy_hello'],
+        author       = ['andy'],
+        author_email = 'wanglupublic@163.com',
+        url          = 'http://www.baidu.com',
+        description =  'this is a python distribution test'
+        )
