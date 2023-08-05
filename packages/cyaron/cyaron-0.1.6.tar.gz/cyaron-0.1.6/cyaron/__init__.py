5f9e1c5a167daaa90222f3aad7b9a7ba339cbf7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:45 +0000

    Add package cyaron/0.1.6/cyaron-0.1.6.tar.gz

---
+++ packages/cyaron/cyaron-0.1.6.tar.gz/cyaron-0.1.6/cyaron/__init__.py
@@ -0,0 +1,15 @@
+"""CYaRon: Yet Another Random Olympic-iNformatics test data generator
+By Luogu
+This is a tool package for everyone to make the test data quickly.
+It has tools for graphs and IO files.
+"""
+
+from .io import IO
+from .graph import Graph, Edge
+from .str import String
+from .sequence import Sequence
+from .utils import *
+from .consts import *
+from .vector import Vector
+from random import randint, randrange, uniform, choice, random
+
