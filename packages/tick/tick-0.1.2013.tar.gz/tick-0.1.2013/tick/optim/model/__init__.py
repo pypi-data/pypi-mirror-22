ae8a0387a5055f07aa86a347aaeaab1fdd2b0d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package tick/0.1.2013/tick-0.1.2013.tar.gz

---
+++ packages/tick/tick-0.1.2013.tar.gz/tick-0.1.2013/tick/optim/model/__init__.py
@@ -0,0 +1,23 @@
+import tick.base
+
+from .linreg import ModelLinReg
+from .linreg_with_intercepts import ModelLinRegWithIntercepts
+from .logreg import ModelLogReg
+from .poisreg import ModelPoisReg
+
+from .coxreg_partial_lik import ModelCoxRegPartialLik
+
+from .hawkes_fixed_expkern_loglik import ModelHawkesFixedExpKernLogLik
+from .hawkes_fixed_expkern_leastsq import ModelHawkesFixedExpKernLeastSq
+from .hawkes_fixed_sumexpkern_leastsq import ModelHawkesFixedSumExpKernLeastSq
+
+
+__all__ = ["ModelLinReg",
+           "ModelLinRegWithIntercepts",
+           "ModelLogReg",
+           "ModelPoisReg",
+           "ModelCoxRegPartialLik",
+           "ModelHawkesFixedExpKernLogLik",
+           "ModelHawkesFixedExpKernLeastSq",
+           "ModelHawkesFixedSumExpKernLeastSq"
+           ]
