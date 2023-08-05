e25bca078143f6d72bc523e744ab3c4736732073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:53 +0000

    Add package django-developer-panel/0.1.2/django-developer-panel-0.1.2.tar.gz

---
+++ packages/django-developer-panel/django-developer-panel-0.1.2.tar.gz/django-developer-panel-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='django-developer-panel',
+    version='0.1.2',
+    description='Django bindings for the Django Developer Panel Chrome extension.',
+    author='Casey Kinsey / Lofty Labs',
+    author_email='casey@hirelofty.com',
+    url='https://github.com/loftylabs/django-developer-panel',
+    packages=['djdev_panel'],
+)
