9bb52b8b33e05cf9f26ead2b800bf843ca283adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package sistdmyy/1.1.2/sistdmyy-1.1.2.tar.gz

---
+++ packages/sistdmyy/sistdmyy-1.1.2.tar.gz/sistdmyy-1.1.2/setup.py
@@ -0,0 +1,31 @@
+from __future__ import print_function  
+from setuptools import setup, find_packages  
+import sys  
+  
+setup(  
+    name="sistdmyy",
+    version="1.1.2",
+    author="lanmengfei",
+    author_email="865377886@qq.com",
+    description="兰孟飞在深圳标准技术研究部的工作",
+    long_description=open("README.txt",encoding="utf-8").read(),
+    license="MIT",
+    url="https://user.qzone.qq.com/865377886",
+    packages=['sistdmyy'],
+    install_requires=[
+        "pandas",
+        "sqlalchemy",
+        "pymssql"
+        ],  
+    classifiers=[  
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],  
+)  
