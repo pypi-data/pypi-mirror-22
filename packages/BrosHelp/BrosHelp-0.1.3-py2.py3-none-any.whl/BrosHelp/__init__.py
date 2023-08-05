99cb6e6739753d3e327798053c95effa61c3e89b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:02 +0000

    Add package BrosHelp/0.1.3/BrosHelp-0.1.3-py2.py3-none-any.whl

---
+++ packages/BrosHelp/BrosHelp-0.1.3-py2.py3-none-any.whl/BrosHelp/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+from Start import *
+
+__title__ = 'BrosHelp'
+__version__ = '0.0.1'
+__author__ = 'Kay'
+__license__ = 'MIT'
+__copyright__ = '2017, Kay'
+version_details = 'BrosHelp {ver} from {path} (python {pv.major}.{pv.minor}.{pv.micro})'.format(
+    ver=__version__, path=__path__[0], pv=sys.version_info)
