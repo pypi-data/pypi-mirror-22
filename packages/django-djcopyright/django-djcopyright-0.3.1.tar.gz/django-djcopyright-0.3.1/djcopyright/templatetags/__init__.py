015eeb7ad1ec400bc5581f5ce72047fc12932acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:10 +0000

    Add package django-djcopyright/0.3.1/django-djcopyright-0.3.1.tar.gz

---
+++ packages/django-djcopyright/django-djcopyright-0.3.1.tar.gz/django-djcopyright-0.3.1/djcopyright/templatetags/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# django-djcopyright
+# djcopyright/templatetags/__init__.py
+
+from __future__ import unicode_literals
+
+
+__all__ = []
---
+++ packages/django-djcopyright/django-djcopyright-0.3.1.tar.gz/django-djcopyright-0.3.1/djcopyright/templatetags/djcopyright_tags.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# django-djcopyright
+# djcopyright/templatetags/djcopyright_tags.py
+
+from __future__ import unicode_literals
+
+from django import template
+
+from djcopyright.utils import get_copyright
+
+
+__all__ = [
+    "djcopyright_years",
+]
+
+
+register = template.Library()
+
+
+@register.assignment_tag()
+def djcopyright_years():
+    """
+    Formatted copyright years string templatetag.
+    Returns:
+        string: formatted copyright years string.
+    """
+
+    return get_copyright()
