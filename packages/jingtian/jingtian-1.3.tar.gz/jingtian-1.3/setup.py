2c87cb7df70c27253e70ac5c3242365faa493463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package jingtian/1.3/jingtian-1.3.tar.gz

---
+++ packages/jingtian/jingtian-1.3.tar.gz/jingtian-1.3/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+
+setup (
+
+        name         = 'jingtian', 
+
+        version      = '1.3',
+
+        py_modules   = ['mytj'],
+
+        author       = 'jingtian',
+
+        author_email = '15030662@qq.com',
+
+        url          = 'http://www.example.com',
+
+        description  = 'A simple printer of lists',
+
+       )
