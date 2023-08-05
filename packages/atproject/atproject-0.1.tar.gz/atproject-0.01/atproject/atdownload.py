6fde23dbd6cdf68782e72f95c14e077b25a72cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:11 +0000

    Add package atproject/0.1.0/atproject-0.1.tar.gz

---
+++ packages/atproject/atproject-0.1.tar.gz/atproject-0.01/atproject/atdownload.py
@@ -0,0 +1,13 @@
+import Download.main as dl
+
+def get(str):
+		path = str
+		obj = dl.CustomLibrary()
+		obj.torrent_path = path
+		path_to_file = obj.get()
+		
+		
+
+
+
+
---
+++ packages/atproject/atproject-0.1.tar.gz/atproject-0.01/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import atproject
+
+setup(
+    name="atproject",
+    version=0.1.0,
+    author="AT_group",
+    author_email="<liuyifei0226@gmail.com>",
+    url="https://github.com/atproject/",
+    description="John Hoffman's fork of the original bittorrent, we do some modify and use it as torrent download library",
+    license="MIT",
+    py_modules=['academictorrents'],
+    package_data={'atproject':['atproject/icons/*.ico']},
+    include_package_data=True,
+    packages=["atproject",
+    		  "atproject.Application",
+              "atproject.Client",
+              "atproject.Meta",
+              "atproject.Network",
+              "atproject.Storage",
+              "atproject.Tracker",
+              "atproject.Download"
+             ],
+    install_requires=[
+          'win_inet_pton',
+      ],
+    scripts=["academictorrents.py"]
+)
