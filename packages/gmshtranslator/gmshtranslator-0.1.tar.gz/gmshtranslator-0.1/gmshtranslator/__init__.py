f587934b2c531fad2808a8d712f11674577767ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:48 +0000

    Add package gmshtranslator/0.1/gmshtranslator-0.1.tar.gz

---
+++ packages/gmshtranslator/gmshtranslator-0.1.tar.gz/gmshtranslator-0.1/gmshtranslator/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+
+print ("gmshtranslator friendly reminder: rules and actions prototypes\n\n")
+print ("def node_condition(tag,x,y,z,physgroups): ")
+print ("def node_action(tag,x,y,z):")
+print ("def element_condition(eletag,eletype,physgrp,nodes):")
+print ("def element_action(eletag,eletype,physgrp,nodes):")
