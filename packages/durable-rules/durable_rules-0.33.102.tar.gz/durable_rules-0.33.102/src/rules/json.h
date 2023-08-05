c0b12f454e034cde68ef35a5a4cd77bb73814c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:07 +0000

    Add package durable-rules/0.33.102/durable_rules-0.33.102.tar.gz

---
+++ packages/durable-rules/durable_rules-0.33.102.tar.gz/durable_rules-0.33.102/src/rules/json.h
@@ -0,0 +1,26 @@
+
+
+#define PARSE_OK 0
+#define PARSE_END 100
+
+#define JSON_STRING 0x01
+#define JSON_INT 0x02
+#define JSON_DOUBLE 0x03
+#define JSON_BOOL 0x04
+#define JSON_ARRAY 0x05
+#define JSON_OBJECT 0x06
+#define JSON_NIL 0x07
+#define JSON_STATE_PROPERTY 0x08
+#define JSON_EVENT_PROPERTY 0x09
+#define JSON_STATE_IDIOM 0x0A
+#define JSON_EVENT_IDIOM 0x0B
+#define JSON_REGEX 0x0C
+#define JSON_IREGEX 0x0D
+
+unsigned int readNextName(char *start, char **first, char **last, unsigned int *hash);
+unsigned int readNextValue(char *start, char **first, char **last, unsigned char *type);
+unsigned int readNextArrayValue(char *start, char **first, char **last, unsigned char *type);
+unsigned int readNextString(char *start, char **first, char **last, unsigned int *hash);
+
+
+
