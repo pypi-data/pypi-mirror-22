0cfa2c8d8a332ccbafcd5c3294f2a05f8999a2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:57 +0000

    Add package tefla/1.0.1/tefla-1.0.1.tar.gz

---
+++ packages/tefla/tefla-1.0.1.tar.gz/tefla-1.0.1/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+with open('requirements.txt') as requirements:
+    REQUIREMENTS = requirements.readlines()
+
+# explicitly config
+test_args = [
+    '--cov-report=term',
+    '--cov-report=html',
+    '--cov=tefla',
+    'tests'
+]
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = test_args
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name='tefla',
+    packages=find_packages(),
+    version='1.0.1',
+    description='Simple end-to-end deep learning with tensorflow. Datasets, data-augmentation, models, training, prediction, and metrics',
+    author='Tefla contributors',
+    author_email='mrinalhaloi11@gmail.com',
+    url='https://github.com/n3011/tefla',
+    download_url='https://github.com/n3011/tefla/tarball/1.0.0',
+    keywords=['tensorflow', 'deeplearning', 'cnn', 'deepcnn'],
+    classifiers=[],
+    install_requires=REQUIREMENTS,
+    test_suite='tests',
+    cmdclass={'test': PyTest},
+    license='MIT',
+    platforms=['linux'],
+)
