d33942757712a9b4613af1480dcfa33fee1f5dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:20 +0000

    Add package showtime/0.4/showtime-0.4.tar.gz

---
+++ packages/showtime/showtime-0.4.tar.gz/showtime-0.4/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(name="showtime",
+	  version="0.4",
+	  description="Stream movies/seasons directly to your vlc. Visit https://github.com/rahulxxarora/showtime for docs",
+	  url="https://github.com/rahulxxarora/showtime",
+	  author="Rahul Arora",
+	  author_email="coderahul94@gmail.com",
+	  license='MIT',
+	  packages=["showtime"],
+	  scripts=["bin/showtime"],
+	  install_requires=[
+		  'BeautifulSoup4',
+		  'requests'],
+	  zip_safe=False)
