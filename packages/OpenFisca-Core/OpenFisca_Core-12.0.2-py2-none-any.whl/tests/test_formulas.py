d5aed1682f7c08e1e0501cc80df1112efbfdad4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:02 +0000

    Add package OpenFisca-Core/12.0.2/OpenFisca_Core-12.0.2-py2-none-any.whl

---
+++ packages/OpenFisca-Core/OpenFisca_Core-12.0.2-py2-none-any.whl/tests/test_formulas.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+
+from openfisca_core.columns import IntCol
+from openfisca_core.periods import MONTH
+from openfisca_core.variables import Variable
+from openfisca_core.formula_helpers import switch
+import openfisca_dummy_country as dummy_country
+from openfisca_dummy_country.entities import Individu
+
+
+class choice(Variable):
+    column = IntCol
+    entity = Individu
+    definition_period = MONTH
+
+
+class uses_multiplication(Variable):
+    column = IntCol
+    entity = Individu
+    label = u'Variable with formula that uses multiplication'
+    definition_period = MONTH
+
+    def function(self, simulation, period):
+        choice = simulation.calculate('choice', period)
+        result = (choice == 1) * 80 + (choice == 2) * 90
+        return result
+
+
+class uses_switch(Variable):
+    column = IntCol
+    entity = Individu
+    label = u'Variable with formula that uses switch'
+    definition_period = MONTH
+
+    def function(self, simulation, period):
+        choice = simulation.calculate('choice', period)
+        result = switch(
+            choice,
+            {
+                1: 80,
+                2: 90,
+                },
+            )
+        return result
+
+
+# TaxBenefitSystem instance declared after formulas
+tax_benefit_system = dummy_country.DummyTaxBenefitSystem()
+tax_benefit_system.add_variables(choice, uses_multiplication, uses_switch)
+month = '2013-01'
+scenario = tax_benefit_system.new_scenario().init_from_attributes(
+    period = month,
+    input_variables = {
+        # 'choice': [1, 1, 1, 2],
+        'choice': np.random.randint(2, size = 1000) + 1,
+        },
+    )
+
+
+def test_switch():
+    simulation = scenario.new_simulation(debug = True)
+    uses_switch = simulation.calculate('uses_switch', period = month)
+    assert isinstance(uses_switch, np.ndarray)
+
+
+def test_multiplication():
+    simulation = scenario.new_simulation(debug = True)
+    uses_multiplication = simulation.calculate('uses_multiplication', period = month)
+    assert isinstance(uses_multiplication, np.ndarray)
+
+
+def test_compare_multiplication_and_switch():
+    simulation = scenario.new_simulation(debug = True)
+    uses_multiplication = simulation.calculate('uses_multiplication', period = month)
+    uses_switch = simulation.calculate('uses_switch', period = month)
+    assert np.all(uses_switch == uses_multiplication)
---
+++ packages/OpenFisca-Core/OpenFisca_Core-12.0.2-py2-none-any.whl/tests/test_yaml.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+import pkg_resources
+import os
+import subprocess
+from nose.tools import nottest, raises
+
+from openfisca_core.tools.test_runner import run_tests, generate_tests
+
+from openfisca_dummy_country import DummyTaxBenefitSystem
+
+tax_benefit_system = DummyTaxBenefitSystem()
+
+openfisca_dummy_country_dir = pkg_resources.get_distribution('OpenFisca-Dummy-Country').location
+yamls_tests_dir = os.path.join(openfisca_dummy_country_dir, 'openfisca_dummy_country', 'tests')
+
+# Declare that these two functions are not tests to run with nose
+nottest(run_tests)
+nottest(generate_tests)
+
+
+@nottest
+def run_yaml_test(file_name, options = {}):
+    yaml_path = os.path.join(yamls_tests_dir, '{}.yaml'.format(file_name))
+    return run_tests(tax_benefit_system, yaml_path, options)
+
+
+def test_success():
+    run_yaml_test('test_success')
+
+
+@raises(AssertionError)
+def test_fail():
+    run_yaml_test('test_failure')
+
+
+def test_relative_error_margin_success():
+    run_yaml_test('test_relative_error_margin')
+
+
+@raises(AssertionError)
+def test_relative_error_margin_fail():
+    run_yaml_test('failing_test_relative_error_margin')
+
+
+def test_absolute_error_margin_success():
+    run_yaml_test('test_absolute_error_margin')
+
+
+@raises(AssertionError)
+def test_absolute_error_margin_fail():
+    run_yaml_test('failing_test_absolute_error_margin')
+
+
+def test_run_tests_from_directory():
+    dir_path = os.path.join(yamls_tests_dir, 'directory')
+    assert run_tests(tax_benefit_system, dir_path) == 5
+
+
+def test_with_reform():
+    run_yaml_test('test_with_reform')
+
+
+@raises(AssertionError)
+def test_run_tests_from_directory_fail():
+    run_tests(tax_benefit_system, yamls_tests_dir)
+
+
+def test_name_filter():
+    nb_tests = run_tests(
+        tax_benefit_system,
+        yamls_tests_dir,
+        options = {'name_filter': 'success'}
+        )
+
+    assert nb_tests == 3
+
+
+def test_nose_style():
+    dir_path = os.path.join(yamls_tests_dir, 'directory')
+    for test in generate_tests(tax_benefit_system, dir_path):
+        yield test
+
+
+def test_shell_script():
+    yaml_path = os.path.join(yamls_tests_dir, 'test_success.yaml')
+    command = ['openfisca-run-test', yaml_path, '-c', 'openfisca_dummy_country']
+    with open(os.devnull, 'wb') as devnull:
+        subprocess.check_call(command, stdout = devnull)
+
+
+def test_shell_script_with_reform():
+    yaml_path = os.path.join(yamls_tests_dir, 'test_with_reform_2.yaml')
+    command = ['openfisca-run-test', yaml_path, '-c', 'openfisca_dummy_country', '-r', 'openfisca_dummy_country.dummy_reforms.neutralization_rsa']
+    with open(os.devnull, 'wb') as devnull:
+        subprocess.check_call(command, stdout = devnull)
+
+
+def test_shell_script_with_extension():
+    extension_dir = os.path.join(openfisca_dummy_country_dir, 'openfisca_dummy_country', 'dummy_extension')
+    command = ['openfisca-run-test', extension_dir, '-c', 'openfisca_dummy_country', '-e', extension_dir]
+    with open(os.devnull, 'wb') as devnull:
+        subprocess.check_call(command, stdout = devnull)
