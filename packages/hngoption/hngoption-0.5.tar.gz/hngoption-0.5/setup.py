899ab35e3a9669d6ba56ccda6daed8ed4c889b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:34 +0000

    Add package hngoption/0.5/hngoption-0.5.tar.gz

---
+++ packages/hngoption/hngoption-0.5.tar.gz/hngoption-0.5/setup.py
@@ -0,0 +1,15 @@
+# Python implementation by Dustin Zacharias (2017), method based on Fabrice Rouah (volopta.com)
+
+from setuptools import setup
+
+setup(name='hngoption',
+      version='0.5',
+      description='Heston Nandi GARCH Option Pricing Model (2000)',
+      url='https://github.com/SW71X/hngoption2',
+      download_url='https://github.com/SW71X/hngoption2/archive/0.5.tar.gz',
+      author='Dustin L. Zacharias',
+      author_email='dustinl.zacharias@gmail.com',
+      license='MIT',
+      packages=['hngoption'],
+      zip_safe=False)
+
