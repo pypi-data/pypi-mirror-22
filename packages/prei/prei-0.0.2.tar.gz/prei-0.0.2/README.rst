63490284e5f3d4cf349c610c07d011b5cb98a202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:25 +0000

    Add package prei/0.0.2/prei-0.0.2.tar.gz

---
+++ packages/prei/prei-0.0.2.tar.gz/prei-0.0.2/README.rst
@@ -0,0 +1,30 @@
+prei
+====
+
+Python Remote Execution Interface
+
+Installation
+------------
+
+From the project root directory::
+
+    $ python setup.py install
+
+Usage
+-----
+
+Simply run it::
+
+    $ prei
+
+Use --help/-h to view info on the arguments::
+
+    $ prei --help
+
+Release Notes
+-------------
+
+:0.0.2:
+    Fix issue where it locked on running commands that are closed
+:0.0.1:
+    Project created
---
+++ packages/prei/prei-0.0.2.tar.gz/prei-0.0.2/setup.py
@@ -0,0 +1,59 @@
+import os
+from setuptools import setup
+
+# prei
+# Python Remote Execution Interface
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="prei",
+    version="0.0.2",
+    description="Python Remote Execution Interface",
+    author="Johan Nestaas",
+    author_email="johannestaas@gmail.com",
+    license="GPLv3+",
+    keywords="",
+    url="https://www.bitbucket.org/johannestaas/prei",
+    packages=['prei'],
+    package_dir={'prei': 'prei'},
+    long_description=read('README.rst'),
+    classifiers=[
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'License :: OSI Approved :: GNU General Public License v3 or later '
+        '(GPLv3+)',
+        'Environment :: Console',
+        'Environment :: X11 Applications :: Qt',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+    ],
+    install_requires=[
+        'paramiko',
+    ],
+    entry_points={
+        'console_scripts': [
+            'prei=prei:main',
+        ],
+    },
+    # If you get errors running setup.py install:
+    # zip_safe=False,
+    #
+    # For including non-python files:
+    # package_data={
+    #     'prei': ['templates/*.html'],
+    # },
+    # include_package_data=True,
+)
