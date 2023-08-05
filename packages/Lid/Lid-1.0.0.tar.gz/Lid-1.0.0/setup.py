4c3b3a3b4ebbb3916c4b75e47227607fafaf7939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:18 +0000

    Add package Lid/1.0.0/Lid-1.0.0.tar.gz

---
+++ packages/Lid/Lid-1.0.0.tar.gz/Lid-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = 'Lid',
+    version = '1.0.0',
+    description = 'tiny project',
+    keywords = ["lid", "python3"],
+    author = 'zouhao',
+    author_email = 'hellworld.zouhao@gmail.com',
+    maintainer = 'zouhao',
+    maintainer_email = 'helloworld.zouhao@gmail.com',
+    url = 'http://blog.owlsn.com',
+    license = 'MIT License',
+    platforms = ['any'],
+    classifiers = [
+        'Development Status :: 1 - Planning'
+    ],
+    packages = find_packages()
+)
