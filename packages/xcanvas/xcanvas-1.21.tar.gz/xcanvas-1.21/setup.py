37922318660521085e4b236fb558196db97ca9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package xcanvas/1.21/xcanvas-1.21.tar.gz

---
+++ packages/xcanvas/xcanvas-1.21.tar.gz/xcanvas-1.21/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='xcanvas',
+    version='1.21',
+    description='Extended tkinter canvas class',
+    url='http://github.com/samyzaf/xcanvas',
+    author='Samy Zafrany',
+    author_email='admin@samyzaf.com',
+    license='MIT',
+    packages = ['xcanvas'],
+    zip_safe=False,
+)
+
