89dab6de32d43b711becf353c15fe977c32b8cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:46 +0000

    Add package PyPreviewGenerator/0.1.19/PyPreviewGenerator-0.1.19.tar.gz

---
+++ packages/PyPreviewGenerator/PyPreviewGenerator-0.1.19.tar.gz/PyPreviewGenerator-0.1.19/PyPreviewGenerator/manager.py
@@ -0,0 +1,122 @@
+import hashlib
+import os
+
+import typing
+
+from PyPreviewGenerator.factory import PreviewBuilderFactory
+
+
+class PreviewManager(object):
+
+    cache_path = ''
+    factory = PreviewBuilderFactory()
+
+    def __init__(self, path: str) -> None:
+        try:
+            os.makedirs(path)
+        except OSError:
+            pass
+        if path[-1] != '/':
+            path = path + '/'
+        self.cache_path = path
+
+    def get_nb_page(self, file_path: str, cache_path: str) -> int:
+        mimetype = self.factory.get_document_mimetype(file_path)
+        builder = self.factory.get_preview_builder(mimetype)
+        preview_name = self.get_file_hash(file_path)
+        page_nb = builder.get_page_number(file_path, preview_name, cache_path)
+        return page_nb
+
+    def get_jpeg_preview(self, file_path: str, page: int=None, height: int=256, width: int=None, force: bool=False) -> str:
+
+        if width == None:
+            width = height
+
+        size = (height, width)
+
+        mimetype = self.factory.get_document_mimetype(file_path)
+        builder = self.factory.get_preview_builder(mimetype)
+        extension = '.jpeg'
+        preview_name=self.get_file_hash(file_path, size)
+        return builder.get_jpeg_preview(
+            file_path=file_path,
+            preview_name=preview_name,
+            page_id=page,
+            cache_path=self.cache_path,
+            extension=extension,
+            force=force,
+            size=size
+        )
+
+    def get_pdf_preview(self, file_path: str, page: int=-1, force: bool=False) -> str:
+
+        mimetype = self.factory.get_document_mimetype(file_path)
+        builder = self.factory.get_preview_builder(mimetype)
+        extension = '.pdf'
+        preview_name = self.get_file_hash(file_path)
+        return builder.get_pdf_preview(
+            file_path=file_path,
+            preview_name=preview_name,
+            cache_path=self.cache_path,
+            force=force,
+            extension=extension,
+            page=page,
+        )
+
+    def get_text_preview(self, file_path: str, force: bool=False) -> str:
+
+        mimetype = self.factory.get_document_mimetype(file_path)
+        builder = self.factory.get_preview_builder(mimetype)
+        extension = '.txt'
+        preview_name = self.get_file_hash(file_path)
+        return builder.get_text_preview(
+            file_path=file_path,
+            preview_name=preview_name,
+            cache_path=self.cache_path,
+            force=force,
+            extension=extension
+        )
+
+    def get_html_preview(self, file_path: str, force: bool=False) -> str:
+        mimetype = self.factory.get_document_mimetype(file_path)
+        builder = self.factory.get_preview_builder(mimetype)
+        extension = '.html'
+        preview_name = self.get_file_hash(file_path)
+        return builder.get_html_preview(
+            file_path=file_path,
+            preview_name=preview_name,
+            cache_path=self.cache_path,
+            force=force,
+            extension=extension,
+        )
+
+    def get_json_preview(self, file_path: str, force: bool=False) -> str:
+        mimetype = self.factory.get_document_mimetype(file_path)
+        builder = self.factory.get_preview_builder(mimetype)
+        extension = '.json'
+        preview_name = self.get_file_hash(file_path)
+        return builder.get_json_preview(
+            file_path=file_path,
+            preview_name=preview_name,
+            cache_path=self.cache_path,
+            force=force,
+            extension=extension
+        )
+
+    def get_file_hash(self, file_path: str, size: typing.Tuple[int, int]=None) -> str:
+        if '.' in file_path:
+            tab_str = file_path.split('.')
+            file_path=''
+            for index in range(0 ,len(tab_str)-1):
+                file_path = file_path + tab_str[index]
+        if size == None:
+            file_name = os.path.basename(file_path)
+        else:
+            file_name = '{fn}-{fh}x{fw}'.format(
+                fn=os.path.basename(file_path),
+                fh=str(size[0]),
+                fw=str(size[1]),
+            )
+
+        file_hash = hashlib.md5(file_path.encode('utf-8')).hexdigest()
+        return '{fn}-{fh}'.format(fn=file_name, fh=file_hash)
---
+++ packages/PyPreviewGenerator/PyPreviewGenerator-0.1.19.tar.gz/PyPreviewGenerator-0.1.19/setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# python setup.py sdist upload -r pypi
+
+
+try:
+    import logging
+    import multiprocessing
+    import os
+except:
+    pass
+
+import sys
+py_version = sys.version_info[:2]
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    documentation = open(os.path.join(here, 'README.rst')).read()
+except IOError:
+    documentation = ''
+
+testpkgs = []
+
+install_requires = ['python-magic', 'Wand', 'PyPDF2', 'Pillow']
+
+if py_version >= (3, 5):
+    install_requires.append("typing")
+
+setup(
+    name='PyPreviewGenerator',
+    version='0.1.19',
+    description='Generation of file previews',
+    long_description=documentation,
+    author='Algoo',
+    author_email='contact@algoo.fr',
+    url='https://github.com/algoo/preview-generator',
+    download_url='',
+    keywords=['preview', 'preview_generator', 'thumbnail', 'cache'],
+    classifiers=['Programming Language :: Python :: 3.4'],
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=install_requires,
+    python_requires='>=3.4',
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=testpkgs,
+    package_data={'preview_generator': [
+        'i18n/*/LC_MESSAGES/*.mo',
+        'templates/*/*',
+        'public/*/*'
+    ]}
+)
