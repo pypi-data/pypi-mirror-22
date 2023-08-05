bfa795438a68bc09190904c96a3a4d81dd13a30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:54 +0000

    Add package nomiepy/0.1/nomiepy-0.1.tar.gz

---
+++ packages/nomiepy/nomiepy-0.1.tar.gz/nomiepy-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'nomiepy',
+  packages = ['nomiepy'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A straightforward utility for retreiving Nomie data via CouchDB',
+  author = 'Noah Huber-Feely',
+  author_email = 'nhuberfeely@gmail.com',
+  url = 'https://github.com/huberf/nomiepy', # use the URL to the github repo
+  download_url = 'https://github.com/huberf/nomiepy/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['nomie'], # arbitrary keywords
+  classifiers = [],
+)
