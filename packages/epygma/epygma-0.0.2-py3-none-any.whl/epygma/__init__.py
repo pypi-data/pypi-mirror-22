333fa6cf8d6e775684865c748e31fe25b273fd22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:20 +0000

    Add package epygma/0.0.2/epygma-0.0.2-py3-none-any.whl

---
+++ packages/epygma/epygma-0.0.2-py3-none-any.whl/epygma/__init__.py
@@ -0,0 +1,9 @@
+__author__ = ["GALODE Alexandre", "FOURNIS Erwan"]
+__COPYRIGHT__ = "Copyright 2017, Epygma Project"
+__CREDITS__ = ["GALODE Alexandre", "FOURNIS Erwan"]
+__LICENCE__ = "GPL"
+__VERSION__ = "0.0.2"
+__maintainer__ = "GALODE Alexandre"
+__email__ = "deusyss@yahoo.fr"
+
+from . import epygma
