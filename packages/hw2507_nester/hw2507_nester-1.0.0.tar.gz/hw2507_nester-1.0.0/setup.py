c0bab3568dce93f336b660b49da282b388cb2784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:14 +0000

    Add package hw2507_nester/1.0.0/hw2507_nester-1.0.0.tar.gz

---
+++ packages/hw2507_nester/hw2507_nester-1.0.0.tar.gz/hw2507_nester-1.0.0/setup.py
@@ -0,0 +1,9 @@
+
+# coding: utf-8
+
+# In[8]:
+
+from distutils.core import setup
+setup(name = 'hw2507_nester',version='1.0.0',py_modules=['hw2507_nester'],
+     author = 'hw2507', author_email='hw2507@columbia.edu',url='',description='list printer')
+
