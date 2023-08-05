03f691483a733fba98c1f6a8e3c010c3989fdd86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:09 +0000

    Add package pytsite/0.99.16/pytsite-0.99.16-py3-none-any.whl

---
+++ packages/pytsite/pytsite-0.99.16-py3-none-any.whl/pytsite/widget/res/assets/js/pager.js
@@ -0,0 +1,44 @@
+define(['jquery', 'pytsite-http-api'], function ($, httpApi) {
+    return function (widget) {
+        var em = widget.em;
+        var ajax = em.data('ajax');
+        if (!ajax)
+            return;
+
+        var buttons = em.find('a');
+        var perPage = parseInt(em.data('perPage'));
+
+        function loadData(pageNum) {
+            var skip = (pageNum - 1) * perPage;
+            if (skip < 0)
+                skip = 0;
+
+            httpApi.get(ajax, {
+                skip: skip,
+                count: perPage
+            }).done(function (r) {
+                $(window).trigger('pytsite.widget.select.pager.load', [r, pageNum, widget]);
+            }).fail(function (r) {
+                $(window).trigger('pytsite.widget.select.pager.error', [r, pageNum, widget]);
+            });
+        }
+
+        buttons.click(function (e) {
+            e.preventDefault();
+
+            var btn = $(this);
+            var li = btn.closest('li');
+
+            if (li.hasClass('active'))
+                return;
+
+            em.find('li').removeClass('active');
+            li.addClass('active');
+
+            loadData(parseInt(btn.data('page')));
+        });
+
+        // Initial data load
+        loadData(parseInt(em.data('currentPage')));
+    }
+});
