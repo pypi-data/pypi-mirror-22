61f5bd60dc89fdd7dc6d4c452677d59d8e6a2d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:12 +0000

    Add package uu-fibonacci/1.0/uu-fibonacci-1.0.tar.gz

---
+++ packages/uu-fibonacci/uu-fibonacci-1.0.tar.gz/uu-fibonacci-1.0/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup
+
+setup(name='uu-fibonacci',
+      version='1.0',
+      description='Example package that calculates fibonacci numbers',
+      url='https://github.com/uu-python/travis-fib',
+      author='Filipe Maia',
+      author_email='filipe.c.maia@gmail.com',
+      license='BSD',
+      py_modules=['fib'],
+      packages=[])
