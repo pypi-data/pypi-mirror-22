ff76328cdebad63629bc2ee7ff24e18ae20ef25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:26 +0000

    Add package hepcrawl/0.3.17/hepcrawl-0.3.17.tar.gz

---
+++ packages/hepcrawl/hepcrawl-0.3.17.tar.gz/hepcrawl-0.3.17/hepcrawl/testlib/fixtures.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of hepcrawl.
+# Copyright (C) 2015, 2016, 2017 CERN.
+#
+# hepcrawl is a free software; you can redistribute it and/or modify it
+# under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+from __future__ import absolute_import, division, print_function
+
+import os
+
+from scrapy.http import Request, TextResponse
+from scrapy.selector import Selector
+
+
+def fake_response_from_file(file_name, url='http://www.example.com', response_type=TextResponse):
+    """Create a Scrapy fake HTTP response from a HTML file
+
+    :param file_name: The relative filename from the responses directory,
+                      but absolute paths are also accepted.
+    :param url: The URL of the response.
+    :param response_type: The type of the scrapy Response to be returned,
+                          depending on the Request (Response, TextResponse, etc)
+
+    :returns: A scrapy HTTP response which can be used for unittesting.
+    """
+    request = Request(url=url)
+
+    if not file_name[0] == '/':
+        file_path = get_responses_path(file_name)
+    else:
+        file_path = file_name
+
+    file_content = open(file_path, 'r').read()
+
+    response = response_type(
+        url=url,
+        request=request,
+        body=file_content,
+        **{'encoding': 'utf-8'}
+    )
+
+    return response
+
+
+def fake_response_from_string(text, url='http://www.example.com', response_type=TextResponse):
+    """Fake Scrapy response from a string."""
+    request = Request(url=url)
+    response = response_type(
+        url=url,
+        request=request,
+        body=text,
+        **{'encoding': 'utf-8'}
+        )
+
+    return response
+
+
+def get_node(spider, tag, response=None, text=None, rtype="xml"):
+    """Get the desired node in a response or an xml string."""
+    if response:
+        selector = Selector(response, type=rtype)
+    elif text:
+        selector = Selector(text=text, type=rtype)
+
+    spider._register_namespaces(selector)
+    node = selector.xpath(tag)
+    return node
+
+
+def get_responses_path(*path_chunks):
+    """
+    :param path_chunks: Optional extra path element to suffix the responses directory with.
+    :return: The absolute path to the responses and if path_chuncks provided the absolute
+     path to path chunks.
+
+    :Example:
+
+        >>> get_responses_path()
+        '/home/myuser/hepcrawl/tests/responses'
+
+        >>> get_responses_path('one', 'two')
+        '/home/myuser/hepcrawl/tests/responses/one/two'
+    """
+    project_root_dir = os.path.abspath(
+        os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            '..',
+            '..',
+        )
+    )
+    return os.path.join(project_root_dir, 'tests', 'unit', 'responses', *path_chunks)
---
+++ packages/hepcrawl/hepcrawl-0.3.17.tar.gz/hepcrawl-0.3.17/tests/functional/WSP/test_wsp.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of hepcrawl.
+# Copyright (C) 2017 CERN.
+#
+# hepcrawl is a free software; you can redistribute it and/or modify it
+# under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+"""Functional tests for WSP spider"""
+
+from __future__ import absolute_import, division, print_function
+
+import pytest
+import json
+import os
+import shutil
+
+from scrapyd_api import ScrapydAPI
+from time import sleep
+
+from hepcrawl.testlib.tasks import app as celery_app
+from hepcrawl.testlib.celery_monitor import CeleryMonitor
+
+
+def get_crawler_instance(crawler_host, *args, **kwargs):
+    """Return current crawler instance."""
+    return ScrapydAPI(
+        crawler_host,
+        *args,
+        **kwargs
+    )
+
+
+def override_generated_fields(record):
+    record['acquisition_source']['datetime'] = u'2017-04-03T10:26:40.365216'
+    record['acquisition_source']['submission_number'] = u'5652c7f6190f11e79e8000224dabeaad'
+
+    return record
+
+
+@pytest.fixture(scope="module")
+def expected_results():
+    file_name = 'fixtures/wsp_smoke_records.json'
+    responses_dir = os.path.dirname(os.path.realpath(__file__))
+    response_file = os.path.join(responses_dir, file_name)
+
+    with open(response_file) as fd:
+        expected_data = json.load(fd)
+
+    return expected_data
+
+
+@pytest.fixture(scope="function")
+def set_up_ftp_environment():
+    netrc_location = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        os.path.join('fixtures', 'ftp_server', '.netrc')
+    )
+
+    yield {
+        'CRAWLER_HOST_URL': 'http://scrapyd:6800',
+        'CRAWLER_PROJECT': 'hepcrawl',
+        'CRAWLER_ARGUMENTS': {
+            'ftp_host': 'ftp_server',
+            'ftp_netrc': netrc_location,
+        }
+    }
+
+    clean_dir()
+
+
+@pytest.fixture(scope="function")
+def set_up_local_environment():
+    package_location = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        os.path.join('fixtures', 'ftp_server', 'WSP')
+    )
+
+    yield {
+        'CRAWLER_HOST_URL': 'http://scrapyd:6800',
+        'CRAWLER_PROJECT': 'hepcrawl',
+        'CRAWLER_ARGUMENTS': {
+            'package_path': package_location,
+        }
+    }
+
+    remove_generated_files(package_location)
+
+
+def remove_generated_files(package_location):
+    clean_dir()
+
+    _, dirs, files = next(os.walk(package_location))
+    for dir_name in dirs:
+        clean_dir(os.path.join(package_location, dir_name))
+    for file_name in files:
+        if not file_name.endswith('.zip'):
+            os.unlink(os.path.join(package_location, file_name))
+
+
+def clean_dir(path='/tmp/WSP/'):
+    shutil.rmtree(path, ignore_errors=True)
+
+
+def test_wsp_ftp(set_up_ftp_environment, expected_results):
+    crawler = get_crawler_instance(set_up_ftp_environment.get('CRAWLER_HOST_URL'))
+
+    # The test must wait until the docker environment is up (takes about 10 seconds).
+    sleep(10)
+
+    results = CeleryMonitor.do_crawl(
+        app=celery_app,
+        monitor_timeout=5,
+        monitor_iter_limit=100,
+        crawler_instance=crawler,
+        project=set_up_ftp_environment.get('CRAWLER_PROJECT'),
+        spider='WSP',
+        settings={},
+        **set_up_ftp_environment.get('CRAWLER_ARGUMENTS')
+    )
+
+    gotten_results = [override_generated_fields(result) for result in results]
+    expected_results = [override_generated_fields(expected) for expected in expected_results]
+
+    assert gotten_results == expected_results
+
+
+def test_wsp_local_package_path(set_up_local_environment, expected_results):
+    crawler = get_crawler_instance(set_up_local_environment.get('CRAWLER_HOST_URL'))
+
+    results = CeleryMonitor.do_crawl(
+        app=celery_app,
+        monitor_timeout=5,
+        monitor_iter_limit=100,
+        crawler_instance=crawler,
+        project=set_up_local_environment.get('CRAWLER_PROJECT'),
+        spider='WSP',
+        settings={},
+        **set_up_local_environment.get('CRAWLER_ARGUMENTS')
+    )
+
+    gotten_results = [override_generated_fields(result) for result in results]
+    expected_results = [override_generated_fields(expected) for expected in expected_results]
+
+    assert gotten_results == expected_results
