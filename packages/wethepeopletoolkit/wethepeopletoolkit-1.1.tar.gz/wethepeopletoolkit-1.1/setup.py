59c3454553f531c674bcdba8664d6edab9287d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:08 +0000

    Add package wethepeopletoolkit/1.1/wethepeopletoolkit-1.1.tar.gz

---
+++ packages/wethepeopletoolkit/wethepeopletoolkit-1.1.tar.gz/wethepeopletoolkit-1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import sys
+
+try:
+  from PyQt4.QtCore import QT_VERSION_STR
+except ImportError:
+  sys.exit("PyQt4 is required to install this package (see README.md for installation instructions)")
+
+setup(
+  name = "wethepeopletoolkit",
+  version = "1.1",
+  author = "Alex Peattie",
+  author_email = "me@alexpeattie.com",
+  description = ("A project for analyzing and visualizing data from the Obama-era 'We the People' petitions site."),
+  license = "MIT",
+  keywords = "wethepeople petitions datascience analysis",
+  url = "https://github.com/alexpeattie/wethepeopletoolkit",
+  download_url = 'https://github.com/alexpeattie/wethepeopletoolkit/archive/1.1.tar.gz',
+  install_requires=[
+    'bs4',
+    'click',
+    'pandas',
+    'numpy',
+    'bitstring',
+    'base58',
+    'matplotlib',
+    'findspark',
+    'sklearn',
+    'scipy'
+  ],
+  pymodules=['main'],
+  entry_points='''
+    [console_scripts]
+    wethepeopletoolkit=main:cli
+  '''
+)
