e91544bae6b88ecbc7a1397b1035bb5964ee89d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:48 +0000

    Add package PIC/0.0.1/PIC-0.1.0.tar.gz

---
+++ packages/PIC/PIC-0.1.0.tar.gz/PIC-0.1.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# coding: utf-8
+from setuptools import find_packages, setup
+
+with open('README.rst', encoding='utf-8') as fp:
+    readme = fp.read()
+setup(
+    name='PIC',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['pillow'],
+    url='https://github.com/DxqS/PIC',
+    license='MIT',
+    author='Dxq',
+    author_email='chk0125@126.com',
+    description='图片合成 API',
+    long_description=readme,
+    keywords=[
+        'PIL',
+        'pillow'
+    ]
+)
