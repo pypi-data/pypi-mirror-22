1160da52cd7dc14c151e4e59370289db9bf4af42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:58 +0000

    Add package cyaron/0.2.1/cyaron-0.2.1.tar.gz

---
+++ packages/cyaron/cyaron-0.2.1.tar.gz/cyaron-0.2.1/cyaron/utils.py
@@ -0,0 +1,23 @@
+def ati(array):
+    """ati(array) -> list
+        Convert all the elements in the array and return them in a list.
+    """
+    return [int(i) for i in array]
+
+
+def list_like(data):
+    """list_like(data) -> bool
+        Judge whether the object data is like a list or a tuple.
+        object data -> the data to judge
+    """
+    return isinstance(data, tuple) or isinstance(data, list)
+
+
+def strtolines(str):
+    lines = str.split('\n')
+    for i in range(len(lines)):
+        lines[i] = lines[i].rstrip()
+
+    while len(lines) > 0 and len(lines[len(lines) - 1]) == 0:
+        del lines[len(lines) - 1]
+    return lines
