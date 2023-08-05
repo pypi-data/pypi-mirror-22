fbafda7ee004fdbf5c6278b65c529063ad193b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:16 +0000

    Add package iologger/1.1.6/iologger-1.1.6-py2.py3-none-any.whl

---
+++ packages/iologger/iologger-1.1.6-py2.py3-none-any.whl/iologger/__init__.py
@@ -0,0 +1,81 @@
+"""
+Decorator which logs the wrapped function/method.
+
+The following are logged:
+    1. name of the function called
+    2. arg(s) passed for the function called (if any)
+    3. kwarg(s) passed for the function called (if any)
+    4. execution time of the function called (in seconds)
+
+    * also catches and logs any exceptions raised gracefully.
+"""
+
+import time
+import json
+from functools import partial, wraps
+from collections import OrderedDict
+
+from logbook import Logger
+
+__version__ = "1.1.6"
+
+
+class FunctionExecutionError(BaseException):
+    pass
+
+
+def iologger(function=None, catch_exceptions=True):
+    """
+    Decorator which logs the wrapped function/method.
+
+    The following are logged:
+        1. name of the function called
+        2. arg(s) passed for the function called (if any)
+        3. kwarg(s) passed for the function called (if any)
+        4. execution time of the function called (in seconds)
+
+        * also catches and logs any exceptions raised gracefully.
+
+    :param catch_exceptions: will catch exceptions gracefully if true.
+    :param function: func to run and all its args/kwargs.
+    :return: returns func(*args, **kwargs)
+    """
+
+    if function is None:
+        return partial(iologger, catch_exceptions=catch_exceptions)
+
+    logger = Logger()
+
+    @wraps(function)
+    def wrapper(*args, **kwargs) -> None:
+
+        def run_function(func):
+            arg_dict = OrderedDict()
+            arg_dict['args'] = args
+            arg_dict['kwargs'] = kwargs
+            result = None
+
+            start = time.time()
+            try:
+                result = func(*args, **kwargs)
+                arg_dict['returned'] = str(result)
+            except Exception as e:
+                result = "Exception: {}".format(e)
+                arg_dict['returned'] = str(result)
+                arg_dict['exception'] = str(e)
+                if not catch_exceptions:
+                    raise FunctionExecutionError
+            finally:
+                end = time.time()
+                arg_dict['exec_time'] = "{:f} seconds".format(end - start)
+
+                logger.info(json.dumps(arg_dict))
+                return result
+
+        if catch_exceptions:
+            with logger.catch_exceptions():
+                return run_function(function)
+        else:
+            return run_function(function)
+
+    return wrapper
