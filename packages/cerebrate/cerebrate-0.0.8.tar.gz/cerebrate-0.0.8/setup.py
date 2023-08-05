baba31f653aa924686e4b9945f2f5049f01b8eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:08 +0000

    Add package cerebrate/0.0.8/cerebrate-0.0.8.tar.gz

---
+++ packages/cerebrate/cerebrate-0.0.8.tar.gz/cerebrate-0.0.8/setup.py
@@ -0,0 +1,43 @@
+import sys
+import os
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        if sys.platform == "darwin":
+            os.system('''echo 'eval "$(_CEREBRATE_COMPLETE=source cerebrate)"' >> ~/.profile''')
+        elif sys.platform.startswith("linux"):
+            os.system('''echo 'eval "$(_CEREBRATE_COMPLETE=source cerebrate)"' >> ~/.bashrc''')
+
+        if os.path.exists(os.path.expanduser("~/.zshrc")):
+            os.system('''echo 'eval "$(_CEREBRATE_COMPLETE=source cerebrate)"' >> ~/.zshrc''')
+
+        install.run(self)
+
+setup(
+    name='cerebrate',
+    version='0.0.8',
+    description='Offline Replay Manager for StarCraft II',
+    author='Hugo Wainwright',
+    author_email='wainwrighthugo@gmail.com',
+    url='https://github.com/frugs/cerebrate',
+    download_url='https://github.com/frugs/cerebrate/tarball/0.0.8',
+    packages=find_packages(),
+    py_modules=['cerebrate'],
+    include_package_data=True,
+    install_requires=[
+        'tinydb',
+        'click',
+        'ujson'
+    ],
+    entry_points='''
+        [console_scripts]
+        cerebrate=cerebrate:cli
+    ''',
+    cmdclass={
+        'install': PostInstallCommand,
+    }
+)
