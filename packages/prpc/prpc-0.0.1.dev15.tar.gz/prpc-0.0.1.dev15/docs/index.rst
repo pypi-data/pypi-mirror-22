c3d7e1df76c8e005b880b3c4cfb913ab0a472c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:44 +0000

    Add package prpc/0.0.1.dev15/prpc-0.0.1.dev15.tar.gz

---
+++ packages/prpc/prpc-0.0.1.dev15.tar.gz/prpc-0.0.1.dev15/docs/index.rst
@@ -0,0 +1,15 @@
+prpc: buh rpc
+=============================================================================
+
+Contents
+--------
+
+.. toctree::
+   :maxdepth: 2
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/prpc/prpc-0.0.1.dev15.tar.gz/prpc-0.0.1.dev15/prpc/client.py
@@ -0,0 +1,82 @@
+import asyncio
+
+import structlog
+
+from prpc.protocol import base, msgpack
+
+
+log = structlog.get_logger()
+
+default_protocol = msgpack.MsgpackProtocol
+
+
+class ProtocolClient(asyncio.Protocol):
+    def __init__(self, protocol_factory=default_protocol):
+        self.protocol = protocol_factory()
+
+        self.futures = {}
+
+    def connection_made(self, transport):
+        self.transport = transport
+        peername = transport.get_extra_info('peername')
+        log.debug('connection_made', peername=peername)
+
+    def data_received(self, data):
+        for message in self.protocol.feed(data):
+            self.handle_message(message)
+
+    def handle_message(self, message):
+        assert isinstance(message, base.Response)
+
+        log.debug('handle_message', message=message)
+
+        if message.id in self.futures:
+            # fixme: handle errors
+            self.futures[message.id].set_result(message.result)
+            del self.futures[message.id]
+
+    def cast(self, method, *args, **kwargs):
+        message = base.Notification(
+            method=method,
+            params=args,
+            kparams=kwargs
+        )
+
+        data = self.protocol.pack(message)
+        self.transport.write(data)
+
+    async def call(self, method, *args, **kwargs):
+        message = base.Request(
+            method=method,
+            params=args,
+            kparams=kwargs
+        )
+
+        data = self.protocol.pack(message)
+
+        future = asyncio.Future()
+        self.futures[message.id] = future
+
+        self.transport.write(data)
+
+        return await future
+
+
+class ClientFactory:
+    def __init__(self, protocol=ProtocolClient, loop=None):
+        self.loop = loop
+        if not self.loop:
+            self.loop = asyncio.get_event_loop()
+
+        self.protocol = protocol
+
+    def connect(self, host, port):
+        client = self.protocol()
+
+        coro = self.loop.create_connection(lambda: client, host, port)
+        # fixme: maybe we don't actually want to do this?
+        self.loop.run_until_complete(coro)
+
+        #uri = 'prpc://{host}:{port}'.format(host=host, port=port)
+
+        return client
