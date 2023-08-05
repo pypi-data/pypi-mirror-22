2f002964cb540d338e73df8090f2bf9425d1778e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:38 +0000

    Add package nester-Justin_LIN/1.0.0/nester-Justin_LIN-1.0.0.zip

---
+++ packages/nester-Justin_LIN/nester-Justin_LIN-1.0.0.zip/nester-Justin_LIN-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name        ='nester-Justin_LIN',
+        version     ='1.0.0',
+        py_modules  =['nester'],
+        author      ='Justin LIN',
+        author_email='lin_zhe@icloud.com',
+        url         ='http://www.qq.com',
+        description ='Justin_LIN',
+    )
