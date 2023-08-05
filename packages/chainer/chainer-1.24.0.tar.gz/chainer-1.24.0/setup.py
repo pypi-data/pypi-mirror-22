a97d1625a8e1f9052c101bd84690023d8c691a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:39 +0000

    Add package chainer/1.24.0/chainer-1.24.0.tar.gz

---
+++ packages/chainer/chainer-1.24.0.tar.gz/chainer-1.24.0/setup.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+import sys
+
+import chainer_setup_build
+
+
+if sys.version_info[:3] == (3, 5, 0):
+    if not int(os.getenv('CHAINER_PYTHON_350_FORCE', '0')):
+        msg = """
+Chainer does not work with Python 3.5.0.
+
+We strongly recommend to use another version of Python.
+If you want to use Chainer with Python 3.5.0 at your own risk,
+set 1 to CHAINER_PYTHON_350_FORCE environment variable."""
+        print(msg)
+        sys.exit(1)
+
+
+setup_requires = []
+install_requires = [
+    'filelock',
+    'nose',
+    'numpy>=1.9.0',
+    'protobuf>=2.6.0',
+    'six>=1.9.0',
+]
+
+ext_modules = chainer_setup_build.get_ext_modules()
+
+setup(
+    name='chainer',
+    version='1.24.0',
+    description='A flexible framework of neural networks',
+    author='Seiya Tokui',
+    author_email='tokui@preferred.jp',
+    url='http://chainer.org/',
+    license='MIT License',
+    packages=['chainer',
+              'chainer.dataset',
+              'chainer.datasets',
+              'chainer.functions',
+              'chainer.functions.activation',
+              'chainer.functions.array',
+              'chainer.functions.caffe',
+              'chainer.functions.connection',
+              'chainer.functions.evaluation',
+              'chainer.functions.loss',
+              'chainer.functions.math',
+              'chainer.functions.noise',
+              'chainer.functions.normalization',
+              'chainer.functions.pooling',
+              'chainer.functions.theano',
+              'chainer.functions.util',
+              'chainer.function_hooks',
+              'chainer.iterators',
+              'chainer.initializers',
+              'chainer.links',
+              'chainer.links.activation',
+              'chainer.links.caffe',
+              'chainer.links.caffe.protobuf2',
+              'chainer.links.caffe.protobuf3',
+              'chainer.links.connection',
+              'chainer.links.loss',
+              'chainer.links.model',
+              'chainer.links.model.vision',
+              'chainer.links.normalization',
+              'chainer.links.theano',
+              'chainer.optimizers',
+              'chainer.serializers',
+              'chainer.testing',
+              'chainer.training',
+              'chainer.training.extensions',
+              'chainer.training.triggers',
+              'chainer.training.updaters',
+              'chainer.utils',
+              'cupy',
+              'cupy.binary',
+              'cupy.core',
+              'cupy.creation',
+              'cupy.cuda',
+              'cupy.ext',
+              'cupy.indexing',
+              'cupy.io',
+              'cupy.linalg',
+              'cupy.logic',
+              'cupy.manipulation',
+              'cupy.math',
+              'cupy.padding',
+              'cupy.prof',
+              'cupy.random',
+              'cupy.sorting',
+              'cupy.statistics',
+              'cupy.testing'],
+    package_data={
+        'cupy': ['core/carray.cuh'],
+    },
+    zip_safe=False,
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    tests_require=['mock',
+                   'nose'],
+    ext_modules=ext_modules,
+)
