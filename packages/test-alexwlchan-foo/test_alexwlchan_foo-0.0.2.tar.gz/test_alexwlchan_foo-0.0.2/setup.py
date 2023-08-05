12d61a76e5d4069984f1abd0f0c01d5e1c8fc7c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:20 +0000

    Add package test-alexwlchan-foo/0.0.2/test_alexwlchan_foo-0.0.2.tar.gz

---
+++ packages/test-alexwlchan-foo/test_alexwlchan_foo-0.0.2.tar.gz/test_alexwlchan_foo-0.0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='test_alexwlchan_foo',
+    version='0.0.2',
+    description='A test package',
+    author='Alex Chan',
+    author_email='alex@alexwlchan.net',
+    url='https://github.com/alexwlchan/pyup-testing',
+    license='MIT'
+)
