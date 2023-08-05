c66dc989dc10d67b9ab811662fe699ca5c022314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:41 +0000

    Add package nodeconductor-openstack/0.25.0/nodeconductor-openstack-0.25.0.tar.gz

---
+++ packages/nodeconductor-openstack/nodeconductor-openstack-0.25.0.tar.gz/nodeconductor-openstack-0.25.0/src/nodeconductor_openstack/openstack/log.py
@@ -0,0 +1,16 @@
+from nodeconductor.logging.loggers import EventLogger, event_logger
+
+
+class TenantQuotaLogger(EventLogger):
+    quota = 'quotas.Quota'
+    tenant = 'openstack.Tenant'
+    limit = float
+
+    class Meta:
+        event_types = ('openstack_tenant_quota_limit_updated',)
+        event_groups = {
+            'resources': event_types,
+        }
+
+
+event_logger.register('openstack_tenant_quota', TenantQuotaLogger)
