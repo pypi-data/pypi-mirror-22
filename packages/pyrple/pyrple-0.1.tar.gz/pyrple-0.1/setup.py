94f53d11b491d49483f4aeaac78de1a0cfa44460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:37 +0000

    Add package pyrple/0.1/pyrple-0.1.tar.gz

---
+++ packages/pyrple/pyrple-0.1.tar.gz/pyrple-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pyrple',
+      version='0.1',
+      description='Python wrapper to make simple get requests with the Purple Wifi API.',
+      url='https://github.com/woblers/pyrple',
+      author='Toby Petty',
+      author_email='me@tobypetty.com',
+      license='MIT',
+      packages=['pyrple'],
+      zip_safe=False)
