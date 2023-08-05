f5326e1eb4760653cbf3f9ff2dfa0256b04d4ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:08 +0000

    Add package test_yl/1.0.0/test_yl-1.0.0.zip

---
+++ packages/test_yl/test_yl-1.0.0.zip/test_yl-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'test_yl',
+        version = '1.0.0',
+        py_modules = ['test_yl'],
+        author = 'hhe',
+        author_email = '76724223@qq.com',
+        url = 'https://www.baidu.com/',
+        description = 'woaini',
+    )
