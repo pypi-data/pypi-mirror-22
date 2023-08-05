f50b2e71cacabe3d8a87eb6dd5cfd7b17cc5fc11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:56 +0000

    Add package example-foo/1.0.1/example-foo-1.0.1.tar.gz

---
+++ packages/example-foo/example-foo-1.0.1.tar.gz/example-foo-1.0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    author='Doug Beck',
+    author_email='doug@douglasbeck.com',
+    name='example-foo',
+    version='1.0.1',
+    url='https://github.com/beck/sandbox/tree/example-foo',
+)
