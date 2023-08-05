cf7a5f7bb38c84c9e9f4d75472e51d2389dd5b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package restfulpy/0.3.1/restfulpy-0.3.1-py3-none-any.whl

---
+++ packages/restfulpy/restfulpy-0.3.1-py3-none-any.whl/restfulpy/cli/utils.py
@@ -0,0 +1,62 @@
+
+from datetime import datetime
+
+
+class ProgressBar:
+
+    def __init__(self, total):
+        self._value = 0
+        self.total = total
+        self.start_time = None
+
+    def increment(self):
+        self._value += 1
+        self._invalidate()
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, v):
+        self._value = v
+        self._invalidate()
+
+    @property
+    def percent(self):
+        return self._value * 100 // self.total
+
+    @property
+    def time(self):
+        td = datetime.now() - self.start_time
+        return '%.2d:%.2d' % (td.seconds // 60, td.seconds % 60)
+
+    @property
+    def estimated_time(self):
+        if self.value is 0:
+            et = 0
+        else:
+            td = datetime.now() - self.start_time
+            et = td.total_seconds() * (self.total / self.value - 1)
+        return '%.2d:%.2d' % (int(et) // 60, int(et) % 60)
+
+    @property
+    def marks(self):
+        scale = 2
+        v = self.percent // scale
+        return '%s%s' % ('#' * int(v), '.' * int(100 // scale - v))
+
+    def _invalidate(self):
+        detailed = ('%%%dd/%%d' % len(str(self.total))) % (self._value, self.total)
+        percent = '%3d%%' % self.percent
+        progress = '|%s|' % self.marks
+        print(' '.join((detailed, percent, progress, self.time, self.estimated_time)), end='\r', flush=True)
+
+    def __enter__(self):
+        self.start_time = datetime.now()
+        self._invalidate()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._invalidate()
+        print()
