b8d10695c2edd08f2fcafbd85a79392000ea9657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:52 +0000

    Add package python-tuya/0.0.2/python-tuya-0.0.2.tar.gz

---
+++ packages/python-tuya/python-tuya-0.0.2.tar.gz/python-tuya-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='python-tuya',
+      version='0.0.2',
+      description='Control and read Tuya based devices',
+      url='http://github.com/python-tuya/python-tuya',
+      author='Jesse Hills',
+      license='MIT',
+      install_requires=['paho-mqtt', 'pycrypto'],
+      packages=find_packages(exclude=["dist"]),
+      zip_safe=True)
