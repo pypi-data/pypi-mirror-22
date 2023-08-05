90340a7736ce78fd5d759fc17a2ac4429dfcc634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:27 +0000

    Add package sunnyjudge/0.1/sunnyjudge-0.1.tar.gz

---
+++ packages/sunnyjudge/sunnyjudge-0.1.tar.gz/sunnyjudge-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sunnyjudge',
+	version='0.1',
+	description='開箱即用的司法陽光網判決',
+	url='https://github.com/yudazilian/SunnyJudge',
+	author='yudazilian',
+	author_email='davisfreeman1015@gmail.com',
+	license='MIT',
+	packages=['sunnyjudge'],
+	install_requires=['requests',],
+	zip_safe=False)
