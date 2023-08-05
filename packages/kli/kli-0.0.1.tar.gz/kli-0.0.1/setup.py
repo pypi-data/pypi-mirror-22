7c2534754c00815ad9b0a975d908853db4b67ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:17 +0000

    Add package kli/0.0.1/kli-0.0.1.tar.gz

---
+++ packages/kli/kli-0.0.1.tar.gz/kli-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'kli', 
+    version = '0.0.1', 
+    packages = find_packages(exclude=['venv', 'backup']),
+    license = 'MIT',
+    url = 'https://github.com/tranquilo12/kl',
+    author = 'Shriram Sunder',
+    author_email = 'shriram.sunder121091@gmail.com',
+    install_requires = [
+        'tqdm', 
+        'pprint',
+        'argcomplete',
+        'bs4', 
+        'click>=6', 
+        'cryptography'
+        ],    
+    entry_points = '''
+        [console_scripts]
+        kli=main:kli
+    ''',
+    data_files=[('', ['comp.json']),
+                ('', ['keys.txt'])]
+        )
