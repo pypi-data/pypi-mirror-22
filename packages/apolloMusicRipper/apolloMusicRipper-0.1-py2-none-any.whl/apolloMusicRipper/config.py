7e6de2bceec7affb6c53ddfd628864b1c5724cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package apolloMusicRipper/0.1/apolloMusicRipper-0.1-py2-none-any.whl

---
+++ packages/apolloMusicRipper/apolloMusicRipper-0.1-py2-none-any.whl/apolloMusicRipper/config.py
@@ -0,0 +1,13 @@
+DIR_UNTAGGED = "songs/untaggedSongs/"
+DIR_TAGGED = "songs/taggedSongs/"
+DIR_FAILED_TAG = "songs/failedTag/"
+
+
+##set up pipeline
+#downloader = TODO:
+
+#list tagger options in order of preference
+tagger = ["spotify"]
+
+#post production
+filters = []
