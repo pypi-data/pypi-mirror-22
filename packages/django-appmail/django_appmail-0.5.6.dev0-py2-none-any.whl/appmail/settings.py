b31921e99b84bf375d52f2b1c02887edf08e0d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:37 +0000

    Add package django-appmail/0.5.6.dev0/django_appmail-0.5.6.dev0-py2-none-any.whl

---
+++ packages/django-appmail/django_appmail-0.5.6.dev0-py2-none-any.whl/appmail/settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+
+# Validate that EmailTemplate can be rendered without
+# error on each save. Defaults to True.
+VALIDATE_ON_SAVE = getattr(settings, 'APPMAIL_VALIDATE_ON_SAVE', True)
+# used as the from_email address for sending emails
+DEFAULT_SENDER = getattr(settings, 'APPMAIL_DEFAULT_SENDER', None)
+# if True then add X-Appmail-* headers to outgoung email objects
+ADD_EXTRA_HEADERS = getattr(settings, 'APPMAIL_ADD_HEADERS', True)
+# list of context processor functions applied on each render
+CONTEXT_PROCESSORS = [import_string(s) for s in getattr(settings, 'APPMAIL_CONTEXT_PROCESSORS', [])]  # noqa
