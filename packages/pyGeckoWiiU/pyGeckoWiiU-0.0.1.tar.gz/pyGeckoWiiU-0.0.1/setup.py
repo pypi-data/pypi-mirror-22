837eee63226a8f8fd85b79dccf4d1f2bf7f7a5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:52 +0000

    Add package pyGeckoWiiU/0.0.1/pyGeckoWiiU-0.0.1.tar.gz

---
+++ packages/pyGeckoWiiU/pyGeckoWiiU-0.0.1.tar.gz/pyGeckoWiiU-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyGeckoWiiU',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/RedSoloFox/pyGecko',
+    license='MIT',
+    author='redsolofox',
+    author_email='RedSoloFox@gmail.com',
+    description='Python Library for use with pygecko on the Wii U'
+)
