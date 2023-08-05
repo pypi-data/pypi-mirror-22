6707312986d253893340f81c167ec260fb9cee52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:30 +0000

    Add package pls2upl/0.0.5/pls2upl-0.0.5.tar.gz

---
+++ packages/pls2upl/pls2upl-0.0.5.tar.gz/pls2upl-0.0.5/pls2upl/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Stavros Korokithakis'
+__email__ = 'hi@stavros.io'
+__version__ = '0.0.5'
+
+from .functions import look_up_track, read_pls, write_upl  # noqa
