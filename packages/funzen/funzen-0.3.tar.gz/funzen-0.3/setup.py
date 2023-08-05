3873cd13f59053bf53e285118b2445ad9ae346e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:13 +0000

    Add package funzen/0.3/funzen-0.3.tar.gz

---
+++ packages/funzen/funzen-0.3.tar.gz/funzen-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funzen',
+      version='0.3',
+      description='Fun jenis bahasa jawa',
+      url='http://github.com/blupy',
+      author='Blupy',
+      author_email='blupy.dev@yahoo.com',
+      license='MIT',
+      packages=['funzen'],
+      zip_safe=False)
