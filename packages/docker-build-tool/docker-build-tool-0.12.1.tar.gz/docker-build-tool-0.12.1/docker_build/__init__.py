669d8bc5102cbf0a59464f6e009ff6b84b3d3042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package docker-build-tool/0.12.1/docker-build-tool-0.12.1.tar.gz

---
+++ packages/docker-build-tool/docker-build-tool-0.12.1.tar.gz/docker-build-tool-0.12.1/docker_build/__init__.py
@@ -0,0 +1,36 @@
+from logging import StreamHandler, Formatter
+import logging
+
+__author__ = "Brian Bason"
+__version__ = "0.12.1"
+
+# create the default handler
+default_handler = StreamHandler()
+default_handler.setFormatter(
+    fmt=Formatter(
+        fmt="%(message)s"
+    )
+)
+
+# create the handler for the container console
+container_console_handler = StreamHandler()
+container_console_handler.setFormatter(
+    fmt=Formatter(
+        fmt="%(message)s",
+    )
+)
+
+# set the root logger
+root_logger = logging.getLogger()
+root_logger.addHandler(default_handler)
+root_logger.setLevel(logging.WARN)
+
+# set the logger for the builder
+docker_build_logger = logging.getLogger("docker_build")
+docker_build_logger.setLevel(logging.INFO)
+
+# set the logger for the container console output
+container_console_logger = logging.getLogger("container_console")
+container_console_logger.setLevel(logging.INFO)
+container_console_logger.addHandler(container_console_handler)
+container_console_logger.propagate = 0
