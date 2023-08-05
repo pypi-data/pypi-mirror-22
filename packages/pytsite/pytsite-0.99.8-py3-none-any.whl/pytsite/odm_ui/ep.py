40a0f5a30436c81653c22bfc50b5c052ef4ece6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package pytsite/0.99.8/pytsite-0.99.8-py3-none-any.whl

---
+++ packages/pytsite/pytsite-0.99.8-py3-none-any.whl/pytsite/odm_ui/ep.py
@@ -0,0 +1,46 @@
+"""ODM UI Endpoints.
+"""
+from pytsite import tpl as _tpl, http as _http, odm as _odm, admin as _admin, assetman as _assetman
+from . import _api, _browser
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+def browse(args: dict, inp: dict) -> str:
+    """Render browser.
+    """
+    _assetman.preload('pytsite.odm_ui@js/odm-ui-browser.js')
+
+    return _admin.render(_tpl.render('pytsite.odm_ui@browser', {
+        'table': _browser.Browser(args.get('model')).render()
+    }))
+
+
+def m_form(args: dict, inp: dict) -> str:
+    """Get entity create/modify form.
+    """
+    try:
+        frm = _api.get_m_form(args.get('model'), args['id'] if args.get('id') != 0 else None)
+        return _admin.render_form(frm)
+
+    except _odm.error.EntityNotFound:
+        raise _http.error.NotFound()
+
+
+def d_form(args: dict, inp: dict) -> str:
+    """Get entity(ies) deletion form.
+    """
+    model = args.get('model')
+
+    # Entities IDs to delete
+    ids = inp.get('ids', [])
+    if isinstance(ids, str):
+        ids = [ids]
+
+    # No required arguments has been received
+    if not model or not ids:
+        raise _http.error.NotFound()
+
+    return _admin.render_form(_api.get_d_form(model, ids))
