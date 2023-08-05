97a6b2c50a0e52a2c6ef5ad7ee5e827d15bc64b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:38 +0000

    Add package py-context/0.2/py_context-0.2-py3-none-any.whl

---
+++ packages/py-context/py_context-0.2-py3-none-any.whl/pycontext/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+
+__author__ = 'Miroslav Shubernetskiy'
+__author_email__ = 'miroslav.shubernetskiy@coxautoinc.com'
+__version__ = '0.2'
+__description__ = 'Python dict with stacked context data'
