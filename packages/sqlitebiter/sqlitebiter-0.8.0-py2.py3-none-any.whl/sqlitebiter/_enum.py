084d3aca14eb68a8762d974323cc7e26fb390355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:02 +0000

    Add package sqlitebiter/0.8.0/sqlitebiter-0.8.0-py2.py3-none-any.whl

---
+++ packages/sqlitebiter/sqlitebiter-0.8.0-py2.py3-none-any.whl/sqlitebiter/_enum.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+
+class Context(object):
+    IS_APPEND_TABLE = "IS_APPEND_TABLE"
+    LOG_LEVEL = "LOG_LEVEL"
+    VERBOSITY_LEVEL = "VERBOSITY_LEVEL"
+
+
+class ExitCode(object):
+    SUCCESS = 0
+    FAILED_LOADER_NOT_FOUND = 1
+    FAILED_CONVERT = 2
+    FAILED_HTTP = 3
+    NO_INPUT = 10
