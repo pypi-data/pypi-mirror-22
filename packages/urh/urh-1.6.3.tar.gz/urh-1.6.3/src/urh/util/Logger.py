c11b2e0ec01bd4cb26487e64a65085c4005c9da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:23 +0000

    Add package urh/1.6.3/urh-1.6.3.tar.gz

---
+++ packages/urh/urh-1.6.3.tar.gz/urh-1.6.3/src/urh/util/Logger.py
@@ -0,0 +1,16 @@
+import logging
+from urh.constants import color
+
+logging.basicConfig(level=logging.WARNING, format='[%(levelname)s::%(filename)s::%(funcName)s] %(message)s')
+
+logging_colors_per_level = {
+    logging.WARNING: color.YELLOW,
+    logging.ERROR: color.RED,
+    logging.CRITICAL: color.RED
+}
+
+for level, level_color in logging_colors_per_level.items():
+    logging.addLevelName(level, "{0}{1}{2}".format(level_color+color.BOLD, logging.getLevelName(level), color.END))
+
+logger = logging.getLogger("urh")
+logger.setLevel(logging.DEBUG)
