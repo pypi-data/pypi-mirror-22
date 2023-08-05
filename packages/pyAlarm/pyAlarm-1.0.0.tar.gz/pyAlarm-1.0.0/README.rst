e862abf1bf2c10f6df194f78381ef2d17c09b1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:40 +0000

    Add package pyAlarm/1.0.0/pyAlarm-1.0.0.tar.gz

---
+++ packages/pyAlarm/pyAlarm-1.0.0.tar.gz/pyAlarm-1.0.0/README.rst
@@ -0,0 +1,19 @@
+Alarm in Python.
+
+## INSTALLATION 
+pip install pyAlarm
+
+Functions :
+getInput() :: returns Alarm and current Time.
+runInBackground(Time,Alarm) :: Alarm & Time of type time.strftime("%H:%M")
+checkYTfile() :: returns False if File not found , True if found.
+runInteractive()
+
+Import :
+from alarm import begin
+
+Use :
+a,t = begin.getInput()
+begin.runInBackground(t,a)
+begin.checkYTfile()
+begin.runInteractive()
---
+++ packages/pyAlarm/pyAlarm-1.0.0.tar.gz/pyAlarm-1.0.0/alarm/begin.py
@@ -0,0 +1,66 @@
+import time
+import webbrowser
+import random
+import os
+from threading import Thread
+
+path = os.path.abspath('YT.txt')
+
+def getInput() :
+	print "What time do you want to wake up?"
+	print "Use this form.\nExample: 06:30"
+	Alarm = raw_input("> ")
+	Time = time.strftime("%H:%M")	
+	return Alarm,Time
+
+def checkYTfile():
+	#Check if the user has the YT.txt file in the same area as alarm.py
+	check = True 
+	if os.path.isfile("YT.txt") == False:
+		print "ERROR: YT.txt file not present"
+		print 'Alarm Video cannot be opened'
+		check = False
+	return check
+
+def runInBackground(Time,Alarm):
+	check = checkYTfile()
+	with open(path) as f:
+		content = f.readlines()
+	while Time != Alarm:
+		# print 'H'
+		Time = time.strftime("%H:%M")
+		time.sleep(0.95)
+	# print 'Working'
+	if Time == Alarm:
+		# print "Time to Wake up!"
+		if check :
+			random_video = random.choice(content)
+			webbrowser.open(random_video)
+	print 'Wake UP! its ',Alarm
+
+
+def runInteractive():
+	check = checkYTfile()
+	
+	#The User can set the time they want to wake up. The String the user puts in must be the same as the example to work.
+	Alarm,Time = getInput()
+	flag = False
+	ans = raw_input('Want to Run in background?(y/n)')
+	thread = Thread(target=runInBackground,name='AlarmBkg',args=(Time,Alarm,))
+	if ans == 'y' :
+		thread.start()
+	else :
+		with open(path) as f:
+			content = f.readlines()
+		while Time != Alarm:
+			os.system("clear")
+			time.sleep(0.19)
+			print "Alarm Time is " + Alarm
+			print "The time is   " + Time
+			Time = time.strftime("%H:%M")
+			time.sleep(0.8)
+		if Time == Alarm:
+			print "Time to Wake up!"
+			if check :
+				random_video = random.choice(content)
+				webbrowser.open(random_video)
