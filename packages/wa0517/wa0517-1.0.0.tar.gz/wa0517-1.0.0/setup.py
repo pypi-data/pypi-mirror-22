541a40181277a9175cba19f37cdb0ab7acf08d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:16 +0000

    Add package wa0517/1.0.0/wa0517-1.0.0.tar.gz

---
+++ packages/wa0517/wa0517-1.0.0.tar.gz/wa0517-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name         = 'wa0517',
+        version      = '1.0.0',
+        py_modules   = ['nester'],
+        author       = 'hfpython',
+        author_email = 'hfpython@headfirstlabs.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printter of nested lists',
+       
+      )
