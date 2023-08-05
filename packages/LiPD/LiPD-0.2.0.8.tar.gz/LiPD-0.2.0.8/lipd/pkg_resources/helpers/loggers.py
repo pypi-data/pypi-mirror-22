050fcd03634ba322c5ae2e26ff32ca8b5770c4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:50 +0000

    Add package LiPD/0.2.0.8/LiPD-0.2.0.8.tar.gz

---
+++ packages/LiPD/LiPD-0.2.0.8.tar.gz/LiPD-0.2.0.8/lipd/pkg_resources/helpers/loggers.py
@@ -0,0 +1,69 @@
+import datetime
+import logging
+from logging.config import dictConfig
+
+
+def update_changelog():
+    """
+    Create or update the changelog txt file. Prompt for update description.
+    :return None:
+    """
+    # description = input("Please enter a short description for this update:\n ")
+    description = 'Placeholder for description here.'
+
+    # open changelog file for appending. if doesn't exist, creates file.
+    with open('changelog.txt', 'a+') as f:
+        # write update line
+        f.write(str(datetime.datetime.now().strftime("%d %B %Y %I:%M%p")) + '\nDescription: ' + description)
+    return
+
+
+def create_logger(name):
+    """
+    Creates a logger with the below attributes.
+    :param str name: Name of the logger
+    :return obj: Logger
+    """
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'simple': {
+                'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+            },
+            'detailed': {
+                'format': '%(asctime)s %(module)-17s line:%(lineno)-4d '
+                          '%(levelname)-8s %(message)s'
+            },
+            'email': {
+                'format': 'Timestamp: %(asctime)s\nModule: %(module)s\n'
+                          'Line: %(lineno)d\nMessage: %(message)s'
+            },
+        },
+        'handlers': {
+            # 'stream': {
+            #     'level': 'DEBUG',
+            #     'class': 'logging.StreamHandler',
+            #     "formatter": "simple"
+            # },
+            "file": {
+                "level": "DEBUG",
+                "formatter": "simple",
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": "debug.log",
+                'mode': 'a',
+                'maxBytes': 30000,
+                'backupCount': 1
+            }
+        },
+        'loggers': {
+            '': {
+                # "handlers": ["stream", "file"],
+                "handlers": ["file"],
+                "level": "DEBUG",
+                'propagate': True
+            }
+        }
+    })
+    return logging.getLogger(name)
+
