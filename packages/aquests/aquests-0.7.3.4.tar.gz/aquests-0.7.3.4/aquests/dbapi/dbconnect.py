f94eee371dfc72605ff2884d432372a5544471bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:03 +0000

    Add package aquests/0.7.3.4/aquests-0.7.3.4.tar.gz

---
+++ packages/aquests/aquests-0.7.3.4.tar.gz/aquests-0.7.3.4/aquests/dbapi/dbconnect.py
@@ -0,0 +1,214 @@
+import asyncore 
+import re 
+import time 
+import sys 
+import threading 	
+
+DEBUG = False
+
+class OperationalError (Exception):
+	pass
+	
+class DBConnect:
+	zombie_timeout = 120
+	
+	def __init__ (self, address, params = None, lock = None, logger = None):
+		self.address = address
+		self.params = params		
+		self.dbname, self.user, self.password = "", "", ""
+		
+		if self.params:
+			self.dbname, auth = self.params
+			if auth:			
+				if type (auth) is not tuple:
+					self.user = auth
+				else:
+					try: self.user, self.password = auth
+					except ValueError: self.username = auth
+						
+		self.lock = lock
+		self.logger = logger
+		
+		self.request_count = 0
+		self.execute_count = 0
+		
+		self._cv = threading.Condition ()
+		
+		# need if there's not any request yet
+		self.active = 0
+		self.request = None
+		self.has_result = False
+		self.__history = []
+		self.__no_more_request = False
+		
+		self.set_event_time ()
+	
+	def get_proto (self):
+		# call by culster_manager
+		return None
+	
+	def handle_abort (self):		
+		# call by dist_call
+		self.close ()
+		
+	def close (self):		
+		self.request = None
+		self.set_active (False)		
+		addr = type (self.address) is tuple and ("%s:%d" % self.address) or str (self.address)
+		self.logger ("[info] .....dbo %s has been closed" % addr)	
+					
+	def get_history (self):
+		return self.__history
+		
+	def duplicate (self):
+		return self.__class__ (self.address, self.params, self.lock, self.logger)
+		
+	def clean_shutdown_control (self, phase, time_in_this_phase):
+		self.__no_more_request = True
+		if self.isactive ():
+			return 1
+		else:
+			self.handle_close ()
+			self.__no_more_request = False
+			return 0
+	
+	def empty_cursor (self):
+		if self.has_result:
+			self.fetchall ()			
+	
+	def maintern (self, object_timeout):
+		# query done but not used
+		if self.has_result and self.isactive () and time.time () - self.event_time > self.zombie_timeout:			
+			self.empty_cursor ()
+			self.set_active (False)
+			
+		if time.time () - self.event_time > object_timeout:
+			if not self.isactive ():
+				self.disconnect ()
+				return True # deletable
+		return False	
+	
+	def reconnect (self):
+		self.disconnect ()
+		self.connect ()
+	
+	def disconnect (self):
+		self.close ()
+	
+	def close_case (self):
+		raise NotImplementedError
+			
+	def set_active (self, flag, nolock = False):
+		if flag:
+			flag = time.time ()
+		else:
+			flag = 0
+		if nolock or self.lock is None:
+			self.active = flag
+			return			
+		self.lock.acquire ()
+		self.active = flag
+		self.request_count += 1
+		self.lock.release ()
+	
+	def get_active (self, nolock = False):
+		if nolock or self.lock is None:
+			return self.active			
+		self.lock.acquire ()	
+		active = self.active
+		self.lock.release ()	
+		return active
+	
+	def isactive (self):	
+		return self.get_active () > 0
+		
+	def isconnected (self):	
+		# self.connected should be defined at __init__ or asyncore
+		return self.connected
+		
+	def get_request_count (self):	
+		return self.request_count
+	
+	def get_execute_count (self):	
+		return self.execute_count
+	
+	def connect (self, force = 0):
+		raise NotImplementedError("must be implemented in subclass")
+	
+	def set_timeout (self, timeout):
+		self.zombie_timeout = timeout
+
+	def handle_timeout (self):
+		self.handle_close (OperationalError, "Operation Timeout")
+	
+	def handle_error (self):
+		dummy, exception_class, exception_str, tbinfo = asyncore.compact_traceback()
+		self.logger.trace ()
+		self.handle_close (exception_class, exception_str)
+	
+	def handle_close (self, expt = None, msg = ""):		
+		self.exception_class, self.exception_str = expt, msg		
+		self.close_case_with_end_tran ()
+		self.close ()
+	
+	def set_event_time (self):
+		self.event_time = time.time ()			
+	
+	def log_history (self, msg):	
+		self.__history.append ("BEGIN TRAN: %s" % sql)
+	
+	def close_case_with_end_tran (self):
+		self.end_tran ()
+		self.close_case ()
+		
+	#-----------------------------------------------------
+	# DB methods
+	#-----------------------------------------------------
+	def fetchall (self):		
+		raise NotImplementedError
+	
+	def end_tran (self):
+		raise NotImplementedError
+						
+	def begin_tran (self, request):
+		if self.__no_more_request:			
+			try: 
+				raise OperationalError ("Entered Shutdown Process")
+			except: 
+				self.handle_error ()
+				return
+		
+		self.request = request	
+		self.__history = []
+		self.has_result = False
+		self.exception_str = ""
+		self.exception_class = None		
+		self.execute_count += 1
+		self.set_event_time ()		
+		
+	def execute (self, request):		
+		self.begin_tran (request)		
+		raise NotImplementedError("must be implemented in subclass")
+
+
+class AsynDBConnect (DBConnect):
+	def is_channel_in_map (self, map = None):
+		if map is None:
+			map = self._map
+		return self._fileno in map
+	
+	def maintern (self, object_timeout):
+		# when in map, mainteren by lifetime with zombie_timeout
+		if self.is_channel_in_map ():
+			return False
+		return DBConnect.maintern (self, object_timeout)
+		
+	def del_channel (self, map=None):
+		# do not remove self._fileno
+		fd = self._fileno
+		if map is None:
+			map = self._map
+		if fd in map:
+			del map[fd]
+	
+	
