38e3f6fe9e9880be1348faaa4c71532cb2f28650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:01 +0000

    Add package limix/1.0.6/limix-1.0.6-py2-none-any.whl

---
+++ packages/limix/limix-1.0.6-py2-none-any.whl/limix/io/_csv.py
@@ -0,0 +1,48 @@
+def read_csv(filename):
+    r"""Read a CSV file.
+
+    Parameters
+    ----------
+    filename : str
+        Path to a CSV file.
+
+    Returns
+    -------
+    data : dask dataframe
+
+    Examples
+    --------
+    .. doctest::
+
+        >>> from limix.io import read_csv
+        >>> from limix.io.examples import csv_file_example
+        >>>
+        >>> df = read_csv(csv_file_example())
+        >>> print(df.compute()) #doctest: +NORMALIZE_WHITESPACE
+           pheno   attr1 attr2 attr3
+        0    sex  string    10     a
+        1   size   float    -3     b
+        2  force     int     f     c
+    """
+    from dask.dataframe import read_csv as _read_csv
+
+    df = _read_csv(filename)
+    df.set_index(df.columns[0], inplace=True)
+    return df
+
+
+def see(filepath):
+    """Shows a human-friendly representation of a CSV file.
+
+    Parameters
+    ----------
+    filepath : str
+        CSV file path.
+
+    Returns
+    -------
+    str
+        CSV representation.
+    """
+    from pandas import read_csv
+    print(read_csv(filepath).head())
---
+++ packages/limix/limix-1.0.6-py2-none-any.whl/limix/stats/__init__.py
@@ -0,0 +1,47 @@
+r"""
+**********
+Statistics
+**********
+
+PCA
+^^^
+
+.. autofunction:: limix.stats.pca
+
+Normalization
+^^^^^^^^^^^^^
+
+.. autofunction:: limix.stats.boxcox
+
+P-values
+^^^^^^^^
+
+.. autofunction:: limix.stats.qvalues
+.. autofunction:: limix.stats.empirical_pvalues
+
+Kinship processing
+^^^^^^^^^^^^^^^^^^
+
+.. autofunction:: limix.stats.gower_norm
+.. autofunction:: limix.stats.indep_pairwise
+.. autofunction:: limix.stats.maf
+
+Chi2
+^^^^
+
+.. autofunction:: limix.stats.Chi2mixture
+
+"""
+
+from .chi2mixture import Chi2mixture
+from .fdr import qvalues
+from .kinship import gower_norm
+from .pca import pca
+from .preprocess import indep_pairwise, maf
+from .teststats import empirical_pvalues
+from .trans import boxcox
+
+__all__ = [
+    'pca', 'boxcox', 'gower_norm', 'qvalues', 'empirical_pvalues',
+    'Chi2mixture', 'indep_pairwise', 'maf'
+]
