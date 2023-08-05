d7ad4a64bc09a908b35b399c795c9cd26bbf4d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:23 +0000

    Add package ckbpy/0.0.1/ckbpy-0.0.1-py3-none-any.whl

---
+++ packages/ckbpy/ckbpy-0.0.1-py3-none-any.whl/ckbpy/__init__.py
@@ -0,0 +1,6 @@
+from .effect import Key, Preset, Effect
+from .types import (RGBColor, ARGBColor,
+                    GradientColorStops, AGradientColorStops)
+from .params import (Long, Double, Bool, RGB, ARGB, Gradient, AGradient, Angle,
+                     String, Label)
+from .constants import Keypress, Time
