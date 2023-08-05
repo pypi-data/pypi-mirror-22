8876bb6ee15c96991b3e2f9109366e4a622e2b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:16 +0000

    Add package dandan/0.0.4.0/dandan-0.0.4.0.tar.gz

---
+++ packages/dandan/dandan-0.0.4.0.tar.gz/dandan-0.0.4.0/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+VERSION = "0.0.4.0"
+
+readme = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README")
+
+setup(
+    name="dandan",
+    version=VERSION,
+    description="Several tools for internet",
+    long_description=open(readme).read(),
+    packages=["dandan"],
+    install_requires=[
+        "requests",
+        "BeautifulSoup4",
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    author="ccyg",
+    author_email="kjp1314@163.com",
+    url="https://github.com/ccyg/dandan",
+    license="MIT",
+    include_package_data=True,
+    zip_safe=True,
+    platforms="any",
+)
