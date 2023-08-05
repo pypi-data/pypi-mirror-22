36cb967ba5b3b837b25392ec7a6ee595f1560ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:43 +0000

    Add package sqrekubespawner/0.0.6/sqrekubespawner-0.0.6.tar.gz

---
+++ packages/sqrekubespawner/sqrekubespawner-0.0.6.tar.gz/sqrekubespawner-0.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sqrekubespawner',
+    version='0.0.6',
+    install_requires=[
+        'jupyterhub',
+        'pyyaml',
+        'pycurl'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    description='LSST SQuaRE flavor of KubeSpawner',
+    url='http://github.com/lsst-sqre/kubespawner',
+    author='Adam Thornton',
+    author_email='athornton@lsst.org',
+    license='BSD',
+    packages=find_packages(),
+)
