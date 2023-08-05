2a89a1229bbe27b65c1ce3f3f9d2ad188729a89b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:51 +0000

    Add package bloom3/0.0.1/bloom3-0.0.1.zip

---
+++ packages/bloom3/bloom3-0.0.1.zip/bloom3-0.0.1/setup.py
@@ -0,0 +1,33 @@
+﻿#!/usr/bin/env python  
+from __future__ import print_function  
+from setuptools import setup, find_packages  
+import sys  
+  
+setup(  
+    name="bloom3",  
+    version="0.0.1",  
+    author="Wei",  
+    author_email="admin@iqianyue.com",  
+    description="bloom3",  
+    long_description="Python3 bloom",  
+    license="MIT",  
+    url="http://www.iqianyue.com",  
+    packages=['b3'],  
+    install_requires=[
+        ],  
+    classifiers=[  
+        "Environment :: Web Environment",  
+        "Intended Audience :: Developers",  
+        "Operating System :: OS Independent",  
+        "Topic :: Text Processing :: Indexing",  
+        "Topic :: Utilities",  
+        "Topic :: Internet",  
+        "Topic :: Software Development :: Libraries :: Python Modules",  
+        "Programming Language :: Python",  
+        "Programming Language :: Python :: 2",  
+        "Programming Language :: Python :: 2.6",  
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+    ],  
+)  
