b22e2a908dc08db45daed6194f1e5187969fb909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:03 +0000

    Add package effigy/0.3.1/effigy-0.3.1.tar.gz

---
+++ packages/effigy/effigy-0.3.1.tar.gz/effigy-0.3.1/effigy/__init__.py
@@ -0,0 +1,6 @@
+from effigy.QNodeView import QNodeView
+from effigy.QNodeScene import QNodeScene
+from effigy.QNodeSceneNode import QNodeSceneNode
+from effigy.NodeIO import NodeIO, NodeInput, NodeOutput, NodeIODirection, NodeIOMultiplicity
+
+__all__ = ["QNodeView.QNodeView", "QNodeScene.QNodeScene", "QNodeSceneNode.QNodeSceneNode"]
