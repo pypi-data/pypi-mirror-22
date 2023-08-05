c969bc752190d99f64d832810105b00d1754a0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:54 +0000

    Add package qbx/2017.5.7.3/qbx-2017.5.7.3.tar.gz

---
+++ packages/qbx/qbx-2017.5.7.3.tar.gz/qbx-2017.5.7.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+from sys import platform
+
+if platform == 'win32':
+    script = 'bin/qbx.cmd'
+else:
+    script = 'bin/qbx'
+setup(name='qbx',
+      author='qbtrade',
+      url='https://github.com/qbtrade/qbx',
+      author_email='admin@qbtrade.org',
+      packages=['qbx'],
+      version='2017.5.7.3',
+      install_requires=[
+          'docopt',
+          'requests'
+      ],
+      zip_safe=False,
+      entry_points={'console_scripts': ['qbx=qbx:run']}
+      )
