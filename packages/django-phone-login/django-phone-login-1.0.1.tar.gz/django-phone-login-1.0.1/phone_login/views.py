477b64b608296bae9bd0383211c49ef004d7b8bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:53 +0000

    Add package django-phone-login/1.0.1/django-phone-login-1.0.1.tar.gz

---
+++ packages/django-phone-login/django-phone-login-1.0.1.tar.gz/django-phone-login-1.0.1/phone_login/views.py
@@ -0,0 +1,64 @@
+
+from django.contrib.auth import authenticate, login
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+
+from phone_login.models import PhoneToken
+from phone_login.utils import user_detail
+
+from .serializers import (PhoneTokenCreateSerializer,
+                          PhoneTokenValidateSerializer)
+
+
+class GenerateOTP(CreateAPIView):
+
+    queryset = PhoneToken.objects.all()
+    serializer_class = PhoneTokenCreateSerializer
+
+    def post(self, request, format=None):
+        # Get the patient if present or result None.
+        ser = self.serializer_class(
+            data=request.data,
+            context={'request': request}
+        )
+        if ser.is_valid():
+            token = PhoneToken.create_otp_for_number(
+                request.data.get('phone_number')
+            )
+            phone_token = self.serializer_class(
+                token, context={'request': request}
+            )
+            return Response(phone_token.data)
+        return Response(
+            {'reason': ser.errors}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+
+class ValidateOTP(CreateAPIView):
+
+    queryset = PhoneToken.objects.all()
+    serializer_class = PhoneTokenValidateSerializer
+
+    def post(self, request, format=None):
+        # Get the patient if present or result None.
+        ser = self.serializer_class(
+            data=request.data, context={'request': request}
+        )
+        if ser.is_valid():
+            pk = request.data.get("pk")
+            otp = request.data.get("otp")
+            try:
+                user = authenticate(pk=pk, otp=otp)
+                if user:
+                    last_login = user.last_login
+                login(request, user)
+                response = user_detail(user, last_login)
+                return Response(response, status=status.HTTP_200_OK)
+            except ObjectDoesNotExist:
+                return Response(
+                    {'reason': "OTP doesn't exist"},
+                    status=status.HTTP_406_NOT_ACCEPTABLE
+                )
+        return Response(
+            {'reason': ser.errors}, status=status.HTTP_406_NOT_ACCEPTABLE)
