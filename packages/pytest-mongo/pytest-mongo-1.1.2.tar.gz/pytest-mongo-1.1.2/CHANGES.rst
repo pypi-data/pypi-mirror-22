0b3d582e9ff5d7b9ee065036012d57d28fe763a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:38 +0000

    Add package pytest-mongo/1.1.2/pytest-mongo-1.1.2.tar.gz

---
+++ packages/pytest-mongo/pytest-mongo-1.1.2.tar.gz/pytest-mongo-1.1.2/CHANGES.rst
@@ -0,0 +1,24 @@
+CHANGELOG
+=========
+
+1.1.2
+-------
+
+- [enhancement] removed path.py depdendency
+
+1.1.1
+-------
+
+- [enhancements] set executor timeout to 60. By default mirakuru waits indefinitely, which might cause test hangs
+
+1.1.0
+-------
+
+- [feature] - migrate usage of getfuncargvalue to getfixturevalue. require at least pytest 3.0.0
+
+1.0.0
+-------
+
+- [feature] defaults logs dir to $TMPDIR by default
+- [feature] run on random port by default (easier xdist integration)
+- [feature] add command line and ini option for: executable, host, port, params and logsdir
---
+++ packages/pytest-mongo/pytest-mongo-1.1.2.tar.gz/pytest-mongo-1.1.2/setup.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016 by Clearcode <http://clearcode.cc>
+# and associates (see AUTHORS).
+
+# This file is part of pytest-mongo.
+
+# pytest-mongo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# pytest-mongo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with pytest-mongo. If not, see <http://www.gnu.org/licenses/>.
+"""Package definition for pytest-mongo."""
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.dirname(__file__)
+
+
+def read(fname):
+    """
+    Read given file's content.
+
+    :param str fname: file name
+    :returns: file contents
+    :rtype: str
+    """
+    return open(os.path.join(here, fname)).read()
+
+
+requirements = [
+    'pytest>=3.0.0',
+    'pymongo',
+    'mirakuru',
+    'port-for'
+]
+
+test_requires = [
+    'pytest',
+    'pytest-cov==2.4.0',
+    'pytest-xdist==1.16.0'
+]
+
+extras_require = {
+    'docs': ['sphinx'],
+    'tests': test_requires
+}
+
+setup(
+    name='pytest-mongo',
+    version='1.1.2',
+    description='MongoDB process and client fixtures plugin for py.test.',
+    long_description=(
+        read('README.rst') + '\n\n' + read('CHANGES.rst')
+    ),
+    keywords='tests py.test pytest fixture mongo mongodb',
+    author='Clearcode - The A Room',
+    author_email='thearoom@clearcode.cc',
+    url='https://github.com/ClearcodeHQ/pytest-mongo',
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: '
+        'GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=requirements,
+    tests_require=test_requires,
+    test_suite='tests',
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=extras_require,
+    entry_points={
+        'pytest11': [
+            'pytest_mongo = pytest_mongo.plugin'
+        ]},
+)
