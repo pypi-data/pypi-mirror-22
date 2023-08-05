14dd13bd79f39aa23e4f80cf1e08f67671c192f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:01 +0000

    Add package shipane-sdk/1.1.0a7/shipane_sdk-1.1.0a7.tar.gz

---
+++ packages/shipane-sdk/shipane_sdk-1.1.0a7.tar.gz/shipane_sdk-1.1.0a7/shipane_sdk/jobs/new_stock_purchase.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from shipane_sdk.jobs.basic_job import BasicJob
+
+
+class NewStockPurchaseJob(BasicJob):
+    def __init__(self, client, client_aliases=None, name=None, **kwargs):
+        super(NewStockPurchaseJob, self).__init__(name, kwargs.get('schedule', None), kwargs.get('enabled', False))
+
+        self._client = client
+        self._client_aliases = client_aliases
+
+    def __call__(self):
+        for client_alias in self._client_aliases:
+            client = self._client_aliases[client_alias]
+            self._client.purchase_new_stocks(client)
