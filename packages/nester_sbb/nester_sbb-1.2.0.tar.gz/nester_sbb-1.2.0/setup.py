eb0c6acaf331420005d9c918a81e357ad5410d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:11 +0000

    Add package nester_sbb/1.2.0/nester_sbb-1.2.0.tar.gz

---
+++ packages/nester_sbb/nester_sbb-1.2.0.tar.gz/nester_sbb-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_sbb',
+    version = '1.2.0',
+    py_modules = ['nester_sbb'],
+    author = 'xianggb',
+    author_email = 'xiangguobo@163.com',
+    url = 'http://www.163.com',
+    description = 'A simple printer of nested lists',
+    )
