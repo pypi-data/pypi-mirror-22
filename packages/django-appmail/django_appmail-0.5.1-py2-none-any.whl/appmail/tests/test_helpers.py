4b397e0a9fe03f318376c774600d63c169ac63d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:06 +0000

    Add package django-appmail/0.5.1/django_appmail-0.5.1-py2-none-any.whl

---
+++ packages/django-appmail/django_appmail-0.5.1-py2-none-any.whl/appmail/tests/test_helpers.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+
+from .. import helpers
+
+
+class HelperTests(TestCase):
+
+    """appmail.helpers module tests."""
+
+    def test_get_context(self):
+        self.assertEqual(
+            helpers.get_context('{{a}} {{b.c}}'),
+            {
+                'a': "A",
+                'b': {
+                    'c': "C"
+                }
+            }
+        )
+
+    def test_extract_vars(self):
+        """Check extract_vars handles expected input."""
+        for x, y in (
+            ('', []),
+            (None, []),
+            ('{{foo}}', ['foo']),
+            ('{{ foo}}', ['foo']),
+            ('{{ foo }}', ['foo']),
+            ('{{ foo }', []),
+            ('{% foo %}', []),
+            ('{{ foo|time }}', []),
+            ('{{foo}} {{bar}}', ['foo', 'bar']),
+        ):
+            self.assertEqual(set(helpers.extract_vars(x)), set(y))
+
+    def test_expand_list(self):
+        """Check dot notation expansion."""
+        self.assertRaises(AssertionError, helpers.expand_list, None)
+        self.assertRaises(AssertionError, helpers.expand_list, '')
+        self.assertEqual(
+            helpers.expand_list(['a', 'b.c']),
+            {
+                'a': {},
+                'b': {
+                    'c': {}
+                }
+            }
+        )
+
+    def test_fill_leaf_values(self):
+        """Check the default func is applied."""
+        self.assertRaises(AssertionError, helpers.fill_leaf_values, None)
+        self.assertRaises(AssertionError, helpers.fill_leaf_values, '')
+        self.assertEqual(
+            helpers.fill_leaf_values(
+                {
+                    'a': {},
+                    'b': {
+                        'c': {}
+                    }
+                }
+            ),
+            {
+                'a': "A",
+                'b': {
+                    'c': "C"
+                }
+            }
+        )
+
+    def test_merge_dicts(self):
+        self.assertEqual(
+            helpers.merge_dicts({'foo': 1}),
+            {'foo': 1}
+        )
+        self.assertEqual(
+            helpers.merge_dicts({'foo': 1}, {'bar': 2}),
+            {'foo': 1, 'bar': 2}
+        )
+        self.assertEqual(
+            helpers.merge_dicts({'foo': 1}, {'foo': 2}),
+            {'foo': 2}
+        )
