d400c26d6d199a4109d70bcd86139374cdf0c8a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package knackhq/0.1.0/knackhq-0.1.0.tar.gz

---
+++ packages/knackhq/knackhq-0.1.0.tar.gz/knackhq-0.1.0/knackhq/__init__.py
@@ -0,0 +1,4 @@
+""" KnackHQ API Client """
+from .knackhq import KnackApp
+
+__version__ = '0.1.0'
---
+++ packages/knackhq/knackhq-0.1.0.tar.gz/knackhq-0.1.0/setup.py
@@ -0,0 +1,31 @@
+import os
+import re
+import textwrap
+from setuptools import setup
+
+
+def version():
+    search = r"^__version__ *= *['\"]([0-9.]+)['\"]"
+    initpy = open('./knackhq/__init__.py').read()
+    return re.search(search, initpy, re.MULTILINE).group(1)
+
+
+setup(name='knackhq',
+      version=version(),
+      author='amancevice',
+      author_email='smallweirdnum@gmail.com',
+      packages=['knackhq'],
+      include_package_data=True,
+      url='http://www.smallweirdnumber.com',
+      description='Interact with KnackHQ API',
+      long_description=textwrap.dedent(
+          '''See GitHub_ for documentation.
+          .. _GitHub: https://github.com/amancevice/knackhq'''),
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Utilities'],
+      install_requires=['requests>=2.11.1'])
