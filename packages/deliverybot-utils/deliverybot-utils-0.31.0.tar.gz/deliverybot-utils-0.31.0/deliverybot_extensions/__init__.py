7f4a921ca33071a441cb972207a7ef694f839e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:56 +0000

    Add package deliverybot-utils/0.31.0/deliverybot-utils-0.31.0.tar.gz

---
+++ packages/deliverybot-utils/deliverybot-utils-0.31.0.tar.gz/deliverybot-utils-0.31.0/deliverybot_extensions/__init__.py
@@ -0,0 +1,8 @@
+"""
+deliverybot_extensions/__init__.py
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A group of common extensions
+"""
+import flask
+
---
+++ packages/deliverybot-utils/deliverybot-utils-0.31.0.tar.gz/deliverybot-utils-0.31.0/deliverybot_libs/__init__.py
@@ -0,0 +1,7 @@
+"""
+deliverybot_libs/__init__.py
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A group of useful libs
+"""
+from . import logging
