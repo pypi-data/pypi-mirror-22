120cf355227d36f39678efd96cf12f89fc75e0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:49 +0000

    Add package ppg-common/1.21/ppg_common-1.21.tar.gz

---
+++ packages/ppg-common/ppg_common-1.21.tar.gz/ppg_common-1.21/ppg_common/clients/pandora.py
@@ -0,0 +1,76 @@
+from ppg_common.clients.base import BaseClient
+
+
+class PandoraClient(BaseClient):
+    urls = {
+        'create_box': '/v1/box',
+        'box': '/v1/box/{}',
+        'files': '/v1/files/{}'
+    }
+
+    def __init__(self, host, port, ssl=False, session=None):
+        super(PandoraClient, self).__init__(host, port, ssl, session)
+
+    def create_box(self, box_name):
+        query_box_url = self.urls.get('create_box')
+        return self.post(query_box_url, body={}, query="name={}".format(
+            box_name))
+
+    def list_box(self, box_id):
+        return self.get(self.urls.get("box").format(box_id))
+
+    def delete_box(self, box_id):
+        return self.delete(self.urls.get("box").format(box_id))
+
+    def list_by_query(self, box_id, inquiry):
+        url = self.urls.get("box").format(box_id)
+        return self.get(url, query="inquiry={}".format(inquiry))
+
+    def update_by_query(self, box_id, inquiry, tag_name, tag_value):
+        url = self.urls.get("box").format(box_id)
+        return self.patch(url,
+                          query="inquiry={}&tag-name={}&tag-value={}".format(
+                              inquiry, tag_name, tag_value))
+
+    def delete_by_query(self, box_id, inquiry):
+        url = self.urls.get("box").format(box_id)
+        return self.delete(url, query="inquiry={}".format(inquiry))
+
+    def upload_file(self, box_id, file_name, uploaded_input_stream):
+        query_params = "?boxID=" + box_id + "&name=" + file_name
+        url = '/files'
+        return self.post(url, query="boxID={}&name={}".format(box_id,
+                                                              file_name))
+
+    def download_file(self, file_id):
+        url = self.urls.get("files").format(file_id)
+        return self.get(url)
+
+    def delete_file(self, file_id):
+        url = self.urls.get("files").format(file_id)
+        return self.delete(url)
+
+    def copy_file(self, file_id, name, box_id):
+        url = self.urls.get("files").format(file_id) + "/copy"
+        return self.get(url, quert="boxID={}&name={}".format(box_id, name))
+
+    def get_info(self, file_id):
+        url = self.urls.get("files").format(file_id) + "/info"
+        return self.get(url)
+
+    def add_tag(self, file_id, tag_name, tag_value):
+        query_params = "?tag-name=" + tag_name + "&tag-value=" + tag_value
+        url = self.urls.get("files").format(file_id) + "/tag"
+        return self.post(url, query="tag-name={}&tag-value={}".format(
+            tag_name, tag_value))
+
+    def delete_tag(self, file_id, tag_name, tag_value):
+        url = self.urls.get("files").format(file_id) + "/tag"
+        return self.post(url, query="tag-name={}&tag-value={}".format(
+            tag_name, tag_value))
+
+
+a = PandoraClient('52.45.172.113', 20101, True,
+                  'e18b0ece-a9f0-4bf5-bf8b-d328f123617d')
+
+print(a.list_box('c1bf5b0a-d71e-47bc-8b04-40e2ac2370cf'))
