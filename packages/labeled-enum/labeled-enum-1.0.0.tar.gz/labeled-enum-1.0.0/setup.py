eb5e31a792e84e528e259210c59a2a0a9dfb7f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:38 +0000

    Add package labeled-enum/1.0.0/labeled-enum-1.0.0.tar.gz

---
+++ packages/labeled-enum/labeled-enum-1.0.0.tar.gz/labeled-enum-1.0.0/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+    name='labeled-enum',
+    version='1.0.0',
+    description='Django friendly, iterable Enum type with labels.',
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    py_modules=['lenum',],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+    ],
+)
