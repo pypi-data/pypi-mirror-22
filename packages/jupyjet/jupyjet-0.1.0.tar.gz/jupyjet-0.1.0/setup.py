793f4d4ce4745c2e04c870f33b5c2c33ad1e4b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:05 +0000

    Add package jupyjet/0.1.0/jupyjet-0.1.0.tar.gz

---
+++ packages/jupyjet/jupyjet-0.1.0.tar.gz/jupyjet-0.1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+
+long_description = """\
+Jupyjet is a IPyhton notebook extention which allow you to dynamically genrate python files from cells.
+
+Code portions registered with %jet or %jet-init magics will be written and updated into a python file.
+This approach make possible to easily separate a notebook into several notebooks and bind them.
+"""
+
+setup(
+    name='jupyjet',
+    url='https://github.com/pelodelfuego/jupyjet',
+    author='Clément CREPY',
+    author_email='clement.crepy@gmail.com',
+    version='0.1.0',
+    py_modules=['jupyjet'],
+    license='MIT',
+    description='jupyter extention to generate python modules',
+    long_description=long_description,
+    keywords = ['jupyter', 'notebook', 'module', 'jupyjet'],
+    install_requires = ['IPython>=0.13', 'codegen==1.0'],
+)
+
+
