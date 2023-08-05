4a8b97a16f54827a48b5a075a6e2c204be3af4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package durable-rules/0.33.103/durable_rules-0.33.103.tar.gz

---
+++ packages/durable-rules/durable_rules-0.33.103.tar.gz/durable_rules-0.33.103/src/rules/state.h
@@ -0,0 +1,62 @@
+
+#define HASH_ID 5863474
+#define HASH_SID 193505797
+#define UNDEFINED_INDEX 0xFFFFFFFF
+#define MAX_STATE_PROPERTIES 64
+#define MAX_NAME_LENGTH 256
+#define UNDEFINED_HASH_OFFSET 0xFFFFFFFF
+
+typedef struct jsonProperty {
+    unsigned int hash;
+    unsigned char type;
+    unsigned char isMaterial;
+    unsigned short valueOffset;
+    unsigned short valueLength;
+    char name[MAX_NAME_LENGTH];
+    unsigned short nameLength;
+    union {
+        long i; 
+        double d; 
+        unsigned char b; 
+    } value;
+} jsonProperty;
+
+typedef struct stateEntry {
+    unsigned int nextHashOffset;
+    unsigned int nextLruOffset;
+    unsigned int prevLruOffset;
+    unsigned int sidHash;
+    unsigned int bindingIndex;
+    unsigned int lastRefresh;
+    unsigned int propertiesLength;
+    jsonProperty properties[MAX_STATE_PROPERTIES];
+    char *state;
+    char *sid;
+} stateEntry;
+
+unsigned int djbHash(char *str, unsigned int len);
+void rehydrateProperty(jsonProperty *property, char *state);
+unsigned int refreshState(void *tree, char *sid);
+unsigned int constructObject(char *root,
+                             char *parentName, 
+                             char *object,
+                             char createHashtable,
+                             unsigned int maxProperties,
+                             jsonProperty *properties, 
+                             unsigned int *propertiesLength, 
+                             unsigned int *midIndex, 
+                             unsigned int *sidIndex,
+                             char **next);
+unsigned int resolveBinding(void *tree, 
+                            char *sid, 
+                            void **rulesBinding);
+unsigned int fetchStateProperty(void *tree,
+                                char *sid, 
+                                unsigned int propertyHash, 
+                                unsigned int maxTime, 
+                                unsigned char ignoreStaleState,
+                                char **state,
+                                jsonProperty **property);
+unsigned int getStateVersion(void *handle, 
+                             char *sid, 
+                             unsigned long *stateVersion);
