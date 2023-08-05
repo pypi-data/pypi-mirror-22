658736d5f14c387b060cb83eaaeb45719ecaf5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:41 +0000

    Add package target-gsheet/0.2.4/target-gsheet-0.2.4.tar.gz

---
+++ packages/target-gsheet/target-gsheet-0.2.4.tar.gz/target-gsheet-0.2.4/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='target-gsheet',
+      version='0.2.4',
+      description='Singer.io target for writing data to Google Sheets',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['target_gsheet'],
+      install_requires=[
+          'jsonschema==2.6.0',
+          'singer-python==0.1.0',
+          'google-api-python-client==1.6.2'
+      ],
+      entry_points='''
+          [console_scripts]
+          target-gsheet=target_gsheet:main
+      ''',
+)
