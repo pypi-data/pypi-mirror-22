d31e514e4aeb071216e53a4a314c4d483bdcc4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:14 +0000

    Add package ScapyPppoeClient/1.0/ScapyPppoeClient-1.0.zip

---
+++ packages/ScapyPppoeClient/ScapyPppoeClient-1.0.zip/ScapyPppoeClient-1.0/setup.py
@@ -0,0 +1,10 @@
+#encoding=utf-8
+from distutils.core import setup  
+setup(name='ScapyPppoeClient',  
+      version='1.0',    
+      description='Analog PPPoE client',    
+      author='qdyxmas',    
+      author_email='qdyxmas@gmail.com',    
+      url='https://github.com/qdyxmas/scapy-Protocol-conformance',    
+      py_modules=['ScapyPppoeClient'],  
+)    
