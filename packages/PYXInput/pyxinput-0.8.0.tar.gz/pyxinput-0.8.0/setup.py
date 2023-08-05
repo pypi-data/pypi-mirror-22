a943103a30e0b370ecd58408593af65f4919a86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:25 +0000

    Add package PYXInput/0.8.0/pyxinput-0.8.0.tar.gz

---
+++ packages/PYXInput/pyxinput-0.8.0.tar.gz/pyxinput-0.8.0/setup.py
@@ -0,0 +1,67 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyxinput',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.8.0',
+
+    description='Python Implmenentation of vXbox and XInput DLL',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/bayangan1991/PYXInput',
+
+    # Author details
+    author='Ryan Barnes (@bayangan1991)',
+    author_email='ryan.bayangan@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='pygta5 virtual xbox controller xinput',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # installs the DLL with the package
+    package_data={'pyxinput': [
+        'vXboxInterface-x64/vXboxInterface.dll',
+        'vXboxInterface-x64/msvcp120.dll',
+        'vXboxInterface-x64/msvcr120.dll',
+        'vXboxInterface-x86/vXboxInterface.dll',
+        'vXboxInterface-x86/msvcp120.dll',
+        'vXboxInterface-x86/msvcr120.dll'
+    ]}
+
+)
