661a33dc2a4031a6066b8712a14c5b29290a6288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:16 +0000

    Add package py-token-stream/0.1.1/py-token-stream-0.1.1.tar.gz

---
+++ packages/py-token-stream/py-token-stream-0.1.1.tar.gz/py-token-stream-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='py-token-stream',
+    version='0.1.1',
+    description='A friendlier API for working with tokens',
+    url='https://github.com/SamDudley/py-token-stream',
+    author='Sam Dudley',
+    author_email='dudley.co.uk@gmail.com',
+    license='MIT',
+    packages=find_packages()
+)
