46ab22d08e25046b3da950bbfeba50e6715b2979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:07 +0000

    Add package flexmatcher/0.4.0/flexmatcher-0.4.0.tar.gz

---
+++ packages/flexmatcher/flexmatcher-0.4.0.tar.gz/flexmatcher-0.4.0/flexmatcher/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from flexmatcher.flexmatcher import FlexMatcher
+import flexmatcher.classify
+
+__author__ = """BigGorilla Team"""
+__email__ = 'thebiggorilla.team@gmail.com'
+__version__ = '0.1.0'
