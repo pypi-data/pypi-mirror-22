78cd4993488329fa63b83279c06aa5cc44ba997f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package unihan-tabular/0.5.1/unihan-tabular-0.5.1.tar.gz

---
+++ packages/unihan-tabular/unihan-tabular-0.5.1.tar.gz/unihan-tabular-0.5.1/__about__.py
@@ -0,0 +1,8 @@
+__title__ = 'unihan-tabular'
+__package_name__ = 'unihan_tabular'
+__description__ = 'Download and export UNIHAN to Python, CSV, JSON and YAML'
+__version__ = '0.5.1'
+__author__ = 'Tony Narlock'
+__email__ = 'cihai@git-pull.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013-2017 Tony Narlock'
