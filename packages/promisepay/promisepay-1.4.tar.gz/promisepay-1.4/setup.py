0d010df0bdde895e69f57ff628f8617e53779fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:34 +0000

    Add package promisepay/1.4/promisepay-1.4.tar.gz

---
+++ packages/promisepay/promisepay-1.4.tar.gz/promisepay-1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+setup(name='promisepay',
+      description='Python SDK for PromisePay/ Assemblypayments',
+      version='1.4',
+      url='https://github.com/Rekvant/promisepay-python',
+      author='Anish Menon',
+      author_email='info@rekvant.com',
+      license='Apache2',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 2.7'
+      ],
+
+      packages=['promisepay'],
+      install_requires=[
+          'requests >=2.14',
+      ],
+     
+)
