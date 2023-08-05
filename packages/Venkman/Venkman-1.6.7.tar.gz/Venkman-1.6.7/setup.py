699a4dc0c2b0e424707b3afb096a9a8b4f100c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:25 +0000

    Add package Venkman/1.6.7/Venkman-1.6.7.tar.gz

---
+++ packages/Venkman/Venkman-1.6.7.tar.gz/Venkman-1.6.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Venkman',
+    version='1.6.7',
+    author='Darryl lane',
+    author_email='DarrylLane101@gmail.com',
+    url='https://github.com/darryllane/venkman',
+    packages=['Venkman'],
+    include_package_data=True,
+    license='LICENSE.txt',
+    description='''
+    Service account and logged in user enumeration. Used to hunt for priv accounts''',
+    scripts=['Venkman/venkman'],
+    install_requires=[
+    ],
+)
+
