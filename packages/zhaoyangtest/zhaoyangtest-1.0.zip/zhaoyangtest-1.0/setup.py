7c28e78e35197ec59f7f73ef9fe9adcc215a7870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:37 +0000

    Add package zhaoyangtest/1.0/zhaoyangtest-1.0.zip

---
+++ packages/zhaoyangtest/zhaoyangtest-1.0.zip/zhaoyangtest-1.0/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  4 15:00:57 2017
+
+@author: zhaoyang2
+"""
+
+from distutils.core import setup #如果没有需要先安装
+setup(name='zhaoyangtest',  #打包后的包文件名 
+      version='1.0',   #版本
+      description='description',   
+      author='zhaoyang ',   
+      author_email='**** ',   
+      url=' ',   
+      py_modules=['zhaoyangtest'],   #与前面的新建文件名一致 
+)  
---
+++ packages/zhaoyangtest/zhaoyangtest-1.0.zip/zhaoyangtest-1.0/zhaoyangtest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  4 14:59:34 2017
+
+@author: zhaoyang2
+"""
+
+def add (a,b):
+    c=a+b
+    return c
