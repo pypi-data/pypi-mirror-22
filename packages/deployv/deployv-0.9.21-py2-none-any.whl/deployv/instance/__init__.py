899006359d94240a42b8ebcd3ec95f1e4a337e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:47 +0000

    Add package deployv/0.9.21/deployv-0.9.21-py2-none-any.whl

---
+++ packages/deployv/deployv-0.9.21-py2-none-any.whl/deployv/instance/__init__.py
@@ -0,0 +1,12 @@
+
+ODOO_BINARY = {
+    '7.0': 'openerp-server',
+    '8.0': 'odoo.py',
+    '9.0': 'odoo.py',
+    '10.0': 'odoo-bin',
+    'saas-14': 'odoo-bin',
+}
+
+SAAS_VERSIONS = {
+    'saas-14': '10.0'
+}
