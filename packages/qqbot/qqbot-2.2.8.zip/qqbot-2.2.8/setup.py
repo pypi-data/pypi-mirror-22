82848cfdd209fee4e765913d2a9bf117b89feae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:03 +0000

    Add package qqbot/2.2.8/qqbot-2.2.8.zip

---
+++ packages/qqbot/qqbot-2.2.8.zip/qqbot-2.2.8/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+version = '2.2.8'
+
+setup(
+    name = 'qqbot',
+    version = version,
+    packages = ['qqbot', 'qqbot.qcontactdb'],
+    entry_points = {
+        'console_scripts': [
+            'qqbot = qqbot:RunBot',
+            'qq = qqbot:QTerm'
+        ]
+    },
+    install_requires = ['requests', 'certifi', 'flask', 'apscheduler'],
+    description = "QQBot: A conversation robot base on Tencent's SmartQQ",
+    author = 'pandolia',
+    author_email = 'pandolia@yeah.net',
+    url = 'https://github.com/pandolia/qqbot/',
+    download_url = 'https://github.com/pandolia/qqbot/archive/%s.tar.gz' % \
+                    version,
+    keywords = ['QQBot', 'conversation robot', 'tencent', 'qq',
+                'web', 'network', 'python', 'http'],
+    classifiers = [],
+)
