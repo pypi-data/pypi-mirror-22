9a085043e2ce4b299cdc2d798dee36cbcb74f5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:50 +0000

    Add package pywhen/0.10.0/pywhen-0.10.0.tar.gz

---
+++ packages/pywhen/pywhen-0.10.0.tar.gz/pywhen-0.10.0/setup.py
@@ -0,0 +1,41 @@
+import os, sys
+
+from when import __version__
+
+readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+long_description = open(readme).read()
+
+SETUP_ARGS = dict(
+    name='pywhen',
+    version=__version__,
+    description=('Collection of random python tools and utilities '),
+    long_description=long_description,
+    url='https://github.com/cltrudeau/wrench',
+    author='Christopher Trudeau',
+    author_email='ctrudeau+pypi@arsensa.com',
+    license='MIT',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='tools',
+    test_suite='load_tests.get_suite',
+    py_modules = ['when',],
+    install_requires=[],
+    tests_require=[
+        'waelstow==0.10.0',
+    ],
+)
+
+if __name__ == '__main__':
+    from setuptools import setup
+    setup(**SETUP_ARGS)
