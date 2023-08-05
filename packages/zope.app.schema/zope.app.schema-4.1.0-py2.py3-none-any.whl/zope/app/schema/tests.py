28e3e8bdc6b24e65299eaade0588775e0a929367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:03 +0000

    Add package zope.app.schema/4.1.0/zope.app.schema-4.1.0-py2.py3-none-any.whl

---
+++ packages/zope.app.schema/zope.app.schema-4.1.0-py2.py3-none-any.whl/zope/app/schema/tests.py
@@ -0,0 +1,18 @@
+import doctest
+import unittest
+
+from zope.testing import cleanup
+
+def setUp(_test):
+    cleanup.setUp()
+
+def tearDown(_test):
+    cleanup.tearDown()
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite('README.rst',
+                     setUp=setUp, tearDown=tearDown,
+                     optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+                     ),
+        ))
