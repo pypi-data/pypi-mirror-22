87e4016b1e2aeac6a42c1d3925540b4c917d2e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:26 +0000

    Add package cla_tool/1.0.4/cla_tool-1.0.4.tar.gz

---
+++ packages/cla_tool/cla_tool-1.0.4.tar.gz/cla_tool-1.0.4/cla/util/test/test_file_util.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+from cla.util import file_util
+
+
+def test_read_strength():
+    strength_path = "cla/res/dict/s.txt"
+    most = file_util.read_as_set(strength_path, encoding="gbk", skip=3)
+    ish = file_util.read_as_set(strength_path, encoding="gbk", skip=157)
+
+    assert most.__sizeof__() > 0
+    assert ish.__sizeof__() > 0
+    assert most.__contains__(u"百分之百")
+    assert ish.__contains__(u"点点滴滴")
+
+
+def test_cut_words_in():
+    test_file_path = "cla/res/test/sentences.txt"
+    result_path = file_util.cut_words_in(test_file_path, )
+    with open(result_path, 'r') as result_file:
+        first_line = result_file.readline()
+        assert first_line is not None
+        assert first_line.split(" ").__sizeof__() > 0
