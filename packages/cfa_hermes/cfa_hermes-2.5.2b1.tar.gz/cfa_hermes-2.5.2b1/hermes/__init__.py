b4115e9d39edf65814137cf2b4940df799d042a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:00 +0000

    Add package cfa_hermes/2.5.2b1/cfa_hermes-2.5.2b1.tar.gz

---
+++ packages/cfa_hermes/cfa_hermes-2.5.2b1.tar.gz/cfa_hermes-2.5.2b1/hermes/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding: utf8
+#-----------------------------------------------------------------
+# hermes: __init__.py
+#
+# Copyright (C) 2016, Christophe Fauchard
+#-----------------------------------------------------------------
+
+import sys
+from hermes._version import __version__, __version_info__
+
+__author__ = "Christophe Fauchard <christophe.fauchard@gmail.com>"
+
+if sys.version_info < (3, 5):
+    raise RuntimeError('You need Python 3.5+ for this module.')
+
+import hermes.connection
+import hermes.thread
+import hermes.ftp
+import hermes.sftp
+import hermes.exception
---
+++ packages/cfa_hermes/cfa_hermes-2.5.2b1.tar.gz/cfa_hermes-2.5.2b1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+import glob
+
+exec(compile(open('hermes/_version.py').read(), 'hermes/_version.py', 'exec'))
+
+setup(name='cfa_hermes',
+      version=__version__,
+      description='file tranfer monitor protocols ftp, sftp',
+      url='https://github.com/cfauchard/hermes',
+      author='Christophe Fauchard',
+      author_email='christophe.fauchard@gmail.com',
+      license='GPLV3',
+      packages=['hermes'],
+      scripts=['bin/hcmd.py','bin/hermesd.py'],
+      data_files=[
+            (
+                  'sample/hermes', [
+                        f for f in glob.glob(
+                              os.path.join('sample', '*.hermes')
+                        )
+                  ]
+            )
+      ],
+      install_requires=[
+            'cfa_zeus >= 3.3.0.b1',
+            'paramiko'
+      ],
+      zip_safe=False)
