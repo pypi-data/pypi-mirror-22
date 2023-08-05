a0a86ebefd79ad5005a76ee88198a974fadf0f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:00 +0000

    Add package seashore/17.5.0/seashore-17.5.0-py2.py3-none-any.whl

---
+++ packages/seashore/seashore-17.5.0-py2.py3-none-any.whl/seashore/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) Shopkick 2017
+# See LICENSE for details.
+"""
+Seashore
+=======
+
+Seashore is a collection of shell abstractions.
+"""
+from seashore.executor import Executor, NO_VALUE
+from seashore.shell import Shell
+from seashore._version import __version__
+
+__version__ = ['Executor', 'NO_VALUE', 'Shell', '__version__']
