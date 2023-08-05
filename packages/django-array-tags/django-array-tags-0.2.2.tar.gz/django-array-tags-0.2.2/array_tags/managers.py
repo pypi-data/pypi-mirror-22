5403e73bb69f919b6827ea147e8e91a0359dc591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:03 +0000

    Add package django-array-tags/0.2.2/django-array-tags-0.2.2.tar.gz

---
+++ packages/django-array-tags/django-array-tags-0.2.2.tar.gz/django-array-tags-0.2.2/array_tags/managers.py
@@ -0,0 +1,34 @@
+from django.db.models import Count, QuerySet, F
+
+from .lookups import Unnest, Intersect
+
+
+class TagQuerySet(QuerySet):
+    '''
+    Mix into your tagged model to provide extra helpers
+    '''
+    def all_tag_values(self, name):
+        return (
+            self.order_by()
+            .annotate(_v=Unnest(name))
+            .values_list('_v', flat=True)
+            .distinct()
+        )
+
+    def count_tag_values(self, name):
+        return (
+            self.order_by()
+            .annotate(_v=Unnest(name))
+            .values('_v')
+            .annotate(count=Count('*'))
+            .values_list('_v', 'count')
+        )
+
+    def most_like(self, field, tags):
+        '''
+        '''
+        return (
+            self.order_by()
+            .annotate(similarity=Intersect(F(field), tags))
+            .order_by('-similarity')
+        )
---
+++ packages/django-array-tags/django-array-tags-0.2.2.tar.gz/django-array-tags-0.2.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-array-tags',
+    version='0.2.2',
+    description='Simple Tagging for Django using PG ArrayField',
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    url='http://github.com/funkybob/django-array-tags',
+    packages = find_packages(exclude=('tests*',)),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    requires = [
+        'Django (>=1.7)',
+    ],
+    install_requires = [
+        'Django>=1.7',
+    ],
+)
