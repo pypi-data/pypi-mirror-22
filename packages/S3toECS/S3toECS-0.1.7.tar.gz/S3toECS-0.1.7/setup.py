4ed62df9a69a32dee3b166eba31dbc5253e6e1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:59 +0000

    Add package S3toECS/0.1.7/S3toECS-0.1.7.tar.gz

---
+++ packages/S3toECS/S3toECS-0.1.7.tar.gz/S3toECS-0.1.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+  name = 'S3toECS', 
+  version = '0.1.7',
+  description = 'Tool that transfers Docker images from S3 to your ECS repository.',
+  author = 'Antonio Maiolo',
+  author_email = 'maiolo.antonio@territory.de',
+  license = 'MIT License',
+  url = 'https://github.com/territory-webguerillas/s3toecs/',
+  download_url = 'https://github.com/territory-webguerillas/s3toecs/archive/0.1.4.tar.gz', 
+  keywords = ['s3', 'ecs', 'aws', 's3 to ecs'],
+  python_requires='>=3.6',
+  py_modules=['s3toecs'],
+entry_points = {
+        'console_scripts': [
+            's3toecs = s3toecs:main'
+        ]
+  },
+  classifiers = [
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+  ],
+)
