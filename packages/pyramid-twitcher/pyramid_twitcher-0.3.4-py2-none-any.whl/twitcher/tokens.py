a7cda8ece9e15b527d2eaf12fdbb9bc727923f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:12 +0000

    Add package pyramid-twitcher/0.3.4/pyramid_twitcher-0.3.4-py2-none-any.whl

---
+++ packages/pyramid-twitcher/pyramid_twitcher-0.3.4-py2-none-any.whl/twitcher/tokens.py
@@ -0,0 +1,197 @@
+"""
+Classes to manage access tokens used in the security middleware.
+
+The implementation is based on `python-oauth2 <http://python-oauth2.readthedocs.io/en/latest/>`_
+
+See access token examples:
+
+* https://www.mapbox.com/developers/api/
+* http://python-oauth2.readthedocs.io/en/latest/store.html
+"""
+
+import uuid
+import time
+
+from twitcher.utils import now_secs
+from twitcher.exceptions import AccessTokenNotFound
+from twitcher.db import mongodb
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+def tokenstore_factory(registry):
+    db = mongodb(registry)
+    return MongodbAccessTokenStore(db.tokens)
+
+
+def tokengenerator_factory(registry):
+    return UuidGenerator()
+
+
+def expires_at(hours=1):
+    return now_secs() + hours * 3600 
+
+class AccessTokenStore(object):
+
+    def save_token(self, access_token):
+        """
+        Stores an access token with additional data.
+        """
+        raise NotImplementedError
+
+          
+    def delete_token(self, token):
+        """
+        Deletes an access token from the store using its token string to identify it.
+        This invalidates both the access token and the token.
+
+        :param token: A string containing the token.
+        :return: None.
+        """
+        raise NotImplementedError
+
+    
+    def fetch_by_token(self, token):
+        """
+        Fetches an access token from the store using its token string to
+        identify it.
+
+        :param token: A string containing the token.
+        :return: An instance of :class:`twitcher.tokens.AccessToken`.
+        """
+        raise NotImplementedError
+
+
+    def clean_tokens(self):
+        """
+        Removes all tokens from database.
+        """
+        raise NotImplementedError
+
+
+
+class MongodbAccessTokenStore(AccessTokenStore):
+    def __init__(self, collection):
+        self.collection = collection
+
+    def save_token(self, access_token):
+        self.collection.insert_one(access_token)
+
+          
+    def delete_token(self, token):
+        self.collection.delete_one({'token': token})
+
+    
+    def fetch_by_token(self, token):
+        token = self.collection.find_one({'token': token})
+        if not token:
+            raise AccessTokenNotFound
+        return AccessToken(token)
+
+
+    def clean_tokens(self):
+        self.collection.drop()
+
+
+class AccessTokenGenerator(object):
+    """
+    Base class for access token generators.
+    """
+    def create_access_token(self, valid_in_hours=1, user_environ=None):
+        """
+        Creates an access token.
+
+        TODO: check valid in hours
+        TODO: maybe specify how often a token can be used
+        """
+        access_token = AccessToken(
+            token = self.generate(),
+            expires_at = expires_at(hours=valid_in_hours),
+            user_environ = user_environ)
+        return access_token
+    
+    def generate(self):
+        raise NotImplementedError
+
+    
+class UuidGenerator(AccessTokenGenerator):
+    """
+    Generate a token using uuid4.
+    """
+    def generate(self):
+        """
+        :return: A new token
+        """
+        return uuid.uuid4().get_hex()
+
+    
+class AccessToken(dict):
+    """
+    Dictionary that contains access token. It always has ``'token'`` key.
+    """
+    
+    def __init__(self, *args, **kwargs):
+        super(AccessToken, self).__init__(*args, **kwargs)
+        if 'token' not in self:
+            raise TypeError("'token' is required")
+
+        self.expires_at = int( self.get("expires_at", 0) )
+
+            
+    @property
+    def token(self):
+        """Access token string."""
+        return self['token']
+
+
+    @property
+    def expires_in(self):
+        """
+        Returns the time until the token expires.
+        :return: The remaining time until expiration in seconds or 0 if the
+                 token has expired.
+        """
+        time_left = self.expires_at - now_secs()
+
+        if time_left > 0:
+            return time_left
+        return 0
+
+    
+    def is_expired(self):
+        """
+        Determines if the token has expired.
+        :return: `True` if the token has expired. Otherwise `False`.
+        """
+        if self.expires_at is None:
+            return True
+
+        if self.expires_in > 0:
+            return False
+
+        return True
+
+    
+    @property
+    def user_environ(self):
+        environ = self.get('user_environ') or {}
+        return environ
+
+    
+    def __str__(self):
+        return self.token
+
+    
+    def __repr__(self):
+        cls = type(self)
+        repr_ = dict.__repr__(self)
+        return '{0}.{1}({2})'.format(cls.__module__, cls.__name__, repr_)
+
+
+
+    
+
+
+
+
---
+++ packages/pyramid-twitcher/pyramid_twitcher-0.3.4-py2-none-any.whl/twitcher/utils/__init__.py
@@ -0,0 +1,81 @@
+import time
+from datetime import datetime
+import pytz
+from urlparse import urlparse
+from lxml import etree
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+def now():
+    return localize_datetime(datetime.utcnow())
+
+
+def now_secs():
+    """
+    Return the current time in seconds since the Epoch.
+    """
+    return int(time.time())
+
+
+def localize_datetime(dt, tz_name='UTC'):
+    """Provide a timzeone-aware object for a given datetime and timezone name
+    """
+    tz_aware_dt = dt
+    if dt.tzinfo is None:
+        utc = pytz.timezone('UTC')
+        aware = utc.localize(dt)
+        timezone = pytz.timezone(tz_name)
+        tz_aware_dt = aware.astimezone(timezone)
+    else:
+        logger.warn('tzinfo already set')
+    return tz_aware_dt
+
+
+def baseurl(url):
+    """
+    return baseurl of given url
+    """
+    parsed_url = urlparse(url)
+    if not parsed_url.netloc or not parsed_url.scheme in ("http", "https"):
+        raise ValueError('bad url')
+    service_url = "%s://%s%s" % (parsed_url.scheme, parsed_url.netloc, parsed_url.path.strip())
+    return service_url
+
+
+def path_elements(path):
+    elements = [el.strip() for el in path.split('/')]
+    elements = [el for el in elements if len(el) > 0]
+    return elements
+
+
+def lxml_strip_ns(tree):
+    for node in tree.iter():
+        try:
+            has_namespace = node.tag.startswith('{')
+        except AttributeError:
+            continue  # node.tag is not a string (node is a comment or similar)
+        if has_namespace:
+            node.tag = node.tag.split('}', 1)[1]
+
+
+def replace_caps_url(xml, url):
+    ns = {
+        'ows': 'http://www.opengis.net/ows/1.1',
+        'xlink': 'http://www.w3.org/1999/xlink'}
+    doc = etree.fromstring(xml)
+    logger.debug("doc.tag: %s", doc.tag)
+    # wms onlineResource
+    if 'WMT_MS_Capabilities' in doc.tag or 'WMS_Capabilities' in doc.tag:
+        for element in doc.findall('.//OnlineResource[@xlink:href]', namespaces=ns):
+            parsed_url = urlparse(element.get('{http://www.w3.org/1999/xlink}href'))
+            new_url = url
+            if parsed_url.query:
+                new_url += '?' + parsed_url.query
+            element.set('{http://www.w3.org/1999/xlink}href', new_url)
+    # wps operations
+    elif 'Capabilities' in doc.tag:
+        for element in doc.findall('ows:OperationsMetadata//*[@xlink:href]', namespaces=ns):
+            element.set('{http://www.w3.org/1999/xlink}href', url)
+    return etree.tostring(doc)
