443bf4505b7b1cca070ccd49d2eac84392527d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:14 +0000

    Add package mprequest/0.1.9/mprequest-0.1.9.tar.gz

---
+++ packages/mprequest/mprequest-0.1.9.tar.gz/mprequest-0.1.9/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+# Installation requirements needed absolutely for the program to run
+install_requires = [
+    'requests>=2.12.4',
+]
+
+# Additional feature sets and their requirements
+extras_require = {
+}
+
+setup(
+    name='mprequest',
+    version='0.1.9',
+    description='MP Requesting Library',
+    author='John Hopper',
+    author_email='john.hopper@jpserver.net',
+    packages=find_packages(),
+
+    install_requires=install_requires,
+    extras_require=extras_require
+)
