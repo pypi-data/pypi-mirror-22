afbb7b177e5bc6768c9c5e86550ca681c3254fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:42 +0000

    Add package guillotina-elasticsearch/1.0.13/guillotina_elasticsearch-1.0.13.tar.gz

---
+++ packages/guillotina-elasticsearch/guillotina_elasticsearch-1.0.13.tar.gz/guillotina_elasticsearch-1.0.13/guillotina_elasticsearch/api.py
@@ -0,0 +1,34 @@
+from guillotina import configure
+from guillotina.component import getUtility
+from guillotina.interfaces import ICatalogUtility
+from guillotina.interfaces import IContainer
+from guillotina.component import queryUtility
+from guillotina_elasticsearch.manager import get_mappings
+from guillotina_elasticsearch.manager import DEFAULT_SETTINGS
+from guillotina import app_settings
+
+
+@configure.service(
+    context=IContainer, name='@update_mapping', method='POST',
+    permission='guillotina.ManageCatalog')
+async def update_mapping(context, request):
+    util = getUtility(ICatalogUtility)
+    await util.migrate_index(context)
+
+
+@configure.service(
+    context=IContainer, name='@force_mapping', method='POST',
+    permission='guillotina.ManageCatalog')
+async def update_mapping(context, request):
+    catalog = queryUtility(ICatalogUtility)
+    index_name = await catalog.get_index_name(request.container)
+    mappings = get_mappings()
+    index_settings = DEFAULT_SETTINGS.copy()
+    index_settings.update(app_settings.get('index', {}))
+    await catalog.conn.indices.put_settings(
+        index_settings, index_name)
+    for key, value in mappings.items():
+        await catalog.conn.indices.put_mapping(index_name, key, value)
+    return {
+        'status': 'ok'
+    }
