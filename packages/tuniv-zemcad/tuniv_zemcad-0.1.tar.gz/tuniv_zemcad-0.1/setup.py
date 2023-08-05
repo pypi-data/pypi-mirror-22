65222b17f42716d3bc2ab9e6f2aed756fe970a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:17 +0000

    Add package tuniv-zemcad/0.1/tuniv_zemcad-0.1.tar.gz

---
+++ packages/tuniv-zemcad/tuniv_zemcad-0.1.tar.gz/tuniv_zemcad-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tuniv_zemcad',
+      version='0.1',
+      description='tuniv zemcad library',
+      url='https://github.com/tlubenov/zemcad2gis',
+      author='Todor Lubenov',
+      author_email='tlubenov@gmail.com',
+      license='MIT',
+      packages=['tuniv_zemcad'],
+      zip_safe=False)
