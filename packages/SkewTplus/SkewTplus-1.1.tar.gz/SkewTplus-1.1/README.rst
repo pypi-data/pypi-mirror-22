6dc8af3a9d2127d1ad11f864dc7d195d1beda30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:10 +0000

    Add package SkewTplus/1.1/SkewTplus-1.1.tar.gz

---
+++ packages/SkewTplus/SkewTplus-1.1.tar.gz/SkewTplus-1.1/README.rst
@@ -0,0 +1,89 @@
+=========================================================
+SkewTplus -- Atmospheric Profile Plotting and Diagnostics
+=========================================================
+
+SkewTplus provides a few useful tools to help with the plotting and analysis of 
+upper atmosphere data. In particular, it provides some useful classes to 
+handle the awkward skew-x projection.
+        
+This package is based on the SkewT Python package developed by Thomas Chubb
+(https://pypi.python.org/pypi/SkewT/)
+        
+The main difference with the original *SkewT package* is that the vertical soundings 
+plots are handled by a special class (SkewT).
+The new *SkewT* class extends the base
+`matplolib's Figure <http://matplotlib.org/api/figure_api.html?highlight=figure#module-matplotlib.figure>`_
+class with an interface similar to 
+`matplolib's pyplot <http://matplotlib.org/api/pyplot_api.html>`_.
+It also allows to create Skew-T type plots in a simple way.
+This new class allows a complete control over the Figure properties like
+multiple plots (normal axis and Skew-T axis).
+
+In addition, the **thermodynamics** module was improved.
+All the intensive computations were migrated to Cython and paralellized.
+   
+The SkewT Python package was a cornerstone of this project.  
+We are grateful to all its collaborators.
+
+
+*Technology builds on technology*
+
+Documentation
+=============
+
+Check the documentation at http://www.meteo.mcgill.ca/~aperez/SkewTplus/
+
+Dependencies
+============
+
+The SkewTplus package need the following dependencies
+
+* matplotlib
+* numpy
+* cython
+* netCDF4
+
+For running the examples:
+
+* Basemap
+
+
+
+Installing SkewT
+================
+
+PIP install
+-----------
+
+The easiest way is to install the package with pip::
+
+    pip install SkewTplus
+
+
+Install from source
+-------------------
+
+The latest version can be installed manually by downloading the sources from
+https://github.com/aperezhortal/SkewTplus
+
+Then, run::
+
+    python setup.py install
+
+If you want to put it somewhere different than your system files, you can do::
+    
+    python setup.py install --prefix=/path/to/local/dir
+
+IMPORTANT: All the dependencies need to be already installed! 
+
+
+
+To-Do List
+==========
+* More column diagnostics.
+* Hodographs 
+
+
+
+
+
---
+++ packages/SkewTplus/SkewTplus-1.1.tar.gz/SkewTplus-1.1/setup.py
@@ -0,0 +1,72 @@
+
+
+
+from __future__ import division, absolute_import, print_function
+
+
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+
+try:
+    import numpy
+except ImportError:
+    raise RuntimeError( "Numpy required to pior running the package installation\n" +
+                        "Try installing it with:\n" + 
+                        "$> pip install numpy" )
+    
+    
+try:
+    from Cython.Build.Dependencies import cythonize
+    CythonPresent = True
+except ImportError:
+    CythonPresent = False
+    
+
+if CythonPresent:
+    
+    thermodynamicsLibExtension = Extension( "SkewTplus._thermodynamics",
+                                            sources = ['SkewTplus/_thermodynamics.pyx'],
+                                            extra_compile_args = ['-fopenmp'],
+                                            extra_link_args = ['-fopenmp'] ,
+                                            include_dirs=[numpy.get_include()],
+                                            language='c++') 
+                                           
+    externalModules = cythonize([thermodynamicsLibExtension])                                       
+else:
+    thermodynamicsLibExtension = Extension( "SkewTplus._thermodynamics",
+                                            sources = ['SkewTplus/_thermodynamics.cpp'],
+                                            extra_compile_args = ['-fopenmp'],
+                                            extra_link_args = ['-fopenmp'] ,
+                                            include_dirs=[numpy.get_include()],
+                                            language='c++') 
+    
+    externalModules = [thermodynamicsLibExtension]
+
+build_requires=['matplotlib','numpy','netCDF4 ']
+
+setup(
+    name='SkewTplus',
+    version='1.1',
+    author = "Andres Perez Hortal",
+    author_email = "andresperezcba@gmail.com",
+    packages=find_packages(),
+    ext_modules = externalModules,
+    url='http://pypi.python.org/pypi/SkewTplus/',
+    license='LICENSE.txt',
+    description='Atmospheric Profile Plotting and Diagnostics',
+    long_description=open('README.rst').read(),
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Atmospheric Science',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Cython'],
+    setup_requires=build_requires,
+    install_requires=build_requires
+    )
+
+
