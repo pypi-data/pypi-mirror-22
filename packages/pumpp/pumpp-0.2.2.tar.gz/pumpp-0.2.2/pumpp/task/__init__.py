6339d6ebc73482f31c3c6abffe4ea3268effa6c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:04 +0000

    Add package pumpp/0.2.2/pumpp-0.2.2.tar.gz

---
+++ packages/pumpp/pumpp-0.2.2.tar.gz/pumpp-0.2.2/pumpp/task/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+Task transformations
+====================
+.. autosummary::
+    :toctree: generated/
+
+    BaseTaskTransformer
+    BeatTransformer
+    ChordTransformer
+    SimpleChordTransformer
+    ChordTagTransformer
+    VectorTransformer
+    DynamicLabelTransformer
+    StaticLabelTransformer
+'''
+
+from .base import *
+from .chord import *
+from .event import *
+from .regression import *
+from .tags import *
