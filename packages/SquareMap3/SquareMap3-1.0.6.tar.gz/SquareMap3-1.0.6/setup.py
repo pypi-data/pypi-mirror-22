af9af4f88b7f1f16ba208b552f5a4fe15725bb82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:50 +0000

    Add package SquareMap3/1.0.6/SquareMap3-1.0.6.tar.gz

---
+++ packages/SquareMap3/SquareMap3-1.0.6.tar.gz/SquareMap3-1.0.6/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+"""Installs SquareMap using distutils
+
+Run:
+	python setup.py install
+to install the package from the source archive.
+"""
+import os
+import versioneer
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = [(line.split('=')[1]).strip().strip('"').strip("'")
+           for line in open(os.path.join('squaremap', '__init__.py'))
+           if line.startswith('__version__')][0]
+
+if __name__ == "__main__":
+    extraArguments = {
+        'classifiers': [
+            """License :: OSI Approved :: BSD License""",
+            """Programming Language :: Python""",
+            """Topic :: Software Development :: Libraries :: Python Modules""",
+            """Intended Audience :: Developers""",
+        ],
+        'keywords':
+        'wxPython,squaremap',
+        'long_description':
+        """Hierarchic visualization control for wxPython 
+
+Hierarchic data visualization control intended for use with 
+structures where "parents" hold collections of weighted children.
+
+Can be used for viewing sizes of directories on file-systems,
+running-time of functions in profiling, or time spent on tasks 
+in a time-tracking application.""",
+        'platforms': ['Any'],
+    }
+    ### Now the actual set up call
+    setup(
+        name="SquareMap3",
+        version=versioneer.get_version(),
+        url="https://launchpad.net/squaremap",
+        description="Hierarchic data-visualisation control for wxPython",
+        author="Mike C. Fletcher",
+        author_email="mcfletch@vrplumber.com",
+        license="BSD",
+        install_requires=['wxPython'],
+        package_dir={
+            'squaremap': 'squaremap',
+        },
+        packages=[
+            'squaremap',
+        ],
+        options={
+            'sdist': {
+                'force_manifest': 1,
+                'formats': ['gztar', 'zip'],
+            },
+        },
+        zip_safe=False,
+        use_2to3=True,
+        cmdclass=versioneer.get_cmdclass(),
+        **extraArguments)
