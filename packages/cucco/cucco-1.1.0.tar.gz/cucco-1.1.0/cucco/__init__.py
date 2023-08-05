6424602c1f9ee728b9228c78d74c720074279924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:09 +0000

    Add package cucco/1.1.0/cucco-1.1.0.tar.gz

---
+++ packages/cucco/cucco-1.1.0.tar.gz/cucco-1.1.0/cucco/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module allows to normalize texts applying different normalizations.
+"""
+
+from __future__ import absolute_import
+
+__version__ = '1.1.0'
+
+from cucco.cucco import Cucco
