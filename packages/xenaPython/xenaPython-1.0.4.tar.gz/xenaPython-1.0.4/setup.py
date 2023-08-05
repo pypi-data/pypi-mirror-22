a7b15506a5b9920234f469b1fd8cc615d6327900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:59 +0000

    Add package xenaPython/1.0.4/xenaPython-1.0.4.tar.gz

---
+++ packages/xenaPython/xenaPython-1.0.4.tar.gz/xenaPython-1.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+print(find_packages())
+setup(
+    name="xenaPython",
+    version="1.0.4",
+    packages=find_packages(),
+    author = '@jingchunzhu, @GiriB, @souravsingh',
+    author_email = 'jingchunzhu@gmail.com',
+    description = 'XENA python API',
+    url = 'https://github.com/ucscXena/xenaPython',
+    keywords = ['xena', 'ucsc', 'xenaAPI'],
+    license='Apache 2.0',
+)
