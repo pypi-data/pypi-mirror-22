9746135471f278287dd86f70f6f88ba80b3084b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:14 +0000

    Add package zku/1.0.0/zku-1.0.0.tar.gz

---
+++ packages/zku/zku-1.0.0.tar.gz/zku-1.0.0/zymkey/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from .module import Zymkey
+
+__all__ = ['Zymkey', 'client']
+
+client = Zymkey()
+
---
+++ packages/zku/zku-1.0.0.tar.gz/zku-1.0.0/zymkey/exceptions.py
@@ -0,0 +1,14 @@
+class CommandError(Exception):
+    pass
+
+
+class VaultError(Exception):
+    pass
+
+
+class VerificationError(Exception):
+    pass
+
+
+class ZymkeyLibraryError(Exception):
+    pass
