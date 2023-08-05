a2e6ec9b6b4256c8f474b169516e785357ce801e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:08 +0000

    Add package guillotina/1.0.0a22/guillotina-1.0.0a22.tar.gz

---
+++ packages/guillotina/guillotina-1.0.0a22.tar.gz/guillotina-1.0.0a22/guillotina/db/storages/base.py
@@ -0,0 +1,16 @@
+
+
+class BaseStorage(object):
+
+    _cache_strategy = 'dummy'
+    _read_only = False
+    _transaction_strategy = 'merge'
+
+    def __init__(self, read_only=False, transaction_strategy='merge',
+                 cache_strategy='dummy'):
+        self._read_only = read_only
+        self._transaction_strategy = transaction_strategy
+        self._cache_strategy = cache_strategy
+
+    def read_only(self):
+        return self._read_only
