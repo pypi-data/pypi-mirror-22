19e2b5a0984e384b280eb08ae7bc248b6a9e3196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:47 +0000

    Add package rlipython/0.1.0/rlipython-0.1.0-py2.py3-none-any.whl

---
+++ packages/rlipython/rlipython-0.1.0-py2.py3-none-any.whl/rlipython/__init__.py
@@ -0,0 +1,10 @@
+"""
+Readline interface for IPython
+"""
+
+version_info = (0,0,1)
+__version__ = '.'.join([str(x) for x in version_info])
+
+from .completer import RLCompleter
+from .shell import TerminalInteractiveShell
+from .lifecycle import install, uninstall
