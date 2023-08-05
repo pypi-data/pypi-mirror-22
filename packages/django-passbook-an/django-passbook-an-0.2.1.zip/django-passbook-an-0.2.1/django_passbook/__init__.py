b00f75c46f77b15201dde0cdd1e5393669842089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:04 +0000

    Add package django-passbook-an/0.2.1/django-passbook-an-0.2.1.zip

---
+++ packages/django-passbook-an/django-passbook-an-0.2.1.zip/django-passbook-an-0.2.1/django_passbook/__init__.py
@@ -0,0 +1,7 @@
+VERSION = ('0', '2', '1')
+
+
+def get_version(*args, **kwargs):
+    return '.'.join(VERSION)
+
+__version__ = get_version()
---
+++ packages/django-passbook-an/django-passbook-an-0.2.1.zip/django-passbook-an-0.2.1/django_passbook/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Pass(models.Model):
+    """
+    Pass instance
+    """
+    pass_type_identifier = models.CharField(max_length=255)
+    serial_number = models.CharField(max_length=255)
+    authentication_token = models.CharField(max_length=255)
+    data = models.FileField(upload_to='passes')
+    updated_at = models.DateTimeField()
+
+    def __unicode__(self):
+        return self.serial_number
+
+    class Meta:
+        unique_together = (('pass_type_identifier', 'serial_number'),)
+
+
+class Registration(models.Model):
+    """
+    Registration of a Pass on a device
+    """
+    device_library_identifier = models.CharField(max_length=64)
+    push_token = models.CharField(max_length=64)
+    pazz = models.ForeignKey(Pass)
+
+    def __unicode__(self):
+        return self.device_library_identifier
+
+
+class Log(models.Model):
+    """
+    Log message sent by a device
+    """
+    message = models.TextField()
+
+    def __unicode__(self):
+        return self.message
