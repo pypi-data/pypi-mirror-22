27444c19a772acf6f0951ed15c61620fedcf6255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:35 +0000

    Add package pydradis/0.2.1/pydradis-0.2.1.tar.gz

---
+++ packages/pydradis/pydradis-0.2.1.tar.gz/pydradis-0.2.1/pydradis/__init__.py
@@ -0,0 +1,1 @@
+from pydradis import Pydradis
---
+++ packages/pydradis/pydradis-0.2.1.tar.gz/pydradis-0.2.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+	name = "pydradis",
+	packages = ["pydradis"],
+	version = "0.2.1",
+	description = "Dradis API Python Wrapper",
+	author = "NCAT Labs",
+	author_email = "psosa@novacoast.com",
+	url = "https://github.com/ncatlabs/pydradis",
+	keywords = ["dradis","security","api","wrapper"],
+	classifiers = ["Topic :: Security"],
+)
