e6316d81dc3fbc6151134367d9608c2acd0f07ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package atproject/1.0/atproject-1.tar.gz

---
+++ packages/atproject/atproject-1.tar.gz/atproject-1/atproject/__init__.py
@@ -0,0 +1,16 @@
+product_name = 'atproject'
+version_short = 'T-0.4.0'
+
+
+__author__ = """
+Christopher J. Markiewicz <effigies@gmail.com>
+
+Original att code by:
+Bram Cohen, Henry 'Pi' James, Bill Bumgarner, Petru Paler,
+Uoti Urpala, Ross Cohen, Edward Keyes and John Hoffman
+"""
+__credits__ = """
+Yejun Yang and Myers Carpenter for NAT port mapping code adapted
+    in natpunch
+"""
+__version__ = version_short
