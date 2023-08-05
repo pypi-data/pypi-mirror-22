8fba6872616fd7518711d861228adae3d45e551f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:14 +0000

    Add package pyfact/0.10.2/pyfact-0.10.2.tar.gz

---
+++ packages/pyfact/pyfact-0.10.2.tar.gz/pyfact-0.10.2/fact/__init__.py
@@ -0,0 +1,17 @@
+from .time import fjd, iso2dt, run2dt, facttime, night, night_integer, datestr
+from . import plotting
+from . import auxservices
+
+__version__ = '0.10.2'
+
+__all__ = [
+    'fjd',
+    'iso2dt',
+    'run2dt',
+    'facttime',
+    'night',
+    'night_integer',
+    'datestr',
+    'plotting',
+    'auxservices',
+]
---
+++ packages/pyfact/pyfact-0.10.2.tar.gz/pyfact-0.10.2/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+from fact import __version__
+
+
+setup(
+    name='pyfact',
+    version=__version__,
+    description='A module containing useful methods for working with fact',
+    url='http://github.com/fact-project/pyfact',
+    author='Maximilian Noethe, Dominik Neise',
+    author_email='maximilian.noethe@tu-dortmund.de',
+    license='MIT',
+    packages=[
+        'fact',
+        'fact.plotting',
+        'fact.slowdata',
+        'fact.credentials',
+        'fact.auxservices',
+        'fact.factdb',
+        'fact.analysis',
+        'fact.instrument',
+    ],
+    package_data={
+        '': ['resources/*', 'credentials/credentials.encrypted']
+    },
+    tests_require=['pytest>=3.0.0'],
+    setup_requires=['pytest-runner'],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib>=1.4',
+        'python-dateutil',
+        'pymongo>=2.7',
+        'simple-crypt',
+        'setuptools',
+        'sqlalchemy',
+        'pymysql',
+        'pandas',
+        'astropy',
+        'peewee',
+        'h5py',
+        'wrapt',
+        'tables',  # pytables in anaconda
+        'python-dateutil',
+    ],
+    zip_safe=False,
+)
