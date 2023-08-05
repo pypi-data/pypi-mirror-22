2408ccb68e7edd0ba01e83f741cd2a064f29b859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:50 +0000

    Add package schul-cloud-ressources-server-tests/1.0.27/schul_cloud_ressources_server_tests-1.0.27-py3-none-any.whl

---
+++ packages/schul-cloud-ressources-server-tests/schul_cloud_ressources_server_tests-1.0.27-py3-none-any.whl/schul_cloud_ressources_server_tests/tests/test_server.py
@@ -0,0 +1,22 @@
+import requests
+from pytest import raises
+
+
+def test_server_is_there(ressources_server):
+    """Test that the server is there."""
+    requests.get(ressources_server.url)
+
+
+def test_server_works_on_data(ressources_server, valid_ressource):
+    """Test that the api adds a ressource."""
+    ressources_server.api.add_ressource(valid_ressource)
+    assert ressources_server.get_ressources() == [valid_ressource]
+
+
+# this must be the last test
+def test_server_stops(ressources_server):
+    """Test that the server stops in the end."""
+    ressources_server.shutdown()
+    with raises(requests.exceptions.ReadTimeout):
+        requests.get(ressources_server.url,timeout=0.01)
+    
