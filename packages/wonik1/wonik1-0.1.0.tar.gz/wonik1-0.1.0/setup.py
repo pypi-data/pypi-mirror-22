a108d21a52253d0efde9f65bb4079a6bbedf6677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:03 +0000

    Add package wonik1/0.1.0/wonik1-0.1.0.tar.gz

---
+++ packages/wonik1/wonik1-0.1.0.tar.gz/wonik1-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'wonik1',
+  packages = ['wonik1'], # this must be the same as the name above
+  version = '0.1.0',
+  description = 'A random test lib',
+  author = 'Wonik Jang',
+  author_email = 'jwimsms@gmail.com',
+  url = 'https://github.com/wonikjang/wonik1', # use the URL to the github repo
+  download_url = 'https://github.com/wonikjang/wonik1/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
