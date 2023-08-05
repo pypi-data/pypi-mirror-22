8af8ec3c116f2868759fc62323faf085df7090f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:11 +0000

    Add package gocept.reference/0.9.4/gocept.reference-0.9.4.tar.gz

---
+++ packages/gocept.reference/gocept.reference-0.9.4.tar.gz/gocept.reference-0.9.4/src/gocept/reference/testing.py
@@ -0,0 +1,62 @@
+# -*- coding: latin-1 -*-
+import doctest
+import os.path
+import zope.annotation.interfaces
+import zope.container.contained
+import zope.app.testing.functional
+import zope.interface
+
+import gocept.reference
+
+
+ftesting_zcml = os.path.join(os.path.dirname(__file__), 'ftesting.zcml')
+FunctionalLayer = zope.app.testing.functional.ZCMLLayer(
+    ftesting_zcml, __name__, 'FunctionalLayer')
+FunctionalLayer.allow_teardown = True
+
+
+def FunctionalDocFileSuite(*paths, **kw):
+    kw['optionflags'] = (kw.get('optionflags', 0) |
+                         doctest.ELLIPSIS |
+                         doctest.NORMALIZE_WHITESPACE)
+    suite = zope.app.testing.functional.FunctionalDocFileSuite(*paths, **kw)
+    suite.layer = FunctionalLayer
+    return suite
+
+
+class Address(zope.container.contained.Contained):
+
+    zope.interface.implements(
+        zope.annotation.interfaces.IAttributeAnnotatable)
+
+    city = gocept.reference.Reference()
+
+
+class City(zope.container.contained.Contained):
+
+    zope.interface.implements(
+        zope.annotation.interfaces.IAttributeAnnotatable)
+
+    cultural_institutions = gocept.reference.ReferenceCollection(
+        ensure_integrity=True)
+
+
+class Village(zope.container.contained.Contained):
+
+    zope.interface.implements(
+        zope.annotation.interfaces.IAttributeAnnotatable)
+
+    cultural_institutions = gocept.reference.ReferenceCollection(
+        ensure_integrity=False)
+
+
+class Monument(zope.container.contained.Contained):
+
+    zope.interface.implements(
+        zope.annotation.interfaces.IAttributeAnnotatable)
+
+    city = gocept.reference.Reference(ensure_integrity=True)
+
+
+class CulturalInstitution(zope.container.contained.Contained):
+    title = None
---
+++ packages/gocept.reference/gocept.reference-0.9.4.tar.gz/gocept.reference-0.9.4/src/gocept/reference/tests.py
@@ -0,0 +1,58 @@
+# -*- coding: latin-1 -*-
+"""Tests for gocept.reference"""
+
+import unittest
+import zope.interface.verify
+
+import gocept.reference.content
+import gocept.reference.interfaces
+import gocept.reference.manager
+import gocept.reference.testing
+
+
+class TestInterfaces(unittest.TestCase):
+
+    def test_IReferenceSource(self):
+        zope.interface.verify.verifyObject(
+            gocept.reference.interfaces.IReferenceSource,
+            gocept.reference.content.ReferenceSource(object))
+
+    def test_IReferenceTarget(self):
+        zope.interface.verify.verifyObject(
+            gocept.reference.interfaces.IReferenceTarget,
+            gocept.reference.content.ReferenceTarget(object))
+
+    def test_IReferenceManager(self):
+        zope.interface.verify.verifyObject(
+            gocept.reference.interfaces.IReferenceManager,
+            gocept.reference.manager.ReferenceManager())
+
+
+class TestContentFunctions(unittest.TestCase):
+
+    def test_find_references_handles_broken_attributes(self):
+        class BrokenDescriptor(object):
+            def __get__(self, instance, *args, **kw):
+                raise AttributeError()
+
+        class BrokenAttribute(object):
+            asdf = BrokenDescriptor()
+        obj = BrokenAttribute()
+        self.assertEquals(
+            [],
+            list(gocept.reference.content.find_references(obj)))
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestInterfaces))
+    suite.addTest(unittest.makeSuite(TestContentFunctions))
+    suite.addTest(gocept.reference.testing.FunctionalDocFileSuite(
+        'reference.txt',
+        'collection.txt',
+        'verify.txt',
+        'regression.txt',
+        'field.txt',
+        'fix.txt',
+    ))
+    return suite
