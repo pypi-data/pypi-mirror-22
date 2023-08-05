c2d79b3730cc6dfda62fff1aeed0451b8a407db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:40 +0000

    Add package Flask-WatQY/0.0.2/Flask-WatQY-0.0.2.tar.gz

---
+++ packages/Flask-WatQY/Flask-WatQY-0.0.2.tar.gz/Flask-WatQY-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Flask-WatQY',
+    version='0.0.2',
+    packages=['flask_watqy'],
+    url='',
+    license='',
+    author='ll1l11',
+    author_email='',
+    description=''
+)
