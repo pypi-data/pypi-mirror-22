3308ad1a35c112afc90e97a9037b232d60ec97f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:16 +0000

    Add package arduino_lights/0.6.0/arduino_lights-0.6.0.tar.gz

---
+++ packages/arduino_lights/arduino_lights-0.6.0.tar.gz/arduino_lights-0.6.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'arduino_lights',
+  packages = ['arduino_lights'],
+  version = '0.6.0',
+  description = 'Control LED lights via Arduino',
+  author = 'Arthur Taylor',
+  author_email = 'arthur.taylor@gmail.com',
+  url = 'https://github.com/craftcodiness/arduino-lights',
+  download_url = 'https://github.com/craftcodiness/arduino-lights/archive/0.6.0.tar.gz', 
+  keywords = ['led', 'serial', 'arduino'],
+  classifiers = [],
+)
