7053e65965c88a742d8107d37e0ccd129b22d5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:49 +0000

    Add package flask-thumbnails/0.2.2/Flask-thumbnails-0.2.2.tar.gz

---
+++ packages/flask-thumbnails/Flask-thumbnails-0.2.2.tar.gz/Flask-thumbnails-0.2.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='Flask-thumbnails',
+    version='0.2.2',
+    url='https://github.com/silentsokolov/flask-thumbnails',
+    license='MIT',
+    author='Dmitriy Sokolov',
+    author_email='silentsokolov@gmail.com',
+    description='A simple extension to create a thumbs for the Flask',
+    packages=['flask_thumbnails'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+    ],
+)
