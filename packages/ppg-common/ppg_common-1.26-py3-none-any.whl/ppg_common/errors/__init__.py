f0c155fe56eac1d0f36671ed9fccdd618c07161d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:22 +0000

    Add package ppg-common/1.26/ppg_common-1.26-py3-none-any.whl

---
+++ packages/ppg-common/ppg_common-1.26-py3-none-any.whl/ppg_common/errors/__init__.py
@@ -0,0 +1,4 @@
+from .bubbles import BubblesHttpError, BubblesHttpErrors
+from .blossom import BlossomHTTPError, BlossomHTTPErrors
+from .buttercup import ButterCupHTTPException, ButterCupHTTPErrors
+from .base import PpgHTTPError, PpgHttpErrors
