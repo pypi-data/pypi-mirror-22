22581aa7f130a9934d70d4f7ce9ca12598d16c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:56 +0000

    Add package ezprogbar/1.2/ezprogbar-1.2.tar.gz

---
+++ packages/ezprogbar/ezprogbar-1.2.tar.gz/ezprogbar-1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='ezprogbar',
+    version='1.2',
+    description='simple textual progress bar',
+    url='http://github.com/samyzaf/ezprogbar',
+    author='Samy Zafrany',
+    author_email='admin@samyzaf.com',
+    license='MIT',
+    packages=['ezprogbar'],
+    zip_safe=False,
+)
+
