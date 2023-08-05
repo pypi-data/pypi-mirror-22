ec2152c8eb54668ff0de1ab1d74c24f97174e239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:49 +0000

    Add package django_busybody/0.1.9/django_busybody-0.1.9-py2.py3-none-any.whl

---
+++ packages/django_busybody/django_busybody-0.1.9-py2.py3-none-any.whl/django_busybody/custom_storages/s3.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+from django.conf import settings
+
+try:
+    from storages.backends.s3boto import S3BotoStorage
+
+    from .hashed import (
+        HashedStorageMixin,
+        CachedHashValueFilesMixin,
+        CachedManifestFilesMixin
+    )
+    from .overwrite import OverwriteStorageMixin
+
+    class StaticS3Storage(CachedHashValueFilesMixin, S3BotoStorage):
+        location = getattr(settings, 'STATICFILES_LOCATION',
+                           '{}/{}'.format(getattr(settings, 'PROJECT_NAME', 'django'), 'static'))
+
+    class ManifestFilesStaticS3Storage(CachedManifestFilesMixin, S3BotoStorage):
+        location = getattr(settings, 'STATICFILES_LOCATION',
+                           '{}/{}'.format(getattr(settings, 'PROJECT_NAME', 'django'), 'static'))
+
+    class MediaS3Storage(S3BotoStorage):
+        location = getattr(settings, 'MEDIAFILES_LOCATION',
+                           '{}/{}'.format(getattr(settings, 'PROJECT_NAME', 'django'), 'media'))
+
+    class HashedMediaS3Storage(HashedStorageMixin, OverwriteStorageMixin, MediaS3Storage):
+        IGNORE_OVERWRITE = True
+
+except ImportError:  # pragma: no cover
+    pass
