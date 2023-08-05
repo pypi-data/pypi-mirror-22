39757cd165740544a758be4bac012cb6800f304f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:58 +0000

    Add package nesterman/1.2.0/nesterman-1.2.0.tar.gz

---
+++ packages/nesterman/nesterman-1.2.0.tar.gz/nesterman-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nesterman',
+    version='1.2.0',
+    py_modules=['nesterman'],
+    author='sethhinckley',
+    author_email='shinckley11@gmail.com',
+    url='http://sethhinckley.site11.com',
+    description='A simple printer of nester lists',
+)
