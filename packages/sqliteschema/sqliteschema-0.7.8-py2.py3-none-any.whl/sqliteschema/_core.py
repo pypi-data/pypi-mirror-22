a110aabf905213c8ac4cae0a5554740c71cfe4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:15 +0000

    Add package sqliteschema/0.7.8/sqliteschema-0.7.8-py2.py3-none-any.whl

---
+++ packages/sqliteschema/sqliteschema-0.7.8-py2.py3-none-any.whl/sqliteschema/_core.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from ._factory import (
+    SqliteSchemaTextExtractorFactory,
+    SqliteSchemaTableExtractorFactory,
+)
+from ._interface import SqliteSchemaExtractorInterface
+
+
+class SqliteSchemaExtractor(SqliteSchemaExtractorInterface):
+    __VALID_FORMAT_LIST = ["text", "table"]
+
+    @property
+    def verbosity_level(self):
+        return self.__writer.verbosity_level
+
+    def __init__(
+            self, database_path, verbosity_level=None, output_format="table"):
+        format_mapping = {
+            "text": SqliteSchemaTextExtractorFactory,
+            "table": SqliteSchemaTableExtractorFactory,
+        }
+        extractor_factory = format_mapping.get(output_format)(database_path)
+
+        if extractor_factory is None:
+            raise ValueError("unknown format: expected={}, actual={}".format(
+                list(format_mapping), output_format))
+
+        self.__writer = extractor_factory.create(verbosity_level)
+
+    def get_table_name_list(self):
+        return self.__writer.get_table_name_list()
+
+    def get_table_schema(self, table_name):
+        return self.__writer.get_table_schema(table_name)
+
+    def get_table_schema_text(self, table_name):
+        return self.__writer.get_table_schema_text(table_name)
+
+    def get_database_schema(self):
+        return self.__writer.get_database_schema()
+
+    def dumps(self):
+        return self.__writer.dumps()
