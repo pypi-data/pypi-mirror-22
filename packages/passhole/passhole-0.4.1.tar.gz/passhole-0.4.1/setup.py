493b169e453197ddfbd82a13ae314b72414ebac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:24 +0000

    Add package passhole/0.4.1/passhole-0.4.1.tar.gz

---
+++ packages/passhole/passhole-0.4.1.tar.gz/passhole-0.4.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='passhole',
+    version='0.4.1',
+    packages=['passhole'],
+    author="Evan Widloski",
+    author_email="evan@evanw.org",
+    description="dmenu interface to KeePass databases",
+    long_description=open('README.rst').read(),
+    license="MIT",
+    keywords="keepass dmenu password store passwords",
+    url="https://github.com/purduelug/passhole",
+    scripts=['passhole/passhole'],
+    install_requires=[
+        "PyUserInput",
+        "pykeepass",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+    ]
+)
