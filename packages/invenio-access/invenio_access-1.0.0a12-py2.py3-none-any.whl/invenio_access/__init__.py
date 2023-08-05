415e7cd82fdbb6c6267568a3014b139bdbd9cc80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:16 +0000

    Add package invenio-access/1.0.0a12/invenio_access-1.0.0a12-py2.py3-none-any.whl

---
+++ packages/invenio-access/invenio_access-1.0.0a12-py2.py3-none-any.whl/invenio_access/__init__.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Invenio module for common role based access control.
+
+It aims to make a process of managing access rigths quick and easy while
+preventing unwanted visitors performing restricted actions due to system
+misconfiguration.
+
+Access module in three points:
+
+- parametrized action needs
+- arbitrary combinations of rules for allowing/denying users and/or roles
+- support lazy loading of permissions at runtime
+
+Initialization
+--------------
+
+First create a Flask application:
+
+>>> from flask import Flask
+>>> app = Flask('myapp')
+>>> app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+
+You initialize Access like a normal Flask extension, however Invenio-Access is
+dependent on Invenio-DB and Invenio-Accounts so first you need to initialize
+these extensions:
+
+>>> from invenio_db import InvenioDB
+>>> from invenio_accounts import InvenioAccounts
+>>> from invenio_access  import InvenioAccess
+>>> ext_db = InvenioDB(app)
+>>> ext_accounts = InvenioAccounts(app)
+>>> ext_access = InvenioAccess(app)
+
+In order for the following examples to work, you need to work within an
+Flask application context so let's push one:
+
+>>> ctx = app.app_context()
+>>> ctx.push()
+
+Also, for the examples to work we need to create the database and tables (note,
+in this example we use an in-memory SQLite database):
+
+>>> from invenio_db import db
+>>> db.create_all()
+
+Dynamic Permissions
+-------------------
+
+Dynamic permissions represent needs associated with ``ActionNeed`` or
+``ParametrizedActionNeed`` or directly the needs which method is different from
+`'action'`. This consequently means that if the action is not attached to
+any user or role then it is **allowed** by default.
+
+The permissions are loaded from the database the first time you perform
+```needs``` or ```excludes``` in the DynamicPermission object and they are
+stored in the instance of DynamicPermission affected to be used in future
+calls.
+
+Action Needs
+~~~~~~~~~~~~
+
+You can start by creating new actions either with or without parameters:
+
+>>> from flask_principal import ActionNeed
+>>> from invenio_access import ParameterizedActionNeed
+>>> action_index = ActionNeed('index')
+>>> action_view_all = ParameterizedActionNeed('view', None)
+>>> action_view_1 = ParameterizedActionNeed('view', '1')
+
+If no role or user is attached anybody can perform the action. You can verify
+this behavior using following commands.
+
+>>> from flask_principal import AnonymousIdentity
+>>> from invenio_access import DynamicPermission, ParameterizedActionNeed
+>>> permission_index = DynamicPermission(action_index)
+>>> permission_view_all = DynamicPermission(action_view_all)
+>>> permission_view_1 = DynamicPermission(action_view_1)
+>>> anonymous_identity = AnonymousIdentity()
+>>> anonymous_identity.can(permission_index)
+True
+>>> anonymous_identity.can(permission_view_all)
+True
+>>> anonymous_identity.can(permission_view_1)
+True
+
+Assign actions
+~~~~~~~~~~~~~~
+
+You can either assign actions to user or roles. First, you need to have
+some users and roles in your database.
+
+>>> from invenio_db import db
+>>> from invenio_accounts.models import User, Role
+>>> admin = User(email='admin@inveniosoftware.org')
+>>> student = User(email='student@inveniosoftware.org')
+>>> db.session.begin(nested=True)
+<sqlalchemy.orm.session.SessionTransaction object ...
+>>> db.session.add(admin)
+>>> db.session.add(student)
+>>> db.session.commit()
+
+With users created you can allow and deny actions:
+
+>>> from invenio_access.models import ActionUsers
+>>> db.session.begin(nested=True)
+<sqlalchemy.orm.session.SessionTransaction object ...
+>>> db.session.add(ActionUsers.allow(action_index, user=admin))
+>>> db.session.commit()
+>>> permission_index = DynamicPermission(action_index)
+>>> anonymous_identity.can(permission_index)
+False
+>>> from flask_principal import Identity, RoleNeed, UserNeed
+>>> admin_identity = Identity(admin.id)
+>>> admin_identity.provides.add(UserNeed(admin.id))
+>>> admin_identity.can(permission_index)
+True
+
+How to discover existing actions
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Modules that intend to implement a given set of actions can register them in
+entry points in the corresponding `setup.py`, e.g.
+
+.. code-block:: python
+
+    entry_points={
+        'invenio_access.actions': [
+            'records_read_all'
+            ' = invenio_records.permissions:records_read_all',
+            'records_create_all'
+            ' = invenio_records.permissions:records_create_all',
+            'records_update_all'
+            ' = invenio_records.permissions:records_update_all',
+            'records_delete_all'
+            ' = invenio_records.permissions:records_delete_all',
+        ],
+    }
+
+In order to discover which actions are available in a given installation one
+can retrieve them via:
+
+>>> sorted(app.extensions['invenio-access'].actions.keys())
+['admin-access', 'superuser-access']
+
+One can also use CLI to discover registered actions. Here's an example:
+
+.. code-block:: console
+
+    $ cd examples
+    $ export FLASK_APP=app.py
+    $ flask access list
+    read:
+    open:
+    admin-access:
+    superuser-access:
+"""
+
+from __future__ import absolute_import, print_function
+
+from .ext import InvenioAccess
+from .permissions import DynamicPermission, ParameterizedActionNeed
+from .proxies import current_access
+from .version import __version__
+
+__all__ = (
+    '__version__',
+    'current_access',
+    'DynamicPermission',
+    'InvenioAccess',
+    'ParameterizedActionNeed',
+)
---
+++ packages/invenio-access/invenio_access-1.0.0a12-py2.py3-none-any.whl/invenio_access/models.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015, 2016, 2017 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Database models for access module."""
+
+from __future__ import absolute_import, print_function
+
+from flask_principal import RoleNeed, UserNeed
+from invenio_accounts.models import Role, User
+from invenio_db import db
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.event import listen
+
+from .proxies import current_access
+
+
+class ActionNeedMixin(object):
+    """Define common attributes for Action needs."""
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    """Primary key to support nullable fields."""
+
+    action = db.Column(db.String(80), index=True)
+    """Name of the action."""
+
+    exclude = db.Column(db.Boolean(name='exclude'), nullable=False,
+                        default=False, server_default='0')
+    """Deny associated objects."""
+
+    argument = db.Column(db.String(255), nullable=True, index=True)
+    """String value of action argument."""
+
+    @classmethod
+    def create(cls, action, **kwargs):
+        """Create new database instance from action need.
+
+        :param action: A object containing a method equal to ``'action'`` and
+            a value.
+        :param argument: The action argument. If this parameter is not passed,
+            then the ``action.argument`` will be taken into account. If the
+            ``action.argument`` does not exist, ``None`` will be set as
+            argument for the new action need.
+        :returns: A :class:`invenio_access.models.ActionNeedMixin` instance.
+        """
+        assert action.method == 'action'
+        argument = kwargs.pop('argument', None) or getattr(
+            action, 'argument', None)
+        return cls(
+            action=action.value,
+            argument=argument,
+            **kwargs
+        )
+
+    @classmethod
+    def allow(cls, action, **kwargs):
+        """Allow given action need.
+
+        :param action: The action to allow.
+        :returns: A :class:`invenio_access.models.ActionNeedMixin` instance.
+        """
+        return cls.create(action, exclude=False, **kwargs)
+
+    @classmethod
+    def deny(cls, action, **kwargs):
+        """Deny usage of given action need.
+
+        :param action: The action to deny.
+        :returns: A :class:`invenio_access.models.ActionNeedMixin` instance.
+        """
+        return cls.create(action, exclude=True, **kwargs)
+
+    @classmethod
+    def query_by_action(cls, action, argument=None):
+        """Prepare query object with filtered action.
+
+        :param action: The action to deny.
+        :param argument: The action argument. If it's ``None`` then, if exists,
+            the ``action.argument`` will be taken. In the worst case will be
+            set as ``None``. (Default: ``None``)
+        :returns: A query object.
+        """
+        query = cls.query.filter_by(action=action.value)
+        argument = argument or getattr(action, 'argument', None)
+        if argument is not None:
+            query = query.filter(db.or_(
+                cls.argument == str(argument),
+                cls.argument.is_(None),
+            ))
+        else:
+            query = query.filter(cls.argument.is_(None))
+        return query
+
+    @property
+    def need(self):
+        """Abstract need."""
+        raise NotImplemented()  # pragma: no cover
+
+
+class ActionUsers(ActionNeedMixin, db.Model):
+    """ActionRoles data model.
+
+    It relates an allowed action with a user.
+    """
+
+    __tablename__ = 'access_actionsusers'
+
+    __table_args__ = (UniqueConstraint(
+        'action', 'exclude', 'argument', 'user_id',
+        name='access_actionsusers_unique'),
+    )
+
+    user_id = db.Column(db.Integer(), db.ForeignKey(User.id), nullable=True)
+
+    user = db.relationship("User")
+
+    @property
+    def need(self):
+        """Return UserNeed instance."""
+        return UserNeed(self.user_id)
+
+
+class ActionRoles(ActionNeedMixin, db.Model):
+    """ActionRoles data model.
+
+    It relates an allowed action with a role.
+    """
+
+    __tablename__ = 'access_actionsroles'
+
+    __table_args__ = (UniqueConstraint(
+        'action', 'exclude', 'argument', 'role_id',
+        name='access_actionsroles_unique'),
+    )
+
+    role_id = db.Column(db.Integer(), db.ForeignKey(Role.id), nullable=False)
+
+    role = db.relationship("Role")
+
+    @property
+    def need(self):
+        """Return RoleNeed instance."""
+        return RoleNeed(self.role.name)
+
+
+def removed_or_inserted_action(mapper, connection, target):
+    """Remove the action from cache when an item is inserted or deleted."""
+    current_access.delete_action_cache(target.action)
+
+
+def changed_action_action(target, value, oldvalue, initiator):
+    """Remove the action from cache when an action is updated.
+
+    Remove the action from cache when ActionRoles.action or ActionUsers.action
+    is updated.
+    """
+    current_access.delete_action_cache(value)
+    current_access.delete_action_cache(oldvalue)
+
+
+def changed_owner_action(target, value, oldvalue, initiator):
+    """Remove the action from cache when the owner is updated.
+
+    Remove the action from cache when ActionRoles.role or ActionUsers.user
+    is updated.
+    """
+    current_access.delete_action_cache(target.action)
+
+
+listen(ActionUsers, 'after_insert', removed_or_inserted_action)
+listen(ActionUsers, 'after_delete', removed_or_inserted_action)
+listen(ActionUsers.action, 'set', changed_action_action)
+listen(ActionUsers.user, 'set', changed_owner_action)
+
+listen(ActionRoles, 'after_insert', removed_or_inserted_action)
+listen(ActionRoles, 'after_delete', removed_or_inserted_action)
+listen(ActionRoles.action, 'set', changed_action_action)
+listen(ActionRoles.role, 'set', changed_owner_action)
