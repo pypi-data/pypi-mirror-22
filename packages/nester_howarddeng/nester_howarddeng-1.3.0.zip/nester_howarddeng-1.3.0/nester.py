e0f5f389d2aef7d0802f2c9084d45760870d8842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:02 +0000

    Add package nester_howarddeng/1.3.0/nester_howarddeng-1.3.0.zip

---
+++ packages/nester_howarddeng/nester_howarddeng-1.3.0.zip/nester_howarddeng-1.3.0/nester.py
@@ -0,0 +1,9 @@
+"""the standard way to include mutiple line comment in your code"""
+def print_lol(mm,level=0) :
+    if isinstance(mm,list) :
+        for each_item in mm :
+            print_lol(each_item,level+1)
+    else :
+        for num in range(level) :
+            print("\t",end='')
+        print(mm)
---
+++ packages/nester_howarddeng/nester_howarddeng-1.3.0.zip/nester_howarddeng-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'nester_howarddeng',
+    version     = '1.3.0',
+    py_modules  = ['nester'],
+    author      = 'howard',
+    author_email= 'hfpython@headfirstlabs.com',
+    url         = 'http://www.headfirstlabs.com',
+    description = 'a simple printer of nested lists',
+)
