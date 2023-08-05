1e87b212376f5ea94f170fc1d027f3f8e4c96d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:22 +0000

    Add package nesterjcs/1.0.0/nesterjcs-1.0.0.zip

---
+++ packages/nesterjcs/nesterjcs-1.0.0.zip/nesterjcs-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+        name          = 'nesterjcs',
+        version       = '1.0.0',
+        py_modules    = ['nesterjcs'],
+        author        = 'csjiang',
+        author_email  = '751275263@qq.com',
+        url           = 'http://www.headfirstlabs.com',
+        description   = 'A simple printer of nested lists',
+      )
