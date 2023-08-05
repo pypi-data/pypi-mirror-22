ea8b246c2841c31a363f5dd2b40b7c3b341b46a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:20 +0000

    Add package myreco/0.41.0/myreco-0.41.0.tar.gz

---
+++ packages/myreco/myreco-0.41.0.tar.gz/myreco-0.41.0/myreco/engines/objects_exporter/model.py
@@ -0,0 +1,72 @@
+# MIT License
+
+# Copyright (c) 2016 Diogo Dutra <dutradda@gmail.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from myreco.utils import extend_swagger_json
+from myreco.engines.data_importer.model import EnginesDataImporterModelBase
+from copy import deepcopy
+import asyncio
+
+
+class EnginesObjectsExporterModelBase(EnginesDataImporterModelBase):
+    __swagger_json__ = extend_swagger_json(
+        EnginesDataImporterModelBase.__swagger_json__,
+        __file__
+    )
+
+    @classmethod
+    async def post_export_objects_job(cls, req, session):
+        session = cls._copy_session(session)
+        engine = await cls._get_engine(req, session)
+        if engine is None:
+            return cls._build_response(404)
+
+        jobs_id = cls._get_jobs_id_exporter(engine)
+        return cls._create_job(cls._run_export_objects_job, jobs_id, req, session, engine)
+
+    @classmethod
+    def _get_jobs_id_exporter(cls, engine):
+        return cls._get_jobs_id(engine) + '_exporter'
+
+    @classmethod
+    def _run_export_objects_job(cls, req, session, engine):
+        import_data = req.query.get('import_data')
+
+        if import_data:
+            importer_result = engine.strategy.get_data(session)
+            exporter_result = engine.strategy.export_objects(session)
+
+            return {
+                'importer': importer_result,
+                'exporter': exporter_result
+            }
+        else:
+            return engine.strategy.export_objects(session)
+
+    @classmethod
+    async def get_export_objects_job(cls, req, session):
+        engine = await cls._get_engine(req, session)
+        if engine is None:
+            return cls._build_response(404)
+
+        jobs_id = cls._get_jobs_id_exporter(engine)
+        return await cls._get_job(jobs_id, req, session)
---
+++ packages/myreco/myreco-0.41.0.tar.gz/myreco-0.41.0/myreco/engines/strategies/objects_exporter.py
@@ -0,0 +1,63 @@
+# MIT License
+
+# Copyright (c) 2016 Diogo Dutra <dutradda@gmail.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from myreco.engines.strategies.base import EngineCoreBase
+from myreco.exceptions import EngineError
+from abc import abstractmethod
+from glob import glob
+from aiofiles import gzip_open
+import os.path
+import asyncio
+
+
+class EngineCoreObjectsExporter(EngineCoreBase):
+
+    @abstractmethod
+    def export_objects(self, session):
+        pass
+
+    async def _build_csv_readers(self, pattern=''):
+        readers = []
+        pattern = os.path.join(self._data_path, '{}*.gz'.format(pattern))
+
+        for filename in glob(pattern):
+            file_ = await gzip_open(filename, 'rt')
+            readers.append(file_)
+
+        return readers
+
+    async def _get_items_indices_map_dict(self, session):
+        items_indices_map_dict = await self._items_indices_map.get_all(session)
+
+        if not items_indices_map_dict.values():
+            raise EngineError(
+                "The Indices Map for '{}' is empty. Please update these items"
+                .format(self.engine['item_type']['name']))
+
+        return items_indices_map_dict
+
+    def _run_coro(self, coro, loop):
+        if loop.is_running():
+            return asyncio.run_coroutine_threadsafe(coro, loop).result()
+        else:
+            return loop.run_until_complete(coro)
