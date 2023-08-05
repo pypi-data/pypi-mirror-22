cea9ccc0d2aef891782efa23b30a8eb7d40d2116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:01 +0000

    Add package aws-lambda-logging/0.0.19/aws_lambda_logging-0.0.19.tar.gz

---
+++ packages/aws-lambda-logging/aws_lambda_logging-0.0.19.tar.gz/aws_lambda_logging-0.0.19/aws_lambda_logging.py
@@ -0,0 +1,53 @@
+import json
+import logging
+
+
+class JsonFormatter(logging.Formatter):
+
+    def __init__(self, **kwargs):
+        super(JsonFormatter, self).__init__()
+        self.format_dict = {
+            'timestamp': '%(asctime)s',
+            'level': '%(levelname)s',
+            'location': '%(name)s.%(funcName)s:%(lineno)d',
+            'message': '%(message)s',
+        }
+        self.format_dict.update(kwargs)
+
+    def format(self, record):
+        record_dict = record.__dict__.copy()
+        record_dict['asctime'] = self.formatTime(record)
+        record_dict['message'] = record.getMessage()
+        log_dict = {k: v % record_dict
+                    for k, v in self.format_dict.items()
+                    if v
+                    }
+
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            # from logging.Formatter:format
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+
+        if record.exc_text:
+            log_dict['exception'] = record.exc_text
+
+        json_record = json.dumps(log_dict)
+
+        if hasattr(json_record, 'decode'):  # pragma: no cover
+            json_record = json_record.decode('utf-8')
+
+        return json_record
+
+
+def setup(level='DEBUG', formatter_cls=JsonFormatter, **kwargs):
+    if formatter_cls:
+        for handler in logging.root.handlers:
+            handler.setFormatter(formatter_cls(**kwargs))
+
+    try:
+        logging.root.setLevel(level)
+    except ValueError:
+        logging.root.setLevel('INFO')
+        logging.root.error('Invalid log level: %s', level)
