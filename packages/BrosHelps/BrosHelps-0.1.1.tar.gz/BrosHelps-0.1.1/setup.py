076054cfadb4751e38e58a3e684a861ea4649d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package BrosHelps/0.1.1/BrosHelps-0.1.1.tar.gz

---
+++ packages/BrosHelps/BrosHelps-0.1.1.tar.gz/BrosHelps-0.1.1/setup.py
@@ -0,0 +1,27 @@
+#python3 setup.py register
+#python3 setup.py sdist bdist_wheel --universal upload
+# use above in same directory of setup.py to upload to PYPI
+from setuptools import setup, find_packages
+
+setup(
+    name='BrosHelps',
+    version='0.1.1',
+    # name='BrosHelp',
+    # version='0.1.12',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    platforms = 'any',
+    include_package_data = True,
+    description='BrosHelp Bot',
+    url='https://github.com/KayShenA/PythonProjects',
+    author='Kay',
+    author_email='skyluckyrrb@126.com',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'BrosHelp = BrosHelp:start'
+        ]
+    },
+    install_requires=[
+        # 'wxpy',
+    ],)
