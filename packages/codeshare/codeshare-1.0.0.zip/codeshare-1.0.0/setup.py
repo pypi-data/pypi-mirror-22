ec6368253d0f56215d09f0e3be5acb8d29d433c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:56 +0000

    Add package codeshare/1.0.0/codeshare-1.0.0.zip

---
+++ packages/codeshare/codeshare-1.0.0.zip/codeshare-1.0.0/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'codeshare',
+    version = '1.0.0',
+    py_modules = ['codeshare'],
+    author = 'han',
+    author_email = 'khg1003@gmail.com',
+    url = 'http://www.sonnonet.com',
+    description = 'A simple printer of nested lists',
+)
