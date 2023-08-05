e2e2a9f1bd3c3961d18abacf1de80a87fbd3cf34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:31 +0000

    Add package LoginRadius/2.8.1/LoginRadius-2.8.1.zip

---
+++ packages/LoginRadius/LoginRadius-2.8.1.zip/LoginRadius-2.8.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+    name = 'LoginRadius',
+    py_modules = ['LoginRadius','UserRegistration'],
+    version = '2.8.1',
+    description = 'Social Login and User Registration for Python.',
+    author='LoginRadius',
+    author_email='developers@loginradius.com',
+    url='http://loginradius.com/',
+    classifiers=['Programming Language :: Python', 'Programming Language :: Python :: 2.7','Programming Language :: Python :: 3.6',
+                 'Operating System :: OS Independent', 'License :: OSI Approved :: MIT License',
+                 'Development Status :: 5 - Production/Stable', 'Intended Audience :: Developers',
+                 'Topic :: Internet', 'Topic :: Internet :: WWW/HTTP','Topic :: Software Development :: Libraries :: Python Modules'
+                 ],
+    keywords=['sociallogin', 'userregistration'],
+)
