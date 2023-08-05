4e50754f4b5d2c7cabe3b5bc69bd988dae3d1101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:04 +0000

    Add package rutextcorrector/0.0.1/rutextcorrector-0.0.1.zip

---
+++ packages/rutextcorrector/rutextcorrector-0.0.1.zip/rutextcorrector-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='rutextcorrector',
+    version='0.0.1',
+    packages=['', 'test', 'rutextcorrector'],
+    url='',
+    license='MIT',
+    author='Kamo Petrosyan',
+    author_email='kamo@haikson.com',
+    description=''
+)
---
+++ packages/rutextcorrector/rutextcorrector-0.0.1.zip/rutextcorrector-0.0.1/test/main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from rutextcorrector.en_layout import en_to_ru_layout
+
+
+if __name__ == '__main__':
+    print(en_to_ru_layout('Ghbdtn? Vfif!'))
