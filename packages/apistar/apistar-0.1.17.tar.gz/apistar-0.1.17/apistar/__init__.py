4eb7fbd5a83e4a068a3f51a29f6895ed5ad6f873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:05 +0000

    Add package apistar/0.1.17/apistar-0.1.17.tar.gz

---
+++ packages/apistar/apistar-0.1.17.tar.gz/apistar-0.1.17/apistar/__init__.py
@@ -0,0 +1,16 @@
+"""
+              _    ____ ___   ____  _
+ __/\__      / \  |  _ \_ _| / ___|| |_ __ _ _ __    __/\__
+ \    /     / _ \ | |_) | |  \___ \| __/ _` | '__|   \    /
+ /_  _\    / ___ \|  __/| |   ___) | || (_| | |      /_  _\
+   \/     /_/   \_\_|  |___| |____/ \__\__,_|_|        \/
+"""
+from apistar.app import App
+from apistar.http import Request, Response
+from apistar.routing import Include, Route
+from apistar.test import TestClient
+
+__version__ = '0.1.17'
+__all__ = [
+    'App', 'Include', 'Route', 'Request', 'Response', 'TestClient'
+]
