22eef44e5439ad5864a06c0e51c0a275db0fa0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:52 +0000

    Add package instagramPictures/1.0/instagramPictures-1.0.tar.gz

---
+++ packages/instagramPictures/instagramPictures-1.0.tar.gz/instagramPictures-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='instagramPictures',
+	packages = ['instagramPictures'],
+	version='1.0',
+	description = 'Script for downloading pictures from instagram.com',
+	author = 'Vadym Prokopets',
+	author_email = 'vadym_prokopec@mail.ru',
+	url = 'https://github.com/fiterV/instagramPictures', # use the URL to the github repo
+	keywords = ['instagram', 'pictures', 'download'], # arbitrary keywords
+	classifiers = [],
+)
