256b379d32b41cf39d1e2a42175ad892b03cdbba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:02 +0000

    Add package DOMdiff/1.0.2/DOMdiff-1.0.2.tar.gz

---
+++ packages/DOMdiff/DOMdiff-1.0.2.tar.gz/DOMdiff-1.0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='DOMdiff',
+      version='1.0.2',
+      description=('Tool that, given two HTML pages, returns sub-trees of the'
+                   'DOM that got removed and added, respectively.'),
+      author='specialchar',
+      author_email='specialchar@riseup.net',
+      license='GPLv3',
+      url='https://github.com/OpenMediaMonitor/domdiff',
+      packages=['domdiff'],
+      install_requires=[
+          'beautifulsoup4>4.5,<5',
+      ],
+      tests_requires=[
+          'pytest>3,<4',
+      ]
+     )
