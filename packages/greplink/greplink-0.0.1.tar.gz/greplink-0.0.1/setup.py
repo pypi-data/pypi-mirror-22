b5eb49ac728ba393b79838ca4e0e1c604653def2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:38 +0000

    Add package greplink/0.0.1/greplink-0.0.1.tar.gz

---
+++ packages/greplink/greplink-0.0.1.tar.gz/greplink-0.0.1/setup.py
@@ -0,0 +1,18 @@
+# flake8: noqa
+from distutils.core import setup
+
+setup(
+    name = "greplink",
+    packages = ["greplink"],
+    version = "0.0.1",
+    description = "web scraper based on asyncio in pure Python3",
+    author = "Darshan Chaudhary",
+    author_email = "deathbullet@gmail.com",
+    url = "https://github.com/greplink/greplink",
+    download_url = "https://github.com/greplink/greplink/archive/0.0.1.tar.gz",
+    keywords = ["scraping", "asyncio", "python3", "scrapy"],
+    license = "LICENSE.txt",
+    classifiers = [],
+    install_requires=[
+    ]
+)
