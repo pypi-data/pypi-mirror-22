e8195e32eddc3543f6ce9921b10a740d3eb83634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:17 +0000

    Add package bespin/0.7.0.6/bespin-0.7.0.6.tar.gz

---
+++ packages/bespin/bespin-0.7.0.6.tar.gz/bespin-0.7.0.6/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+from bespin import VERSION
+
+setup(
+      name = "bespin"
+    , version = VERSION
+    , packages = ['bespin'] + ['bespin.%s' % pkg for pkg in find_packages('bespin')]
+    , include_package_data = True
+
+    , install_requires =
+      [ "delfick_app==0.9.4"
+      , "option_merge==1.5"
+      , "input_algorithms==0.5.9"
+
+      , "six"
+      , "pytz"
+      , "slacker"
+      , "humanize"
+      , "argparse"
+      , "requests"
+      , "paramiko==1.16.0"
+
+      , "radssh==1.0.5"
+      , "pyrelic==0.8.0"
+      , "boto==2.40.0"
+      , "boto3"
+      , "pyYaml==3.10"
+      , "ultra_rest_client==0.1.4"
+      , "FileChunkIO==1.6"
+      , "dnslib==0.9.4"
+      ]
+
+    , extras_require =
+      { "tests":
+        [ "noseOfYeti>=1.5.0"
+        , "nose"
+        , "mock"
+        , "moto"
+        , "coverage"
+
+        # Need to ensure httpretty is not 0.8.7
+        # To prevent an infinite loop in python3 tests
+        , "httpretty==0.8.6"
+        ]
+      }
+
+    , entry_points =
+      { 'console_scripts' :
+        [ 'bespin = bespin.executor:main'
+        ]
+      }
+
+    # metadata for upload to PyPI
+    , url = "https://github.com/delfick/bespin"
+    , author = "Stephen Moore"
+    , author_email = "delfick755@gmail.com"
+    , description = "Opinionated wrapper around boto that reads yaml"
+    , license = "MIT"
+    , keywords = "cloudformation boto"
+    )
+
