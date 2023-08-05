f271ae85280ea990030c238edfea18477a19afec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:33 +0000

    Add package guillotina-elasticsearch/1.0.12/guillotina_elasticsearch-1.0.12-py3-none-any.whl

---
+++ packages/guillotina-elasticsearch/guillotina_elasticsearch-1.0.12-py3-none-any.whl/guillotina_elasticsearch/api.py
@@ -0,0 +1,28 @@
+from guillotina import configure
+from guillotina.component import getUtility
+from guillotina.interfaces import ICatalogUtility
+from guillotina.interfaces import IContainer
+from guillotina.component import queryUtility
+from guillotina_elasticsearch.manager import get_mappings
+
+
+@configure.service(
+    context=IContainer, name='@update_mapping', method='POST',
+    permission='guillotina.ManageCatalog')
+async def update_mapping(context, request):
+    util = getUtility(ICatalogUtility)
+    await util.migrate_index(context)
+
+
+@configure.service(
+    context=IContainer, name='@force_mapping', method='POST',
+    permission='guillotina.ManageCatalog')
+async def update_mapping(context, request):
+    catalog = queryUtility(ICatalogUtility)
+    index_name = await catalog.get_index_name(request.container)
+    mappings = get_mappings()
+    for key, value in mappings.items():
+        await catalog.conn.indices.put_mapping(index_name, key, value)
+    return {
+        'status': 'ok'
+    }
