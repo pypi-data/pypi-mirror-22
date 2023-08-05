fa5ae292174ae0662d5658a8bdbc827df412e143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:25 +0000

    Add package sharemux/0.2.3/sharemux-0.2.3.tar.gz

---
+++ packages/sharemux/sharemux-0.2.3.tar.gz/sharemux-0.2.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+version = '0.2.3'
+
+setup(
+    name='sharemux',
+    version=version,
+    description='Share your tmux session via a hosted web page',
+    long_description='Share your tmux session via a hosted web page',
+    url='https://github.com/doylezdead/sharemux',
+    download_url='https://github.com/doylezdead/sharemux/tarball/{}'.format(version),
+    author='Ryan Doyle',
+    author_email='rcdoyle@mtu.edu',
+    license='MIT',
+    packages=['sharemux'],
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='tmux share host broadcast',
+    entry_points={
+        'console_scripts': [
+            'sharemux = sharemux:cli'
+        ]
+    },
+    install_requires=[
+        'click',
+        'pexpect',
+        'Flask'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
