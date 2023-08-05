b62c45f0420ccef8ff0e1d274f50c6cfc2ffe844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:06 +0000

    Add package WaveGliDA/0.1.0/WaveGliDA-0.1.0.tar.gz

---
+++ packages/WaveGliDA/WaveGliDA-0.1.0.tar.gz/WaveGliDA-0.1.0/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="WaveGliDA",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="Luke Gregor",
+    author_email="luke.e.gregor@gmail.com",
+
+    # Packages
+    packages=["WaveGliDA/", 'WaveGliDA/static', 'WaveGliDA/templates', 'WaveGliDA/data'],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # this creates a command line thingy
+    entry_points={'console_scripts': ['WaveGliDA = WaveGliDA.__main__:main']},
+
+    # Details
+    url="http://luke-gregor.github.io",
+
+    #
+    # license="LICENSE.txt",
+    description="Useful towel-related stuff.",
+
+    long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "click",
+        "Flask",
+        "itsdangerous",
+        "Jinja2",
+        "MarkupSafe",
+        "numpy",
+        "pandas",
+        "PyExcelerate",
+        "Pygments",
+        "python-dateutil",
+        "pytz",
+        "seawater",
+        "six",
+        "Werkzeug",
+    ],
+)
