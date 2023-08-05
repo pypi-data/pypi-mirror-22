66d246b04ce2419cd7cfaf94abd12a4d0211ffc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:10 +0000

    Add package projectowl/0.3.1/projectowl-0.3.1.tar.gz

---
+++ packages/projectowl/projectowl-0.3.1.tar.gz/projectowl-0.3.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="projectowl",
+    version="0.3.1",
+    description="high level computer vision library",
+    keywords="computer vision image feature pipeline",
+    url="https://github.com/flyfj/owl",
+    author="Jie Feng",
+    author_email="jiefengdev@gmail.com",
+    license="XXX",
+    packages=find_packages(),
+    install_requires=["", ],
+    include_package_data=True,
+    zip_safe=False)
