bc4b9c94018e3d5b7561539047bbf125c4e37849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:23 +0000

    Add package django-background-tasks/1.1.10/django-background-tasks-1.1.10.tar.gz

---
+++ packages/django-background-tasks/django-background-tasks-1.1.10.tar.gz/django-background-tasks-1.1.10/background_task/apps.py
@@ -0,0 +1,13 @@
+try:
+    from django.apps import AppConfig
+except ImportError:
+    pass  # Django < 1.7
+else:
+
+    class BackgroundTasksAppConfig(AppConfig):
+        name = 'background_task'
+        from background_task import __version__ as version_info
+        verbose_name = 'Background Tasks ({})'.format(version_info)
+
+        def ready(self):
+            import background_task.signals  # noqa
