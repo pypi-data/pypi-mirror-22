a4468181a2078f00552155f52b3f79c9137bcde5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:56 +0000

    Add package anoa/0.1a0/anoa-0.1a0.tar.gz

---
+++ packages/anoa/anoa-0.1a0.tar.gz/anoa-0.1a0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='anoa',
+    version='0.1a',
+    description='A differentiable programming for scientists',
+    url='https://github.com/mfkasim91/anoa',
+    author='mfkasim91',
+    author_email='firman.kasim@gmail.com',
+    license='MIT',
+    packages=['anoa'],
+    # packages=find_packages(exclude=["test", "example"]),
+    install_requires=[
+        "numpy>=1.12",
+        "scipy>=0.15"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "License :: OSI Approved :: MIT License",
+        
+        "Programming Language :: Python :: 2.7"
+    ],
+    keywords="autodiff compressed-sensing differentiable-programming optimization",
+    zip_safe=False)
