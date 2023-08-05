c72502685e0883d74495e68228e681a630f041a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:48 +0000

    Add package aws_credentials_replacer/0.1.3/aws_credentials_replacer-0.1.3.tar.gz

---
+++ packages/aws_credentials_replacer/aws_credentials_replacer-0.1.3.tar.gz/aws_credentials_replacer-0.1.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+required = [
+    'credstash',
+    'jinja2',
+    'click'
+]
+
+setup(
+    name='aws_credentials_replacer',
+    version='0.1.3',
+    description='AWS credentials replacer',
+    author='Anton Prokhorov',
+    author_email='betrayer11@gmail.com',
+    url='https://github.com/singleton11/aws-credential-replacer',
+    packages=['credentials_replacer'],
+    requires=required,
+    install_requires=required,
+)
