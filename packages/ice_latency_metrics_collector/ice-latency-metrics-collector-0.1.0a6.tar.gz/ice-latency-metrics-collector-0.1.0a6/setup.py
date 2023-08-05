53824a9438b6b70cd053cf89b7369b082b985b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:31 +0000

    Add package ice_latency_metrics_collector/0.1.0a6/ice-latency-metrics-collector-0.1.0a6.tar.gz

---
+++ packages/ice_latency_metrics_collector/ice-latency-metrics-collector-0.1.0a6.tar.gz/ice-latency-metrics-collector-0.1.0a6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ice-latency-metrics-collector',
+    version='0.1.0-alpha.6',
+    description='ZerocIce latency metrics collector',
+    author='3g0r',
+    author_email='eg0r.n1k0l43v@gmail.com',
+    url='https://github.com/3g0r/ice-latency-metrics-collector',
+    keywords=['ice', 'latency', 'statsd', 'zeroc'],
+    classifiers=[],
+    long_description=open('README.rst').read(),
+    license='MIT',
+    install_requires=[
+        'statsd==3.2.1',
+    ],
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+)
