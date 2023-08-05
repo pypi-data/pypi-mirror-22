165a5356bf110db097824da996c8eef2701b436c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:37 +0000

    Add package sco-datastore/0.5.1/sco-datastore-0.5.1.tar.gz

---
+++ packages/sco-datastore/sco-datastore-0.5.1.tar.gz/sco-datastore-0.5.1/setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='sco-datastore',
+    version='0.5.1',
+    description='Library to manipulate objects in the Standard Cortical Observer Data Store',
+    keywords='neuroscience vision cortex ',
+    author='Heiko Mueller',
+    author_email='heiko.muller@gmail.com',
+    url='https://github.com/heikomuller/sco-datastore',
+    license='GPLv3',
+    packages=['scodata'],
+    package_data={'': ['LICENSE']},
+    install_requires=['pymongo']
+)
