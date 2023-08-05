4cc0cf49b7b85487577024b4ebfb33067ccbb862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:21 +0000

    Add package shiftboiler/0.1.1/shiftboiler-0.1.1-py3-none-any.whl

---
+++ packages/shiftboiler/shiftboiler-0.1.1-py3-none-any.whl/boiler/boiler_template/config/config.dist.py
@@ -0,0 +1,23 @@
+from project.backend import config
+
+class DefaultConfig(config.DefaultConfig):
+    """ Local development config """
+
+    # set this for offline mode
+    SERVER_NAME = None
+    SECRET_KEY = None
+
+
+class DevConfig(config.DevConfig, DefaultConfig):
+    """ Local development config """
+    pass
+
+
+class TestingConfig(config.TestingConfig, DefaultConfig):
+    """ Local testing config """
+
+
+
+
+
+
