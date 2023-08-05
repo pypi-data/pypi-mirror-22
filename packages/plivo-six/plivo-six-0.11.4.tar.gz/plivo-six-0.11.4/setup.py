63d145857c38cd3ec9e511eb15064a577657a3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:59 +0000

    Add package plivo-six/0.11.4/plivo-six-0.11.4.tar.gz

---
+++ packages/plivo-six/plivo-six-0.11.4.tar.gz/plivo-six-0.11.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import sys
+
+requires = ['requests>=0.10.8', 'six']
+if sys.version_info < (2, 6):
+    requires.append('simplejson')
+
+setup(
+    name = "plivo-six",
+    py_modules = ['plivo', "plivoxml"],
+    version = "0.11.4",
+    description = "Plivo Python library",
+    author = "Plivo Team",
+    author_email = "hello@plivo.com",
+    url = "https://github.com/phuong/plivo-python",
+    keywords = ["plivo", "rest"],
+    install_requires = requires,
+    classifiers = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Communications :: Telephony"
+        ],
+    long_description = """\
+        Plivo Python library
+         """ )
