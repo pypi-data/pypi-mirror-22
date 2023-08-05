262311a315c4f11cdd3ff9c4ff8651383e2b280c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:58 +0000

    Add package skillful/0.8.2/skillful-0.8.2.tar.gz

---
+++ packages/skillful/skillful-0.8.2.tar.gz/skillful-0.8.2/setup.py
@@ -0,0 +1,42 @@
+import sys
+import os
+from setuptools import setup
+
+__version__ = '0.8.2'
+__description__ = 'A Python package for building Alexa skills.'
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+try:
+    import pypandoc
+    dirname = os.path.dirname(__file__)
+    txt = pypandoc.convert(os.path.join(dirname, 'README.md'), 'rst')
+    with open(os.path.join(dirname, 'README.rst'), 'w') as f:
+        f.write(txt)
+except(IOError, ImportError):
+    pass
+
+setup(name='skillful',
+      version=__version__,
+      author='bmweiner',
+      author_email='bmweiner@users.noreply.github.com',
+      url='https://github.com/bmweiner/skillful',
+      description=__description__,
+      classifiers = [
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          ],
+      platforms=['py27', 'py35'],
+      license='MIT License',
+      packages=['skillful', 'skillful.tests'],
+      install_requires=['six',
+                        'python-dateutil',
+                        'cryptography'],
+      setup_requires=[] + pytest_runner,
+      tests_require=['pytest'])
