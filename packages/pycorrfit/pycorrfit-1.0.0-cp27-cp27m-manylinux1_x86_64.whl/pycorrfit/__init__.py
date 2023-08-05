8e2b4d3da4087add2016a9005ad8b8dff8cbac2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:36 +0000

    Add package pycorrfit/1.0.0/pycorrfit-1.0.0-cp27-cp27m-manylinux1_x86_64.whl

---
+++ packages/pycorrfit/pycorrfit-1.0.0-cp27-cp27m-manylinux1_x86_64.whl/pycorrfit/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+PyCorrFit is a tool to fit correlation curves on a logarithmic scale.
+"""
+
+from . import meta
+from . import models
+from . import openfile
+from . import readfiles
+
+from .correlation import Correlation
+from .fit import Fit
+from .trace import Trace
+
+__version__ = meta.get_version()
+__author__ = u"Paul Müller"
+__license__ = "GPL v2"
+
