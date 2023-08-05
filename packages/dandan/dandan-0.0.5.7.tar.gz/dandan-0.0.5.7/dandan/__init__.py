8a1c6a73736a3211ec3126019bd3232e57da97ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:02 +0000

    Add package dandan/0.0.5.7/dandan-0.0.5.7.tar.gz

---
+++ packages/dandan/dandan-0.0.5.7.tar.gz/dandan-0.0.5.7/dandan/__init__.py
@@ -0,0 +1,24 @@
+import error
+import query
+import system
+import traffic
+import value
+import utils
+
+__all__ = [
+    "error",
+    "query",
+    "system",
+    "traffic",
+    "value",
+    "utils",
+]
+
+__version__ = ".".join(
+    [str(var) for var in
+        [
+            0,
+            0,
+            5,
+            7, ]
+     ])
