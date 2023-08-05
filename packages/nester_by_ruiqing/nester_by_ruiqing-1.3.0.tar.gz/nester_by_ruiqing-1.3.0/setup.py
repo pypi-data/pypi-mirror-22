2be65286aee73c165f9970b7e4883ddb8f3dbfdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package nester_by_ruiqing/1.3.0/nester_by_ruiqing-1.3.0.tar.gz

---
+++ packages/nester_by_ruiqing/nester_by_ruiqing-1.3.0.tar.gz/nester_by_ruiqing-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester_by_ruiqing',
+        version         = '1.3.0',
+        py_modules      = ['nester'],
+        author          = 'ruiqing0706',
+        author_email    = 'ruiqing0706@163.com',
+        url             = 'http://ruiqing.me',
+        description     = 'A simple printer of nested lists',
+)
