392e154bb87d0238f4831a61d08ae0955b468953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:40 +0000

    Add package gfxlcd/0.2.0/gfxlcd-0.2.0-py2.py3-none-any.whl

---
+++ packages/gfxlcd/gfxlcd-0.2.0-py2.py3-none-any.whl/gfxlcd/drawing/pixel.py
@@ -0,0 +1,106 @@
+"""Common drawing functions"""
+import math
+
+
+class Pixel(object):
+    """Pixel class"""
+    def __init__(self, driver):
+        self.driver = driver
+        self.options['color'] = {
+            'R': 255, 'G': 255, 'B': 255
+        }
+        self.options['background_color'] = {
+            'R': 0, 'G': 0, 'B': 0,
+        }
+        self.options['threshold'] = 50
+        self.options['transparency_color'] = None
+
+    @property
+    def threshold(self):
+        """get threshold for B&W conversion"""
+        return self.options['threshold']
+
+    @threshold.setter
+    def threshold(self, threshold):
+        """set B&W threshold for conversion """
+        self.options['threshold'] = threshold
+
+    @property
+    def transparency_color(self):
+        """get transparency color"""
+        return self.options['transparency_color']
+
+    @transparency_color.setter
+    def transparency_color(self, transparency_color):
+        """set transparency color """
+        self.options['transparency_color'] = transparency_color
+
+    def draw_pixel(self, pos_x, pos_y):
+        """dummy fuction"""
+        pass
+
+    def draw_line(self, pos_x1, pos_y1, pos_x2, pos_y2):
+        """dummy fuction"""
+        pass
+
+    def draw_rect(self, pos_x1, pos_y1, pos_x2, pos_y2):
+        """draw a rectangle"""
+        self.draw_line(pos_x1, pos_y1, pos_x2, pos_y1)
+        self.draw_line(pos_x1, pos_y2, pos_x2, pos_y2)
+        self.draw_line(pos_x1, pos_y1, pos_x1, pos_y2)
+        self.draw_line(pos_x2, pos_y1, pos_x2, pos_y2)
+
+    def draw_circle(self, pos_x, pos_y, radius):
+        """draw a circle"""
+        err = 0
+        offset_x = radius
+        offset_y = 0
+        while offset_x >= offset_y:
+            self.draw_pixel(pos_x + offset_x, pos_y + offset_y)
+            self.draw_pixel(pos_x + offset_y, pos_y + offset_x)
+            self.draw_pixel(pos_x - offset_y, pos_y + offset_x)
+            self.draw_pixel(pos_x - offset_x, pos_y + offset_y)
+            self.draw_pixel(pos_x - offset_x, pos_y - offset_y)
+            self.draw_pixel(pos_x - offset_y, pos_y - offset_x)
+            self.draw_pixel(pos_x + offset_y, pos_y - offset_x)
+            self.draw_pixel(pos_x + offset_x, pos_y - offset_y)
+            if err <= 0:
+                offset_y += 1
+                err += 2*offset_y + 1
+            else:
+                offset_x -= 1
+                err -= 2*offset_x + 1
+
+    def draw_arc(self, pos_x, pos_y, radius, start, end):
+        """draw an arc"""
+        start = start * math.pi / 180
+        end = end * math.pi / 180
+
+        err = 0
+        offset_x = radius
+        offset_y = 0
+        while offset_x >= offset_y:
+            if start <= math.atan2(offset_y, offset_x) <= end:
+                self.draw_pixel(pos_x + offset_x, pos_y + offset_y)
+            if start <= math.atan2(offset_x, offset_y) <= end:
+                self.draw_pixel(pos_x + offset_y, pos_y + offset_x)
+            if start <= math.atan2(offset_x, -offset_y) <= end:
+                self.draw_pixel(pos_x - offset_y, pos_y + offset_x)
+            if start <= math.atan2(offset_y, -offset_x) <= end:
+                self.draw_pixel(pos_x - offset_x, pos_y + offset_y)
+
+            if start <= math.atan2(-offset_y, -offset_x) + 2*math.pi <= end:
+                self.draw_pixel(pos_x - offset_x, pos_y - offset_y)
+            if start <= math.atan2(-offset_x, -offset_y) + 2*math.pi <= end:
+                self.draw_pixel(pos_x - offset_y, pos_y - offset_x)
+            if start <= math.atan2(-offset_x, offset_y) + 2*math.pi <= end:
+                self.draw_pixel(pos_x + offset_y, pos_y - offset_x)
+            if start <= math.atan2(-offset_y, offset_x) + 2*math.pi <= end:
+                self.draw_pixel(pos_x + offset_x, pos_y - offset_y)
+
+            if err <= 0:
+                offset_y += 1
+                err += 2*offset_y + 1
+            else:
+                offset_x -= 1
+                err -= 2*offset_x + 1
---
+++ packages/gfxlcd/gfxlcd-0.2.0-py2.py3-none-any.whl/gfxlcd/driver/ili9325/gpio.py
@@ -0,0 +1,67 @@
+"""GPIO communication driver"""
+import time
+import RPi.GPIO  # pylint: disable=I0011,F0401
+from gfxlcd.abstract.driver import Driver
+RPi.GPIO.setmode(RPi.GPIO.BCM)
+
+
+class GPIO(Driver):
+    """GPIO communication driver"""
+    def __init__(self):
+        self.pins = {
+            'RS': 27,
+            'W': 17,
+            'DB8': 22,
+            'DB9': 23,
+            'DB10': 24,
+            'DB11': 5,
+            'DB12': 12,
+            'DB13': 16,
+            'DB14': 20,
+            'DB15': 21,
+            'RST': 25,
+        }
+        self.data_pins = [
+            'DB8', 'DB9', 'DB10', 'DB11', 'DB12', 'DB13', 'DB14', 'DB15',
+        ]
+
+    def init(self):
+        """initialize pins"""
+        for pin in self.pins:
+            RPi.GPIO.setup(self.pins[pin], RPi.GPIO.OUT)
+            RPi.GPIO.output(self.pins[pin], 0)
+
+    def reset(self):
+        """reset a display"""
+        RPi.GPIO.output(self.pins['RST'], 1)
+        time.sleep(0.005)
+        RPi.GPIO.output(self.pins['RST'], 0)
+        time.sleep(0.005)
+        RPi.GPIO.output(self.pins['RST'], 1)
+        time.sleep(0.005)
+
+    def _set_pins(self, bits):
+        """set rpi pins"""
+        for pin in self.data_pins:
+            value = bits & 0x01
+            RPi.GPIO.output(self.pins[pin], value)
+            bits >>= 1
+
+    def cmd(self, data, enable):
+        """send command to display"""
+        RPi.GPIO.output(self.pins['RS'], 0)
+        self.send(data, enable)
+
+    def send(self, char, enable):
+        """send 16bit as 2*8bit"""
+        self._set_pins(char >> 8)
+        RPi.GPIO.output(self.pins['W'], 0)
+        RPi.GPIO.output(self.pins['W'], 1)
+        self._set_pins(char)
+        RPi.GPIO.output(self.pins['W'], 0)
+        RPi.GPIO.output(self.pins['W'], 1)
+
+    def data(self, data, enable):
+        """send data to display"""
+        RPi.GPIO.output(self.pins['RS'], 1)
+        self.send(data, enable)
