07e7b9a4e7f89b7a388556719d2ca113d4188653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:44 +0000

    Add package jhTAlib/20170514.0/jhTAlib-20170514.0.tar.gz

---
+++ packages/jhTAlib/jhTAlib-20170514.0.tar.gz/jhTAlib-20170514.0/jhtalib/behavioral_techniques/behavioral_techniques.py
@@ -0,0 +1,25 @@
+def FIBOPR(df, price='Close'):
+    """
+    Fibonacci Price Retracements
+    """
+
+def FIBOTR(df, price='Close'):
+    """
+    Fibonacci Time Retracements
+    """
+
+def GANNPR(df, price='Close'):
+    """
+    W. D. Gann Price Retracements
+    """
+
+def GANNTR(df, price='Close'):
+    """
+    W. D. Gann Time Retracements
+    """
+
+def LUNARPHASES(df):
+    """
+    Lunar Phases
+    """
+
