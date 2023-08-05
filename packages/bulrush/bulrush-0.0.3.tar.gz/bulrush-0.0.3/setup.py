1740029fa690346e16e2a35663f200eaa46d0df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:45 +0000

    Add package bulrush/0.0.3/bulrush-0.0.3.tar.gz

---
+++ packages/bulrush/bulrush-0.0.3.tar.gz/bulrush-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    author='Jonathan Sharpe',
+    author_email='mail@jonrshar.pe',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pelican :: Themes',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    description='Bulrush theme for Pelican',
+    install_requires=['webassets'],
+    license='License :: OSI Approved :: ISC License (ISCL)',
+    name='bulrush',
+    package_data={
+        'bulrush': [
+            'templates/*.html',
+            'static/css/*.css',
+            'static/css/*.less',
+        ]
+    },
+    packages=['bulrush'],
+    test_suite='tests',
+    url='https://github.com/textbook/bulrush',
+    version='0.0.3',
+)
