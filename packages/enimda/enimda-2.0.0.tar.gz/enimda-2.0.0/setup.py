b805055b830a7544e22584974a09b4bbba550544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package enimda/2.0.0/enimda-2.0.0.tar.gz

---
+++ packages/enimda/enimda-2.0.0.tar.gz/enimda-2.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='enimda',
+    version='2.0.0',
+    description='Entropy-based image border detection algorithm library',
+    long_description=long_description,
+    url='https://github.com/embali/enimda/',
+    author='Anton Smolin',
+    author_email='smolin.anton@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='image border algorithm whitespace enimda',
+    py_modules=['enimda'],
+    install_requires=['Pillow>=4.1.1', 'numpy>=1.12.1'],
+)
