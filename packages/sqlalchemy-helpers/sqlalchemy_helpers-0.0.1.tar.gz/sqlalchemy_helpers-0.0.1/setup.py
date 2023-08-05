da4ad3ade0be0b9206086e2b1709ad2cef9b2b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package sqlalchemy-helpers/0.0.1/sqlalchemy_helpers-0.0.1.tar.gz

---
+++ packages/sqlalchemy-helpers/sqlalchemy_helpers-0.0.1.tar.gz/sqlalchemy_helpers-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sqlalchemy_helpers',
+      version='0.0.1',
+      description='Helper functions for comfortable working with SQLAlchemy.',
+      author='Azat Ibrakov',
+      author_email='azatibrakov@gmail.com',
+      url='https://github.com/lycantropos/sqlalchemy_helpers',
+      download_url='https://github.com/lycantropos/sqlalchemy_helpers/archive/'
+                   'master.tar.gz',
+      keywords=['SQLAlchemy'],
+      install_requires=['sqlalchemy>=1.1.6'])
