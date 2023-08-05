61f019edb4853958e11bbfe40c9963b132cd9a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:48 +0000

    Add package wofry/1.0.3/wofry-1.0.3.tar.gz

---
+++ packages/wofry/wofry-1.0.3.tar.gz/wofry-1.0.3/wofry/propagator/wavefront.py
@@ -0,0 +1,12 @@
+
+class Wavefront(object):
+
+    def __init__(self):
+        super().__init__()
+
+    def get_dimension(self):
+        raise NotImplementedError("method is abstract")
+
+class WavefrontDimension:
+    ONE = "1"
+    TWO = "2"
