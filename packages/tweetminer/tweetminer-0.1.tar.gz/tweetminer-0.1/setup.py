06e47c7f6aea38bb542238c395a97918546c4cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package tweetminer/0.1/tweetminer-0.1.tar.gz

---
+++ packages/tweetminer/tweetminer-0.1.tar.gz/tweetminer-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='tweetminer',
+      version='0.1',
+      description='Mine for tweets with certain hashtag',
+      author='saif rehman',
+      author_email='saif.urrehman@alasbab.com',
+      license='MIT',
+      packages=['tweetminer'],
+      install_requires=['tweepy','MySQL-python'],
+      keywords = ['testing', 'logging', 'example','twitter','mining'],
+      zip_safe=False)
