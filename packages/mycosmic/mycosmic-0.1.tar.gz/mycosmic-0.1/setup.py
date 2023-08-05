15bb1f35ca7591c366816e2a637d99c428251729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:38 +0000

    Add package mycosmic/0.1/mycosmic-0.1.tar.gz

---
+++ packages/mycosmic/mycosmic-0.1.tar.gz/mycosmic-0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from distutils.core import setup
+setup(
+  name = 'mycosmic',
+  packages = ['mycosmic'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'The Mighty Flying Circus',
+  author_email = 'fly@circus.com',
+  url = 'https://github.com/abbageneration/mycosmic', # use the URL to the github repo
+  download_url = 'https://github.com/abbageneration/mycosmic/archive/mycosmic-0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
+
