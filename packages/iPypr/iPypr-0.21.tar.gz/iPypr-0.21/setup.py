35e5b38527bb8e2864add87bf02ab5b5364f6b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:10 +0000

    Add package iPypr/0.21/iPypr-0.21.tar.gz

---
+++ packages/iPypr/iPypr-0.21.tar.gz/iPypr-0.21/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# setup.py
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name="iPypr",
+    version="0.21",
+    author="Casstiel",
+    author_email="emo_ooppwwqq0@163.com",
+    maintainer_email="emo_ooppwwqq0@163.com",
+    description="Like PHP print_r for Python",
+    url="http://c.isme.pub/Pypr",
+    packages= find_packages(),
+)
