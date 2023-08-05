af73cc643f61363c0d73c6ac316bd593c893f5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:00 +0000

    Add package gamtools/1.1.0/gamtools-1.1.0.tar.gz

---
+++ packages/gamtools/gamtools-1.1.0.tar.gz/gamtools-1.1.0/lib/gamtools/data/scripts/mapping_stats.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo -e "Sample\tReads_sequenced\tReads_mapped\tUnique_reads_mapped\tPercent_mapped_reads"
+
+for fq in $*; 
+ do 
+    if [ ${fq: -10} == '.rmdup.bam' ]; then continue; fi;
+    sample=$(basename $fq);
+    name_no_ext=$(echo -n $fq | tr "." "\n" | sed '/^gz$/ d' | sed '$ d' | perl -pe 'chomp if eof' | tr "\n" ".");
+    nlines=$(zcat ${fq} | wc -l);
+    nreads=$(echo "${nlines} / 4" | bc);
+    nmap=$(samtools view -c ${name_no_ext}.bam);
+    if [ -z $nmap ]; then $nmap=0; fi;
+    nnodup=$(samtools view -c ${name_no_ext}.rmdup.bam);
+    if [ -z $nnodup ]; then $nnodup=0; fi;
+    pct_mapped=$(echo "scale=2; 100*$nmap/$nreads" | bc);
+    echo -e "${sample%%.*}\t${nreads}\t${nmap}\t${nnodup}\t${pct_mapped}"; 
+ done
