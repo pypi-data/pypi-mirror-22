110095611cdfae4feac04163ae6199450b284dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:25 +0000

    Add package kyber-k8s/0.5.0/kyber_k8s-0.5.0-py2-none-any.whl

---
+++ packages/kyber-k8s/kyber_k8s-0.5.0-py2-none-any.whl/kyber/context.py
@@ -0,0 +1,124 @@
+import click
+import dulwich
+import os
+import sys
+import yaml
+from functools import wraps
+
+name = None
+docker = None
+git_hash = None
+tag = None
+target = None
+dirty = None
+dirty_reason = None
+kube_ctx = None
+
+
+class ContextError(Exception):
+    pass
+
+
+class Context(object):
+    """ A Context object represents the current kyber context, derived from a combination of:
+    - the app config found in $CWD/.kyber/config
+    - the git repo in $CWD
+    - the current kubectl context
+    """
+    name = None             # current app name
+    docker = None           # docker image (repo/app)
+    port = None             # app port (not used?) XXX
+    git_hash = None         # git HEAD commit
+    tag = None              # Deployment tag (git_XXX)
+    target = None           # kubectl context
+    git_dirty = False       # is the git repo dirty? (we only deploy built images)
+    _git_status = None
+
+    def __init__(self, checks=None):
+        tasks = dict(
+            config=self.load_config,
+            git_tag=self.git_tag,
+            git_status=self.git_status,
+        )
+        if checks is None:
+            checks = ['config', 'git_tag', 'git_status']
+
+        self.cwd = os.path.abspath('.')
+        for task in checks:
+            tasks[task]()
+
+    def load_config(self):
+        cfg_name = os.path.join(self.cwd, '.kyber/config')
+        try:
+            with open(cfg_name) as app_config:
+                cfg = yaml.load(app_config.read())
+        except IOError:
+            raise ContextError("Not working within a kyber context (can't read {})".format(
+                cfg_name))
+        except yaml.ParserError:
+            raise ContextError("YAML parsing failed for '{}', unable to load context".format(
+                cfg_name))
+
+        from kyber.lib.kube import kube_api
+        kube_ctx = kube_api.config.current_context
+        if kube_ctx not in cfg:
+            raise ContextError(("No configuration found for kube context `{}` in kyber context."
+                                " Forgot to run 'kb init'?").format(kube_ctx))
+
+        self.kube_ctx = kube_api.config.contexts[kube_ctx]
+        self.kube_ctx['name'] = kube_ctx
+        self.name = cfg[kube_ctx]['name']
+        self.docker = cfg[kube_ctx]['docker']
+        self.port = cfg[kube_ctx]['port']
+
+    def git_status(self):
+        from dulwich import porcelain as git
+        try:
+            repo = git.Repo.discover()
+            status = git.status(repo=repo)
+        except dulwich.errors.NotGitRepository:
+            raise ContextError("{} is not a valid git repository".format(self.cwd))
+
+        for state in ('add', 'modify', 'delete'):
+            if status.staged[state]:
+                self.is_dirty = True
+                break
+        if status.unstaged:
+            self.is_dirty = True
+        self._git_status = status
+
+    def git_tag(self):
+        from dulwich import porcelain as git
+        head = git.Repo.discover(self.cwd).head()
+        self.git_hash = head
+        self.tag = 'git_{}'.format(head)
+
+    def export(self):
+        global name, docker, git_hash, tag, target, dirty, dirty_reason, kube_ctx
+        name = self.name
+        docker = self.docker
+        git_hash = self.git_hash
+        tag = self.tag
+        target = self.target
+        dirty = self.git_dirty
+        dirty_reason = self._git_status
+        kube_ctx = self.kube_ctx
+
+
+def required(**ctx_kwargs):
+    def wrapper(fn, *args, **kwargs):
+        @wraps(fn)
+        def inner(*args, **kwargs):
+            try:
+                ctx = Context(**ctx_kwargs)
+                ctx.export()
+            except ContextError as e:
+                click.echo("Unable to load kyber context: {}".format(e.message))
+                sys.exit(1)
+
+            return fn(*args, **kwargs)
+        return inner
+    return wrapper
+
+
+__all__ = [required, name, docker, tag, target, dirty, dirty_reason, kube_ctx, Context]
---
+++ packages/kyber-k8s/kyber_k8s-0.5.0-py2-none-any.whl/kyber/deploy.py
@@ -0,0 +1,46 @@
+""" kyber.deploy - logic and cli commands related to deploying a kyber project.
+"""
+import click
+import time
+
+from .objects import Deployment, Environment
+from .lib.kube import kube_api
+
+
+class DeploymentSpec(object):
+    def __init__(self, deployment):
+        self.spec = deployment.obj
+
+    def update_image(self, app):
+        self.spec['spec']['template']['spec']['containers'][0]['image'] = app.image
+
+    def update_metadata(self, app):
+        # labels must adhere to regex (([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])? (e.g. 'MyValue' or 'my_value'
+        # or '12345') # so we use unix timestamp integer, instead of ios8601 string which contains ':' and '.'
+        self.spec['spec']['template']['metadata']['labels']['deploy_time'] = str(int(time.time()))
+
+
+def execute(app, force=False):
+    environment = Environment(app.name)
+    deployment = environment.deployment
+    if not force and environment.app and environment.app.tag == app.tag:
+        click.echo("`{}` is already deployed, use force to trigger redeployment".format(app.tag))
+        return deployment
+
+    update = DeploymentSpec(deployment)
+    update.update_image(app)
+    update.update_metadata(app)
+    deployment.set_obj(update.spec)
+    deployment.update()
+    return deployment
+
+
+def wait_for(deployment):
+    old_generation = deployment.generation
+    for event in Deployment.objects(kube_api).filter(namespace=kube_api.config.namespace).watch():
+        depl = event.object
+        if depl.name == deployment.name:
+            click.echo(".", nl=False)
+            if depl.ready is True:
+                click.echo("\nDeployment complete, generation {} -> {}".format(old_generation, depl.generation))
+                return
