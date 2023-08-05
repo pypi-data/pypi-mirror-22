430b4890a5809ade81cab516bb02db90b866a1a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:48 +0000

    Add package tsaw/0.4.10/tsaw-0.4.10.tar.gz

---
+++ packages/tsaw/tsaw-0.4.10.tar.gz/tsaw-0.4.10/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version = open("tsaw/VERSION").read().strip()
+download_url = 'https://github.com/ylaizet/tsaw/archive/%s.tar.gz' % version
+
+
+setup(
+    name = 'tsaw',
+    packages=find_packages(),
+    version = version,
+    description = 'TSAW : Torrent Server API Wrapper.',
+    long_description=open('README.md').read(),
+    author = "Yec'han Laizet",
+    author_email = 'y.laizet@bordeaux.unicancer.fr',
+    url = 'https://github.com/ylaizet/tsaw',
+    download_url = download_url,
+    include_package_data=True,
+    license = "GPL3",
+    keywords = ['api', 'sequencer', 'Torrent server'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ]
+)
