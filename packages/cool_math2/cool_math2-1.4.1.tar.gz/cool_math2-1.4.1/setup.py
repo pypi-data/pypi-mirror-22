ad81a6678b64854c035aac8667e0f1fa7a9dabf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:11 +0000

    Add package cool_math2/1.4.1/cool_math2-1.4.1.tar.gz

---
+++ packages/cool_math2/cool_math2-1.4.1.tar.gz/cool_math2-1.4.1/setup.py
@@ -0,0 +1,17 @@
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='cool_math2',
+    packages=find_packages(exclude=['test', '*.test', '*.test.*']),
+    version='1.4.1',
+    description='Cool math',
+    author='missingdays',
+    author_email='rebovykin@gmail.com',
+    url='https://github.com/missingdays/cool_math2',
+    download_url='https://github.com/missingdays/cool_math2/archive/1.4.1.tar.gz',
+    keywords=['math'],
+    scripts=['bin/cool_math2'],
+    install_requires=['scipy']
+)
