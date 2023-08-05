addb09005e817e249a2f722790ebb94d3a464d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package basicmodem/0.7/basicmodem-0.7.tar.gz

---
+++ packages/basicmodem/basicmodem-0.7.tar.gz/basicmodem-0.7/basicmodem/__init__.py
@@ -0,0 +1,1 @@
+"""A simple modem implementation that supports caller ID."""
---
+++ packages/basicmodem/basicmodem-0.7.tar.gz/basicmodem-0.7/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='basicmodem',
+    version='0.7',
+    packages=['basicmodem'],
+    url='https://github.com/vroomfonde1/basicmodem',
+    download_url='https://github.com/vroomfonde1/basicmodem/archive/0.7.tar.gz',
+    license='MIT',
+    author='Tim Vitz',
+    author_email='tgvitz@gmail.com',
+    description='A basic modem implementation for receiving caller id.'
+)
