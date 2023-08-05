4829f0bdf84c48ccedfb796dea4b6313e54705de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:49 +0000

    Add package emrt.necd.content/2.0.22/emrt.necd.content-2.0.22-py2.7.egg

---
+++ packages/emrt.necd.content/emrt.necd.content-2.0.22-py2.7.egg/emrt/necd/content/utils.py
@@ -0,0 +1,41 @@
+import concurrent.futures
+from operator import itemgetter
+
+
+def principals_with_roles(context, rolenames):
+    def match_roles(roles):
+        return tuple(set(roles).intersection(rolenames))
+
+    def filter_entry(entry):
+        principal = itemgetter(0)
+        roles = itemgetter(1)
+        return principal(entry) if match_roles(roles(entry)) else None
+
+    principals = map(filter_entry, context.get_local_roles())
+    return tuple(filter(bool, principals))
+
+
+def find_parent_with_interface(interface, context):
+    parent = context.aq_parent
+    if interface.providedBy(parent):
+        return parent
+    return find_parent_with_interface(interface, parent)
+
+
+def concurrent_loop(workers, timeout, func, items, *args):
+    """ Run as:
+        my_concurrent = partial(utils.concurrent_loop, 32, 600.0)
+        result = my_concurrent(lambda item: ..., [item, item, ...])
+    """
+    results = []
+    tpe = concurrent.futures.ThreadPoolExecutor
+    with tpe(max_workers=workers) as executor:
+        futures = [executor.submit(func, item, *args) for item in items]
+        for idx, future in enumerate(
+                concurrent.futures.as_completed(futures, timeout=timeout)):
+            results.append(future.result())
+    return results
+
+
+def append_string(sep, base, tail):
+    return '{}{}{}'.format(base, sep, tail)
