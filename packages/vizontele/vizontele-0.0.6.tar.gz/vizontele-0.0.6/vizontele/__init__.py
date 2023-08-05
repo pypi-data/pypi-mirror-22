9b48de542a5b4c3b37f5820f9a8e0b83400bd2b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:09 +0000

    Add package vizontele/0.0.6/vizontele-0.0.6.tar.gz

---
+++ packages/vizontele/vizontele-0.0.6.tar.gz/vizontele-0.0.6/vizontele/__init__.py
@@ -0,0 +1,17 @@
+import re
+
+
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    """
+    import unicodedata
+    value = unicode(value)
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    return re.sub('[-\s]+', '-', value)
+
+
+def sort_video_links(video_links):
+    return sorted(video_links, key=lambda k: re.search(r'\d+', k['res']).group())
