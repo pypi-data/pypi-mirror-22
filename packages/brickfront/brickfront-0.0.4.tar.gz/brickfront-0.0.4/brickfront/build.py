3ef7f37dd03a37ead5b388506aee6b3fb1fa003e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:39 +0000

    Add package brickfront/0.0.4/brickfront-0.0.4.tar.gz

---
+++ packages/brickfront/brickfront-0.0.4.tar.gz/brickfront-0.0.4/brickfront/build.py
@@ -0,0 +1,126 @@
+class Build(object):
+    '''
+    A class holding the information of a LEGO set. Some attributes may be `None`. Check them before using them.
+
+    Called automatically by other functions. Do not manually call.
+
+    :ivar setID: The `int` ID of the set on Brickset.
+    :ivar number: The number of the set on Brickset, held as a `str`.
+    :ivar variant: The `int` variant of the LEGO set.
+    :ivar year: The `str` year in which the set came out.
+    :ivar theme: `str` The theme of the set.
+    :ivar themeGroup: `str` The type of the theme.
+    :ivar subtheme: `str` Any other theme that the set may belong to.
+    :ivar pieces: `int` The number of pieces in the set.
+    :ivar minifigs: `int` The number of minifigs that the set may come with.
+    :ivar imageURL: `str` The URL of the image of the set.
+    :ivar bricksetURL: `str` The link to the Brickset page of the set.
+    :ivar released: `bool` Whether or not the set has been released.
+    :ivar priceUK: `str` The UK price of the set.
+    :ivar priceUS: `str` The US price of the set.
+    :ivar priceCA: `str` The CA price of the set.
+    :ivar priceEU: `str` The EU price of the set.
+    :ivar rating: `float` The Brickset rating of the LEGO set.
+    :ivar additionalImages: List[`str`]
+    :ivar reviews: List[`brickfront.review.Review`]
+    :ivar instructions: List[`str`]
+    '''
+
+    def __init__(self, data, userHash, client):
+
+        self.raw = data
+        self.client = client
+
+        if userHash != '':
+            del data[20]
+
+        self.setID = str(data[0].text)
+        self.number = data[1].text
+        self.variant = int(data[2].text)
+        self.name = data[3].text
+        self.year = data[4].text
+        self.theme = data[5].text
+        self.themeGroup = data[6].text
+        self.subtheme = data[7].text
+        try: z = int(data[8].text)
+        except TypeError: z = None
+        self.pieces = z
+        try: z = int(data[9].text)
+        except TypeError: z = None
+        self.minifigs = z
+        self.imageURL = data[14].text
+        self.bricksetURL = data[15].text
+        self.priceUK = data[23].text
+        self.priceUS = data[24].text
+        self.priceCA = data[25].text
+        self.priceEU = data[26].text
+        try: z = float(data[29].text)
+        except TypeError: z = None
+        self.rating = z
+        try:
+            z = {
+                'true': True,
+                '0': False,
+                '1': True,
+                'false': False,
+                'none': False
+            }[str(data[16].text).lower()]
+        except KeyError:
+            z = data[16].text 
+        self.released = z
+
+        self.__additionalImages = None 
+        self.__reviews = None
+        self.__instructions = None
+
+    def __getAdditionalImages(self) -> list:
+        '''
+        The same as calling `client.getAdditionalImages(build.setID)`
+
+        :returns: A list of URL strings.
+        :rtype: List[`str`]
+        '''
+
+        self.__additionalImages = self.client.getAdditionalImages(self.setID)
+        return self.__additionalImages
+
+    @property 
+    def additionalImages(self):
+        if self.__additionalImages == None:
+            self.__additionalImages = self.__getAdditionalImages()
+        return self.__additionalImages
+
+    def __getReviews(self) -> list:
+        '''
+        The same as calling `client.getReviews(build.setID)`
+
+        :returns: A list of reviews.
+        :rtype: List[:class:`brickfront.review.Review`]
+        '''
+
+        self.__reviews = self.client.getReviews(self.setID)
+        return self.__reviews
+
+    @property
+    def reviews(self):
+        if self.__reviews == None:
+            self.__reviews = self.__getReviews()
+        return self.__reviews
+
+    def __getInstructions(self) -> list:
+        '''
+        The same as calling `client.getInstructions(build.setID)`
+
+        :returns: A list of instructions.
+        :rtype: List[`str`]
+        '''
+
+        self.__instructions = self.client.getInstructions(self.setID)
+        return self.__instructions
+
+    @property 
+    def instructions(self):
+        if self.__instructions == None:
+            self.__instructions = self.__getInstructions()
+        return self.__instructions
+
---
+++ packages/brickfront/brickfront-0.0.4.tar.gz/brickfront-0.0.4/brickfront/client.py
@@ -0,0 +1,292 @@
+from requests import get as _get
+from xml.etree import ElementTree as _ET
+from .errors import InvalidRequest as _InvalidRequest
+from .errors import InvalidKey as _InvalidKey
+from .errors import InvalidLogin as _InvalidLogin
+from .errors import InvalidSetID as _InvalidSetID
+from .build import Build as _Build
+from .review import Review as _Review
+
+
+class Client(object):
+    '''
+    A frontend authenticator for Brickset.com's API.
+    All endpoints require an API key.
+
+    :param str apiKey: The API key you got from Brickset.
+    :raises brickfront.errors.InvalidKey: If the key provided is invalid.
+    '''
+
+    __base = 'http://brickset.com/api/v2.asmx/{}?'
+
+    def __init__(self, apiKey: str):
+
+        self.__apiKey = apiKey
+        self.__userHash = ''
+
+        if not self.checkKey():
+            raise _InvalidKey('The provided key was invalid.')
+
+    def __getURL(self, method: str, arguments) -> str:
+        '''
+        Returns a HTTP request.
+        '''
+
+        # Format the base
+        x = Client.__base.format(method)
+
+        # Format the url with the arguments
+        if type(arguments) == str:
+            x = x + arguments
+        elif type(arguments) == list:
+            x = x + '&'.join(arguments)
+        elif type(arguments) == dict:
+            sendValues = []
+            for i, o in arguments.items():
+                addedValue = '{}={}'.format(i, o)
+                sendValues.append(addedValue)
+            x = x + '&'.join(sendValues)
+        else:
+            raise ValueError('Passed invalid type.')
+
+        # Return the valid call url
+        return x
+
+    def __isOkayRequest(self, request) -> bool:
+        '''
+        Returns if a request has an okay error code, otherwise raises InvalidRequest.
+        '''
+
+        # Check the status code of the returned request
+        if str(request.status_code)[0] not in ['2', '3']:
+            w = str(request.text).split('\\r')[0][2:]
+            raise _InvalidRequest(w)
+        return
+
+    def checkKey(self) -> bool:
+        '''
+        Checks that an API key is valid.
+
+        :returns: Whether the key is valid or not.
+        :rtype: `bool`
+        '''
+
+        # Get site
+        returned = _get(self.__getURL('checkKey', 'apiKey={}'.format(self.__apiKey)))
+        
+        # Make sure all is well
+        self.__isOkayRequest(returned)
+
+        # Parse XML
+        root = _ET.fromstring(returned.text)
+        
+        # Return bool
+        return root.text == 'OK'
+
+    def login(self, username: str, password: str) -> bool:
+        '''
+        Logs into Brickset as a user, returning a userhash, which can be used in other methods.
+        The userhash is stored inside the client.
+
+        :param str username: Your Brickset username.
+        :param str password: Your Brickset password.
+        :returns: A boolean value of whether or not the login request was done properly.
+        :rtype: `bool`
+        :raises brickfront.errors.InvalidLogin: If your login details are incorrect.
+        '''
+
+        # These are the values of the arguments that are to be sent to the site
+        values = {
+            'apiKey': self.__apiKey,
+            'username': username,
+            'password': password
+        }
+
+        # Get a login
+        returned = _get(self.__getURL('login', values))
+
+        # Make sure that you didn't send anything to the wrong URL
+        self.__isOkayRequest(returned)
+
+        # Check if the login is invalid
+        root = _ET.fromstring(returned.text)
+        if root.text.startswith('ERROR'):
+            raise _InvalidLogin(root.text[7:])
+
+        # Store the user hash
+        self.__userHash = root.text
+        return True
+
+    def getSets(self, **kwargs) -> list:
+        '''
+        A way to get different sets from a query.
+        All parameters are optional, but you should *probably* use some.
+
+        :param str query: The thing you're searching for.
+        :param str theme: The theme of the set.
+        :param str subtheme: The subtheme of the set.
+        :param str setNumber: The LEGO set number.
+        :param str year: The year in which the set came out.
+        :param str owned: Whether or not you own the set. Only works when logged in with :meth:`login`. Set to `1` to make true.
+        :param str wanted: Whether or not you want the set. Only works when logged in with :meth:`login`. Set to `1` to make true.
+        :param str orderBy: How you want the set ordered. Accepts 'Number', 'YearFrom', 'Pieces', 'Minifigs', 'Rating', 'UKRetailPrice', 'USRetailPrice', 'CARetailPrice', 'EURetailPrice', 'Theme', 'Subtheme', 'Name', 'Random'. Add 'DESC' to the end to sort descending, e.g. NameDESC. Case insensitive. Defaults to 'Number'.
+        :param str pageSize: How many results are on a page. Defaults to 20.
+        :param str pageNumber: The number of the page you're looking at. Defaults to 1.
+        :param str userName: The name of a user whose sets you want to search.
+        :returns: A list of LEGO sets.
+        :rtype: List[:class:`brickfront.build.Build`]
+        '''
+
+        # Generate a dictionary to post
+        values = {
+            'apiKey': self.__apiKey,
+            'userHash': self.__userHash,
+            'query': kwargs.get('query', ''),
+            'theme': kwargs.get('theme', ''),
+            'subtheme': kwargs.get('subtheme', ''),
+            'setNumber': kwargs.get('setNumber', ''),
+            'year': kwargs.get('year', ''),
+            'owned': kwargs.get('owned', ''),
+            'wanted': kwargs.get('wanted', ''),
+            'orderBy': kwargs.get('orderBy', 'Number'),
+            'pageSize': kwargs.get('pageSize', '20'),
+            'pageNumber': kwargs.get('pageNumber', '1'),
+            'userName': kwargs.get('userName', '')
+        }
+
+        # Send the GET request.
+        returned = _get(self.__getURL('getSets', values))
+        
+        # Make sure all is well
+        self.__isOkayRequest(returned)
+
+        root = _ET.fromstring(returned.text)
+        return [_Build(i, self.__userHash, self) for i in root]
+
+    def getSet(self, setID: str) -> _Build:
+        '''
+        Gets the information of one build, using its Brickset set ID.
+
+        :param str setID: The ID of the build from Brickset.
+        :returns: A single LEGO set object.
+        :rtype: :class:`brickfront.build.Build`
+        :raises brickfront.errors.InvalidSetID: If no sets exist by that ID.
+        '''
+
+        values = {
+            'apiKey': self.__apiKey,
+            'userHash': self.__userHash,
+            'setID': setID
+        }
+
+        # Send the GET request.
+        returned = _get(self.__getURL('getSet', values))
+        
+        # Make sure all is well
+        self.__isOkayRequest(returned)
+
+        root = _ET.fromstring(returned.text)
+        v = [_Build(i, self.__userHash, self) for i in root]
+        if len(v) == 0:
+            raise _InvalidSetID
+        return v[0]
+
+    def getRecentlyUpdatedSets(self, minutesAgo: int) -> list:
+        '''
+        Gets the information of recently updated sets.
+
+        :param int minutesAgo: The amount of time ago that the set was updated.
+        :returns: A list of sets that were updated within the given time.
+        :rtype: List[:class:`brickfront.build.Build`]
+        '''
+
+        values = {
+            'apiKey': self.__apiKey,
+            'minutesAgo': minutesAgo
+        }
+
+        # Send the GET request
+        returned = _get(self.__getURL('getRecentlyUpdatedSets', values))
+
+        # Make sure all is well
+        self.__isOkayRequest(returned)
+
+        root = _ET.fromstring(returned.text)
+        return [_Build(i, self.__userHash, self) for i in root]
+
+    def getAdditionalImages(self, setID: str) -> list:
+        '''
+        Gets a list of URLs containing images of the set.
+
+        :param str setID: The ID of the set you want to grab the images for.
+        :returns: A list of URL strings.
+        :rtype: List[`str`]
+        .. warning:: An empty list will be returned if there are no additional images, or if the set ID is invalid.
+        '''
+
+        values = {
+            'apiKey': self.__apiKey,
+            'setID': setID
+        }
+
+        # Send the GET request
+        returned = _get(self.__getURL('getAdditionalImages', values))
+
+        # Make sure all is well
+        self.__isOkayRequest(returned)
+
+        root = _ET.fromstring(returned.text)
+        urlList = []
+
+        for imageHolder in root:
+            urlList.append(imageHolder[-1].text)
+
+        return urlList
+
+    def getReviews(self, setID: str) -> list:
+        '''
+        Get the reviews for a set.
+
+        :param str setID: The ID of the set you want to get the reviews of.
+        :returns: A list of reviews.
+        :rtype: List[:class:`brickfront.review.Review`]
+        .. warning:: An empty list will be returned if there are no reviews, or if the set ID is invalid.
+        '''
+
+        values = {
+            'apiKey': self.__apiKey,
+            'setID': setID
+        }
+
+        # Send the GET request
+        returned = _get(self.__getURL('getReviews', values))
+
+        # Make sure all is well
+        self.__isOkayRequest(returned)
+
+        root = _ET.fromstring(returned.text)
+        return [_Review(i) for i in root]
+
+    def getInstructions(self, setID: str) -> list:
+        '''
+        Get the instructions for a set.
+
+        :param str setID: The ID for the set you want to get the instructions of.
+        :returns: A list of URLs to instructions.
+        :rtype: List[`str`]
+        .. warning:: An empty list will be returned if there are no instructions, or if the set ID is invalid.
+        '''
+
+        values = {
+            'apiKey': self.__apiKey,
+            'setID': setID
+        }
+
+        # Send the GET request
+        returned = _get(self.__getURL('getInstructions', values))
+
+        # Make sure all is well
+        self.__isOkayRequest(returned)
+
+        root = _ET.fromstring(returned.text)
+        return [i[0].text for i in [o for o in root]]
