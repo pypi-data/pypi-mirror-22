1dac3dca7d5faa98e1c5685fc5fbc4f931ae5296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:31 +0000

    Add package simple_ws/0.2.2/simple_ws-0.2.2.zip

---
+++ packages/simple_ws/simple_ws-0.2.2.zip/simple_ws-0.2.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+try:
+   import pypandoc
+   description = pypandoc.convert('README.md', 'rst')
+   description = description.replace("\r","")
+except (IOError, ImportError):
+
+   description = 'Simple websocket implementation in python using asyncio'
+
+setup(
+  name = 'simple_ws',
+  packages = ['simple_ws'], 
+  version = '0.2.2',
+  description='Simple websocket implementation in python using asyncio',
+  long_description = description,
+  author = 'Ole Kristian Aune, Even Dalen, Audun Wigum Arbo',
+  author_email = 'even.dalen@live.no',
+  url = 'https://github.com/WSnettverksprog/simple_ws', 
+  download_url = 'https://github.com/WSnettverksprog/simple_ws/archive/0.1.tar.gz', 
+  keywords = ['websocket', 'ws', 'asyncio', 'simple'], 
+)
