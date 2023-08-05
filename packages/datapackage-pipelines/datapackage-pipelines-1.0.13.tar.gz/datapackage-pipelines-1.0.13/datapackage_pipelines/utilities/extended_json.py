6e514261456a67377490cc1952a0e0ae495b9093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:26 +0000

    Add package datapackage-pipelines/1.0.13/datapackage-pipelines-1.0.13.tar.gz

---
+++ packages/datapackage-pipelines/datapackage-pipelines-1.0.13.tar.gz/datapackage-pipelines-1.0.13/datapackage_pipelines/utilities/extended_json.py
@@ -0,0 +1,89 @@
+import datetime
+import json as _json
+
+import decimal
+
+
+class CommonJSONDecoder(_json.JSONDecoder):
+    """
+    Common JSON Encoder
+    json.loads(myString, cls=CommonJSONEncoder)
+    """
+
+    @classmethod
+    def object_hook(cls, obj):
+        if 'type{decimal}' in obj:
+            try:
+                return decimal.Decimal(obj['type{decimal}'])
+            except decimal.InvalidOperation:
+                pass
+        if 'type{date}' in obj:
+            try:
+                return datetime.datetime \
+                    .strptime(obj["type{date}"], '%Y-%m-%d') \
+                    .date()
+            except ValueError:
+                pass
+        if 'type{datetime}' in obj:
+            try:
+                return datetime.datetime \
+                    .strptime(obj["type{datetime}"], '%Y-%m-%d %H:%M:%S')
+            except ValueError:
+                pass
+        if 'type{set}' in obj:
+            try:
+                return set(obj['type{set}'])
+            except ValueError:
+                pass
+
+        return obj
+
+    def __init__(self, **kwargs):
+        kwargs['object_hook'] = self.object_hook
+        super(CommonJSONDecoder, self).__init__(**kwargs)
+
+
+class CommonJSONEncoder(_json.JSONEncoder):
+    """
+    Common JSON Encoder
+    json.dumps(myString, cls=CommonJSONEncoder)
+    """
+
+    def default(self, obj):
+
+        if isinstance(obj, decimal.Decimal):
+            return {'type{decimal}': str(obj)}
+        elif isinstance(obj, datetime.datetime):
+            return {'type{datetime}': str(obj)}
+        elif isinstance(obj, datetime.date):
+            return {'type{date}': str(obj)}
+        elif isinstance(obj, set):
+            return {'type{set}': list(obj)}
+
+
+def _dumps(*args, **kwargs):
+    kwargs['cls'] = CommonJSONEncoder
+    return _json.dumps(*args, **kwargs)
+
+
+def _loads(*args, **kwargs):
+    kwargs['cls'] = CommonJSONDecoder
+    return _json.loads(*args, **kwargs)
+
+
+def _dump(*args, **kwargs):
+    kwargs['cls'] = CommonJSONEncoder
+    return _json.dump(*args, **kwargs)
+
+
+def _load(*args, **kwargs):
+    kwargs['cls'] = CommonJSONDecoder
+    return _json.load(*args, **kwargs)
+
+
+class json(object):
+    dumps = _dumps
+    loads = _loads
+    dump = _dump
+    load = _load
+    JSONDecodeError = _json.JSONDecodeError
