634be1b0d93ee8e318f7af5d82f1293b0834b340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:36 +0000

    Add package guillotina/1.0.0a18/guillotina-1.0.0a18.tar.gz

---
+++ packages/guillotina/guillotina-1.0.0a18.tar.gz/guillotina-1.0.0a18/guillotina/factory/app.py
@@ -0,0 +1,238 @@
+from aiohttp import web
+from guillotina import app_settings
+from guillotina import configure
+from guillotina import interfaces
+from guillotina import languages
+from guillotina.async import IAsyncUtility
+from guillotina.component import getAllUtilitiesRegisteredFor
+from guillotina.component import getUtility
+from guillotina.component import provideUtility
+from guillotina.configure.config import ConfigurationMachine
+from guillotina.content import load_cached_schema
+from guillotina.content import StaticDirectory
+from guillotina.content import StaticFile
+from guillotina.contentnegotiation import ContentNegotiatorUtility
+from guillotina.factory.content import ApplicationRoot
+from guillotina.interfaces import IApplication
+from guillotina.interfaces import IDatabase
+from guillotina.interfaces import IDatabaseConfigurationFactory
+from guillotina.interfaces.content import IContentNegotiation
+from guillotina.traversal import TraversalRouter
+from guillotina.utils import resolve_dotted_name
+
+import asyncio
+import collections
+import inspect
+import json
+import logging.config
+import os
+import pathlib
+
+
+try:
+    from Crypto.PublicKey import RSA
+except ImportError:
+    RSA = None
+
+
+def update_app_settings(settings):
+    for key, value in settings.items():
+        if isinstance(app_settings.get(key), dict):
+            app_settings[key].update(value)
+        else:
+            app_settings[key] = value
+
+
+def load_application(module, root, settings):
+    app = root.app
+    # includeme function
+    if hasattr(module, 'includeme'):
+        args = [root]
+        if len(inspect.getargspec(module.includeme).args) == 2:
+            args.append(settings)
+        module.includeme(*args)
+    # app_settings
+    if hasattr(module, 'app_settings') and app_settings != module.app_settings:
+        update_app_settings(module.app_settings)
+    # services
+    configure.load_all_configurations(app.config, module.__name__)
+
+
+# XXX use this to delay imports for these settings
+_delayed_default_settings = {
+    "default_layer": interfaces.IDefaultLayer,
+    "http_methods": {
+        "PUT": interfaces.IPUT,
+        "POST": interfaces.IPOST,
+        "PATCH": interfaces.IPATCH,
+        "DELETE": interfaces.IDELETE,
+        "GET": interfaces.IGET,
+        "OPTIONS": interfaces.IOPTIONS,
+        "HEAD": interfaces.IHEAD,
+        "CONNECT": interfaces.ICONNECT
+    },
+    # pass in tuple to force ordering for default provided renderers here
+    # XXX ordering is *required* for some views to work as if no accept
+    # header is provided, it'll default to the first type provided
+    "renderers": collections.OrderedDict((
+        ("application/json", interfaces.IRendererFormatJson),
+        ("text/html", interfaces.IRendererFormatHtml)
+    )),
+    "languages": {
+        "en": languages.IEN,
+        "en-us": languages.IENUS,
+        "ca": languages.ICA
+    }
+}
+
+
+def make_app(config_file=None, settings=None, loop=None):
+    app_settings.update(_delayed_default_settings)
+
+    if loop is None:
+        loop = asyncio.get_event_loop()
+
+    if config_file is not None:
+        with open(config_file, 'r') as config:
+            settings = json.load(config)
+    elif settings is None:
+        raise Exception('Neither configuration or settings')
+
+    middlewares = [resolve_dotted_name(m) for m in settings.get('middlewares', [])]
+    # Initialize aiohttp app
+    app = web.Application(
+        router=TraversalRouter(),
+        loop=loop,
+        middlewares=middlewares,
+        **settings.get('aiohttp_settings', {}))
+
+    # Create root Application
+    root = ApplicationRoot(config_file)
+    root.app = app
+    provideUtility(root, IApplication, 'root')
+
+    # Initialize global (threadlocal) ZCA configuration
+    app.config = ConfigurationMachine()
+
+    import guillotina
+    import guillotina.db.factory
+    import guillotina.db.partition
+    import guillotina.db.writer
+    import guillotina.db.db
+    configure.scan('guillotina.translation')
+    configure.scan('guillotina.renderers')
+    configure.scan('guillotina.api')
+    configure.scan('guillotina.content')
+    configure.scan('guillotina.registry')
+    configure.scan('guillotina.auth')
+    configure.scan('guillotina.json')
+    configure.scan('guillotina.behaviors')
+    configure.scan('guillotina.languages')
+    configure.scan('guillotina.permissions')
+    configure.scan('guillotina.security.security_local')
+    configure.scan('guillotina.security.policy')
+    configure.scan('guillotina.auth.participation')
+    configure.scan('guillotina.catalog.index')
+    configure.scan('guillotina.catalog.catalog')
+    configure.scan('guillotina.framing')
+    configure.scan('guillotina.files')
+    configure.scan('guillotina.annotations')
+    configure.scan('guillotina.constraintypes')
+    configure.scan('guillotina.subscribers')
+    configure.scan('guillotina.db.strategies')
+    load_application(guillotina, root, settings)
+    app.config.execute_actions()
+    app.config.commit()
+
+    for module_name in settings.get('applications', []):
+        app.config.begin(module_name)
+        load_application(resolve_dotted_name(module_name), root, settings)
+        app.config.execute_actions()
+        app.config.commit()
+
+    # XXX we clear now to save some memory
+    # it's unclear to me if this is necesary or not but it seems to me that
+    # we don't need things registered in both components AND here.
+    configure.clear()
+
+    # update *after* plugins loaded
+    update_app_settings(settings)
+
+    if 'logging' in app_settings:
+        logging.config.dictConfig(app_settings['logging'])
+
+    content_type = ContentNegotiatorUtility(
+        'content_type', app_settings['renderers'].keys())
+    language = ContentNegotiatorUtility(
+        'language', app_settings['languages'].keys())
+
+    provideUtility(content_type, IContentNegotiation, 'content_type')
+    provideUtility(language, IContentNegotiation, 'language')
+
+    for database in app_settings['databases']:
+        for key, dbconfig in database.items():
+            factory = getUtility(
+                IDatabaseConfigurationFactory, name=dbconfig['storage'])
+            if asyncio.iscoroutinefunction(factory):
+                future = asyncio.ensure_future(
+                    factory(key, dbconfig, app), loop=app.loop)
+
+                app.loop.run_until_complete(future)
+                root[key] = future.result()
+            else:
+                root[key] = factory(key, dbconfig)
+
+    for static in app_settings['static']:
+        for key, file_path in static.items():
+            if ':' in file_path:
+                # referencing a module
+                dotted_mod_name, _, rel_path = file_path.partition(':')
+                module = resolve_dotted_name(dotted_mod_name)
+                if module is None:
+                    raise Exception('Invalid module for static directory {}'.format(file_path))
+                file_path = os.path.join(
+                    os.path.dirname(os.path.realpath(module.__file__)), rel_path)
+            path = pathlib.Path(file_path)
+            if not path.exists():
+                raise Exception('Invalid static directory {}'.format(file_path))
+            if path.is_dir():
+                root[key] = StaticDirectory(path)
+            else:
+                root[key] = StaticFile(path)
+
+    root.set_root_user(app_settings['root_user'])
+
+    if RSA is not None and not app_settings.get('rsa'):
+        key = RSA.generate(2048)
+        pub_jwk = {'k': key.publickey().exportKey('PEM')}
+        priv_jwk = {'k': key.exportKey('PEM')}
+        app_settings['rsa'] = {
+            'pub': pub_jwk,
+            'priv': priv_jwk
+        }
+
+    # Set router root
+    app.router.set_root(root)
+
+    for utility in getAllUtilitiesRegisteredFor(IAsyncUtility):
+        # In case there is Utilties that are registered
+        ident = asyncio.ensure_future(utility.initialize(app=app), loop=app.loop)
+        root.add_async_task(utility, ident, {})
+
+    app.on_cleanup.append(close_utilities)
+
+    for util in app_settings['utilities']:
+        root.add_async_utility(util, loop=app.loop)
+
+    # Load cached Schemas
+    load_cached_schema()
+
+    return app
+
+
+async def close_utilities(app):
+    for utility in getAllUtilitiesRegisteredFor(IAsyncUtility):
+        asyncio.ensure_future(utility.finalize(app=app), loop=app.loop)
+    for db in app.router._root:
+        if IDatabase.providedBy(db[1]):
+            await db[1]._db.finalize()
---
+++ packages/guillotina/guillotina-1.0.0a18.tar.gz/guillotina-1.0.0a18/guillotina/tests/test_postgres.py
@@ -0,0 +1,549 @@
+from guillotina.behaviors.dublincore import IDublinCore
+from guillotina.content import Folder
+from guillotina.db.resolution import record_object_change
+from guillotina.db.storages.pg import PostgresqlStorage
+from guillotina.db.transaction_manager import TransactionManager
+from guillotina.exceptions import ConflictError
+from guillotina.interfaces import IResource
+from guillotina.tests.utils import create_content
+import concurrent
+import asyncio
+import pytest
+
+
+async def cleanup(aps):
+    conn = await aps.open()
+    txn = conn.transaction()
+    await txn.start()
+    await conn.execute("DROP TABLE IF EXISTS objects;")
+    await conn.execute("DROP TABLE IF EXISTS blobs;")
+    await conn.execute("UPDATE transaction SET tid = 1")
+    await txn.commit()
+    await aps._pool.release(conn)
+
+
+async def get_aps(strategy='merge', pool_size=5):
+    dsn = "postgres://postgres:@localhost:5432/guillotina"
+    partition_object = "guillotina.db.interfaces.IPartition"
+    aps = PostgresqlStorage(
+        dsn=dsn, partition=partition_object, name='db',
+        transaction_strategy=strategy, pool_size=pool_size,
+        conn_acquire_timeout=0.1)
+    await aps.initialize()
+    return aps
+
+
+async def test_read_obs(postgres, dummy_request):
+    """Low level test checks that root is not there"""
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps()
+    tm = TransactionManager(aps)
+    await tm.begin()
+    txn = tm._txn
+
+    ob = create_content()
+    txn.register(ob)
+
+    assert len(txn.modified) == 1
+
+    await tm.commit()
+
+    await tm.begin()
+    txn = tm._txn
+
+    lasttid = await aps.last_transaction(txn)
+    assert lasttid is not None
+
+    ob2 = await txn.get(ob._p_oid)
+
+    assert ob2._p_oid == ob._p_oid
+    await tm.commit()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_deleting_parent_deletes_children(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps()
+    tm = TransactionManager(aps)
+    await tm.begin()
+    txn = tm._txn
+
+    folder = create_content(Folder, 'Folder')
+    txn.register(folder)
+    ob = create_content()
+    await folder.async_set('foobar', ob)
+
+    assert len(txn.modified) == 2
+
+    await tm.commit()
+    await tm.begin()
+    txn = tm._txn
+
+    ob2 = await txn.get(ob._p_oid)
+    folder2 = await txn.get(folder._p_oid)
+
+    assert ob2._p_oid == ob._p_oid
+    assert folder2._p_oid == folder._p_oid
+
+    # delete parent, children should be gone...
+    txn.delete(folder2)
+    assert len(txn.deleted) == 1
+
+    await tm.commit()
+    await tm.begin()
+    txn = tm._txn
+
+    with pytest.raises(KeyError):
+        await txn.get(ob._p_oid)
+    with pytest.raises(KeyError):
+        await txn.get(folder._p_oid)
+
+    await tm.abort()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_create_blob(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps()
+    tm = TransactionManager(aps)
+    await tm.begin()
+    txn = tm._txn
+
+    ob = create_content()
+    txn.register(ob)
+
+    await txn.write_blob_chunk('X' * 32, ob._p_oid, 0, b'foobar')
+
+    await tm.commit()
+    await tm.begin()
+    txn = tm._txn
+
+    blob_record = await txn.read_blob_chunk('X' * 32, 0)
+    assert blob_record['data'] == b'foobar'
+
+    # also get data from ob that started as a stub...
+    ob2 = await txn.get(ob._p_oid)
+    assert ob2.type_name == 'Item'
+    assert ob2.id == 'foobar'
+
+    await tm.abort()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_delete_resource_deletes_blob(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps()
+    tm = TransactionManager(aps)
+    await tm.begin()
+    txn = tm._txn
+
+    ob = create_content()
+    txn.register(ob)
+
+    await txn.write_blob_chunk('X' * 32, ob._p_oid, 0, b'foobar')
+
+    await tm.commit()
+    await tm.begin()
+    txn = tm._txn
+
+    ob = await txn.get(ob._p_oid)
+    txn.delete(ob)
+
+    await tm.commit()
+    await tm.begin()
+    txn = tm._txn
+
+    assert await txn.read_blob_chunk('X' * 32, 0) is None
+
+    with pytest.raises(KeyError):
+        await txn.get(ob._p_oid)
+
+    await tm.abort()
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_should_raise_conflict_error(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps()
+    tm1 = TransactionManager(aps)
+    tm2 = TransactionManager(aps)
+
+    # create object first, commit it...
+    await tm1.begin()
+    txn = tm1._txn
+
+    ob = create_content()
+    ob.title = 'foobar'
+    txn.register(ob)
+
+    await tm1.commit()
+
+    # 1 started before 2
+    await tm1.begin()
+    await tm2.begin()
+    txn1 = tm1._txn
+    txn2 = tm2._txn
+
+    ob1 = await txn1.get(ob._p_oid)
+    ob2 = await txn2.get(ob._p_oid)
+    ob1.title = 'foobar1'
+    record_object_change(ob1, IResource['title'], 'foobar')
+    ob2.title = 'foobar2'
+
+    txn1.register(ob1)
+    txn2.register(ob2)
+
+    # commit 2 before 1
+    await tm2.commit()
+    with pytest.raises(ConflictError):
+        await tm1.commit()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_should_not_raise_conflict_error_when_editing_diff_data(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps()
+    tm1 = TransactionManager(aps)
+    tm2 = TransactionManager(aps)
+
+    # create object first, commit it...
+    await tm1.begin()
+    txn = tm1._txn
+
+    ob = create_content()
+    ob.title = 'foobar'
+    ob.description = 'foobar'
+    txn.register(ob)
+
+    await tm1.commit()
+
+    # 1 started before 2
+    await tm1.begin()
+    await tm2.begin()
+    txn1 = tm1._txn
+    txn2 = tm2._txn
+
+    ob1 = await txn1.get(ob._p_oid)
+    ob2 = await txn2.get(ob._p_oid)
+    ob1.title = 'foobar1'
+    record_object_change(ob1, IResource['title'], 'foobar')
+    ob2.description = 'foobar2'
+    record_object_change(ob2, IDublinCore['description'], 'foobar')
+
+    txn1.register(ob1)
+    txn2.register(ob2)
+
+    # commit 2 before 1
+    await tm2.commit()
+    await tm1.commit()
+
+    # and new object should have data from each
+    await tm1.begin()
+    ob1 = await txn1.get(ob._p_oid)
+    assert ob1.title == 'foobar1'
+    assert ob2.description == 'foobar2'
+
+    await tm1.commit()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_should_raise_conflict_error_when_editing_diff_data_with_resolve_strat(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps('resolve')
+    tm1 = TransactionManager(aps)
+    tm2 = TransactionManager(aps)
+
+    # create object first, commit it...
+    await tm1.begin()
+    txn = tm1._txn
+
+    ob = create_content()
+    ob.title = 'foobar'
+    ob.description = 'foobar'
+    txn.register(ob)
+
+    await tm1.commit()
+
+    # 1 started before 2
+    await tm1.begin()
+    await tm2.begin()
+    txn1 = tm1._txn
+    txn2 = tm2._txn
+
+    ob1 = await txn1.get(ob._p_oid)
+    ob2 = await txn2.get(ob._p_oid)
+    ob1.title = 'foobar1'
+    record_object_change(ob1, IResource['title'], 'foobar')
+    ob2.description = 'foobar2'
+    record_object_change(ob2, IDublinCore['description'], 'foobar')
+
+    txn1.register(ob1)
+    txn2.register(ob2)
+
+    # commit 2 before 1
+    await tm2.commit()
+    with pytest.raises(ConflictError):
+        await tm1.commit()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_should_reset_changes_after_commit(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps()
+    tm = TransactionManager(aps)
+    await tm.begin()
+    txn = tm._txn
+
+    ob = create_content()
+    ob.title = 'foobar'
+    record_object_change(ob, IResource['title'], 'foobar')
+    txn.register(ob)
+
+    assert len(txn.modified) == 1
+    assert len(ob.__changes__) == 1
+    await tm.commit()
+    assert len(ob.__changes__) == 0
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_should_resolve_conflict_error(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps()
+    tm1 = TransactionManager(aps)
+    tm2 = TransactionManager(aps)
+
+    # create object first, commit it...
+    await tm1.begin()
+    txn = tm1._txn
+
+    ob1 = create_content()
+    ob2 = create_content()
+    txn.register(ob1)
+    txn.register(ob2)
+
+    await tm1.commit()
+
+    # 1 started before 2
+    await tm1.begin()
+    await tm2.begin()
+    txn1 = tm1._txn
+    txn2 = tm2._txn
+
+    ob1 = await txn1.get(ob1._p_oid)
+    ob2 = await txn2.get(ob2._p_oid)
+
+    txn1.register(ob1)
+    txn2.register(ob2)
+
+    # commit 2 before 1
+    await tm2.commit()
+    # XXX should not raise conflict error
+    await tm1.commit()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_should_not_resolve_conflict_error_with_simple_strat(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps('simple')
+    tm1 = TransactionManager(aps)
+    tm2 = TransactionManager(aps)
+
+    # create object first, commit it...
+    await tm1.begin()
+    txn = tm1._txn
+
+    ob1 = create_content()
+    ob2 = create_content()
+    txn.register(ob1)
+    txn.register(ob2)
+
+    await tm1.commit()
+
+    # 1 started before 2
+    await tm1.begin()
+    await tm2.begin()
+    txn1 = tm1._txn
+    txn2 = tm2._txn
+
+    ob1 = await txn1.get(ob1._p_oid)
+    ob2 = await txn2.get(ob2._p_oid)
+
+    txn1.register(ob1)
+    txn2.register(ob2)
+
+    # commit 2 before 1
+    await tm2.commit()
+    # XXX should not raise conflict error
+    with pytest.raises(ConflictError):
+        await tm1.commit()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_none_strat_allows_trans_commits(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps('none')
+    tm1 = TransactionManager(aps)
+    tm2 = TransactionManager(aps)
+
+    # create object first, commit it...
+    await tm1.begin()
+
+    ob1 = create_content()
+    tm1._txn.register(ob1)
+
+    await tm1.commit()
+
+    await tm1.begin()
+    await tm2.begin()
+    ob1 = await tm1._txn.get(ob1._p_oid)
+    ob2 = await tm2._txn.get(ob1._p_oid)
+    ob1.title = 'foobar1'
+    ob2.title = 'foobar2'
+    tm1._txn.register(ob1)
+    tm2._txn.register(ob2)
+
+    await tm2.commit()
+    await tm1.commit()
+
+    await tm1.begin()
+    ob1 = await tm1._txn.get(ob1._p_oid)
+    assert ob1.title == 'foobar1'
+
+    await tm1.abort()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_count_total_objects(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps()
+    tm1 = TransactionManager(aps)
+
+    # create object first, commit it...
+    await tm1.begin()
+    txn = tm1._txn
+
+    ob = create_content()
+    txn.register(ob)
+
+    await tm1.commit()
+    await tm1.begin()
+    txn1 = tm1._txn
+
+    assert await txn1.get_total_number_of_objects() == 1
+    assert await txn1.get_total_number_of_resources() == 1
+
+    await tm1.abort()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_using_gather_with_queries_before_prepare(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps()
+    tm = TransactionManager(aps)
+
+    # create object first, commit it...
+    await tm.begin()
+
+    ob1 = create_content()
+    tm._txn.register(ob1)
+
+    await tm.commit()
+
+    await tm.begin()
+
+    async def get_ob():
+        await tm._txn.get(ob1._p_oid)
+
+    # before we introduced locking on the connection, this would error
+    await asyncio.gather(get_ob(), get_ob(), get_ob(), get_ob(), get_ob())
+
+    await tm.abort()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_using_gather_with_queries_after_prepare(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    aps = await get_aps()
+    tm = TransactionManager(aps)
+
+    # create object first, commit it...
+    await tm.begin()
+
+    ob1 = create_content()
+    tm._txn.register(ob1)
+
+    await tm.commit()
+
+    await tm.begin()
+
+    async def get_ob():
+        await tm._txn.get(ob1._p_oid)
+
+    # one initial call should load prepared statement
+    await tm._txn.get(ob1._p_oid)
+
+    # before we introduction locking on the connection, this would error
+    await asyncio.gather(get_ob(), get_ob(), get_ob(), get_ob(), get_ob())
+
+    await tm.abort()
+
+    await aps.remove()
+    await cleanup(aps)
+
+
+async def test_exhausting_pool_size(postgres, dummy_request):
+    request = dummy_request  # noqa so magically get_current_request can find
+
+    # base aps uses 1 connection from the pool for starting transactions
+    aps = await get_aps(pool_size=2)
+    tm1 = TransactionManager(aps)
+    await tm1.begin()
+
+    tm2 = TransactionManager(aps)
+    with pytest.raises(concurrent.futures._base.TimeoutError):
+        # should throw an error because we've run out of connections in pool
+        await tm2.begin()
+
+    await tm1.abort()
+
+    await aps.remove()
+    await cleanup(aps)
