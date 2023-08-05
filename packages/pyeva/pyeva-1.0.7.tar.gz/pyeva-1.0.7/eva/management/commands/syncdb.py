0c7b09b34d8843b304086fd91f85afe063f93e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:02 +0000

    Add package pyeva/1.0.7/pyeva-1.0.7.tar.gz

---
+++ packages/pyeva/pyeva-1.0.7.tar.gz/pyeva-1.0.7/eva/management/commands/syncdb.py
@@ -0,0 +1,16 @@
+from eva.management.common import EvaManagementCommand
+from eva.management.db import load_models
+from eva.orm import create_all
+
+
+class Command(EvaManagementCommand):
+
+    def __init__(self):
+        super(Command, self).__init__()
+
+        self.cmd = 'syncdb'
+        self.help = '同步数据库(如果表不存在，则创建之)'
+
+    def run(self):
+        load_models()
+        create_all(self.args.db_echo)
