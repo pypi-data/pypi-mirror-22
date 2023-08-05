bf0549f9d95059bf17c24a38ef5911a46392553a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:09 +0000

    Add package mera2qasm/0.1/mera2qasm-0.1.tar.gz

---
+++ packages/mera2qasm/mera2qasm-0.1.tar.gz/mera2qasm-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name = 'mera2qasm',
+      version='0.1',
+      description='Studies the noise resilience of mera circuit',
+      author ='Isaac Kim',
+      author_email='ikim@us.ibm.com',
+      license='MIT',
+      packages=['mera2qasm'],
+      zip_safe=False)
