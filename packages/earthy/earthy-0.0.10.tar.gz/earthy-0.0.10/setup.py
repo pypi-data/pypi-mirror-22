c305530098c1e7b83bff6a546897fefe214a9a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:45 +0000

    Add package earthy/0.0.10/earthy-0.0.10.tar.gz

---
+++ packages/earthy/earthy-0.0.10.tar.gz/earthy-0.0.10/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+requires = ['gensim', 'scikit-learn', 'nltk',
+            'six', 'beautifulsoup4', 'requests',
+            'numpy', 'pandas']
+
+setup(
+  name='earthy',
+  packages=find_packages(),
+  version='0.0.10',
+  description='',
+  install_requires=requires,
+  author='Mere Mortal',
+  license='MIT',
+  url='https://github.com/alvations/earthy',
+  #download_url='https://github.com/alvations/earthy/tarball/0.0.2',
+  keywords=['natural language processing', 'computational linguistics'],
+  classifiers=[],
+)
