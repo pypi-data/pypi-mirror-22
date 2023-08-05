74d8d81e42d7ed3eb58b97582bc056df25d6a20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package pyjapc/1.2.6/pyjapc-1.2.6.tar.gz

---
+++ packages/pyjapc/pyjapc-1.2.6.tar.gz/pyjapc-1.2.6/pyjapc/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from .pyjapc import PyJapc
+from .rbac_dialog import PasswordEntryDialogue, getPw
+
+__version__ = "1.2.6"
+
+__cmmnbuild_deps__ = [
+    "japc",
+    "japc-value",
+    "japc-ext-cmwrda",
+    "japc-ext-cmwrda3",
+    "japc-ext-dirservice",
+    "inca-client",
+    "slf4j-log4j12",
+    "slf4j-api",
+    "log4j"
+]
