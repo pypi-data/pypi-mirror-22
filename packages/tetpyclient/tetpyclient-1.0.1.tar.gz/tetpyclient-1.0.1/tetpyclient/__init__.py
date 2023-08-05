e8de015f0fdb059fc45cefe7bbaa7459a43c1665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:01 +0000

    Add package tetpyclient/1.0.1/tetpyclient-1.0.1.tar.gz

---
+++ packages/tetpyclient/tetpyclient-1.0.1.tar.gz/tetpyclient-1.0.1/tetpyclient/__init__.py
@@ -0,0 +1,22 @@
+# Copyright 2017 Cisco Systems or its affiliates. All Rights Reserved.
+#
+# Licensed under the Cisco API License (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://www.cisco.com/c/en/us/about/legal/end-user-license-and-cloud-terms/cloud-services-acceptable-use-policy.html
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either expressed or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import tetpyclient
+
+__author__ = 'Tetration Analytics'
+__version__ = '0.1.0'
+
+__all__ = ["tetpyclient"]
+
+RestClient = tetpyclient.RestClient
+MultiPartOption = tetpyclient.MultiPartOption
