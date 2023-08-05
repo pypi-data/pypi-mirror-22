46fe4a016257f9d224fc2daaac514165372cf628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:49 +0000

    Add package c7n/0.8.24.1/c7n-0.8.24.1.tar.gz

---
+++ packages/c7n/c7n-0.8.24.1.tar.gz/c7n-0.8.24.1/c7n/resources/route53.py
@@ -0,0 +1,71 @@
+# Copyright 2016 Capital One Services, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from c7n.query import QueryResourceManager
+from c7n.manager import resources
+
+
+@resources.register('hostedzone')
+class HostedZone(QueryResourceManager):
+
+    class resource_type(object):
+        service = 'route53'
+        type = 'hostedzone'
+        enum_spec = ('list_hosted_zones', 'HostedZones', None)
+        # detail_spec = ('get_hosted_zone', 'Id', 'Id', None)
+        id = 'Id'
+        filter_name = None
+        name = 'Name'
+        date = None
+        dimension = None
+
+
+@resources.register('healthcheck')
+class HealthCheck(QueryResourceManager):
+
+    class resource_type(object):
+        service = 'route53'
+        type = 'healthcheck'
+        enum_spec = ('list_health_checks', 'HealthChecks', None)
+        name = id = 'Id'
+        filter_name = None
+        date = None
+        dimension = None
+
+
+@resources.register('rrset')
+class ResourceRecordSet(QueryResourceManager):
+
+    class resource_type(object):
+        service = 'route53'
+        type = 'rrset'
+        enum_spec = ('list_resource_record_sets', 'ResourceRecordSets', None)
+        name = id = 'Name'
+        filter_name = None
+        date = None
+        dimension = None
+
+
+@resources.register('r53domain')
+class Route53Domain(QueryResourceManager):
+
+    class resource_type(object):
+        service = 'route53domains'
+        type = 'r53domain'
+        enum_spec = ('list_domains', 'Domains', None)
+        name = id = 'Route53Domain'
+        filter_name = None
+        date = None
+        dimension = None
---
+++ packages/c7n/c7n-0.8.24.1.tar.gz/c7n-0.8.24.1/c7n/resources/sqs.py
@@ -0,0 +1,72 @@
+# Copyright 2016 Capital One Services, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from botocore.exceptions import ClientError
+
+from c7n.filters import CrossAccountAccessFilter
+from c7n.manager import resources
+from c7n.utils import local_session
+from c7n.query import QueryResourceManager
+
+
+@resources.register('sqs')
+class SQS(QueryResourceManager):
+
+    class resource_type(object):
+        service = 'sqs'
+        type = 'queue'
+        enum_spec = ('list_queues', 'QueueUrls', None)
+        detail_spec = ("get_queue_attributes", "QueueUrl", None, "Attributes")
+        id = 'QueueUrl'
+        filter_name = 'QueueNamePrefix'
+        filter_type = 'scalar'
+        name = 'QueueUrl'
+        date = 'CreatedTimestamp'
+        dimension = 'QueueName'
+
+        default_report_fields = (
+            'QueueArn',
+            'CreatedTimestamp',
+            'ApproximateNumberOfMessages',
+        )
+
+    def get_permissions(self):
+        perms = super(SQS, self).get_permissions()
+        perms.append('sqs:GetQueueAttributes')
+        return perms
+
+    def augment(self, resources):
+
+        def _augment(r):
+            client = local_session(self.session_factory).client('sqs')
+            try:
+                queue = client.get_queue_attributes(
+                    QueueUrl=r,
+                    AttributeNames=['All'])['Attributes']
+            except ClientError as e:
+                if e.response['Error']['Code'] == 'AccessDenied':
+                    self.log.warning("Denied access to sqs %s" % r)
+                    return
+                raise
+
+            queue['QueueUrl'] = r
+            return queue
+
+        self.log.debug('retrieving details for %d queues' % len(resources))
+        with self.executor_factory(max_workers=4) as w:
+            return filter(None, w.map(_augment, resources))
+
+
+@SQS.filter_registry.register('cross-account')
+class SQSCrossAccount(CrossAccountAccessFilter):
+    permissions = ('sqs:GetQueueAttributes',)
