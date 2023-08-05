817db0956c8183a0c52412b06e9b7dcea795e682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:04 +0000

    Add package dockerwithgitlabsecrets/0.1.1/dockerwithgitlabsecrets-0.1.1.tar.gz

---
+++ packages/dockerwithgitlabsecrets/dockerwithgitlabsecrets-0.1.1.tar.gz/dockerwithgitlabsecrets-0.1.1/README.md
@@ -0,0 +1,48 @@
+[![Build Status](https://travis-ci.org/wtsi-hgi/docker-with-gitlab-secrets.svg)](https://travis-ci.org/wtsi-hgi/docker-with-gitlab-secrets)
+[![codecov.io](https://codecov.io/gh/wtsi-hgi/docker-with-gitlab-secrets/graph/badge.svg)](https://codecov.io/github/wtsi-hgi/docker-with-gitlab-secrets)
+[![PyPI version](https://badge.fury.io/py/dockerwithgitlabsecrets.svg)](https://badge.fury.io/py/dockerwithgitlabsecrets)
+
+
+# Docker With GitLab Secrets
+*Wraps Docker to run with GitLab build variables.*
+
+## Usage
+```bash
+usage: docker-with-gitlab-secrets [-h] [--dwgs-config DWGS_CONFIG]
+                                  [--dwgs-project DWGS_PROJECT]
+                                  [--env-file ENV_FILE]
+
+Docker With GitLab Secrets
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --dwgs-config DWGS_CONFIG
+                        location of the configuration file (will default to
+                        /root/.dwgs-config.yml)
+  --dwgs-project DWGS_PROJECT
+                        GitLab project (if not namespaced in the form
+                        "namespace/project", the default namespace defined in
+                        the configuration file will be used). If not defined,
+                        the default project in the configuration file will be
+                        used
+  --env-file ENV_FILE   Docker argument in which this program wants to know
+                        about - see: https://docs.docker.com/engine/reference/
+                        commandline/run/#set-environment-variables--e---env---
+                        env-file
+```
+
+
+## Configuration
+Example:
+```yml
+gitlab:
+  url: https://gitlab.example.com
+  token: my-token
+  project: hgi-systems  # Optional default project, which will be overriden by if `dwgs-project` is specified
+  namespace: hgi        # Optional default namespace  
+```
+
+
+## Known Issues
+- Docker [cannot pass newlines in variables via `--env-file`](https://github.com/moby/moby/issues/12997). Therefore 
+multiline GitLab variables with have their line-breaks escaped to \\n.
---
+++ packages/dockerwithgitlabsecrets/dockerwithgitlabsecrets-0.1.1.tar.gz/dockerwithgitlabsecrets-0.1.1/dockerwithgitlabsecrets/wrapper.py
@@ -0,0 +1,91 @@
+import os
+import subprocess
+from tempfile import NamedTemporaryFile
+from time import sleep
+
+from gitlabbuildvariables.manager import ProjectVariablesManager
+import logging
+from typing import List, Tuple, Optional, Iterable, Dict
+
+_DOCKER_ENV_FILE_SUFFIX = ".env"
+_DOCKER_BINARY = "docker"
+_SUPPORTED_DOCKER_ACTIONS = ["exec", "run"]
+
+_LINE_BREAK = "\n"
+SAFE_LINE_BREAK = "\\n"
+
+_ENCODING = "utf-8"
+
+StdOutType = str
+StdErrType = str
+ReturnCodeType = int
+ProgramOutputType = Tuple[ReturnCodeType, StdOutType, StdErrType]
+
+_logger = logging.getLogger(__name__)
+
+
+def get_supported_action_index(docker_arguments: List[str]) -> Optional[int]:
+    """
+    TODO
+    :param docker_arguments: 
+    :return: 
+    """
+    if len(docker_arguments) == 0:
+        return None
+
+    docker_action_index = 0
+    while docker_arguments[docker_action_index] not in _SUPPORTED_DOCKER_ACTIONS:
+        docker_action_index += 1
+        if docker_action_index == len(docker_arguments):
+            return None
+    return docker_action_index
+
+
+def warn_if_new_lines_in_variables(variables: Dict[str, str]):
+    """
+    TODO
+    :param variables: 
+    :return: 
+    """
+    for key, value in variables.items():
+        if _LINE_BREAK in value:
+            _logger.warning(f"New line characters in variable with key \"{key}\" have been escaped to \\\\n")
+
+
+def run_wrapped(docker_arguments: List[str], project_variables_manager: ProjectVariablesManager,
+                env_file_location: str=None) -> ProgramOutputType:
+    """
+    TODO
+    :param docker_arguments: 
+    :param project_variables_manager: 
+    :param env_file_location: 
+    :return: 
+    """
+    docker_action_index = get_supported_action_index(docker_arguments)
+    docker_call = [_DOCKER_BINARY]
+
+    if docker_action_index is None:
+        assert env_file_location is None
+        docker_call += docker_arguments
+        process = subprocess.Popen(docker_call, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+    else:
+        with NamedTemporaryFile(suffix=_DOCKER_ENV_FILE_SUFFIX, mode="w") as env_file:
+            if env_file_location is not None:
+                with open(env_file_location, "r") as other_env_file:
+                    env_file.write(other_env_file.read())
+
+            variables = project_variables_manager.get()
+            warn_if_new_lines_in_variables(variables)
+            env_variables = os.linesep.join([f"{key}={value.replace(_LINE_BREAK, SAFE_LINE_BREAK)}"
+                                             for key, value in variables.items()])
+            env_file.write(env_variables)
+            env_file.flush()
+
+            from dockerwithgitlabsecrets.entrypoint import ENV_FILE_PARAMETER
+            docker_call += docker_arguments[0:docker_action_index+1] + [f"--{ENV_FILE_PARAMETER}", env_file.name] \
+                           + docker_arguments[docker_action_index+1:]
+            process = subprocess.Popen(docker_call, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+
+    return process.returncode, stdout.decode(_ENCODING), stderr.decode(_ENCODING)
