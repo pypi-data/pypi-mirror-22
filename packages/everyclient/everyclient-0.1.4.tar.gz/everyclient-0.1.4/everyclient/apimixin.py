1f47339acb90916582298ee4b5d398e68b3e3dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:42 +0000

    Add package everyclient/0.1.4/everyclient-0.1.4.tar.gz

---
+++ packages/everyclient/everyclient-0.1.4.tar.gz/everyclient-0.1.4/everyclient/apimixin.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from .http import HTTP
+from .config import open_apis
+
+
+class APIMixin(HTTP):
+
+    def __getattr__(self, name):
+        if not hasattr(super(), name):
+            self.set_attr(name)
+        return self.__getattribute__(name)
+
+    def set_attr(self, name):
+        if name in open_apis:
+            super().__setattr__(name, self._create_func(name))
+        else:
+            raise AttributeError(
+                "\n-----------------------------------------------------\n"
+                "The API method \'%s\' is not supported by Everysense."
+                "\n-----------------------------------------------------\n"
+                "Surpported API methods are: "
+                "%s" 
+                "\n-----------------------------------------------------\n"
+                % (str(name), str(open_apis))
+                )
+
+    def _create_func(self, name):
+        def func(args):
+            return self.request(name, args)
+        func.__name__ = name
+        return func
+        
