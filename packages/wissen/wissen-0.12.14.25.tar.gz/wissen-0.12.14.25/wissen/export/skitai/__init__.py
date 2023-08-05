034f705600524236aa0b73ca6b035104d079a06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package wissen/0.12.14.25/wissen-0.12.14.25.tar.gz

---
+++ packages/wissen/wissen-0.12.14.25.tar.gz/wissen-0.12.14.25/wissen/export/skitai/__init__.py
@@ -0,0 +1,19 @@
+import skitai
+import sys, os
+from .appack import mirror, indexer
+
+def bootstrap (pref):	
+	config = pref.config
+	
+	script = None
+	if config.get ("enable_mirror", False):
+		script = "%s %s %s" % (mirror.__file__, config.remote, config.local)		
+	elif config.get ("enable_index", False):
+		script = "%s %s" % (indexer__file__, config.resource_dir)
+	
+	if script:
+		logpath = config.get ("logpath")
+		redirect = logpath and (" > %s 2>&1" % os.path.join (logpath, "cron.log")) or ""
+		skitai.cron (config.sched, r"%s %s%s" % (sys.executable, script, redirect))
+
+
