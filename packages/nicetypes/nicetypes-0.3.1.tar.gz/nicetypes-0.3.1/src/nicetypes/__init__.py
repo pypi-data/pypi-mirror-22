3f89f808a82391aaf0140037e6dda7648f9af6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:00 +0000

    Add package nicetypes/0.3.1/nicetypes-0.3.1.tar.gz

---
+++ packages/nicetypes/nicetypes-0.3.1.tar.gz/nicetypes-0.3.1/src/nicetypes/__init__.py
@@ -0,0 +1,17 @@
+# pylint: disable=redefined-builtin
+# pylint: disable=wildcard-import
+from .lists import *
+from .dicts import *
+
+__version__ = "0.3.1"
+
+__title__ = "nicetypes"
+__description__ = "Advanced types for Python3"
+__uri__ = "https://github.com/tvuillemin/nicetypes"
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Thibaut Vuillemin"
+__email__ = "contact@thibautvuillemin.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2017 Thibaut Vuillemin"
