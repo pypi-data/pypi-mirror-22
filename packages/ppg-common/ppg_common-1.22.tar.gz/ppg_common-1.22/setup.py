caf68b6e5de437dc52e2d8c4c70bcf9453c2f498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package ppg-common/1.22/ppg_common-1.22.tar.gz

---
+++ packages/ppg-common/ppg_common-1.22.tar.gz/ppg_common-1.22/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='ppg_common',
+    version='1.22',
+    description='Clients',
+    url='https://bitbucket.org/sbg-interns/ppg_common',
+    author='Sbg Core Interns',
+    author_email='strahinja.kovacevic994@gmail.com',
+    include_package_data=True,
+    install_requires=[
+        'requests==2.13.0',
+        'tornado==4.5',
+        'PyYAML==3.12',
+        'SQLAlchemy==1.1.9',
+        'Jinja2==2.9.6'
+    ],
+    packages=[
+        'ppg_common',
+        'ppg_common.errors',
+        'ppg_common.clients',
+        'ppg_common.handlers',
+        'ppg_common.config'
+    ]
+)
