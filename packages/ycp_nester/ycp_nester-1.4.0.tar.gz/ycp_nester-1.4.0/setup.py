a16d6d82743fb6769c08705f2dd34b17152ab5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:10 +0000

    Add package ycp_nester/1.4.0/ycp_nester-1.4.0.tar.gz

---
+++ packages/ycp_nester/ycp_nester-1.4.0.tar.gz/ycp_nester-1.4.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+      name         = 'ycp_nester',
+      version      = '1.4.0',
+      ## TODO: be sure to change these next few lines to match your details!
+      py_modules   = ['ycp_nester'],
+      author       = 'hfpython',
+      author_email = 'hfpython@headfirstlabs.com',
+      url          = 'http://www.headfirstlabs.com',
+      description  = 'A simple printer of nested lists',
+      )
+
