ac3cd9344c90148ec71a725da4c4bfa8432a41cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:02 +0000

    Add package prpc/0.0.1.dev17/prpc-0.0.1.dev17.tar.gz

---
+++ packages/prpc/prpc-0.0.1.dev17.tar.gz/prpc-0.0.1.dev17/examples/server.py
@@ -0,0 +1,16 @@
+from prpc.service import ServiceBase
+from prpc.decorator import rpc
+from prpc.server import run_app
+from prpc.application import Application
+
+
+class Service(ServiceBase):
+    @rpc
+    def foo(self, *args, **kwargs):
+        print('foo called', self, args, kwargs)
+
+        return {'my': 'object'}
+
+
+app = Application([Service])
+run_app(app)
