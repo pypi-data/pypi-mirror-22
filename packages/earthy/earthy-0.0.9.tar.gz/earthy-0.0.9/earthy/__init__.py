13c7de74cf391bf369e3f5f038094111f56594fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:25 +0000

    Add package earthy/0.0.9/earthy-0.0.9.tar.gz

---
+++ packages/earthy/earthy-0.0.9.tar.gz/earthy-0.0.9/earthy/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, division
+
+from earthy.nltk_wrappers import download, sent_tokenize, word_tokenize
+from earthy.nltk_wrappers import pos_tag, pos_tag_sents
