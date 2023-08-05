71dcf4c45b9c126ed86587d8b415d819f40e8d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:27 +0000

    Add package pyEDFlib/0.1.8/pyEDFlib-0.1.8-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/pyEDFlib/pyEDFlib-0.1.8-cp36-cp36m-manylinux1_x86_64.whl/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.8'
+version = '0.1.8'
+full_version = '0.1.8'
+git_revision = '0d841cfbb23e6d8c7759a22789776c04a301c485'
+release = True
+
+if not release:
+    version = full_version
