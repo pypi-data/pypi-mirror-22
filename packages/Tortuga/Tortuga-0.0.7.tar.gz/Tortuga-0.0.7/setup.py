d073269e7370242dd347dfa7ca4a748369021341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:21 +0000

    Add package Tortuga/0.0.7/Tortuga-0.0.7.tar.gz

---
+++ packages/Tortuga/Tortuga-0.0.7.tar.gz/Tortuga-0.0.7/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='Tortuga',
+    version='0.0.7',
+    url='https://github.com/asweigart/tortuga',
+    author='Al Sweigart',
+    author_email='al@inventwithpython.com',
+    description=("Una aplicación española del módulo turtle.py de Python. A Spanish implementation of Python's turtle.py module."),
+    license='BSD',
+    packages=['tortuga'],
+    install_requires=['pymsgbox'],
+    test_suite='tests',
+    keywords="turtle logo spanish",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
