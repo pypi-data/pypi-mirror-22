c00b853e4ce828d16560c30ac2076e65389c434b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:23 +0000

    Add package biweeklybudget/0.1.0/biweeklybudget-0.1.0.tar.gz

---
+++ packages/biweeklybudget/biweeklybudget-0.1.0.tar.gz/biweeklybudget-0.1.0/biweeklybudget/tests/acceptance/flaskapp/views/test_index.py
@@ -0,0 +1,163 @@
+"""
+The latest version of this package is available at:
+<http://github.com/jantman/biweeklybudget>
+
+################################################################################
+Copyright 2016 Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+
+    This file is part of biweeklybudget, also known as biweeklybudget.
+
+    biweeklybudget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    biweeklybudget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with biweeklybudget.  If not, see <http://www.gnu.org/licenses/>.
+
+The Copyright and Authors attributions contained herein may not be removed or
+otherwise altered, except to add the Author attribution of a contributor to
+this work. (Additional Terms pursuant to Section 7b of the AGPL v3)
+################################################################################
+While not legally required, I sincerely request that anyone who finds
+bugs please submit them at <https://github.com/jantman/biweeklybudget> or
+to me via email, and that you send any contributions or improvements
+either as a pull request on GitHub, or to me via email.
+################################################################################
+
+AUTHORS:
+Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+################################################################################
+"""
+
+import pytest
+from biweeklybudget.tests.acceptance_helpers import AcceptanceHelper
+
+
+@pytest.mark.acceptance
+class TestIndexNavigation(AcceptanceHelper):
+
+    @pytest.fixture(autouse=True)
+    def get_page(self, base_url, selenium, testflask, refreshdb):  # noqa
+        self.baseurl = base_url
+        self.get(selenium, base_url)
+
+    def test_heading(self, selenium):
+        heading = selenium.find_element_by_class_name('navbar-brand')
+        assert heading.text == 'BiweeklyBudget'
+
+    def test_nav_menu(self, selenium):
+        ul = selenium.find_element_by_id('side-menu')
+        assert ul is not None
+        assert 'nav' in ul.get_attribute('class')
+        assert ul.tag_name == 'ul'
+
+    def test_notifications(self, selenium):
+        div = selenium.find_element_by_id('notifications-row')
+        assert div is not None
+        assert div.get_attribute('class') == 'row'
+
+
+@pytest.mark.acceptance
+class TestIndexAccounts(AcceptanceHelper):
+
+    @pytest.fixture(autouse=True)
+    def get_page(self, base_url, selenium, testflask, refreshdb):  # noqa
+        self.baseurl = base_url
+        self.get(selenium, base_url)
+
+    def test_bank_table(self, selenium):
+        table = selenium.find_element_by_xpath(
+            "//div[@id='panel-bank-accounts']//table"
+        )
+        assert self.thead2list(table) == [
+            'Account', 'Balance', 'Unreconciled', 'Difference'
+        ]
+        assert self.tbody2textlist(table) == [
+            ['BankOne', '$12,789.01 (14 hours ago)', '$0.00', '$12,789.01'],
+            ['BankTwoStale', '$100.23 (18 days ago)', '-$333.33', '$433.56']
+        ]
+        links = []
+        tbody = table.find_element_by_tag_name('tbody')
+        for tr in tbody.find_elements_by_tag_name('tr'):
+            td = tr.find_elements_by_tag_name('td')[0]
+            links.append(td.get_attribute('innerHTML'))
+        assert links == [
+            '<a href="/accounts/1">BankOne</a>',
+            '<a href="/accounts/2">BankTwoStale</a>',
+        ]
+
+    def test_bank_stale_span(self, selenium):
+        tbody = selenium.find_element_by_xpath(
+            "//div[@id='panel-bank-accounts']//table/tbody"
+        )
+        rows = tbody.find_elements_by_tag_name('tr')
+        bankTwoStale_bal_td = rows[1].find_elements_by_tag_name('td')[1]
+        bal_span = bankTwoStale_bal_td.find_elements_by_tag_name('span')[1]
+        assert bal_span.text == '(18 days ago)'
+        assert bal_span.get_attribute('class') == 'data_age text-danger'
+
+    def test_credit_table(self, selenium):
+        table = selenium.find_element_by_xpath(
+            "//div[@id='panel-credit-cards']//table"
+        )
+        assert self.thead2list(table) == [
+            'Account', 'Balance', 'Available', 'Avail - Unrec'
+        ]
+        assert self.tbody2textlist(table) == [
+            ['CreditOne', '-$952.06 (13 hours ago)', '$1,047.94', '$825.72'],
+            ['CreditTwo', '-$5,498.65 (a day ago)', '$1.35', '$1.35']
+        ]
+        links = []
+        tbody = table.find_element_by_tag_name('tbody')
+        for tr in tbody.find_elements_by_tag_name('tr'):
+            td = tr.find_elements_by_tag_name('td')[0]
+            links.append(td.get_attribute('innerHTML'))
+        assert links == [
+            '<a href="/accounts/3">CreditOne</a>',
+            '<a href="/accounts/4">CreditTwo</a>',
+        ]
+
+    def test_investment_table(self, selenium):
+        table = selenium.find_element_by_xpath(
+            "//div[@id='panel-investment']//table"
+        )
+        assert self.thead2list(table) == ['Account', 'Value']
+        assert self.tbody2textlist(table) == [
+            ['InvestmentOne', '$10,362.91 (13 days ago)']
+        ]
+        links = []
+        tbody = table.find_element_by_tag_name('tbody')
+        for tr in tbody.find_elements_by_tag_name('tr'):
+            td = tr.find_elements_by_tag_name('td')[0]
+            links.append(td.get_attribute('innerHTML'))
+        assert links == [
+            '<a href="/accounts/5">InvestmentOne</a>'
+        ]
+
+
+@pytest.mark.acceptance
+@pytest.mark.usefixtures('refreshdb', 'testflask')
+class TestIndexBudgets(AcceptanceHelper):
+
+    @pytest.fixture(autouse=True)
+    def get_page(self, base_url, selenium):  # noqa
+        self.baseurl = base_url
+        self.get(selenium, base_url + '/')
+
+    def test_budgets_table(self, selenium):
+        stable = selenium.find_element_by_id('table-standing-budgets')
+        stexts = self.tbody2textlist(stable)
+        assert stexts == [
+            ['Standing1 (4)', '$1,284.23'],
+            ['Standing2 (5)', '$9,482.29']
+        ]
+        selems = self.tbody2elemlist(stable)
+        assert selems[0][0].get_attribute(
+            'innerHTML') == '<a href="/budgets/4">' \
+                            'Standing1 (4)</a>'
