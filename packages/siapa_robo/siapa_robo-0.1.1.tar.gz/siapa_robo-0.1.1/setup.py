f6143389863d88105e05cd7e1cfeec028de1743f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:46 +0000

    Add package siapa_robo/0.1.1/siapa_robo-0.1.1.tar.gz

---
+++ packages/siapa_robo/siapa_robo-0.1.1.tar.gz/siapa_robo-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name="siapa_robo",
+    version="0.1.1",
+    author="Rafael Alves Ribeiro",
+    author_email="rafael.alves.ribeiro@gmail.com",
+    packages=["siapa_robo"],
+    install_requires=[
+        "brazilnum == 0.8.8",
+        "selenium == 3.3.1",
+        "colorama == 0.3.9"
+    ],
+)
