87765d23b13e59438337bf36d6c9a8fddd8e491a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:37 +0000

    Add package populous/0.1.0/populous-0.1.0.tar.gz

---
+++ packages/populous/populous-0.1.0.tar.gz/populous-0.1.0/setup.py
@@ -0,0 +1,56 @@
+import sys
+
+from setuptools import setup, find_packages
+
+import populous
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+requirements = [
+    "click",
+    "click-log",
+    "cached-property",
+    "Faker",
+    "dateutils",
+    "PyYAML",
+    "peloton_bloomfilters",
+    "Jinja2"
+]
+
+if sys.version_info < (3, 2):
+    requirements.append('functools32')
+
+setup(
+    name="populous",
+    version=populous.__version__,
+    url=populous.__url__,
+    description=populous.__doc__,
+    author=populous.__author__,
+    author_email=populous.__author_email__,
+    license=populous.__license__,
+    long_description=readme(),
+    packages=find_packages(exclude=['demo']),
+    install_requires=requirements,
+    extras_require={
+        'tests': ['tox', 'pytest', 'pytest-mock', 'flake8'],
+    },
+    entry_points={
+        'console_scripts': [
+            'populous = populous.__main__:cli'
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities",
+    ],
+    keywords='populous populate database',
+    zip_safe=False,
+)
