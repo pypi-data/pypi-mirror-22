00e81833b026f087c858ad28d17b0f40b9ebf7c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:07 +0000

    Add package asyncpg/0.11.0/asyncpg-0.11.0-cp36-cp36m-win32.whl

---
+++ packages/asyncpg/asyncpg-0.11.0-cp36-cp36m-win32.whl/asyncpg/protocol/codecs/numeric.pyx
@@ -0,0 +1,28 @@
+# Copyright (C) 2016-present the asyncpg authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of asyncpg and is released under
+# the Apache 2.0 License: http://www.apache.org/licenses/LICENSE-2.0
+
+
+import decimal
+
+
+_Dec = decimal.Decimal
+
+
+cdef numeric_encode(ConnectionSettings settings, WriteBuffer buf, obj):
+    text_encode(settings, buf, str(obj))
+
+
+cdef numeric_decode(ConnectionSettings settings, FastReadBuffer buf):
+    return _Dec(text_decode(settings, buf))
+
+
+cdef init_numeric_codecs():
+    register_core_codec(NUMERICOID,
+                        <encode_func>&numeric_encode,
+                        <decode_func>&numeric_decode,
+                        PG_FORMAT_TEXT)
+
+init_numeric_codecs()
