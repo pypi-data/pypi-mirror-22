0af733f5609579ad4279b06f89397fed56af649e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:56 +0000

    Add package andyg-test-package/1.0/andyg-test-package-1.0.tar.gz

---
+++ packages/andyg-test-package/andyg-test-package-1.0.tar.gz/andyg-test-package-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(name="andyg-test-package",
+    packages=["andyg_test_package"],
+    version="1.0",
+    description="Test Package",
+    author="Andy Gimma",
+    author_email="andy.n.gimma@gmail.com",
+    url="https://github.com/andygimma/test-package",
+    download_url="https://github.com/andygimma/test-package/tarball/1.0",
+    keywords=["test", "python"],
+    classifiers=[]
+)
