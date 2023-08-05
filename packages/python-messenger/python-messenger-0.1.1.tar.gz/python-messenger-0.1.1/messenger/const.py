536fdefad013ef02feb133ca2659ca47b9e91cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:42 +0000

    Add package python-messenger/0.1.1/python-messenger-0.1.1.tar.gz

---
+++ packages/python-messenger/python-messenger-0.1.1.tar.gz/python-messenger-0.1.1/messenger/const.py
@@ -0,0 +1,21 @@
+
+OBJECT_PAGE = 'page'
+MESSAGE_DATA_OBJECT = 'object'
+MESSAGE_DATA_ENTRY = 'entry'
+PAGE_ENTRY_MESSAGING = 'messaging'
+
+EVENT_OPTIN = 'optin'
+EVENT_MESSAGE = 'message'
+EVENT_DELIVERY = 'delivery'
+EVENT_POSTBACK = 'postback'
+EVENT_READ = 'read'
+EVENT_ACCOUNT_LINKING = 'account_linking'
+
+ATTACHMENT_TYPE_IMAGE = 'image'
+ATTACHMENT_TYPE_AUDIO = 'audio'
+ATTACHMENT_TYPE_VIDEO = 'video'
+ATTACHMENT_TYPE_FILE = 'file'
+
+SENDER_ACTION_MARK_SEEN = 'mark_seen'
+SENDER_ACTION_TYPING_ON = 'typing_on'
+SENDER_ACTION_TYPING_OFF = 'typing_off'
