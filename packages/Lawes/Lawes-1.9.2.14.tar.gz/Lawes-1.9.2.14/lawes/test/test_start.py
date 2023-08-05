06a5e8e07615a791a6e3ac725281c9421ad98cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:40 +0000

    Add package Lawes/1.9.2.14/Lawes-1.9.2.14.tar.gz

---
+++ packages/Lawes/Lawes-1.9.2.14.tar.gz/Lawes-1.9.2.14/lawes/test/test_start.py
@@ -0,0 +1,10 @@
+
+
+import unittest
+from lawes.test.test_db_models_fields import *
+from lawes.test.test_db_models_base import *
+from lawes.test.all_doctest import all_doctest
+
+if __name__ == "__main__":
+    all_doctest()
+    unittest.main()
