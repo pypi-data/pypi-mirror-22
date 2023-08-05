c96ddb6b2ec90cbe047e92be469521fc3961cc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:59 +0000

    Add package logging-gelf/0.0.7/logging-gelf-0.0.7.tar.gz

---
+++ packages/logging-gelf/logging-gelf-0.0.7.tar.gz/logging-gelf-0.0.7/src/logging_gelf/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. codeauthor:: Cédric Dumay <cedric.dumay@gmail.com>
+
+
+"""
+import logging
+
+SYSLOG_LEVELS = {
+    logging.CRITICAL: 2, logging.ERROR: 3, logging.WARNING: 4, logging.INFO: 6,
+    logging.DEBUG: 7,
+}
