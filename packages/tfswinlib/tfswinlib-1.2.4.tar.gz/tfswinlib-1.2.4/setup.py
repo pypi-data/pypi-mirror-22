8e057fdaa653c04c7774f7afbabf151777c388d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:13 +0000

    Add package tfswinlib/1.2.4/tfswinlib-1.2.4.tar.gz

---
+++ packages/tfswinlib/tfswinlib-1.2.4.tar.gz/tfswinlib-1.2.4/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='tfswinlib',
+      version='1.2.4',
+      description='tfswinlib provides a comfort layer for working with Microsofts TeamFoundationServer using the TFS API.',
+      author='Axel Seibert',
+      author_email='software@ergorion.com',
+      url='https://github.com/ergorion/tfswinlib',
+      py_modules=['tfswinlib'],
+      )
