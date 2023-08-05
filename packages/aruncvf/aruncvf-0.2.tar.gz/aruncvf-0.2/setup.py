c203eb33b1133700b516fc6c46c58d35974a2484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:13 +0000

    Add package aruncvf/0.2/aruncvf-0.2.tar.gz

---
+++ packages/aruncvf/aruncvf-0.2.tar.gz/aruncvf-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+ 
+setup(
+    name='aruncvf',    # This is the name of your PyPI-package.
+    version='0.2',                          # Update the version number for new releases
+    scripts=['aruncvf']                  # The name of your script, and also the command you'll be using for calling it
+)
