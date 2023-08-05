7271b7c227baae3670cf8f21e185857c2f9ed4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:49 +0000

    Add package openwhisk_docker_action/0.1.6/openwhisk_docker_action-0.1.6.tar.gz

---
+++ packages/openwhisk_docker_action/openwhisk_docker_action-0.1.6.tar.gz/openwhisk_docker_action-0.1.6/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="openwhisk_docker_action",
+    version="0.1.6",
+    description="A class to make writing openwhisk docker actions easier to write in python",
+    license="MIT",
+    author="Joshua B. Smith",
+    author_email='kognate@gmail.com',
+    url='https://github.com/kognate/openwhisk_docker_action',
+    packages=find_packages(),
+    install_requires=[ 'flask' ],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ]
+)
