b253b0f77d4fe9812dc4762314efa9141bd1be63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:42 +0000

    Add package wordinserter/0.9.6/wordinserter-0.9.6.tar.gz

---
+++ packages/wordinserter/wordinserter-0.9.6.tar.gz/wordinserter-0.9.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import platform
+import warnings
+
+requires = ["BeautifulSoup4", "cssutils", 'requests', 'webcolors', 'pygments', 'lxml']
+
+if platform.system() != "Windows":
+    warnings.warn("wordinserter currently only supports Windows for generating documents,"
+                  " functionality will be impaired.")
+
+
+setup(
+    name='wordinserter',
+    version='0.9.6',
+    packages=find_packages(),
+    url='https://github.com/orf/wordinserter',
+    license='MIT',
+    author='Tom',
+    author_email='tom@tomforb.es',
+    description='Render HTML and Markdown to a specific portion of a word document',
+    install_requires=requires,
+    long_description="""\
+Render HTML and Markdown into a word document using win32com.
+Check out the github repo for more information and code samples.
+""",
+    package_data={'wordinserter': ['images/*']}
+)
