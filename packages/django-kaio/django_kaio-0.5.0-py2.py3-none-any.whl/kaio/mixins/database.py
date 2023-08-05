ab6564668195028fbc15e10c41c53c1c2aa9036c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:04 +0000

    Add package django-kaio/0.5.0/django_kaio-0.5.0-py2.py3-none-any.whl

---
+++ packages/django-kaio/django_kaio-0.5.0-py2.py3-none-any.whl/kaio/mixins/database.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from functools import partial
+from kaio import Options
+
+
+opts = Options()
+get = partial(opts.get, section='Database')
+
+
+class DatabasesMixin(object):
+
+    def get_engine(self, prefix):
+        """
+        Retrieve the database engine.
+        Only change the full engine string if there is no «backends» in it.
+        """
+        engine = get('{}DATABASE_ENGINE'.format(prefix), 'sqlite3')
+        if 'backends' in engine:
+            return engine
+        return 'django.db.backends.' + engine
+
+    def get_options(self):
+        """
+        Retrive the many options for a database connection.
+        """
+        options = {}
+        splitter  ='DATABASE_OPTIONS_'
+        for key in opts.keys():
+            if splitter in key.upper():
+                options[str(key.upper().split(splitter)[1].lower())] = get(key)
+        return options
+
+    def get_databases(self, prefix=''):
+        return {
+            'default': {
+                'ENGINE': self.get_engine(prefix),
+                'NAME': get('{}DATABASE_NAME'.format(prefix), '{}db.sqlite'.format(prefix.lower())),
+                'USER': get('{}DATABASE_USER'.format(prefix), None),
+                'PASSWORD': get('{}DATABASE_PASSWORD'.format(prefix), ''),
+                'HOST': get('{}DATABASE_HOST'.format(prefix), ''),
+                'PORT': get('{}DATABASE_PORT'.format(prefix), ''),
+                'CONN_MAX_AGE': get('{}DATABASE_CONN_MAX_AGE'.format(prefix), 0),
+                'OPTIONS': self.get_options(),
+                'TEST': {
+                    'NAME': get('{}DATABASE_NAME'.format(prefix), None),
+                }
+            }
+        }
+
+    def DATABASES(self):
+        return self.get_databases()
