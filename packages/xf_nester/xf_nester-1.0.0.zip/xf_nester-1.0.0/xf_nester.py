1bc048326c28b9b5c938442eb839bf076882c568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:06 +0000

    Add package xf_nester/1.0.0/xf_nester-1.0.0.zip

---
+++ packages/xf_nester/xf_nester-1.0.0.zip/xf_nester-1.0.0/xf_nester.py
@@ -0,0 +1,14 @@
+
+def print_lol(the_list,level=0):
+	"""
+	这个函数取一个位置参数，名为“the_list”。
+	 这可以是任何python列表（也可以是包含嵌套列表的列表）
+	所指定的列表中的每个数据项会（递归地）输出到屏幕上，各数据项各站一行。
+	"""
+	for e_item in the_list:
+		if isinstance(e_item,list):
+			print_lol(e_item,level+1)
+		else:
+			for tab_stop in range(level):
+				    print('--------------------')
+			print(e_item)
