f85389bced8ad3252f95648757566396b70b14f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:39 +0000

    Add package markout/0.9/markout-0.9.tar.gz

---
+++ packages/markout/markout-0.9.tar.gz/markout-0.9/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name = 'markout',
+    version = '0.9',
+    description = 'Print Your Output using markdown beautifully!',
+    author = 'Samuel Mueller',
+    author_email = 'samuel.g.mueller@web.de',
+    url = 'https://github.com/SamuelGabriel/markout', # use the URL to the github repo
+    download_url = 'https://github.com/SamuelGabriel/markout/archive/0.9.tar.gz', # I'll explain this in a second
+    keywords = ['testing', 'logging', 'markdown', 'beatiful'], # arbitrary keywords
+    classifiers = [],
+    install_requires=['markdown2'],
+    scripts = ['markout']
+)
