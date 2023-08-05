3ba3ac8c8655fbad77fbf0dc0edb71a3816d38f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:11 +0000

    Add package iosbuildversions/0.4.0/iosbuildversions-0.4.0.zip

---
+++ packages/iosbuildversions/iosbuildversions-0.4.0.zip/iosbuildversions-0.4.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="iosbuildversions",
+    version="0.4.0",
+    description="iOS Version lookup based on build number",
+    author="Mike Herold",
+    author_email="me@secretobsession.me",
+    url="https://github.com/SecretObsession/ios-build-versions-python",
+    packages=["iosbuildversions"],
+    license="MIT",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers', 
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
