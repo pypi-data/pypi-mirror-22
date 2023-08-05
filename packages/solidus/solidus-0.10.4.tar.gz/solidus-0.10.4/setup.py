5eab2f64352f9ae3fb595fdbd8f29317c0c50bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:11 +0000

    Add package solidus/0.10.4/solidus-0.10.4.tar.gz

---
+++ packages/solidus/solidus-0.10.4.tar.gz/solidus-0.10.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="solidus",
+      version="0.10.4",
+      description="Solidus Security Agent for OSX",
+      url="http://github.com/SolidusSecurity/SolidusOSX",
+      author="Joe Kovacic",
+      author_email="j.kovacic@SolidusSecurity.com",
+      packages=['solidus'],
+      package_data={'solidus' : ['*']},
+      zip_safe= False)
+
+
+      
