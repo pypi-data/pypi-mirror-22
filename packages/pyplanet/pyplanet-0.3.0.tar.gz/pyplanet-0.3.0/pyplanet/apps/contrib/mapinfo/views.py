ca9f557f399407e88ef74a258d8bd9db27fd104e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:22 +0000

    Add package pyplanet/0.3.0/pyplanet-0.3.0.tar.gz

---
+++ packages/pyplanet/pyplanet-0.3.0.tar.gz/pyplanet-0.3.0/pyplanet/apps/contrib/mapinfo/views.py
@@ -0,0 +1,41 @@
+from pyplanet.views.generics.widget import WidgetView
+from pyplanet.utils import times
+from pyplanet.contrib.player.exceptions import PlayerNotFound
+
+
+class MapInfoWidget(WidgetView):
+	widget_x = 124
+	widget_y = 90.5
+	size_x = 38
+	size_y = 14
+	title = None  # 'Current map'
+	icon_style = 'Icons128x128_1'
+	icon_substyle = 'Challenge'
+
+	template_name = 'mapinfo/mapinfo.xml'
+
+	def __init__(self, app):
+		super().__init__(self)
+		self.app = app
+		self.manager = app.context.ui
+		self.id = 'pyplanet__widgets_mapinfo'
+
+	async def get_context_data(self):
+		map = self.app.instance.map_manager.current_map
+		map_author = map.author_login
+		try:
+			author = await self.app.instance.player_manager.get_player(map.author_login)
+			map_author = author.nickname
+		except PlayerNotFound:
+			if map.author_nickname:
+				map_author = map.author_nickname
+
+		context = await super().get_context_data()
+		context.update({
+			'map_name': map.name,
+			'map_author': map_author,
+			'map_authortime': times.format_time(map.time_author),
+			'map_environment': map.environment,
+		})
+
+		return context
