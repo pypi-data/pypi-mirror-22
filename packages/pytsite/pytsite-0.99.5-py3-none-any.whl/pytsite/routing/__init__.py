5b5e8215550b3f0fa3e697c134428edb77b720da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:12 +0000

    Add package pytsite/0.99.5/pytsite-0.99.5-py3-none-any.whl

---
+++ packages/pytsite/pytsite-0.99.5-py3-none-any.whl/pytsite/routing/__init__.py
@@ -0,0 +1,10 @@
+"""PytSite Routing
+"""
+from . import _error as error
+from ._rule import Rule
+from ._rules_map import RulesMap
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
---
+++ packages/pytsite/pytsite-0.99.5-py3-none-any.whl/pytsite/theme/_error.py
@@ -0,0 +1,22 @@
+"""PytSite Theme Errors
+"""
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class ThemeRegistrationFailed(Exception):
+    pass
+
+
+class ThemeNotRegistered(Exception):
+    pass
+
+
+class NoThemesRegistered(Exception):
+    pass
+
+
+class ThemeAlreadyLoaded(Exception):
+    pass
