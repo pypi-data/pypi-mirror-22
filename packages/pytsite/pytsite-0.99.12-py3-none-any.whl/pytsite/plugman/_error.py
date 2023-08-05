1292dca3607dab4e757dcb6142642f59a27c2e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:40 +0000

    Add package pytsite/0.99.12/pytsite-0.99.12-py3-none-any.whl

---
+++ packages/pytsite/pytsite-0.99.12-py3-none-any.whl/pytsite/plugman/_error.py
@@ -0,0 +1,50 @@
+"""PytSite Plugin Manager Errors.
+"""
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class ApiRequestError(Exception):
+    pass
+
+
+class UnknownPlugin(Exception):
+    pass
+
+
+class PluginNotInstalled(Exception):
+    pass
+
+
+class PluginAlreadyInstalled(Exception):
+    pass
+
+
+class PluginInstallationInProgress(Exception):
+    pass
+
+
+class PluginUninstallationInProgress(Exception):
+    pass
+
+
+class PluginInstallError(Exception):
+    pass
+
+
+class PackageInstallError(Exception):
+    pass
+
+
+class PluginUninstallError(Exception):
+    pass
+
+
+class PluginStartError(Exception):
+    pass
+
+
+class PluginAlreadyStarted(PluginStartError):
+    pass
