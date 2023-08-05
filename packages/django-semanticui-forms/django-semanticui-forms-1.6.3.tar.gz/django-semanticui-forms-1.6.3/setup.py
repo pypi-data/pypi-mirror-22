ef993ed065d493cb310c3d605017e4397e93407e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:15 +0000

    Add package django-semanticui-forms/1.6.3/django-semanticui-forms-1.6.3.tar.gz

---
+++ packages/django-semanticui-forms/django-semanticui-forms-1.6.3.tar.gz/django-semanticui-forms-1.6.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# python setup.py sdist upload -r pypi
+from setuptools import setup, find_packages
+
+setup(
+	name="django-semanticui-forms",
+	version=str("1.6.3"),
+	description="Effortlessly style all of your Django forms and form fields with Semantic UI wrappers.",
+	author="Michael",
+	author_email="thetarkus@users.noreply.github.com",
+	url="https://github.com/thetarkus/django-semanticui-forms",
+	install_requires=["django>=1.8"],
+	packages=find_packages()
+)
