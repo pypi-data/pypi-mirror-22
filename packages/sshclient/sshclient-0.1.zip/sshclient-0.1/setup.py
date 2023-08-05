0e42effd3db3bdfd1ea836966d3a3e45d2bc81bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:29 +0000

    Add package sshclient/0.1/sshclient-0.1.zip

---
+++ packages/sshclient/sshclient-0.1.zip/sshclient-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sshclient',
+    version='0.1',
+    py_modules=['sshclient'],
+    url='https://github.com/joranbeasley/sshclient',
+    license='MIT',
+    author='joran',
+    author_email='',
+    description=''
+)
