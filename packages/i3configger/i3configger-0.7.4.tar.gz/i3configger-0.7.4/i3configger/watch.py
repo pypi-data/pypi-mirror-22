1d177b794c5986300f9094a0bd90fb957c712a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:02 +0000

    Add package i3configger/0.7.4/i3configger-0.7.4.tar.gz

---
+++ packages/i3configger/i3configger-0.7.4.tar.gz/i3configger-0.7.4/i3configger/watch.py
@@ -0,0 +1,28 @@
+import logging
+
+from i3configger import base, build, ipc
+from i3configger.inotify_simple import INotify, flags
+
+log = logging.getLogger(__name__)
+
+
+_MASK = (flags.CREATE | flags.ATTRIB | flags.DELETE | flags.MODIFY |
+         flags.CLOSE_WRITE | flags.MOVED_FROM | flags.MOVED_TO |
+         flags.DELETE_SELF | flags.MOVE_SELF)
+"""Tell inotify to trigger on changes"""
+
+
+def forever(configPath):
+    partialsPath = configPath.parent
+    watcher = INotify()
+    watcher.add_watch(str(partialsPath).encode(), mask=_MASK)
+    log.debug(f"initialized {watcher}")
+    while True:
+        events = watcher.read(read_delay=50)
+        log.debug(f"events: {[f'{e[3]}:m={e[1]}' for e in events]}")
+        if not events:
+            continue
+        paths = [partialsPath / e[-1] for e in events]
+        if any(p.suffix in [base.SUFFIX, '.json'] for p in paths):
+            build.build_all(configPath)
+            ipc.communicate(refresh=True)
