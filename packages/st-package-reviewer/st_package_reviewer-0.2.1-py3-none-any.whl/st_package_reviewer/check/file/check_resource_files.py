e4d514cd85fdf007e10b173b3ade81834ee10878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:44 +0000

    Add package st-package-reviewer/0.2.1/st_package_reviewer-0.2.1-py3-none-any.whl

---
+++ packages/st-package-reviewer/st_package_reviewer-0.2.1-py3-none-any.whl/st_package_reviewer/check/file/check_resource_files.py
@@ -0,0 +1,64 @@
+import logging
+
+from . import FileChecker
+
+
+l = logging.getLogger(__name__)
+
+
+class CheckPluginsInRoot(FileChecker):
+
+    def check(self):
+        if self.glob("*.py"):
+            return
+
+        python_files_in_package = self.glob("*/**/*.py")
+        if python_files_in_package:
+            l.debug("Non-plugin Python files: %s", python_files_in_package)
+            if not self.glob("**/*.sublime-build"):
+                self.fail("The package contains {} Python file(s), "
+                          "but none of them are in the package root "
+                          "and no build system is specified"
+                          .format(len(python_files_in_package)))
+
+
+class CheckHasResourceFiles(FileChecker):
+
+    def check(self):
+        resource_file_globs = {
+            "*.py",
+            "**/*.sublime-build",
+            "**/*.sublime-commands",
+            "**/*.sublime-completions",
+            "**/*.sublime-keymap",
+            "**/*.sublime-macro",  # almost useless without other files
+            "**/*.sublime-menu",
+            "**/*.sublime-mousemap",
+            "**/*.sublime-settings",
+            "**/*.sublime-snippet",
+            "**/*.sublime-syntax",
+            "**/*.sublime-theme",
+            "**/*.tmLanguage",
+            "**/*.tmPreferences",
+            "**/*.tmSnippet",
+            "**/*.tmTheme",
+            # hunspell dictionaries
+            "**/*.aff",
+            "**/*.dic",
+        }
+
+        has_resource_files = any(self.glob(ptrn) for ptrn in resource_file_globs)
+        if not has_resource_files:
+            self.fail("The package does not define any file that interfaces with Sublime Text")
+
+
+class CheckHasSublimeSyntax(FileChecker):
+
+    def check(self):
+        syntax_files = self.glob("**/*.sublime-syntax")
+
+        for path in syntax_files:
+            if not path.with_suffix(".tmLanguage").is_file():
+                with self.file_context(path):
+                    self.warn("'.sublime-syntax' support has been added in build 3092 and there "
+                              "is no '.tmLanguage' fallback file")
