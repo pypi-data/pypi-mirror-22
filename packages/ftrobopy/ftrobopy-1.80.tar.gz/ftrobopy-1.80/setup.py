5609f0204f4cb78b25cdf7361906d2a6733fa302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:34 +0000

    Add package ftrobopy/1.80/ftrobopy-1.80.tar.gz

---
+++ packages/ftrobopy/ftrobopy-1.80.tar.gz/ftrobopy-1.80/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='ftrobopy',
+      description='Python Interface for Fischertechnik ROBOTICS TXT Controller',
+      version='1.80',
+      author='Torsten Stuehn',
+      author_email='Torsten Stuehn',
+      url='https://github.com/ftrobopy/ftrobopy',
+      download_url='https://github.com/ftrobopy/ftrobopy/archive/1.80.tar.gz',
+      license='MIT',
+      py_modules=['ftrobopy']
+      )
