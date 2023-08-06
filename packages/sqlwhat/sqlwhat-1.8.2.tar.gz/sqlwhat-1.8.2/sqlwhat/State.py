258f0d77be49b10cdd57c6dd8315d2add27f360e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:43 +0000

    Add package sqlwhat/1.8.2/sqlwhat-1.8.2.tar.gz

---
+++ packages/sqlwhat/sqlwhat-1.8.2.tar.gz/sqlwhat-1.8.2/sqlwhat/State.py
@@ -0,0 +1,49 @@
+from copy import copy
+import inspect
+
+from sqlwhat.selectors import Dispatcher
+
+class State:
+    def __init__(self,
+                 student_code,
+                 solution_code,
+                 pre_exercise_code,
+                 student_conn,
+                 solution_conn,
+                 student_result,
+                 solution_result,
+                 reporter,
+                 solution_ast = None,
+                 student_ast = None,
+                 ast_dispatcher = None):
+
+        for k,v in locals().items():
+            if k != 'self': setattr(self, k, v)
+
+        if ast_dispatcher is None:
+            # MCE doesn't always have connection - fallback on postgresql
+            dn = student_conn.dialect.name if student_conn else 'postgresql'
+            self.ast_dispatcher = Dispatcher.from_dialect(dn)
+
+        # Parse solution and student code
+        # solution code raises an exception if can't be parsed
+        if solution_ast is None: self.solution_ast = self.ast_dispatcher.parse(solution_code)
+        if student_ast  is None: self.student_ast  = self.ast_dispatcher.parse(student_code)
+
+    def do_test(self, *args, highlight=None, **kwargs):
+        highlight = self.student_ast if highlight is None else highlight
+
+        return self.reporter.do_test(*args, highlight=highlight, **kwargs)
+
+    def to_child(self, **kwargs):
+        """Basic implementation of returning a child state"""
+
+        good_pars = inspect.signature(self.__init__).parameters
+        bad_pars = set(kwargs) - set(good_pars)
+        if bad_pars:
+            raise KeyError("Invalid init params for State: %s"% ", ".join(bad_pars))
+
+        child = copy(self)
+        for k, v in kwargs.items(): setattr(child, k, v)
+        child.parent = self
+        return child
