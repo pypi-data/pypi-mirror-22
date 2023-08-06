fe42eda51fca84bf5a4fa02ad6d60eabbcf7fc39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:31 +0000

    Add package alibaba/2.0.0/alibaba-2.0.0.tar.gz

---
+++ packages/alibaba/alibaba-2.0.0.tar.gz/alibaba-2.0.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='alibaba',
+    version='2.0.0',
+    packages=[''],
+    url='http://woodpecker.rocks',
+    license='',
+    author='linpeng',
+    author_email='linpeng1219@gmail.com',
+    description='My Test'
+)
