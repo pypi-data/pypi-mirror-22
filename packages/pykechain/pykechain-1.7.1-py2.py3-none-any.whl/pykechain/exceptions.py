4506538fa7efc88a1269ece45d633b187315e547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:44 +0000

    Add package pykechain/1.7.1/pykechain-1.7.1-py2.py3-none-any.whl

---
+++ packages/pykechain/pykechain-1.7.1-py2.py3-none-any.whl/pykechain/exceptions.py
@@ -0,0 +1,26 @@
+
+class APIError(Exception):
+    """A general API Error.
+
+    A end-user descriptive message is required
+    """
+
+    pass
+
+
+class ForbiddenError(APIError):
+    """A login is required."""
+
+    pass
+
+
+class MultipleFoundError(APIError):
+    """Multiple objects are found, while a single object is requested."""
+
+    pass
+
+
+class NotFoundError(APIError):
+    """Object not found error."""
+
+    pass
