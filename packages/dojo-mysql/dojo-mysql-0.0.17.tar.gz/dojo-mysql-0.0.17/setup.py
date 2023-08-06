e382b82bbd87dd9e9b7b1b2327ce540a51ee72de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:36 +0000

    Add package dojo-mysql/0.0.17/dojo-mysql-0.0.17.tar.gz

---
+++ packages/dojo-mysql/dojo-mysql-0.0.17.tar.gz/dojo-mysql-0.0.17/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dojo-mysql',
+    version='0.0.17',
+    description='Dojo source and sink adapters for MySQL connections.',
+    author='Steven Normore',
+    author_email='steven@dataup.io',
+    url='https://dojo.dataup.io/',
+    packages=find_packages(),
+    install_requires=[
+      'dojo',
+      'mysqlclient',
+      'mysqlclient-deps'
+    ]
+)
