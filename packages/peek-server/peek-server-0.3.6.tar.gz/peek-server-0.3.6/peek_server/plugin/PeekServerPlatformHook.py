1293b2caca4af4a3a3bd1907994416c0041085be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:43 +0000

    Add package peek-server/0.3.6/peek-server-0.3.6.tar.gz

---
+++ packages/peek-server/peek-server-0.3.6.tar.gz/peek-server-0.3.6/peek_server/plugin/PeekServerPlatformHook.py
@@ -0,0 +1,32 @@
+from peek_platform import PeekPlatformConfig
+from peek_plugin_base.PeekPlatformFrontendHookABC import PeekPlatformFrontendHookABC
+from peek_plugin_base.server.PeekServerPlatformHookABC import PeekServerPlatformHookABC
+
+
+class PeekServerPlatformHook(PeekServerPlatformHookABC):
+    def __init__(self):
+        PeekPlatformFrontendHookABC.__init__(self)
+
+    @property
+    def dbConnectString(self) -> str:
+        from peek_platform import PeekPlatformConfig
+        return PeekPlatformConfig.config.dbConnectString
+
+    def _getOtherPluginApi(self, pluginName):
+        serverPluginLoader = PeekPlatformConfig.pluginLoader
+
+        otherPlugin = serverPluginLoader.pluginEntryHook(pluginName)
+        if not otherPlugin:
+            return None
+
+        from peek_plugin_base.server.PluginServerEntryHookABC import \
+            PluginServerEntryHookABC
+
+        assert isinstance(otherPlugin, PluginServerEntryHookABC), (
+            "Not an instance of PluginServerEntryHookABC")
+
+        return otherPlugin
+
+    def getOtherPluginApi(self, pluginName):
+        otherPlugin = self._getOtherPluginApi(pluginName)
+        return otherPlugin.publishedServerApi if otherPlugin else None
