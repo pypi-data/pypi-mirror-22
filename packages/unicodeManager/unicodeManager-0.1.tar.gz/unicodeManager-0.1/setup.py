b7ed20d285412b6cea65356101b44d018f37ca5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:47 +0000

    Add package unicodeManager/0.1/unicodeManager-0.1.tar.gz

---
+++ packages/unicodeManager/unicodeManager-0.1.tar.gz/unicodeManager-0.1/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='unicodeManager',
+    version="0.1",
+    description="Wrapper for the Python csv module that handles UTF-8 correctly",
+    packages=['unicodeManager']
+)
