49ee9416ca5849e6b09a74c996ae76b56b7bb92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:27 +0000

    Add package django-states2/1.6.10/django_states2-1.6.10-py2-none-any.whl

---
+++ packages/django-states2/django_states2-1.6.10-py2-none-any.whl/django_states/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+State engine for django models.
+
+Define a state graph for a model and remember the state of each object.
+State transitions can be logged for objects.
+"""
+from __future__ import absolute_import
