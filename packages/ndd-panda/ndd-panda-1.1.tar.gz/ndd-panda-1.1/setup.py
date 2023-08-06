fafa0a047e843058c858fc7569adbe5158d5a710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:52 +0000

    Add package ndd-panda/1.1/ndd-panda-1.1.tar.gz

---
+++ packages/ndd-panda/ndd-panda-1.1.tar.gz/ndd-panda-1.1/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+from setuptools import setup
+import panda
+
+VERSION = 1.1
+
+setup(
+    name='ndd-panda',
+    packages=[
+        'panda',
+        'panda.methods',
+        'panda.utils'
+    ],
+    version=VERSION,
+    description='Neuro Data Design EEG Preprocessing Pipeline',
+    author='Ryan Marren and Nitin Kumar',
+    author_email='rmarren1@jhu.edu, nkumarcc@gmail.com',
+    url='https://github.com/NeuroDataDesign/orange-panga',
+    keywords=[
+        'eeg',
+        'pipeline'
+    ],
+    classifiers=[],
+    install_requires=[  # We didnt put versions for numpy, scipy, b/c travis-ci
+        'numpy',  # We use nump v1.10.4
+        'scipy',  # We use 0.17.0
+        'seaborn',
+        'pywavelets',
+        'boto3',
+        'matplotlib',
+    ]
+)
