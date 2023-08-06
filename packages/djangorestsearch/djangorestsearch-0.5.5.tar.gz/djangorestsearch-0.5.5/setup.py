f55497c6f523c2a1a2d76f1467a9d2f73510b010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:43 +0000

    Add package djangorestsearch/0.5.5/djangorestsearch-0.5.5.tar.gz

---
+++ packages/djangorestsearch/djangorestsearch-0.5.5.tar.gz/djangorestsearch-0.5.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='djangorestsearch',
+    version='0.5.5',
+    url='https://github.com/wemap/django-rest-search',
+    license='BSD',
+    description='ElasticSearch integration for Django.',
+    long_description=open('README.rst', 'r').read(),
+    author='Jeremy Lainé',
+    author_email='jeremy@getwemap.com',
+    packages=['rest_search'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
