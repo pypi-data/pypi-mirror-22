4526c8ef22b57922785e1bca5565d6ae98c2b30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:37 +0000

    Add package rivalcfg/2.6.0/rivalcfg-2.6.0.tar.gz

---
+++ packages/rivalcfg/rivalcfg-2.6.0.tar.gz/rivalcfg-2.6.0/rivalcfg/helpers.py
@@ -0,0 +1,148 @@
+import re
+
+import pyudev
+
+
+NAMED_COLORS = {
+      "white": (0x00, 0x00, 0x00),
+     "silver": (0xC0, 0xC0, 0xC0),
+       "gray": (0x80, 0x80, 0x80),
+      "black": (0x00, 0x00, 0x00),
+        "red": (0xFF, 0x00, 0x00),
+     "maroon": (0x80, 0x00, 0x00),
+     "yellow": (0xFF, 0xFF, 0x00),
+      "olive": (0x80, 0x80, 0x00),
+       "lime": (0x00, 0xFF, 0x00),
+      "green": (0x00, 0x80, 0x00),
+       "aqua": (0x00, 0xFF, 0xFF),
+       "teal": (0x00, 0x80, 0x80),
+       "blue": (0x00, 0x00, 0xFF),
+       "navy": (0x00, 0x00, 0x80),
+    "fuchsia": (0xFF, 0x00, 0xFF),
+     "purple": (0x80, 0x00, 0x80),
+
+    # Rival 300 CS:GO Fade Edition presets
+    "preset1": (0xFF, 0x52, 0x00),
+    "preset2": (0x1D, 0xC5, 0xFF),
+    "preset3": (0x64, 0x03, 0xFC),
+    "preset4": (0xFF, 0xF2, 0x00),
+    "preset5": (0xFF, 0x00, 0x00),
+}
+
+
+def usb_device_is_connected(vendor_id, product_id):
+    """Checks if the given device is connected to the USB bus.
+
+    Arguments:
+    vendor_id -- the vendor id of the device
+    product_id -- the product id of the device
+    """
+    ctx = pyudev.Context()
+    devices = ctx.list_devices(ID_VENDOR_ID=vendor_id)
+
+    for device in devices:
+        if (device["ID_MODEL_ID"] == product_id):
+            return True
+    return False
+
+
+def find_hidraw_device_path(vendor_id, product_id, interface_num=0):
+    """
+    Find the first HID interface for the given USB vendor id and product id.
+
+    Arguments:
+    vendor_id -- the vendor id of the device
+    product_id -- the product id of the device
+    interface_num -- the interface number (default: 0)
+    """
+    ctx = pyudev.Context()
+    devices = ctx.list_devices(ID_VENDOR_ID=vendor_id)
+
+    for device in devices:
+        if (device["ID_MODEL_ID"] != product_id):
+            continue
+        if (device["SUBSYSTEM"] != "hidraw"):
+            continue
+        if int(device["ID_USB_INTERFACE_NUM"]) != interface_num:
+            continue
+
+        return device["DEVNAME"]
+
+    # Old Kernels...
+    devices = ctx.list_devices(SUBSYSTEM="hidraw")
+    deviceMatcher = re.compile(r"^.*/usb[0-9]+/[0-9/.-]+:[0-9]+\.%i/[0-9]+:%s:%s.*$" % (
+        interface_num,
+        vendor_id.upper(),
+        product_id.upper()
+        ))
+    for device in devices:
+        if not deviceMatcher.match(device["DEVPATH"]):
+            continue
+
+        return device["DEVNAME"]
+
+
+
+def is_color(string):
+    """Checks if the given string is a valid color.
+
+    Arguments:
+    string -- the string to check
+    """
+    return string in NAMED_COLORS or bool(re.match(r"^#?[0-9a-f]{3}([0-9a-f]{3})?$", string, re.IGNORECASE))
+
+
+def color_string_to_rgb(color_string):
+    """Converts the color string into an RGB tuple.
+
+    Arguments:
+    color_string -- the string to converts
+
+    Returns:
+    an (R, G, B) tuple
+    """
+    # Named color
+    if color_string in NAMED_COLORS:
+        return NAMED_COLORS[color_string]
+    # #f00 or #ff0000 -> f00 or ff0000
+    if color_string.startswith("#"):
+        color_string = color_string[1:]
+    # f00 -> ff0000
+    if len(color_string) == 3:
+        color_string = color_string[0] * 2 + color_string[1] * 2 + color_string[2] * 2
+    # ff0000 -> (255, 0, 0)
+    return (
+        int(color_string[0:2], 16),
+        int(color_string[2:4], 16),
+        int(color_string[4:], 16)
+        )
+
+
+def choices_to_list(choices):
+    """Transforms choices dict to an ordered string list.
+
+    Arguments:
+    choices -- the dict containing available choices
+    """
+    return list(map(str, sorted(choices.keys(), key=lambda v: v if type(v) == int else -1)))
+
+
+def choices_to_string(choices):
+    """Transforms choices dict to a printable string.
+
+    Arguments:
+    choices -- the dict containing available choices
+    """
+    return ", ".join(choices_to_list(choices))
+
+
+def merge_bytes(*args):
+    """Merge byte and list of byte into a single list of byte."""
+    result = []
+    for arg in args:
+        if type(arg) in [list, tuple]:
+            result.extend(arg)
+        else:
+            result.append(arg)
+    return result
+
---
+++ packages/rivalcfg/rivalcfg-2.6.0.tar.gz/rivalcfg-2.6.0/rivalcfg/mice/__init__.py
@@ -0,0 +1,13 @@
+from .rival import rival
+from .rival100 import rival100
+from .rival300 import rival300
+from .rival300csgofadeedition import rival300csgofadeedition
+from .rival300csgohyperbeastedition import rival300csgohyperbeastedition
+
+mice_list = [
+    rival,
+    rival100,
+    rival300,
+    rival300csgofadeedition,
+    rival300csgohyperbeastedition,
+]
