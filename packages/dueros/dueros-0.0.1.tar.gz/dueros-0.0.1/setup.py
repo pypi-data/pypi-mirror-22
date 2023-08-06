b6e994247d6ca5704f46c3bf6545db45433d842b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:18 +0000

    Add package dueros/0.0.1/dueros-0.0.1.tar.gz

---
+++ packages/dueros/dueros-0.0.1.tar.gz/dueros-0.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env Python  
+#coding=utf-8
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='dueros',
+    version='0.0.1',
+    description = 'Testing API',
+    author = 'dialog manager',
+    author_email = 'dialogbot@163.com',
+    url = '',
+    license="MIT",
+    keywords='dueros,api',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
