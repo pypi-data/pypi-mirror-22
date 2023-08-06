949db52370a8c2a534dfd163b0c7616f8c65870c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:22 +0000

    Add package sciencedates/1.2.7/sciencedates-1.2.7.tar.gz

---
+++ packages/sciencedates/sciencedates-1.2.7.tar.gz/sciencedates-1.2.7/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+req = ['nose','numpy','pytz','python-dateutil','xarray','matplotlib']
+# %%
+import pip
+try:
+    import conda.cli
+    conda.cli.main('install',*req)
+except ImportError:
+    pip.main(['install'] + req)
+# %%
+from setuptools import setup
+
+setup(name='sciencedates',
+      packages=['sciencedates'],
+      version = '1.2.7',
+      description='Date conversions used in the sciences.',
+      author = 'Michael Hirsch, Ph.D.',
+      url = 'https://github.com/scivision/sciencedates',
+      classifiers=[
+      'Intended Audience :: Science/Research',
+      'Development Status :: 5 - Production/Stable',
+      'License :: OSI Approved :: MIT License',
+      'Topic :: Scientific/Engineering :: GIS',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      ],
+      install_requires=req,
+	  )
+
