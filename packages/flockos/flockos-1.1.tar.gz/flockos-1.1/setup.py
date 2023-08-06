73099a0d27ddc6be4986733c13fa837145e12ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:35 +0000

    Add package flockos/1.1/flockos-1.1.tar.gz

---
+++ packages/flockos/flockos-1.1.tar.gz/flockos-1.1/setup.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+
+
+import sys
+from setuptools import setup, find_packages
+
+NAME = "flockos"
+VERSION = "1.1"
+
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["requests", "six >= 1.10", "PyJWT"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Flock API",
+    url="https://github.com/flockchat/pyflock",
+    keywords=["Flock API"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    Integrate your apps with flock using this API
+    """
+)
