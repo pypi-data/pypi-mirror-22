eacdab9839b23a0adac8dff592d73c357a36090c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:25 +0000

    Add package benderthon/0.1.0/benderthon-0.1.0.tar.gz

---
+++ packages/benderthon/benderthon-0.1.0.tar.gz/benderthon-0.1.0/benderthon/cmdline.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Set of utilities to work easier with Bender."""
+
+import argparse
+
+from benderthon import tf_freeze
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Set of utilities for Bender.")
+    subparsers = parser.add_subparsers()
+
+    parser_freeze = subparsers.add_parser('tf-freeze',
+                                          help="Utility to easily convert TensorFlow checkpoints into minimal frozen "
+                                               "graphs in binary protobuf format.")
+    parser_freeze.add_argument('input_checkpoint', help="checkpoint path to load")
+    parser_freeze.add_argument('output_file', help="file to save the binary protobuf graph")
+    parser_freeze.add_argument('output_node_names', help="the name of the output nodes, comma separated")
+    parser_freeze.add_argument('--no-weights', action='store_true',
+                               help="indicate that the variables are not converted to consts")
+    args = parser.parse_args()
+    if args.no_weights:
+        tf_freeze.save_graph_only_from_checkpoint(args.input_checkpoint, args.output_file, args.output_node_names)
+    else:
+        tf_freeze.freeze_from_checkpoint(args.input_checkpoint, args.output_file, args.output_node_names)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/benderthon/benderthon-0.1.0.tar.gz/benderthon-0.1.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+import pypandoc
+
+setup(
+    name="benderthon",
+    version="0.1.0",
+    description="Set of utilities to work easier with Bender.",
+    long_description=pypandoc.convert('README.md', 'rst'),
+    url="https://github.com/xmartlabs/benderthon",
+    keywords=["Bender", "machine learning", "artificial intelligence", "freeze", "model", "utility", "utilities",
+              "TensorFlow"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'benderthon = benderthon.cmdline:main',
+        ],
+    },
+    install_requires=[
+        'tensorflow>=1.2.0rc1',
+    ],
+    author="Xmartlabs",
+    author_email="hi@xmartlabs.com",
+    maintainer="Santiago Castro",
+    maintainer_email="santiago@xmartlabs.com",
+    license="Apache 2.0",
+)
