337543b29183c7302742c05d4bb0b6ef02617a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:26 +0000

    Add package bplist/0.1/bplist-0.1.tar.gz

---
+++ packages/bplist/bplist-0.1.tar.gz/bplist-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'bplist',
+  packages = ['bplist'],  
+  version = '0.1',
+  description = 'Apple binary Property List reader/writer for Python',
+  install_requires=[],
+  author = 'Vladimir Pouzanov',
+  author_email = 'farcaller@gmail.com',
+  url = 'https://github.com/gamikun/bplist-python',
+  keywords = ['bplist'], 
+  classifiers = [],
+)
