966d8bdd4df27e197d3d879d4bf3b2cf73edf13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:21 +0000

    Add package kedsort/0.1.8/kedsort-0.1.8.tar.gz

---
+++ packages/kedsort/kedsort-0.1.8.tar.gz/kedsort-0.1.8/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'kedsort',
+  packages = ['kedsort'], # this must be the same as the name above
+  version = '0.1.8',
+  description = 'sort pdfs',
+  author = 'Andy Gimma',
+  author_email = 'andy.n.gimma@gmail.com',
+  url = 'https://github.com/andygimma/pdfsort_cli', # use the URL to the github repo
+  download_url = 'https://github.com/andygimma/pdfsort_cli/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['pdf'],
+  classifiers = [],
+)
