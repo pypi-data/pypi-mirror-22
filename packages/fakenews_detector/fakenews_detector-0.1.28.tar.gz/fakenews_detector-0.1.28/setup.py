f0eccd86babaff423816c8c38a9bc271fea6cc0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:26 +0000

    Add package fakenews_detector/0.1.28/fakenews_detector-0.1.28.tar.gz

---
+++ packages/fakenews_detector/fakenews_detector-0.1.28.tar.gz/fakenews_detector-0.1.28/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Command to update package python setup.py sdist upload -r pypi
+
+setup(
+    name='fakenews_detector',
+    packages=['fakenews_detector', 'fakenews_detector.fake_fact_ai'],
+    version='0.1.28',
+    description='Detect fake and get information about the source.',
+    author='Mieszko Makuch',
+    author_email='mmakuch@googlemail.com',
+    url='https://github.com/',
+    download_url='https://mieszko_makuch@bitbucket.org/mieszko_makuch/fakenews_detector.git',
+    keywords=['fakenews', 'fake', 'news', 'detector'],
+    package_data={'fakenews_detector': ['json_local/*.dat', 'json_local/opensources/*.dat', 'fake_fact_ai/static/*.dat']},
+    zip_safe=False,
+    install_requires=[
+        "validators==0.11.3",
+        "numpy==1.11.3",
+        "scipy==0.18.1",
+        "sklearn==0.0",
+        "nltk==3.2.4"
+    ]
+)
