9e9cbed56b6ccf17bcf7d4894be05fb75b35fce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:11 +0000

    Add package clink/0.17.0/clink-0.17.0.tar.gz

---
+++ packages/clink/clink-0.17.0.tar.gz/clink-0.17.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+'''
+SYNOPSIS
+
+    pip install -e .
+
+DESCRIPTION
+
+    Metadata for this package.
+
+REFERENCES
+
+    Building and Distributing Packages with Setuptools
+        http://setuptools.readthedocs.io/en/latest/setuptools.html
+'''
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='clink',
+    version='0.17.0',
+    description='HTTP APIs Framework',
+    keywords='http api framework',
+    author='Kevin Leptons',
+    author_email='kevin.leptons@gmail.com',
+    url='https://github.com/kevin-leptons/clink',
+    download_url='https://github.com/kevin-leptons/clink',
+    python_requires=">=3.4",
+    install_requires=[
+        'pymongo==3.4.0', 'PyJWT==1.4.0', 'jsonschema==2.6.0'
+    ],
+    packages=find_packages(exclude=['tool', 'test']),
+    package_data={'clink': ['asset/**/*']}
+)
