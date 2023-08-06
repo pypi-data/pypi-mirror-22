c021d943696dc7d519b3aa35283289254922dff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:00 +0000

    Add package xillypy/0.1.1/xillypy-0.1.1-py2.py3-none-any.whl

---
+++ packages/xillypy/xillypy-0.1.1-py2.py3-none-any.whl/xillypy-0.1.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,32 @@
+XillyPy |PyPi| |License|
+=========================
+
+A simple Xillybus interface written in Python.
+It supports streaming as well as reading/writing bytes to an address/data interface.
+
+Usage
+=====
+.. code:: python
+
+  import xillypy
+  # write 16 (0x10, 0b00010000) to address 2
+  xillypy.memory_write('/dev/xillybus_mem', 2, (16,))
+  # write some bytes (.encode()) to a streaming interface
+  xillypy.stream_write('/dev/xillybus_write', 'this is my data i want to write'.encode())
+
+
+
+An example can be found in `benchmark.py <https://github.com/krabo0om/XillyPy/blob/master/benchmark.py>`_.
+
+Compatibility
+=============
+The code was tested with Python 3.5 and 3.6 on a Linux Host (Ubuntu 16.04).
+
+
+.. |PyPi| image:: https://img.shields.io/pypi/v/xillypy.svg
+    :target: https://pypi.python.org/pypi/xillypy/
+
+.. |License| image:: https://img.shields.io/badge/license-MIT-blue.svg
+    :target: https://raw.githubusercontent.com/krabo0om/XillyPy/master/LICENSE
+
+
---
+++ packages/xillypy/xillypy-0.1.1-py2.py3-none-any.whl/xillypy/lowlevel.py
@@ -0,0 +1,67 @@
+import array
+import struct
+
+__author__ = 'Paul Genssler'
+
+
+def memory_read(dev_file: str, address: int, length=1) -> tuple:
+    """
+    reads bytes from a rc2f memory file
+    @param dev_file: the memory to use
+    @param address: where to read
+    @param length: how many bytes to read
+    @return: the read bytes as a tuple
+    @rtype: tuple
+    """
+    with open(dev_file, 'rb') as mem:
+        mem.seek(address)
+        data = mem.read(length)
+        return struct.unpack('B' * length, data)
+
+
+def memory_write(dev_file: str, address: int, data: tuple):
+    """
+    reads a byte from a rc2f memory file
+    @param dev_file: the memory to use
+    @param address: which byte to read
+    @param data: a tuple of bytes to write
+    """
+    with open(dev_file, 'wb') as mem:
+        mem.seek(address)
+        mem.write(struct.pack('B' * len(data), *data))
+
+
+def stream_write(dev_file: str, data) -> int:
+    """
+    stream data into the device
+    @param dev_file: the target device
+    @param data: data to write, iterable and slicable container (like a list) with bytes objects
+    @return: how many bytes were written
+    @rtype: int
+    """
+    with open(dev_file, 'wb') as fifo:
+        wrote = 0
+        data_len = len(data)
+        while data_len > wrote:
+            wrote += fifo.write(data[wrote:data_len])
+        return wrote
+
+
+def stream_read(dev_file: str, length=-1, chunk_size=2 ** 12):
+    """
+    reads data from the device into an array of chuck_size bytes and yields it
+    @param dev_file: the target device
+    @param length: how many bytes to read, -1 until eof
+    @param chunk_size: how much data per read, low values impact performance
+    """
+    with open(dev_file, 'rb') as fifo:
+        read = 0
+        while length < 0 or read < length:
+            data = array.array('B')
+            try:
+                data.fromfile(fifo, min(length - read, chunk_size))
+                read += len(data)
+                yield data
+            except EOFError:
+                yield data
+                break
