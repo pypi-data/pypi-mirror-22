d7b2eb0deaee2e215cdfae3293d17fa7616af27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:10 +0000

    Add package pyresult/0.2.0/pyresult-0.2.0.tar.gz

---
+++ packages/pyresult/pyresult-0.2.0.tar.gz/pyresult-0.2.0/pyresult/operators.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+'''Result operators'''
+
+from toolz import curry
+
+from pyresult.result import (
+    VALUE_IDX,
+    ok,
+    error,
+    result,
+    is_ok,
+    is_error,
+)
+
+
+@curry
+def errmap(func, res):
+    '''Transform error with `func`.
+
+    errmap: (x -> y) -> Result a x -> Result a y
+    '''
+    return error(func(res[VALUE_IDX])) if is_error(res) else res
+
+
+@curry
+def rmap(func, res):
+    '''Map `func` to result `res`
+
+    rmap: (a -> value) -> Result a -> Result value
+    '''
+    return ok(func(res[VALUE_IDX])) if is_ok(res) else res
+
+
+@curry
+def and_then(func, res):
+    '''Chain together a sequence of computations that may fail.
+
+    and_then: (a -> Result b x) -> Result a x -> Result b x
+    '''
+    return result(func(res[VALUE_IDX])) if is_ok(res) else res
+
+
+@curry
+def and_else(func, res):
+    '''When `res` is error, then call `func` with it
+
+    and_else: (x -> Result b x) -> Result a x -> Result a x
+    '''
+    return result(func(res[VALUE_IDX])) if is_error(res) else res
+
+
+def resolve(res):
+    '''Flatten nested results
+
+    resolve: Result (Result a x) x -> Result a x
+    '''
+    return res if is_error(res) else result(res[VALUE_IDX])
---
+++ packages/pyresult/pyresult-0.2.0.tar.gz/pyresult-0.2.0/tests/test_operators.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring, invalid-name
+
+from pyresult.result import OK, ERROR, ok, error
+from pyresult.operators import (
+    errmap,
+    rmap,
+    and_then,
+    and_else,
+    resolve
+)
+
+
+def test_errmap_is_ok():
+    val = (ERROR, 'msg')
+
+    def f(q):
+        return q + " hello"
+
+    assert errmap(f, val) == (ERROR, 'msg hello')
+
+
+def test_errmap_original_res():
+    val = (OK, 'val')
+
+    def f(val):
+        return val
+
+    assert errmap(f, val) == val
+
+
+def test_rmap_is_ok():
+    val = (OK, 'val')
+
+    def f(val):
+        return val
+
+    assert rmap(f, val) == val
+
+
+def test_rmap_original_res():
+    val = (ERROR, 'msg')
+
+    def f(val):
+        return val
+
+    assert rmap(f, val) == val
+
+
+def test_and_then_is_ok():
+
+    val = 1
+
+    def f(a):
+        return (OK, a + 1)
+
+    assert and_then(f, ok(val)) == (OK, val + 1)
+
+
+def test_and_then_original_res():
+    val = (ERROR, 'msg')
+
+    def f(a):
+        return a + 1
+
+    assert and_then(f, val) == val
+
+
+def test_and_else_is_ok():
+
+    val = 1
+
+    def f(a):
+        return (ERROR, a + 1)
+
+    assert and_else(f, error(val)) == (ERROR, val + 1)
+
+
+def test_and_else_original_res():
+    val = (OK, 'val')
+
+    def f(val):
+        return val
+
+    assert and_else(f, val) == val
+
+
+def test_resolve_original_res():
+
+    val = (ERROR, (ERROR, 'msg'))
+
+    assert resolve(val) == val
+
+
+def test_resolve_isnt_ok():
+
+    val = (OK, (OK, 'val'))
+
+    assert resolve(val) == (OK, 'val')
