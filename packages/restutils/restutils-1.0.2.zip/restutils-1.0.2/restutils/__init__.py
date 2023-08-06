834dd045cd74e8f3ab538754f527ae6047a1addb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:37 +0000

    Add package restutils/1.0.2/restutils-1.0.2.zip

---
+++ packages/restutils/restutils-1.0.2.zip/restutils-1.0.2/restutils/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#######################################################################
+# Module Name   : __init__.py
+# Author        : Pulak Pattanayak
+# Date          : 17-05-2017
+#######################################################################
+from _restclient import *
+
+__all__ = ["RESTClient"]
