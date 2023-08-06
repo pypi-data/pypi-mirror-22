ca45cb04a95d9851b08f194325a418fd35751781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:49 +0000

    Add package sensor-outlier-detector/0.3/sensor_outlier_detector-0.3.tar.gz

---
+++ packages/sensor-outlier-detector/sensor_outlier_detector-0.3.tar.gz/sensor_outlier_detector-0.3/sensor_outlier_detector/__init__.py
@@ -0,0 +1,4 @@
+from adv.fib import fibonacci
+from adv.sqrt import squareroot
+from add import add
+from subtract import subtract
---
+++ packages/sensor-outlier-detector/sensor_outlier_detector-0.3.tar.gz/sensor_outlier_detector-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sensor_outlier_detector',
+      version='0.3',
+      description='sensor data outlier in the world',
+      url='http://github.com/vijayendra-g/sensor_outlier_detector',
+      author='vijay G',
+      author_email='vijayendra.iiith@gmail.com',
+      license='GNU',
+      packages=['sensor_outlier_detector'],
+      zip_safe=False)
