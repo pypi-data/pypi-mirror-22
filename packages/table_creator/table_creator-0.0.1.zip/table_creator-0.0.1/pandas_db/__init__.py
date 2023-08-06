ee94525c924a6a339257ca71cc101be387144ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:21 +0000

    Add package table_creator/0.0.1/table_creator-0.0.1.zip

---
+++ packages/table_creator/table_creator-0.0.1.zip/table_creator-0.0.1/pandas_db/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#from table_creator import table_creator
+"""
+    Ce module proclame la bonne parole de sieurs Sam et Max. Puissent-t-ils
+    retrouver une totale liberté de pensée cosmique vers un nouvel age
+    reminiscent.
+"""
+ 
+__version__ = "0.0.1"
