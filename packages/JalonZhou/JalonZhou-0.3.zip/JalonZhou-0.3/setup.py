656f9603b2571cc384a55d44009511f9e7688aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:01 +0000

    Add package JalonZhou/0.3/JalonZhou-0.3.zip

---
+++ packages/JalonZhou/JalonZhou-0.3.zip/JalonZhou-0.3/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='JalonZhou',
+    version='0.3',
+    packages=['jalon',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
