70eb8a52199e6bd52ef2ef1288dec124d9aefcf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:08 +0000

    Add package rickytest/1.0.0/rickytest-1.0.0.zip

---
+++ packages/rickytest/rickytest-1.0.0.zip/rickytest-1.0.0/setup.py
@@ -0,0 +1,2 @@
+from distutils.core import setup
+setup(name='rickytest',version='1.0.0',py_modules=['rickytest'],author='Ricky',author_email='rickychen0701@163.com',url='http://headfirstlabs.com',description='Test')
