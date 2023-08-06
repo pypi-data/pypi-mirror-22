cbf17f05b98fbdf5d3020a790f0aaf73484cae9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:33 +0000

    Add package nflex-connector-utils/0.1.9/nflex-connector-utils-0.1.9.tar.gz

---
+++ packages/nflex-connector-utils/nflex-connector-utils-0.1.9.tar.gz/nflex-connector-utils-0.1.9/nflex_connector_utils/ip_address.py
@@ -0,0 +1,35 @@
+class IpAddress(object):
+    """
+        A representation of an IP address
+
+        Args:
+            ip_address (str): IPv4 or IPv6 address
+            network_id (str): Optional network id. This isn't used yet, but may be used in the future to associate an IP address with a network.
+            network_name (str): Optional network name. This isn't used yet.
+
+    """  # noqa
+
+    def __init__(self, ip_address=None, network_id=None, network_name=None,
+                 description=None):
+        self._ip_address = ip_address
+        self._network_id = network_id
+        self._network_name = network_name
+        self._description = description
+
+    def serialize(self):
+        """Serialize the contents"""
+
+        results = {}
+        if self._ip_address is not None:
+            results['ip_address'] = self._ip_address
+
+        if self._network_id is not None and self._network_name is not None:
+            results['network'] = {
+                'id': self._network_id,
+                'name': self._network_name,
+            }
+
+        if self._description is not None:
+            results['description'] = self._description
+
+        return results
