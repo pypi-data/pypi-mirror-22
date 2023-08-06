f0718fd2ca71a582cc75bdef298d39eeaa303bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:28 +0000

    Add package python-gammu/2.8/python-gammu-2.8.tar.gz

---
+++ packages/python-gammu/python-gammu-2.8.tar.gz/python-gammu-2.8/setup.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# vim: expandtab sw=4 ts=4 sts=4:
+#
+# Copyright © 2003 - 2016 Michal Čihař <michal@cihar.com>
+#
+# This file is part of python-gammu <https://wammu.eu/python-gammu/>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+'''
+python-gammu - Phone communication libary
+'''
+
+import distutils.spawn
+from setuptools import setup, Extension
+import os
+import codecs
+
+# some defines
+VERSION = '2.8'
+GAMMU_REQUIRED = '1.37.90'
+README_FILE = os.path.join(os.path.dirname(__file__), 'README.rst')
+with codecs.open(README_FILE, 'r', 'utf-8') as readme:
+    README = readme.read()
+
+
+def check_minimum_gammu_version():
+    distutils.spawn.spawn([
+        'pkg-config',
+        "--print-errors",
+        "--atleast-version={0}".format(GAMMU_REQUIRED),
+        "gammu"
+    ])
+
+
+def get_pkgconfig_data(args, mod, required=True):
+    """Run pkg-config to and return content associated with it"""
+    f = os.popen("pkg-config {0} {1}".format(" ".join(args), mod))
+
+    line = f.readline()
+    if line is not None:
+        line = line.strip()
+
+    if line is None or line == "":
+        if required:
+            raise Exception(
+                "Cannot determine '{0}' from pkg-config".format(" ".join(args))
+            )
+        else:
+            return ""
+
+    return line
+
+
+def get_module():
+    libs = get_pkgconfig_data(["--libs-only-l"], "gammu gammu-smsd", False)
+    libs = libs.replace('-l', '').split()
+    module = Extension(
+        'gammu._gammu',
+        libraries=libs,
+        include_dirs=['include/'],
+        sources=[
+            'gammu/src/errors.c',
+            'gammu/src/data.c',
+            'gammu/src/misc.c',
+            'gammu/src/convertors/misc.c',
+            'gammu/src/convertors/string.c',
+            'gammu/src/convertors/time.c',
+            'gammu/src/convertors/base.c',
+            'gammu/src/convertors/sms.c',
+            'gammu/src/convertors/memory.c',
+            'gammu/src/convertors/todo.c',
+            'gammu/src/convertors/calendar.c',
+            'gammu/src/convertors/bitmap.c',
+            'gammu/src/convertors/ringtone.c',
+            'gammu/src/convertors/backup.c',
+            'gammu/src/convertors/file.c',
+            'gammu/src/convertors/call.c',
+            'gammu/src/convertors/wap.c',
+            'gammu/src/convertors/diverts.c',
+            'gammu/src/gammu.c',
+            'gammu/src/smsd.c',
+        ]
+    )
+    ldflags = get_pkgconfig_data(["--libs-only-L"], "gammu gammu-smsd", False)
+    cflags = get_pkgconfig_data(["--cflags"], "gammu gammu-smsd", False)
+    module.extra_compile_args.append(
+        '-DPYTHON_GAMMU_VERSION="{0}"'.format(VERSION)
+    )
+    if cflags:
+        module.extra_compile_args.append(cflags)
+    if ldflags:
+        module.extra_link_args.append(ldflags)
+    return module
+
+
+check_minimum_gammu_version()
+setup(
+    name='python-gammu',
+    version=VERSION,
+    description='Gammu bindings',
+    long_description=README,
+    author='Michal Čihař',
+    author_email='michal@cihar.com',
+    platforms=['Linux', 'Mac OSX', 'Windows XP/2000/NT', 'Windows 95/98/ME'],
+    keywords=[
+        'mobile', 'phone', 'SMS', 'contact', 'gammu', 'calendar', 'todo'
+    ],
+    license='GPLv2+',
+    url='https://wammu.eu/python-gammu/',
+    download_url='https://wammu.eu/download/python-gammu/',
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: '
+        'GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: C',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Communications :: Telephony',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Hardware',
+    ],
+    test_suite="test",
+    packages=['gammu'],
+    ext_modules=[get_module()]
+)
---
+++ packages/python-gammu/python-gammu-2.8.tar.gz/python-gammu-2.8/test/test_config.py
@@ -0,0 +1,135 @@
+# -*- coding: UTF-8 -*-
+# vim: expandtab sw=4 ts=4 sts=4:
+#
+# Copyright © 2003 - 2016 Michal Čihař <michal@cihar.com>
+#
+# This file is part of python-gammu <https://wammu.eu/python-gammu/>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+import unittest
+import tempfile
+import gammu
+from .test_sms import PDU_DATA
+
+
+class ConfigTest(unittest.TestCase):
+    def test_config_bool(self):
+        state_machine = gammu.StateMachine()
+        state_machine.SetConfig(
+            0,
+            {
+                'StartInfo': True,
+                'UseGlobalDebugFile': True,
+                'DebugFile': None,
+                'SyncTime': True,
+                'Connection': 'at',
+                'LockDevice': True,
+                'DebugLevel': 'textalldate',
+                'Device': '',
+                'Model': '',
+            },
+        )
+        cfg = state_machine.GetConfig(0)
+        self.assertEqual(cfg['StartInfo'], 1)
+
+    def test_config_string(self):
+        state_machine = gammu.StateMachine()
+        state_machine.SetConfig(
+            0,
+            {
+                'StartInfo': 'yes',
+                'UseGlobalDebugFile': 'no',
+                'DebugFile': 'dbg.log',
+                'SyncTime': 'true',
+                'Connection': 'fbus',
+                'LockDevice': 'FALSE',
+                'DebugLevel': 'textall',
+                'Device': '',
+                'Model': '',
+            },
+        )
+        cfg = state_machine.GetConfig(0)
+        self.assertEqual(cfg['StartInfo'], 1)
+
+    def test_config_none(self):
+        state_machine = gammu.StateMachine()
+        state_machine.SetConfig(
+            0,
+            {
+                'StartInfo': None,
+                'UseGlobalDebugFile': None,
+                'DebugFile': 'dbg.log',
+                'SyncTime': 'true',
+                'Connection': 'dlr3',
+                'LockDevice': 'NO',
+                'DebugLevel': 'binary',
+                'Device': '',
+                'Model': '',
+            },
+        )
+        cfg = state_machine.GetConfig(0)
+        self.assertEqual(cfg['StartInfo'], 0)
+
+    def test_init_error(self):
+        self.assertRaises(TypeError, gammu.StateMachine, Bar=1)
+
+
+class DebugTest(unittest.TestCase):
+    def setUp(self):
+        gammu.SetDebugLevel('textall')
+
+    def check_operation(self, filename):
+        """
+        Executes gammu operation which causes debug logs.
+        """
+        gammu.DecodePDU(PDU_DATA)
+        if filename is not None:
+            with open(filename, 'r') as handle:
+                self.assertTrue('SMS type: Status report' in handle.read())
+
+    def test_file(self):
+        testfile = tempfile.NamedTemporaryFile(suffix='.debug')
+        try:
+            gammu.SetDebugFile(testfile.file)
+            self.check_operation(testfile.name)
+        finally:
+            gammu.SetDebugFile(None)
+            testfile.close()
+
+    def test_filename(self):
+        testfile = tempfile.NamedTemporaryFile(suffix='.debug')
+        try:
+            gammu.SetDebugFile(testfile.name)
+            self.check_operation(testfile.name)
+        finally:
+            gammu.SetDebugFile(None)
+            testfile.close()
+
+    def test_none(self):
+        gammu.SetDebugFile(None)
+        self.check_operation(None)
+
+    def test_nothing(self):
+        gammu.SetDebugLevel('nothing')
+        testfile = tempfile.NamedTemporaryFile(suffix='.debug')
+        try:
+            gammu.SetDebugFile(testfile.file)
+            self.check_operation(None)
+            with open(testfile.name, 'r') as handle:
+                self.assertEqual('', handle.read())
+        finally:
+            gammu.SetDebugFile(None)
+            testfile.close()
