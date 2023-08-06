3b373cb6a7f61f613b31c99de2dd0e5c2487bc57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:31 +0000

    Add package i3configger/0.3.2/i3configger-0.3.2.tar.gz

---
+++ packages/i3configger/i3configger-0.3.2.tar.gz/i3configger-0.3.2/tests/test_build.py
@@ -0,0 +1,13 @@
+# from pathlib import Path
+#
+# from i3configger import base, build
+#
+# HERE = Path(__file__).parent
+# EXAMPLES = HERE.parent / 'examples'
+# TARGET = HERE / 'target'
+#
+#
+# def test_build():
+#     base.DEBUG = 0
+#     builder = build.Builder()
+#     builder.build()
