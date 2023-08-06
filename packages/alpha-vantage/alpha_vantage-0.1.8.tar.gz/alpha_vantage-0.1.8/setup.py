2b89fa917c712a0f82618b044119988328cb4e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:16 +0000

    Add package alpha-vantage/0.1.8/alpha_vantage-0.1.8.tar.gz

---
+++ packages/alpha-vantage/alpha_vantage-0.1.8.tar.gz/alpha_vantage-0.1.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from os import path
+
+setup(
+    name='alpha_vantage',
+    version='0.1.8',
+    author='Romel J. Torres',
+    author_email='romel.torres@gmail.com',
+    license='MIT',
+    description='Python module to get stock data from the Alpha Vantage Api',
+    url = 'https://github.com/RomelTorres/alpha_vantage',
+    install_requires=[
+        'simplejson',
+        'pandas',
+        'nose'
+    ],
+    keywords=['stocks', 'market', 'finance', 'alpha_vantage', 'quotes',
+    'shares'],
+    packages= find_packages(exclude=['helpers','test_alpha_vantage', 'images']),
+    package_data={
+        'alpha_vantage':[],
+    }
+)
