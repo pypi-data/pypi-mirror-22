dd62e0a0e2b72c14f4430b3432fec47a51b9a51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:07 +0000

    Add package OmMongo/1.1.1/OmMongo-1.1.1.tar.gz

---
+++ packages/OmMongo/OmMongo-1.1.1.tar.gz/OmMongo-1.1.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+VERSION = '1.1.1'
+DESCRIPTION = 'Document-Object Mapper/Toolkit for Mongo Databases'
+
+setup(
+    name='OmMongo',
+    version=VERSION,
+    description=DESCRIPTION,
+    author='Bapakode Open Source',
+    license='MIT',
+    author_email='opensource@bapakode.org',
+    url='http://bapakode.org/ommongo',
+    packages=['ommongo', 'ommongo.fields'],
+    install_requires=['pymongo'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
