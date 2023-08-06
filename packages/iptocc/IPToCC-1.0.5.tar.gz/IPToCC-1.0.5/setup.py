fd1bcf3865fa15f4fc37a7a2da21df9f2c677f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:55 +0000

    Add package iptocc/1.0.5/IPToCC-1.0.5.tar.gz

---
+++ packages/iptocc/IPToCC-1.0.5.tar.gz/IPToCC-1.0.5/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import ssl
+from setuptools import setup
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+VERSION = open('VERSION').read().strip()
+REQUIREMENTS = []
+with open('requirements.txt') as f:
+    for line in f:
+        REQUIREMENTS.append(line.strip())
+
+setup(
+    name='IPToCC',
+    version=VERSION,
+    packages=['iptocc'],
+    url='https://github.com/Code-ReaQtor/IPToCC',
+    download_url='https://github.com/Code-ReaQtor/IPToCC/tarball/{}'.format(VERSION),
+    license='MIT',
+    author='Ronie Martinez',
+    author_email='ronmarti18@gmail.com',
+    description='Get country code of IPv4/IPv6 address. Address lookup is done offline.',
+    long_description=open('README').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=REQUIREMENTS,
+    setup_requires=['Cython==0.25.2'],
+    package_data={'iptocc': ['rir_statistics_exchange.db']}
+)
