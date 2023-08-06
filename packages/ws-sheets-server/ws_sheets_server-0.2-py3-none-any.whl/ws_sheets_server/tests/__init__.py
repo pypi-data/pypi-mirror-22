be9348009a434a66d40a4d7f8144484b5a3f2f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:38 +0000

    Add package ws-sheets-server/0.2/ws_sheets_server-0.2-py3-none-any.whl

---
+++ packages/ws-sheets-server/ws_sheets_server-0.2-py3-none-any.whl/ws_sheets_server/tests/__init__.py
@@ -0,0 +1,5 @@
+
+from .check_stored import *
+from .client import *
+
+
---
+++ packages/ws-sheets-server/ws_sheets_server-0.2-py3-none-any.whl/ws_sheets_server/tests/check_stored.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import json
+import logging
+import logging.config
+import modconf
+import unittest
+
+import ws_storage.filesystem
+import ws_sheets
+
+class TestCheckStored(unittest.TestCase):
+    def test(self):
+        
+        conf = modconf.import_conf('ws_sheets_server.tests.conf.simple')
+
+        #logging.config.dictConfig(settings_module.LOGGING)
+    
+        #port = settings_module.PORT
+        
+        folder = conf.STORAGE_FOLDER_PRODUCTION
+        
+        stor = ws_storage.filesystem.Storage(ws_sheets.Book, folder)
+    
+        #server = sheets_backend.sockets.Server(stor, port)
+    
+        #return stor
+        
+        lst = os.listdir(stor.folder)
+    
+        print('files:',lst)
+    
+        for f in lst:
+            h, t = os.path.splitext(f)
+            if t == '.bin':
+                try:
+                    o = stor.read(h)
+                    print(repr(o))
+                    print(dir(o))
+                except Exception as e:
+                    print(e)
+    
+                print()
+    
