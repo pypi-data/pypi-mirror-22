31f27e9f123a84c5d767207b17df2709eb23bbc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:14 +0000

    Add package moderngl/4.1.0/ModernGL-4.1.0-cp35-cp35m-win32.whl

---
+++ packages/moderngl/ModernGL-4.1.0-cp35-cp35m-win32.whl/ModernGL/framebuffers.py
@@ -0,0 +1,76 @@
+'''
+    ModernGL framebuffers
+'''
+
+from .common import InvalidObject
+
+
+class Framebuffer:
+    '''
+        Create a :py:class:`Framebuffer` using :py:meth:`Context.framebuffer`.
+    '''
+
+    def __init__(self):
+        self.mglo = None
+        raise NotImplementedError()
+
+    def release(self):
+        '''
+            Release the ModernGL object.
+        '''
+
+        self.mglo.release()
+        self.__class__ = InvalidObject
+
+    @staticmethod
+    def new(obj):
+        '''
+            For internal use only.
+        '''
+
+        res = Framebuffer.__new__(Framebuffer)
+        res.mglo = obj
+        return res
+
+    def clear(self, red=0, green=0, blue=0, alpha=0, *, viewport=None) -> None:
+        '''
+            Clear the framebuffer.
+
+            Values must be in ``(0, 255)`` range.
+            If the `viewport` is not ``None`` then scrissor test
+            will be used to clear the given viewport.
+
+            If the `viewport` is a 2-tuple it will clear the
+            ``(0, 0, width, height)`` where ``(width, height)`` is the 2-tuple.
+
+            If the `viewport` is a 4-tuple it will clear the given viewport.
+
+            Args:
+                red (int): color component.
+                green (int): color component.
+                blue (int): color component.
+                alpha (int): alpha component.
+
+            Keyword Args:
+                viewport (tuple): The viewport.
+        '''
+
+        if viewport is not None:
+            viewport = tuple(viewport)
+
+        self.mglo.clear(red, green, blue, alpha, viewport)
+
+    def use(self) -> None:
+        '''
+            Bind the framebuffer. Set the target for the
+            `VertexArray.render` or `VertexArray.transform` methods.
+        '''
+
+        self.mglo.use()
+
+    def read(self, viewport=None, components=3, *, floats=False) -> bytes:
+        '''
+            Read the framebuffer content.
+        '''
+
+        return self.mglo.read(viewport, components, floats)
---
+++ packages/moderngl/ModernGL-4.1.0-cp35-cp35m-win32.whl/ModernGL/renderbuffers.py
@@ -0,0 +1,90 @@
+'''
+    ModernGL renderbuffers
+'''
+
+from .common import InvalidObject
+
+
+class Renderbuffer:
+    '''
+        Renderbuffer objects are OpenGL objects that contain images.
+        They are created and used specifically with :py:class:`Framebuffer` objects.
+        They are optimized for use as render targets, while :py:class:`Texture` objects
+        may not be, and are the logical choice when you do not need to sample
+        from the produced image. If you need to resample, use Textures instead.
+        Renderbuffer objects also natively accommodate multisampling.
+
+        A Renderbuffer object cannot be instantiated directly, it requires a context.
+        Use :py:meth:`Context.renderbuffer` or :py:meth:`Context.depth_renderbuffer`
+        to create one.
+    '''
+
+    def __init__(self):
+        self.mglo = None
+        raise NotImplementedError()
+
+    def release(self):
+        '''
+            Release the ModernGL object.
+        '''
+
+        self.mglo.release()
+        self.__class__ = InvalidObject
+
+    @staticmethod
+    def new(obj):
+        '''
+            For internal use only.
+        '''
+
+        res = Renderbuffer.__new__(Renderbuffer)
+        res.mglo = obj
+        return res
+
+    @property
+    def width(self) -> int:
+        '''
+            int: The width of the renderbuffer.
+        '''
+
+        return self.mglo.width
+
+    @property
+    def height(self) -> int:
+        '''
+            int: The height of the renderbuffer.
+        '''
+
+        return self.mglo.height
+
+    @property
+    def size(self) -> tuple:
+        '''
+            tuple: The size of the renderbuffer.
+        '''
+
+        return (self.mglo.width, self.mglo.height)
+
+    @property
+    def samples(self) -> int:
+        '''
+            int: The samples of the renderbuffer.
+        '''
+
+        return self.mglo.samples
+
+    @property
+    def components(self) -> int:
+        '''
+            int: The components of the renderbuffer.
+        '''
+
+        return self.mglo.components
+
+    @property
+    def depth(self) -> bool:
+        '''
+            bool: Is the renderbuffer a depth renderbuffer?
+        '''
+
+        return self.mglo.depth
