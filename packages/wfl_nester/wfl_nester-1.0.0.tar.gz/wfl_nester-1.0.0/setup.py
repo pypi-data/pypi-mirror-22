c5f9d469dba88b21f0197187aaa6dab8a04b703c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:47 +0000

    Add package wfl_nester/1.0.0/wfl_nester-1.0.0.tar.gz

---
+++ packages/wfl_nester/wfl_nester-1.0.0.tar.gz/wfl_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'wfl_nester',
+	version = '1.0.0',
+	py_modules = ['wfl_nester'],
+	author	= 'wfl',
+	author_email='85197959@qq.com',
+	url	= 'http://www.dd.com',
+	description	= 'A simple printer of nested lists',
+)
