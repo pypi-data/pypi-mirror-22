97f5a8add331c0d6a070d770bcaf28b86b4afcd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:07 +0000

    Add package dh/0.10.1/dh-0.10.1-py3-none-any.whl

---
+++ packages/dh/dh-0.10.1-py3-none-any.whl/dh/thirdparty/__init__.py
@@ -0,0 +1,17 @@
+"""
+Third-party modules which are essential and must always available.
+
+For maximum compatibility, these modules should be pure Python without
+non-standard dependencies.
+
+List of current modules:
+
+  * atomicwrites-1.0.0 (https://github.com/untitaker/python-atomicwrites)
+  * tabulate-0.7.5 (https://bitbucket.org/astanin/python-tabulate)
+  * tqdm-4.7.6 (https://github.com/tqdm/tqdm)
+
+List of potential modules to add:
+
+  * pypng (https://github.com/drj11/pypng)
+
+"""
