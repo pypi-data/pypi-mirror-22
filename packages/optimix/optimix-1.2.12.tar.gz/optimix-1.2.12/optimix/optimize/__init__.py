daf380bcae5d5acb3ab3bfe50672439657eed560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:10 +0000

    Add package optimix/1.2.12/optimix-1.2.12.tar.gz

---
+++ packages/optimix/optimix-1.2.12.tar.gz/optimix-1.2.12/optimix/optimize/__init__.py
@@ -0,0 +1,6 @@
+from .brent import minimize as minimize_scalar
+from .brent import maximize as maximize_scalar
+
+from .bfgs import minimize
+from .bfgs import maximize
+from .exception import BadSolutionError
