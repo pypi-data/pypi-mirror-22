a5f0cd730d1f05774ebea54e8a7b2ffae87ebf6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:48 +0000

    Add package yamlord/0.1/yamlord-0.1.tar.gz

---
+++ packages/yamlord/yamlord-0.1.tar.gz/yamlord-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='yamlord',
+      version='0.1',
+      description='wrapper for yaml package to read/write OrderedDict',
+      author='Ryan J. Dillon',
+      author_email='ryanjamesdillon@gmail.com',
+      url='https://github.com/ryanjdillon/yamlord',
+      download_url='https://github.com/ryanjdillon/yamlord/archive/0.1.tar.gz',
+      license='MIT',
+      packages=['yamlord'],
+      zip_safe=False)
