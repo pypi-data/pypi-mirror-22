76ac172e7769c38a5a9b17cb35fddd57c8b3f538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:58 +0000

    Add package iparapheur-utils/0.2.0.post6178/iparapheur-utils-0.2.0.post6178.tar.gz

---
+++ packages/iparapheur-utils/iparapheur-utils-0.2.0.post6178.tar.gz/iparapheur-utils-0.2.0.post6178/parapheur/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+
+from requests.packages.urllib3.exceptions import SubjectAltNameWarning
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from requests.packages.urllib3.exceptions import InsecurePlatformWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
+requests.packages.urllib3.disable_warnings(SubjectAltNameWarning)
+
+"""
+    Ce module est un test de client parapheur
+"""
+__version__ = "0.2.0-6178"
+
+
+def getrestclient():
+    # We have to define it here, because we use it everywhere in scripts
+    import parapheur
+    return parapheur.getrestclient()
+
+
+def getsoapclient(user=None, password=None):
+    import parapheur
+    return parapheur.getsoapclient(user, password)
