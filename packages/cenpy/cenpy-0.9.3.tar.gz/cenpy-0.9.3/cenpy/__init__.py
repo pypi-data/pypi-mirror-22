90a79af9e6ff9b7cd216902f578970245b6a5de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:49 +0000

    Add package cenpy/0.9.3/cenpy-0.9.3.tar.gz

---
+++ packages/cenpy/cenpy-0.9.3.tar.gz/cenpy-0.9.3/cenpy/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'Levi John Wolf ljw2@asu.edu'
+#__version__ == '0.9.8.dev'
+
+import cenpy.explorer
+import cenpy.base
+from cenpy._version import version as _version
+from cenpy.tools import _load_sitekey
+
+SITEKEY = _load_sitekey()
+
+#__all__ = ['explorer', 'base']
