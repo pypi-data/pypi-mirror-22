44ad7d87c8def90d6dde7ddc557fd4cdb7e5e11c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:30 +0000

    Add package Mocha/0.5.1/Mocha-0.5.1.tar.gz

---
+++ packages/Mocha/Mocha-0.5.1.tar.gz/Mocha-0.5.1/mocha/skel/create-view/view.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Mocha
+
+Views
+
+"""
+from mocha import (Mocha,
+                   page_attr,
+                   config,
+                   flash_success,
+                   flash_error,
+                   abort,
+                   request,
+                   url_for,
+                   redirect,
+                   models,
+                   utils,
+                   paginate,
+                   decorators as deco
+                   )
+
+
+# ------------------------------------------------------------------------------
+
+
+@deco.route("/%ROUTE%/")
+@deco.nav_title("%NAV_TITLE%")
+class Index(Mocha):
+
+    @deco.nav_title("Home", order=1)
+    def index(self):
+        page_attr(title="Hello View!")
+        return
