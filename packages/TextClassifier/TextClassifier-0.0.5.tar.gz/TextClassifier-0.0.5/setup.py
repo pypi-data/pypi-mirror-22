02cddab5231af3144c5a2b8ac31309dab14d11ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:17 +0000

    Add package TextClassifier/0.0.5/TextClassifier-0.0.5.tar.gz

---
+++ packages/TextClassifier/TextClassifier-0.0.5.tar.gz/TextClassifier-0.0.5/setup.py
@@ -0,0 +1,33 @@
+#coding:utf-8
+'''
+Created on 2017Äê6ÔÂ5ÈÕ
+
+@author: Arnold_Gaius,ChiangClaire
+'''
+
+from __future__ import print_function  
+from setuptools import setup, find_packages  
+import sys  
+  
+setup(  
+    name="TextClassifier",  
+    version="0.0.5",  
+    author="Arnold_Gaius,ChiangClaire",  
+    author_email="jiangcmd@qq.com",  
+    description="Short text Classifier based on Numpy,scikit-learn,Pandas,Matplotlib",  
+    long_description=open("README.rst").read(),  
+    license="MIT",  
+    url="https://github.com/ArnoldGaius/Text_Classifier",  
+    packages=['TextClassifier'],  
+    install_requires=[  
+        "scikit-learn",  
+        "numpy",
+        "matplotlib",
+        "pandas",
+        ],  
+    classifiers=[  
+        "Programming Language :: Python",  
+        "Programming Language :: Python :: 2",    
+        "Programming Language :: Python :: 2.7",  
+    ],  
+)  
