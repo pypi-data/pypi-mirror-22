9d720d660b6d3f33671995d8e7a8282a8095368c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:38 +0000

    Add package skfeature-chappers/1.0.1/skfeature-chappers-1.0.1.zip

---
+++ packages/skfeature-chappers/skfeature-chappers-1.0.1.zip/skfeature-chappers-1.0.1/skfeature/function/information_theoretical_based/JMI.py
@@ -0,0 +1,37 @@
+from . import LCSI
+from skfeature.utility.util import reverse_argsort
+
+def jmi(X, y, mode="rank", **kwargs):
+    """
+    This function implements the JMI feature selection
+
+    Input
+    -----
+    X: {numpy array}, shape (n_samples, n_features)
+        input data, guaranteed to be discrete
+    y: {numpy array}, shape (n_samples,)
+        input class labels
+    kwargs: {dictionary}
+        n_selected_features: {int}
+            number of features to select
+
+    Output
+    ------
+    F: {numpy array}, shape (n_features,)
+        index of selected features, F[0] is the most important feature
+
+    Reference
+    ---------
+    Brown, Gavin et al. "Conditional Likelihood Maximisation: A Unifying Framework for Information Theoretic Feature Selection." JMLR 2012.
+    """
+    if 'n_selected_features' in list(kwargs.keys()):
+        n_selected_features = kwargs['n_selected_features']
+        F = LCSI.lcsi(X, y, function_name='JMI', n_selected_features=n_selected_features)
+    else:
+        F = LCSI.lcsi(X, y, function_name='JMI')
+    
+    if mode=="index":
+        return np.array(F, dtype=int)
+    else:
+        # make sure that F is the same size??
+        return reverse_argsort(F, size=X.shape[1])
