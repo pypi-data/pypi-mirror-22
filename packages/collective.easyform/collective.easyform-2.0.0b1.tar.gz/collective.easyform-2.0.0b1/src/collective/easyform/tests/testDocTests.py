50e85449464f6028f10c6172602b7320749ad240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:04 +0000

    Add package collective.easyform/2.0.0b1/collective.easyform-2.0.0b1.tar.gz

---
+++ packages/collective.easyform/collective.easyform-2.0.0b1.tar.gz/collective.easyform-2.0.0b1/src/collective/easyform/tests/testDocTests.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from collective.easyform.tests.base import EasyFormFunctionalTestCase
+from Testing import ZopeTestCase as ztc
+
+# from plone.testing import layered
+import doctest
+import unittest
+
+
+optionflags = (
+    doctest.REPORT_ONLY_FIRST_FAILURE |
+    doctest.NORMALIZE_WHITESPACE |
+    doctest.ELLIPSIS |
+    doctest.REPORTING_FLAGS)
+
+testfiles = (
+    'browser.rst',
+    'attachment.rst',
+    'ssl.txt',
+    'serverside_field.txt',
+    '../api.py',
+    '../README.txt',
+)
+
+
+def test_suite():
+    return unittest.TestSuite([
+        ztc.FunctionalDocFileSuite(
+            f, package='collective.easyform.tests',
+            test_class=EasyFormFunctionalTestCase,
+            optionflags=optionflags)
+        for f in testfiles
+    ])
