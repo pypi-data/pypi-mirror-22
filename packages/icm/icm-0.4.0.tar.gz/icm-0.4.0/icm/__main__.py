7715906932dcb6de49710e8e31363939148dfb33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:59 +0000

    Add package icm/0.4.0/icm-0.4.0.tar.gz

---
+++ packages/icm/icm-0.4.0.tar.gz/icm-0.4.0/icm/__main__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# -- This file is part of the Icestudio project
+# -- (C) 2017 FPGAwars
+# -- Author Jesús Arroyo
+# -- Licence GPLv2
+
+import click
+
+from icm.commands import cmd_create, cmd_validate, cmd_update
+
+
+@click.group()
+@click.version_option()
+def cli():
+    """Icestudio collections manager"""
+
+
+@cli.command()
+def create():
+    """Create a collection structure."""
+    cmd_create.create()
+
+
+@cli.command()
+def update():
+    """Update docs and translation."""
+    cmd_update.update()
+
+
+@cli.command()
+def validate():
+    """Validate a collection."""
+    cmd_validate.validate()
---
+++ packages/icm/icm-0.4.0.tar.gz/icm-0.4.0/icm/resources/README.tpl.md
@@ -0,0 +1,21 @@
+# $name Collection
+
+[![Icestudio](https://img.shields.io/badge/collection-icestudio-blue.svg)](https://github.com/FPGAwars/icestudio)
+![Version](https://img.shields.io/badge/version-v$version-orange.svg)
+
+$description.
+
+## Install
+
+* Download the collection$links
+* Install the collection: *Tools > Collections > Add*
+* Load the collection: *Select > Collection*
+
+$blocks
+$examples
+$languages
+$authors
+$contributors
+## License
+
+Licensed under [$license](https://opensource.org/licenses/$license).
