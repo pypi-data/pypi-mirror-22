bd265c593de4163904d85a6eeeb3949d9b2bb7ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:16 +0000

    Add package ubuntudesign.gsa/1.1.0/ubuntudesign.gsa-1.1.0.tar.gz

---
+++ packages/ubuntudesign.gsa/ubuntudesign.gsa-1.1.0.tar.gz/ubuntudesign.gsa-1.1.0/setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+# Core
+from setuptools import setup
+
+setup(
+    name='ubuntudesign.gsa',
+    version='1.1.0',
+    author='Canonical webteam',
+    author_email='robin+pypi@canonical.com',
+    url='https://github.com/ubuntudesign/python-gsa',
+    packages=[
+        'ubuntudesign',
+        'ubuntudesign.gsa',
+    ],
+    description=(
+        'A client for communicating with the Google Search Appliance.'
+    ),
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "requests>=2.10.0",
+        "lxml>=3.3.0"
+    ],
+)
