5370cfbb5cb7200bd04401190aa084c8320af9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:28 +0000

    Add package cellarpy/1.2/cellarpy-1.2.tar.gz

---
+++ packages/cellarpy/cellarpy-1.2.tar.gz/cellarpy-1.2/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="cellarpy",
+    version="1.2",
+    author="Jonathan Huot",
+    author_email="jonathan.huot@gmail.com",
+    description=("cellarpy empower python web-applications based on bottlepy"),
+    license="MIT",
+    keywords="bottle web wsgi",
+    url="https://github.com/JonathanHuot/cellarpy",
+    packages=['cellar'],
+    test_suite="tests",
+    long_description=read('README.md'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Framework :: Bottle',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
