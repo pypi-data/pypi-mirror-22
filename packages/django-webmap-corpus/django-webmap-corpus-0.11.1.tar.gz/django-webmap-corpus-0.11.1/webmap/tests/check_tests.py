90219d5537fc6e1f7b130efd27f6d523015516b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:53 +0000

    Add package django-webmap-corpus/0.11.1/django-webmap-corpus-0.11.1.tar.gz

---
+++ packages/django-webmap-corpus/django-webmap-corpus-0.11.1.tar.gz/django-webmap-corpus-0.11.1/webmap/tests/check_tests.py
@@ -0,0 +1,13 @@
+"""Tests for the models of the webmap app."""
+import django
+from django.test import TestCase
+
+
+class RunChecksTestCase(TestCase):
+    def test_checks(self):
+        django.setup()
+        from django.core import checks
+        all_issues = checks.run_checks()
+        errors = [str(e) for e in all_issues if e.level >= checks.ERROR]
+        if errors:
+            self.fail('checks failed:\n' + '\n'.join(errors))
