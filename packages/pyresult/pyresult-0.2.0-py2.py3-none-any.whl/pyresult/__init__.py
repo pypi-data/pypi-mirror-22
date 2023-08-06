27f3dd6902e5578c7920f6efc7410948d007cace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:10 +0000

    Add package pyresult/0.2.0/pyresult-0.2.0-py2.py3-none-any.whl

---
+++ packages/pyresult/pyresult-0.2.0-py2.py3-none-any.whl/pyresult/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''A result pattern for python'''
+
+from pyresult.result import (  # noqa
+    ok,
+    error,
+    result,
+    is_ok,
+    is_error,
+    value
+)
+
+__author__ = """Jindrich Kralevic Smitka"""
+__email__ = 'smitka.j@gmail.com'
+__version__ = '0.2.0'
