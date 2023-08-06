af8f8e5afbf7f13b9a13752802c31e09d3a4d721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:03 +0000

    Add package arcpylogger/2.1.0/arcpylogger-2.1.0.tar.gz

---
+++ packages/arcpylogger/arcpylogger-2.1.0.tar.gz/arcpylogger-2.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='arcpylogger',
+    version='2.1.0',
+    packages=['arcpylogger'],
+    url='https://github.com/gisinc/ArcPyLogger',
+    license='MIT',
+    author='cfricke, carnold',
+    author_email='cfricke@gisinc.com, carnold@gisinc.com',
+    maintainer='cfricke, carnold',
+    maintainer_email='cfricke@gisinc.com, carnold@gisinc.com',
+    description='Classes to define ArcPy message stream handlers and a helper function for us noobs.'
+)
