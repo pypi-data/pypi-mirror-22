5a0c5bddf53131a04fe61311daf16ca6a306822b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:20 +0000

    Add package sssss_nester/1.0.0/sssss_nester-1.0.0.zip

---
+++ packages/sssss_nester/sssss_nester-1.0.0.zip/sssss_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'sssss_nester',
+    version = '1.0.0',
+    py_modules = ['sssss_nester'],
+    author = 'sss',
+    author_email = '',
+ 
+)
