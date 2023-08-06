b215b9ed1b0326d3060d99ee2d1d4f95dbf68b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:34 +0000

    Add package limix/1.0.9/limix-1.0.9-py2-none-any.whl

---
+++ packages/limix/limix-1.0.9-py2-none-any.whl/limix/varDecomp/__init__.py
@@ -0,0 +1,14 @@
+r"""
+*****************************
+Variance Decomposition Module
+*****************************
+
+- :class:`.VarianceDecomposition`
+
+Public interface
+^^^^^^^^^^^^^^^^
+"""
+
+from .varianceDecomposition import VarianceDecomposition
+
+__all__ = ['VarianceDecomposition']
