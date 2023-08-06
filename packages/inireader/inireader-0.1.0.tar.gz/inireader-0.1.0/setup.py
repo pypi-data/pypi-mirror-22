21e7ce46ed487561fb7d1f99d8f19f6bc5981c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:09 +0000

    Add package inireader/0.1.0/inireader-0.1.0.tar.gz

---
+++ packages/inireader/inireader-0.1.0.tar.gz/inireader-0.1.0/setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from distutils.core import setup
+
+
+setup(name='inireader',
+      version='0.1.0',
+      description='ini file reader',
+      author='Aurélien Campéas',
+      author_email='aurelien.campeas@pythonian.fr',
+      url='https://bitbucket.org/pythonian/inireader',
+      packages=['inireader'],
+      package_dir={'inireader': '.'}
+)
+
+
