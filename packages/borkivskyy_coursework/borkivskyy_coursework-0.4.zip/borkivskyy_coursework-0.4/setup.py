6c92b2d07ed233a0d3c3572a24eae833040eabd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:01 +0000

    Add package borkivskyy_coursework/0.4/borkivskyy_coursework-0.4.zip

---
+++ packages/borkivskyy_coursework/borkivskyy_coursework-0.4.zip/borkivskyy_coursework-0.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'borkivskyy_coursework',
+  packages = ['borkivskyy_coursework'], # this must be the same as the name above
+  version = '0.4',
+  description = 'Anton Borkivskyy\'s coursework',
+  author = 'Anton Borkivskyy',
+  author_email = 'borkivskyy@ucu.edu.ua',
+  url = 'https://github.com/AntonBorkivskyy/coursework', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.4.tar.gz', # I'll explain this in a second
+  keywords = ['coursework'], # arbitrary keywords
+  classifiers = [],
+  install_requires=['itunespy==1.5.3']
+)
