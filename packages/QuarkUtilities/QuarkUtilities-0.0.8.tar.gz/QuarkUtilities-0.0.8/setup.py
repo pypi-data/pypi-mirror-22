31da16afcb1f838bde3056143c73514be0f1b453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:12 +0000

    Add package QuarkUtilities/0.0.8/QuarkUtilities-0.0.8.tar.gz

---
+++ packages/QuarkUtilities/QuarkUtilities-0.0.8.tar.gz/QuarkUtilities-0.0.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+setup(
+    name='QuarkUtilities',
+    version="0.0.8",
+    license='BSD',
+    author='Ali Arda Orhan',
+    author_email='arda.orhan@dogantv.com.tr',
+    description='Quark-CMS Utilities.',
+    long_description=__doc__,
+    packages=['quark_utilities'],
+    platforms='any',
+    install_requires=[
+        'FlaskyTornado==0.0.28',
+        'PyJWT==1.4.2',
+        'jsonschema==2.6.0',
+        'pymongo==3.4.0',
+        'user_agents==1.1.0'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
