a41f8cd4610c193d494c8be2666321ac8e4174a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:05 +0000

    Add package daovoice-sdk/0.0.1/daovoice-sdk-0.0.1.tar.gz

---
+++ packages/daovoice-sdk/daovoice-sdk-0.0.1.tar.gz/daovoice-sdk-0.0.1/daovoice/api_operations.py
@@ -0,0 +1,16 @@
+class Get(object):
+    def get(self, **params):
+        url = self.resource_url(params.get("id"))
+
+        result = self.client._result(self.client._get(url))
+
+        return self.resource_class.from_resp_json(result)
+
+
+class SimpleAll(object):
+    def all(self, **kwargs):
+        url = self.resource_url()
+
+        result = self.client._result(self.client._get(url))
+
+        return self.resource_class.from_resp_json(result)
