d582017f9a0552368f08cdfd6582c4c010b264f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:07 +0000

    Add package django-appregistration/0.0.4/django-appregistration-0.0.4.tar.gz

---
+++ packages/django-appregistration/django-appregistration-0.0.4.tar.gz/django-appregistration-0.0.4/setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import logging
+
+__author__ = 'Tim Schneider <tim.schneider@northbridge-development.de>'
+__copyright__ = "Copyright 2016, Northbridge Development Konrad & Schneider GbR"
+__credits__ = ["Tim Schneider", ]
+__maintainer__ = "Tim Schneider"
+__email__ = "mail@northbridge-development.de"
+__status__ = "Release"
+
+logger = logging.getLogger(__name__)
+
+
+import os
+from setuptools import setup, find_packages
+
+long_description = ''
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-appregistration',
+    version='0.0.4',
+    packages=find_packages(exclude=['*.tests',]),
+    include_package_data=True,
+    install_requires=['Django >=1.6',],
+    license='MIT License',
+    description='Dynamic registration between django apps.',
+    long_description=long_description,
+    url='http://github.com/NB-Dev/django-appregistration',
+    author='Nothrbridge Development Konrad & Schneider GbR',
+    author_email='mail@nb-dev.de',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
+
