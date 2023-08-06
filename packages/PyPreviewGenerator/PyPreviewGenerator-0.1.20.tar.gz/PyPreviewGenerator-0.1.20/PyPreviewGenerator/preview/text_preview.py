261c0b2f72d66a22801b67c4e041ba5d88e246dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:12 +0000

    Add package PyPreviewGenerator/0.1.20/PyPreviewGenerator-0.1.20.tar.gz

---
+++ packages/PyPreviewGenerator/PyPreviewGenerator-0.1.20.tar.gz/PyPreviewGenerator-0.1.20/PyPreviewGenerator/preview/text_preview.py
@@ -0,0 +1,26 @@
+import typing
+from PyPreviewGenerator import file_converter
+from PyPreviewGenerator.preview.generic_preview import OnePagePreviewBuilder
+
+
+class TextPreviewBuilder(OnePagePreviewBuilder):
+    mimetype = ['text/plain']
+
+    def build_text_preview(self, file_path: str, preview_name: str,
+                           cache_path: str, page_id: int = 0,
+                           extension: str = '.txt') -> None:
+        """
+        generate the text preview
+        """
+        with open(file_path, 'rb') as txt:
+            result = file_converter.txt_to_txt(
+                txt)  # type: typing.IO[typing.Any]
+            with open('{path}{extension}'.format(
+                    path=cache_path + preview_name,
+                    extension=extension
+            ),
+                    'wb') as jpeg:
+                buffer = result.read(1024)
+                while buffer:
+                    jpeg.write(buffer)
+                    buffer = result.read(1024)
