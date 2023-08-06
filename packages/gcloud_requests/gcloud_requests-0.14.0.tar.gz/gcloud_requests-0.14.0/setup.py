aae9e9594fa25a3ed34ef526932bec59786774bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:18 +0000

    Add package gcloud_requests/0.14.0/gcloud_requests-0.14.0.tar.gz

---
+++ packages/gcloud_requests/gcloud_requests-0.14.0.tar.gz/gcloud_requests-0.14.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+from gcloud_requests import __version__
+
+setup(
+    name="gcloud_requests",
+    description="Thread-safe client functionality for gcloud-python via requests.",
+    long_description="""For documentation and usage examples, see the project on GitHub_.
+
+    .. _GitHub: https://github.com/leadpages/gcloud_requests""",
+    version=__version__,
+    url="https://github.com/leadpages/gcloud_requests",
+    author="Leadpages",
+    author_email="opensource@leadpages.net",
+    license="MIT",
+    packages=["gcloud_requests"],
+    install_requires=[
+        "requests>=2.9,<3"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7"
+    ]
+)
