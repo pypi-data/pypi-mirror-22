88385cc8694c8579a91537981f69094ed5ae8a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:51 +0000

    Add package faucet/1.5.2/faucet-1.5.2.tar.gz

---
+++ packages/faucet/faucet-1.5.2.tar.gz/faucet-1.5.2/docker/runtests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "================= Starting OVS =================="
+service openvswitch-switch start
+
+cd /faucet-src/tests
+
+echo "========== Running faucet config tests =========="
+./test_config.py || exit 1
+./test_check_config.py || exit 1
+
+echo "=========== Running faucet unit tests ==========="
+./test_valve.py || exit 1
+
+echo "=========== Running faucet system tests ==========="
+./faucet_mininet_test.py -c
+time ./faucet_mininet_test.py $FAUCET_TESTS || exit 1
