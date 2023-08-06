e659c79b2d4a1cab5f924a97470474c0d1415c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:40 +0000

    Add package joesnester/1.1.0/joesnester-1.1.0.zip

---
+++ packages/joesnester/joesnester-1.1.0.zip/joesnester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'joesnester',
+version = '1.1.0',
+py_modules = ['nester'],
+author = 'joe',
+author_email = '83727743@qq.com',
+url = '',
+description = 'A simple printer of nested lists',
+)
