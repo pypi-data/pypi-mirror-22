46d94641e15903012546d62333b96e99369a27ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:06 +0000

    Add package dgcli/0.0.1/dgcli-0.0.1.tar.gz

---
+++ packages/dgcli/dgcli-0.0.1.tar.gz/dgcli-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+  name = 'dgcli',
+  packages = ['dgcli'],
+  version = '0.0.1',
+  description = 'Command Line Interface for Deep Generator',
+  author = 'Deep Generator Inc',
+  author_email = 'contact@deepgenerator.com',
+  keywords = ['deepgenerator', 'cli', 'deep learning', 'infrastructure'],
+  classifiers = [],
+)
