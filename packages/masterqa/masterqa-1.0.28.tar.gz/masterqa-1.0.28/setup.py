ede50e3b8032deab29f50b4c63546a3f4f3b96e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:54 +0000

    Add package masterqa/1.0.28/masterqa-1.0.28.tar.gz

---
+++ packages/masterqa/masterqa-1.0.28.tar.gz/masterqa-1.0.28/setup.py
@@ -0,0 +1,24 @@
+"""
+The setup package to install MasterQA requirements
+"""
+
+from setuptools import setup, find_packages  # noqa
+
+setup(
+    name='masterqa',
+    version='1.0.28',
+    url='http://masterqa.com',
+    author='Michael Mintz',
+    author_email='@mintzworld',
+    maintainer='Michael Mintz',
+    description='Automation-Assisted Manual Testing - http://masterqa.com',
+    license='The MIT License',
+    install_requires=[
+        'seleniumbase>=1.3.22',
+        'flake8==3.3.0',
+        ],
+    packages=['masterqa'],
+    entry_points={
+        'nose.plugins': []
+        }
+    )
