7476e59e56f323bb606d079fa9fbd97dfd523341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:01 +0000

    Add package regexp/0.1/regexp-0.1.tar.gz

---
+++ packages/regexp/regexp-0.1.tar.gz/regexp-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'regexp',
+  packages = ['regexp'],
+  version = '0.1',
+  description = 'Python "re" shortcut library',
+  author = 'Fyodor Sizov',
+  author_email = 'fedorsarbeit296@gmail.com',
+  url = 'https://github.com/prodotiscus/regexp',
+  download_url = 'https://github.com/prodotiscus/regexp/archive/0.1.tar.gz',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
