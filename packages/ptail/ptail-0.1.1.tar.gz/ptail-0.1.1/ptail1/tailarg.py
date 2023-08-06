32441e037a7c10b10ac9881bae0f50e3db627824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:21 +0000

    Add package ptail/0.1.1/ptail-0.1.1.tar.gz

---
+++ packages/ptail/ptail-0.1.1.tar.gz/ptail-0.1.1/ptail1/tailarg.py
@@ -0,0 +1,54 @@
+import argparse
+import sys
+
+APP_DESC="""
+命令:ptail1
+"""
+
+print(APP_DESC)
+if len(sys.argv) == 1:
+    sys.argv.append('--help')
+parser = argparse.ArgumentParser()
+parser.add_argument('-f',action="store_true",help='实时更新')
+parser.add_argument('-n','-N',type=int,default=10,help='输出最后几行')
+parser.add_argument('file',)
+args = parser.parse_args()
+
+print(args.file)
+print(args.n)
+print(args.f)
+
+def ptail():
+    with open(args.file, 'rt') as f:
+        data = f.readlines()
+        for x in data[0 - args.n:]:
+            print(x, end='')
+
+if __name__ == "__main__":
+    ptail()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     if len(sys.argv) < 2:
+#         print('illegal!')
+#     elif len(sys.argv) == 2:
+#         ptail1(parone=sys.argv[1], parthree=10)
+#     elif len(sys.argv) == 3:
+#         ptail1(parone=sys.argv[1], partwo=sys.argv[2])
+#     elif len(sys.argv) == 4:
+#         if sys.argv[2] == '-N' or '-n':
+#             ptail1(parone=sys.argv[1], parthree=int(sys.argv[3]))
