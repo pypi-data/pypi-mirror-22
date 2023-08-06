246f1000088120fe0fa35e4055e6a90c40e0ee5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:42 +0000

    Add package fbd/0.0.1b12/fbd-0.0.1b12.tar.gz

---
+++ packages/fbd/fbd-0.0.1b12.tar.gz/fbd-0.0.1b12/fbd/__init__.py
@@ -0,0 +1,6 @@
+from fbd import tools
+from fbd.gatherer import Gatherer
+from fbd.storage import Storage
+from fbd.visualizer import Visualizer
+
+__all__ = ['Gatherer', 'Storage', 'Visualizer', 'tools']
---
+++ packages/fbd/fbd-0.0.1b12.tar.gz/fbd-0.0.1b12/fbd/tools.py
@@ -0,0 +1,35 @@
+# STL imports
+import datetime
+
+# Package imports
+import geocoder
+
+# Project imports
+from .storage import Topic
+
+LAT_PER_100M = 0.001622 / 1.8
+LONG_PER_100M = 0.005083 / 5.5
+
+
+def lat_from_met(met):
+    return LAT_PER_100M * float(met) / 100.0
+
+
+def lon_from_met(met):
+    return LONG_PER_100M * float(met) / 100
+
+
+def get_coords(city):
+    # with open('google_api.json', 'r') as f:
+    #     key = json.load(f)['key']
+    return geocoder.google(city).latlng
+
+
+def default_json_serializer(obj):
+    """JSON serializer for objects not supported by the default json package"""
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    if isinstance(obj, Topic):
+        return obj.to_dict()
+    raise TypeError('{} type could not be serialized.'
+                    .format(type(obj)))
