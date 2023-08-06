4f69a29977009bfe02e9547be893b020c611d6a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:18 +0000

    Add package pyplanet/0.4.0/pyplanet-0.4.0.tar.gz

---
+++ packages/pyplanet/pyplanet-0.4.0.tar.gz/pyplanet-0.4.0/tests/_scripts/travis_dedicated_start.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Check if we should run at all
+if [[ ${TOXENV} == *"integration"* ]]; then
+  echo "Integration test requires the dedicated server.. Starting..."
+else
+  exit 0
+fi
+
+BEFORE_PWD=`pwd`
+cd "$(dirname "$0")"
+cd ../../dedicated
+
+# Start the dedicated server.
+./ManiaPlanetServer /dedicated_cfg=dedicated_cfg.txt /title=TMCanyon /game_settings=MatchSettings/matchsettings_1.txt /login=${MP_USER} /password=${MP_PASS} /loadcache
+# Make sure we exit with non-zero code on error
+if [ $? -ne 0 ]; then
+  echo "Starting failed!"
+  exit 10
+fi
+
+# Sleep 10 seconds to let the server boot.
+sleep 10
+
+##########
+cd $BEFORE_PWD
