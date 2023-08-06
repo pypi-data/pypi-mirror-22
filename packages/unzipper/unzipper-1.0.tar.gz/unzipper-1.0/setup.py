d6f7bebbc7c20f9ce530c2a1e622e92f71cda99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:49 +0000

    Add package unzipper/1.0/unzipper-1.0.tar.gz

---
+++ packages/unzipper/unzipper-1.0.tar.gz/unzipper-1.0/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools  import setup
+
+
+setup(
+      name='unzipper',
+      packages=['unzipper'],
+      version='1.0',
+      description='',
+      license = 'MIT',
+      author='Sarah Wooller',
+      author_email='s.k.wooller@sussex.ac.uk',
+      url='https://github.com/SarahWooller/',
+      download_url=('https://github.com/SarahWooller/unzipper/archive/0.1.tar.gz'),
+      keywords=['zip', 'unzip'],
+      classifiers=[],
+      install_requires=[],
+      setup_requires=[],
+      tests_require=[],
+      zip_safe = False
+      )
+
---
+++ packages/unzipper/unzipper-1.0.tar.gz/unzipper-1.0/unzipper/__init__.py
@@ -0,0 +1,32 @@
+import zipfile
+import os
+import re
+import subprocess
+
+"""
+zipped files are sometimes automatically given a name relating to an id. 
+However, when the directory is unzipped, that information is lost. Therefore 
+this script is used to unzip a zipped file putting the files into directory with the same name.
+Can be used from command line"""
+
+directory = input('name of directory with files in')
+
+zipper = r'(.*).zip'
+for name in os.listdir(directory):
+    try:
+        #extract the student id
+        student_id = re.match(zipper,name).group(1)+'/'
+        #make a directory named student id
+        p=subprocess.Popen(['mkdir', directory+student_id])
+        #wait to finish that before proceeding
+        p.communicate()
+        #move the file to the new directory
+        p=subprocess.Popen(['mv',directory+name,directory+student_id+name])
+        #wait again
+        p.communicate()
+        #extract all the information
+        with zipfile.ZipFile(directory+student_id+name, "r") as z:
+            z.extractall(directory+student_id)
+    #don't do anything with none zip files (which throw an AttributeError for regex)
+    except AttributeError:
+        pass
