32999680e713e57040e4f6688bf1cba47eca83a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:18 +0000

    Add package mosfit/0.6.0/mosfit-0.6.0.tar.gz

---
+++ packages/mosfit/mosfit-0.6.0.tar.gz/mosfit-0.6.0/mosfit/__init__.py
@@ -0,0 +1,11 @@
+"""MOSFiT: Modular light curve fitting software."""
+from . import constants  # noqa: F401
+from . import fitter  # noqa: F401
+from . import model  # noqa: F401
+from . import plotting  # noqa: F401
+from . import printer  # noqa: F401
+from . import utils  # noqa: F401
+
+__version__ = '0.6.0'
+__author__ = 'James Guillochon & Matt Nicholl'
+__license__ = 'MIT'
