345da1c94d6cefe9d9d70fdb5a9100cd484a8b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:30 +0000

    Add package FileSorter/0.1.8.2/FileSorter-0.1.8.2.tar.gz

---
+++ packages/FileSorter/FileSorter-0.1.8.2.tar.gz/FileSorter-0.1.8.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+setup(
+    name="FileSorter",
+    version = "0.1.8.2",
+    packages=find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=['docutils>=0.3', 'fire>=0.1.0', 'simplejson>=3.10.0'],
+
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.json']
+    },
+    entry_points={
+        'console_scripts': [
+            'file_sort=file_sort:main'
+        ]
+    },
+
+    # metadata for upload to PyPI
+    author="Daniel Alexander Ross Evans",
+    author_email="me@danielarevans.com",
+    long_description=open('README.txt').read(),
+    license="MIT",
+    keywords="files organizer organize file management",
+    url="http://github.com/migetman9/file_sort"
+)
