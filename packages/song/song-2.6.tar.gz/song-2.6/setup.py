9c8bd05a8b842d79428140ccaaf21c2ed7dfa3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:06 +0000

    Add package song/2.6/song-2.6.tar.gz

---
+++ packages/song/song-2.6.tar.gz/song-2.6/setup.py
@@ -0,0 +1,40 @@
+from codecs import open
+from os.path import abspath, dirname, join
+from subprocess import call
+
+from setuptools import Command, find_packages, setup
+from song import __version__ as VERSION
+this_dir = abspath(dirname(__file__))
+with open(join(this_dir, 'README.rst'), encoding='utf-8') as file:
+    long_description = file.read()
+
+
+setup(
+	name = 'song',
+	version = VERSION,
+	author = 'ankit mathur',
+	author_email = 'ankitmathur.dtu@gmail.com',
+	license = 'UNLICENSE',
+	description = ' A script to download hindi and punjabi songs from internet',
+	long_description = long_description,
+	url = 'https://github.com/ankitmathur3193/song-cli',
+	keywords = 'song download script',
+	packages = find_packages(exclude=['docs', 'tests*']),
+	install_requires = ['docopt','requests','BeautifulSoup','tqdm'],
+	classifiers = [
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+	entry_points = {
+        'console_scripts': [
+            'song=song.cli:main',
+        ],
+    },
+
+	)	
---
+++ packages/song/song-2.6.tar.gz/song-2.6/song/cli.py
@@ -0,0 +1,35 @@
+"""
+song
+ 
+Usage:
+  song -d TEXT...
+  song -h | --help
+  song --version
+ 
+Options:
+  -h --help                         Show this screen.
+  --version                         Show version.
+ 
+Examples:
+  song -d wo lamhe
+ 
+Help:
+  For help using this tool, please open an issue on the Github repository:
+  https://github.com/ankitmathur3193/song-cli
+"""
+from inspect import getmembers, isclass
+
+from docopt import docopt
+
+from . import __version__ as VERSION
+
+def main():
+    """Main CLI entrypoint."""
+    #print VERSION
+    from commands.download import Download
+    options = docopt(__doc__, version=VERSION)
+    #print "You reached here"
+    #print options
+    
+    p=Download(options)
+    p.run()
