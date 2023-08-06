3a770e8ff21bb90f0e316b82578c1bad28a74cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:30 +0000

    Add package pygost/3.2/pygost-3.2.tar.gz

---
+++ packages/pygost/pygost-3.2.tar.gz/pygost-3.2/pygost/stubs/pygost/gost28147_mac.pyi
@@ -0,0 +1,22 @@
+from pygost.iface import PEP247
+
+
+class MAC(PEP247):
+    def __init__(
+        self,
+        key: bytes,
+        data: bytes=...,
+        iv: bytes=...,
+        sbox: str=...,
+    ) -> None: ...
+
+    @property
+    def digest_size(self) -> int: ...
+
+    def copy(self) -> "MAC": ...
+
+    def update(self, data: bytes) -> None: ...
+
+    def digest(self) -> bytes: ...
+
+    def hexdigest(self) -> str: ...
