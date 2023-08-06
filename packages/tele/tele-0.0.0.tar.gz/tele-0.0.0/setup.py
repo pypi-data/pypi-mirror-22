2b0039630dbc44ec1789339d4ca504f4084b4782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:35 +0000

    Add package tele/0.0.0/tele-0.0.0.tar.gz

---
+++ packages/tele/tele-0.0.0.tar.gz/tele-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='tele',
+  version='0.0.0',
+  packages=find_packages(),
+  author='Aiden Nibali',
+  description='Telemetry for PyTorch',
+)
