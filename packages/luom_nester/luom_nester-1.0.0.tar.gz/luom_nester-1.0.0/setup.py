b95bdaeef295be21197f4ed3efe877a4f8f376be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:08 +0000

    Add package luom_nester/1.0.0/luom_nester-1.0.0.tar.gz

---
+++ packages/luom_nester/luom_nester-1.0.0.tar.gz/luom_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'luom_nester',
+    version         = '1.0.0',
+    py_modules      = ['luom_nester'],
+    author          = 'luomeng',
+    author_email    ='sober_lm@126.com',    
+    url		   ='http://www.test.com',
+    description     ='A simple printer of nested lists',
+    )
