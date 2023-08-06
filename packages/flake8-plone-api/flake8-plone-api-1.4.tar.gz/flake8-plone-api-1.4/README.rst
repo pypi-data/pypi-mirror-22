5028727f2454f1315a9bf27112b4d96557e0f645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:50 +0000

    Add package flake8-plone-api/1.4/flake8-plone-api-1.4.tar.gz

---
+++ packages/flake8-plone-api/flake8-plone-api-1.4.tar.gz/flake8-plone-api-1.4/README.rst
@@ -0,0 +1,29 @@
+.. -*- coding: utf-8 -*-
+
+.. image:: https://travis-ci.org/gforcada/flake8-plone-api.svg?branch=master
+   :target: https://travis-ci.org/gforcada/flake8-plone-api
+
+.. image:: https://coveralls.io/repos/gforcada/flake8-plone-api/badge.svg?branch=master&service=github
+   :target: https://coveralls.io/github/gforcada/flake8-plone-api?branch=master
+
+Flake8 Plone API plugin
+=======================
+Checks for code usages that can be replaced with Plone API method calls.
+
+`See the current replacements (on DATA variable at the bottom) <https://github.com/gforcada/flake8-plone-api/blob/master/flake8_plone_api.py>`_
+and `propose new ones / fix existing ones <https://github.com/gforcada/flake8-plone-api/pulls>`_.
+
+Install
+-------
+Install with pip::
+
+    $ pip install flake8-plone-api
+
+Requirements
+------------
+- Python 2.7, 3.5
+- flake8
+
+License
+-------
+GPL 2.0
---
+++ packages/flake8-plone-api/flake8-plone-api-1.4.tar.gz/flake8-plone-api-1.4/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+short_description = \
+    'Checks for code usages that can be replaced with Plone API method calls.'
+
+
+long_description = '{0}\n{1}'.format(
+    open('README.rst').read(),
+    open('CHANGES.rst').read()
+)
+
+setup(
+    name='flake8-plone-api',
+    version='1.4',
+    description=short_description,
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: 5.0',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+    ],
+    keywords='pep8 flake8 plone',
+    author='Gil Forcada',
+    author_email='gil.gnome@gmail.com',
+    url='https://github.com/gforcada/flake8-plone-api',
+    license='GPL version 2',
+    py_modules=['flake8_plone_api', ],
+    include_package_data=True,
+    test_suite='run_tests',
+    zip_safe=False,
+    install_requires=[
+        'flake8',
+    ],
+    entry_points={
+        'flake8.extension': [
+            'P001 = flake8_plone_api:PloneAPIChecker',
+        ],
+    },
+)
