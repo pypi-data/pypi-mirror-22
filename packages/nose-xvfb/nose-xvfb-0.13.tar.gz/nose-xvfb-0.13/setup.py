f6f1eb9f3df18f161049a08ac92e1e970a660c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:20 +0000

    Add package nose-xvfb/0.13/nose-xvfb-0.13.tar.gz

---
+++ packages/nose-xvfb/nose-xvfb-0.13.tar.gz/nose-xvfb-0.13/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+requires = ['nose', 'xvfbwrapper==0.2.9']
+
+setup(
+    name='nose-xvfb',
+    description='Virtual display nose plugin through Xvfb',
+    version='0.13',
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Testing",
+    ],
+    entry_points={'nose.plugins.0.10': ['nosexvfb = nosexvfb:Xvfb']},
+    install_requires=requires,
+    packages=find_packages(),
+    url='https://github.com/grupotaric/nose-xvfb',
+    author='Javier Santacruz',
+    author_email='javier.santacruz.lc@gmail.com',
+    keywords=['testing', 'nose', 'xvfb', 'x.org']
+)
