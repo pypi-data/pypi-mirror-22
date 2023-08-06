efe988138d669433b2be46973f840b6732dcda9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:29 +0000

    Add package limix/1.0.7/limix-1.0.7.tar.gz

---
+++ packages/limix/limix-1.0.7.tar.gz/limix-1.0.7/limix/io/util.py
@@ -0,0 +1,13 @@
+def file_type(filepath):
+    imexts = ['.png', '.bmp', '.jpg', 'jpeg']
+    if filepath.endswith('.hdf5') or filepath.endswith('.h5'):
+        return 'hdf5'
+    if filepath.endswith('.csv'):
+        return 'csv'
+    if filepath.endswith('.grm.raw'):
+        return 'grm.raw'
+    if filepath.endswith('.npy'):
+        return 'npy'
+    if any([filepath.endswith(ext) for ext in imexts]):
+        return 'image'
+    return 'unknown'
---
+++ packages/limix/limix-1.0.7.tar.gz/limix-1.0.7/limix/plot/__init__.py
@@ -0,0 +1,35 @@
+r"""
+******************
+Plotting utilities
+******************
+
+Manhattan
+^^^^^^^^^
+
+.. autofunction:: limix.plot.plot_manhattan
+
+QQ plot
+^^^^^^^
+
+.. autofunction:: limix.plot.qqplot
+
+Kinship
+^^^^^^^
+
+.. autofunction:: limix.plot.plot_kinship
+
+Normal distribution
+^^^^^^^^^^^^^^^^^^^
+
+.. autofunction:: limix.plot.plot_normal
+
+"""
+
+from .plot_normal import plot_normal
+from .qqplot import qqplot
+from .manhattan import plot_manhattan
+from .kinship import plot_kinship
+from .image import see_image
+
+__all__ = ['plot_manhattan', 'qqplot', 'plot_normal', 'plot_kinship',
+           'see_image']
