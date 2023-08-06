7c8376f3f11136db00aa49c7909a6a708689c447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:33 +0000

    Add package wfl_nester/1.2.0/wfl_nester-1.2.0.tar.gz

---
+++ packages/wfl_nester/wfl_nester-1.2.0.tar.gz/wfl_nester-1.2.0/wfl_nester.py
@@ -0,0 +1,13 @@
+"""this is nester.py,print the list in recursion"""
+
+def print_lol(the_list,indent=False,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(each_item)
+
+
