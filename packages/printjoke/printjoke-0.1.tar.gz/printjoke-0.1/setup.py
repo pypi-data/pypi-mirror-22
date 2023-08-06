f77bfb362f6b72e818ba0993d18c2baee3722f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:29 +0000

    Add package printjoke/0.1/printjoke-0.1.tar.gz

---
+++ packages/printjoke/printjoke-0.1.tar.gz/printjoke-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+	name="printjoke",
+	version="0.1",
+	description="print no jkoke",
+	author="joker",
+	author_email="joker@gmail.com",
+	license="MIT",
+	packages=["printjoke"],
+	zip_safe=False)
+
