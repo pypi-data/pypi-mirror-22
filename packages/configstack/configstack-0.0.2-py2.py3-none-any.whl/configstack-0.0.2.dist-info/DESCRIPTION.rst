16fb60f654b437804945a817319640b306226f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:22 +0000

    Add package configstack/0.0.2/configstack-0.0.2-py2.py3-none-any.whl

---
+++ packages/configstack/configstack-0.0.2-py2.py3-none-any.whl/configstack-0.0.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,13 @@
+ConfigStack is a Python library for stacking program configuration:
+
+* Command-line options, defined with `argparse`.
+
+* Environment variables, derived from other configuration options.
+
+* Configuration files, defined with `configparser`.
+
+* Default values.
+
+These are all unified at run time, in a single collection of settings
+for the program to use.
+
---
+++ packages/configstack/configstack-0.0.2-py2.py3-none-any.whl/configstack/exceptions.py
@@ -0,0 +1,33 @@
+# configstack/exceptions.py
+# Part of ConfigStack, a library for configuring Python programs.
+#
+# Copyright © 2017 Ben Finney <ben+python@benfinney.id.au>
+#
+# This is free software, and you are welcome to redistribute it under
+# certain conditions; see the end of this file for copyright
+# information, grant of license, and disclaimer of warranty.
+
+""" Custom exception classes. """
+
+
+class ConfigStackException:
+    """ Mix-in class for an exception raised by the `ConfigStack` library. """
+
+
+class LayerNotPopulatedError(ConfigStackException, RuntimeError):
+    """ Exception raised when a layer is accessed before it is populated. """
+
+
+# Copyright © 2017 Ben Finney <ben+python@benfinney.id.au>
+#
+# This is free software: you may copy, modify, and/or distribute this work
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 3 of that license or any later version.
+# No warranty expressed or implied. See the file ‘LICENSE.GPL-3’ for details.
+
+
+# Local variables:
+# coding: utf-8
+# mode: python
+# End:
+# vim: fileencoding=utf-8 filetype=python :
