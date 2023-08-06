6f6fb501b09b68dd27a56f6f2cec4ec929a98a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:54 +0000

    Add package sqlite-db-tools/0.1/sqlite_db_tools-0.1.tar.gz

---
+++ packages/sqlite-db-tools/sqlite_db_tools-0.1.tar.gz/sqlite_db_tools-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sqlite_db_tools',
+    description='Tools for managing sqlite3 databases.',
+    url='https://github.com/Adam_Davenport/sqlite_db_tools',
+    author='Adam Davenport',
+    license='MIT',
+    packages=['sqlite_db_tools'],
+    zip_safe=False,
+    version='0.01'
+    )
