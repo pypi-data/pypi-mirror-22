f5a1f195b268e9fcdf045964c5ceb6f8e31c05d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:25 +0000

    Add package kibitzr/4.0.2b3/kibitzr-4.0.2b3.tar.gz

---
+++ packages/kibitzr/kibitzr-4.0.2b3.tar.gz/kibitzr-4.0.2b3/kibitzr/notifier/custom.py
@@ -0,0 +1,31 @@
+import logging
+
+from ..conf import settings
+
+
+logger = logging.getLogger(__name__)
+NAME = 'python'
+
+
+class PythonNotify(object):
+
+    def __init__(self, conf, value):
+        self.code = value
+        self.context = {
+            'conf': conf,
+            'creds': settings().creds,
+        }
+
+    def __call__(self, report):
+        context = dict(
+            self.context,
+            text=report,  # legacy
+            content=report,
+        )
+        logger.info("Executing Python notifier")
+        logger.debug(self.code)
+        exec(self.code, context)
+
+
+def notify_factory(conf, value):
+    return PythonNotify(conf, value)
