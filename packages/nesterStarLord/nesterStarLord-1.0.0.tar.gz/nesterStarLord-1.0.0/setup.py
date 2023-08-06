d911853834e6a4709b024c99e7f8a41afc4d83ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:34 +0000

    Add package nesterStarLord/1.0.0/nesterStarLord-1.0.0.tar.gz

---
+++ packages/nesterStarLord/nesterStarLord-1.0.0.tar.gz/nesterStarLord-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='nesterStarLord',
+        version='1.0.0',
+        py_modules=['nesterStarLord'],
+        author='starlord',
+        author_email='lokaksha@gmail.com',
+        url='http://www.myweb.com',
+        description='Print nested lists',
+    )
