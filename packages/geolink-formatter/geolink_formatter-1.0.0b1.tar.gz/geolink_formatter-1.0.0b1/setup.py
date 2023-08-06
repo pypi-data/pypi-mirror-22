86a9fcdd07e1582e124a9a3cd8964cdc7a62432b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:59 +0000

    Add package geolink-formatter/1.0.0b1/geolink_formatter-1.0.0b1.tar.gz

---
+++ packages/geolink-formatter/geolink_formatter-1.0.0b1.tar.gz/geolink_formatter-1.0.0b1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'description.rst')) as f:
+    description = f.read()
+
+requires = [
+    'lxml>=3.7.0',
+    'requests'
+]
+
+setup(name='geolink_formatter',
+      version='1.0.0-beta.1',
+      description='Python geoLink Formatter',
+      license='BSD',
+      long_description=description,
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Scientific/Engineering :: GIS",
+          "Topic :: Software Development :: Libraries :: Python Modules"
+      ],
+      author='Karsten Deininger',
+      author_email='karsten.deininger@bl.ch',
+      url='https://gitlab.com/gf-bl/python-geolink-formatter',
+      keywords='oereb lex geolink formatter html',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires
+      )
