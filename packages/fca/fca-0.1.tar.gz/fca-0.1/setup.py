bd896e6570a22ea80f2d1fc2fd2470bd5dd40a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:18 +0000

    Add package fca/0.1/fca-0.1.tar.gz

---
+++ packages/fca/fca-0.1.tar.gz/fca-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'fca',
+  packages = ['fca'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A library to implement FCA tools',
+  author = 'Victor Codocedo',
+  author_email = 'victor.codocedo@gmail.com',
+  url = 'https://github.com/codocedo/fca',
+  download_url = 'https://github.com/codocedo/fca/archive/0.1.tar.gz',
+  keywords = ['formal concept analysis', 'pattern mining', 'add intent', 'data mining'],
+  classifiers = [],
+)
