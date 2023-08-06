725904fa9a291cdab43d41462497eeed55558f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:48 +0000

    Add package opentrons/2.4.2/opentrons-2.4.2-py3.5.egg

---
+++ packages/opentrons/opentrons-2.4.2-py3.5.egg/opentrons/json_importer/__init__.py
@@ -0,0 +1,11 @@
+from opentrons.json_importer.json_importer import (
+    JSONProtocolProcessor,
+    JSONProcessorRuntimeError,
+    JSONProcessorValidationError
+)
+
+__all__ = [
+    JSONProtocolProcessor,
+    JSONProcessorRuntimeError,
+    JSONProcessorValidationError
+]
