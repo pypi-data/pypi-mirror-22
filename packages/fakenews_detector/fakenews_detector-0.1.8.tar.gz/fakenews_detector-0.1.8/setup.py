38ec8a7fb5ba6b24572299ad52adaeb79ca1e6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:13 +0000

    Add package fakenews_detector/0.1.8/fakenews_detector-0.1.8.tar.gz

---
+++ packages/fakenews_detector/fakenews_detector-0.1.8.tar.gz/fakenews_detector-0.1.8/setup.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist upload')  # bdist_wininst
+    sys.exit()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='fakenews_detector',
+    packages=['fakenews_detector'],
+    version='0.1.8',
+    description='Detect fake and get information about the source.',
+    author='Mieszko Makuch',
+    author_email='mmakuch@googlemail.com',
+    url='https://github.com/',
+    download_url='https://mieszko_makuch@bitbucket.org/mieszko_makuch/fakenews_detector.git',
+    keywords=['fakenews', 'fake', 'news', 'detector'],
+    classifiers=[],
+    package_data={'fakenews_detector': ['json_local/*', 'json_local/opensources/*']},
+    include_package_data=True,
+    install_requires=required,
+    zip_safe=False
+)
