493e54ffb7a47652f6b31842bf5b7e6674daf9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:40 +0000

    Add package ntfy/2.4.4/ntfy-2.4.4-py2.py3-none-any.whl

---
+++ packages/ntfy/ntfy-2.4.4-py2.py3-none-any.whl/ntfy/backends/default.py
@@ -0,0 +1,23 @@
+from sys import platform
+from importlib import import_module
+
+
+class DefaultNotifierError(Exception):
+    def __init__(self, exception, module):
+        self.exception = exception
+        self.module = module
+
+
+def notify(title, message, **kwargs):
+    """
+    This backend automatically selects the correct desktop notification backend
+    for your operating system.
+    """
+    for os in ['linux', 'win32', 'darwin']:
+        if platform.startswith(os):
+            module = import_module('ntfy.backends.{}'.format(os))
+            try:
+                module.notify(title=title, message=message, **kwargs)
+            except Exception as e:
+                raise DefaultNotifierError(e, module)
+            break
