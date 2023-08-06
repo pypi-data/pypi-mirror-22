11db0857f0d64e122414b5dfc5ae4c6dcc5e731e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:22 +0000

    Add package clubsandwich/0.1.1/clubsandwich-0.1.1.tar.gz

---
+++ packages/clubsandwich/clubsandwich-0.1.1.tar.gz/clubsandwich-0.1.1/clubsandwich/babysit.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+This program is added to your ``$PATH`` when you install clubsandwich.
+It repeatedly runs a command until you ctrl+c the ``babysit`` process.
+
+Imagine you're working on a flame effect for your main screen. It takes
+a lot of tweaking. Every time you make a change, you go to run your game::
+
+  python flamegame.py
+
+This is annoying. With ``babysit``, you can make it continuously relaunch
+every time you quit, so as soon as you exit you see the latest code running
+in seconds::
+
+  babysit python flamegame.py
+
+It will keep relaunching until you ctrl+c the ``babysit`` process.
+
+Relaunches are never more often than 5 seconds, in case you've got a
+crash-on-launch bug.
+"""
+import sys
+import subprocess
+import time
+from math import floor
+
+cmd = sys.argv[1:]
+
+message = '< NEW BABYSIT SESSION (Ctrl+C to stop) >'
+left_padding = floor((78 - len(message)) / 2)
+right_padding = 78 - len(message) - left_padding
+
+def cli():
+  if len(sys.argv) == 1 or len(sys.argv) > 1 and sys.argv[1] in ('-h', '--help'):
+    print(__doc__.strip())
+    return
+
+  cont = True
+  last_time = time.time()
+  while cont:
+    print('-' * left_padding + message + '-' * right_padding, file=sys.stderr)
+    p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+    try:
+      p.wait()
+      time.sleep(max(0, 5 - (time.time() - last_time)))
+      last_time = time.time()
+    except KeyboardInterrupt:
+      cont = False
---
+++ packages/clubsandwich/clubsandwich-0.1.1.tar.gz/clubsandwich-0.1.1/clubsandwich/ui/ui_scene.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from bearlibterminal import terminal
+from clubsandwich.blt.state import blt_state
+from clubsandwich.director import Scene
+from clubsandwich.geom import Size
+from clubsandwich.ui import (
+    FirstResponderContainerView,
+)
+
+
+class UIScene(Scene):
+  """
+  :param list|View views: One or more subviews of the root view
+
+  See :class:`~clubsandwich.director.Scene` for the other args.
+
+  Scene that renders a view hierarchy inside a
+  :py:class:`FirstResponderContainerView`.
+
+  Log the view hierarchy by pressing the backslash key at any time.
+  """
+
+  def __init__(self, views, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+    if not isinstance(views, list):
+        views = [views]
+
+    self.view = FirstResponderContainerView(subviews=views, scene=self)
+    self.add_terminal_reader(self.view)
+
+  def terminal_read(self, val):
+    super().terminal_read(val)
+    if val == terminal.TK_BACKSLASH:
+        self.view.debug_print()
+
+  def terminal_update(self, is_active=False):
+    terminal.bkcolor('#000000')
+    self.view.frame = self.view.frame.with_size(
+        Size(blt_state.width, blt_state.height))
+    self.view.perform_layout()
+    self.view.perform_draw(self.ctx)
+    terminal.bkcolor('#000000')
