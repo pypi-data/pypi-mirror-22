cfffe3451a3df57736d9f8ce649b1ab9c9b1feb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:02 +0000

    Add package onyxerp/1.4.0/onyxerp-1.4.0-py2.py3-none-any.whl

---
+++ packages/onyxerp/onyxerp-1.4.0-py2.py3-none-any.whl/onyxerp/core/services/auth_service.py
@@ -0,0 +1,24 @@
+from core.api.request import Request
+from core.services.onyxerp_service import OnyxErpService
+
+
+class AuthService(Request, OnyxErpService):
+
+    jwt = None
+
+    def __int__(self, base_url):
+        super(AuthService, self).__init__(base_url)
+
+    def auth(self):
+        response = self.post("/v1/auth/")
+
+        status = response.get_status_code()
+        data = response.get_decoded()
+
+        if status == 200 or status == 401:
+            return data['access_token']
+        else:
+            return {
+                "status": status,
+                "response": response.get_content()
+            }
