07fb83ba9cee2f62b2d078938fe1b8abdeae452d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:06 +0000

    Add package qbit/0.1.4/qbit-0.1.4.tar.gz

---
+++ packages/qbit/qbit-0.1.4.tar.gz/qbit-0.1.4/qbit/__init__.py
@@ -0,0 +1,10 @@
+from client import client, quadprog
+from qbit_pb2 import (MathRequest, CompareRequest, QuadraticRequest,
+                      QuadprogRequest, GetOperationRequest,
+                      CancelOperationRequest, ListOperationsRequest)
+
+__all__ = [
+    'client', 'quadprog', 'MathRequest', 'CompareRequest', 'QuadraticRequest',
+    'QuadprogRequest', 'GetOperationRequest', 'CancelOperationRequest',
+    'ListOperationsRequest'
+]
