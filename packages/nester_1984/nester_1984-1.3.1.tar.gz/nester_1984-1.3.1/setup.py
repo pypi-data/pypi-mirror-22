c2eeeb792f7ac3c39f4184c1e20919eb45e61073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:45 +0000

    Add package nester_1984/1.3.1/nester_1984-1.3.1.tar.gz

---
+++ packages/nester_1984/nester_1984-1.3.1.tar.gz/nester_1984-1.3.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name    = 'nester_1984',
+        version ='1.3.1',
+        py_modules=['nester'],
+        author  ='hfpython',
+        author_email  ='alvinchang16@gmail.com',
+        url ='',
+        description ='v.1.3.1',
+     )
