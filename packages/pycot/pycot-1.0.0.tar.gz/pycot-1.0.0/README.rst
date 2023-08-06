7020c37d1cbfc7386f8463332b56519a5b73cbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:49 +0000

    Add package pycot/1.0.0/pycot-1.0.0.tar.gz

---
+++ packages/pycot/pycot-1.0.0.tar.gz/pycot-1.0.0/README.rst
@@ -0,0 +1,42 @@
+pycot - Python Cursor on Target Module
+**************************************
+
+Renders and Parses Cursor on Target XML events. Currently a work-in-progress.
+
+
+Installation
+============
+Install from pypi using pip: ``pip install pycot``
+
+
+Build Status
+============
+
+Master:
+
+.. image:: https://travis-ci.org/ampledata/pycot.svg?branch=master
+    :target: https://travis-ci.org/ampledata/pycot
+
+Develop:
+
+.. image:: https://travis-ci.org/ampledata/pycot.svg?branch=develop
+    :target: https://travis-ci.org/ampledata/pycot
+
+
+Source
+======
+Github: https://github.com/ampledata/pycot
+
+Author
+======
+Greg Albrecht oss@undef.net
+
+http://orionlabs.io/
+
+Copyright
+=========
+Copyright 2017 Orion Labs, Inc.
+
+License
+=======
+Apache License, Version 2.0. See LICENSE for details.
---
+++ packages/pycot/pycot-1.0.0.tar.gz/pycot-1.0.0/pycot/classes.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Python Cursor on Target Module Class Definitions."""
+
+import uuid
+
+import gexml
+
+__author__ = 'Greg Albrecht <oss@undef.net>'
+__copyright__ = 'Copyright 2017 Orion Labs, Inc.'
+__license__ = 'Apache License, Version 2.0'
+
+
+class Point(gexml.Model):
+    class meta:
+        tagname = 'point'
+    lat = gexml.fields.Float()
+    lon = gexml.fields.Float()
+    hae = gexml.fields.Float()
+    ce = gexml.fields.Float()
+    le = gexml.fields.Float()
+
+
+class Detail(gexml.Model):
+    class meta:
+        tagname = 'detail'
+
+
+class Event(gexml.Model):
+    class meta:
+        tagname = 'event'
+
+    __str__ = gexml.Model.render
+
+    version = gexml.fields.Float()
+    event_type = gexml.fields.String(attrname='type')
+
+    # FIXME: This should be default=uuid.uuid4(), but declaring a default
+    # also causes required=False, and so won't render correctly.
+    uid = gexml.fields.String()
+
+    time = gexml.fields.DateTime()
+    start = gexml.fields.DateTime(required=False)
+    stale = gexml.fields.DateTime(required=False)
+    point = gexml.fields.Model(Point, required=False)  # FIXME
+    detail = gexml.fields.Model(Detail, required=False)
+    access = gexml.fields.String(required=False)
+    qos = gexml.fields.String(required=False)
+    opex = gexml.fields.String(required=False)
+    how = gexml.fields.String()
+
+
+class EventType(object):
+
+    describes = None
+    type_fields = []
+
+    def __init__(self, event_type=None):
+        self.event_type = event_type
+        split_event = event_type.split('-')
+        d = dict(zip(self.type_fields[0:len(split_event)], split_event))
+        for k, v in d.iteritems():
+            setattr(self, k, v)
+
+    def __str__(self):
+        return self.event_type
+
+
+class AtomEventType(EventType):
+    "a - atoms (anything you drop on your foot), based on MS2525B."
+    describes = 'Thing'
+    type_fields = ['_describes', 'affiliation', 'battle_dimension', 'function']
+
+
+class DataEventType(EventType):
+    describes = 'Data'
+    type_fields = ['_describes', 'dimension']
