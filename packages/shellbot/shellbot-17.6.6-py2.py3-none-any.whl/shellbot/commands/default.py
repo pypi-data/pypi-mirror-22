2ec976800cd54dd1b47ffac0989948841b5d9b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:48 +0000

    Add package shellbot/17.6.6/shellbot-17.6.6-py2.py3-none-any.whl

---
+++ packages/shellbot/shellbot-17.6.6-py2.py3-none-any.whl/shellbot/commands/default.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+
+from .base import Command
+
+
+class Default(Command):
+    """
+    Handles unmatched command
+    """
+
+    keyword = u'*default'
+    information_message = u'Handle unmatched command'
+    is_hidden = True
+
+    default_message = u"Sorry, I do not know how to handle '{}'"
+
+    def execute(self, arguments):
+        """
+        Handles unmatched command
+
+        Arguments provided should include all of the user input, including
+        the first token that has not been recognised as a valid command.
+        """
+        self.bot.say(self.default_message.format(arguments))
