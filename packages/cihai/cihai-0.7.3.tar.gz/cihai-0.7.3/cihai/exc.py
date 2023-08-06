330c2b96c916b81f6808259d499355a75e6f79f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:16 +0000

    Add package cihai/0.7.3/cihai-0.7.3.tar.gz

---
+++ packages/cihai/cihai-0.7.3.tar.gz/cihai-0.7.3/cihai/exc.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 - *-
+"""Exceptions for Cihai.
+
+cihai.exc
+~~~~~~~~~
+
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+
+class CihaiException(Exception):
+    """Base Cihai Exception class."""
