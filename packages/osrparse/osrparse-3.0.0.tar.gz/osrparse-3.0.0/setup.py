5b4577e9a02900f59a734bf6083a24054ff1e9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:42 +0000

    Add package osrparse/3.0.0/osrparse-3.0.0.tar.gz

---
+++ packages/osrparse/osrparse-3.0.0.tar.gz/osrparse-3.0.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+version = '3.0.0'
+
+setup(
+    name = 'osrparse',
+    version = version,
+    description = "Python implementation of osu! rhythm game replay parser.",
+    classifiers = [
+        'Topic :: Games/Entertainment',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords = ['osu!, osr, replay, replays, parsing, parser, python'],
+    author = 'Kevin Lim',
+    author_email = 'kszlim@gmail.com',
+    url = 'https://github.com/kszlim/osu-replay-parser',
+    download_url = 'https://github.com/kszlim/osu-replay-parser/tarball/' + version,
+    license = 'MIT',
+    test_suite="tests",
+    packages = find_packages()
+)
