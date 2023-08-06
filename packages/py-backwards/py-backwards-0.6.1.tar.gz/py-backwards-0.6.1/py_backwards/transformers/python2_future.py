57f2c32a3660a14635e0b9348687d86865c8f277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:10 +0000

    Add package py-backwards/0.6.1/py-backwards-0.6.1.tar.gz

---
+++ packages/py-backwards/py-backwards-0.6.1.tar.gz/py-backwards-0.6.1/py_backwards/transformers/python2_future.py
@@ -0,0 +1,25 @@
+from typed_ast import ast3 as ast
+from ..utils.snippet import snippet
+from .base import BaseNodeTransformer
+
+
+@snippet
+def imports(future):
+    from future import absolute_import
+    from future import division
+    from future import print_function
+
+
+class Python2FutureTransformer(BaseNodeTransformer):
+    """Prepends module with:
+        from __future__ import absolute_import
+        from __future__ import division
+        from __future__ import print_function
+            
+    """
+    target = (2, 7)
+
+    def visit_Module(self, node: ast.Module) -> ast.Module:
+        self._tree_changed = True
+        node.body = imports.get_body(future='__future__') + node.body  # type: ignore
+        return self.generic_visit(node)  # type: ignore
