cb71d4c4d25eb376327772f2155bd23f8814f370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:11 +0000

    Add package lektor-shortcodes/0.1.4/lektor-shortcodes-0.1.4.tar.gz

---
+++ packages/lektor-shortcodes/lektor-shortcodes-0.1.4.tar.gz/lektor-shortcodes-0.1.4/lektor_shortcodes/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from jinja2 import Template
+from lektor.pluginsystem import Plugin
+from lektor.markdown import Markdown
+from markupsafe import Markup
+
+import scodes
+
+
+def shortcode_factory(config):
+    def shortcodes(text, **options):
+        section = options.get("section", "main")
+
+        for item, conf in config.section_as_dict(section).items():
+            @scodes.register(item)
+            def handler(context, content, pargs, kwargs):
+                return Template(conf).render(kwargs)
+
+        parser = scodes.Parser()
+        if isinstance(text, Markdown):
+            text.source = parser.parse(text.source)
+        elif isinstance(text, Markup):
+            text = Markup(parser.parse(text))
+        else:
+            text = parser.parse(text)
+        return text
+    return shortcodes
+
+
+class ShortcodesPlugin(Plugin):
+    name = u'lektor-shortcodes'
+    description = u'Shortcodes for Lektor.'
+
+    def on_setup_env(self, **extra):
+        self.env.jinja_env.filters['shortcodes'] = shortcode_factory(self.get_config())
