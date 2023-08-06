c5316fa8e0661ec196414296658733fd308a3e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:04 +0000

    Add package cfw/0.1.0/cfw-0.1.0.tar.gz

---
+++ packages/cfw/cfw-0.1.0.tar.gz/cfw-0.1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='cfw',
+    version='0.1.0',
+    description='Python Command Line Interface Library',
+
+    author='John Hopper',
+    author_email='john.hopper@jpserver.net',
+
+    packages=find_packages(),
+
+    license='MIT'
+)
