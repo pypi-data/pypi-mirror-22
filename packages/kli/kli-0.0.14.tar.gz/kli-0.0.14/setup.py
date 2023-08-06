9eee2ef8c998ea9c8176f3d6b2adb83a4307eac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:13 +0000

    Add package kli/0.0.14/kli-0.0.14.tar.gz

---
+++ packages/kli/kli-0.0.14.tar.gz/kli-0.0.14/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'kli', 
+    version = '0.0.14', 
+    packages = find_packages(exclude=['venv', 'backup']),
+    license = 'MIT',
+    url = 'https://github.com/tranquilo12/kli',
+    author = 'Shriram Sunder',
+    author_email = 'shriram.sunder121091@gmail.com',
+    install_requires = [
+        'tqdm', 
+        'pprint',
+        'argcomplete',
+        'bs4', 
+        'click>=6', 
+        'cryptography', 
+        'requests',
+        #'lxml',
+        ],    
+    entry_points = '''
+        [console_scripts]
+        kli=kli.main:kli
+    ''',
+    keywords = ['kaggle', 'cli'],
+    #data_files=[('data', ['comp.json']),
+    #            ('data', ['keys.txt'])]
+        )
