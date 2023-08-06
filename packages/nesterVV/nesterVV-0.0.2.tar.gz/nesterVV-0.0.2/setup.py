91b712cf9c511220b8fe212c4a357d75a9f6e4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:51 +0000

    Add package nesterVV/0.0.2/nesterVV-0.0.2.tar.gz

---
+++ packages/nesterVV/nesterVV-0.0.2.tar.gz/nesterVV-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name='nesterVV',
+version='0.0.2',
+py_modules=['nesterVV'],
+author='vivian',
+author_email='vivian@169.com',
+url='http://www.baidu.com',
+description='This is the first'
+)
