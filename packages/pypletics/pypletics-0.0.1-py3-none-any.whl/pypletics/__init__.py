0a09df2fc8ad7fd91e802ff734ff887415125587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:09 +0000

    Add package pypletics/0.0.1/pypletics-0.0.1-py3-none-any.whl

---
+++ packages/pypletics/pypletics-0.0.1-py3-none-any.whl/pypletics/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+'''
+    Le définition de ce package est à initialiser selon les réponses auxquelles 
+    ce dernier répondra !
+'''
+ 
+__version__ = '0.0.1'
+ 
+from pypletics.introduction import introduction
