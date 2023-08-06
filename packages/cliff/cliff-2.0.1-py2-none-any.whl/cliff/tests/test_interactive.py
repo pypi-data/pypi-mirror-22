d8e71c643038b34b935a40bd8bf614b860439ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:40 +0000

    Add package cliff/2.0.1/cliff-2.0.1-py2-none-any.whl

---
+++ packages/cliff/cliff-2.0.1-py2-none-any.whl/cliff/tests/test_interactive.py
@@ -0,0 +1,71 @@
+# -*- encoding: utf-8 -*-
+
+import cmd2
+
+from cliff.interactive import InteractiveApp
+
+
+class FakeApp(object):
+    NAME = 'Fake'
+
+
+def make_interactive_app(*command_names):
+    fake_command_manager = [(x, None) for x in command_names]
+    return InteractiveApp(FakeApp, fake_command_manager,
+                          stdin=None, stdout=None)
+
+
+def _test_completenames(expecteds, prefix):
+    app = make_interactive_app('hips', 'hippo', 'nonmatching')
+    assert set(app.completenames(prefix)) == set(expecteds)
+
+
+def test_cmd2_completenames():
+    # cmd2.Cmd define do_help method
+    _test_completenames(['help'], 'he')
+
+
+def test_cliff_completenames():
+    _test_completenames(['hips', 'hippo'], 'hip')
+
+
+def test_no_completenames():
+    _test_completenames([], 'taz')
+
+
+def test_both_completenames():
+    # cmd2.Cmd define do_history method
+    # NOTE(dtroyer): Before release 0.7.0 do_hi was also defined so we need
+    #                to account for that in the list of possible responses.
+    #                Remove this check after cmd2 0.7.0 is the minimum
+    #                requirement.
+    if hasattr(cmd2.Cmd, "do_hi"):
+        _test_completenames(['hi', 'history', 'hips', 'hippo'], 'hi')
+    else:
+        _test_completenames(['history', 'hips', 'hippo'], 'hi')
+
+
+def _test_completedefault(expecteds, line, begidx):
+    command_names = set(['show file', 'show folder', 'show  long', 'list all'])
+    app = make_interactive_app(*command_names)
+    observeds = app.completedefault(None, line, begidx, None)
+    assert set(observeds) == set(expecteds)
+    assert set([line[:begidx] + x for x in observeds]) <= command_names
+
+
+def test_empty_text_completedefault():
+    # line = 'show ' + begidx = 5 implies text = ''
+    _test_completedefault(['file', 'folder', ' long'], 'show ', 5)
+
+
+def test_nonempty_text_completedefault2():
+    # line = 'show f' + begidx = 6 implies text = 'f'
+    _test_completedefault(['file', 'folder'], 'show f', 5)
+
+
+def test_long_completedefault():
+    _test_completedefault(['long'], 'show  ', 6)
+
+
+def test_no_completedefault():
+    _test_completedefault([], 'taz ', 4)
