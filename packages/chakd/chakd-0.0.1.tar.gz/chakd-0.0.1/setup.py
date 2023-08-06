20e49750713e3102376020c795c8c62c0b3c2b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:03 +0000

    Add package chakd/0.0.1/chakd-0.0.1.tar.gz

---
+++ packages/chakd/chakd-0.0.1.tar.gz/chakd-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Extension
+
+setup(
+    name='chakd',
+    version='0.0.1',
+    keyword=['query','express'],
+    description='a python script for querying express',
+    author='Macrovve',
+    author_email='macrovve@gmail.com',
+    license='MIT License',
+    install_requires=[],
+    url='https://github.com/macrovve/chakd',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Multimedia',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ])
