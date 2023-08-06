f15d57c72b149ebc50e756ba78a3cf52bb636a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:13 +0000

    Add package Flask-SPF/0.0.0/Flask-SPF-0.0.0.tar.gz

---
+++ packages/Flask-SPF/Flask-SPF-0.0.0.tar.gz/Flask-SPF-0.0.0/setup.py
@@ -0,0 +1,23 @@
+#
+# Flask-SPF
+#
+# Copyright (C) 2017 Boris Raicheff
+# All rights reserved
+#
+
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name='Flask-SPF',
+    version='0.0.0',
+    description='Flask-SPF',
+    author='Boris Raicheff',
+    author_email='b@raicheff.com',
+    url='https://github.com/raicheff/flask-spf',
+    packages=find_packages(),
+)
+
+
+# EOF
