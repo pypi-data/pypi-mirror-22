2ba8ac77a65be266348628c3cad798ca8e5cc8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:45 +0000

    Add package mol/0.0.1/mol-0.0.1.tar.gz

---
+++ packages/mol/mol-0.0.1.tar.gz/mol-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='mol',
+    version='0.0.1',
+    author='Autodesk Life Sciences',
+    author_email='molsimtools@autodesk.com',
+    namespace_packages=['mol'],
+    url='http://pypi.python.org/pypi/mol/',
+    license='Apache 2.0',
+    description='Top-level namespace for Autodesk Molecular tools.',
+    long_description='Top-level namespace for Autodesk Molecular tools.',
+    install_requires=[],
+)
