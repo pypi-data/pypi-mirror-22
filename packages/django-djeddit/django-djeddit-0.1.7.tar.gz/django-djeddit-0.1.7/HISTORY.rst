7ba884f10a915c18205d03cdeac943436239e944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:38 +0000

    Add package django-djeddit/0.1.7/django-djeddit-0.1.7.tar.gz

---
+++ packages/django-djeddit/django-djeddit-0.1.7.tar.gz/django-djeddit-0.1.7/HISTORY.rst
@@ -0,0 +1,50 @@
+.. :changelog:
+
+History
+-------
+
+0.1.0 (2017-04-15)
+++++++++++++++++++
+
+* First release on PyPI.
+
+0.1.1 (2017-05-12)
+++++++++++++++++++
+
+* Added tests for models, forms, views
+* Urls that include topic titles with spaces in them don't work // fixed
+* Competability fixes for Django 1.9 and 1.8 and Python 2.7
+
+0.1.2 (2017-05-19)
+++++++++++++++++++
+
+* Updated documentation
+* client side fixes for added compatibility with mobile screen sizes
+
+0.1.3 (2017-05-23)
+++++++++++++++++++
+
+* font-awsome fonts are not loading // fixed
+
+0.1.4 (2017-05-24)
+++++++++++++++++++
+
+* Added description field for Topic
+* added description for topics in /topics page
+* updated migrations
+
+0.1.5 (2017-05-26)
+++++++++++++++++++
+
+* moved base.html to root template dir
+
+0.1.6 (2017-06-03)
+++++++++++++++++++
+
+* Admins can lock/unlock & delete threads, delete posts
+* Added description to Topic creation form
+
+0.1.7 (2017-06-09)
+++++++++++++++++++
+
+* Admins can thread title, url, and post content
---
+++ packages/django-djeddit/django-djeddit-0.1.7.tar.gz/django-djeddit-0.1.7/djeddit/static/djeddit/js/thread.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Roman on 12-Dec-16.
+ */
+
+window.postFuncs = {
+    removePostForm: function (post) {
+        $('#' + post + '>.bs-callout-main').next().remove();
+    },
+    togglePostForm: function (post, toggle, url) {
+        url = window.util.getAbsoluteURL(url + post);
+        var $placeAfter = $('#' + post + '>.bs-callout-main');
+        window.util.toggleForm(url, $placeAfter, {}, $(toggle));
+    },
+    toggleEditForm: function (post, toggle, toggleHeader) {
+        var $placeAfter = $('#' + post + '>.bs-callout-main');
+        $placeAfter.toggle();
+        this.togglePostForm(post, toggle, 'edit_post/');
+        if (toggleHeader)
+            $('.bs-callout-heading').toggle();
+    },
+    votePost: function (post, upvoted, downvoted) {
+        var vote = 0;
+        if (upvoted)
+            vote += 1;
+        if (downvoted)
+            vote -= 1;
+        var url = window.util.getAbsoluteURL('vote_post');
+        var params = {post: post, vote: vote};
+        $.post(url, params, function (data) {
+            var $post = $('#' + post);
+            // update arrow icons
+            var $upvoteIcon = $post.find('>.bs-callout-main>.minicol>.glyphicon-chevron-up');
+            var $downvoteIcon = $post.find('>.bs-callout-main>.minicol>.glyphicon-chevron-down');
+            if (vote === 1)
+                $upvoteIcon.addClass('color-primary');
+            else
+                $upvoteIcon.removeClass('color-primary');
+            if (vote === -1)
+                $downvoteIcon.addClass('color-primary');
+            else
+                $downvoteIcon.removeClass('color-primary');
+            // update post's displayed score
+            $post.find('>.post-heading>.post-score').text(data.scoreStr);
+            $post.find('>.bs-callout-main>.minicol>.post-score').text(data.score);
+        })
+    },
+    toggleReplies: function (post) {
+        var $icon = $('#' + post + '>.post-heading>.fa.toggle-replies');
+        $icon.toggleClass('fa-minus-square-o');
+        $icon.toggleClass('fa-plus-square-o');
+        $('>.bs-callout-main', '#' + post).slideToggle('fast');
+        $('.post-container', '#' + post).slideToggle('fast');
+    }
+};
