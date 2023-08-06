a37a4fa91abdd545663499021edb1cf17be0a698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:13 +0000

    Add package mlens/0.1.3/mlens-0.1.3-py2.py3-none-any.whl

---
+++ packages/mlens/mlens-0.1.3-py2.py3-none-any.whl/mlens/__init__.py
@@ -0,0 +1,21 @@
+"""ML-ENSEMBLE
+
+:author: Sebastian Flennerhag
+:copyright: 2017
+:licence: MIT
+
+ML-Ensemble, a Python library for memory efficient parallelized ensemble
+learning.
+"""
+
+__version__ = "0.1.3"
+
+__all__ = ['base',
+           'ensemble',
+           'externals',
+           'metrics',
+           'model_selection',
+           'parallel',
+           'preprocessing',
+           'utils',
+           'visualization']
