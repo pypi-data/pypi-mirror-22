37d549ddde7f1dde43598799c88810c079e3d323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:09 +0000

    Add package sqla-yaml-fixtures/0.1.2/sqla_yaml_fixtures-0.1.2.tar.gz

---
+++ packages/sqla-yaml-fixtures/sqla_yaml_fixtures-0.1.2.tar.gz/sqla_yaml_fixtures-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as fp:
+        return fp.read()
+
+setup(name = 'sqla_yaml_fixtures',
+      description = 'Load YAML data fixtures for SQLAlchemy',
+      version = '0.1.2',
+      license = 'MIT',
+      author = 'Eduardo Naufel Schettino',
+      author_email = 'schettino72@gmail.com',
+      url = 'https://github.com/schettino72/sqla_yaml_fixtures',
+      keywords = ['fixture', 'sqlalchemy'],
+      classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Developers',
+        ],
+      py_modules = ['sqla_yaml_fixtures'],
+      install_requires = ['SQLAlchemy', 'PyYAML'],
+      long_description = readme(),
+      )
