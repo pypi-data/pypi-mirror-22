0ebbd0a31c9bdb09fdf705c652fae8a259367315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:15 +0000

    Add package guizero/0.3.3/guizero-0.3.3-py3-none-any.whl

---
+++ packages/guizero/guizero-0.3.3-py3-none-any.whl/guizero/Text.py
@@ -0,0 +1,68 @@
+from tkinter import Label, StringVar
+
+from . import utilities as utils
+    
+class Text(Label):
+
+    def __init__(self, master, text="", size=12, color="black", font="Helvetica", grid=None, align=None):
+
+        # Description of this object (for friendly error messages)
+        self.description = "[Text] object with text \"" + str(text) + "\""
+
+        # Save some of the config
+        self.current_size = size
+        self.current_color = color
+        self.current_font = font
+
+        self.text = str(text)
+
+        # Attempt to instantiate the object and raise an error if failed
+        try:
+            super().__init__(master, text=self.text)
+        except AttributeError:
+            utils.error_format( self.description + "\n" +
+            "The first argument was a " + str(type(master)) +". First argument must be [App] or [Box]")
+
+        # Initial config on setup       
+        self.config(fg=color, font=(font, size))
+
+        # Pack or grid depending on parent
+        utils.auto_pack(self, master, grid, align)
+
+
+    # Clear text (set to empty string)
+    def clear(self):
+        self.text = ""
+        self.config(text="")
+        
+    # Returns the text
+    def get(self):
+        return self.text
+
+    # Sets the text
+    def set(self, text):
+        self.text = str(text)
+        self.config(text=self.text)
+        self.description = "[Text] object with text \"" + str(text) + "\""
+
+    # Sets the text colour
+    def color(self, color):
+        self.config(fg=color)
+
+    # Set the font 
+    def font_face(self, font):
+        self.current_font = font
+        self.config(font=(self.current_font, self.current_size))
+
+    # Set the font size
+    def font_size(self, size):
+        self.current_size = size
+        self.config(font=(self.current_font, self.current_size))
+
+    # Append to the StringVar controlling this text
+    def append(self, text):
+        new_text = self.text + str(text)
+        self.text = new_text
+        self.config(text=new_text)
+        self.description = "[Text] object with text \"" + new_text + "\""
+
---
+++ packages/guizero/guizero-0.3.3-py3-none-any.whl/guizero/utilities.py
@@ -0,0 +1,60 @@
+# Auto pack or grid position the element
+# INTERNAL ONLY
+def auto_pack(self, master, grid, align): 
+
+    #print("Master is " + str(type(master)) + " for object " + self.description)
+    #print("Grid is " + str(grid))
+
+    # If the master widget specifies grid, don't pack, otherwise auto pack
+    if master.layout_manager != "grid":
+        self.pack()
+    else:
+
+        # If they failed to specify grid coords
+        if grid is None:            
+            error_format("Missing grid reference for " + self.description + ".\n" +
+            "Please add a grid reference to make this object appear.")            
+
+        # They didn't specify 2 coords
+        elif len(grid) != 2:
+            error_msg = self.description + " has no grid position argument.\n"
+            error_msg += "This widget will not be displayed!\n" 
+            error_msg += "Should be: List of two grid coordinates [row, column]"
+            error_format(error_msg)
+        else:
+
+            # If no alignment, just place in grid with center align default
+            if align is None:
+                self.grid(row=grid[0], column=grid[1])
+            else:
+                # Conversion to child friendly specifications (diags?)
+                directions = {"top": "N", "bottom": "S", "left": "W", "right": "E"}
+                align_this = "W" # Default to align left if they didn't specify something valid
+
+                try:
+                    align_this = directions[align]
+                except KeyError:                               
+                    error_msg = "Invalid align value ('"+ str(align) +"') for " + self.description + "\nShould be: top, bottom, left, right"
+                    error_format(error_msg)
+                    
+
+                # Place on grid
+                self.grid(row=grid[0], column=grid[1], sticky=align_this)
+                print("Packed component" + self.description)
+
+            
+
+
+# Lambda-izer for making it easy to pass arguments with function calls
+# without having to know what lambda does
+def with_args( func_name, *args):
+    return lambda: func_name(*args)
+
+
+
+# Format errors in a pretty way
+def error_format(error_message):
+    print("------------------------------------------------------------")
+    print("*** GUIZERO WARNING ***" )
+    print(error_message)
+    print("------------------------------------------------------------")
