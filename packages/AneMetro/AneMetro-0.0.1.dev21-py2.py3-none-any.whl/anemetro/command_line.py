bbca7073654634ac4f1364b281817d7f0a434ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:39 +0000

    Add package AneMetro/0.0.1.dev21/AneMetro-0.0.1.dev21-py2.py3-none-any.whl

---
+++ packages/AneMetro/AneMetro-0.0.1.dev21-py2.py3-none-any.whl/anemetro/command_line.py
@@ -0,0 +1,35 @@
+from anemetro.anemetro import putt
+import os.path
+import textwrap
+
+def main():
+    putt() # FORE!
+
+def config():
+    full_path = os.path.abspath("./anemetro.yml")
+    if os.path.exists(full_path):
+        print("The %s config file already exists." % full_path)
+    else:
+        default = textwrap.dedent("""\
+        # AneMetro Config
+        meetup_root: "https://api.meetup.com"
+        group_name: "PUT_MEETUP_URL_GROUP_NAME_HERE"
+        group_timezone: "US/Central"
+        template_name: "meetup"
+        posts_path: "source/_posts"
+
+        # **DO NOT** put your real MeetUp API key in here into source control.
+        # Do that via your build environment variables.
+        # You have been warned.
+        meetup_apikey: "PUT_KEY_HERE"
+
+        # Config for committing new post to the repo and pushing.
+        #
+        posts_branch: "master"
+
+        # Where to get the git url to push to.
+        yaml_filename: "_config.yml"
+        yaml_query: "deploy/repository"
+        """)
+        with open("anemetro.yml", mode="w") as config_file:
+            config_file.write(default)
