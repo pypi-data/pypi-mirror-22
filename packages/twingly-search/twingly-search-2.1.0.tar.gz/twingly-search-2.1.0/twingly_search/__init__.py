39c309e3c070f5df94561b086a29002bac9ffb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:34 +0000

    Add package twingly-search/2.1.0/twingly-search-2.1.0.tar.gz

---
+++ packages/twingly-search/twingly-search-2.1.0.tar.gz/twingly-search-2.1.0/twingly_search/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""A library provides Python interface to the Twingly Search API"""
+from __future__ import absolute_import
+
+__author__ = 'Twingly AB'
+__version__ = '2.1.0'
+
+from .client import Client
+from .errors import TwinglySearchException
+from .errors import TwinglySearchErrorException
+from .errors import TwinglySearchServerException
+from .errors import TwinglySearchClientException
+from .errors import TwinglySearchAuthenticationException
+from .errors import TwinglySearchQueryException
+from .errors import Error
+from .query import Query
+from .parser import Parser
+from .post import Post
+from .result import Result
+from .constants import TWINGLY_SEARCH_KEY
+from .constants import DATE_TIME_FORMAT
---
+++ packages/twingly-search/twingly-search-2.1.0.tar.gz/twingly-search-2.1.0/twingly_search/constants.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+
+TWINGLY_SEARCH_KEY = 'TWINGLY_SEARCH_KEY'
+DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
