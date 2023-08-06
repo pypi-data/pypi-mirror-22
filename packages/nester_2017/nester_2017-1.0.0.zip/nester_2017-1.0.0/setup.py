07a428e6d4084bca4942e4a55ab57ca1a7bd4e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:01 +0000

    Add package nester_2017/1.0.0/nester_2017-1.0.0.zip

---
+++ packages/nester_2017/nester_2017-1.0.0.zip/nester_2017-1.0.0/setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup  
+   
+setup(  
+    name            = 'nester_2017',  
+    version         ='1.0.0',  
+    py_modules      =['nester'],  
+    auther          ='sky',
+    auther_email    ='a@s.com',
+    description     ='',
+    )  
