325391fce5667b2820236295a371439fa646549e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:21 +0000

    Add package DTIlib-test-001/0.0.1/DTIlib_test-001-0.0.1.tar.gz

---
+++ packages/DTIlib-test-001/DTIlib_test-001-0.0.1.tar.gz/DTIlib_test-001-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+ 
+setup(
+	name='DTIlib_test-001',    # This is the name of your PyPI-package.
+	version='0.0.1',                          # Update the version number for new releases
+	license='GNU',
+	packages=find_packages()#,
+	#scripts=['DTIlib.py'],
+	)
