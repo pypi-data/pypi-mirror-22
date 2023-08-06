8ec6b0a30ba60703aa86ff419a25b06775b8b248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:07 +0000

    Add package pycreate2/0.5.4/pycreate2-0.5.4-py2-none-any.whl

---
+++ packages/pycreate2/pycreate2-0.5.4-py2-none-any.whl/pycreate2/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+from __future__ import division
+from .create2api import Create2
+#from pycreate2 import OI
+from .create2api import Fatal, Error, Warning
+
+from .version import __version__
+
+__license__ = 'MIT'
+__author__ = 'Kevin Walchko'
