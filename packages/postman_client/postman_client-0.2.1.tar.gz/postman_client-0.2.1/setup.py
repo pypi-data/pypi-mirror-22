8ef4d24df9659eba402de7bcec052c09c62aede7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:44 +0000

    Add package postman_client/0.2.1/postman_client-0.2.1.tar.gz

---
+++ packages/postman_client/postman_client-0.2.1.tar.gz/postman_client-0.2.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='postman_client',
+    version='0.2.1',
+    install_requires=[
+        'requests==2.11.0',
+        'simplejson==3.6.4',
+        'apysignature==0.1.3'
+    ],
+    url='https://github.com/ThCC/postman-client',
+    description='Client service, to send simple text emails or, using a template created at Postman, send more complex emails.',
+    long_description=open("README.rst").read(),
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries',
+    ],
+    author='Thiago Cardoso de Castro',
+    author_email='thiago.decastro2@gmail.com',
+)
