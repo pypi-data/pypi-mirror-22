72607f5ce8457201dbd87d257fad2ab5ed1527a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:33 +0000

    Add package roda/0.1.5/roda-0.1.5.tar.gz

---
+++ packages/roda/roda-0.1.5.tar.gz/roda-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='roda',
+    version='0.1.5',
+    packages=find_packages(where='.src', exclude=['test', 'test.*']),
+
+    install_requires=['nose'],
+    author='x6doooo',
+    author_email='x6doooo@gmail.com',
+    description='...',
+    license='MIT',
+)
