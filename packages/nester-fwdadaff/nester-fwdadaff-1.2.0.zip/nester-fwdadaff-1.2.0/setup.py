f3555a5eacae1d247f968ea90fb52662e1e4500c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:35 +0000

    Add package nester-fwdadaff/1.2.0/nester-fwdadaff-1.2.0.zip

---
+++ packages/nester-fwdadaff/nester-fwdadaff-1.2.0.zip/nester-fwdadaff-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='nester-fwdadaff',
+        version='1.2.0',
+        py_modules =['nester'],
+        author = 'fwdadaff',
+        author_email='13080684589@163.com',
+        url='http://www.headfirstlabs.com',
+        description='a simple printer of nested lists',
+     )
