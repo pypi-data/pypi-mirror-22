f73a4aed1be9a3b054afba6f74d640ee28d5eece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:13 +0000

    Add package mastercard-qkr/1.0.0/mastercard_qkr-1.0.0.tar.gz

---
+++ packages/mastercard-qkr/mastercard_qkr-1.0.0.tar.gz/mastercard_qkr-1.0.0/mastercardqkr/__init__.py
@@ -0,0 +1,22 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardqkr.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardqkr.card import *
+from mastercardqkr.cart import *
+from mastercardqkr.cartitem import *
+from mastercardqkr.country import *
+from mastercardqkr.forgottenpassword import *
+from mastercardqkr.lightbox import *
+from mastercardqkr.merchant import *
+from mastercardqkr.prodgroup import *
+from mastercardqkr.resendotp import *
+from mastercardqkr.securityquestion import *
+from mastercardqkr.trans import *
+from mastercardqkr.updatepassword import *
+from mastercardqkr.user import *
+from mastercardqkr.usersession import *
+from mastercardqkr.verifyotp import *
