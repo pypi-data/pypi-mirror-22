73618f1c92062b557c2c0645ac54d5bbd22c0239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:11 +0000

    Add package Battlefield/1.0.2/Battlefield-1.0.2.tar.gz

---
+++ packages/Battlefield/Battlefield-1.0.2.tar.gz/Battlefield-1.0.2/battlefield/engine/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from .config import Config
+from .robot import Robot
+from .base import Engine
+from .turn import TurnEngine
---
+++ packages/Battlefield/Battlefield-1.0.2.tar.gz/Battlefield-1.0.2/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='Battlefield',
+    version='1.0.2',
+    packages=find_packages(),
+    install_requires=['pika'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='robot game battlefield',
+    url='https://github.com/tehpug/Battlefield',
+    license='GPLv3',
+    author='Mehdy Khoshnoody',
+    author_email='mehdy.khoshnoody@gmail.com',
+    description='Utilities to create games and robots for Battlefield')
