ddb09ed71df65a7ecabebd64ce0863014c28b46f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:53 +0000

    Add package BABASemantics/0.2/BABASemantics-0.2.tar.gz

---
+++ packages/BABASemantics/BABASemantics-0.2.tar.gz/BABASemantics-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'BABASemantics',
+  packages = ['BABASemantics'],
+  version = '0.2',
+  description = 'Parsing, computation of semantics for ABA frameworks and semantic probabilities for BABA frameworks.',
+  author = 'Tomasz Szymaniec',
+  author_email = 'tomek.szymaniec@gmail.com',
+  url = 'https://github.com/tomekhotdog/BABASemantics',
+  download_url = 'https://github.com/tomekhotdog/BABASemantics/archive/0.1.tar.gz',
+  keywords = ['BABA', 'ABA', 'framework', 'argumentation', 'semantics'],
+  classifiers = [],
+)
