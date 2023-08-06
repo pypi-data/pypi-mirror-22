7521c3bd4d05a5b6de4d86353550bc1ba092457e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:32 +0000

    Add package metering/0.0.71/metering-0.0.71.tar.gz

---
+++ packages/metering/metering-0.0.71.tar.gz/metering-0.0.71/metering/equipment.py
@@ -0,0 +1,9 @@
+
+class Equipment():
+
+    def __init__(self):
+        self.idy = str()
+        self.kind = str()
+        self.make = str()
+        self.model = str()
+        self.ophash = dict()
