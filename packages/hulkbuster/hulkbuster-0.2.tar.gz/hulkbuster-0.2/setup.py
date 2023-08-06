fa9ec16b4fa4ccbcdad8d0145ee8d9b7358f7c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:04 +0000

    Add package hulkbuster/0.2/hulkbuster-0.2.tar.gz

---
+++ packages/hulkbuster/hulkbuster-0.2.tar.gz/hulkbuster-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hulkbuster',
+      version='0.2',
+      description='IITBombayX EDX API',
+      url='https://github.com/DhruvThakker/hulkbuster',
+      author='HulkBuster441',
+      author_email='dthakker1997@gmail.com',
+      license='MIT',
+      packages=['hulkbuster'],
+      zip_safe=False)
