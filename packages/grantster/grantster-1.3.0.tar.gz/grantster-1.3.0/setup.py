1b5dd93b4531beb7122c11684ebfa7a678d0b424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:08 +0000

    Add package grantster/1.3.0/grantster-1.3.0.tar.gz

---
+++ packages/grantster/grantster-1.3.0.tar.gz/grantster-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'grantster',
+        version         = '1.3.0',
+        py_modules      = ['grantster'],
+        author          = 'Grant Rosario',
+        author_email    = 'nothing@x.com',
+        url             = 'http://www.hello.com',
+        description     = 'A simple printer of nested lists',
+    )
