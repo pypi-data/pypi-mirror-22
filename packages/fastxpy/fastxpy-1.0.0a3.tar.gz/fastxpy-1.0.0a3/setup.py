f94521d3c75ccc7986175031456545cda5d9f05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:36 +0000

    Add package fastxpy/1.0.0a3/fastxpy-1.0.0a3.tar.gz

---
+++ packages/fastxpy/fastxpy-1.0.0a3.tar.gz/fastxpy-1.0.0a3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(  name = "fastxpy",
+        version = "1.0.0a3",
+        description = "fastx functions",
+        url = "https://github.com/mustafa-guler/fastpy",
+        author = "Mustafa Guler",
+        packages = ["fastxpy"],
+        zip_safe = False)
+
