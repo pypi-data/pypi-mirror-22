b524b10910413ac803335daf27c605aa9646aadf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:33 +0000

    Add package pypki2/0.7.1/pypki2-0.7.1.tar.gz

---
+++ packages/pypki2/pypki2-0.7.1.tar.gz/pypki2-0.7.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+# vim: expandtab tabstop=4 shiftwidth=4
+
+setup(
+    version='0.7.1',
+    name='pypki2',
+    description='More user-friendly way to access PKI-enabled services',
+    url='https://github.com/nbgallery/pypki2',
+    author="Bill Allen",
+    author_email="photo.allen@gmail.com",
+    scripts=[],
+    packages=['pypki2',],
+    package_data={},
+    install_requires=[],
+    download_url='https://github.com/nbgallery/pypki2/archive/0.7.1.tar.gz',
+    keywords=['pki','ssl','pem','pkcs12','p12']
+)
