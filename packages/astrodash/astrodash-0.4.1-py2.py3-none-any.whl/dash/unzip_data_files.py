346e9e1b154a998a5c616a8b91c2948f7fbad99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:41 +0000

    Add package astrodash/0.4.1/astrodash-0.4.1-py2.py3-none-any.whl

---
+++ packages/astrodash/astrodash-0.4.1-py2.py3-none-any.whl/dash/unzip_data_files.py
@@ -0,0 +1,14 @@
+import zipfile
+import os
+
+
+def unzip_data_files(dataZipFilename):
+    scriptDirectory = os.path.dirname(os.path.abspath(__file__))
+    zipRef = zipfile.ZipFile(dataZipFilename, 'r')
+    zipRef.extractall(os.path.join(scriptDirectory, dataZipFilename.strip(".zip")))  # #
+    zipRef.close()
+    os.remove(os.path.join(scriptDirectory, dataZipFilename))
+
+
+if __name__ == '__main__':
+    unzip_data_files('models_v01.zip')
