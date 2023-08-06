6fb8e773ac2eebfaa7cc5499b2d5341e1699e32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:50 +0000

    Add package vyper-bot/0.9/vyper-bot-0.9.zip

---
+++ packages/vyper-bot/vyper-bot-0.9.zip/vyper-bot-0.9/setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+setup(name='vyper-bot',
+      version='0.9',
+      author='Josh Bacon',
+      author_email='bacon.josh09@gmail.com',
+      packages=['vyper',],
+      requires=[
+            'requests',
+            'json'
+          ])
