b7fe38e3ed893cc329dc19ddf40d6909409e48c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:47 +0000

    Add package rudra_nester/1.3.0/rudra_nester-1.3.0.tar.gz

---
+++ packages/rudra_nester/rudra_nester-1.3.0.tar.gz/rudra_nester-1.3.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name		= 'rudra_nester',
+	version		= '1.3.0',
+	py_modules 	= ['rudra_nester'],
+	author		= 'hfpython',
+	author_email 	= 'hfpython@headfirstlabs.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested lists',
+
+
+) 
