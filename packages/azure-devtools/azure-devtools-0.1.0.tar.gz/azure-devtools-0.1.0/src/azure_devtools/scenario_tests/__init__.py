3d2fb701760b91fbc72319288ff31e474970420f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:56 +0000

    Add package azure-devtools/0.1.0/azure-devtools-0.1.0.tar.gz

---
+++ packages/azure-devtools/azure-devtools-0.1.0.tar.gz/azure-devtools-0.1.0/src/azure_devtools/scenario_tests/__init__.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from .base import ScenarioTest, LiveTest
+from .exceptions import AzureTestError
+from .decorators import live_only, record_only
+from .utilities import get_sha1_hash
+
+__all__ = ['ScenarioTest', 'LiveTest', 'AzureTestError', 'get_sha1_hash',
+           'live_only', 'record_only']
+__version__ = '0.1.0+dev'
