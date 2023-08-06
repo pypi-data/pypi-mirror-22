d45ae35de017919d8658f5da2e9433cd4e1b8c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:31 +0000

    Add package jetpy/0.0.0/jetpy-0.0.0.tar.gz

---
+++ packages/jetpy/jetpy-0.0.0.tar.gz/jetpy-0.0.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name='jetpy',
+    version='0.0.0',
+    description='JetPy root package',
+    author='Vitalii Maslov',
+    author_email='me@pyvim.com',
+    license='MIT',
+    packages=setuptools.find_packages('src'),
+)
