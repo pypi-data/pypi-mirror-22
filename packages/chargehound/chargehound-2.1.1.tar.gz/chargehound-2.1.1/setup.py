9687b21307dd326bcbc9013e082d121fe64477bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:22 +0000

    Add package chargehound/2.1.1/chargehound-2.1.1.tar.gz

---
+++ packages/chargehound/chargehound-2.1.1.tar.gz/chargehound-2.1.1/setup.py
@@ -0,0 +1,38 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from disutils.core import setup
+
+try:
+    from pypandoc import convert
+    long_description = convert('README.md', 'rst')
+except:
+    """
+    Don't fail if pandoc or pypandoc are not installed.
+    However, it is better to publish the package with
+    a formatted README.
+    """
+    print("""
+Warning: Missing pypandoc, which is used to format \
+the README. Install pypandoc before publishing \
+a new version.""")
+    long_description = open('README.md').read()
+
+from chargehound.version import VERSION
+
+setup(
+    name='chargehound',
+    version=VERSION,
+    author='Chargehound',
+    author_email='support@chargehound.com',
+    packages=['chargehound'],
+    description='Chargehound Python Bindings',
+    long_description=long_description,
+    url='https://www.chargehound.com',
+    license='MIT',
+    test_suite='test.all',
+    install_requires=[
+        'requests',
+        'munch'
+        ],
+    )
