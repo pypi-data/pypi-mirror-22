cc2b9ef7b7c08dc1d96c4b6f9ea6ef018f68c31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:39 +0000

    Add package pygres/1.3.2/pygres-1.3.2.tar.gz

---
+++ packages/pygres/pygres-1.3.2.tar.gz/pygres-1.3.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name = 'pygres',
+    packages = ['pygres'], # this must be the same as the name above
+    version = '1.3.2',
+    description = 'Postgres simple connector',
+    author = 'Rodrigo Gamba',
+    author_email = 'gamba.lavin@gmail.com',
+    url = 'https://github.com/rogamba/pygres', # use the URL to the github repo
+    download_url = 'https://github.com/rogamba/pygres/tarball/0.1', # I'll explain this in a second
+    keywords = ['postgres', 'psql', 'postgresql'],
+    install_requires=[
+        'psycopg2>=2.6.2',
+    ],
+    classifiers = [],
+)
