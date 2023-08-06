c867c5d58ec1edb612547f67a761718c5ad2f27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:17 +0000

    Add package cogito/0.2.0/cogito-0.2.0.tar.gz

---
+++ packages/cogito/cogito-0.2.0.tar.gz/cogito-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cogito',
+    version='0.2.0',
+    py_modules=['cogito'],
+    description='A tiny python library that links against libcogito',
+    url='https://github.com/localytics/cogito-py',
+    author='Localytics',
+    author_email='oss@localytics.com',
+    license='MIT'
+)
