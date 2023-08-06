f5bd386760bcdf53c6bc3662e4175716e91c54bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:49 +0000

    Add package libtrust-py3/0.1.1/libtrust-py3-0.1.1.tar.gz

---
+++ packages/libtrust-py3/libtrust-py3-0.1.1.tar.gz/libtrust-py3-0.1.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup
+
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
+
+install_requires = [
+    'enum34>=1.1.6',
+    'cryptography>=1.5.2',
+]
+
+setup(
+    name="libtrust-py3",
+    version='0.1.1',
+    description="Integrate docker/libtrust with python.",
+    url='https://github.com/tomwei7/libtrust-py3',
+    packages=['libtrust'],
+    install_requires=install_requires,
+    zip_safe=False,
+)
