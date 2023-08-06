cc15dea38a4091607c48e9abdf58065407726a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:56 +0000

    Add package certsrv/1.5.0/certsrv-1.5.0.tar.gz

---
+++ packages/certsrv/certsrv-1.5.0.tar.gz/certsrv-1.5.0/setup.py
@@ -0,0 +1,27 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='certsrv',
+    description='A Python client for the Microsoft AD Certificate Services web page',
+    author='Magnus Watn',
+    license='MIT',
+    url='https://github.com/magnuswatn/certsrv',
+    keywords='ad adcs certsrv pki certificate',
+    version='1.5.0',
+    py_modules=['certsrv'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Systems Administration',
+        ],
+)
