6999a754948c92208355d1106054225dc3ce68ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:37 +0000

    Add package lalaland/1.0.3/lalaland-1.0.3.tar.gz

---
+++ packages/lalaland/lalaland-1.0.3.tar.gz/lalaland-1.0.3/lalaland.py
@@ -0,0 +1,15 @@
+"""this model for python just a example of my study,
+so don't take it consideration."""
+
+def print_plus(the_list,level):
+
+	"""this function is just print all the item of a list including inner list"""
+
+	for item in the_list:
+		if isinstance(item,list):
+			print_plus(item,level+1);
+		else:
+			for tab_item in range(level):
+				print('\t',end='');
+			print(item);
+
