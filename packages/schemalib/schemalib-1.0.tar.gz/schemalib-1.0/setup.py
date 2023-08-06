691322e36c3e6d5c255aa80d75eaa09b132c2ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:44 +0000

    Add package schemalib/1.0/schemalib-1.0.tar.gz

---
+++ packages/schemalib/schemalib-1.0.tar.gz/schemalib-1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+def get_requirements():
+    with open('requirements.txt') as fd:
+        return fd.read().splitlines()
+
+setup(
+    name='schemalib',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=get_requirements(),
+    include_package_data=True,
+    author='Pavel Odvody',
+    author_email='podvody@redhat.com',
+    description='Library for working with JSON Schemas',
+    license='GPLv-3',
+    keywords='lib',
+    url='https://github.com/shaded-enmity/schemalib'
+)
