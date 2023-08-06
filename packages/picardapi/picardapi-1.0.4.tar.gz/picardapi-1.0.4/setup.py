554c077839cdf95de8191b402a4d60aaa8c2c918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:58 +0000

    Add package picardapi/1.0.4/picardapi-1.0.4.tar.gz

---
+++ packages/picardapi/picardapi-1.0.4.tar.gz/picardapi-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='picardapi',
+    version='1.0.4',
+    packages=find_packages(),
+    author='picard_username',
+    author_email='brig@trialomics.com',
+    url='https://picard.io/',
+    install_requires=['simplejson==3.6.5','requests==2.5.1','fake-factory>=0.5.3','ipaddress>=1.0.16','Babel>=2.2.0']
+)
