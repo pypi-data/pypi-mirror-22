20aaa6f29c19e6f61f82e8d1f814f01e66b1df7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:25 +0000

    Add package fangcloud/2.2.2/fangcloud-2.2.2.tar.gz

---
+++ packages/fangcloud/fangcloud-2.2.2.tar.gz/fangcloud-2.2.2/fangcloud/base_type.py
@@ -0,0 +1,39 @@
+
+class ItemType:
+    File = "file"
+    Folder = "folder"
+    All = "all"
+
+
+class SortBy:
+    Name = "name"
+    Size = "size"
+    Score = "score"
+
+
+class SortDirection:
+    Desc = "desc"
+    ASC = "asc"
+
+
+class ShareLinkAccess:
+    Public = "public"
+    Company = "company"
+
+
+class CollaborationRole:
+    Edit = "edit"
+    Viewer = "viewer"
+    Previewer = "previewer"
+    Uploader = "uploader"
+    Coowner = "coowner"
+    ViewerUploader = "viewer_uploader"
+    Owner = "owner"
+
+
+
+
+
+
+
+
