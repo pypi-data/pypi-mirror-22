130f027a067f0ba416fc81f92266857e2308650a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:03 +0000

    Add package opal/0.8.3/opal-0.8.3.tar.gz

---
+++ packages/opal/opal-0.8.3.tar.gz/opal-0.8.3/opal/static/js/opal/app.js
@@ -0,0 +1,12 @@
+var app = OPAL.module('opal', [
+    'ngRoute',
+    'ngProgressLite',
+    'opal.config',
+  	'opal.filters',
+  	'opal.services',
+  	'opal.directives',
+  	'opal.controllers',
+    'ui.bootstrap',
+    'ui.select'
+]);
+OPAL.run(app);
