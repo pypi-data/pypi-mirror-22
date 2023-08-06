7cf42e675385fd42eaac00938cf5898d547935b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:40 +0000

    Add package django-htcpcp/0.1.1/django-htcpcp-0.1.1.tar.gz

---
+++ packages/django-htcpcp/django-htcpcp-0.1.1.tar.gz/django-htcpcp-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='django-htcpcp',
+  packages=['django-htcpcp'],
+  version='0.1.1',
+  description='Django HyperText Coffee Pot Protocol Middleware (HTCPCP)',
+  author='Dash Winterson',
+  author_email='dashdanw@gmail.com',
+  url='https://github.com/dashdanw/django-htcpcp',
+  download_url='https://github.com/dashdanw/django-htcpcp/archive/0.1.1.tar.gz',
+  keywords=['htcpcp', 'django', 'middleware', 'RFC 2324'],
+  classifiers=[],
+)
