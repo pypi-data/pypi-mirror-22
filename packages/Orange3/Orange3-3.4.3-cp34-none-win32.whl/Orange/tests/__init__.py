d6f05132cca1c40feb00182af9269b6625a4ced0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:35 +0000

    Add package Orange3/3.4.3/Orange3-3.4.3-cp34-none-win32.whl

---
+++ packages/Orange3/Orange3-3.4.3-cp34-none-win32.whl/Orange/tests/__init__.py
@@ -0,0 +1,68 @@
+import os
+import unittest
+import tempfile
+from contextlib import contextmanager
+
+import Orange
+
+
+@contextmanager
+def named_file(content, encoding=None, suffix=''):
+    file = tempfile.NamedTemporaryFile("wt", delete=False,
+                                       encoding=encoding, suffix=suffix)
+    file.write(content)
+    name = file.name
+    file.close()
+    try:
+        yield name
+    finally:
+        os.remove(name)
+
+
+def test_dirname():
+    """
+    Return the absolute path to the Orange.tests package.
+
+    Returns
+    -------
+    path : str
+    """
+    return os.path.dirname(__file__)
+
+
+def test_filename(path):
+    """
+    Return an absolute path to a resource within Orange.tests package.
+
+    Parameters
+    ----------
+    path : str
+        Path relative to `test_dirname()`
+    Returns
+    -------
+    abspath : str
+        Absolute path
+    """
+    return os.path.join(test_dirname(), path)
+
+
+def suite(loader=None, pattern='test*.py'):
+    test_dir = os.path.dirname(__file__)
+    if loader is None:
+        loader = unittest.TestLoader()
+    if pattern is None:
+        pattern = 'test*.py'
+    top_level_dir = os.path.dirname(os.path.dirname(Orange.__file__))
+    all_tests = [
+        loader.discover(test_dir, pattern, top_level_dir),
+    ]
+
+    return unittest.TestSuite(all_tests)
+
+
+def load_tests(loader, tests, pattern):
+    return suite(loader, pattern)
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
