a180e285e067660807c806e018f16053b292776e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:21 +0000

    Add package django-sm8-jobs/0.4/django-sm8-jobs-0.4.tar.gz

---
+++ packages/django-sm8-jobs/django-sm8-jobs-0.4.tar.gz/django-sm8-jobs-0.4/jobs/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.forms import ModelForm
+from jobs.models import ClientProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class JobSubmissionForm(forms.Form):
+    job_description = forms.CharField(label="Job description", widget=forms.Textarea)
+
+class ProfileSubmissionForm(ModelForm):
+    class Meta:
+        model = ClientProfile
+        fields = ['phone', 'street', 'city', 'state', 'zip_code']
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
