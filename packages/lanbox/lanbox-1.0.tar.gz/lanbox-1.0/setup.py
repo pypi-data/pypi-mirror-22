4b10be9c4a6c1476abfb87d480a87fe02a600007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:21 +0000

    Add package lanbox/1.0/lanbox-1.0.tar.gz

---
+++ packages/lanbox/lanbox-1.0.tar.gz/lanbox-1.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='lanbox',
+    version='1.0',
+    url='https://github.com/bit/python-lanbox',
+    description='python interface for your LanBox controller',
+    author='j',
+    author_email='j@mailb.org',
+    license='GPL',
+    packages=['lanbox'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
