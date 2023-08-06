72254493ae870ccf2834b1adf26f07989f05e52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:21 +0000

    Add package Lawes/1.9.2.21/Lawes-1.9.2.21.tar.gz

---
+++ packages/Lawes/Lawes-1.9.2.21.tar.gz/Lawes-1.9.2.21/lawes/test/__init__.py
@@ -0,0 +1,14 @@
+
+from lawes.db import models
+
+conf_dict = {'mongo_uri': 'mongodb://127.0.0.1:27017/', 'db_name': 'testdb'}
+models.setup(conf=conf_dict)
+
+class Fruit(models.Model):
+    class Meta:
+        db_table = 'fruit'
+
+    name = models.CharField(default='')
+    color = models.CharField(default='')
+    sid = models.AutoField()
+    nid = models.AutoField(start=1000000000)
