4392bf55d081352163c7c260c96c3e5451247a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:19 +0000

    Add package pubmedasync/0.3.0/pubmedasync-0.3.0.tar.gz

---
+++ packages/pubmedasync/pubmedasync-0.3.0.tar.gz/pubmedasync-0.3.0/pubmedasync/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import pubmedasync.fetch # noqa
+
+__author__ = """Pokey Rule"""
+__email__ = 'pokey.rule@gmail.com'
+__version__ = '0.3.0'
+
+__all__ = [
+    'test',
+]
