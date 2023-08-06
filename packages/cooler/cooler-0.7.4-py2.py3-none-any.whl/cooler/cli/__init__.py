45de4ef235d218723ec2a76469bf7e83df0f9541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:19 +0000

    Add package cooler/0.7.4/cooler-0.7.4-py2.py3-none-any.whl

---
+++ packages/cooler/cooler-0.7.4-py2.py3-none-any.whl/cooler/cli/__init__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function
+import logging
+import sys
+
+import click
+from .. import __version__, get_logger
+
+logging.basicConfig(stream=sys.stderr)
+logger = get_logger()
+logger.setLevel(logging.INFO)
+
+
+# Monkey patch
+click.core._verify_python3_env = lambda: None
+
+
+CONTEXT_SETTINGS = {
+    'help_option_names': ['-h', '--help'],
+}
+
+
+@click.version_option(version=__version__)
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.option(
+    '--debug/--no-debug', 
+    help="Verbose logging", 
+    default=False)
+def cli(debug):
+    if debug:
+        logger.setLevel(logging.DEBUG)
+
+
+from . import (
+    makebins,
+    digest,
+    csort,
+    cload,
+    load,
+    merge,
+    copy,
+    list_,
+    info,
+    dump,
+    balance,
+    aggregate,
+    show,
+)
