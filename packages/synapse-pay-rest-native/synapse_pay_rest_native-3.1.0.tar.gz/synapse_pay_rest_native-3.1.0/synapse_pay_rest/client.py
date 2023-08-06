78e63e17809931aae6b7e17f959e217eda5dfd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:07 +0000

    Add package synapse-pay-rest-native/3.1.0/synapse_pay_rest_native-3.1.0.tar.gz

---
+++ packages/synapse-pay-rest-native/synapse_pay_rest_native-3.1.0.tar.gz/synapse_pay_rest_native-3.1.0/synapse_pay_rest/client.py
@@ -0,0 +1,36 @@
+from .http_client import HttpClient
+from .api.users import Users
+from .api.trans import Trans
+from .api.nodes import Nodes
+
+
+class Client():
+    """Handles configuration and requests to the SynapsePay API.
+    """
+
+    def __init__(self, **kwargs):
+        """Create a new API client.
+
+        Args:
+            client_id (str): your API client id
+            client_secret (str): your API client secret
+            fingerprint (str): the user's fingerprint
+            ip_address (str): the user's IP address
+            development_mode (bool): if True, requests sent to sandbox
+            endpoints (else production)
+            logging (bool): if True, requests logged to stdout
+
+        Todo:
+            Allow logging to file
+        """
+        self.base_url = 'https://synapsepay.com/api/3'
+        if kwargs.get('development_mode'):
+            self.base_url = 'https://uat-api.synapsefi.com/v3.1'
+
+        self.http_client = HttpClient(base_url=self.base_url, **kwargs)
+        self.users = Users(self.http_client)
+        self.nodes = Nodes(self.http_client)
+        self.trans = Trans(self.http_client)
+
+    def __repr__(self):
+        return '{0}(base_url={1})'.format(self.__class__, self.base_url)
