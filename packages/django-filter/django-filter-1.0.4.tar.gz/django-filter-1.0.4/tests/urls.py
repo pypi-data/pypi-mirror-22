ef12cd861180cd9fb232d05a6168f946fe92b269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:54 +0000

    Add package django-filter/1.0.4/django-filter-1.0.4.tar.gz

---
+++ packages/django-filter/django-filter-1.0.4.tar.gz/django-filter-1.0.4/tests/urls.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from django.conf.urls import url
+
+from django_filters.views import FilterView, object_filter
+from .models import Book
+
+def _foo():
+    return 'bar'
+
+urlpatterns = [
+    url(r'^books-legacy/$', object_filter, {'model': Book, 'extra_context': {'foo': _foo, 'bar': 'foo'}}),
+    url(r'^books/$', FilterView.as_view(model=Book)),
+]
