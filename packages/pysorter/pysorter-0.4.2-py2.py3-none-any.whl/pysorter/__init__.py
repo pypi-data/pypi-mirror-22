c81a224e66ee7213e931288dcf795de46c83bd82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:33 +0000

    Add package pysorter/0.4.2/pysorter-0.4.2-py2.py3-none-any.whl

---
+++ packages/pysorter/pysorter-0.4.2-py2.py3-none-any.whl/pysorter/__init__.py
@@ -0,0 +1,8 @@
+import os
+BASE = os.path.dirname(__file__)
+__version__ = open(os.path.join(BASE, 'version.txt')).read().strip()
+
+del BASE
+del os
+
+from .rules import Skip, SkipRecurse, Unhandled
