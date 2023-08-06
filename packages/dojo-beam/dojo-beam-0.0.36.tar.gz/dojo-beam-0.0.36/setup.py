9c8275fd53f92e6491a8df29320b2aa3c38d742d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:02 +0000

    Add package dojo-beam/0.0.36/dojo-beam-0.0.36.tar.gz

---
+++ packages/dojo-beam/dojo-beam-0.0.36.tar.gz/dojo-beam-0.0.36/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dojo-beam',
+    version='0.0.36',
+    description='Apache Beam adapters and datasets for the Dojo data framework.',
+    author='Steven Normore',
+    author_email='steven@dataup.io',
+    url='https://dojo.dataup.io/',
+    packages=find_packages(),
+    scripts=[],
+    install_requires=[
+        'google-cloud-dataflow',
+        'google-apitools',
+        'oauth2client<4.0.0dev'
+    ]
+)
