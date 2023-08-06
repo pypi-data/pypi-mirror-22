c0c89b82d5428498cfddd64c2731d1d8ba4e6a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:48 +0000

    Add package testpirnt/1.0.0/testpirnt-1.0.0.tar.gz

---
+++ packages/testpirnt/testpirnt-1.0.0.tar.gz/testpirnt-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "testpirnt",
+    version = "1.0.0",
+    py_modules = ['testprint'],
+    author = "hfpython",
+    author_email = 'hfpython@headfirstlabs.com',
+    url = "https://www.google.com"
+    )
+
