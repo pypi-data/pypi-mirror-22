d3d1ff64e8e8c6250cf38c044419f0daf391359f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:42 +0000

    Add package restutils/1.0.3/restutils-1.0.3.zip

---
+++ packages/restutils/restutils-1.0.3.zip/restutils-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='restutils',
+    version='1.0.3',
+    packages=['restutils'],
+    url='https://github.com/pupattan/restutils',
+    license='MIT',
+    author='pulak pattanayak',
+    author_email='pulak.pattanayak@gmail.com',
+    description='Provides libraries for REST API applications',
+    keywords="rest http",
+    use_2to3=True,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ]
+)
