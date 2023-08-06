c563585e674988f592623e0d099ef6105b46265b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:11 +0000

    Add package inqry/1.2.2/inqry-1.2.2.tar.gz

---
+++ packages/inqry/inqry-1.2.2.tar.gz/inqry-1.2.2/inqry/form_instructions.py
@@ -0,0 +1,68 @@
+class FormInstructions:
+    def __init__(self, specs, form_factor=None, user=None):
+        self.short_delay = '~d'
+        self.long_delay = '~d~d'
+        self.tab = '~t'
+        self.enter = '~e'
+        self.space = '\x20'
+        self.status = 'Ready'
+        self.model_id = specs.model_identifier
+        self.serial = specs.serial_number
+        self.fieldset = form_factor or 'Desktop'
+        self.user = user or ''
+        self.processor = '{} {}'.format(specs.processor_speed, specs.processor_name)
+        self.fieldsets = {'Desktop': (self._text_box(self.processor) +
+                                      self._text_box(specs.memory) +
+                                      self._text_box(specs.drive1) +
+                                      self._text_box(specs.drive2) +
+                                      self._text_box(specs.drive3) +
+                                      self._text_box(specs.drive4)),
+
+                          'Portable': (self._text_box(self.processor) +
+                                       self._text_box(specs.memory) +
+                                       self._text_box(specs.drive1)),
+
+                          'New Model': (self._text_box(specs.model_name) +
+                                        [self.tab,
+                                         self.tab,
+                                         self.short_delay +
+                                         self.model_id]),
+
+                          'Mobile': (self._text_box(specs.imei) +
+                                     self._text_box(specs.mobile_storage))}
+
+    def _common_fields(self, unique_fields):
+        return (self._list_box(self.model_id) +
+                unique_fields +
+                self._list_box(self.status) +
+                [self.short_delay,
+                 self.space,
+                 self.short_delay,
+                 self.user,
+                 self.short_delay,
+                 self.enter,
+                 self.short_delay,
+                 self.tab] +
+                [self.short_delay,
+                 self.serial])
+
+    def _text_box(self, field_content):
+        return [self.short_delay,
+                field_content,
+                self.tab]
+
+    def _list_box(self, field_content):
+        return [self.space,
+                self.short_delay,
+                field_content,
+                self.long_delay,
+                self.enter,
+                self.long_delay,
+                self.tab,
+                self.short_delay]
+
+    def instruction_steps(self):
+        if self.fieldset == 'New Model':
+            return self.fieldsets['New Model']
+        else:
+            return self._common_fields(self.fieldsets[self.fieldset])
---
+++ packages/inqry/inqry-1.2.2.tar.gz/inqry-1.2.2/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='inqry',
+    author=['Apex Lab'],
+    author_email='apxlab@microsoft.com',
+    description='A cross-platform utility used to generate a QR code containing hardware specs',
+    license='MIT',
+    long_description=open('README.md').read(),
+    packages=['inqry', 'inqry.system_specs'],
+    install_requires=['Pillow',
+                      'pytest',
+                      'PyYAML',
+                      'wmi;platform_system=="Windows"',
+                      'pypiwin32;platform_system=="Windows"'],
+    tests_require=['pytest'],
+    url='https://github.com/Microsoft/InQRy',
+    version='1.2.2',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only']
+)
