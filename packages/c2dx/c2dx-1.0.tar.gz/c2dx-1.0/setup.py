28e2ec672854a0d9738bcb22e4b87244fb1bb82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:07 +0000

    Add package c2dx/1.0/c2dx-1.0.tar.gz

---
+++ packages/c2dx/c2dx-1.0.tar.gz/c2dx-1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="c2dx",
+    version='1.0',
+    description='Cocos2d-x Class Generator for C++',
+    url='https://bitbucket.org/cajun_code/c2dx.py',
+    author='Allan Davis',
+    author_email='cajun.code@gmail.com',
+    license='MIT',
+    packages=['c2dx'],
+    install_requires=[
+        'Click',
+        'Jinja2',
+        'inflection',
+    ],
+    entry_points='''
+        [console_scripts]
+        c2dx=c2dx:cli
+    ''',
+    zip_safe=False
+)
