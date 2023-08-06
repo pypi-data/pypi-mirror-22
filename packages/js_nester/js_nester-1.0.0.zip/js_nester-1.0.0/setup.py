590af513b4d446b1dd2e4a408fc1540fbf6a1d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:30 +0000

    Add package js_nester/1.0.0/js_nester-1.0.0.zip

---
+++ packages/js_nester/js_nester-1.0.0.zip/js_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'js_nester',
+	version = '1.0.0',
+	py_modules = ['js_nester'],
+	author = 'hfpython',
+	author_email = 'hfpython@headfirstlabs.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of net lists',
+	)
