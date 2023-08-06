2ffd12ceb2bf03a351fb57d73e7b1d94bf890c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:19 +0000

    Add package moageet/1.0.4/moageet-1.0.4.tar.gz

---
+++ packages/moageet/moageet-1.0.4.tar.gz/moageet-1.0.4/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+ 
+import os
+import codecs
+setup(
+
+	name='moageet',
+	version='1.0.4',
+	description='Moageet al salah',
+	author='Mansour A Almansour',
+	author_email='almansour2345@gmail.com',
+	license='GPL-3.0',
+	scripts=['moageet.py'],
+	install_requires=[
+        'python-requests >= 2.9.1-3']
+
+	)
+    
