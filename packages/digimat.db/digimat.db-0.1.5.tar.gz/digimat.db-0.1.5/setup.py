b5f27421f1c1d6f2416505e68bc7e09f79f4734c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:40 +0000

    Add package digimat.db/0.1.5/digimat.db-0.1.5.tar.gz

---
+++ packages/digimat.db/digimat.db-0.1.5.tar.gz/digimat.db-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='digimat.db',
+    version='0.1.5',
+    description='Digimat DB',
+    namespace_packages=['digimat'],
+    author='Frederic Hess',
+    author_email='fhess@splust.ch',
+    url='http://www.digimat.ch',
+    license='PSF',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'MySQL',
+        # 'pymssql,'
+        'setuptools'
+    ],
+    dependency_links=[
+        ''
+    ],
+    zip_safe=False)
