11d8e00c8e803b0c75ef38a480db5827afba3c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:55 +0000

    Add package datavision/2017.5.26.1740/datavision-2017.5.26.1740.tar.gz

---
+++ packages/datavision/datavision-2017.5.26.1740.tar.gz/datavision-2017.5.26.1740/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "datavision",
+        version          = "2017.05.26.1740",
+        description      = "Python data visualisation",
+        long_description = long_description(),
+        url              = "https://github.com/wdbm/datavision",
+        author           = "Will Breaden Madden",
+        author_email     = "wbm@protonmail.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "datavision"
+                           ],
+        install_requires = [
+                           "python-dateutil",
+                           "dataset",
+                           "matplotlib",
+                           "numpy",
+                           "Pillow",
+                           "pygame",
+                           "pyprel",
+                           "scipy",
+                           "shijian"
+                           ],
+        scripts          = [
+                           "change_field_name_database_SQLite.py",
+                           "datavision_TTY_plot.py",
+                           "duplicates_database_SQLite.py",
+                           "search_database_SQLite.py",
+                           "view_database_SQLite.py"
+                           ],
+        entry_points     = """
+                           [console_scripts]
+                           datavision = datavision:datavision
+                           """
+    )
+
+def long_description(
+    filename = "README.md"
+    ):
+
+    if os.path.isfile(os.path.expandvars(filename)):
+        try:
+            import pypandoc
+            long_description = pypandoc.convert_file(filename, "rst")
+        except ImportError:
+            long_description = open(filename).read()
+    else:
+        long_description = ""
+    return long_description
+
+if __name__ == "__main__":
+    main()
