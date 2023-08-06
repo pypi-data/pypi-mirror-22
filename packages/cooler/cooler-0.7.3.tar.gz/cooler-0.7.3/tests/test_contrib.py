133b8e4b28dc9c628b87a3b39f7771661b8e13ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:07 +0000

    Add package cooler/0.7.3/cooler-0.7.3.tar.gz

---
+++ packages/cooler/cooler-0.7.3.tar.gz/cooler-0.7.3/tests/test_contrib.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+import cooler.contrib.higlass as cch
+import h5py
+import os.path as op
+
+testdir = op.realpath(op.dirname(__file__))
+
+
+def test_data_retrieval():
+    data_file = op.join(testdir, 'data', 'dixon2012-h1hesc-hindiii-allreps-filtered.1000kb.multires.cool')
+    
+    f = h5py.File(data_file, 'r')
+    
+    data = cch.get_data(f, 0, 0, 3276799999, 0, 3276799999)
+
+    assert(data['genome_start1'].iloc[0] == 0.)
+    assert(data['genome_start2'].iloc[0] == 0.)
+
+    data = cch.get_data(f, 4, 0, 256000000, 0, 256000000)
+
+    assert(data['genome_start1'].iloc[-1] > 255000000)
+    assert(data['genome_start1'].iloc[-1] < 256000000)
+    #print("ge1", data['genome_end1'])
+
+
