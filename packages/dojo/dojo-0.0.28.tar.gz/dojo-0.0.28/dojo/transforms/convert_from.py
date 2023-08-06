74a969c0b28c822f2eb5010aa909b25f916c7ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:30 +0000

    Add package dojo/0.0.28/dojo-0.0.28.tar.gz

---
+++ packages/dojo/dojo-0.0.28.tar.gz/dojo-0.0.28/dojo/transforms/convert_from.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+import json
+
+from dojo.transform import Transform
+
+
+class ConvertFrom(Transform):
+
+    def process(self, row):
+        return self.parse_from_json(row)
+
+    def parse_from_json(self, row):
+        return json.loads(row)
