4b0fde769370f5f6bd5b5f482c9dbd2a74fa948e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:16 +0000

    Add package kroissan/0.2.2/kroissan-0.2.2.tar.gz

---
+++ packages/kroissan/kroissan-0.2.2.tar.gz/kroissan-0.2.2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+__version__ = '0.2.2'
+
+setup(
+    name='kroissan',
+    version=__version__,
+    description='YAML spreadsheet',
+    author='Noel Martignoni',
+    author_email='noel@martignoni.fr',
+    url='https://gitlab.com/kroissan/kroissan',
+    scripts = ['scripts/kroissan'],
+    install_requires=[],
+    packages = ['kroissan'],
+)
