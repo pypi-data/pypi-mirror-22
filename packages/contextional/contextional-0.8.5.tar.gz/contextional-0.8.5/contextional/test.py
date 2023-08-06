e5e540a187fb02f05cf9ee3acff4762b64ed0267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:45 +0000

    Add package contextional/0.8.5/contextional-0.8.5.tar.gz

---
+++ packages/contextional/contextional-0.8.5.tar.gz/contextional-0.8.5/contextional/test.py
@@ -0,0 +1,88 @@
+from __future__ import print_function
+
+import unittest
+
+from contextional import GroupContextManager as GCM
+
+
+class T(object):
+
+    def assertThing(self, arg):
+        assert arg == 4
+
+
+GCM.utilize_asserts(T)
+
+
+
+#
+# with GCM("TEst") as t:
+#
+#     @t.add_test("stuff")
+#     def test(case):
+#         case.assertThing(4)
+#
+#     with t.add_group("other"):
+#         @t.add_test("stuff")
+#         def test(case):
+#             case.assertThing(2)
+#
+#
+# with GCM("Predefined Group") as PG:
+#
+#     @PG.add_test("value is 1")
+#     def test(case):
+#         case.assertEqual(
+#             PG.value,
+#             1,
+#         )
+#
+#     with PG.add_group("Sub Group"):
+#
+#         @PG.add_test("value is still 1")
+#         def test(case):
+#             case.assertEqual(
+#                 PG.value,
+#                 1,
+#             )
+
+def multiplier(num_1, num_2):
+    return num_1 * num_2
+
+
+with GCM("value test") as vt:
+
+    @vt.add_test("value")
+    def test(case):
+        case.assertEqual(
+            vt.value,
+            vt.expected_value,
+        )
+
+x = list("abcde")
+
+with GCM("Main Group") as MG:
+
+    for l in x:
+
+        with MG.add_group(l):
+
+            @MG.add_setup
+            def setUp(l=l):
+                MG.l = l
+
+            @MG.add_test("letter: {}".format(l))
+            def test(case):
+                case.assertIn(
+                    MG.l,
+                    "abce",
+                )
+            @MG.add_teardown
+            def tearDown():
+                MG.l = m
+
+MG.create_tests(globals())
+
+
+if __name__ == '__main__':
+    unittest.main()
