f1c3e894947cc29e721228291e553240d2afd580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:19 +0000

    Add package solent/0.44/solent-0.44.tar.gz

---
+++ packages/solent/solent-0.44.tar.gz/solent-0.44/solent/__init__.py
@@ -0,0 +1,48 @@
+# // license
+# Copyright 2016, Free Software Foundation.
+#
+# This file is part of Solent.
+#
+# Solent is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Solent is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Solent. If not, see <http://www.gnu.org/licenses/>.
+
+from .common import uniq
+from .common import ns
+
+from .cpair import solent_cpair
+from .cpair import solent_cpair_pairs
+
+from .exceptions import SolentQuitException
+
+from .keycode import solent_keycode
+
+from .mempool import mempool_new
+
+from .paths import dget_root
+from .paths import dget_static
+from .paths import dget_wres
+
+from .ref import ref_create
+from .ref import ref_lookup
+from .ref import ref_acquire
+from .ref import ref_release
+
+def are_we_in_a_pyinstaller_bundle():
+    # In pyinstaller bundles, there is a magic variable called frozen
+    # attached to sys.
+    import sys
+    if getattr(sys, 'frozen', False):
+        return True
+    else:
+        return False
+
