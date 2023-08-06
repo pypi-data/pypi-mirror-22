2143bad7988e93aebfe6eec0118553d028f1af7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:30 +0000

    Add package bacman/0.2/bacman-0.2.tar.gz

---
+++ packages/bacman/bacman-0.2.tar.gz/bacman-0.2/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='bacman',
+    version='0.2',
+    author=u'Will & Skill AB',
+    author_email='info@willandskill.se',
+    packages=find_packages(),
+    url='https://github.com/willandskill/bacman',
+    license='BSD licence, see LICENCE.txt',
+    description='Easily take snapshots of Postgres and MySQL databases and upload to AWS S3.',
+    keywords=['backup', 'database', 'mysql', 'postgres'],
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    install_requires=[
+        "dj-database-url>=0.3.0",
+        "boto>=2.38.0"
+    ]
+    # test_suite = "testproject.runtests.runtests",
+)
