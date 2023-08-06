15a241db75003eb4495047ca18197db7ba542ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:11 +0000

    Add package rpclient/0.8/rpclient-0.8.tar.gz

---
+++ packages/rpclient/rpclient-0.8.tar.gz/rpclient-0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='rpclient',
+      version='0.8',
+      description='The RabbitMQ Server\'s RPC Client',
+      keywords='rpc rpclient rpcclient rabbitmq pika',
+      url='http://infinite.mn/',
+      author='Infinite Solutions LLC',
+      author_email='ganzorig@infinite.mn',
+      license='GNU',
+      packages=['rpclient'],
+      install_requires=[
+          'pika',
+      ],
+      data_files=[('/etc', ['cfg/rpclient.ini'])],
+      zip_safe=False)
