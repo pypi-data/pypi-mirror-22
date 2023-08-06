d54a6a928fd2f09973655e2205fa5b74c3384017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:20 +0000

    Add package ampersand/0.3.0/ampersand-0.3.0.tar.gz

---
+++ packages/ampersand/ampersand-0.3.0.tar.gz/ampersand-0.3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(
+    name = "ampersand",
+    version = "0.3.0",
+    description = "A really, really minimalistic static site generator",
+    long_description = readme(),
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: Developers",
+        "Topic :: Internet",
+        "Environment :: Console"
+    ],
+    keywords = "ampersand static site generator localization globalization translation",
+    url = "http://github.com/natejms/ampersand",
+    author = "Nathan Scott",
+    author_email = "natejms@outlook.com",
+    license = "MIT",
+    packages = ["ampersand"],
+    entry_points = {
+        "console_scripts": ["ampersand=ampersand.command_line:main"]
+    },
+    install_requires = [
+        "pystache"
+    ],
+    include_package_data = True,
+    zip_safe = False
+)
