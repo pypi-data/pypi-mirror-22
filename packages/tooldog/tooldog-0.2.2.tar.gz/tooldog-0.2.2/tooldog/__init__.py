f87ca7ccf682707364345b846994ddedc9436af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:38 +0000

    Add package tooldog/0.2.2/tooldog-0.2.2.tar.gz

---
+++ packages/tooldog/tooldog-0.2.2.tar.gz/tooldog-0.2.2/tooldog/__init__.py
@@ -0,0 +1,4 @@
+from .version import __version__
+from tooldog.biotool_model import Biotool, Informations, Credit, Publication, Documentation,\
+                                  Contact, Function, Data, Input, Output, Edam, Operation,\
+                                  DataType, Format, Topic
