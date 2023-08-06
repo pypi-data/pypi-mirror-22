cd812a7a7b218faf6bef78e1a45c7d6874b8ccec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:43 +0000

    Add package uncertainty/0.1.0/uncertainty-0.1.0-py3-none-any.whl

---
+++ packages/uncertainty/uncertainty-0.1.0-py3-none-any.whl/uncertainty/constants.py
@@ -0,0 +1,5 @@
+from pkg_resources import resource_filename
+
+BCLASS_CLASSIFIER_PATH = resource_filename('uncertainty', 'models/bclass.p')
+MCLASS_CLASSIFIER_PATH = resource_filename('uncertainty', 'models/mclass.p')
+VECTORIZER_PATH = resource_filename('uncertainty', 'vectorizers/vectorizer.p')
