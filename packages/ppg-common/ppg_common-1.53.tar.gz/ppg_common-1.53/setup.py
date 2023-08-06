3ee5c33928c963240389d0676cc2c13378bf373f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:06 +0000

    Add package ppg-common/1.53/ppg_common-1.53.tar.gz

---
+++ packages/ppg-common/ppg_common-1.53.tar.gz/ppg_common-1.53/setup.py
@@ -0,0 +1,26 @@
+import io
+import os
+from setuptools import setup
+REQUIREMENTS_DIR = os.getcwd()
+REQUIREMENTS_PATH = os.path.join(REQUIREMENTS_DIR, 'requirements.txt')
+
+with io.open(REQUIREMENTS_PATH, 'r') as requirements:
+    setup(
+        name='ppg_common',
+        version='1.53',
+        description='Clients',
+        url='https://bitbucket.org/sbg-interns/ppg_common',
+        author='Sbg Core Interns',
+        author_email='strahinja.kovacevic994@gmail.com',
+        include_package_data=True,
+        install_requires=[
+            requirement[:-1] for requirement in requirements.readlines()
+        ],
+        packages=[
+            'ppg_common',
+            'ppg_common.errors',
+            'ppg_common.clients',
+            'ppg_common.handlers',
+            'ppg_common.config'
+        ]
+    )
