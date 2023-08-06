645b40a61307b779d251f9bb5ac43738567596fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:06 +0000

    Add package fanfou-cli/0.1.20/fanfou-cli-0.1.20.tar.gz

---
+++ packages/fanfou-cli/fanfou-cli-0.1.20.tar.gz/fanfou-cli-0.1.20/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+import fanfoucli
+
+setup(name='fanfou-cli',
+      version=fanfoucli.__version__,
+      packages=['fanfoucli'],
+      install_requires=['requests-oauthlib', 'arrow'],
+      keywords=['internet', 'oauth', 'sns'],
+      description='Fanfou Command Line Client',
+      long_description=open('README.rst', encoding='utf8').read(),
+      author='John Jiang',
+      author_email='nigelchiang@outlook.com',
+      license='MIT',
+      url='https://github.com/j178/fanfou-cli',
+
+      classifiers=[],
+
+      entry_points={
+          'console_scripts': [
+              'fan = fanfoucli.cli:main'
+          ]
+      })
