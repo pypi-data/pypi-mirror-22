86cd82e236134beaedd32fb200d0061007dbe86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:22 +0000

    Add package RPi.bme280/0.1.3/RPi.bme280-0.1.3.tar.gz

---
+++ packages/RPi.bme280/RPi.bme280-0.1.3.tar.gz/RPi.bme280-0.1.3/CHANGES.rst
@@ -0,0 +1,22 @@
+BME280 ChangeLog
+================
+
+0.1.3
+-----
+* [Bug fix] Correct H5 compensation param
+* Add uncompensated_readings ``__repr__``
+
+
+0.1.2
+-----
+* Use unicode degree symbol (°C) in ``__repr__`` & docstrings
+
+0.1.1
+-----
+* Port argument is optional, defaults to 0x76
+* Compensated readings now contains a UUID
+* Inclusion of changelog
+
+0.1.0
+-----
+* Initial version
---
+++ packages/RPi.bme280/RPi.bme280-0.1.3.tar.gz/RPi.bme280-0.1.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+import bme280
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(
+    name="RPi.bme280",
+    version=bme280.__version__,
+    author="Richard Hull",
+    author_email="richard.hull@destructuring-bind.org",
+    description="A library to drive a Bosch BME280 temperature, humidity, pressure sensor over I2C",
+    long_description=README,
+    license="MIT",
+    keywords=["raspberry pi", "orange pi", "banana pi", "rpi", "bosch", "BME280", "i2c", "temperature", "humidity", "pressure"],
+    url="https://github.com/rm-hull/bme280",
+    download_url="https://github.com/rm-hull/bme280/tarball/" + bme280.__version__,
+    packages=['bme280'],
+    install_requires=["smbus2"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Topic :: Education",
+        "Topic :: System :: Hardware",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ]
+)
