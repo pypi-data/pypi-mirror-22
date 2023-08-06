ae1fbc6450a5f90af5e48561eb378c41afa54e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:53 +0000

    Add package ccw_socket_sample/1.4.2/ccw_socket_sample-1.4.2.zip

---
+++ packages/ccw_socket_sample/ccw_socket_sample-1.4.2.zip/ccw_socket_sample-1.4.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+#from astroid.__pkginfo__ import author, author_email, description
+
+setup(
+      name = 'ccw_socket_sample',
+      version = '1.4.2',
+      py_modules = ['ccw_socket_sample'],
+      author = 'ccw',
+      author_email = 'ccw3435@naver.com',
+      description = 'a simple socket test'
+      )
