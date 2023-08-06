3920728d9793dbe60e25f285784dc2e692dc9ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:30 +0000

    Add package dojo/0.0.27/dojo-0.0.27.tar.gz

---
+++ packages/dojo/dojo-0.0.27.tar.gz/dojo-0.0.27/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dojo',
+    version='0.0.27',
+    description='A framework for building and running your Data platform',
+    author='Steven Normore',
+    author_email='steven@dataup.io',
+    url='https://dojo.dataup.io/',
+    packages=find_packages(),
+    scripts=['bin/dojo'],
+    install_requires=[
+        'pyyaml',
+        'jsonschema',
+        'python-dateutil'
+    ]
+)
