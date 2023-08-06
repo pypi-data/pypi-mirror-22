4b1ef4e79844223eafb0e979a708a73e53a4284a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:55 +0000

    Add package django-mysql/2.1.0/django-mysql-2.1.0.tar.gz

---
+++ packages/django-mysql/django-mysql-2.1.0.tar.gz/django-mysql-2.1.0/setup.py
@@ -0,0 +1,64 @@
+# -*- encoding:utf-8 -*-
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
+
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('django_mysql')
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+setup(
+    name='django-mysql',
+    version=version,
+    description="Extensions to Django for use with MySQL/MariaDB",
+    long_description=readme + '\n\n' + history,
+    author="Adam Johnson",
+    author_email='me@adamj.eu',
+    url='https://github.com/adamchainz/django-mysql',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.8',
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords=['Django', 'MySQL', 'MariaDB'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Database',
+    ],
+)
