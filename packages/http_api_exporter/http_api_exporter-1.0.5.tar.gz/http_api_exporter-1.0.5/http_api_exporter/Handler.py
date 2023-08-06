81552383046c154858f7f64be6e9ed077821e719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:54 +0000

    Add package http_api_exporter/1.0.5/http_api_exporter-1.0.5.tar.gz

---
+++ packages/http_api_exporter/http_api_exporter-1.0.5.tar.gz/http_api_exporter-1.0.5/http_api_exporter/Handler.py
@@ -0,0 +1,75 @@
+import json
+import copy
+import tornado
+import traceback
+
+class WelcomeHandler(tornado.web.RequestHandler):
+    def initialize(self, WelcomePage, logger):
+        self.__WelcomePage = WelcomePage
+        self.__logger = logger
+    
+    def get(self):
+        self.write(self.__WelcomePage)
+
+class MainHandler(tornado.web.RequestHandler):
+    def initialize(self, Function, logger):
+        self.__Function = Function
+        self.__logger = logger
+
+    def post(self):
+        logger = self.__logger
+        
+        logger.info('A coming request at route "%s".' % (self.request.uri))
+        
+        try:
+            form = json.loads(self.request.body.decode('utf8'))
+        except Exception as e:
+            self.set_status(400)
+            ErrorMsg = self.__getErrorMsg("Body parse error, only JSON is accepted.")
+            logger.error(e)
+            self.finish(ErrorMsg)
+            return
+        
+        try:
+            if 'Input' in form:
+                Input = copy.deepcopy(form['Input'])
+                del form['Input']
+                ArgsDict = form
+            else:
+                Input = list()
+                ArgsDict = form
+                
+            if not isinstance(Input, list):
+                self.set_status(400)
+                ErrorMsg = self.__getErrorMsg("Input must be a list.")
+                self.finish(ErrorMsg)
+                return
+            
+            result = self.__Function(*Input, **ArgsDict)
+            
+            if not isinstance(result, dict):
+                self.set_status(400)
+                ErrorMsg = self.__getErrorMsg("You are calling a function whose result is not a dictionary, please contact with author.")
+                self.finish(ErrorMsg)
+                return
+            
+            JsonOutput = self.__getSuccessMsg(result)
+            self.set_status(200)
+            self.finish(JsonOutput)
+            return
+        except Exception as e:
+            self.set_status(500)
+            ErrorMsg = self.__getErrorMsg(str(e))
+            logger.error(traceback.format_exc())
+            self.finish(ErrorMsg)
+            return
+
+    def __getErrorMsg(self, msg):
+        msgDict = {
+            'msg' : msg
+        }
+        return msgDict
+
+    def __getSuccessMsg(self, result):
+        msgDict = result
+        return msgDict
