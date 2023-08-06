ffc5945d3f0981efc143393e127443cca4c7f251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:37 +0000

    Add package mysqlclient-deps/0.0.2/mysqlclient-deps-0.0.2.tar.gz

---
+++ packages/mysqlclient-deps/mysqlclient-deps-0.0.2.tar.gz/mysqlclient-deps-0.0.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import subprocess
+import platform
+
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class CustomInstallCommand(install):
+
+    def run(self):
+        print('Platform = %s' % (platform.dist(), ))
+        if platform.dist()[0] == 'Ubuntu':
+            cmd = 'apt-get -y update && apt-get -y --force-yes install libmysqlclient-dev'
+            print('Running command: %s' % (cmd, ))
+            output = subprocess.check_output(cmd, shell=True)
+            print(output)
+        install.run(self)
+
+
+setup(
+    name='mysqlclient-deps',
+    version='0.0.2',
+    description='',
+    author='Steven Normore',
+    author_email='steven@dataup.io',
+    url='https://dataup.io/',
+    packages=[],
+    cmdclass={
+        'install': CustomInstallCommand,
+    }
+)
