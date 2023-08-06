79cdd141cdcb28e35ef442529cf6c8eea831f30c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:38 +0000

    Add package django-public-id/2.1.1/django_public_id-2.1.1-py2.py3-none-any.whl

---
+++ packages/django-public-id/django_public_id-2.1.1-py2.py3-none-any.whl/public_id/utils.py
@@ -0,0 +1,30 @@
+import string
+import uuid
+
+DEFAULT_CHARS = string.digits + string.ascii_lowercase
+
+
+def generate_id(chars=None):
+    if chars:
+        max_base = len(chars)
+        return base_n(uuid.uuid4().int, max_base, chars)
+    else:
+        return str(uuid.uuid4())
+
+
+def base_n(num, base, chars=DEFAULT_CHARS):
+    """
+    >>> base_n(42, 10)
+    '42'
+    >>> base_n(42, 2)
+    '101010'
+    """
+    if num == 0:
+        return '0'
+    if not 2 <= base <= len(chars):
+        raise ValueError('Base must be between 2-%d' % len(chars))
+    left_digits = num // base
+    if left_digits == 0:
+        return chars[num % base]
+    else:
+        return base_n(left_digits, base, chars) + chars[num % base]
