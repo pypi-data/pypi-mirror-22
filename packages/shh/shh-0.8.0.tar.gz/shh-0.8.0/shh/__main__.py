3583394db0c06ca66b36453be7f7af63dfb7a7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:02 +0000

    Add package shh/0.8.0/shh-0.8.0.tar.gz

---
+++ packages/shh/shh-0.8.0.tar.gz/shh-0.8.0/shh/__main__.py
@@ -0,0 +1,34 @@
+import shh
+import time
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument(
+    '-p',
+    '--port',
+    default=None,
+    type=str,
+    help='local port to serve through Tor (otherwise random)',
+)
+parser.add_argument(
+    '-k',
+    '--key',
+    default=None,
+    type=str,
+    help='key file to use (will generate if not found)',
+)
+
+args = parser.parse_args()
+
+if args.port is None:
+    port = shh.utils.find_port()
+else:
+    port = args.port
+print('Local port: {}'.format(port))
+
+print('Creating hidden service...')
+hidden = shh.HiddenService(ports={80: port}, key_file=args.key)
+print('Serving at: ' + hidden.onion)
+
+while True:
+    time.sleep(10)
