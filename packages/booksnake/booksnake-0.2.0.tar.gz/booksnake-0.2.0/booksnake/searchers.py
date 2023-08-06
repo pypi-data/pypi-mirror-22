ebcde1f780f8f497ca57df0539d89be053594f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:41 +0000

    Add package booksnake/0.2.0/booksnake-0.2.0.tar.gz

---
+++ packages/booksnake/booksnake-0.2.0.tar.gz/booksnake-0.2.0/booksnake/searchers.py
@@ -0,0 +1,171 @@
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+
+# Uniformize "urlretrieve"
+try:
+    urlretrieve = urllib.urlretrieve
+except:
+    urlretrieve = urllib.request.urlretrieve
+
+
+class BooksnakeOption(object):
+    def __init__(self, url, size, fmt, title, author):
+        self.url = url
+        self.size = size
+        self.fmt = fmt
+        self.title = title
+        self.author = author
+
+
+class BooksnakeSearcher(object):
+    """
+    The abstract searcher class for remote query lookup.
+    Not useful to you!
+
+    """
+    def __init__(self):
+        self.base_url = ""
+
+    def url(self, suffix=""):
+        """
+        Generate a URL for this searcher
+        """
+        return "{}/{}".format(self.base_url, suffix)
+
+    def get_options(self, query):
+        """
+        Return options based on a query, using this searcher
+        """
+        pass
+
+
+class LibreLibSearcher(BooksnakeSearcher):
+    """
+    Searches www.librelib.com
+
+    Appears to be defunct.
+    """
+    def __init__(self):
+        self.base_url = "http://www.librelib.com"
+
+    def get_options(self, query):
+        query = query.replace(' ', '+')
+        query = "/book?s=" + query
+        f = urllib.request.FancyURLopener({}).open(self.base_url + query)
+        content = f.read()
+        soup = BeautifulSoup(content, 'html.parser')
+        options = [BooksnakeOption(
+            author=a.findAll('a')[0].text.strip(),     # author
+            title=a.findAll('a')[1].text.strip(),      # title
+            fmt=a.findAll('a')[-1].text.strip(),       # format
+            url=a.findAll('a')[-1].get('href'),        # link
+            size=a.findAll('td')[-2].text.strip()      # size
+        ) for a in soup.findAll('tr')[1:]]
+        return options
+
+
+class LibgenSearcher(BooksnakeSearcher):
+    """
+    Searches libgen.io
+    """
+    def __init__(self):
+        self.base_url = (
+            "http://libgen.io" +
+            "/foreignfiction/index.php?" +
+            "s={}&f_lang=English&f_columns=0&f_ext=All"
+        )
+
+    def get_options(self, query):
+        query = query.replace(' ', '+')
+        f = urllib.request.FancyURLopener({}).open(self.base_url.format(query))
+        content = f.read()
+        soup = BeautifulSoup(content, 'html.parser')
+        options = []
+        for a in soup.findAll('table')[-1].findAll('tr'):
+            link = a.findAll('a')[-2].get('href').replace(
+                'ads.php', 'get.php'
+            )
+            if link[0] == "/":
+                link = "http://libgen.io" + link
+
+            size = "   "
+            try:
+                size = re.findall(".*\((.*)\).*", a.findAll('td')[-1].text.strip())[0]
+            except:
+                pass
+            options.append(BooksnakeOption(
+                url=link,
+                size=size,
+                fmt=a.findAll('td')[-1].text.strip().split('(')[0],
+                title=(
+                    a.findAll('td')[1].text.strip() +
+                    " " +
+                    a.findAll('td')[2].text.strip()
+                ),
+                author=a.findAll('td')[0].text.strip()
+            ))
+            # options.append([
+            #     a.findAll('td')[0].text.strip(),
+            #     (
+            #         a.findAll('td')[1].text.strip() +
+            #         " " +
+            #         a.findAll('td')[2].text.strip()
+            #     ),
+            #     a.findAll('td')[-1].text.strip().split('(')[0],
+            #     link,
+            #     a.findAll('td')[-1].text.strip().split('(')[1][:-1]
+            # ])
+        return options
+
+
+class GutenbergSearcher(BooksnakeSearcher):
+    """
+    Searches Project Gutenberg
+    """
+    def __init__(self):
+        self.base_url = "https://www.gutenberg.org"
+
+    def get_options(self, query):
+        query = query.replace(' ', '+')
+        query = "/ebooks/search/?query=" + query
+        f = urllib.request.FancyURLopener({}).open(self.base_url + query)
+        try:
+            content = f.read()
+        except:
+            pretty_print([RED, UNDERLINE], "RATE LIMITED!")
+            pretty_print(
+                [YELLOW],
+                "You've probably hit the Gutenberg page too many times " +
+                "and have been banned for 24 hours by their rate-limiter. " +
+                "The --no-gutenberg flag has been added to ~/.booksnakerc " +
+                "to prevent this from happening. Use the --gutenberg flag " +
+                "to explicitly enable this for a query."
+            )
+            settings['searchers.gutenberg'] = False
+            save_settings()
+            return []
+        soup = BeautifulSoup(content, 'html.parser')
+        links = soup.findAll("li", {"class": "booklink"})
+        options = []
+        for a in links:
+            author = a.find('span', {
+                'class': 'subtitle'
+            })
+            author = author.text.strip() if author else " "
+            title = a.find('span', {
+                'class': 'title'
+            }).text.strip()
+
+            options.append([
+                author,                         # author
+                title,                          # title
+                'mobi',                         # format (cheating)
+                self.url(
+                    a.find('a').get('href') +
+                    ".kindle.images"
+                ),                              # link
+                ' ',                            # size unlisted
+            ])
+        return options
---
+++ packages/booksnake/booksnake-0.2.0.tar.gz/booksnake-0.2.0/setup.py
@@ -0,0 +1,33 @@
+import os
+from distutils.core import setup
+
+"""
+git tag {VERSION}
+git push --tags
+python setup.py sdist upload -r pypi
+"""
+
+VERSION = "0.2.0"
+
+setup(
+    name="booksnake",
+    version=VERSION,
+    author="Jordan Matelsky",
+    author_email="j6k4m8@gmail.com",
+    description=("Download books from the internet and send to kindle"),
+    license="BSD",
+    keywords=[
+        "kindle",
+        "ebook",
+        "download"
+    ],
+    url="https://github.com/j6k4m8/booksnake/tarball/" + VERSION,
+    packages=['booksnake'],
+    scripts=[
+        'scripts/booksnake'
+    ],
+    classifiers=[],
+    install_requires=[
+        'beautifulsoup4'
+    ],
+)
