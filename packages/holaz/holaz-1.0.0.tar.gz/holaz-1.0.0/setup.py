3acbd7673a28530f7f96e4e9c7164f34c7c5006a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:34 +0000

    Add package holaz/1.0.0/holaz-1.0.0.tar.gz

---
+++ packages/holaz/holaz-1.0.0.tar.gz/holaz-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+   name = 'holaz',
+   version = '1.0.0',
+   py_modules = ['holaz'],
+   author  = 'p0wn',
+   author_email = 'poojarysurya4@gmail.com',
+   url = '',
+   description = 'A hello world python module.',
+   )
+   
+   
+   
