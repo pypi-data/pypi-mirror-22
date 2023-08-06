aa7b7eabd51d47f9508bbf3f4022747391c2e22f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:13 +0000

    Add package nester-fwdadaff/3.2.8/nester-fwdadaff-3.2.8.zip

---
+++ packages/nester-fwdadaff/nester-fwdadaff-3.2.8.zip/nester-fwdadaff-3.2.8/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='nester-fwdadaff',
+        version='3.2.8',
+        py_modules =['nester'],
+        author = 'fwdadaff',
+        author_email='13080684589@163.com',
+        url='http://www.headfirstlabs.com',
+        description='dfwe',
+     )
