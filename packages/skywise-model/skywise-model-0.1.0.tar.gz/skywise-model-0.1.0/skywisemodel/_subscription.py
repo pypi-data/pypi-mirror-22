5a04cbe7f16eed54e46806bcf9246f3e344b330b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:42 +0000

    Add package skywise-model/0.1.0/skywise-model-0.1.0.tar.gz

---
+++ packages/skywise-model/skywise-model-0.1.0.tar.gz/skywise-model-0.1.0/skywisemodel/_subscription.py
@@ -0,0 +1,47 @@
+from voluptuous import Any, Schema
+from . import ModelApiResource
+
+_subscription_deserialize = Schema({
+    'id': Any(None, str, unicode),
+    'model_id': Any(str, unicode),
+    'event': Any(str, unicode),
+    'subscriber_email': Any(str, unicode),
+    'options': dict
+})
+
+_subscription_serialize = Schema({
+    'id': Any(None, str, unicode),
+    'model_id': Any(str, unicode),
+    'event': Any(str, unicode),
+    'subscriber_email': Any(str, unicode),
+    'options': dict
+})
+
+
+class Subscription(ModelApiResource):
+
+    _path = '/models/{model_id}/subscriptions'
+
+    _deserialize = _subscription_deserialize
+
+    _serialize = _subscription_serialize
+
+    _args = Schema({
+        'event': Any(str, unicode),
+        'subscriber_email': Any(str, unicode)
+    })
+
+    @classmethod
+    def find(cls, id_=None, **kwargs):
+        if id_ is not None:
+            return _SubscriptionById.find(id_)
+        return super(Subscription, cls).find(**kwargs)
+
+
+class _SubscriptionById(ModelApiResource):
+
+    _path = '/subscriptions'
+
+    _deserialize = _subscription_deserialize
+
+    _serialize = _subscription_serialize
---
+++ packages/skywise-model/skywise-model-0.1.0.tar.gz/skywise-model-0.1.0/skywisemodel/forecast.py
@@ -0,0 +1,92 @@
+from voluptuous import Any, Schema
+from . import ModelApiResource
+from ._validation import datetime, datetime_to_str
+from ._variable import Variable
+
+STATUS_INITIALIZING = 'INITIALIZING'
+STATUS_INCOMPLETE = 'INCOMPLETE'
+STATUS_COMPLETE = 'COMPLETE'
+STATUS_EXPIRED = 'EXPIRED'
+
+_forecast_deserialize = Schema({
+    'id': Any(None, str, unicode),
+    'model': Any(str, unicode),
+    'createTime': datetime,
+    'initTime': datetime,
+    'variables': Any(str, unicode),
+    'status': Any(str, unicode),
+    'model_id': Any(str, unicode),
+    'expirationTime': datetime
+})
+
+_forecast_serialize = Schema({
+    'id': Any(None, str, unicode),
+    'model_id': Any(str, unicode),
+    'initTime': datetime_to_str,
+    'status': Any(str, unicode),
+    'expirationTime': datetime_to_str
+})
+
+
+class _ForecastMixin(object):
+
+    def get_variables(self, **kwargs):
+        return Variable.find(forecast_id=self.id, **kwargs)
+
+    def add_variable(self, platform_product_id, platform_forecast_id):
+        variable = Variable()
+        variable.forecast_id = self.id
+        variable.platform_forecast_product_id = platform_product_id
+        variable.platform_forecast_id = platform_forecast_id
+        variable.save()
+        return variable
+
+
+class Forecast(ModelApiResource, _ForecastMixin):
+
+    _path = '/models/{model_id}/forecasts'
+
+    _args = Schema({
+        'start': datetime_to_str,
+        'end': datetime_to_str,
+        'initTime': datetime_to_str,
+        'limit': int,
+        'sort': Any('asc', 'desc'),
+        'status': Any(str, unicode)
+    })
+
+    _deserialize = _forecast_deserialize
+
+    _serialize = _forecast_serialize
+
+    @classmethod
+    def find(cls, id_=None, **kwargs):
+        if id_ is not None:
+            return _ForecastById.find(id_)
+        return super(Forecast, cls).find(**kwargs)
+
+    def save(self, **kwargs):
+        if self.id:
+            forecast = _ForecastById()
+            forecast._data = self._data
+            forecast.save(**kwargs)
+            return forecast.id
+        return super(Forecast, self).save(**kwargs)
+
+
+class _ForecastById(ModelApiResource, _ForecastMixin):
+
+    _path = '/forecasts'
+
+    _deserialize = _forecast_deserialize
+
+    _serialize = _forecast_serialize
+
+
+class LatestForecast(ModelApiResource, _ForecastMixin):
+
+    _path = '/models/{model_id}/latest-forecast'
+
+    _deserialize = _forecast_deserialize
+
+    _serialize = _forecast_serialize
