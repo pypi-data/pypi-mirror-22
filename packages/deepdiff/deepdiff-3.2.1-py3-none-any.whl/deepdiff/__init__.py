1af6799acb29666b92ea94fdbbc861252393a8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:35 +0000

    Add package deepdiff/3.2.1/deepdiff-3.2.1-py3-none-any.whl

---
+++ packages/deepdiff/deepdiff-3.2.1-py3-none-any.whl/deepdiff/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s %(levelname)8s %(message)s')
+
+from .diff import DeepDiff
+from .search import DeepSearch, grep
+from .contenthash import DeepHash
+from .helper import py3
