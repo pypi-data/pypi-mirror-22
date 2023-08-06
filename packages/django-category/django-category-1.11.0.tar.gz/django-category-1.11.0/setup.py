0c18e454f3cc0144d44b7b688db41e0fd5d52f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:35 +0000

    Add package django-category/1.11.0/django-category-1.11.0.tar.gz

---
+++ packages/django-category/django-category-1.11.0.tar.gz/django-category-1.11.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-category",
+    version="1.11.0",
+    description="Django categorize content app.",
+    long_description = open("README.rst", "r").read() + open("AUTHORS.rst", "r").read() + open("CHANGELOG.rst", "r").read(),
+    author="Praekelt Consulting",
+    author_email="dev@praekelt.com",
+    license="BSD",
+    url="http://github.com/praekelt/django-category",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires = [
+        "django",
+        "django-object-tools>=1.9",
+        "South>=1.0.2"
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
+
