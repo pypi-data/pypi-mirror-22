50916d20d57e90a7238e7c9b13e0768f6c981e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:21 +0000

    Add package clubsandwich/0.1.0/clubsandwich-0.1.0.tar.gz

---
+++ packages/clubsandwich/clubsandwich-0.1.0.tar.gz/clubsandwich-0.1.0/clubsandwich/blt/state.py
@@ -0,0 +1,33 @@
+"""
+.. py:attribute:: state
+
+    Object with attributes that map to all BearLibTerminal
+    `event and state constants <http://foo.wyrd.name/en:bearlibterminal:reference:input>`_.
+
+    For example, ``blt_state.shift`` returns ``True`` iff the Shift key is
+    down. This is forwarded to
+    ``bearlibterminal.terminal.state(terminal.TK_SHIFT)``.
+
+    Numbers are prefixed with ``num_``. So ``blt_state.num_1`` is true iff the
+    "1" key is being pressed.
+"""
+from bearlibterminal import terminal
+
+
+class _TerminalState:
+    pass
+
+for constant_key in (c for c in dir(terminal) if c.startswith('TK_')):
+    def getter(k):
+        constant_value = getattr(terminal, k)
+        def get(self):
+            return terminal.state(constant_value)
+        return get
+    constant_name = constant_key[3:].lower()
+    attr_name = 'num_{}'.format(constant_name) if constant_name[0].isdigit() else constant_name
+    setattr(
+        _TerminalState,
+        attr_name,
+        property(getter(constant_key)))
+
+blt_state = _TerminalState()
---
+++ packages/clubsandwich/clubsandwich-0.1.0.tar.gz/clubsandwich-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clubsandwich',
+    version='0.1.0',
+    author='Steve Johnson',
+    author_email='steve@steveasleep.com',
+    license='MIT',
+    packages=find_packages(exclude=["docs"]),
+    url='http://steveasleep.com/clubsandwich',
+    install_requires=[
+        'bearlibterminal>=0.15',
+    ],
+    entry_points='''
+        [console_scripts]
+        babysit=clubsandwich.babysit:cli
+    ''',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
