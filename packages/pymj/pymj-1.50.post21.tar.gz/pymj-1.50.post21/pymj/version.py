a1e8909eb153fa14d33fe1eb069da73726c0b741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:28 +0000

    Add package pymj/1.50.post21/pymj-1.50.post21.tar.gz

---
+++ packages/pymj/pymj-1.50.post21.tar.gz/pymj-1.50.post21/pymj/version.py
@@ -0,0 +1,4 @@
+version="1.50-21"
+
+major, minor = version.split("-")
+minor = int(minor)
---
+++ packages/pymj/pymj-1.50.post21.tar.gz/pymj-1.50.post21/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from os.path import dirname, realpath
+from setuptools import find_packages, setup
+
+with open('pymj/version.py') as source_file:
+    exec(source_file.read())
+
+def _read_requirements_file():
+    req_file_path = '%s/requirements.txt' % dirname(realpath(__file__))
+    with open(req_file_path) as f:
+        return [line.strip() for line in f]
+
+
+setup(
+    name='pymj',
+    version=version,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=_read_requirements_file())
