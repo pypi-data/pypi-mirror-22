b7c6018bf749d49ffab1b4c23b5660ac7b14b2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:53 +0000

    Add package BattleShiPy/0.0.1/BattleShiPy-0.0.1.zip

---
+++ packages/BattleShiPy/BattleShiPy-0.0.1.zip/BattleShiPy-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'BattleShiPy',
+  packages = ['battleshipy'],
+  version = '0.0.1',
+  description = 'Visualized battleship game',
+  author = 'Bohdan Borkivskyy',
+  author_email = 'b.borkivskyy@ucu.edu.ua',
+  url = 'https://github.com/BohdanBorkivskyy',
+  download_url = 'https://github.com/BohdanBorkivskyy',
+  keywords = ['sea battle', 'battleship', 'flight'],
+  classifiers = [],
+  install_requires=['pygame==1.9.3']
+)
