9b0df6ff86ae4b5de17cce870f89539f288375ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:00 +0000

    Add package rapydo-utils/0.4.1/rapydo_utils-0.4.1.tar.gz

---
+++ packages/rapydo-utils/rapydo_utils-0.4.1.tar.gz/rapydo_utils-0.4.1/rapydo/utils/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.4.1'
+
+FRAMEWORK_NAME = 'RAPyDo'
+DEFAULT_FILENAME = 'projects_defaults'
+
+PROJECT_CONF_FILENAME = 'project_configuration'
+PROJECT_DIR = 'projects'
+SWAGGER_DIR = 'swagger'
+SWAGGER_MODELS_FILE = 'params_models'
+CONF_PATH = 'confs'
+CONTAINERS_YAML_DIRNAME = CONF_PATH
+
+UTILS_PKGNAME = __package__.split('.')[::-1][0]
