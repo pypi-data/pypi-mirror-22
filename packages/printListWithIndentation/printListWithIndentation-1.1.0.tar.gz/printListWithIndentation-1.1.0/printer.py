d4a74530c57db615053ab97efd12d173b728ef0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:51 +0000

    Add package printListWithIndentation/1.1.0/printListWithIndentation-1.1.0.tar.gz

---
+++ packages/printListWithIndentation/printListWithIndentation-1.1.0.tar.gz/printListWithIndentation-1.1.0/printer.py
@@ -0,0 +1,14 @@
+""" This module is designed to print
+all the atomic elements in the list.
+This is done using learnings from "Head First Python" """
+
+def printer(list_elem,level=0):
+    for each_item in list_elem:
+        if(isinstance(each_item,list)):
+            printer(each_item,level+1)
+        else:
+            for each_level in range(level):
+                print("\t",end="")
+            print(each_item)
+            
+        
