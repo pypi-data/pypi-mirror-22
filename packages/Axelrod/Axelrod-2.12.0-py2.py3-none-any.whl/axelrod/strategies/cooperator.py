bdcd6fbba787f0294e7c15f77f6ba0586b8d4028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:32 +0000

    Add package Axelrod/2.12.0/Axelrod-2.12.0-py2.py3-none-any.whl

---
+++ packages/Axelrod/Axelrod-2.12.0-py2.py3-none-any.whl/axelrod/strategies/cooperator.py
@@ -0,0 +1,59 @@
+from axelrod.actions import Actions, Action
+from axelrod.player import Player
+
+C, D = Actions.C, Actions.D
+
+
+class Cooperator(Player):
+    """A player who only ever cooperates."""
+
+    name = 'Cooperator'
+    classifier = {
+        'memory_depth': 0,
+        'stochastic': False,
+        'makes_use_of': set(),
+        'long_run_time': False,
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    @staticmethod
+    def strategy(opponent: Player) -> Action:
+        return C
+
+
+class TrickyCooperator(Player):
+    """A cooperator that is trying to be tricky."""
+
+    name = "Tricky Cooperator"
+    classifier = {
+        'memory_depth': 10,
+        'stochastic': False,
+        'makes_use_of': set(),
+        'long_run_time': False,
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    _min_history_required_to_try_trickiness = 3
+    _max_history_depth_for_trickiness = -10
+
+    def strategy(self, opponent: Player) -> Action:
+        """Almost always cooperates, but will try to trick the opponent by defecting.
+
+        Defect once in a while in order to get a better payout.
+        After 3 rounds, if opponent has not defected to a max history depth of 10, Defect.
+        """
+        if (self._has_played_enough_rounds_to_be_tricky() and
+                self._opponents_has_cooperated_enough_to_be_tricky(opponent)):
+            return D
+        return C
+
+    def _has_played_enough_rounds_to_be_tricky(self):
+        return len(self.history) >= self._min_history_required_to_try_trickiness
+
+    def _opponents_has_cooperated_enough_to_be_tricky(self, opponent):
+        rounds_to_be_checked = opponent.history[self._max_history_depth_for_trickiness:]
+        return D not in rounds_to_be_checked
