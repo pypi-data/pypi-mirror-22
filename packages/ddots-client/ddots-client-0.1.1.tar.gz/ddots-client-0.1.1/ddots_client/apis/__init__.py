e49b12497bb472c1cd7dfdd2c13d48b8a0fc644a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:43 +0000

    Add package ddots-client/0.1.1/ddots-client-0.1.1.tar.gz

---
+++ packages/ddots-client/ddots-client-0.1.1.tar.gz/ddots-client-0.1.1/ddots_client/apis/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .auth_api import AuthApi
+from .problems_api import ProblemsApi
+from .programming_languages_api import ProgrammingLanguagesApi
+from .solutions_api import SolutionsApi
+from .teams_api import TeamsApi
+from .users_api import UsersApi
