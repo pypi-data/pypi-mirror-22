68eeb6429175ae580b736c9a425fc78762a4c968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:41 +0000

    Add package cltk/0.1.48/cltk-0.1.48.tar.gz

---
+++ packages/cltk/cltk-0.1.48.tar.gz/cltk-0.1.48/cltk/corpus/multilingual/corpora.py
@@ -0,0 +1,13 @@
+"""Multilingual language corpora and software available for download or loading locally.
+All remote corpora hosted by github on the cltk organization account, eg:
+'http://github.com/cltk' + name
+"""
+
+MULTILINGUAL_CORPORA = [
+    {'encoding': 'utf-8',
+     'markup': ['conll', 'xml'],
+     'location': 'remote',
+     'type': 'treebank',
+     'origin': 'https://github.com/cltk/multilingual_treebank_proiel.git',
+     'name': 'multilingual_treebank_proiel'},
+]
