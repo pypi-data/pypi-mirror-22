d18a53d717556df100b894122b8cdbc7c9d05022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:13 +0000

    Add package mriqc/0.9.4/mriqc-0.9.4.tar.gz

---
+++ packages/mriqc/mriqc-0.9.4.tar.gz/mriqc-0.9.4/mriqc/__init__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+The mriqc package provides a series of :abbr:`NR (no-reference)`,
+:abbr:`IQMs (image quality metrics)` to used in :abbr:`QAPs (quality
+assessment protocols)` for :abbr:`MRI (magnetic resonance imaging)`.
+
+"""
+from __future__ import print_function, division, absolute_import, unicode_literals
+import logging
+
+from .__about__ import (
+    __version__,
+    __author__,
+    __email__,
+    __maintainer__,
+    __copyright__,
+    __credits__,
+    __license__,
+    __status__,
+    __description__,
+    __longdesc__,
+    __url__,
+    __download__,
+)
+
+
+LOG_FORMAT = '%(asctime)s %(name)s:%(levelname)s %(message)s'
+logging.basicConfig(level=logging.DEBUG,
+                    format=LOG_FORMAT)
+MRIQC_LOG = logging.getLogger()
+
+DEFAULTS = {
+    'ants_nthreads': 6,
+    'float32': False
+}
+
