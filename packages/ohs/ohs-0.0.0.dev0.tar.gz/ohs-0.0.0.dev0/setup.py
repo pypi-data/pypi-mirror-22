c7d866c98d1ffd058c0f5de920b8d329fed35b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:14 +0000

    Add package ohs/0.0.0.dev0/ohs-0.0.0.dev0.tar.gz

---
+++ packages/ohs/ohs-0.0.0.dev0.tar.gz/ohs-0.0.0.dev0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='ohs',
+    version='0.0.0.dev0',
+    description='Oregon Historical Society (namespace package)',
+    url='http://ohs.org/',
+    author='Portland State University - Office of Information Technology - Web Dev Team',
+    author_email='webteam@pdx.edu',
+    maintainer='Wyatt Baldwin',
+    maintainer_email='wbaldwin@pdx.edu',
+    packages=['ohs'],
+)
