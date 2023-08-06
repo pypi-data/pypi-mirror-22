d35b211e26c249e2634fcc452773361cf2627d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:19 +0000

    Add package styleframe/1.0/StyleFrame-1.0-py2.py3-none-any.whl

---
+++ packages/styleframe/StyleFrame-1.0-py2.py3-none-any.whl/StyleFrame/__init__.py
@@ -0,0 +1,27 @@
+import sys
+
+# Python 2
+if sys.version_info < (3, 0):
+    # noinspection PyUnresolvedReferences
+    from style_frame import StyleFrame
+    # noinspection PyUnresolvedReferences
+    from container import Container
+    # noinspection PyUnresolvedReferences
+    from styler import Styler
+    # noinspection PyUnresolvedReferences
+    import utils
+    # noinspection PyUnresolvedReferences,PyPackageRequirements
+    from version import _version_, _versions_, _openpyxl_version_, _pandas_version_, _python_version_
+
+# Python 3
+else:
+    from StyleFrame.style_frame import StyleFrame
+    from StyleFrame.container import Container
+    from StyleFrame.styler import Styler
+    from StyleFrame import utils
+    from StyleFrame.version import _version_, _versions_, _openpyxl_version_, _pandas_version_, _python_version_
+
+from StyleFrame import warnings_conf
+
+if 'utrunner' not in sys.argv[0]:
+    from StyleFrame.tests import style_frame_tests as tests
