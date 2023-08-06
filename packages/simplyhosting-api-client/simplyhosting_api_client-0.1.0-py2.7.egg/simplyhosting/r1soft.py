be810580b7a60802d5df34342d1734f95e5406bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:59 +0000

    Add package simplyhosting-api-client/0.1.0/simplyhosting_api_client-0.1.0-py2.7.egg

---
+++ packages/simplyhosting-api-client/simplyhosting_api_client-0.1.0-py2.7.egg/simplyhosting/r1soft.py
@@ -0,0 +1,3 @@
+class R1Soft(object):
+    def __init__(self, apiClient):
+        self.apiClient = apiClient
---
+++ packages/simplyhosting-api-client/simplyhosting_api_client-0.1.0-py2.7.egg/simplyhosting/reseller_vlan.py
@@ -0,0 +1,3 @@
+class ResellerVlan(object):
+    def __init__(self, apiClient):
+        self.apiClient = apiClient
