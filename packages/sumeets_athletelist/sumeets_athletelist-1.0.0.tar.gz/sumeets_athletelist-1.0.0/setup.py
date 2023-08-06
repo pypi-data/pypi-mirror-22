ef3c2b3ba8c0263b0ac730ff8445b46eaa702416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:18 +0000

    Add package sumeets_athletelist/1.0.0/sumeets_athletelist-1.0.0.tar.gz

---
+++ packages/sumeets_athletelist/sumeets_athletelist-1.0.0.tar.gz/sumeets_athletelist-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+       name="sumeets_athletelist",
+       version='1.0.0',
+       py_modules=["sumeets_athletelist"],
+       author='sumeet',
+       author_email='sumeetmishra1991@gmail.com',
+       url='',
+       description="list of athlete's 3 best times ",
+     )
