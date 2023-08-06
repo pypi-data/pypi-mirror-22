d4de943da32b8224b3051192aee2d5d971585ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:14 +0000

    Add package pmagpy-cli/3.9.1.post5/pmagpy-cli-3.9.1.post5.tar.gz

---
+++ packages/pmagpy-cli/pmagpy-cli-3.9.1.post5.tar.gz/pmagpy-cli-3.9.1.post5/pmagpy_tests/__init__.py
@@ -0,0 +1,27 @@
+import matplotlib
+matplotlib.use('WXAgg')
+import sys
+import os
+if "-pip" in sys.argv:
+    os.chdir(sys.prefix)
+
+import test_builder
+import test_er_magic_dialogs
+import test_ipmag
+import test_magic_gui
+import test_pmag
+import test_pmag_gui
+import test_thellier_gui
+import test_validations
+import test_programs
+import test_demag_gui
+import test_magic_gui2
+import test_new_builder
+import test_find_pmag_dir
+import test_map_magic
+
+__all__ = [test_builder, test_er_magic_dialogs, test_ipmag,
+           test_magic_gui, test_pmag, test_pmag_gui, test_thellier_gui,
+           test_validations, test_programs, test_demag_gui,
+           test_magic_gui2, test_new_builder, test_find_pmag_dir,
+           test_map_magic]
