96277e14a18d538f70bb1f099900e105c205b656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:09 +0000

    Add package jerelyn/0.1.6/jerelyn-0.1.6.tar.gz

---
+++ packages/jerelyn/jerelyn-0.1.6.tar.gz/jerelyn-0.1.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='jerelyn',
+      description='An English/Filipino to Jejemon-speak translator',
+      long_description='This tiny library promptly \
+       converts any string to an equivalent Jejemon-inspired counterpart.',
+      version='0.1.6',
+      url='https://github.com/hadrianpaulo/jerelyn',
+      author='Hadrian Paulo Lim',
+      author_email='hadrianpaulo@gmail.com',
+      license='Apache2',
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3'
+      ],
+      packages=['jerelyn'],
+      install_requires=[
+      ],
+     )
