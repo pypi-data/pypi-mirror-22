9420d54d91d44f774d374b23e22cb71a40dc37cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:47 +0000

    Add package servicemanager/1.0.0/servicemanager-1.0.0.tar.gz

---
+++ packages/servicemanager/servicemanager-1.0.0.tar.gz/servicemanager-1.0.0/servicemanager/smfile.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+
+def remove_if_exists(path):
+    if os.path.exists(path):
+        os.remove(path)
+
+
+def remove_folder_if_exists(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+
+def makedirs_if_not_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def force_chdir(path):
+    makedirs_if_not_exists(path)
+    os.chdir(path)
+
+
+def force_pushdir(path):
+    makedirs_if_not_exists(path)
+    os.chdir(path)
