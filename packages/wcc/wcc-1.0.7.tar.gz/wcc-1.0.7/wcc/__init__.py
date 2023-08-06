3f5d9e61b319fab8b83a31c179886aa78686d2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:56 +0000

    Add package wcc/1.0.7/wcc-1.0.7.tar.gz

---
+++ packages/wcc/wcc-1.0.7.tar.gz/wcc-1.0.7/wcc/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.0.7'
+#从本文件夹里import models.py和exceptions.py
+#from . import models, exceptions
+from . import osskey
+#从本目录下api.py里import Service和Bucket这个类
+#from .api import Service, Bucket
+from .osskey import osskey
