201db8a91954d6a23061fbe3892a3a9316523aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:33 +0000

    Add package openbrokerapi/0.2.0/openbrokerapi-0.2.0.tar.gz

---
+++ packages/openbrokerapi/openbrokerapi-0.2.0.tar.gz/openbrokerapi-0.2.0/openbrokerapi/response.py
@@ -0,0 +1,82 @@
+from typing import List
+
+from openbrokerapi.catalog import Service
+
+
+class EmptyResponse:
+    pass
+
+
+class ErrorResponse:
+    def __init__(self,
+                 error: str = None,
+                 description: str = None):
+        self.error = error
+        self.description = description
+
+
+class CatalogResponse:
+    def __init__(self, services: List[Service]):
+        self.services = services
+
+
+class ProvisioningResponse:
+    def __init__(self,
+                 dashboard_url: str,
+                 operation: str):
+        self.dashboard_url = dashboard_url
+        self.operation = operation
+
+
+class UpdateResponse:
+    def __init__(self, operation: str):
+        self.operation = operation
+
+
+class DeprovisionResponse:
+    def __init__(self, operation: str):
+        self.operation = operation
+
+
+class LastOperationResponse:
+    def __init__(self,
+                 state: dict,  # LastOperationState,
+                 description: str
+                 ):
+        self.state = state
+        self.description = description
+
+
+class ExperimentalVolumeMountPrivate:
+    def __init__(self,
+                 driver: str,
+                 group_id: str,
+                 config: str
+                 ):
+        self.driver = driver
+        self.group_id = group_id
+        self.config = config
+
+
+class ExperimentalVolumeMount:
+    def __init__(self,
+                 container_path: str,
+                 mode: str,
+                 private: ExperimentalVolumeMountPrivate
+                 ):
+        self.container_path = container_path
+        self.mode = mode
+        self.private = private
+
+
+class ExperimentalVolumeMountBindingResponse:
+    def __init__(self,
+                 credentials: dict,
+                 syslog_drain_url: str,
+                 route_service_url: str,
+                 volume_mounts: List[ExperimentalVolumeMount]
+                 ):
+        self.credentials = credentials
+        self.syslog_drain_url = syslog_drain_url
+        self.route_service_url = route_service_url
+        self.volume_mounts = volume_mounts
---
+++ packages/openbrokerapi/openbrokerapi-0.2.0.tar.gz/openbrokerapi-0.2.0/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+
+def get_long_description():
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except (IOError, ImportError, RuntimeError):
+        print('WARNING: Please install pypandoc and pandoc. Otherwise long description will not be available.')
+
+
+setup(
+    name='openbrokerapi',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=['flask'],
+    test_suite="test",
+
+    # Metadata
+    author="Maic Siemering",
+    author_email="eruvanos@ewetel.net",
+    license="MIT",
+    description="A python package for the V2 CF Service Broker API and Open Broker API",
+    long_description=get_long_description(),
+    url="https://github.com/eruvanos/openbrokerapi",
+    keywords=["cloudfoundry", "cfbrokerapi", "openbrokerapi", "servicebroker", "flask"],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+    ],
+
+)
