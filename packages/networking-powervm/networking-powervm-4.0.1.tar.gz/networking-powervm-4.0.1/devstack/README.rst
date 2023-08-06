f9519bbab542324a392757e85c3eeaa5ffc6f8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:51 +0000

    Add package networking-powervm/4.0.1/networking-powervm-4.0.1.tar.gz

---
+++ packages/networking-powervm/networking-powervm-4.0.1.tar.gz/networking-powervm-4.0.1/devstack/README.rst
@@ -0,0 +1,39 @@
+========================
+Installing with Devstack
+========================
+
+1. Download DevStack::
+
+    $ git clone https://git.openstack.org/openstack-dev/devstack /opt/stack/devstack
+
+2. Modify DevStack's local.conf to pull in this project by adding::
+
+    [[local|localrc]]
+    ...
+    enable_plugin networking-powervm http://git.openstack.org/openstack/networking-powervm
+
+   Example files are available in the nova-powervm project to provide
+   reference on using this driver with the corresponding nova-powervm
+   and ceilometer-powervm drivers. Following these example files will enable
+   the appropriate drivers and services for each node type. Example config
+   files for all-in-one, compute, and control nodes `can be found here. <https://github.com/openstack/nova-powervm/tree/master/devstack>`_
+
+3. See networking-powervm/doc/source/devref/usage.rst, then configure the
+   installation through options in local.conf as needed for your environment.
+   The Q_PLUGIN_CONF_FILE (ML2) options are only needed for advanced configurations.::
+
+    [[local|localrc]]
+    ...
+    Q_PLUGIN=ml2
+    Q_ML2_TENANT_NETWORK_TYPE=vlan
+    Q_ML2_PLUGIN_TYPE_DRIVERS=vlan
+
+    [[post-config|/$Q_PLUGIN_CONF_FILE]]
+    [agent]
+    bridge_mappings = ''
+    automated_powervm_vlan_cleanup = True
+
+4. Run ``stack.sh`` from devstack::
+
+    $ cd /opt/stack/devstack
+    $ ./stack.sh
---
+++ packages/networking-powervm/networking-powervm-4.0.1.tar.gz/networking-powervm-4.0.1/networking_powervm/plugins/ibm/agent/powervm/exceptions.py
@@ -0,0 +1,39 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron_lib import exceptions
+
+from networking_powervm._i18n import _LE
+
+
+class MultipleHostsFound(exceptions.NeutronException):
+    message = _LE("Expected exactly one host; found %(host_count)d.")
+
+
+class NoNetworkBridges(exceptions.NeutronException):
+    message = _LE('There are no network bridges (Shared Ethernet Adapters) on '
+                  'the system.  Can not start the Neutron agent.')
+
+
+class MultiBridgeNoMapping(exceptions.NeutronException):
+    message = _LE('The system has more than one network bridge, but the '
+                  'bridge_mappings have not been specified.  Please configure '
+                  'the bridge_mappings before proceeding.')
+
+
+class DeviceNotFound(exceptions.NeutronException):
+    message = _LE('Device %(dev)s on Virtual I/O Server %(vios)s was not '
+                  'found.  Unable to set up physical network %(phys_net)s.')
