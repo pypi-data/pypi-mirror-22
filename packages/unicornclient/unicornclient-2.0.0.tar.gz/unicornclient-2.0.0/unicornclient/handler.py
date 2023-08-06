ec3f833b970141feabb06c69d14e5e83596ad317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:36 +0000

    Add package unicornclient/2.0.0/unicornclient-2.0.0.tar.gz

---
+++ packages/unicornclient/unicornclient-2.0.0.tar.gz/unicornclient-2.0.0/unicornclient/handler.py
@@ -0,0 +1,26 @@
+import json
+
+class MissingInfoException(Exception):
+    pass
+
+class Handler(object):
+    def __init__(self, manager):
+        self.manager = manager
+        self.sender = None
+
+        self.last_ping_id = None
+
+    def set_sender(self, sender):
+        self.sender = sender
+
+    def handle(self, message):
+        payload = json.loads(message)
+
+        if 'type' not in payload:
+            print("Ignored because no type")
+            return None
+
+        payload_type = payload['type']
+        payload.pop('type')
+
+        self.manager.forward(payload_type, payload)
---
+++ packages/unicornclient/unicornclient-2.0.0.tar.gz/unicornclient-2.0.0/unicornclient/routines/status.py
@@ -0,0 +1,13 @@
+import threading
+import queue
+
+class Routine(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.queue = queue.Queue()
+
+    def run(self):
+        while True:
+            self.queue.get()
+            self.sender.send_status()
+            self.queue.task_done()
