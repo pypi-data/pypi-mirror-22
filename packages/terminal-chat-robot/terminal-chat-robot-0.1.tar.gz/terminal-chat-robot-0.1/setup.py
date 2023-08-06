2b6a5033605925006705dc5f928049f47dd7cca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:52 +0000

    Add package terminal-chat-robot/0.1/terminal-chat-robot-0.1.tar.gz

---
+++ packages/terminal-chat-robot/terminal-chat-robot-0.1.tar.gz/terminal-chat-robot-0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='terminal-chat-robot',
+    version='0.1',
+    keywords=('terminal', 'chat', 'robot'),
+    description='It is a terminal robot which can chat with you.',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    license='MIT License',
+    install_requires=['requests'],
+
+    author='xingbofeng',
+    author_email='503908971@qq.com',
+
+    packages=find_packages(),
+    platforms='any',
+)
