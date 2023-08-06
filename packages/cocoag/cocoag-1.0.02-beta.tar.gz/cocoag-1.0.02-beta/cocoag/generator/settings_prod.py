1b4162ae42830cbbd422995919036e8b93be423b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:15 +0000

    Add package cocoag/1.0.02-beta/cocoag-1.0.02-beta.tar.gz

---
+++ packages/cocoag/cocoag-1.0.02-beta.tar.gz/cocoag-1.0.02-beta/cocoag/generator/settings_prod.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+
+# TODO: Move these to some kind of a common settings file
+current_file_path = os.path.realpath(__file__)
+GENERATOR_DIR_PATH = Path(current_file_path).parent
+PACKAGE_ROOT_PATH = Path(GENERATOR_DIR_PATH).parent
+S3_DIR_PATH = os.path.join(PACKAGE_ROOT_PATH, "s3")
+
+# The location of the default config file the generator should use.
+# You can change this to whatever you like. It defaults to the `coverage_gen_settings.py` file in this directory.
+DEFAULT_CONFIG_FILE = "/Users/stard/development/cocoag/cocoag/generator/coverage_gen_settings_prod.json"
+LOGGING_OUTPUT_FILE_DIR = "/Users/stard/development/cocoag/cocoag/generator/generator.log"
+OUTPUT_FILE_NAME = "cocoag.svg"
