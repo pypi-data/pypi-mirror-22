b59d3b7df41821f55530ede47220ab1c4869dd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:15 +0000

    Add package argpar/1.2.0/argpar-1.2.0.tar.gz

---
+++ packages/argpar/argpar-1.2.0.tar.gz/argpar-1.2.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name="argpar",
+	  version="1.2.0",
+	  description="A simple arugments parser.",
+	  long_description=readme(),
+	  url="https://github.com/ondra6ak/argpar.git",
+	  author="Ondrej Sestak",
+	  author_email="ondra6ak@gmail.com",
+	  license="BSD",
+	  packages=["argpar"],
+	  zip_safe=False)
