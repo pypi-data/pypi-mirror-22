a6e58c3508b256c9fe983b67819a3bc503130165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:14 +0000

    Add package lalaland/1.0.5/lalaland-1.0.5.tar.gz

---
+++ packages/lalaland/lalaland-1.0.5.tar.gz/lalaland-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		='lalaland',
+	version		 = '1.0.5',
+	py_modules	= ['lalaland'],
+	author		= 'brainy',
+	author_email	= 'brainysoon@163.com',
+	url		= 'http://www.coolbhu.cn',
+	description	= 'A Simple printer for list'
+	)	
