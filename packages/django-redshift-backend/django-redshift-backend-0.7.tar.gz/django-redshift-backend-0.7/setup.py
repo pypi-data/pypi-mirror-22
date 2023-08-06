9146952aa67b551d099e4cc6311d2ca258d917e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:39 +0000

    Add package django-redshift-backend/0.7/django-redshift-backend-0.7.tar.gz

---
+++ packages/django-redshift-backend/django-redshift-backend-0.7.tar.gz/django-redshift-backend-0.7/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'django',
+    'psycopg2',
+]
+
+
+def read(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+setup(
+    name='django-redshift-backend',
+    version='0.7',
+    packages=find_packages(),
+    url='https://github.com/shimizukawa/django-redshift-backend',
+    license='Apache Software License',
+    author='shimizukawa',
+    author_email='shimizukawa@gmail.com',
+    description='Redshift database backend for Django',
+    long_description=read('README.rst'),
+    install_requires=requires,
+)
+
