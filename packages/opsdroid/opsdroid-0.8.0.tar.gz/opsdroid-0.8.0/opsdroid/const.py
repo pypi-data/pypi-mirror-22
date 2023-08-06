ef769b01860149e9c53befaeec6c5c62433261b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:49 +0000

    Add package opsdroid/0.8.0/opsdroid-0.8.0.tar.gz

---
+++ packages/opsdroid/opsdroid-0.8.0.tar.gz/opsdroid-0.8.0/opsdroid/const.py
@@ -0,0 +1,9 @@
+"""Constants used by OpsDroid."""
+
+__version__ = "0.8.0"
+
+LOG_FILENAME = 'output.log'
+DEFAULT_GIT_URL = "https://github.com/opsdroid/"
+MODULES_DIRECTORY = "opsdroid-modules"
+DEFAULT_MODULES_PATH = "~/.opsdroid/modules"
+DEFAULT_MODULE_BRANCH = "master"
