22e534ac9e58d02eac0be6e021a29fb6104d1ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:14 +0000

    Add package clearly/0.2.4/clearly-0.2.4.tar.gz

---
+++ packages/clearly/clearly-0.2.4.tar.gz/clearly-0.2.4/clearly/__init__.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
+__author__ = 'Rogério Sampaio de Almeida'
+
+VERSION = (0, 2, 4)  # 'major', 'minor', 'release'
+__version__ = '.'.join(map(str, VERSION))
+
+from .client import ClearlyClient
+
+__all__ = ('__author__', '__version__', 'ClearlyClient',)
+
+from kombu import log
+import logging
+
+log.get_logger('').setLevel(logging.ERROR)
