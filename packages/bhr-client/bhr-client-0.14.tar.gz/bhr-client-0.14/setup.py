fbebb2fafab7bae92ff97f5a7b8eb01b38b4d74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:19 +0000

    Add package bhr-client/0.14/bhr-client-0.14.tar.gz

---
+++ packages/bhr-client/bhr-client-0.14.tar.gz/bhr-client-0.14/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '0.14'
+
+setup(name='bhr-client',
+    version=version,
+    description="BHR Client",
+    long_description="",
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='passive dns',
+    author='Justin Azoff',
+    author_email='JAzoff@illinois.edu',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "requests>=2.0",
+    ],
+    extras_require = {
+        'cli' : ['Click'],
+    },
+    entry_points = {
+        'console_scripts': [
+            'bhr-client = bhr_client.cli:main',
+            'bhr-client-run-stdout = bhr_client.run:main',
+        ]
+    }
+)
