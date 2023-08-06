e734b97b93e13fd1b7c6508613cefdcd729d3b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:12 +0000

    Add package hologram-python/0.5.11/hologram-python-0.5.11.tar.gz

---
+++ packages/hologram-python/hologram-python-0.5.11.tar.gz/hologram-python-0.5.11/Hologram/Network/Network.py
@@ -0,0 +1,42 @@
+# Network.py - Hologram Python SDK Network interface
+#
+# Author: Hologram <support@hologram.io>
+#
+# Copyright 2016 - Hologram (Konekt, Inc.)
+#
+#
+# LICENSE: Distributed under the terms of the MIT License
+#
+from ..Event import Event
+import os
+import logging
+
+class Network(object):
+
+    def __repr__(self):
+        return type(self).__name__
+
+    def __init__(self, event=Event()):
+        self.event = event
+        # Logging setup.
+        self.logger = logging.getLogger(type(self).__name__)
+
+    def connect(self):
+        self.event.broadcast('network.connected')
+
+    def disconnect(self):
+        self.event.broadcast('network.disconnected')
+
+    def reconnect(self):
+        raise NotImplementedError('Must instantiate a defined Network type')
+
+    def getConnectionStatus(self):
+        raise NotImplementedError('Must instantiate a defined Network type')
+
+    @property
+    def interfaceName(self):
+        return self._interfaceName
+
+    @interfaceName.setter
+    def interfaceName(self, name):
+        self._interfaceName = name
---
+++ packages/hologram-python/hologram-python-0.5.11.tar.gz/hologram-python-0.5.11/scripts/examples/example-receive-sms-iota.py
@@ -0,0 +1,48 @@
+#
+# example-receive-sms-iota.py - Example of receiving SMS on the iota.
+#
+# Author: Hologram <support@hologram.io>
+#
+# Copyright 2016 - Hologram (Konekt, Inc.)
+#
+# LICENSE: Distributed under the terms of the MIT License
+#
+
+import sys
+import time
+sys.path.append(".")
+sys.path.append("..")
+sys.path.append("../..")
+
+from Hologram.HologramCloud import HologramCloud
+
+if __name__ == "__main__":
+    print ""
+    print ""
+    print "Testing Hologram Cloud class..."
+    print ""
+    print "* Note: You can obtain device keys from the Devices page"
+    print "* at https://dashboard.hologram.io"
+    print ""
+
+    device_key = raw_input("What is your device key? ")
+
+    credentials = {'devicekey': device_key}
+
+    hologram = HologramCloud(credentials, enable_inbound=False, network='cellular-iota')
+
+    hologram.enableSMS()
+
+    print 'waiting for 20 seconds...'
+    time.sleep(20)
+
+    hologram.disableSMS()
+
+    sms_obj = hologram.popReceivedSMS()
+
+    if sms_obj is None:
+        print 'sms_obj: ' + str(sms_obj)
+    else:
+        print 'sender: ' + str(sms_obj.sender)
+        print sms_obj.timestamp
+        print 'message: ' + str(sms_obj.message)
