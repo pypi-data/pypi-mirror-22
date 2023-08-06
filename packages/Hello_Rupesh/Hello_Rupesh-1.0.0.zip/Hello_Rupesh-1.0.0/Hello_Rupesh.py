fbae9c96423c562f5bd0aa33472dacef8f850c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:44 +0000

    Add package Hello_Rupesh/1.0.0/Hello_Rupesh-1.0.0.zip

---
+++ packages/Hello_Rupesh/Hello_Rupesh-1.0.0.zip/Hello_Rupesh-1.0.0/Hello_Rupesh.py
@@ -0,0 +1,7 @@
+''' this function prints welcome to python before the name pass
+    as a parameter to the function '''
+
+def Say_Hello(name):
+    print("Hello ")
+    print(name)
+    
---
+++ packages/Hello_Rupesh/Hello_Rupesh-1.0.0.zip/Hello_Rupesh-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+
+		name = 'Hello_Rupesh',
+		version = '1.0.0',
+		py_modules = ['Hello_Rupesh'],
+		author = 'Rupesh Yadav',
+		description = 'A simple hello program',
+	 )
