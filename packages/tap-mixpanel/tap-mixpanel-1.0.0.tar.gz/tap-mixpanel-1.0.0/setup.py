3537964a9321396ea91945ea0658ea528bfc5624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:27 +0000

    Add package tap-mixpanel/1.0.0/tap-mixpanel-1.0.0.tar.gz

---
+++ packages/tap-mixpanel/tap-mixpanel-1.0.0.tar.gz/tap-mixpanel-1.0.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tap-mixpanel',
+      version='1.0.0',
+      description='Singer.io tap for extracting data from the Mixpanel API',
+      author='Kieran Lynn',
+      author_email='Kieran.J.Lynn@gmail.com',
+      url='https://github.com/Kierchon/tap-mixpanel',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_mixpanel'],
+      install_requires=['singer-python==1.5.0',
+                        'requests>=2.12.4'],
+      entry_points='''
+          [console_scripts]
+          tap-mixpanel=tap_mixpanel:main
+      ''',
+      packages=['tap_mixpanel'],
+      package_data={
+          'tap_mixpanel/schemas': [
+              'events.json'
+              ]
+          },
+      include_package_data=True,
+)
