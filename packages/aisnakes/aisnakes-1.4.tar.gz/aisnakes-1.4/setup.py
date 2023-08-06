55c8491ca9f06e72d8d3aca5adaa1aaa2beca9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:16 +0000

    Add package aisnakes/1.4/aisnakes-1.4.tar.gz

---
+++ packages/aisnakes/aisnakes-1.4.tar.gz/aisnakes-1.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="aisnakes",
+    version="1.4",
+    scripts=["aisnakes", "config.py", "brains.py", "snakes.py"],
+    install_requires=["numpy", "arcade"],
+
+    author="JanKaifer",
+    author_email="kaifer741@gmail.com",
+    description="Snakes learning to live with one vs all perceptrons",
+    keywords=["snake", "ai", "perceptron", "machine", "learning", "cool", "useless"],
+    url="https://github.com/JanKaifer/AISnakes",
+    download_url="https://github.com/JanKaifer/AISnakes/tarball/master"
+)
