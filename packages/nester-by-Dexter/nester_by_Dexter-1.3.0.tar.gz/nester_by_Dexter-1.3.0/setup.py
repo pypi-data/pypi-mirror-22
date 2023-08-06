d00e19ee6bc33df104d511d99b8c5d569d0ccf95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:37 +0000

    Add package nester-by-Dexter/1.3.0/nester_by_Dexter-1.3.0.tar.gz

---
+++ packages/nester-by-Dexter/nester_by_Dexter-1.3.0.tar.gz/nester_by_Dexter-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "nester_by_Dexter",
+    version = "1.3.0",
+    description = "A simple printer of listception",
+    author = "Dexter",
+    author_email = "Dextermaddock1006@gmail.com",
+    url = "http://www.Idonthaveaurl.com",
+    
+    )
