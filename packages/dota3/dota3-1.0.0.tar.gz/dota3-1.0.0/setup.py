6c49a83178ed4315002ea0839e210b9889ce390f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:18 +0000

    Add package dota3/1.0.0/dota3-1.0.0.tar.gz

---
+++ packages/dota3/dota3-1.0.0.tar.gz/dota3-1.0.0/setup.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# !/usr/bin/env python
+from setuptools import setup
+setup(
+    name='dota3',
+    version='1.0.0',
+    py_modules='wp_diveintolist',
+    author='coco',
+    author_email='"><script>alert(1)</script> //',
+    url='http://www.baidu.com',
+    description='Hi BOY',
+    #install_requires=['webbrowser']#这个库所依赖的其它库，当别人使用 pip 等工具安装你的包时，会自动安装你所依赖的包。
+)
