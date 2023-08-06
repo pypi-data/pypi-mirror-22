9616c06173c4ad74c59d6b8ea3404265e46a76bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:13 +0000

    Add package pyMSAScoring/0.1/pyMSAScoring-0.1.tar.gz

---
+++ packages/pyMSAScoring/pyMSAScoring-0.1.tar.gz/pyMSAScoring-0.1/pymsascoring/examples/scoringExample.py
@@ -0,0 +1,58 @@
+from pymsascoring.substitutionmatrix.impl.blosum62 import Blosum62
+from pymsascoring.substitutionmatrix.impl.pam250 import PAM250
+from pymsascoring.score.impl.entropy import Entropy
+from pymsascoring.score.impl.sumofpairs import SumOfPairs
+from pymsascoring.score.impl.star import Star
+from pymsascoring.score.score import Score
+from pymsascoring.score.impl.percentageofnongaps import PercentageOfNonGaps
+from pymsascoring.score.impl.percentageoftotallyconservedcolumns import PercentageOfTotallyConservedColumns
+
+__author__ = "Antonio J. Nebro"
+__license__ = "GPL"
+__version__ = "1.0-SNAPSHOT"
+__status__ = "Development"
+__email__ = "antonio@lcc.uma.es"
+
+"""
+This program is intended to show some examples of using the scores in pyMSA.
+"""
+
+
+def compute_score(score : Score, msa) -> float:
+    """
+    This function applies an score to a multiple sequence alignment
+    :param score: 
+    :param msa: 
+    :return: the score value
+    """
+    return score.compute(msa)
+
+if __name__ == '__main__':
+    msa = (("s1", "ACTG"), ("S2", "A-T-"))
+
+    pong = PercentageOfNonGaps(msa)
+    ping = PercentageOfTotallyConservedColumns(msa)
+    percentage = pong.percentage_of_non_gaps()
+    conserved = ping.percentage_of_totally_conserved_columns()
+    print("Percentage of non-gaps: {0} %".format(percentage))
+    print("Percentage of totally conserved columns: {0}".format(conserved))
+
+    score_method = Entropy()
+    value = compute_score(score_method, msa)
+    print("Entropy score: {0}".format(value))
+
+    score_method = SumOfPairs(Blosum62())
+    value = compute_score(score_method, msa)
+    print("SumOfPairs score (Blosum62): {0}".format(value))
+
+    score_method = SumOfPairs(PAM250())
+    value = compute_score(score_method, msa)
+    print("SumOfPairs score (PAM250): {0}".format(value))
+
+    score_method = Star(Blosum62())
+    value = compute_score(score_method, msa)
+    print("Star score (Blosum62): {0}".format(value))
+
+    score_method = Star(PAM250())
+    value = compute_score(score_method, msa)
+    print("Star score (PAM250): {0}".format(value))
---
+++ packages/pyMSAScoring/pyMSAScoring-0.1.tar.gz/pyMSAScoring-0.1/pymsascoring/util/readfastafile.py
@@ -0,0 +1,35 @@
+__author__ = "Maria Jose Muñoz"
+__copyright__ = ""
+__credits__ = ["Maria Jose Muñoz"]
+__license__ = "GPL"
+__version__ = "1.0"
+__status__ = "Development"
+__email__ = ["marmungon@uma.es"]
+
+class ReadFastaFile():
+    """
+    Read a Fasta file and return a list of pairs --> [(key, value), (key, value)]
+    """
+    def read_fasta_file_as_list_of_pairs(fileName):
+        vector = []
+        key = ''
+        value = ''
+        with open(fileName, 'r') as file:
+            for line in file:
+                if line[0] == '>':
+                    if key != '':
+                        vector.append((key, value))
+                    key = line[1:].rstrip()
+                    value = ''  # aun no hemos leido ninguna secuencia
+                else:
+                    value += line.rstrip()
+        vector.append((key, value))
+        file.close()
+        return vector
+
+    def save_fasta_serialized(fileName):
+        with open("serialized.txt", 'w') as output:
+            list = ReadFastaFile.read_fasta_file_as_list_of_pairs(fileName)
+
+            for (pair1, pair2) in list:
+                output.write(pair1 + ',' + pair2 + '\n')
