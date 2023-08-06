b3acda6a3f2cdb1f77815f27fc00390d4f784b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:31 +0000

    Add package pytgasu/0.2.0/pytgasu-0.2.0.tar.gz

---
+++ packages/pytgasu/pytgasu-0.2.0.tar.gz/pytgasu-0.2.0/README.rst
@@ -0,0 +1,118 @@
+=======
+PyTgASU
+=======
+
+Telegram sticker sets creation automated (partially).
+
+Features
+--------
+PyTgASU frees you from selecting files and emojis repeatedly when creating sticker sets on Telegram.
+
+Better yet, it makes sticker sets kind of "distributable" and "installable".
+
+Motivation
+----------
+There does is a `Telegram Stickers Uploader <http://telegramsu.lostberry.com/>`_, but:
+
+1. It has limited choices of emojis
+    - Well, 250. Really? This is not enough. UTR #51 4.0 defined 910 code points with Emoji_Presentation=Yes.
+
+2. It does not work with current Telegram Desktop
+    - It does send commands through it, but it cannot upload anything. At least not for me.
+
+And now ``pytgasu`` comes to your rescue.
+
+Installation
+------------
+
+Requirement
++++++++++++
+- Python >= 3.5
+
+Dependency
+++++++++++
+- `Telethon <https://github.com/LonamiWebs/Telethon>`_
+- `Click <http://github.com/mitsuhiko/click>`_
+- `regex <https://bitbucket.org/mrabarnett/mrab-regex>`_
+- `Pillow <https://python-pillow.org/>`_
+
+Use ``pip`` to install:
+
+.. code-block:: bash
+
+    $ pip install pytgasu
+
+or
+
+.. code-block:: bash
+
+   $ git clone https://github.com/alemonmk/pytgasu.git
+   $ cd pytgasu
+   $ pip install .
+
+Usage
+-----
+
+Generate set definition
++++++++++++++++++++++++
+You need set definition file for each sticker set you want to create.
+
+Generate definition files by running and enter some detail:
+
+.. code-block:: bash
+
+    $ pytgasu defgen <dir>...
+
+Then open the generate ``.ssd`` file with text editor of your choice to assign emojis (and **only** emojis, preferably copied from Telegram).
+
+Upload sticker sets
++++++++++++++++++++
+Once you are done editing the ``.ssd`` file(s), you can let ``pytgasu`` do the heavy lifting.
+
+.. code-block:: bash
+
+    $ pytgasu upload (<dir>|<path_to.ssd>)...
+
+By specifying ``upload -s``, it also automatically subscribe to the set once it's uploaded.
+
+You have to log in to Telegram at the first run, it won't ask you again after that. A Telegram session file will be created at ``~/.pytgasu/asu.session``.
+
+Log out of Telegram
++++++++++++++++++++
+If you have no business with ``pytgasu`` anymore, you may want to log it out from Telegram.
+
+.. code-block:: bash
+
+    $ pytgasu logout
+
+This terminates your session from Telegram and deletes the stored session file and its folder, saving you few clicks in other Telegram client and file manager.
+
+Limitions & TODOs
+-----------------
+1. It does not help scale up/down if image is not appropriate size.
+    - I can bundle ``waifu2x-caffe``, but that would be a Windows-only feature.
+2. It does not help sink image file size if it is too large.
+    - I will bundle ``pngquant``.
+3. No GUI.
+    - Well...I hope you are crazy enough to make one for me ;)
+
+Contributing
+------------
+You may request new features, report bugs or leave suggestions through `GitHub issue <https://github.com/alemonmk/pytgasu/issues>`_, or just code and fire a `pull request <https://github.com/alemonmk/pytgasu/pulls>`_.
+
+You may contact me via e-mail if you want to stay private or just don't bother with GitHub.
+
+Stuff in need
++++++++++++++
+- Tests. Preferably something ``nose`` uses.
+- Better documents. There's not much to document though ...
+- Feature requests. Keep it simple, don't complicate this tool.
+- Debug. I'm not that exhaustive and may leave edge cases here and there.
+
+Author
+------
+\(c) 2017 Lemon Lam <'%s@%s' % ('almk', 'rmntn.net')>
+
+License
+-------
+Licensed under `GNU General Public License Version 3 <https://www.gnu.org/licenses/gpl-3.0.en.html>`_.
---
+++ packages/pytgasu/pytgasu-0.2.0.tar.gz/pytgasu-0.2.0/pytgasu/cli.py
@@ -0,0 +1,139 @@
+#! /usr/bin/env python3
+# pytgasu - Automating creation of Telegram sticker packs
+# Copyright (C) 2017 Lemon Lam <almk@rmntn.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from pathlib import Path
+import click
+from pytgasu.constants import *
+
+
+@click.group()
+@click.version_option()
+def cli():
+    pass
+
+
+@cli.command(short_help=CLI_SHELP_UPLOAD_COMMAND)
+@click.option('-s', '/s', is_flag=True,  help=CLI_SHELP_UPLOAD_SUBFLAG)
+@click.argument('paths', nargs=-1, required=True,
+                type=click.Path(exists=True))
+def upload(paths, s):
+    """Upload sticker sets to Telegram.
+
+    \b
+    Paths can be:
+        1. directories with a .ssd (sticker set definitions) file, or
+        2. .ssd files themselves
+    """
+    import logging
+    from telethon import TelegramClient
+    from telethon.errors import RPCError
+    from pytgasu.uploader import CustomisedSession, SetDefParse, SetUploader
+
+    # region Telegram init
+    # TODO: strip Telethon to avoid too much implicit import
+    # Probably have to let the 'update' thread stay even we don't need it
+    # as ping-pongs prevent server from disconnecting us,
+    # but what's the point as we are constantly talking while running this function?
+    tc = TelegramClient(
+        session=CustomisedSession.try_load_or_create_new(),
+        api_id=TG_API_ID,
+        api_hash=TG_API_HASH)
+    logging.getLogger('TelethonLogger').setLevel(logging.ERROR)  # suppress logging from telethon
+
+    # Stolen from telethon.InteractiveTelegramClient :P
+    tc.connect()
+    if not tc.is_user_authorized():
+        print(PROMPT_ON_FIRST_LAUNCH)
+        user_phone = input(PROMPT_PHONE_NUMBER)
+        tc.send_code_request(user_phone)
+        code_ok = False
+        while not code_ok:
+            code = input(PROMPT_LOGIN_CODE)
+            try:
+                code_ok = tc.sign_in(user_phone, code)
+
+            # Two-step verification may be enabled
+            except RPCError as e:
+                from getpass import getpass
+                if e.password_required:
+                    pw = getpass(PROMPT_2FA_PASSWORD)
+                    code_ok = tc.sign_in(password=pw)
+                else:
+                    raise e
+    # endregion
+
+    # region Set list init
+    sticker_sets = list()
+    for setpath in paths:
+        print(NOTICE_PREPARING % setpath)
+        path = Path(setpath).resolve()
+        set_def_tuple = ()
+        if path.is_dir():
+            for d in path.glob('*.ssd'):
+                set_def_tuple = SetDefParse(d)  # only process one
+                break
+        elif path.suffix == '.ssd':
+            set_def_tuple = SetDefParse(path)
+        if set_def_tuple:
+            sticker_sets.append(set_def_tuple)
+    # endregion
+
+    SetUploader(tc=tc, sets=sticker_sets, subscribe=s)
+
+    tc.disconnect()
+
+
+# TODO: rename to prepare
+@cli.command(short_help=CLI_SHELP_DEFGEN_COMMAND)
+@click.argument(
+    'sets', nargs=-1, required=True,
+    type=click.Path(exists=True, file_okay=False, writable=True))
+def defgen(sets):
+    """Generate sticker set definition.
+
+    Reads any given directory.
+    Overwrites existing .ssd file.
+    """
+    from pytgasu.defgen import SetDefGenerator
+
+    # TODO: do first-run configurations to setting paths to executables of pngquant and/or waifu2x
+
+    SetDefGenerator(set_dirs=sets)
+
+
+@cli.command(short_help=CLI_SHELP_LOGOUT_COMMAND)
+def logout():
+    """Logout from Telegram."""
+    if not Path(PATH_TGSESSION_FILE).exists():
+        # Return early because TelegramClient creates a session first,
+        # but only when you can log_out() the session file gets deleted.
+        # If it's not there, don't create it and waste time talking to Telegram.
+        print(ERROR_NOT_LOGGEDIN)
+        return
+
+    from telethon import TelegramClient
+    from pytgasu.uploader import CustomisedSession
+    tc = TelegramClient(
+        session=CustomisedSession.try_load_or_create_new(),
+        api_id=TG_API_ID,
+        api_hash=TG_API_HASH)
+    tc.connect()
+    # I guess even if user is not authorised, invoking LogOutRequest does not cause problems
+    tc.log_out()
+    Path(PATH_TGSESSION_FILE).parent.unlink()
+
+if __name__ == "__main__":
+    cli()
