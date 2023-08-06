6634012649cc0c2df312b3cc3cbff6a2307133df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:23 +0000

    Add package gizoogle/0.0.1/gizoogle-0.0.1.tar.gz

---
+++ packages/gizoogle/gizoogle-0.0.1.tar.gz/gizoogle-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#
+# Gizoogle
+#
+# Copyright (C) 2017 Boris Raicheff
+# All rights reserved
+#
+
+from setuptools import find_packages, setup
+
+setup(
+    name='gizoogle',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=('beautifulsoup4', 'requests'),
+)
+
+# EOF
