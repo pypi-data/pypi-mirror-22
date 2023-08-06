2a46d0c888171f2e4fd3bb1f5660b697b074497a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:25 +0000

    Add package blacknet/2.0.0/blacknet-2.0.0-py2.7.egg

---
+++ packages/blacknet/blacknet-2.0.0-py2.7.egg/blacknet/version.py
@@ -0,0 +1,8 @@
+name = "blacknet"
+version = "2.0.0"
+description = 'SSH Distributed Honeypot Solution'
+license = "MIT"
+author = "Romain Bezut"
+author_email = "blacknet@xdec.net"
+url = "http://blacknet.xdec.net"
+packages = ["blacknet"]
