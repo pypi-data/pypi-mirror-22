22f018001408fb2e5d2d21ff87cba65c856de1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:21 +0000

    Add package icheckin/1.0.3/icheckin-1.0.3.tar.gz

---
+++ packages/icheckin/icheckin-1.0.3.tar.gz/icheckin-1.0.3/icheckin/icheckin.py
@@ -0,0 +1,59 @@
+import requests
+import getpass
+import sys
+
+def main():
+
+	LOGIN = r'https://izone.sunway.edu.my/login'
+	CHECKIN = r'https://izone.sunway.edu.my/icheckin/iCheckinNowWithCode'
+
+	argumentsPassed = False
+	if len(sys.argv) > 1:
+		if len(sys.argv) == 3:
+			studentID = sys.argv[1]
+			password = sys.argv[2]
+			argumentsPassed = True
+		else:
+			print('Usage: icheckin <student id> <password>')
+			sys.exit(1)
+
+	s = requests.Session()
+
+	while True:
+		if not argumentsPassed:
+			studentID = input('Student ID: ')
+			if studentID == '':
+				sys.exit(0)
+			password = getpass.getpass('Password: ')
+			if password == '':
+				sys.exit(0)
+		payload = {
+			'form_action': 'submitted',
+			'student_uid': studentID,
+			'password': password,
+		}
+		r = s.post(LOGIN, data=payload)
+		if r.history:
+			break
+		else:
+			print('Invalid credentials.')
+			if argumentsPassed:
+				sys.exit(1)
+			else:
+				print()
+
+	while True:
+		if not argumentsPassed:
+			print()
+		code = input('Code: ')
+		if code == '':
+			sys.exit(0)
+		r = s.post(CHECKIN, data={'checkin_code': code})
+		if 'Checkin code not valid.' in r.text:
+			print('Invalid code.\n')
+		else:
+			print('Successfully checked in.')
+			break
+
+if __name__ == '__main__':
+	main()
