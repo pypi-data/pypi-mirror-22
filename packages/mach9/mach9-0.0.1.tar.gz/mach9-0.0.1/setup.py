cb83ad5bb9a76d5b1029ab82909cadea57c32091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:47 +0000

    Add package mach9/0.0.1/mach9-0.0.1.tar.gz

---
+++ packages/mach9/mach9-0.0.1.tar.gz/mach9-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='mach9',
+    version='0.0.1',
+    author='38elements',
+    description='',
+    license='MIT License',
+    url='',
+    install_requires=[],
+    packages=[
+        'mach9'
+    ],
+    keywords='',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers'
+    ]
+)
