7137ce5f4e42f4a7870a93cd408c3c3ceebe8cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:15 +0000

    Add package zopyx.txng3.ext/4.0.0/zopyx.txng3.ext-4.0.0.zip

---
+++ packages/zopyx.txng3.ext/zopyx.txng3.ext-4.0.0.zip/zopyx.txng3.ext-4.0.0/build_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PATH=\
+/opt/buildout.python/bin:\
+$PATH:
+
+if [[ "$1" = "python-2.7" ]]
+then
+    python_version=2.7
+fi
+
+virtualenv-$python_version .
+bin/pip install zc.buildout
+bin/zc.buildout bootstrap
+bin/buildout
+bin/test
+
