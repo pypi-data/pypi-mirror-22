4fb3bc661119c4d93f11e776eb7aa0e80c915b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:59 +0000

    Add package hostsman/1.1.3/hostsman-1.1.3-py2.py3-none-any.whl

---
+++ packages/hostsman/hostsman-1.1.3-py2.py3-none-any.whl/hostsman-1.1.3.dist-info/DESCRIPTION.rst
@@ -0,0 +1,124 @@
+|Build Status|
+
+# hostsman #
+============
+
+We replaced the project name to ``hostsman`` from ``hoste``
+
+Add, remove or list mappings in hosts file
+
+INSTALLATION
+------------
+
+Package Link: `hostsman <https://pypi.python.org/pypi/hostsman>`__
+
+You can use pip to install this tool.
+
+Run ``pip install hostsman``.
+
+USAGE
+-----
+
+*$ Help*
+~~~~~~~~
+
+Run ``hostsman`` or ``hostsman -h`` to check the help doc:
+
+::
+
+    (pypi) ➜  hosts git:(master) ✗ hostsman
+    usage: hostsman [-h] [-l | -c HOSTNAME [HOSTNAME ...] | -i HOSTNAME[:IP]
+                 [HOSTNAME[:IP] ...] | -r HOSTNAME [HOSTNAME ...]]
+
+    Add, remove or list mappings in hosts file
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -l, --list            Show the content of hosts file
+      -c HOSTNAME [HOSTNAME ...], --check HOSTNAME [HOSTNAME ...]
+                            Check if the host name existed in the host file
+      -i HOSTNAME[:IP] [HOSTNAME[:IP] ...], --insert HOSTNAME[:IP] [HOSTNAME[:IP] ...]
+                            Insert HOSTNAME[:IP] mappings
+      -r HOSTNAME [HOSTNAME ...], --remove HOSTNAME [HOSTNAME ...]
+                            Remove mapping for HOSTNAME from hosts file.
+
+    hosts file location: /etc/hosts
+
+The last line of help gives the hosts file location on your pc.
+
+*$ List mappings in hosts file*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Run **``hostsman -l``** to list out the content of hosts file.
+
+::
+
+    127.0.0.1       localhost
+    255.255.255.255 broadcasthost
+    ::1             localhost
+    fe80::1%lo0     localhost
+    127.0.0.1       my.local
+
+*$ Check HOSTNAME*
+~~~~~~~~~~~~~~~~~~
+
+Check if a hostname is configured in hosts file.
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Run ``hostsman -c hostname``,
+
+It will return the result if ``hostname`` is in hosts file:
+
+::
+
+    # Search result:
+    127.0.0.1       localhost
+    ::1             localhost
+    fe80::1%lo0     localhost
+
+Check multiple hostnames
+^^^^^^^^^^^^^^^^^^^^^^^^
+
+Run ``hostsman -c my.local my.local2``,
+
+It will print out the mappings for given hostnames
+
+::
+
+    # Search result:
+    127.0.0.1       my.local
+    127.0.0.1       my.local2
+
+*$ Insert mappings*
+~~~~~~~~~~~~~~~~~~~
+
+Add single mapping
+^^^^^^^^^^^^^^^^^^
+
+Run ``hostsman -i my.local3:192.1.1.3``, it will insert new mapping:
+
+``192.1.1.3      my.local3``
+
+If not given ip, the default value ``127.0.0.1`` will be used:
+
+Run ``hostsman -i my.local4``, it will insert mapping:
+
+``127.0.0.1      my.local4``
+
+Add multiple mappings.
+^^^^^^^^^^^^^^^^^^^^^^
+
+Run ``hostsman -r my.local my.local2:192.1.1.3``
+
+*$ Remove mapping*
+~~~~~~~~~~~~~~~~~~
+
+Run ``hostsman -r my.local`` to remove ``my.local``
+
+*A backup file will be created for every removal operation(when the
+hostname is existed) *
+
+.. |Build Status| image:: https://travis-ci.org/qszhuan/hostsman.svg?branch=master
+   :target: https://travis-ci.org/qszhuan/hostsman
+
+
---
+++ packages/hostsman/hostsman-1.1.3-py2.py3-none-any.whl/hostsman.py
@@ -0,0 +1,171 @@
+﻿# -*- coding: utf-8 -*-
+
+import sys
+from pygments import highlight
+from pygments.lexers import *
+from pygments.formatters import *
+import argparse
+from shutil import copyfile
+import datetime
+from HostsLexer import *
+from utils import *
+from context import *
+
+env = Environment()
+
+class Host:
+    def __init__(self):
+        platform = Platform()
+        if platform.is_linux() or platform.is_mac():
+            self.hostFile = "/etc/hosts"
+        elif platform.is_windows():
+            self.hostFile = 'c:\windows\system32\drivers\etc\hosts'
+        else:
+            self.hostFile = '/etc/hosts'
+
+    def list(self):
+        with open(self.hostFile, 'r') as f:
+            return [line.strip() for line in f]
+
+    def add(self, hostname, ip='127.0.0.1'):
+        if self.exists(hostname, ip):
+            return False
+
+        added = False
+        now = datetime.datetime.utcnow()
+        back_file = self.hostFile + now.strftime("-%Y.%m.%d.%H.%M.%S.%f")
+        copyfile(self.hostFile, back_file)
+
+        with open(back_file, 'r') as input:
+            with open(self.hostFile, 'w') as output:
+                for line in input:
+                    if line.startswith('#') or line == '\n':
+                        output.write(line)
+                    else:
+                        segment = line.split()
+                        if ip == segment[0]:
+                            segment.append(hostname)
+                            added = True
+                        output.write(segment[0])
+                        output.write('\t')
+                        output.write(' '.join(segment[1:]))
+                        output.write('\n')
+                if not added:
+                    output.write(ip)
+                    output.write('\t')
+                    output.write(hostname)
+                    output.write('\n')
+
+        return True
+
+    def remove(self, hostname):
+        if not self.exists(hostname):
+            return False,
+
+        now = datetime.datetime.utcnow()
+        back_file = self.hostFile + now.strftime("-%Y.%m.%d.%H.%M.%S.%f")
+        copyfile(self.hostFile, back_file)
+        found = False
+        with open(back_file, 'r') as input:
+            with open(self.hostFile, 'w') as output:
+                for line in input:
+                    segment = line.split()
+
+                    if line.startswith('#') or line == '\n':
+                        output.write(line)
+                    elif len(segment) == 2 and hostname == segment[1]:
+                        found = True
+                        continue
+                    elif len(segment) >= 2 and hostname in segment[1:]:
+                        found = True
+                        segment.remove(hostname)
+                        output.write(segment[0])
+                        output.write('\t')
+                        output.write(' '.join(segment[1:]))
+                        output.write('\n')
+                    else:
+                        output.write(line)
+        return found, back_file
+
+    def exists(self, hostname, ip = None):
+        with open(self.hostFile, 'r') as f:
+            for line in list(f):
+                segment = line.split()
+                if line.startswith('#') or line == '\n':
+                    continue
+                if hostname in segment[1:] and (ip is None or ip == segment[0]):
+                    return True
+            else:
+                return False
+
+    def check(self, *host_names):
+        result = []
+        with open(self.hostFile, 'r') as f:
+            for line in list(f):
+                if line.startswith('#') or line == '\n':
+                    continue
+                if len([x for x in line.split()[1:] if x in host_names]):
+                    result.append(line.strip())
+
+            return result
+
+    def location(self):
+        return self.hostFile
+
+
+def init_parser(file_path):
+    parser = argparse.ArgumentParser(description='add, remove or list mappings in hosts file',
+                                     epilog='hosts file location: ' + file_path)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-l", "--list", action="store_true", help="Show the content of hosts file")
+    group.add_argument("-c", "--check", metavar='HOSTNAME', nargs='+',
+                       help="Check if the host name existed in the host file")
+    group.add_argument("-i", "--insert", metavar='HOSTNAME[:IP]', nargs='+',
+                       help="Insert HOSTNAME[:IP] mappings")
+    group.add_argument("-r", "--remove", metavar='HOSTNAME', nargs='+',
+                       help="Remove mapping for HOSTNAME from hosts file.")
+    return parser
+
+
+def highlight_line(content):
+    return highlight(content, HostsLexer(ensurenl=False), TerminalFormatter())
+
+
+def print_highlight(*a_list):
+    for each in a_list:
+        env.stdout.write(highlight_line(each) + '\n')
+    env.stdout.flush()
+
+def main():
+    host = Host()
+    parser = init_parser(host.location())
+    args = parser.parse_args()
+
+    if args.list:
+        content = host.list()
+        print_highlight(*content)
+    elif args.check:
+        print_highlight('# Search result:')
+        result = host.check(*args.check)
+        print_highlight(*result)
+    elif args.insert:
+        print_highlight('# Insert mapping:')
+        for each in args.insert:
+            arg = each.split(':')
+            if host.add(*arg):
+                print_highlight('> inserted ' + each)
+            else:
+                print_highlight('> failed to insert ' + each)
+    elif args.remove:
+        print_highlight('# Remove mapping:')
+        for each in args.remove:
+            result = host.remove(each)
+            if result[0]:
+                print_highlight('## removed ' + each + ', backup file: ' + result[1])
+            else:
+                print_highlight('## Not found ' + each)
+    else:
+        parser.print_help()
+
+if __name__ == '__main__':
+    main()
