8c4b22a6e84664f6819c3768425124bfed5816e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:44 +0000

    Add package fasttld/0.2/fasttld-0.2.tar.gz

---
+++ packages/fasttld/fasttld-0.2.tar.gz/fasttld-0.2/fasttld/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: Jophy
+@file: __init__.py
+@time: 17/6/1 23:46
+
+Copyright (c) 2017 Jophy
+"""
+
+from fasttld.FastTLDExtract import FastTLDExtract
+from fasttld.psl import update
