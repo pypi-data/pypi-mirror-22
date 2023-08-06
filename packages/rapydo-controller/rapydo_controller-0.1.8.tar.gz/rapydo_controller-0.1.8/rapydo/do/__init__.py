94bd7fd09bab03b325bc28619c465fe2098ae39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:16 +0000

    Add package rapydo-controller/0.1.8/rapydo_controller-0.1.8.tar.gz

---
+++ packages/rapydo-controller/rapydo_controller-0.1.8.tar.gz/rapydo_controller-0.1.8/rapydo/do/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.1.8'
+
+FRAMEWORK_NAME = 'RAPyDo'
+
+CONTAINERS_YAML_DIRNAME = 'containers'
+PROJECT_YAML_SPECSDIR = 'specs'
+PROJECT_CONF_FILENAME = 'project_configuration'
+COMPOSE_ENVIRONMENT_FILE = '.env'
+PLACEHOLDER = '#@$%-REPLACE-#@%$-ME-#@$%'
