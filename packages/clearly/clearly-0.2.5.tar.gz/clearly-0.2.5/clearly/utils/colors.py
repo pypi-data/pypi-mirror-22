e59d2efe6f352a57838f2bd7547a43344c4a5cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:21 +0000

    Add package clearly/0.2.5/clearly-0.2.5.tar.gz

---
+++ packages/clearly/clearly-0.2.5.tar.gz/clearly-0.2.5/clearly/utils/colors.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
+from functools import partial
+
+class colors:
+    _col = lambda color, text: color + unicode(text) + '\033[0m'
+    BLUE = partial(_col, '\033[94m')
+    GREEN = partial(_col, '\033[92m')
+    YELLOW = partial(_col, '\033[93m')
+    RED = partial(_col, '\033[91m')
+    MAGENTA = partial(_col, '\033[95m')
+    CYAN = partial(_col, '\033[96m')
+    ORANGE = partial(_col, '\033[38;5;208m')
+    BOLD = partial(_col, '\033[1m')
+    DIM = partial(_col, '\033[2m')
