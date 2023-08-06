8b65f45b0e38efb823cd0751e0e062b60839a2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:08 +0000

    Add package thinker/1.1.0/thinker-1.1.0.tar.gz

---
+++ packages/thinker/thinker-1.1.0.tar.gz/thinker-1.1.0/setup.py
@@ -0,0 +1,29 @@
+
+from distutils.core import setup
+import thinker
+
+setup(
+    name='thinker',
+    description='Rethinkdb wrapper for asyncio',
+    version=thinker.__version__,
+    author=thinker.__author__,
+    author_email=thinker.__email__,
+    packages=['thinker'],
+    url='https://github.com/mehmetkose/thinker',
+    license='MIT',
+    platforms=('Any'),
+    keywords='asyncio rethinkdb layer'.split(),
+    install_requires=[
+        'rethinkdb',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+        "Topic :: Utilities",
+    ],
+)
