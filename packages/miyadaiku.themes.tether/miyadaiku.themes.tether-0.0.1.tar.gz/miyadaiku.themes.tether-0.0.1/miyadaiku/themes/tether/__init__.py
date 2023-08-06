171928386ff752eb288ebfade75ef1a370feb0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:02 +0000

    Add package miyadaiku.themes.tether/0.0.1/miyadaiku.themes.tether-0.0.1.tar.gz

---
+++ packages/miyadaiku.themes.tether/miyadaiku.themes.tether-0.0.1.tar.gz/miyadaiku.themes.tether-0.0.1/miyadaiku/themes/tether/__init__.py
@@ -0,0 +1,16 @@
+from miyadaiku.core.contents import get_content_from_package, bin_loader
+from miyadaiku.core import config
+
+TETHER_MIN = 'tether.min.js'
+TETHER = 'tether.js'
+DEST_PATH = 'static/tether/'
+
+def load_package(site):
+    f = site.config.get('/', 'tether_compressed')
+    f = config.to_bool(f)
+    tether = TETHER_MIN if f else TETHER
+    src_path = 'externals/js/'+tether
+    
+    content = get_content_from_package(site, __name__, src_path, DEST_PATH+tether, bin_loader)
+    site.contents.add(content)
+    site.config.add('/', {'tether_path': DEST_PATH+tether})
