b1f25026c423d64ba89b38667a3972003a503f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:21 +0000

    Add package pygmsh/3.0.18/pygmsh-3.0.18-py2.py3-none-any.whl

---
+++ packages/pygmsh/pygmsh-3.0.18-py2.py3-none-any.whl/pygmsh/compound_surface.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+from .surface_base import SurfaceBase
+
+
+class CompoundSurface(SurfaceBase):
+    def __init__(self, surfaces):
+        super(CompoundSurface, self).__init__()
+
+        self.surfaces = surfaces
+
+        self.code = '\n'.join([
+            '{} = news;'.format(self.id),
+            'Compound Surface({}) = {{{}}};'.format(
+                self.id, ','.join([s.id for s in surfaces])
+            )])
+        return
---
+++ packages/pygmsh/pygmsh-3.0.18-py2.py3-none-any.whl/pygmsh/line_loop.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+
+
+class LineLoop(object):
+    _ID = 0
+
+    def __init__(self, lines):
+        self.lines = lines
+
+        self.id = 'll{}'.format(LineLoop._ID)
+        LineLoop._ID += 1
+
+        self.code = '\n'.join([
+            '{} = newll;'.format(self.id),
+            'Line Loop({}) = {{{}}};'.format(
+                self.id, ', '.join([l.id for l in lines])
+            )])
+        return
