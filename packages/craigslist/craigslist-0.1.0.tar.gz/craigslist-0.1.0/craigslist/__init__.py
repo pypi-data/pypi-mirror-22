3f4e31fd799bd35e59b266c489d8d9c7547f03e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:36 +0000

    Add package craigslist/0.1.0/craigslist-0.1.0.tar.gz

---
+++ packages/craigslist/craigslist-0.1.0.tar.gz/craigslist-0.1.0/craigslist/__init__.py
@@ -0,0 +1,5 @@
+from craigslist._search import search
+from craigslist.post import get_post as get
+
+from craigslist._search import search_async
+from craigslist.post import get_post_async as get_async
---
+++ packages/craigslist/craigslist-0.1.0.tar.gz/craigslist-0.1.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+readme = open('README.rst').read()
+
+setup(
+    name='craigslist',
+    version='0.1.0',
+    description='Python wrapper for craigslist.',
+    long_description=readme,
+    author='Al Johri',
+    author_email='al.johri@gmail.com',
+    url='https://github.com/AlJohri/craigslist',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'craigslist': ['data/*.json']},
+    install_requires=['requests', 'cssselect', 'lxml', 'blessings', 'arrow'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'craigslist=craigslist.cli:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
