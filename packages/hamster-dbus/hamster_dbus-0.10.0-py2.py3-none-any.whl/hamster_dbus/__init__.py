6f87b3ba129454210f4a6460555630de47d25811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:09 +0000

    Add package hamster-dbus/0.10.0/hamster_dbus-0.10.0-py2.py3-none-any.whl

---
+++ packages/hamster-dbus/hamster_dbus-0.10.0-py2.py3-none-any.whl/hamster_dbus/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""A package to export ``hamster-lib`` functionality over DBus."""
+
+__author__ = 'Eric Goller'
+__email__ = 'eric.goller@ninjaduck.solutions'
+__version__ = '0.10.0'
