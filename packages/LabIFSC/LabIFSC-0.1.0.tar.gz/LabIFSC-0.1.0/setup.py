e94a8e124f4bd428952c9851e749d0043bbbc1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:09 +0000

    Add package LabIFSC/0.1.0/LabIFSC-0.1.0.tar.gz

---
+++ packages/LabIFSC/LabIFSC-0.1.0.tar.gz/LabIFSC-0.1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='LabIFSC',
+    version='0.1.0',
+    description='Uma biblioteca para automatizar tarefas ligadas às disciplinas de laboratório de física do IFSC-USP',
+    author='Gabriel Queiroz',
+    author_email='gabrieljvnq@gmail.com',
+    url = 'https://github.com/gjvnq/LabIFSC',
+    download_url = 'https://github.com/gjvnq/LabIFSC/archive/v0.1.0.tar.gz',
+    packages=['LabIFSC'],
+    keywords = [],
+    classifiers = []
+)
