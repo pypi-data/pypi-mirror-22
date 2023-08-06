a3c0a2cdd1c94915b2e2b3e1fa138f65f554c1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:47 +0000

    Add package firebase-client/0.1/firebase-client-0.1.zip

---
+++ packages/firebase-client/firebase-client-0.1.zip/firebase-client-0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='firebase-client',
+        version='0.1',
+        url='https://github.com/Qotto/firebase-client',
+        license='New BSD License',
+        author='Alexandre Syenchuk',
+        author_email='alexpirine@gmail.com',
+        description='Python Firebase Client',
+        zip_safe=False,
+        classifiers=[
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+        ],
+    )
