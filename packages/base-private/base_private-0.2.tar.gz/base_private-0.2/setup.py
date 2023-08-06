c950ce15a99bdb7a7cd8bc1f93757b043f4a62cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:37 +0000

    Add package base-private/0.2/base_private-0.2.tar.gz

---
+++ packages/base-private/base_private-0.2.tar.gz/base_private-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='base_private',
+    version='0.2',
+    description='Encode arbitrary bits in private-use codepoints.',
+    url='https://github.com/morganwahl/base-private-use',
+    author='Morgan Wahl',
+    author_email='morgan.wahl@gmail.com',
+    license='GPLv3',
+    packages=['base_private'],
+    zip_safe=False,
+)
