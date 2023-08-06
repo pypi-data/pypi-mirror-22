f780fd35d5536cfa5304c1d2ba8fed10ade4cbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:05 +0000

    Add package tencent/0.0.1/tencent-0.0.1.tar.gz

---
+++ packages/tencent/tencent-0.0.1.tar.gz/tencent-0.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env Python  
+#coding=utf-8
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='tencent',
+    version='0.0.1',
+    description = 'MyTest API',
+    author = 'Derek',
+    author_email = 'dingo0927@126.com',
+    url = '',
+    license="MIT",
+    keywords='tencent, api',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
---
+++ packages/tencent/tencent-0.0.1.tar.gz/tencent-0.0.1/tencent/tencent.py
@@ -0,0 +1,7 @@
+import numpy as np 
+
+def main():
+    print ("My test API for tencent websit")
+
+if __name__ == '__main__':
+    main()
