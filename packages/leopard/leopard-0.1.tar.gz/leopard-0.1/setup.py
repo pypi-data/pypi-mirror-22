93b36423542eb171bac4d541d57eba935f8bec6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:13 +0000

    Add package leopard/0.1/leopard-0.1.tar.gz

---
+++ packages/leopard/leopard-0.1.tar.gz/leopard-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name = 'leopard',
+      version = '0.1',
+      description = 'Fast lab reporting python package',
+      url = 'https://github.com/beukueb/leopard',
+      author = 'Christophe Van Neste',
+      author_email = 'christophe.vanneste@ugent.be',
+      license = 'MIT',
+      packages = ['leopard'],
+      install_requires = [
+          'pylatex',
+      ],
+      zip_safe = False,
+      test_suite = 'nose.collector',
+      tests_require = ['nose']
+)
+
+#To install with symlink, so that changes are immediately available:
+#pip install -e . 
