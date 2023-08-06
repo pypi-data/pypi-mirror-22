e2fa139377a43e5f42daeac80a695f2f5335e513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:48 +0000

    Add package twitter_controller/0.2/twitter_controller-0.2.tar.gz

---
+++ packages/twitter_controller/twitter_controller-0.2.tar.gz/twitter_controller-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='twitter_controller',
+      version='0.2',
+      description='This is a package to analyze tweets of a certain person',
+      url='https://github.com/AGHPythonCourse2017/zad3-kanes115.git',
+      author='Dominik Stanaszek',
+      author_email='dominikstanaszek@icloud.com',
+      license='NONE',
+      packages=['twitter_controller'],
+      install_requires=[
+        'numpy',
+        'indicoio',
+        'pytest'
+      ],
+      zip_safe=False)
