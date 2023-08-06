0d44275db394224ea9b45e38e30661aa31d5a60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:25 +0000

    Add package gain/0.1.1/gain-0.1.1.tar.gz

---
+++ packages/gain/gain-0.1.1.tar.gz/gain-0.1.1/gain/item.py
@@ -0,0 +1,30 @@
+from .selector import Selector
+
+
+class ItemType(type):
+    def __new__(mcs, name, bases, namespace):
+        selectors = {}
+        namespace['_item_name'] = name
+        namespace['_item_count'] = 0
+        for name, value in namespace.items():
+            if isinstance(value, Selector):
+                selectors[name] = value
+        namespace['selectors'] = selectors
+        for name, value in selectors.items():
+            del namespace[name]
+        return type.__new__(mcs, name, bases, namespace)
+
+
+class Item(metaclass=ItemType):
+    def __init__(self, html):
+        self.results = {}
+        for name, selector in self.selectors.items():
+            self.results[name] = selector.parse_detail(html)
+
+    def __getattr__(self, item):
+        if item not in self.results:
+            raise AttributeError
+        return self.results[item]
+
+    async def save(self):
+        raise NotImplementedError
