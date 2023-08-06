4668d2f21f21ac11375bf43fe78db307268a98e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:36 +0000

    Add package bonobo/0.3.0/bonobo-0.3.0-py3.5.egg

---
+++ packages/bonobo/bonobo-0.3.0-py3.5.egg/bonobo/config/__init__.py
@@ -0,0 +1,13 @@
+from bonobo.config.configurables import Configurable
+from bonobo.config.options import Option, Method
+from bonobo.config.processors import ContextProcessor
+from bonobo.config.services import Container, Service
+
+__all__ = [
+    'Configurable',
+    'Container',
+    'ContextProcessor',
+    'Option',
+    'Method',
+    'Service',
+]
