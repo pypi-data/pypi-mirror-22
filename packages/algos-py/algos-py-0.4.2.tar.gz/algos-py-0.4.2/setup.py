927a9a00a1987ba689e317564cdc1f692ac13b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:10 +0000

    Add package algos-py/0.4.2/algos-py-0.4.2.tar.gz

---
+++ packages/algos-py/algos-py-0.4.2.tar.gz/algos-py-0.4.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.rst") as readme:
+    long_description = readme.read()
+
+setup(
+    name='algos-py',
+    version='0.4.2',
+    license='MIT',
+    author='Aleksandr Lisianoi',
+    author_email='all3fox@gmail.com',
+    url='https://github.com/all3fox/algos-py',
+    packages=find_packages(),
+    description="Classic computer science algorithms in Python",
+    long_description=long_description,
+    platforms=['linux', 'windows', 'macos'],
+)
