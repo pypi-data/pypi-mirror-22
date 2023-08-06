49c23582deb1fe16c91ac3897155a9361c385519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:07 +0000

    Add package gogonbsdo/1.0.0/gogonbsdo-1.0.0.tar.gz

---
+++ packages/gogonbsdo/gogonbsdo-1.0.0.tar.gz/gogonbsdo-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'gogonbsdo',
+	version = '1.0.0',
+	py_modules = ['gogonbsdo'],
+	author	= 'liuguiliang',
+	author_email = "316659141@qq.com",
+	description = " a study test",
+)
