e7d558bce5036baff657006ea8529343e0f1a563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:05 +0000

    Add package nester_v0cn058/1.0.0/nester_v0cn058-1.0.0.tar.gz

---
+++ packages/nester_v0cn058/nester_v0cn058-1.0.0.tar.gz/nester_v0cn058-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         ='nester_v0cn058',
+        version      ='1.0.0',
+        py_modules   =['nester_v0cn058'],
+        author       ='Carysong',
+        author_email ='cary.song@volvo.com',
+        url          ='http://www.carysong.com',
+        description  ='A simple printer of nested lists',
+    )
