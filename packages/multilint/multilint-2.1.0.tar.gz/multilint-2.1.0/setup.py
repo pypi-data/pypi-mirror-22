d5a8a17a746698d65f58129a87c11e9ebfebb78c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:51 +0000

    Add package multilint/2.1.0/multilint-2.1.0.tar.gz

---
+++ packages/multilint/multilint-2.1.0.tar.gz/multilint-2.1.0/setup.py
@@ -0,0 +1,63 @@
+# -*- encoding:utf-8 -*-
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
+
+import codecs
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    with codecs.open(os.path.join(package, '__init__.py'), 'r', 'utf-8') as fp:
+        init_py = fp.read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('multilint')
+
+with codecs.open('README.rst', 'r', 'utf-8') as readme_file:
+    readme = readme_file.read()
+
+with codecs.open('HISTORY.rst', 'r', 'utf-8') as history_file:
+    history = history_file.read()
+
+setup(
+    name='multilint',
+    version=version,
+    description='Run multiple python linters easily',
+    long_description=readme + '\n\n' + history,
+    author="Adam Johnson",
+    author_email='me@adamj.eu',
+    url='https://github.com/adamchainz/multilint',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=[],
+    license="ISC license",
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'multilint = multilint:main'
+        ],
+    },
+    keywords='lint, flake8, pep8, pycodestyle, codestyle, mccabe, setup.py',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
