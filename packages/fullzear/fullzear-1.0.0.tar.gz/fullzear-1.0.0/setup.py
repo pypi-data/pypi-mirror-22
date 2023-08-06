1af6df94cb10c949ca4bc4a0af6118263645b537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:07 +0000

    Add package fullzear/1.0.0/fullzear-1.0.0.tar.gz

---
+++ packages/fullzear/fullzear-1.0.0.tar.gz/fullzear-1.0.0/setup.py
@@ -0,0 +1,15 @@
+# encoding:utf-8
+# 发布文件
+# 从Python发布工具导入"setup"函数
+from distutils.core import setup
+
+
+setup(
+    name='fullzear',
+    version='1.0.0',
+
+    author='yangpeng',
+    author_email='877768999@qq.com',
+    url='https://github.com/hyypan',
+    description='这是我的第一次发布安装文件'
+)
