7cc2f58c84809ff1a771b0e8984bf982b6989b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:04 +0000

    Add package taqini/0.0.1/taqini-0.0.1.tar.gz

---
+++ packages/taqini/taqini-0.0.1.tar.gz/taqini-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+        name         = 'taqini',
+        version      = '0.0.1',
+        py_modules   = ['taqini'],
+        author       = 'TaQini',
+        author_email = 'smallosnail@gmail.com',
+        url          = 'http://github.com/TaQini',
+        description  = 'test modules',
+        )
+
---
+++ packages/taqini/taqini-0.0.1.tar.gz/taqini-0.0.1/taqini.py
@@ -0,0 +1,3 @@
+def welcome():
+    print("Welcome to TaQini's modules")
+
