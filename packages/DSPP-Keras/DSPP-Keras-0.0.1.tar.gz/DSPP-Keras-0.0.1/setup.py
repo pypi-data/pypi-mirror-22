adf8fa1017f758772338ec98a8a2921708aa4d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:21 +0000

    Add package DSPP-Keras/0.0.1/DSPP-Keras-0.0.1.tar.gz

---
+++ packages/DSPP-Keras/DSPP-Keras-0.0.1.tar.gz/DSPP-Keras-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='DSPP-Keras',
+      version='0.0.1',
+      description='Integration of DSPP database with Keral Machine Learning Library',
+      author='Jan Domanski',
+      author_email='jan@peptone.io',
+      url='https://github.com/PeptoneInc/dspp-keras',
+      download_url='https://github.com/PeptoneInc/dspp-keras/archive/v0.0.1.tar.gz',
+      license='MIT',
+      install_requires=['keras', 'numpy'],
+      packages=find_packages())
