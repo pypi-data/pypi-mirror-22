b5e0c7325e0de648daaf659ed684d67c4ed05d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:20 +0000

    Add package bongo-magic/0.0.2/bongo-magic-0.0.2.tar.gz

---
+++ packages/bongo-magic/bongo-magic-0.0.2.tar.gz/bongo-magic-0.0.2/magic/magic.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import sys
+import xlsxwriter as x
+import pandas as pd
+import os
+import os.path as p
+from termcolor import colored
+
+
+class Logger:
+    """
+    Static helper class for logging.
+    """
+
+    def replace(message="", continued=False):
+        if continued:
+            sys.stdout.write("\r" + message)
+        elif message == "" and continued == False:
+            sys.stdout.write("\n")
+
+
+    def single(message):
+        sys.stdout.write(message + "\n")
+
+
+    def warn(message):
+        sys.stdout.write(colored(message, "yellow") + "\n")
+
+
+    def error(messge):
+        sys.stdout.write(colored(message, "red") + "\n")
+
+
+def clean(dest):
+    """
+    Removes files from the specified destination directory.
+
+    :param dest: destination path.
+    """
+    Logger.single("cleaning {}".format(dest))
+
+    for f in os.listdir(dest):
+        os.remove(p.join(dest, f))
+
+
+def args(arglist):
+    """
+    If no source or destination directories are specified, take the
+    current working directory and pick out /stoptimes/ and /merged/.
+    Otherwise, use the specified source and destinations.
+
+    :param arglist: list of system arguments.
+    """
+    cur = os.getcwd()
+
+    if len(arglist) == 1:
+        return [cur + "/stoptimes/", cur + "/merged/"]
+        Logger.warn("no source or destination paths specified:")
+    else:
+        return arglist[1:]
+
+
+def createWorkbook(src, dest):
+    """
+    Creates a workbook at the specified destination directory. Populates
+    the workbook with individual sheets and their route data from the
+    corresponding .csv files.
+
+    :param src: source directory for stop data.
+    :param dest: destination directory for new workbooks.
+    """
+    # create a new xcel workbook
+    Logger.single("created workbook at {}".format(dest))
+    workbook = x.Workbook(dest + ".xlsx")
+    workbook.close()
+    
+    # open created file with ExcelWriter
+    with pd.ExcelWriter(dest + ".xlsx") as writer:
+
+        # iterate over source .csv files
+        for route in os.listdir(src):
+            # get sheet name
+            sheet = route.split(".")[0]
+
+            # try to extract data
+            try:
+                data = pd.read_csv(p.join(src, route))
+                del data["relative_time"]
+                del data["head_sign"]
+            except:
+                Logger.warn("\n{}.csv gives a read error. Writing a blank sheet.".format(sheet))
+                continue
+
+            # write to excel file, reset dataframe
+            data.to_excel(writer, sheet_name=sheet, index=False)
+            data = None
+
+            # log
+            Logger.replace("\twriting data from {}".format(sheet), True)
+
+        # close continuous write stream
+        Logger.replace()
+
+
+def findDirectories(src, dest):
+    """
+    Finds the specified directories, the location of course .csv files,
+    and verifies whether those directories exist. If they do, hand off
+    to workbook creation.
+
+    :param src: source directory.
+    :param dest: destination directory.
+    """
+    # if the source path doesn't exists, throw an error
+    if not p.lexists(dest) or not p.lexists(src):
+       raise Exception("Invalid source or destination path.") 
+    # otherwise, continue
+    else:
+        for dirpath in os.listdir(src):
+            # check if directory is meant to be private
+            if dirpath[0] is not ".":
+                # locations of csv files and destination excel files
+                csvlocation = p.join(src, dirpath)
+                xlsxlocation = p.join(dest, dirpath)
+
+                # handoff to create workbooks
+                createWorkbook(csvlocation, xlsxlocation)
+
+
+def main():
+    a = args(sys.argv)
+    clean(a[1])
+    findDirectories(a[0], a[1])
+    Logger.single("done.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/bongo-magic/bongo-magic-0.0.2.tar.gz/bongo-magic-0.0.2/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="bongo-magic",
+    version="0.0.2",
+
+    author="Anthony Pizzimenti",
+    author_email="anthony-pizzimenti@uiowa.edu",
+
+    install_requires=[
+        "pandas",
+        "xlsxwriter"
+    ],
+
+    include_package_data=True,
+
+    packages=find_packages(),
+
+    entry_points={
+        "console_scripts": ["magic = magic:main"]
+    }
+)
+
