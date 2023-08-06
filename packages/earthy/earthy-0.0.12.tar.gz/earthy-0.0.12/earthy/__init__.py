4ab63d41eb3a3671573fdeb6610a332f70783f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:33 +0000

    Add package earthy/0.0.12/earthy-0.0.12.tar.gz

---
+++ packages/earthy/earthy-0.0.12.tar.gz/earthy-0.0.12/earthy/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, division
+
+from earthy import nltk_wrappers
+from earthy import wordlist
+from earthy import preprocessing
