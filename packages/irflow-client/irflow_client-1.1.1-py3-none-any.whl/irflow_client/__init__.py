d803d495bda52f6bc598d2ca0fa5eb62a66210ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:02 +0000

    Add package irflow-client/1.1.1/irflow_client-1.1.1-py3-none-any.whl

---
+++ packages/irflow-client/irflow_client-1.1.1-py3-none-any.whl/irflow_client/__init__.py
@@ -0,0 +1,4 @@
+# In order to make the IRFlowApi Class available globally we need the below input statement
+# TODO Determine if we should call irflow_api.py irflow_client.py
+
+from irflow_client import IRFlowClient
