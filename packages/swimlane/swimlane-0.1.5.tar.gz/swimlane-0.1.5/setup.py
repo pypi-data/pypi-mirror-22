94581066941b10b34ddf5e31bba38ed7d8b83b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:40 +0000

    Add package swimlane/0.1.5/swimlane-0.1.5.tar.gz

---
+++ packages/swimlane/swimlane-0.1.5.tar.gz/swimlane-0.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="swimlane",
+    author="Swimlane LLC",
+    author_email="info@swimlane.com",
+    version="0.1.5",
+    url="https://github.com/swimlane/swimlane-python",
+    packages=find_packages(),
+    description="A Python client for Swimlane.",
+    install_requires=[
+        "requests==2.8.1",
+        "ordereddict==1.1",
+        "combomethod==1.0.6"
+    ],
+    test_suite='nose.collector',
+    tests_require=['coverage', 'mock', 'nose'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: PyPy"
+    ]
+)
