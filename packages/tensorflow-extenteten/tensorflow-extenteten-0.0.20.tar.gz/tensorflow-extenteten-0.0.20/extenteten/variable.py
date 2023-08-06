4ed29d97ed8eed7d047b7903388936ab02ec032b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:12 +0000

    Add package tensorflow-extenteten/0.0.20/tensorflow-extenteten-0.0.20.tar.gz

---
+++ packages/tensorflow-extenteten/tensorflow-extenteten-0.0.20.tar.gz/tensorflow-extenteten-0.0.20/extenteten/variable.py
@@ -0,0 +1,21 @@
+import functools
+import tensorflow as tf
+
+from .assertion import is_natural_num_sequence
+
+
+__all__ = ['variable']
+
+
+def variable(shape_or_initial, name=None):
+    create_variable = functools.partial(tf.Variable, name=name)
+
+    if is_natural_num_sequence(shape_or_initial):
+        shape = shape_or_initial
+        return create_variable(
+            (tf.contrib.layers.xavier_initializer()
+             if len(shape) == 2 else
+             tf.truncated_normal_initializer(stddev=0.1))(shape))
+
+    initial = shape_or_initial
+    return create_variable(initial)
