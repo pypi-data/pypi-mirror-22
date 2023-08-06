690bd76dab66ce569a9cd53c6adf75d1ac23bcb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:48 +0000

    Add package django-admin-interface/0.4.6/django-admin-interface-0.4.6.tar.gz

---
+++ packages/django-admin-interface/django-admin-interface-0.4.6.tar.gz/django-admin-interface-0.4.6/admin_interface/templatetags/admin_interface_tags.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+
+from admin_interface.models import Theme
+
+
+register = template.Library()
+
+
+@register.assignment_tag(takes_context = True)
+def get_admin_interface_theme(context):
+
+    theme = None
+    request = context.get('request', None)
+
+    if request:
+        theme = getattr(request, 'admin_interface_theme', None)
+
+    if not theme:
+        theme = Theme.get_active_theme()
+
+    if request:
+        request.admin_interface_theme = theme
+
+    return theme
+
