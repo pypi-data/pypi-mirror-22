3d4d7f3a70bac9931dc0ee7e4ce645547613a2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:44 +0000

    Add package ncephes/1.0.40/ncephes-1.0.40.tar.gz

---
+++ packages/ncephes/ncephes-1.0.40.tar.gz/ncephes-1.0.40/ncephes/test/test_misc.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from numpy.testing import assert_almost_equal
+
+from ncephes import misc
+
+
+def test_lbeta():
+    print(misc.lbeta(10, 3))
+    assert_almost_equal(misc.lbeta(10, 3), -6.4922398350204711)
