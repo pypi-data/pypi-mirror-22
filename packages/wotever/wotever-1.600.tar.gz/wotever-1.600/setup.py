e8c6abbaed3caf3573e877d701c6b3f4263329c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:58 +0000

    Add package wotever/1.600/wotever-1.600.tar.gz

---
+++ packages/wotever/wotever-1.600.tar.gz/wotever-1.600/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wotever',
+      version='1.600',
+      description='some useful tools',
+      author='wotever',
+      author_email='scott@bpp.so',
+      url='http://bpp.so',
+      packages=['wotever'],
+     )
---
+++ packages/wotever/wotever-1.600.tar.gz/wotever-1.600/wotever/__init__.py
@@ -0,0 +1,9 @@
+import logger
+import ExecutorMgr
+test=logger.test
+d=logger.d
+i=logger.i
+w=logger.w
+e=logger.e
+exemgr=ExecutorMgr
+job=Job
