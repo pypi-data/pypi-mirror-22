9e2ec78b7ac93eb7c4a888edd04ca53fa8692e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:40 +0000

    Add package gitnb/0.0.1.10/gitnb-0.0.1.10.tar.gz

---
+++ packages/gitnb/gitnb-0.0.1.10.tar.gz/gitnb-0.0.1.10/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'gitnb',
+  packages = ['gitnb'],
+  version = '0.0.1.10',
+  description = 'Git Tracking for Python Notebooks',
+  author = 'Brookie Guzder-Williams',
+  author_email = 'brook.williams@gmail.com',
+  url = 'https://github.com/brookisme/gitnb',
+  download_url = 'https://github.com/brookisme/gitnb/tarball/0.1',
+  keywords = ['ipython', 'notebook','git'],
+  include_package_data=True,
+  data_files=[
+    ('config',[
+        'gitnb/default.config.yaml',
+        'gitnb/dot_gitnb/notebooks',
+        'gitnb/dot_gitnb/precommit',
+        'gitnb/dot_gitnb/default_gitignore'
+      ]
+    )
+  ],
+  classifiers = [],
+  entry_points={
+      'console_scripts': [
+          'gitnb=gitnb:main',
+      ],
+  }
+)
