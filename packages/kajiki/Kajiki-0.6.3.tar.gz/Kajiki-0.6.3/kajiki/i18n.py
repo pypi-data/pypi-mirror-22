d097ff06c5da38d3b07cc5c33d726d7b67ab20c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:26 +0000

    Add package kajiki/0.6.3/Kajiki-0.6.3.tar.gz

---
+++ packages/kajiki/Kajiki-0.6.3.tar.gz/Kajiki-0.6.3/kajiki/i18n.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from io import BytesIO
+from .ir import TranslatableTextNode, ExprNode
+
+
+def gettext(s):
+    return s
+
+
+def extract(fileobj, keywords, comment_tags, options):
+    """Babel entry point that extracts translation strings from XML templates."""
+    from .xml_template import _Parser, _Compiler, _DomTransformer
+
+    try:
+        from babel.messages.extract import extract_python
+        extract_expr = options.get('extract_python', False)
+    except ImportError:
+        extract_python = None
+        extract_expr = False
+
+    source = fileobj.read()
+    if isinstance(source, bytes):
+        source = source.decode('utf-8')
+    doc = _Parser(filename='<string>', source=source).parse()
+    doc = _DomTransformer(doc, strip_text=options.get('strip_text', False)).transform()
+    compiler = _Compiler(filename='<string>', doc=doc,
+                         mode=options.get('mode', 'xml'),
+                         is_fragment=options.get('is_fragment', False))
+    ir = compiler.compile()
+    for node in ir:
+        if isinstance(node, TranslatableTextNode):
+            if node.text.strip():
+                yield (node.lineno, '_', node.text, [])
+        elif extract_expr and isinstance(node, ExprNode):
+            for e in extract_python(BytesIO(node.text.encode('utf-8')),
+                                    keywords, comment_tags, options):
+                yield (node.lineno, e[1], e[2], e[3])
