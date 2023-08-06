3f72d6c582a663ac569a2c95d26bfe5ac96de86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:30 +0000

    Add package yphtest/0.1/yphtest-0.1.tar.gz

---
+++ packages/yphtest/yphtest-0.1.tar.gz/yphtest-0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/5/23 下午3:57
+# @Author  : 袁平华
+# @Site    : 
+# @File    : setup.py
+# @Software: PyCharm Community Edition
+
+from  setuptools import  setup
+
+
+setup(name='yphtest',
+      version='0.1',
+      description='The yphtest joke in the world',
+      url='http://github.com/storborg/yphtest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['yphtest'],
+      zip_safe=False)
---
+++ packages/yphtest/yphtest-0.1.tar.gz/yphtest-0.1/yphtest/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/5/23 下午3:57
+# @Author  : 袁平华
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm Community Edition
+
+
+def joke():
+    return (u'How do you tell HTML from HTML5?'
+            u'Try it out in Internet Explorer.'
+            u'Does it work?'
+            u'No?'
+            u'It\'s HTML5.')
+
+
