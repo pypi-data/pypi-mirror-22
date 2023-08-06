3a4a9e7f767713a26d29ff6701598e5774221caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:39 +0000

    Add package comp/0.3.3/comp-0.3.3.tar.gz

---
+++ packages/comp/comp-0.3.3.tar.gz/comp-0.3.3/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from os import listdir
+from os.path import join
+
+from setuptools import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='comp',
+    version='0.3.3',
+    description=('Curses Omni Media Player'),
+    long_description=long_description,
+    url='https://github.com/McSinyx/comp',
+    author='Nguyễn Gia Phong',
+    author_email='vn.mcsinyx@gmail.com',
+    license='AGPLv3+',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console :: Curses',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Natural Language :: English',
+        'Natural Language :: Vietnamese',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+        'Topic :: Multimedia :: Video :: Display'],
+    keywords='youtube-dl mpv-wrapper curses console-application multimedia',
+    packages=['omp'],
+    install_requires=['python-mpv', 'youtube-dl'],
+    package_data={'omp': ['locale/*/LC_MESSAGES/omp.mo']},
+    scripts=['comp'],
+    platforms=['POSIX'])
