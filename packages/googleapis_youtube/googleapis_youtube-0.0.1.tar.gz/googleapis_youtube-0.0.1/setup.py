efecc5f828a1910e2240fda089eb82e32808d223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:28 +0000

    Add package googleapis_youtube/0.0.1/googleapis_youtube-0.0.1.tar.gz

---
+++ packages/googleapis_youtube/googleapis_youtube-0.0.1.tar.gz/googleapis_youtube-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='googleapis_youtube',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Dmitry Kalinin',
+    url='https://github.com/null-none/python-youtube-api'
+)
