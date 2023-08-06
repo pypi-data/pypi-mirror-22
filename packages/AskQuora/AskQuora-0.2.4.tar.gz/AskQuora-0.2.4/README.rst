c7a7e6c7d59f79bcc6e81da15966bbd1d39a6b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:47 +0000

    Add package AskQuora/0.2.4/AskQuora-0.2.4.tar.gz

---
+++ packages/AskQuora/AskQuora-0.2.4.tar.gz/AskQuora-0.2.4/README.rst
@@ -0,0 +1,40 @@
+# AskQuora
+
+Quora Q&amp;A right from the command line!
+
+<img src="http://i.imgur.com/qbgEVJf.png">
+
+Inspired by https://github.com/gleitz/howdoi
+
+## Installation & Usage:
+
+`sudo pip install -U askquora`
+
+or if you like to live on the bleeding edge: `sudo python setup.py install`
+
+Run it using `askquora`
+
+```
+usage: askquora [-h] [-n] [QUERY [QUERY ...]]
+
+Quora Q&A right from the command-line
+
+positional arguments:
+  QUERY           the question to find the answer of
+
+optional arguments:
+  -h, --help      show this help message and exit
+  -n, --no-color  do not colorize or style text
+```
+
+## Contributing:
+
+Any PR's in bug fixes, features or even this documentation are most welcome!
+
+## Disclaimer:
+
+This is not an official Quora product
+
+## License:
+
+`The MIT License`
---
+++ packages/AskQuora/AskQuora-0.2.4.tar.gz/AskQuora-0.2.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import askquora
+
+setup(name='AskQuora',
+      version='0.2.4',
+      description='Quora Q&A right from the command line',
+      author='Ritiek Malhotra',
+      author_email='ritiekmalhotra123@gmail.com',
+      packages = find_packages(),
+      entry_points={
+            'console_scripts': [
+                  'askquora = askquora.askquora:command_line',
+            ]
+      },
+      url='https://www.github.com/Ritiek/AskQuora',
+      keywords=['quora', 'terminal', 'command-line', 'question', 'python'],
+      license='MIT',
+      download_url='https://github.com/Ritiek/AskQuora/archive/v0.2.4.tar.gz',
+      classifiers=[],
+      install_requires=[
+            'requests',
+            'BeautifulSoup4',
+            'colorama',
+            'requests-cache'
+      ]
+     )
