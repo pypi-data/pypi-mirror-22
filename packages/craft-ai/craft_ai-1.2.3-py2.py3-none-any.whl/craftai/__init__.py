79414da9f41a1046c3a41a883bfaf92694508844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:56 +0000

    Add package craft-ai/1.2.3/craft_ai-1.2.3-py2.py3-none-any.whl

---
+++ packages/craft-ai/craft_ai-1.2.3-py2.py3-none-any.whl/craftai/__init__.py
@@ -0,0 +1,7 @@
+"""craft ai API python 2/3 client"""
+
+__title__ = 'craft-ai'
+__version__ = '1.2.3'
+__author__ = 'craft ai'
+__license__ = 'BSD-3-Clause'
+__copyright__ = 'Copyright (c) 2016, craft ai'
