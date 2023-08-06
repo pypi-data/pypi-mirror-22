4ff8d3d3e460073afd1a8eb09d3d60813bfe4391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:25 +0000

    Add package EasyTravel/0.1/EasyTravel-0.1.zip

---
+++ packages/EasyTravel/EasyTravel-0.1.zip/EasyTravel-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'EasyTravel',
+  packages = ['EasyTravel'],
+  version = '0.1',
+  description = 'Module for searching flight tickets based on Skyscanner API',
+  author = 'Bohdan Borkivskyy',
+  author_email = 'b.borkivskyy@ucu.edu.ua',
+  url = 'https://github.com/BohdanBorkivskyy/EasyTravel',
+  download_url = 'https://github.com/BohdanBorkivskyy/EasyTravel/archive/0.1.tar.gz',
+  keywords = ['travel', 'skyscanner', 'flight'],
+  classifiers = [],
+  install_requires=['pygame==1.9.3']
+)
