96d4d77eeaac600525f4fcc89c42ab85984fd96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:15 +0000

    Add package ScapyDhcpServer/1.0.0/ScapyDhcpServer-1.0.0.zip

---
+++ packages/ScapyDhcpServer/ScapyDhcpServer-1.0.0.zip/ScapyDhcpServer-1.0.0/setup.py
@@ -0,0 +1,15 @@
+#encoding=utf-8
+from distutils.core import setup  
+setup(name='ScapyDhcpServer',  #需要打包的文件
+      version='1.0.0',    #版本号
+      description='use Scapy Simulated DHCP server',    #描述信息
+      author='qdyxmas',   #作者  
+      author_email='qdyxmas@gmail.com',    #作者邮箱
+      url='https://github.com/qdyxmas/scapy-Protocol-conformance',# 源码链接地址
+      keywords='scapy dhcpd dhcpserver dhcp-server', 
+      install_requires=[	
+        'IPy scapy' #依赖包
+      ],
+      platform='all',	#适用平台
+      py_modules=['ScapyDhcpServer'],  #模块名
+)    
