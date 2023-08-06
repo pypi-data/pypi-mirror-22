91cfa4382b9211a49fb72930ceb81031ca75a0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:51 +0000

    Add package lexus/0.2/lexus-0.2.tar.gz

---
+++ packages/lexus/lexus-0.2.tar.gz/lexus-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='lexus',
+version='0.2',
+description='Easy development tool for AWS Lex',
+url='https://github.com/ysak-y/lexus-test.git',
+author='Yoshiaki Yamada',
+author_email='yoshiaki.0614@gmail.com',
+license='MIT',
+packages=['lexus'],
+zip_safe=False)
+
