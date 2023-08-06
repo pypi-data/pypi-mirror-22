653854d3cb2c4de9ab1497436b14fd1cc363aee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:22 +0000

    Add package speake3/0.2/speake3-0.2.tar.gz

---
+++ packages/speake3/speake3-0.2.tar.gz/speake3-0.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+name = 'speake3',
+packages = ['speake3'], # this must be the same as the name above
+version = '0.2',
+description = 'A wrapper for espeak for python3',
+license='MIT',
+platforms='Linux',
+author = 'Brian Gikonyo',
+author_email = 'colasgikonyo@gmail.com',
+url = 'https://github.com/GikonyoBrian/speake3', # use the URL to the github
+download_url = 'https://github.com/GikonyoBrian/speake3/archive/speake3.tar.gz',
+keywords = ['espeak', 'tts', 'text-to-speech', 'engine', 'python3', 'python'], 
+classifiers = [
+	'Development Status :: 3 - Alpha',
+ 	'Environment :: Console',
+ 	'Intended Audience :: Developers',
+ 	'Programming Language :: Python',
+ 	'Programming Language :: Python :: 3',
+ 	'Programming Language :: Python :: 3.0',
+ 	'Programming Language :: Python :: 3.1',
+ 	'Programming Language :: Python :: 3.2',
+ 	'Programming Language :: Python :: 3.3',
+ 	'Programming Language :: Python :: 3.4'],
+)
