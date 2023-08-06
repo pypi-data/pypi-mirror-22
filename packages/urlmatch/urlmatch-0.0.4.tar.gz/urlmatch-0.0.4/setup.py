b193fd4ac133845fb2644b40a0999d58cc99f63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:49 +0000

    Add package urlmatch/0.0.4/urlmatch-0.0.4.tar.gz

---
+++ packages/urlmatch/urlmatch-0.0.4.tar.gz/urlmatch-0.0.4/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import find_packages
+from distutils.core import setup
+
+short_description = 'Python library for matching URLs.'
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
+else:
+    long_description = short_description
+
+setup(
+    name='urlmatch',
+    version='0.0.4',
+    author='Jesse Pollak',
+    author_email='jpollak92@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/jessepollak/urlmatch',
+    description=short_description,
+    long_description=long_description,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    test_suite='tests',
+    install_requires=[]
+)
