467091c48f941a741de27f75857e84b06264ccd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:06 +0000

    Add package pycnf/0.0.8/pycnf-0.0.8.tar.gz

---
+++ packages/pycnf/pycnf-0.0.8.tar.gz/pycnf-0.0.8/pycnf/read.py
@@ -0,0 +1,40 @@
+from utils import *
+from configtype import get_config_type
+
+def load(filename, extra_dict={}, other_config="", **kwargs):
+    file_type = get_config_type(filename)
+    config_dict = load_config(filename = filename, file_type = file_type, extra_dict = extra_dict, 
+            other_config = other_config, **kwargs)
+    return(config_dict)
+
+def eval(filename, section = "default", option = "", **kwargs):
+    config_dict = load(filename, **kwargs)
+    if option == "":
+        return(config_dict[section])
+    else:
+        return(config_dict[section][option])
+
+def merge(filename, sections = [], **kwargs):
+    config_dict = load(filename, **kwargs)
+    merge_dict = dict_merge(config_dict, sections, **kwargs)
+    return(merge_dict)
+
+def dict_merge(config_dict, sections = [], **kwargs):
+    merge_dict = {}
+    for section in sections:
+        try:
+            dict(config_dict[section])
+            merge_dict.update(config_dict[section])
+        except:
+            merge_dict.update({section:config_dict[section]})
+    return(merge_dict)
+
+def sections(filename, **kwargs):
+    config_dict = load(filename, **kwargs)
+    return(config_dict.keys())
+
+def options(filename, section, **kwargs):
+    config_dict = load(filename, **kwargs)
+    section_content = config_dict[section]
+    return(section_content.keys())
+
