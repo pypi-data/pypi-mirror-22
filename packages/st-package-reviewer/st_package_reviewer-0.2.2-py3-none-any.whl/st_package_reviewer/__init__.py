dc5492479995e57793b234c0c5e0f2caaff938e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:53 +0000

    Add package st-package-reviewer/0.2.2/st_package_reviewer-0.2.2-py3-none-any.whl

---
+++ packages/st-package-reviewer/st_package_reviewer-0.2.2-py3-none-any.whl/st_package_reviewer/__init__.py
@@ -0,0 +1,15 @@
+"""Review Sublime Text packages.
+"""
+
+__version__ = "0.2.2"
+
+_debug = False
+
+
+def debug_active():
+    return _debug
+
+
+def set_debug(value):
+    global _debug
+    _debug = value
