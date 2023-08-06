7245ba624ae6d74ee9140bbfe7372166d8d29926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:56 +0000

    Add package zoomus/0.1.7/zoomus-0.1.7.tar.gz

---
+++ packages/zoomus/zoomus-0.1.7.tar.gz/zoomus-0.1.7/zoomus/components/__init__.py
@@ -0,0 +1,13 @@
+"""Zoom.us REST API Python Client Components"""
+
+from __future__ import absolute_import
+
+from . import (
+    meeting,
+    recording,
+    report,
+    user,
+    webinar)
+
+__author__ = "Patrick R. Schmid"
+__email__ = "prschmid@act.md"
---
+++ packages/zoomus/zoomus-0.1.7.tar.gz/zoomus-0.1.7/zoomus/components/user.py
@@ -0,0 +1,43 @@
+"""Zoom.us REST API Python Client -- User component"""
+
+from __future__ import absolute_import
+
+from zoomus import util
+from zoomus.components import base
+
+
+__author__ = "Patrick R. Schmid"
+__email__ = "prschmid@act.md"
+
+
+class UserComponent(base.BaseComponent):
+    """Component dealing with all user related matters"""
+
+    def list(self, **kwargs):
+        return self.post_request("/user/list", params=kwargs)
+
+    def pending(self, **kwargs):
+        return self.post_request("/user/pending", params=kwargs)
+
+    def create(self, **kwargs):
+        return self.post_request("/user/create", params=kwargs)
+
+    def update(self, **kwargs):
+        util.require_keys(kwargs, 'id')
+        return self.post_request("/user/update", params=kwargs)
+
+    def delete(self, **kwargs):
+        util.require_keys(kwargs, 'id')
+        return self.post_request("/user/delete", params=kwargs)
+
+    def cust_create(self, **kwargs):
+        util.require_keys(kwargs, ['type', 'email'])
+        return self.post_request("/user/custcreate", params=kwargs)
+
+    def get(self, **kwargs):
+        util.require_keys(kwargs, 'id')
+        return self.post_request("/user/get", params=kwargs)
+
+    def get_by_email(self, **kwargs):
+        util.require_keys(kwargs, ['email', 'login_type'])
+        return self.post_request("/user/getbyemail", params=kwargs)
