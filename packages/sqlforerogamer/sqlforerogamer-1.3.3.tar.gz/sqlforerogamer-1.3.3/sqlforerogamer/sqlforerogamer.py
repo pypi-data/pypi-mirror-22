709dc4b2afe0a5ae604c895dc11c0980e3e8ebd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:47 +0000

    Add package sqlforerogamer/1.3.3/sqlforerogamer-1.3.3.tar.gz

---
+++ packages/sqlforerogamer/sqlforerogamer-1.3.3.tar.gz/sqlforerogamer-1.3.3/sqlforerogamer/sqlforerogamer.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+EROGAME_SCAPE_SQL_URL = 'http://erogamescape.dyndns.org/~ap2/ero/toukei_kaiseki/sql_for_erogamer_form.php'
+
+def read(sql):
+    http_responce = requests.post(EROGAME_SCAPE_SQL_URL, {'sql': sql})
+    html = http_responce.content
+    try:
+         df = pd.read_html(html)[0].pipe( lambda df: df.rename(columns=df.iloc[0]) ).drop(0).reset_index()
+
+    except ValueError:
+        message = BeautifulSoup(html, "lxml").find(id='query_result_main').get_text()
+        raise ValueError(message)
+    return df
+
+def tables():
+    return read('select schemaname, relname from pg_stat_user_tables');
