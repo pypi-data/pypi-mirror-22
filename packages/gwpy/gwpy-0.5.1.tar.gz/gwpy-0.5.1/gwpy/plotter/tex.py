93b21031b750d22115554b776d1326f1d8ac98c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:18 +0000

    Add package gwpy/0.5.1/gwpy-0.5.1.tar.gz

---
+++ packages/gwpy/gwpy-0.5.1.tar.gz/gwpy-0.5.1/gwpy/plotter/tex.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) Duncan Macleod (2013)
+#
+# This file is part of GWpy.
+#
+# GWpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Handle TeX formatting for matplotlib output
+"""
+
+from __future__ import division
+
+import os
+
+__author__ = "Duncan M. Macleod <duncan.macleod@ligo.org>"
+
+# -- tex configuration --------------------------------------------------------
+
+MACROS = [
+    r'\def\rtHz{\ensuremath{\sqrt{\mathrm{Hz}}}}',  # \sqrt{Hz} label
+]
+
+HAS_TEX = os.system('which pdflatex > %s 2>&1' % os.devnull) == 0
+
+# -- tex formatting -----------------------------------------------------------
+
+LATEX_CONTROL_CHARS = ["%", "\\", "_", "~", "&"]
+
+
+def float_to_latex(x, format="%.2g"):
+    """Convert a floating point number to a latex representation.
+
+    In particular, scientific notation is handled gracefully: e -> 10^
+
+    Parameters
+    ----------
+    x : `float`
+        the number to represent
+
+    format : `str`, optional
+        the output string format
+
+    Returns
+    -------
+    tex : `str`
+        a TeX representation of the input
+
+    Examples
+    --------
+    >>> float_to_latex(2000)
+    '2\times 10^{3}'
+    """
+    base_str = format % x
+    if "e" not in base_str:
+        return base_str
+    mantissa, exponent = base_str.split("e")
+    if float(mantissa).is_integer():
+        mantissa = int(float(mantissa))
+    exponent = exponent.lstrip("0+")
+    if exponent.startswith('-0'):
+        exponent = '-' + exponent[2:]
+    if float(mantissa) == 1.0:
+        return r"10^{%s}" % exponent
+    else:
+        return r"%s\!\!\times\!\!10^{%s}" % (mantissa, exponent)
+
+
+def label_to_latex(text):
+    """Convert an abitrary string of text into a latex-passable
+    representation.
+    """
+    if text is None:
+        return ''
+    for ch in LATEX_CONTROL_CHARS:
+        text = text.replace(ch, "\\%s" % ch)
+    return text
+
+
+def unit_to_latex(unit):
+    from astropy import units
+    from astropy.units.format import utils as unit_utils
+
+    if unit is None:
+        return ''
+    elif isinstance(unit, units.NamedUnit):
+        s = label_to_latex(unit.name)
+    elif isinstance(unit, units.CompositeUnit):
+        if unit.scale != 1:
+            s = float_to_latex(unit.scale) + r'\ '
+        else:
+            s = ''
+        if len(unit.bases):
+            positives, negatives = unit_utils.get_grouped_by_powers(
+                unit.bases, unit.powers)
+            if len(negatives) == 1:
+                negatives = format_unit_list(negatives)
+                positives = positives and format_unit_list(positives) or 1
+                s += r'{0}/{1}'.format(positives, negatives)
+            elif len(negatives):
+                if len(positives):
+                    positives = format_unit_list(positives)
+                else:
+                    positives = ''
+                negatives = format_unit_list(negatives, negative=True)
+                s += r'{0}\,{1}'.format(positives, negatives)
+            else:
+                positives = format_unit_list(positives)
+                s += positives
+    elif isinstance(unit, units.UnitBase):
+        return s.to_string('latex_inline')
+    else:
+        s = str(unit)
+    if s:
+        return r'$\mathrm{{{0}}}$'.format(s)
+    else:
+        return ''
+
+
+def format_unit_list(unitlist, negative=False):
+    from astropy.units.format import latex
+
+    out = []
+    texformatter = latex.Latex()
+    for base, power in unitlist:
+        if power == 1 and not negative:
+            out.append(texformatter._get_unit_name(base))
+        elif power == 0.5 and not negative:
+            out.append('\sqrt{{{0}}}'.format(label_to_latex(base.name)))
+        elif power != 1 and 1/power == int(1/power):
+            out.append('{0}^{{1/{1}}}'.format(
+                label_to_latex(base.name), int(1/power)))
+        elif negative:
+            out.append('{0}^{{-{1}}}'.format(
+                label_to_latex(base.name), power))
+        else:
+            out.append('{0}^{{{1}}}'.format(
+                label_to_latex(base.name), power))
+    return r'\,'.join(out)
