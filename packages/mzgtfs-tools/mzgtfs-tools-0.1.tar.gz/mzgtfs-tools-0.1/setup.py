0a84c637305bcb18b880c7ce98f4f60839858b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:37 +0000

    Add package mzgtfs-tools/0.1/mzgtfs-tools-0.1.tar.gz

---
+++ packages/mzgtfs-tools/mzgtfs-tools-0.1.tar.gz/mzgtfs-tools-0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mzgtfs-tools',
+      version='0.1',
+      description='tools using mapzen-gtfs library',
+      author='Tony Laidig',
+      author_email='laidig@gmail.com',
+      url='http://https://github.com/BusTechnology/mzgtfs-tools',
+      packages=['mzgtfs-tools'],
+      license='Apache',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
