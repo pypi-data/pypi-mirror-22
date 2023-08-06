0d5d47ce2f9b786f499ac30d1144e5760b22fab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:58 +0000

    Add package cipher/0.1/cipher-0.1.tar.gz

---
+++ packages/cipher/cipher-0.1.tar.gz/cipher-0.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+setup(
+    name='cipher',
+    version='0.1',
+    description='encryption message and decryption message',
+    author='Quintin',
+    author_email='quintinqiang@gmail.com',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    url='https://github.com/FrankQiang/cipher',
+    keywords='encryption and decryption',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
