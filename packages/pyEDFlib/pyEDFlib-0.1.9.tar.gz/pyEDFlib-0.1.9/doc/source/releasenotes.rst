7ff6cd496e848a4b75c03e82f0a9505c13694efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:33 +0000

    Add package pyEDFlib/0.1.9/pyEDFlib-0.1.9.tar.gz

---
+++ packages/pyEDFlib/pyEDFlib-0.1.9.tar.gz/pyEDFlib-0.1.9/doc/source/releasenotes.rst
@@ -0,0 +1,10 @@
+Release Notes
+=============
+
+.. toctree::
+   :maxdepth: 1
+
+   release.0.1.6
+   release.0.1.7
+   release.0.1.8
+   release.0.1.9
---
+++ packages/pyEDFlib/pyEDFlib-0.1.9.tar.gz/pyEDFlib-0.1.9/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.9'
+version = '0.1.9'
+full_version = '0.1.9'
+git_revision = 'Unknown'
+release = True
+
+if not release:
+    version = full_version
