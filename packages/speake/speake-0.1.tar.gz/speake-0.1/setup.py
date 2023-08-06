1da783375b2e974c33da1f3fbb8eac1250b71cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package speake/0.1/speake-0.1.tar.gz

---
+++ packages/speake/speake-0.1.tar.gz/speake-0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+name = 'speake',
+packages = ['speake'], # this must be the same as the name above
+version = '0.1',
+description = 'A wrapper for espeak for python2',
+long_description=open('README.md').read(),
+license='MIT',
+platforms='Linux',
+author = 'Brian Gikonyo',
+author_email = 'colasgikonyo@gmail.com',
+url = 'https://github.com/GikonyoBrian/speake', 
+download_url = 'https://github.com/GikonyoBrian/speake/archive/speake.tar.gz',
+keywords = ['espeak', 'tts', 'text-to-speech', 'engine', 'python2', 'python'],
+classifiers = [
+	'Development Status :: 3 - Alpha',
+ 	'Environment :: Console',
+ 	'Intended Audience :: Developers',
+ 	'Programming Language :: Python',
+ 	'Programming Language :: Python :: 2',
+ 	'Programming Language :: Python :: 2.6',
+ 	'Programming Language :: Python :: 2.7',],
+)
