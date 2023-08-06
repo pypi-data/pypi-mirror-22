d0b0b2a4d1b230c3ac981baaffdd9de7d746d47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:17 +0000

    Add package jsail/0.4.0/jsail-0.4.0.tar.gz

---
+++ packages/jsail/jsail-0.4.0.tar.gz/jsail-0.4.0/HISTORY.rst
@@ -0,0 +1,39 @@
+.. :changelog:
+
+History
+=======
+
+0.4.0 (2017-05-18)
+------------------
+
+* Unbuffered stdin usage was fixed and now program
+  exists correctly if it encounter EOF.
+
+0.3.0 (2016-10-03)
+------------------
+
+* Now jsail recognizes as timestamp keys ``timestamp``,
+  ``time``, ``@fields.timestamp``, ``@fields.time``.
+  And similarly, for message these keys are checked:
+  ``message``, ``msg``, ``@fields.message``, ``@fields.msg``.
+* Now jsail quietly skips lines which can't be parsed
+  as single JSON document.
+
+0.2.1 (2016-03-13)
+------------------
+
+* Fixed issue with STDIN bufering. Previously,
+  input given from STDIN was buffered and sometimes
+  ``jsail`` didn't show content until buffer fills up.
+  Now this behaviour is fixed.
+
+0.2.0 (2015-07-06)
+------------------
+
+* 'anyjson' was added into the requrements list.
+* Now jsail will process STDIN if filename wasn't given as a command line argument. This makes possible to use it in unix pipelines.
+
+0.1.0 (2015-04-16)
+---------------------
+
+* First release on PyPI.
---
+++ packages/jsail/jsail-0.4.0.tar.gz/jsail-0.4.0/jsail/cmd.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+"""JSON log tailer.
+
+Usage:
+  tail [--cross-finger] [--message=<text>] [--grep=<text> <filename>
+  tail (-h | --help)
+  tail --version
+
+Options:
+  -h --help       Show this screen.
+  --version       Show version.
+  --cross-finger  Output only if there was ERROR.
+  --message=<text> Show only items with this text in message.
+  --grep=<text> Show only items with this text in any field.
+
+"""
+
+import tailer
+import anyjson
+import re
+import click
+import sys
+
+from click import echo
+from collections import defaultdict
+
+
+def print_item(item):
+    fields = item['@fields']
+
+    if '@timestamp' in item:
+        ts = item['@timestamp'].replace('T', ' ')
+    if 'timestamp' in item:
+        ts = item['timestamp'].replace('T', ' ')
+    if 'time' in item:
+        ts = item['time'].replace('T', ' ')
+    elif 'timestamp' in fields:
+        ts = fields['timestamp'].replace('T', ' ')
+    elif 'time' in fields:
+        ts = fields['time'].replace('T', ' ')
+    else:
+        ts = None
+
+    if '@message' in item:
+        msg = item['@message']
+    elif 'message' in item:
+        msg = item['message']
+    elif 'msg' in item:
+        msg = item['msg']
+    elif 'message' in fields:
+        msg = fields['message']
+    elif 'msg' in fields:
+        msg = fields['msg']
+    else:
+        msg = 'No message'
+
+    if ts:
+        echo(u'{ts} {msg}'.format(ts=ts, msg=msg))
+    else:
+        echo(u'{msg}'.format(ts=ts, msg=msg))
+
+    for kv in fields.items():
+        echo((u' ' * 20 + u'{0} = {1}').format(*kv))
+
+
+@click.command()
+@click.option('--cross-finger', is_flag=True,
+              help='Output only if there was ERROR.')
+@click.option('--message',
+              help='Show only items with this text in message.')
+@click.option('--grep',
+              help='Show only items with this text in any field.')
+@click.option('--name',
+              help='Show only items with logger name which matches to this regexp.')
+@click.argument('filename', required=False)
+def main_func(filename, cross_finger, message, grep, name):
+    buffer = defaultdict(list)
+
+    def print_if_needed(item, line):
+        if message:
+            if re.search(message, item['@message'], re.I) is None:
+                return
+        if grep:
+            if re.search(grep, line, re.I) is None:
+                return
+        if name:
+            if re.search(name, item['@fields'].get('name', ''), re.I) is None:
+                return
+        print_item(item)
+
+
+    if filename:
+        stream = open(filename)
+        stream = tailer.follow(stream)
+    else:
+        def unbuffered_lines(stream):
+            while True:
+                line = stream.readline()
+                if line == '':
+                    return
+
+                yield line
+
+        stream = unbuffered_lines(sys.stdin)
+
+    for line in stream:
+        try:
+            item = anyjson.deserialize(line)
+        except:
+            continue
+
+        if cross_finger:
+            fields = item['@fields']
+            if 'uuid' in fields:
+                uuid = fields['uuid']
+                buffer[uuid].append(item)
+                if fields['level'] == 'ERROR':
+
+                    echo('')
+                    echo('=' * 80)
+                    for item in buffer[uuid]:
+                        print_if_needed(item, line)
+                    del buffer[uuid]
+            else:
+                if fields['level'] == 'ERROR':
+                    print_if_needed(item, line)
+        else:
+            print_if_needed(item, line)
