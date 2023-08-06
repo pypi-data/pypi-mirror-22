da3d3d6808b7cab7a0a68a40b407a5348ecddfb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:10 +0000

    Add package logtime-cli/0.4.1/logtime-cli-0.4.1.zip

---
+++ packages/logtime-cli/logtime-cli-0.4.1.zip/logtime-cli-0.4.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='logtime-cli',
+    version='0.4.1',
+    scripts=['lt'],
+    packages=['logtime_cli'],
+    description='CLI utility for logging the time you spend on things',
+    author='Kyle Fitzpatrick',
+    author_email='kyle.a.fitzpatrick@gmail.com',
+    url='https://github.com/K-Fitzpatrick/logtime-cli',
+    include_package_data=True,
+)
