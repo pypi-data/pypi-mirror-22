2ef9a9fc83c564ca7d6980521eb7d145aaad1a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:41 +0000

    Add package pytc-fitter/1.1.0/pytc-fitter-1.1.0.tar.gz

---
+++ packages/pytc-fitter/pytc-fitter-1.1.0.tar.gz/pytc-fitter-1.1.0/pytc/experiments/__init__.py
@@ -0,0 +1,11 @@
+__description__ = \
+"""
+Classes for holding onto ITC experiments.
+"""
+__author__ = "Michael J. Harms"
+__date__ = "2016-12-19"
+__all__ = [] 
+
+from .origin import OriginExperiment
+from .nitpic import NitpicExperiment
+from .chooser import ITCExperiment
