ced940295cdcefdafd9db608c98d1cdb8cef64ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:11 +0000

    Add package Products.OFSP/4.0.1/Products.OFSP-4.0.1.tar.gz

---
+++ packages/Products.OFSP/Products.OFSP-4.0.1.tar.gz/Products.OFSP-4.0.1/README.rst
@@ -0,0 +1,4 @@
+Overview
+========
+
+OFSP registers the Zope 2 OFS content types.
---
+++ packages/Products.OFSP/Products.OFSP-4.0.1.tar.gz/Products.OFSP-4.0.1/setup.py
@@ -0,0 +1,53 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+__version__ = '4.0.1'
+
+
+setup(name='Products.OFSP',
+      version=__version__,
+      url='http://pypi.python.org/pypi/Products.OFSP',
+      license='ZPL 2.1',
+      description="General Zope 2 help screens.",
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=(open('README.rst').read() + '\n' +
+                        open('CHANGES.rst').read()),
+      packages=find_packages('src'),
+      namespace_packages=['Products'],
+      package_dir={'': 'src'},
+      classifiers=[
+          "Development Status :: 6 - Mature",
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: Zope Public License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy",
+      ],
+      install_requires=[
+          'setuptools',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
