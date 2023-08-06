13e5e5514bfbf619ba34f7f0480d4b472bc3c871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:26 +0000

    Add package nester_zhangzheng/1.1.0/nester_zhangzheng-1.1.0.tar.gz

---
+++ packages/nester_zhangzheng/nester_zhangzheng-1.1.0.tar.gz/nester_zhangzheng-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+		name			= 'nester_zhangzheng',
+		version			= '1.1.0',
+		py_modules		= ['nester'],
+		author			= 'hfpython',
+		author_email	= 'zhangz15@fudan.edu.cn',
+		url				= 'http://www.headfirstlab.com',
+		description		= 'A simple printer of nested list',
+	)
+	
