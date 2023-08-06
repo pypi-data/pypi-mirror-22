6b32103a67734a0bfd310d65e8b4aa194340d553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:15 +0000

    Add package thejoyofpynting/1.4.1/thejoyofpynting-1.4.1-py3-none-any.whl

---
+++ packages/thejoyofpynting/thejoyofpynting-1.4.1-py3-none-any.whl/thejoyofpynting/bottypes.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""""""
+
+import os
+
+bob_types = {
+    0: {
+        "image": os.path.join(os.path.dirname(__file__), "1.png"),
+        "place_x": 20,
+        "place_y": 60,
+        "size_x": 360,
+        "size_y": 280
+    },
+    1: {
+        "image": os.path.join(os.path.dirname(__file__), "2.png"),
+        "place_x": 450,
+        "place_y": 35,
+        "size_x": 260,
+        "size_y": 305
+    },
+    2: {
+        "image": os.path.join(os.path.dirname(__file__), "3.png"),
+        "place_x": 797,
+        "place_y": 15,
+        "size_x": 225,
+        "size_y": 450
+    },
+    3: {
+        "image": os.path.join(os.path.dirname(__file__), "4.png"),
+        "place_x": 250,
+        "place_y": 30,
+        "size_x": 140,
+        "size_y": 160
+    },
+    4: {
+        "image": os.path.join(os.path.dirname(__file__), "5.png"),
+        "place_x": 315,
+        "place_y": 10,
+        "size_x": 145,
+        "size_y": 205
+    },
+    5: {
+        "image": os.path.join(os.path.dirname(__file__), "6.png"),
+        "place_x": 560,
+        "place_y": 330,
+        "size_x": 875,
+        "size_y": 750
+    }
+}
