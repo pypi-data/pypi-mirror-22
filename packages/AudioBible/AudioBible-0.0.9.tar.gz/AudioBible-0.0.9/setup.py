c6f5bb2fc938ba319715f63ee80974a97b3bb5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:12 +0000

    Add package AudioBible/0.0.9/AudioBible-0.0.9.tar.gz

---
+++ packages/AudioBible/AudioBible-0.0.9.tar.gz/AudioBible-0.0.9/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="AudioBible",
+    version=open(os.path.join('audiobible', 'VERSION')).read().strip(),
+    description="KJV Audio Bible",
+    long_description="King James Version Audio Bible - download, listen, read and find verses using search",
+    license="MIT",
+    maintainer="Alex Goretoy",
+    maintainer_email="alex@goretoy.com",
+    url="https://github.com/gxela/AudioBible",
+
+    packages=find_packages(),
+    install_requires=['scrapy'],
+    entry_points={
+        'console_scripts': [
+            'audiobible=audiobible:use_parse_args',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
