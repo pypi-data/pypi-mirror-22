1c4a6762bbe3388e235dcf8ac93fe6763e8a239c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:57 +0000

    Add package test_yl/1.1.1/test_yl-1.1.1.zip

---
+++ packages/test_yl/test_yl-1.1.1.zip/test_yl-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'test_yl',
+        version = '1.1.1',
+        py_modules = ['test_yl'],
+        author = 'yanglian',
+        author_email = '76724223@qq.com',
+        url = 'https://www.baidu.com/',
+        description = '增加了输出位置变量',
+    )
