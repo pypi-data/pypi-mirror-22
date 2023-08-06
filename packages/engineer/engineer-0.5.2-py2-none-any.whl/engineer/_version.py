1c3c51ffbd08af25e300185e033c25c994078468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:24 +0000

    Add package engineer/0.5.2/engineer-0.5.2-py2-none-any.whl

---
+++ packages/engineer/engineer-0.5.2-py2-none-any.whl/engineer/_version.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+import time
+from datetime import date
+from propane_distribution import version_class
+
+# This file is originally generated from Git information by running 'setup.py
+# version'. Distributions contain a pre-generated copy of this file.
+
+__version__ = '0.5.2'
+__date__ = date(2017, 5, 26)
+__time__ = time.gmtime(1495817264.87)
+
+version = version_class(__version__, __date__, __time__)
