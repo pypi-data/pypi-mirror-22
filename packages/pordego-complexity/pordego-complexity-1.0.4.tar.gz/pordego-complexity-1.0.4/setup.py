9b732fd674b232894e46dd02597c564c5a899ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:31 +0000

    Add package pordego-complexity/1.0.4/pordego-complexity-1.0.4.tar.gz

---
+++ packages/pordego-complexity/pordego-complexity-1.0.4.tar.gz/pordego-complexity-1.0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+ANALYSIS_PLUGINS = [
+    "complexity = pordego_complexity.complexity_analysis:analyze_complexity"
+]
+
+with open('LICENSE') as f:
+    LICENSE = f.read()
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7"
+]
+
+VERSION = "1.0.4"
+
+setup(name="pordego-complexity",
+      version=VERSION,
+      license=LICENSE,
+      author="Tim Treptow",
+      author_email="tim.treptow@gmail.com",
+      description="Pordego plugin for code complexity analysis using the Radon library",
+      packages=find_packages(exclude=('tests', 'docs', "tests.*")),
+      url="https://github.com/ttreptow/pordego-complexity",
+      download_url="https://github.com/ttreptow/pordego-complexity/tarball/{}".format(VERSION),
+      entry_points={"pordego.analysis": ANALYSIS_PLUGINS},
+      classifiers=CLASSIFIERS,
+      install_requires=["radon==2.0.0"],
+      include_package_data=True
+      )
