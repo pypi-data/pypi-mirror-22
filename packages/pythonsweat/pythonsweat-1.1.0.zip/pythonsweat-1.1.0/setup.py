2f458e20eab8ee282a943e4373c43ecd7b9540b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:12 +0000

    Add package pythonsweat/1.1.0/pythonsweat-1.1.0.zip

---
+++ packages/pythonsweat/pythonsweat-1.1.0.zip/pythonsweat-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(         name         = 'pythonsweat',
+               version      = '1.1.0',
+               py_modules   = ['python'],
+               author       = 'hfpython',
+               author_email = 'hfpython@headfirstlabs.com',
+               url          = 'http://www.headfirstlabs.com',
+               description  = 'pythonsweat',
+               )
