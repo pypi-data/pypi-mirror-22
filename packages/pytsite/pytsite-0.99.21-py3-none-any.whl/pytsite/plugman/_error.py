785fb17e11239e87e585e23dfe5b3349cca237da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:13 +0000

    Add package pytsite/0.99.21/pytsite-0.99.21-py3-none-any.whl

---
+++ packages/pytsite/pytsite-0.99.21-py3-none-any.whl/pytsite/plugman/_error.py
@@ -0,0 +1,62 @@
+"""PytSite Plugin Manager Errors.
+"""
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class ApiRequestError(Exception):
+    pass
+
+
+class UnknownPlugin(Exception):
+    pass
+
+
+class PluginNotInstalled(Exception):
+    pass
+
+
+class PluginAlreadyInstalled(Exception):
+    pass
+
+
+class PluginInstallationInProgress(Exception):
+    pass
+
+
+class PluginUninstallationInProgress(Exception):
+    pass
+
+
+class PluginInstallError(Exception):
+    pass
+
+
+class PackageInstallError(Exception):
+    pass
+
+
+class PluginUninstallError(Exception):
+    pass
+
+
+class PluginStartError(Exception):
+    pass
+
+
+class PluginAlreadyStarted(Exception):
+    def __init__(self, plugin_name: str):
+        self._name = plugin_name
+
+    def __str__(self) -> str:
+        return "Plugin '{}' is already started".format(self._name)
+
+
+class PluginNotStarted(Exception):
+    def __init__(self, plugin_name: str):
+        self._name = plugin_name
+
+    def __str__(self) -> str:
+        return "Plugin '{}' is not started".format(self._name)
