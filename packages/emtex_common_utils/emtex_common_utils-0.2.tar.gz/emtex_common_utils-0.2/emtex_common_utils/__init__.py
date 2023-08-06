75111b558f5ec8af30c66321bc8b205b44419b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:35 +0000

    Add package emtex_common_utils/0.2/emtex_common_utils-0.2.tar.gz

---
+++ packages/emtex_common_utils/emtex_common_utils-0.2.tar.gz/emtex_common_utils-0.2/emtex_common_utils/__init__.py
@@ -0,0 +1,14 @@
+from threading import local
+
+__version__ = '0.2'
+
+__thread_locals = local()
+
+
+def get_current_user():
+    return getattr(__thread_locals, 'emtex_user', 'AnonymousUser')
+
+
+    def set_current_user(user_email):
+        setattr(__thread_locals, 'emtex_user', user_email)
+
