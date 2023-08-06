efc153815f4d83309926be6a4fb12313a96b8117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:51 +0000

    Add package logdissect/2.0/logdissect-2.0.tar.gz

---
+++ packages/logdissect/logdissect-2.0.tar.gz/logdissect-2.0/logdissect/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '2.0'
+__author__ = 'Dan Persons <dpersonsdev@gmail.com>'
+__license__ = 'MIT License'
+__github__ = 'https://github.com/dogoncouch/logdissect'
+__all__ = ['core', 'data']
+
+import logdissect.parsers
+import logdissect.morphers
+import logdissect.output
+import data
+import core
