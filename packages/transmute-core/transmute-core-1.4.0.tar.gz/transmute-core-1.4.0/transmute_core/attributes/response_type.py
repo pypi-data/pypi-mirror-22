fb8c8829eb0c6458af8b1998771d5d878f84a891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:38 +0000

    Add package transmute-core/1.4.0/transmute-core-1.4.0.tar.gz

---
+++ packages/transmute-core/transmute-core-1.4.0.tar.gz/transmute-core-1.4.0/transmute_core/attributes/response_type.py
@@ -0,0 +1,44 @@
+import attr
+import copy
+from ..compat import string_type
+from swagger_schema import Response
+
+# @attr.s
+# class ParameterType(object):
+#     description =
+
+@attr.s
+class ResponseType(object):
+    type = attr.ib()
+    description = attr.ib(
+        validator=[
+            attr.validators.instance_of(string_type)
+        ],
+        default=""
+    )
+    headers = attr.ib(
+        validator=[
+            attr.validators.instance_of(dict)
+        ],
+        default=attr.Factory(dict)
+    )
+
+    def swagger_definition(self, context):
+        type_definition = context.serializers.to_json_schema(self.type)
+        schema = context.response_shape.swagger(copy.deepcopy(type_definition))
+        headers = {}
+        for name, header in self.headers.items():
+            header_definition = context.serializers.to_json_schema(header["type"])
+            if "description" in header:
+                header_definition["description"] = header["description"]
+            headers[name] = header_definition
+
+        response = {
+            "description": self.description,
+            "schema": schema
+        }
+
+        if headers:
+            response["headers"] = headers
+
+        return Response(response)
