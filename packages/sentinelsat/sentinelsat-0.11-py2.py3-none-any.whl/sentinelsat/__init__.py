55a4501e05181a006bcf73f0dc2f3d104aedf50d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:40 +0000

    Add package sentinelsat/0.11/sentinelsat-0.11-py2.py3-none-any.whl

---
+++ packages/sentinelsat/sentinelsat-0.11-py2.py3-none-any.whl/sentinelsat/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.11'
+
+# Import for backwards-compatibility
+from . import sentinel
+
+from .sentinel import SentinelAPI, SentinelAPIError, InvalidChecksumError, read_geojson, geojson_to_wkt
