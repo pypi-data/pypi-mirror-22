b00d34631ece6f9d716fc306643d49ed508168ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:59 +0000

    Add package isogeo-pysdk/2.18.0.post491/isogeo-pysdk-2.18.0.post491.tar.gz

---
+++ packages/isogeo-pysdk/isogeo-pysdk-2.18.0.post491.tar.gz/isogeo-pysdk-2.18.0.post491/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ############################################################################
+# ########## Libraries #############
+# ##################################
+
+# standard library
+from codecs import open
+from os import path
+from setuptools import setup, find_packages
+
+# custom module
+import isogeo_pysdk
+
+# SETUP ######################################################################
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="isogeo-pysdk",
+    version=isogeo_pysdk.__version__,
+    packages=find_packages(),
+    author="GeoJulien",
+    author_email="julien.moura at isogeo.com",
+    description="Abstraction class to use Isogeo REST API",
+    install_requires=["requests>=2.9.1",
+                      "future",
+                      ],
+    extras_require={
+        ":python_version == '2.7'": [
+            "configparser",
+            "six"
+        ],
+    },
+    include_package_data=True,
+    url="https://github.com/Guts/isogeo-api-py-minsdk",
+    keywords=["GIS", "metadata", "INSPIRE", "Isogeo",
+              "API", "REST", "geographical data"],
+    license='GPL3',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=long_description,
+)
