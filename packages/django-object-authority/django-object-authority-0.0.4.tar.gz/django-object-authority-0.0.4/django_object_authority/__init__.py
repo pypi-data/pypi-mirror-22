c1d402653b3ecc60ca7b6d1219cc84de0b82c2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:43 +0000

    Add package django-object-authority/0.0.4/django-object-authority-0.0.4.tar.gz

---
+++ packages/django-object-authority/django-object-authority-0.0.4.tar.gz/django-object-authority-0.0.4/django_object_authority/__init__.py
@@ -0,0 +1,23 @@
+from .authorizations import authorization, NoAuthorizationRegistered, AlreadyAuthorizationRegistered
+from .decorators import register
+from .filters import AuthorityBaseFilter, BaseFilter
+from .loader import autodiscover_authorizations
+from .mixins import AuthorizationMixin
+from .options import BaseObjectAuthorization, BaseUserObjectAuthorization
+
+
+__version__ = "0.0.4"
+
+
+default_app_config = 'django_object_authority.apps.DjangoObjectAuthorityConfig'
+
+
+__all__ = [
+    'AuthorityBaseFilter', 'AlreadyAuthorizationRegistered', 'AuthorizationMixin', 'BaseFilter',
+    'BaseObjectAuthorization', 'BaseUserObjectAuthorization', 'NoAuthorizationRegistered', 'authorization',
+    'autodiscover', 'register',
+]
+
+
+def autodiscover():
+    autodiscover_authorizations('authorizations', register_to=authorization)
