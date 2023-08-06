b20184bb5ee7cbc8da6f894254c8e17c8c8770b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:48 +0000

    Add package UISoup/2.5.5/UISoup-2.5.5.tar.gz

---
+++ packages/UISoup/UISoup-2.5.5.tar.gz/UISoup-2.5.5/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from os import path
+
+from pip.req import parse_requirements
+from setuptools import setup
+
+
+def package_env(file_name, strict=False):
+    file_path = path.join(path.dirname(__file__), file_name)
+    if path.exists(file_path) or strict:
+        return open(file_path).read()
+    else:
+        return ''
+
+if __name__ == '__main__':
+    setup(
+        name='UISoup',
+        version='2.5.5',
+        description='Cross Platform GUI Test Automation tool.',
+        long_description=package_env('README.rst'),
+        author='Max Beloborodko',
+        author_email='f1ashhimself@gmail.com',
+        packages=['uisoup'],
+        include_package_data=True,
+        install_requires=[str(ir.req) for ir in
+                          parse_requirements('requirements.txt',
+                                             session=False)],
+        zip_safe=False,
+        entry_points={
+            'console_scripts': [
+                'ui-inspector = uisoup.ui_inspector:main'
+            ]
+        }
+    )
