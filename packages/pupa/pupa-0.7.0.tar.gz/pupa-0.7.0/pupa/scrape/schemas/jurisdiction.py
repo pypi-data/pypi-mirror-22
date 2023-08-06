f9a8e9e5af308be9bdb8e10daa78c149f2ea2bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:13 +0000

    Add package pupa/0.7.0/pupa-0.7.0.tar.gz

---
+++ packages/pupa/pupa-0.7.0.tar.gz/pupa-0.7.0/pupa/scrape/schemas/jurisdiction.py
@@ -0,0 +1,23 @@
+from .common import extras, fuzzy_date_blank
+from opencivicdata import common
+
+
+schema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "url": {"type": "string"},
+        "classification": {"type": "string"},   # TODO: enum
+        "division_id": {"type": "string"},
+        "legislative_sessions": {
+            "type": "array", "items": {"type": "object", "properties": {
+                "name": {"type": "string"},
+                "type": {"type": "string", "enum": ["primary", "special"]},
+                "start_date": fuzzy_date_blank,
+                "end_date": fuzzy_date_blank,
+            }},
+        },
+        "feature_flags": { "type": "array", "items": {"type": "string"} },
+        "extras": extras,
+    }
+}
