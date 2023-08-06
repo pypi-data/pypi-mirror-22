43ab7c1f10c09bffa8d18375f5172dfc619f4543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:11 +0000

    Add package falsy/2017.6.6.dev113/falsy-2017.6.6.dev113-py3-none-any.whl

---
+++ packages/falsy/falsy-2017.6.6.dev113-py3-none-any.whl/falsy/netboy/fetch.py
@@ -0,0 +1,34 @@
+
+from falsy.netboy.request import get_request, post_request
+import asyncio as aio
+
+
+async def get_boy(payload):
+    targets = []
+    for p in payload:
+        targets.append(get_request(p))
+    res = await aio.gather(
+        *targets, return_exceptions=True
+    )
+    return res
+
+async def post_boy(payload):
+    targets = []
+    for payload in payload:
+        targets.append(post_request(payload))
+    res = await aio.gather(
+        *targets, return_exceptions=True
+    )
+    return res
+
+async def net_boy(payload):
+    targets = []
+    for payload in payload:
+        if payload.get('postfields'):
+            targets.append(post_request(payload))
+        else:
+            targets.append(get_request(payload))
+    res = await aio.gather(
+        *targets, return_exceptions=True
+    )
+    return res
