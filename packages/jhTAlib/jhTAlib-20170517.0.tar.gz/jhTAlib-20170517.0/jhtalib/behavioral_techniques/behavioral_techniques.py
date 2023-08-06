b02ca1b62e3d5f54de25d10af1a448121bf54d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:53 +0000

    Add package jhTAlib/20170517.0/jhTAlib-20170517.0.tar.gz

---
+++ packages/jhTAlib/jhTAlib-20170517.0.tar.gz/jhTAlib-20170517.0/jhtalib/behavioral_techniques/behavioral_techniques.py
@@ -0,0 +1,75 @@
+def FIBOPR(df, price='Close'):
+    """
+    Fibonacci Price Retracements
+    """
+
+def FIBOTR(df, price='Close'):
+    """
+    Fibonacci Time Retracements
+    """
+
+def GANNPR(df, price='Close'):
+    """
+    W. D. Gann Price Retracements
+    """
+
+def GANNTR(df, price='Close'):
+    """
+    W. D. Gann Time Retracements
+    """
+
+def SUNC(df):
+    """
+    Sun Cycle
+    """
+
+def MERCURYC(df):
+    """
+    Mercury Cycle
+    """
+
+def VENUSC(df):
+    """
+    Venus Cycle
+    """
+
+def EARTHC(df):
+    """
+    Earth Cycle
+    """
+
+def MARSC(df):
+    """
+    Mars Cycle
+    """
+
+def JUPITERC(df):
+    """
+    Jupiter Cycle
+    """
+
+def SATURNC(df):
+    """
+    Saturn Cycle
+    """
+
+def URANUSC(df):
+    """
+    Uranus Cycle
+    """
+
+def NEPTUNEC(df):
+    """
+    Neptune Cycle
+    """
+
+def PLUTOC(df):
+    """
+    Pluto Cycle
+    """
+
+def MOONC(df):
+    """
+    Moon Cycle
+    """
+
