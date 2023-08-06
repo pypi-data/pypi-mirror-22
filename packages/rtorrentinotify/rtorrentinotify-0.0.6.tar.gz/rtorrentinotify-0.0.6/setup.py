6a0c40dea03d947514887c8da7037b0fb893a033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:57 +0000

    Add package rtorrentinotify/0.0.6/rtorrentinotify-0.0.6.tar.gz

---
+++ packages/rtorrentinotify/rtorrentinotify-0.0.6.tar.gz/rtorrentinotify-0.0.6/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="rtorrentinotify",
+    version="0.0.6",
+    author="eayin2",
+    author_email="eayin2@gmail.com",
+    packages=find_packages(),
+    description="Watches a directory for torrents and moves them to the actual rtorrent watch dir",
+    install_requires=["helputils", "pyinotify"],
+    entry_points={
+        'console_scripts': [
+            'rtorrentinotify = rtorrentinotify.core:clidoor',
+        ]
+    }
+)
