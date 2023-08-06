2518fe0494b1d6ff5ae219bee3fdd67519e2b911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:02 +0000

    Add package importanize/0.5.3/importanize-0.5.3-py3-none-any.whl

---
+++ packages/importanize/importanize-0.5.3-py3-none-any.whl/importanize/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+
+
+__author__ = 'Miroslav Shubernetskiy'
+__email__ = 'miroslav@miki725.com'
+__version__ = '0.5.3'
+__description__ = (
+    'Utility for organizing Python imports using PEP8 or custom rules'
+)
