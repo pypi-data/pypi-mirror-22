6377bb25aa49898bc5655560d0143f2db6a92e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:13 +0000

    Add package miyadaiku.themes.jquery/0.0.5/miyadaiku.themes.jquery-0.0.5.tar.gz

---
+++ packages/miyadaiku.themes.jquery/miyadaiku.themes.jquery-0.0.5.tar.gz/miyadaiku.themes.jquery-0.0.5/setup.py
@@ -0,0 +1,43 @@
+import os
+import pathlib
+from setuptools import setup, find_packages
+
+DIR = pathlib.Path(__file__).resolve().parent
+os.chdir(DIR)
+
+
+requires = [
+    "miyadaiku"
+]
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def list_packages(root):
+    yield root
+    for dirpath, dirnames, filenames in os.walk(root):
+        for d in dirnames:
+            if not d.startswith('_'):
+                path = os.path.join(dirpath, d).replace(os.path.sep, '.')
+                yield path
+
+setup(
+    name="miyadaiku.themes.jquery",
+    version="0.0.5",
+    author="Atsuo Ishimoto",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    description='jQuery files for miyadaiku static site generator',
+    long_description=read('README.rst'),
+    packages=list(list_packages('miyadaiku')),
+    package_data={
+        '': ['*.rst', '*.md', '*.html', '*.css', '*.js', '*.yml', '*.png', '*.jpg', '*.jpeg'],
+    },
+    install_requires=requires,
+    include_package_data=True,
+    zip_safe=False
+)
