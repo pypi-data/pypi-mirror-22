6270d96ef22db3aba6260c43f7c04cd32c4ce40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:34 +0000

    Add package GitFeed/0.1.8/GitFeed-0.1.8.tar.gz

---
+++ packages/GitFeed/GitFeed-0.1.8.tar.gz/GitFeed-0.1.8/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+import gitfeed
+
+setup(name='GitFeed',
+      version='0.1.8',
+      description='Check your GitHub Newsfeed via the command-line.',
+      author='Ritiek Malhotra',
+      author_email='ritiekmalhotra123@gmail.com',
+      packages = find_packages(),
+      entry_points={
+            'console_scripts': [
+                  'gitfeed = gitfeed.gitfeed:cli',
+            ]
+      },
+      url='https://www.github.com/Ritiek/GitFeed',
+      keywords=['GitHub', 'news', 'feed', 'command-line', 'python'],
+      license='MIT',
+      download_url='https://github.com/Ritiek/GitFeed/archive/v0.1.8.tar.gz',
+      classifiers=[],
+      install_requires=[
+            'requests >= 2.17.3',
+            'colorama >= 0.3.7',
+      ]
+     )
+
