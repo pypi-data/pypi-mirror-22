a66911dacd8a87bfdc4f4e79e85e841b90d93d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:55 +0000

    Add package django-bootstrap3-datetimepicker-vinta/2.4.3/django-bootstrap3-datetimepicker-vinta-2.4.3.tar.gz

---
+++ packages/django-bootstrap3-datetimepicker-vinta/django-bootstrap3-datetimepicker-vinta-2.4.3.tar.gz/django-bootstrap3-datetimepicker-vinta-2.4.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup
+
+# ./setup.py sdist upload
+
+long_description = ''
+if 'upload' in sys.argv or 'register' in sys.argv:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+
+setup(
+    name='django-bootstrap3-datetimepicker-vinta',
+    packages=['bootstrap3_datetime'],
+    include_package_data=True,
+    version='2.4.3',
+    description='Bootstrap3 compatible datetimepicker for Django projects.',
+    long_description=long_description,
+    author='Nakahara Kunihiko/Samuel Colvin',
+    author_email='nakahara.kunihiko@gmail.com/s@muelcolvin.com',
+    url='https://github.com/samuelcolvin/django-bootstrap3-datetimepicker',
+    license='Apache License 2.0',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
+)
