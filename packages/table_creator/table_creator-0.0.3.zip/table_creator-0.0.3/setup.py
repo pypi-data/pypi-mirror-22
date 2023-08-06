27be755984c70047d28174b20481b187925961cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:27 +0000

    Add package table_creator/0.0.3/table_creator-0.0.3.zip

---
+++ packages/table_creator/table_creator-0.0.3.zip/table_creator-0.0.3/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='table_creator',
+    version="0.0.3",
+    author = "laurent.erreca",
+    author_email = "erreca.laurent@gmail.com",
+    description = ("Package to create tables in various sgbd"),
+    packages=find_packages()
+    )
+
