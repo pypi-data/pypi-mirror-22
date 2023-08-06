0bde8352b516cb61bbc0ed69b0851b1e051e1cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:28 +0000

    Add package definition/0.1.0/definition-0.1.0.tar.gz

---
+++ packages/definition/definition-0.1.0.tar.gz/definition-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+add_keywords = dict(
+    entry_points={
+        'console_scripts': ['definition = definition.definition:main'],
+    },
+)
+
+fhan = open('requirements.txt', 'rU')
+requires = [line.strip() for line in fhan.readlines()]
+fhan.close()
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    fhan = open('README.txt')
+    long_description = fhan.read()
+    fhan.close()
+
+setup(
+        name='definition',
+        description='Definition fetcher from 5 online dictionaries',
+        version='0.1.0',
+        packages=['definition'],
+        license='GPLv3+',
+        author='Shubham Chaudhary',
+        author_email='me@shubhamchaudhary.in',
+        url='https://github.com/shubhamchaudhary/definition',
+        long_description=long_description,
+        install_requires=requires,
+        **add_keywords
+)
