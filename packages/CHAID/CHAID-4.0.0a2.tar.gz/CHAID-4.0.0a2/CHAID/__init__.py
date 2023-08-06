5a8ffaa1720e1fcfb5a73a254514d5c0d6418ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:31 +0000

    Add package CHAID/4.0.0a2/CHAID-4.0.0a2.tar.gz

---
+++ packages/CHAID/CHAID-4.0.0a2.tar.gz/CHAID-4.0.0a2/CHAID/__init__.py
@@ -0,0 +1,7 @@
+from .split import Split
+from .tree import Tree
+from .node import Node
+from .column import NominalColumn, OrdinalColumn, ContinuousColumn
+from .stats import Stats
+
+__version__ = "4.0.0-alpha.2"
