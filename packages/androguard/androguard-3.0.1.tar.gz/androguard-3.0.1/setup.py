5f277e4db29cb7a0a341b20962c0d97e33012aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:07 +0000

    Add package androguard/3.0.1/androguard-3.0.1.tar.gz

---
+++ packages/androguard/androguard-3.0.1.tar.gz/androguard-3.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'androguard',
+    version = '3.0.1',
+    packages = find_packages(),
+    scripts = ['androaxml.py', 'androcsign.py', 'androdiff.py', 'androgexf.py',
+               'androlyze.py', 'androsign.py', 'androsim.py', 'apkviewer.py',
+               'androdd.py', 'androgui.py',
+               ],
+)
