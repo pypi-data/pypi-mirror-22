a37c87a0df8cdd0fb6dbbabbcc6b88db06232a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:29 +0000

    Add package tornadoist2/0.2.0/tornadoist2-0.2.0.tar.gz

---
+++ packages/tornadoist2/tornadoist2-0.2.0.tar.gz/tornadoist2-0.2.0/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "tornadoist2",
+    version = "0.2.0",
+    author = "Eren G\xc3\xbcven, Yalei Du",
+    author_email = "erenguven0@gmail.com",
+    description = "mixins for tornado",
+    license = "Apache License, Version 2",
+    keywords = ["tornado", "celery"],
+    url = "https://github.com/badbye/tornadoist.git",
+    packages = ['tornadoist2',],
+    long_description = open("README.rst").read(),
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
