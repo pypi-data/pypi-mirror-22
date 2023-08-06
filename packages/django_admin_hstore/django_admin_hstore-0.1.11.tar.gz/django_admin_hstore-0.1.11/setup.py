5f30ba3f701b77aadbf9fc9c04d7aec2cf1c6864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:58 +0000

    Add package django_admin_hstore/0.1.11/django_admin_hstore-0.1.11.tar.gz

---
+++ packages/django_admin_hstore/django_admin_hstore-0.1.11.tar.gz/django_admin_hstore-0.1.11/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+from django_admin_hstore import __version__
+
+setup(
+    name='django_admin_hstore',
+    version=__version__,
+    url='https://bitbucket.org/deusesprl/django_hstore',
+    author='Maxime Deuse',
+    author_email='m.deuse@deuse.be',
+    description='An admin widget for the Hstore, this widget usefulness is to have dynamic fields for a given model.',
+    license='MIT',
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'django>=1.11.0',
+    ],
+)
