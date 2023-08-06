fa0bdc3b8ea5bb5bfda33788011311387ca952b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:41 +0000

    Add package gems/0.2.0/gems-0.2.0.tar.gz

---
+++ packages/gems/gems-0.2.0.tar.gz/gems-0.2.0/gems/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Blake Printy'
+__email__ = 'bprinty@gmail.com'
+__version__ = '0.2.0'
+
+
+from .datatypes import composite
+from .datatypes import filetree
+
