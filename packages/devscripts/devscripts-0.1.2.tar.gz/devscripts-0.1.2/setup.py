e712d40b67b882115793895fd2db6e1c6841eaaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:12 +0000

    Add package devscripts/0.1.2/devscripts-0.1.2.tar.gz

---
+++ packages/devscripts/devscripts-0.1.2.tar.gz/devscripts-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='devscripts',
+    packages=['devscripts'],
+    version='0.1.2',
+    description='Scripts and functions to use while coding',
+    author='Theo Massard',
+    author_email='massar_t@etna-alternance.net',
+    url='https://github.com/massard-t/devscripts',
+    download_url='https://github.com/massard-t/devscripts/archive/0.1.2.tar.gz',
+    keywords=['scripts', 'functions', 'optimisation'],
+    classifiers=[],
+)
