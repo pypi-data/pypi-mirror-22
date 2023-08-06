7c143b74532d9e015012e4bf2df00d20491f0476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:35 +0000

    Add package pycreate2/0.5.8/pycreate2-0.5.8-py3-none-any.whl

---
+++ packages/pycreate2/pycreate2-0.5.8-py3-none-any.whl/pycreate2-0.5.8.data/scripts/shutdown.py
@@ -0,0 +1,26 @@
+#!python
+
+from __future__ import print_function
+import pycreate2
+import time
+
+
+port = '/dev/tty.usbserial-DA01NX3Z'
+
+baud = {
+	'default': 115200,
+	'alt': 19200
+}
+
+bot = pycreate2.Create2(port=port, baud=baud['default'])
+
+bot.start()
+# bot.shutdown()
+# time.sleep(1)
+bot.stop()
+time.sleep(1)
+
+print('=====================================================')
+print('\n\tCreate Shutdown')
+print('\tHit power button to wake-up\n')
+print('=====================================================')
