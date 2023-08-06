aeb518200d1ee87de762f8b24aa00a03ec20d26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:48 +0000

    Add package FlowCal/1.1.4/FlowCal-1.1.4.tar.gz

---
+++ packages/FlowCal/FlowCal-1.1.4.tar.gz/FlowCal-1.1.4/FlowCal/__init__.py
@@ -0,0 +1,17 @@
+"""
+`FlowCal`: Calibration and analysis of flow cytometry data.
+
+"""
+
+# Versions should comply with PEP440.  For a discussion on single-sourcing
+# the version across setup.py and the project code, see
+# https://packaging.python.org/en/latest/single_source_version.html
+__version__ = '1.1.4'
+
+import io
+import excel_ui
+import gate
+import transform
+import mef
+import plot
+import stats
