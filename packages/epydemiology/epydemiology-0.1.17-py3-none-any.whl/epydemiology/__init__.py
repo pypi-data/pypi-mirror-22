3cc777e0192ebe8afbb4bb043c95a28d5e083512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:20 +0000

    Add package epydemiology/0.1.17/epydemiology-0.1.17-py3-none-any.whl

---
+++ packages/epydemiology/epydemiology-0.1.17-py3-none-any.whl/epydemiology/__init__.py
@@ -0,0 +1,7 @@
+from .phjRROR import phjOddsRatio
+from .phjRROR import phjRelativeRisk
+from .phjSelectData import phjSelectCaseControlDataset
+from .phjGetData import phjReadDataFromExcelNamedCellRange
+from .phjGetDBData import phjGetDataFromDatabase
+from .phjGetDBData import phjReadTextFromFile
+from .phjMatrices import phjBinaryVarsToSquareMatrix
