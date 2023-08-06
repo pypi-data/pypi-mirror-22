786ac8d67e1b7f7f6b0c9248698bb3f2a966d0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:40 +0000

    Add package sentry/8.16.1/sentry-8.16.1.tar.gz

---
+++ packages/sentry/sentry-8.16.1.tar.gz/sentry-8.16.1/src/sentry/static/sentry/app/components/group/participants.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Avatar from '../avatar';
+
+const GroupParticipants = React.createClass({
+  propTypes: {
+    participants: React.PropTypes.array.isRequired
+  },
+
+  render() {
+    let participants = this.props.participants;
+
+    return (
+      <div>
+        <h6>
+          <span>
+            {participants.length} {'Participant' + (participants.length === 1 ? '' : 's')}
+          </span>
+        </h6>
+        <ul className="faces">
+          {participants.map((user, i) => {
+            return (
+              <li key={i}>
+                <Avatar size={32} user={user} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+});
+
+export default GroupParticipants;
---
+++ packages/sentry/sentry-8.16.1.tar.gz/sentry-8.16.1/src/sentry/static/sentry/dist/sentry.js
@@ -0,0 +1,2 @@
+var exports=webpackJsonpexports([12],{1087:function(o,p){}},[1087]);
+//# sourceMappingURL=sentry.js.map
