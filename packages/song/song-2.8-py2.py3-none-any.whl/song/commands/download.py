f8de8b95c972776909c195a4029a11550fdf96bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:03 +0000

    Add package song/2.8/song-2.8-py2.py3-none-any.whl

---
+++ packages/song/song-2.8-py2.py3-none-any.whl/song/commands/download.py
@@ -0,0 +1,41 @@
+# skele/commands/download.py
+"""The hello command."""
+ 
+ 
+from json import dumps
+ 
+from .base import Base
+from .FactoryProducer import FactoryProducer
+from .FileDownload import FileDownload
+class Download(Base):
+    """Say hello, world!"""
+ 
+    def run(self):
+        #print 'Hello, world!'
+        #print 'You supplied the following options:', dumps(self.options, indent=2, sort_keys=True)
+        name=self.options["TEXT"]
+        '''p=Parse(name)
+       	download_url=p.get_download_url()
+    	file_url=p.get_file_download_url(download_url)
+    	p.file_download_using_wget(file_url)
+    	'''
+        factory = FactoryProducer()
+    	p = factory.getFactory("search engine parser factory")
+        if self.options['-y'] == True:   
+            print "Download from youtube" 
+            q = factory.getFactory("music website parser factory")
+            q = q.getParser("youtube")
+            link = q.Parse(name)
+            print link
+            file_download=FileDownload()
+            file_download.file_download_using_youtube_dl(link)
+        elif self.options['-d'] == True:
+            p=p.getParser("google")
+            website_url = p.Parse(name,"mr jatt")
+            q = factory.getFactory("music website parser factory")
+            q = q.getParser("mr jatt")
+            download_url = q.Parse(website_url,name)
+            file_download=FileDownload()
+            file_download.file_download_cross_platform(download_url)
+
+
