0d77c101a8b9af11f1f2444a050cf5c6a8adf1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:21 +0000

    Add package ifrs/0.0.0.1/ifrs-0.0.0.1.tar.gz

---
+++ packages/ifrs/ifrs-0.0.0.1.tar.gz/ifrs-0.0.0.1/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+
+long_desc = """
+This is made for some specific environment.
+This contains ...
+"""
+
+setup(name='ifrs',
+        version='0.0.0.1',
+        description='useful tools for Korean Financial Statement disclosured on https://dart.fss.or.kr',
+        long_description=long_desc,
+        url='http://github.com/pydemia/ifrs',
+        author='Young-Ju Kim',
+        author_email='pydemia@gmail.com',
+        license='MIT License',
+        classifiers=[
+                # How Mature: 3 - Alpha, 4 - Beta, 5 - Production/Stable
+                'Development Status :: 3 - Alpha',
+                'Programming Language :: Python :: 3.5'
+                ],
+        packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+        install_requires=['python-xbrl'],
+        zip_safe=False)
