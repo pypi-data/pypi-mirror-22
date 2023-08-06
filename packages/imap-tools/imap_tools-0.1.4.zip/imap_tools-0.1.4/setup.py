1f9ddde6544a08612026aca2d32e9ed47a49fdae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:14 +0000

    Add package imap-tools/0.1.4/imap_tools-0.1.4.zip

---
+++ packages/imap-tools/imap_tools-0.1.4.zip/imap_tools-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='imap_tools',
+    version='0.1.4',
+    packages=['imap_tools'],
+    url='https://github.com/ikvk/imap_tools',
+    license='MIT',
+    long_description=open("README.rst").read(),
+    author='v.kaukin',
+    author_email='workkvk@gmail.com',
+    description='Tool for work with e-mail messages and not with the imap protocol.',
+)
