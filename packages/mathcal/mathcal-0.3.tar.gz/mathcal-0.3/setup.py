59e7f295c7901cec2008633d218f8e0db700d32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:30 +0000

    Add package mathcal/0.3/mathcal-0.3.tar.gz

---
+++ packages/mathcal/mathcal-0.3.tar.gz/mathcal-0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+
+setup(
+	name="mathcal",
+	version="0.3",
+	author="jagadish",
+	author_email="jagadish@hgmail.com",
+	description="math calculations",
+	license="MIT",
+	packages=["mathcal"]
+
+)
