ac274843baf6687f479048a80d577c7dc80a9b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:31 +0000

    Add package hostlist/1.3.3/hostlist-1.3.3-py3-none-any.whl

---
+++ packages/hostlist/hostlist-1.3.3-py3-none-any.whl/hostlist/daemon.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+# import types
+# from distutils.util import strtobool
+# import sys
+
+import git
+import logging
+import datetime
+import cherrypy
+
+from . import hostlist
+from . import cnamelist
+from . import output_services
+from .config import CONFIGINSTANCE as Config
+
+
+class Inventory():
+
+    def __init__(self):
+        try:
+            self.repo = git.Repo('.')
+        except git.InvalidGitRepositoryError:
+            self.repo = git.Repo('../')
+        self.last_update = None
+        self.fetch_hostlist()
+
+    def fetch_hostlist(self, timeout=600):
+        if self.last_update and datetime.datetime.now() - self.last_update < datetime.timedelta(seconds=timeout):
+            return
+        self.last_update = datetime.datetime.now()
+
+        try:
+            pullresult = self.repo.remote().pull()[-1]
+            if not pullresult.flags & pullresult.HEAD_UPTODATE:
+                logging.error("Hosts repo not up to date after pull.")
+        except:
+            logging.error("Failed to pull hosts repo.")
+        Config.load()
+        self.hostlist = hostlist.YMLHostlist()
+        self.cnames = cnamelist.FileCNamelist()
+        print("Refreshed cache.")
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def ansible(self):
+        self.fetch_hostlist()
+        return output_services.AnsibleOutput.gen_content(self.hostlist, self.cnames)
+
+    @cherrypy.expose
+    def munin(self):
+        self.fetch_hostlist()
+        return output_services.MuninOutput.gen_content(self.hostlist, self.cnames)
+
+    @cherrypy.expose
+    def dhcp(self):
+        self.fetch_hostlist()
+        return output_services.DhcpOutput.gen_content(self.hostlist, self.cnames)
+
+    @cherrypy.expose
+    def hosts(self):
+        self.fetch_hostlist()
+        return output_services.HostsOutput.gen_content(self.hostlist, self.cnames)
+
+    @cherrypy.expose
+    def ethers(self):
+        self.fetch_hostlist()
+        return output_services.EthersOutput.gen_content(self.hostlist, self.cnames)
+
+    @cherrypy.expose
+    def list(self):
+        self.fetch_hostlist()
+        return output_services.WebOutput.gen_content(self.hostlist, self.cnames)
+
+    @cherrypy.expose
+    def status(self):
+        result = 'Have a hostlist with %s hosts and %s cnames.' % (len(self.hostlist), len(self.cnames))
+        result += '\nLast updated: %s' % self.last_update
+        return result
+
+    @cherrypy.expose
+    @cherrypy.config(**{'tools.caching.delay': 10})
+    def refreshcache(self):
+        cherrypy.lib.caching.cherrypy._cache.clear()
+        self.fetch_hostlist(timeout=10)
+
+    @cherrypy.expose
+    def index(self):
+        return 'See <a href="https://github.com/particleKIT/hostlist">github.com/particleKIT/hostlist</a> how to use this API.'
+
+
+def _auth_config(app):
+    if app.config.get('/', {}).get('tools.auth_digest.on', False):
+        users = app.config['authentication']
+        app.config['/'].update({'tools.auth_digest.get_ha1': cherrypy.lib.auth_digest.get_ha1_dict_plain(users)})
+    elif app.config.get('/', {}).get('tools.auth_basic.on', False):
+        users = app.config['authentication']
+
+        def check_pass(realm, user, password):
+            if user not in users:
+                return False
+            else:
+                return users[user] == password
+        app.config['/'].update({'tools.auth_basic.checkpassword': check_pass})
+
+
+def main():
+    cherrypy.config.update('daemon.conf')
+    app = cherrypy.tree.mount(Inventory(), '/', 'daemon.conf')
+    _auth_config(app)
+    cherrypy.engine.signals.subscribe()
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/hostlist/hostlist-1.3.3-py3-none-any.whl/hostlist/output_services.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+import os
+
+from .host import Host
+from .hostlist import Hostlist
+from .cnamelist import CNamelist
+from .config import CONFIGINSTANCE as Config
+
+
+class Ssh_Known_HostsOutput:
+    "Generate hostlist for ssh-keyscan"
+
+    @classmethod
+    def gen_content(cls, hostlist: Hostlist, cnames: CNamelist) -> str:
+        # only scan keys on hosts that are in ansible
+        scan_hosts = [
+            h for h in hostlist
+            if 'ssh_known_hosts' in h.groups
+        ]
+        aliases = [alias
+                   for host in scan_hosts
+                   for alias in host.aliases
+                   if host.ip]
+        aliases += [str(host.ip)
+                    for host in scan_hosts
+                    if host.ip]
+        aliases += [cname.fqdn for cname in cnames]
+
+        fcont = '\n'.join(aliases)
+
+        return fcont
+
+
+class HostsOutput:
+    "Config output for /etc/hosts format"
+
+    @classmethod
+    def gen_content(cls, hostlist: Hostlist, cnames: CNamelist) -> str:
+        hoststrings = (str(h.ip) + " " + " ".join(h.aliases) for h in hostlist if h.ip)
+        content = '\n'.join(hoststrings)
+        return content
+
+
+class MuninOutput:
+    "Config output for Munin"
+
+    @classmethod
+    def gen_content(cls, hostlist: Hostlist, cnames: CNamelist) -> str:
+        hostnames = (
+            h for h in hostlist
+            if 'muninnode' in h.groups
+        )
+        fcont = ''
+        for host in hostnames:
+            fcont += cls._get_hostblock(host)
+        return fcont
+
+    @staticmethod
+    def _get_hostblock(host: Host) -> str:
+        cont = '[{institute}{hosttype};{h}]\naddress {h}\n'.format(
+            h=host.fqdn,
+            institute=host.vars['institute'],
+            hosttype=host.vars['hosttype'],
+        )
+        for line in host.vars.get('munin', []):
+            cont += line + '\n'
+        return cont
+
+
+class DhcpOutput:
+    "DHCP config output"
+
+    @classmethod
+    def gen_content(cls, hostlist: Hostlist, cnames: CNamelist) -> str:
+        out = ""
+        for host in hostlist:
+            entry = cls._gen_hostline(host)
+            if not entry:
+                continue
+            out += entry + '\n'
+        return out
+
+    @staticmethod
+    def _gen_hostline(host: Host) -> str:
+        if host.mac and host.ip:
+            # curly brackets doubled for python format function
+            return """host {fqdn} {{
+        hardware ethernet {mac};
+        fixed-address {ip};
+        option host-name "{hostname}";
+        option domain-name "{domain}";
+        }}""".format(fqdn=host.fqdn, mac=host.mac, ip=host.ip, hostname=host.hostname, domain=host.domain)
+
+
+class AnsibleOutput:
+    "Ansible inventory output"
+
+    @classmethod
+    def gen_content(cls, hostlist: Hostlist, cnames: CNamelist) -> dict:
+        """generate json inventory for ansible
+        form:
+            {
+        "_meta": {
+            "hostvars": {
+                "myhost.abc.kit.edu": {
+                    "hosttype": "desktop",
+                    "institute": "abc",
+                    "custom_variable": "foo",
+                }
+                ...
+                }
+             },
+          "groupname" : [
+              "myhost2.abc.kit.edu",
+          ]
+            ...
+
+        """
+        resultdict = defaultdict(lambda: {'hosts': []})  # type: dict
+        #  with python>=3.6 # type: defaultdict[str, Any]
+        hostvars = {}
+        docker_services = {}
+        for host in hostlist:
+            # online add hosts that have ansible=yes
+            if 'ansible' not in host.groups:
+                continue
+            ans = cls._gen_host_content(host)
+            hostvars[ans['fqdn']] = ans['vars']
+            for groupname in ans['groups']:
+                resultdict[groupname]['hosts'] += [ans['fqdn']]
+
+            if ans['vars']['hosttype'] == 'docker':
+                resultdict['dockerhost-' + host.hostname]['hosts'] += [ans['vars']['docker']['host']]
+                docker_services[host.hostname] = ans['vars']['docker']
+                docker_services[host.hostname]['fqdn'] = host.fqdn
+                docker_services[host.hostname]['ip'] = str(host.ip)
+
+        resultdict['_meta'] = {'hostvars': hostvars}
+        if docker_services:
+            resultdict['vserverhost']['vars'] = {'docker_services': docker_services}
+
+        return resultdict
+
+    @staticmethod
+    def _gen_host_content(host):
+        "Generate output for one host"
+
+        result = {
+            'fqdn': host.fqdn,
+            'groups': host.groups,
+            'vars': {},
+        }
+        if host.ip:
+            result['vars']['ip'] = str(host.ip)
+
+        ansiblevars = Config.get('ansiblevars', []) + ['hosttype', 'institute', 'docker']
+        for avar in ansiblevars:
+            if avar in host.vars:
+                result['vars'][avar] = host.vars[avar]
+        return result
+
+
+class EthersOutput:
+    "/etc/ethers format output"
+
+    @classmethod
+    def gen_content(cls, hostlist: Hostlist, cnames: CNamelist) -> str:
+        entries = (
+            "%s %s" % (h.mac, alias)
+            for h in hostlist
+            for alias in h.aliases
+            if h.mac
+        )
+        out = '\n'.join(entries)
+        return out
+
+
+class WebOutput:
+    "HTML Table of hosts"
+
+    @classmethod
+    def gen_content(cls, hostlist, cnames):
+        if os.path.exists('header.html'):
+            with open('header.html') as file:
+                header = file.read()
+        else:
+            header = '<html><body>'
+
+        fields = Config.get('weboutput_columns', ['institute', 'hosttype', 'hostname'])
+        thead = '<table><thead><tr><th>' + '</th><th>'.join(fields) + '</th></tr></thead>\n'
+        footer = '</table></body></html>'
+        hostlist = '\n'.join(
+            '<tr><td>' + '</td><td>'.join(str(h.vars.get(field, '')) for field in fields) + '</td></tr>'
+            for h in hostlist
+        )
+        return header + thead + hostlist + footer
