94ee16fd72bcfb0862dfd1b436608906d1e0f560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:14 +0000

    Add package pybullet/1.1.6/pybullet-1.1.6.tar.gz

---
+++ packages/pybullet/pybullet-1.1.6.tar.gz/pybullet-1.1.6/examples/SharedMemory/gpu_sim/GpuPhysicsServerExample.h
@@ -0,0 +1,9 @@
+#ifndef GPU_PHYSICS_SERVER_EXAMPLE_H
+#define GPU_PHYSICS_SERVER_EXAMPLE_H
+
+
+class CommonExampleInterface*    GpuPhysicsServerCreateFunc(struct CommonExampleOptions& options);
+
+#endif //GPU_PHYSICS_SERVER_EXAMPLE_H
+
+
