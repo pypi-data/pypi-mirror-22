d04bd49b226682b67151a7c443a725b555d16f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:57 +0000

    Add package wikitables/0.4.1/wikitables-0.4.1-py2.py3-none-any.whl

---
+++ packages/wikitables/wikitables-0.4.1-py2.py3-none-any.whl/wikitables/util.py
@@ -0,0 +1,40 @@
+import sys
+import json
+import logging
+
+log = logging.getLogger('wikitables')
+
+def ftag(*args):
+    return lambda node: node.tag in args
+
+def guess_type(s):
+    """ attempt to convert string value into numeric type """
+    sc = s.replace(',', '') # remove comma from potential numbers
+
+    try:
+        return int(sc)
+    except ValueError:
+        pass
+
+    try:
+        return float(sc)
+    except ValueError:
+        pass
+
+    return s
+
+def ustr(s):
+    if sys.version_info < (3, 0):
+        #py2
+        try:
+            return unicode(s).encode('utf-8')
+        except UnicodeDecodeError:
+            return str(s)
+    else:
+        return str(s)
+
+class TableJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, '__json__'):
+            return obj.__json__()
+        return json.JSONEncoder.default(self, obj)
