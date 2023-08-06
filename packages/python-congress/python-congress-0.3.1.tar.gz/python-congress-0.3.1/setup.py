724db21c5176888581dca4ac959bb1d0dcbb4245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:04 +0000

    Add package python-congress/0.3.1/python-congress-0.3.1.tar.gz

---
+++ packages/python-congress/python-congress-0.3.1.tar.gz/python-congress-0.3.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.md') as f:
+    README = f.read()
+
+VERSION = "0.3.1"
+
+setup(
+    name = "python-congress",
+    version = VERSION,
+    description = "A Python client for the ProPublica Congress API",
+    long_description = README,
+    author = "Chris Amico",
+    author_email = "eyeseast@gmail.com",
+    py_modules = ['congress'],
+    install_requires = ['httplib2', 'six'],
+    platforms= ['any'],
+    classifiers = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    test_suite='test',
+)
+
