3af83203e2d76702521c0ad3380dc8d63286b8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:42 +0000

    Add package ameliabot/1.3.0/ameliabot-1.3.0.tar.gz

---
+++ packages/ameliabot/ameliabot-1.3.0.tar.gz/ameliabot-1.3.0/ameliabot/plugins/booklist.py
@@ -0,0 +1,43 @@
+""" 
+Overview
+========
+
+Used to list files that are being shared.
+
+Commands
+========
+
+Command: @dcc-list
+Description: List all files that exist in a given folder that was shared.
+
+"""
+
+import libpad
+from untwisted.network import xmap
+from untwisted.plugins.irc import send_msg
+from ameliabot.cmd import command
+import os
+
+class BookList(object):
+    def __init__(self, server, folder):
+        """
+        """
+
+        self.folder = folder
+        xmap(server, 'CMSG', self.list_folder)
+    
+    @command('@dcc-list')
+    def list_folder(self, server, nick, user, host, target, msg):
+        """
+        """
+        content = '\n'.join(os.listdir(self.folder))
+    
+        url, _ = libpad.sandbox(content, '') 
+        send_msg(server, target, url)
+        
+
+
+
+
+
+
---
+++ packages/ameliabot/ameliabot-1.3.0.tar.gz/ameliabot-1.3.0/ameliabot/tools.py
@@ -0,0 +1,19 @@
+from untwisted.plugins.irc import send_msg
+from untwisted.timer import Sched, CancelCall
+from re import escape
+
+def consume(seq):
+    try:
+        seq.next()
+    except StopIteration:
+        raise CancelCall
+
+def send_lines(server, target, msg, delay=0.7):
+    def lazy():
+        for ind in msg.splitlines():
+            send_msg(server, target, ind)
+            yield    
+    Sched(delay, consume, lazy())
+
+
+
