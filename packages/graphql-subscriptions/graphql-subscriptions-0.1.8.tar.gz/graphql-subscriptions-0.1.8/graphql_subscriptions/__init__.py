d8efe4be27127a19a71627337415d2e684b56a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:35 +0000

    Add package graphql-subscriptions/0.1.8/graphql-subscriptions-0.1.8.tar.gz

---
+++ packages/graphql-subscriptions/graphql-subscriptions-0.1.8.tar.gz/graphql-subscriptions-0.1.8/graphql_subscriptions/__init__.py
@@ -0,0 +1,5 @@
+from subscription_manager import RedisPubsub, SubscriptionManager
+from subscription_transport_ws import SubscriptionServer
+
+__all__ = ['RedisPubsub', 'SubscriptionManager', 'SubscriptionServer']
+
---
+++ packages/graphql-subscriptions/graphql-subscriptions-0.1.8.tar.gz/graphql-subscriptions-0.1.8/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = open('README.md').read()
+
+tests_dep = [
+    'pytest', 'pytest-mock', 'fakeredis', 'graphene', 'subprocess32',
+    'flask', 'flask-graphql', 'flask-sockets', 'multiprocess', 'requests'
+]
+
+setup(
+    name='graphql-subscriptions',
+    version='0.1.8',
+    author='Heath Ballard',
+    author_email='heath.ballard@gmail.com',
+    description=('A port of apollo-graphql subscriptions for python, using\
+                 gevent websockets, promises, and redis'),
+    license='MIT',
+    keywords='graphql websockets concurrent subscriptions',
+    url='https://github.com/hballard/graphql-python-subscriptions',
+    packages=find_packages(exclude=['tests']),
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 3 - Alpha', 'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License'
+    ],
+    install_requires=[
+        'gevent-websocket', 'redis', 'graphql-core', 'promise<=1.0.1'
+    ],
+    test_suite='pytest',
+    tests_require=tests_dep,
+    extras_require={
+        'test': tests_dep
+    },
+    include_package_data=True)
