5be4af837f62b5d48182a710cd6be934bd26ae06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:22 +0000

    Add package strictyaml/0.5.7/strictyaml-0.5.7.tar.gz

---
+++ packages/strictyaml/strictyaml-0.5.7.tar.gz/strictyaml-0.5.7/strictyaml/__init__.py
@@ -0,0 +1,44 @@
+# The all important loader
+from strictyaml.parser import load
+
+# YAML object
+from strictyaml.representation import YAML
+
+# Validators
+from strictyaml.validators import Optional
+from strictyaml.validators import Validator
+from strictyaml.validators import OrValidator
+from strictyaml.validators import Any
+from strictyaml.validators import Scalar
+from strictyaml.validators import Enum
+from strictyaml.validators import Str
+from strictyaml.validators import Int
+from strictyaml.validators import Bool
+from strictyaml.validators import Float
+from strictyaml.validators import Decimal
+from strictyaml.validators import Map
+from strictyaml.validators import MapPattern
+from strictyaml.validators import Seq
+from strictyaml.validators import UniqueSeq
+from strictyaml.validators import FixedSeq
+from strictyaml.validators import Datetime
+from strictyaml.validators import EmptyNone
+from strictyaml.validators import EmptyDict
+from strictyaml.validators import EmptyList
+from strictyaml.validators import CommaSeparated
+
+# Base exception from ruamel.yaml (all exceptions inherit from this)
+from ruamel.yaml import YAMLError
+
+# Exceptions
+from strictyaml.exceptions import StrictYAMLError
+from strictyaml.exceptions import YAMLValidationError
+
+# Disallowed token exceptions
+from strictyaml.exceptions import DisallowedToken
+
+from strictyaml.exceptions import TagTokenDisallowed
+from strictyaml.exceptions import FlowMappingDisallowed
+from strictyaml.exceptions import AnchorTokenDisallowed
+from strictyaml.exceptions import DuplicateKeysDisallowed
+from strictyaml import exceptions
