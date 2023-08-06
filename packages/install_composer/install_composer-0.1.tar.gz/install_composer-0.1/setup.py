4c45fea068dfc7fb8c4f8802772ccc4c2ae1652c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:18 +0000

    Add package install_composer/0.1/install_composer-0.1.tar.gz

---
+++ packages/install_composer/install_composer-0.1.tar.gz/install_composer-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='install_composer',
+      version='0.1',
+      description='install composer',
+      author='haradashinya',
+      author_email='haradashinya@gmail.com',
+      license='MIT',
+      url='https://github.com/haradashinya/install_composer',
+      packages=['install_composer'],
+      zip_safe=False)
