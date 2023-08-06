10c3b28e270923c5ee6d1eb6fa2344409b07d090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:29 +0000

    Add package pyley/0.2.1/pyley-0.2.1.tar.gz

---
+++ packages/pyley/pyley-0.2.1.tar.gz/pyley-0.2.1/setup.py
@@ -0,0 +1,16 @@
+__author__ = 'ziyasal'
+
+from setuptools import setup
+
+setup(
+    name='pyley',
+    version='0.2.1',
+    author='Ziya SARIKAYA',
+    author_email='sarikayaziya@gmail.com',
+    packages=['pyley'],
+    url='https://github.com/ziyasal/pyley',
+    license='LICENSE',
+    description='Python client for an open-source graph database Cayley',
+    install_requires=['requests'],
+    include_package_data=True
+)
