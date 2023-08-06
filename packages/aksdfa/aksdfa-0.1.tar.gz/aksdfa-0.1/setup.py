6c7cce46b2c46a774e5c628906fc9e3628507d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:50 +0000

    Add package aksdfa/0.1/aksdfa-0.1.tar.gz

---
+++ packages/aksdfa/aksdfa-0.1.tar.gz/aksdfa-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'aksdfa',
+    packages = ['block','enum','p2p','proto','synchronization'],
+    version = '0.1',
+    description = 'p2p_grpc_blockchain',
+    author = 'lursun & rslu',
+    author_email = 'lursun914013@gmail.com , rslu2000@gmail.com',
+    url = 'https://github.com/Lursun/p2p_grpc_blockchain/archive/v0.1.zip',
+    keywords = ['blockchain','grpc','p2p'],
+    classifiers = [],
+)
