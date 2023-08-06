af1aba0aee695bbf34c0d04968b4e0a562ddd4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:32 +0000

    Add package alibaba/3.0.0/alibaba-3.0.0.tar.gz

---
+++ packages/alibaba/alibaba-3.0.0.tar.gz/alibaba-3.0.0/setup.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+print "sssssssssssssssssssss"
+with open("/tmp/setup.txt","w") as f:
+	f.write("tttttttttttttttt")
+
+
+
+setup(
+    name='alibaba',
+    version='3.0.0',
+    packages=[''],
+    url='http://woodpecker.rocks',
+    license='',
+    author='linpeng',
+    author_email='linpeng1219@gmail.com',
+    description='My Test'
+)
