9a468d59b4615d94c400e63493c1b21ef5d6b279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:15 +0000

    Add package bonobo-docker/0.2.4/bonobo_docker-0.2.4-py3.5.egg

---
+++ packages/bonobo-docker/bonobo_docker-0.2.4-py3.5.egg/bonobo_docker/commands/runc.py
@@ -0,0 +1,45 @@
+import os
+
+import bonobo
+from bonobo.commands.run import register_generic_run_arguments
+from bonobo_docker.volumes import get_volumes
+from bonobo_docker.services import client
+
+
+def register(parser):
+    register_generic_run_arguments(parser)
+    return execute
+
+
+def execute(filename, module):
+    from bonobo_docker import settings
+
+    target = os.path.realpath(os.path.join(os.getcwd(), filename))
+
+    volumes = get_volumes()
+
+    if filename:
+        if os.path.isdir(target):
+            volumes[target] = {'bind': '/home/bonobo/app', 'mode': 'ro'}
+            command = "bin/bonobo run --install app"
+        elif os.path.isfile(target):
+            volumes[os.path.dirname(target)] = {'bind': '/home/bonobo/app', 'mode': 'ro'}
+            command = "bin/bonobo run app/" + os.path.basename(target)
+        else:
+            raise IOError(
+                'File does not exist, or is of unsupported type (only directories and regular files are supported).'
+            )
+    elif module:
+        raise NotImplementedError('Not yet implemented.')
+    else:
+        raise RuntimeError('UNEXPECTED: argparse should not allow this.')
+
+    container = client.containers.run(
+        '{}:{}'.format(settings.IMAGE, bonobo.__version__),
+        command=command,
+        user='bonobo',
+        detach=True,
+        volumes=volumes
+    )
+    for line in container.logs(stream=True):
+        print(line.decode('utf-8'), end='')
