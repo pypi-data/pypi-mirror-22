74161d1c8eb53a787a60453939177a16e84f0f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:47 +0000

    Add package gcdt/0.1.293/gcdt-0.1.293.tar.gz

---
+++ packages/gcdt/gcdt-0.1.293.tar.gz/gcdt-0.1.293/gcdt/gcdt_defaults.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+"""This file contains configuration for gcdt tools so we do not need
+hardcoded values.
+"""
+
+# basic structure:
+'''
+{
+    'kumo': {},
+    'tenkai': {},
+    'ramuda': {},
+    'yugen': {},
+    'plugins': {
+        '<plugin_name>': {}
+    }
+}
+'''
+
+
+DEFAULT_CONFIG = {
+    'ramuda': {
+        'settings_file': 'settings.json',
+        'runtime': ['python2.7', 'nodejs4.3']
+    }
+}
+
+
+# note this config is used in the config_reader to "overlay" the
+# gcdt_defaults of gcdt.
+CONFIG_READER_CONFIG = {
+    'lookups': ['secret', 'ssl', 'stack', 'baseami'],
+    'plugins': {
+        'gcdt_datadog_integration': {
+            'datadog_api_key': 'lookup:secret:datadog.api_key'
+        },
+        'gcdt_slack_integration': {
+            'slack_webhook': 'lookup:secret:slack.webhook:CONTINUE_IF_NOT_FOUND'
+        },
+        'gcdt_lookups': {
+            'ami_accountid': '569909643510'
+        }
+    }
+}
