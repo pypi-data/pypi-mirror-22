eba6aae60272f91992eaa35dd9f57ed858456d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:23 +0000

    Add package t3cl/0.0.1/t3cl-0.0.1.tar.gz

---
+++ packages/t3cl/t3cl-0.0.1.tar.gz/t3cl-0.0.1/t3cl/__main__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#!coding=utf-8
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import sys
+
+def main():
+
+    print('t3cl :: Work in progress')
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
