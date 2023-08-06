6f0318183fdc9b523acb8a929319cb232b78f15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:13 +0000

    Add package iamport_idist/1.0.0/iamport_idist-2.0.0.zip

---
+++ packages/iamport_idist/iamport_idist-2.0.0.zip/iamport_idist-2.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+ 
+setup (
+	name = 'iamport_idist',
+	version = '2.0.0',
+	py_modules = ['iamport_idist'],
+	author = 'kimjuchan',
+	author_email = 'ceo@idist.ai',
+	url = 'http://grammer.kr',
+	description = 'iamport for IDIST',
+	)
