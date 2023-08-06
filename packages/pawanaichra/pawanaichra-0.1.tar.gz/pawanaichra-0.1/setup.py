5434bed0d7b35fb4cff342f47c9729749f3fdb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:55 +0000

    Add package pawanaichra/0.1/pawanaichra-0.1.tar.gz

---
+++ packages/pawanaichra/pawanaichra-0.1.tar.gz/pawanaichra-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pawanaichra',
+  packages = ['pawanaichra'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Pawan Aichra',
+  author_email = 'aichrapawan@gmail.com',
+  url = 'https://github.com/pawanaichra/pip-test', # use the URL to the github repo
+  download_url = 'https://github.com/pawanaichra/pip-test/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['pawan', 'aichra', 'kgp'], # arbitrary keywords
+  classifiers = [],
+)
