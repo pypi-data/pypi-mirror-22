5826a9a44c6ef4d38d8f9eb253eebf4e5154c603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:35 +0000

    Add package zaifbot/0.0.4/zaifbot-0.0.4.tar.gz

---
+++ packages/zaifbot/zaifbot-0.0.4.tar.gz/zaifbot-0.0.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zaifbot',
+    version='0.0.4',
+    description='Zaif Bot Library',
+    long_description='https://pypi.python.org/pypi/zaifbot',
+    url='https://github.com/Akira-Taniguchi/zaifbot',
+    author='AkiraTaniguchi Monji',
+    author_email='a.taniguchi@techbureau.jp ttskmnj@gmail.com',
+    packages=find_packages(),
+    license='MIT',
+    keywords='zaif bit coin btc xem mona jpy virtual currency block chain bot',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+    ],
+    install_requires=[
+        'zaifapi==1.5.2',
+        'numpy==1.12.0',
+        'pandas==0.20.1',
+        'reportlab==3.4.0',
+        'SQLAlchemy==1.1.5',
+        'websocket-client==0.40.0',
+        'slackclient==1.0.5',
+        'plotly==2.0.8',
+        'pytz==2017.2'
+    ],
+    entry_points="""\
+      [console_scripts]
+      update_assignee_id = zaifbot:main
+      init_database = zaifbot.modules.dao:init_database
+      """,
+    include_package_data=True
+)
