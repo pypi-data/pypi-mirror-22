b8923e6390b7349bd83bb320cea47539fb828829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:45 +0000

    Add package nester_ll/1.3.0/nester_ll-1.3.0.tar.gz

---
+++ packages/nester_ll/nester_ll-1.3.0.tar.gz/nester_ll-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name         = 'nester_ll',
+    version      = '1.3.0',
+    py_modules   = ['nester_ll'],
+    author       = 'lpython',
+    author_email = 'kll_1115@qq.com',
+    url          = 'http://headfirstlabs.com',
+    description  = 'A simple printer of nested list',
+)
