a3c624ebc80e3373c84567fe4c5c92d7512daaf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:28 +0000

    Add package wordsmiths/0.1.1/wordsmiths-0.1.1.tar.gz

---
+++ packages/wordsmiths/wordsmiths-0.1.1.tar.gz/wordsmiths-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wordsmiths',
+      description='Operational Transformation implementation in Python (WIP)',
+      version='0.1.1',
+      url='https://github.com/kingdion/wordsmiths',
+      author='D. Misic',
+      author_email='dion@gmail.com',
+      packages=['wordsmiths'],
+      license='MIT',
+)
