e2fa3c154e3073229ffe5acd230d1cfce8273b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:52 +0000

    Add package dojo/0.0.32/dojo-0.0.32.tar.gz

---
+++ packages/dojo/dojo-0.0.32.tar.gz/dojo-0.0.32/dojo/vanilla/stores/local_file.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+import os
+
+from dojo.transforms import ConvertTo
+
+from ..storage import Storage
+
+
+class VanillaLocalFileStore(Storage):
+
+    def read(self):
+        with open(self.path, 'r') as f:
+            return f.read().split('\n')
+
+    def write(self, path, rows, format='json'):
+        rows = [ConvertTo(format).process(row) for row in rows]
+        output_path = '%s-00000-of-00001' % (path, )
+        if 'prefix' in self.config:
+            output_path = os.path.join(self.config['prefix'], output_path)
+        output_dir = os.path.dirname(output_path)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        with open(output_path, 'w') as f:
+            f.write('\n'.join(rows).encode('utf-8'))
