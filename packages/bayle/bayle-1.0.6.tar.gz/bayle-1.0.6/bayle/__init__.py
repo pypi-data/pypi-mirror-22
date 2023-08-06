e478dfae2d279393c12e487200084eaac2f91cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:01 +0000

    Add package bayle/1.0.6/bayle-1.0.6.tar.gz

---
+++ packages/bayle/bayle-1.0.6.tar.gz/bayle-1.0.6/bayle/__init__.py
@@ -0,0 +1,1 @@
+from bayle import isrc
---
+++ packages/bayle/bayle-1.0.6.tar.gz/bayle-1.0.6/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'bayle',
+  packages = ['bayle'],
+  version = '1.0.6',
+  description = 'A Python package that provides reproducible code and data for my scientific research',
+  author = 'Yann Bayle',
+  author_email = 'bayle.yann@live.fr',
+  url = 'https://github.com/ybayle/research',
+  download_url = 'https://github.com/ybayle/research/archive/v1.0.6.tar.gz',
+  keywords = ['satin', 'MIR', 'dataset', 'research', 'reproducibility'],
+  classifiers = [],
+  include_package_data = True,
+  license = 'LICENSE.txt',
+  install_requires=[
+    'requests>=2.9.1',
+  ],
+  entry_points={
+    'console_scripts': [
+        'bayle=bayle.satin:main',
+    ],
+  },
+)
