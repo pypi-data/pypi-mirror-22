b9e1b05dca07bc7c985b91b2ad5a031be44f5113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:26 +0000

    Add package Nesterd/1.0.2/Nesterd-1.0.2.zip

---
+++ packages/Nesterd/Nesterd-1.0.2.zip/Nesterd-1.0.2/Nesterd.py
@@ -0,0 +1,13 @@
+"""This is a function that defines the printing process of a list"""
+"""A list is passed as an argument"""
+def print_lol(element,indent=0):
+    """The For loop is used to iterate"""
+    for element1 in element:
+        """Checking if it contains any nested lists"""
+        if isinstance(element1,list):
+            """Printing nested lists if present"""
+            print_lol(element1,indent+1)
+        else:
+            for i in range(indent):
+                print("\t",end='') 
+            print(element1)
