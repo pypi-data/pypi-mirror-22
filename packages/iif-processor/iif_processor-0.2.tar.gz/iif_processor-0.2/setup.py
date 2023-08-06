eec2cd4573f0f16824490cd8778e1ba9e6ef6ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:02 +0000

    Add package iif-processor/0.2/iif_processor-0.2.tar.gz

---
+++ packages/iif-processor/iif_processor-0.2.tar.gz/iif_processor-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='iif_processor',
+      version='0.2',
+      description='Processes all images in a directory for IIF server',
+      url='http://github.com/j0hnds/diva.js',
+      author='Dave Sieh',
+      author_email='dj0nve@gmail.com',
+      license='MIT',
+      packages=['iif_processor'],
+      scripts=['bin/iif_process.py'],
+      zip_safe=False)
