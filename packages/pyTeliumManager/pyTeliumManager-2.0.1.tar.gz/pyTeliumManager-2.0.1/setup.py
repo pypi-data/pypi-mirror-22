a3c0870cf6ec8c59751d7bec9d4e60bd22f1a2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:06 +0000

    Add package pyTeliumManager/2.0.1/pyTeliumManager-2.0.1.tar.gz

---
+++ packages/pyTeliumManager/pyTeliumManager-2.0.1.tar.gz/pyTeliumManager-2.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='pyTeliumManager',
+    version='2.0.1',
+    author='Ahmed TAHRI, @Ousret',
+    author_email='ahmed@tahri.space',
+    description=('A cross-platform point of sales payment manager tool with Telium Manager '
+                 'Support every device with Telium Manager like Ingenico terminals.'),
+    license='MIT',
+    packages=['telium'],
+    test_suite='tests',
+    url='https://github.com/Ousret/pyTeliumManager',
+    download_url='https://github.com/Ousret/pyTeliumManager/tarball/2.0.1',
+    install_requires=['pyserial', 'pycountry', 'unidecode'],
+    keywords=['ingenico', 'telium manager', 'telium', 'payment', 'credit card', 'debit card', 'visa', 'mastercard',
+              'merchant', 'pos'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
