33e320b472f1b03676976697da82daedffa1376c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:52 +0000

    Add package betterplotlib/1.2.1/betterplotlib-1.2.1-py2.py3-none-any.whl

---
+++ packages/betterplotlib/betterplotlib-1.2.1-py2.py3-none-any.whl/betterplotlib/colors.py
@@ -0,0 +1,24 @@
+from palettable.tableau import Tableau_10
+
+almost_black = '#262626'
+light_gray = '#E5E5E5'
+light_grey = light_gray
+steel_blue = '#3F5D7D'
+# I saw the national park road signs and loved the color, so I found it. Here
+# is the source if you want (PDF):
+# http://www.pc.gc.ca/eng/docs/bib-lib/~/media/docs/bib-lib/pdfs/Exterior_Signage.ashx
+# Search for Parks Canada Heritage Green, and you will see that it is the
+# color on the signs, and that they give the pantone shade. I found a tool
+# online that can convert that to the hex color.
+parks_canada_heritage_green = '#284734'
+
+color_cycle = [Tableau_10.hex_colors[3],
+               Tableau_10.hex_colors[0],
+               Tableau_10.hex_colors[2],
+               Tableau_10.hex_colors[4],
+               Tableau_10.hex_colors[1],
+               Tableau_10.hex_colors[6],
+               Tableau_10.hex_colors[7],
+               Tableau_10.hex_colors[9],
+               Tableau_10.hex_colors[8],
+               Tableau_10.hex_colors[5]]
