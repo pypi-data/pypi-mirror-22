c8cb2f9bd2f7e4cafa580b70e58a100dcc0d6071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:59 +0000

    Add package keras-resnet/0.0.2/keras-resnet-0.0.2.tar.gz

---
+++ packages/keras-resnet/keras-resnet-0.0.2.tar.gz/keras-resnet-0.0.2/keras_resnet/__init__.py
@@ -0,0 +1,15 @@
+from .block import (
+    basic,
+    bottleneck,
+    shortcut
+)
+
+from .models import (
+    ResNet,
+    ResNet18,
+    ResNet34,
+    ResNet50,
+    ResNet101,
+    ResNet152,
+    ResNet200
+)
