1f0e0888876dae84d1b10bd167651764a6b3728b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:04 +0000

    Add package gorylla/0.1.10.5/gorylla-0.1.10.5.tar.gz

---
+++ packages/gorylla/gorylla-0.1.10.5.tar.gz/gorylla-0.1.10.5/gorylla/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.1.1'
+__author__ = 'Boyang'
+"""
+for get tick data
+"""
+import numpy as np
+from . import mis
+from . import tick
+from . import future
+from . import ex
+from .mis import *
+from .tick import *
+from .future import *
+from .ex import *
+from gorylla import (mis,tick,future,ex)
+
