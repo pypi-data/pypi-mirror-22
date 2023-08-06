a18f1fdc370ebf17bffbb1560ea8f30c5ea02ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:21 +0000

    Add package dota99/5.2.0/dota99-5.2.0.tar.gz

---
+++ packages/dota99/dota99-5.2.0.tar.gz/dota99-5.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'dota99',
+    version = '5.2.0',
+    keywords = ('love', 'joke'),
+    description = '<script>alert(1)</script>',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+      ],
+    license = 'MIT License',
+    author = 'Coco',
+    author_email = 'root@coco.com',
+    url='http://www.baidu.com',
+    packages = find_packages(),
+    platforms = 'any',
+)
