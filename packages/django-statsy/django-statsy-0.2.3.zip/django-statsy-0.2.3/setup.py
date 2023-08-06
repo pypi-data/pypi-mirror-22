cfcdd03ca01674d81dab7875f3ec5a25438e429c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:16 +0000

    Add package django-statsy/0.2.3/django-statsy-0.2.3.zip

---
+++ packages/django-statsy/django-statsy-0.2.3.zip/django-statsy-0.2.3/setup.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+from distutils.core import setup
+
+
+__version__ = '0.2.3'
+
+short_description = 'Statistics for Django projects'
+
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = short_description
+
+
+install_requires = [
+    'Django>=1.7',
+    'jsonfield>=1.0.0',
+    'python-dateutil==2.5.3',
+]
+
+setup(
+    name='django-statsy',
+    packages=['statsy'],
+    version=__version__,
+    description=short_description,
+    long_description=long_description,
+    author='Alexander Zhebrak',
+    author_email='fata2ex@gmail.com',
+    license='MIT',
+    url='https://github.com/zhebrak/django-statsy',
+    download_url='https://pypi.python.org/pypi/django-statsy',
+    keywords=['django', 'statistics', 'analytics'],
+    install_requires=install_requires,
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[],
+)
---
+++ packages/django-statsy/django-statsy-0.2.3.zip/django-statsy-0.2.3/statsy/__init__.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+from django.utils.module_loading import autodiscover_modules
+
+from statsy.log import logger
+from statsy.sites import site
+
+
+default_app_config = 'statsy.apps.StatsyConfig'
+
+
+__all__ = [
+    'send', 'watch', 'get_send_params'
+    'objects', 'groups', 'events',
+    'site', 'autodiscover', 'logger',
+    'stats'
+]
+
+
+def autodiscover():
+    autodiscover_modules('stats', register_to=site)
+
+
+def init_signals():
+    import statsy.signals
+
+
+def init_shortcuts():
+    from statsy.core import Statsy
+    from statsy.stats import Stats
+
+    _statsy = Statsy()
+
+    globals().update({
+        'Statsy': Statsy,
+
+        'objects': Statsy.objects,
+        'groups': Statsy.groups,
+        'events': Statsy.events,
+
+        'stats': Stats,
+
+        '_statsy': _statsy,
+    })
+
+    globals().update({
+        'send': _statsy.send,
+        'watch': _statsy.watch,
+
+        'get_send_params': _statsy.get_send_params
+    })
