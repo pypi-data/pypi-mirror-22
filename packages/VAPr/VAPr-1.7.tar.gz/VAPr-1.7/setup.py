5fab59de9b25813370e9dfdebe3a85d959ec59e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:40 +0000

    Add package VAPr/1.7/VAPr-1.7.tar.gz

---
+++ packages/VAPr/VAPr-1.7.tar.gz/VAPr-1.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='VAPr',
+      version='1.7',
+      description='Package for NoSQL variant data storage, annotation and prioritization.',
+      url='https://github.com/ucsd-ccbb/VAPr',
+      author='Carlo Mazzaferro',
+      author_email='cmazzafe@ucsd.edu',
+      install_requires=['pymongo', 'myvariant', 'pyvcf', 'watchdog', 'pandas'],
+      license='MIT',
+      packages=['VAPr'],
+      zip_safe=False)
