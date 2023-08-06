c71815f8bf6d9f5a35bd44b914248678e3417734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:07 +0000

    Add package ljudge/0.2/ljudge-0.2.tar.gz

---
+++ packages/ljudge/ljudge-0.2.tar.gz/ljudge-0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='ljudge',
+    packages=['ljudge'],
+    version='0.2',
+    description='python bindings for ljudge',
+    author='shengrang',
+    author_email='shengrang0@gmail.com',
+    url='https://github.com/NJUST-FishTeam/ljudge-py',
+)
