098b3a889be3851ccd087abbedb34d55f580d58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:03 +0000

    Add package inv_content_list/1.3/inv_content_list-1.3.tar.gz

---
+++ packages/inv_content_list/inv_content_list-1.3.tar.gz/inv_content_list-1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'inv_content_list',
+  packages = ['inv_content_list'], # this must be the same as the name above
+  version = '1.3',
+  description = 'A random test lib11',
+  author = 'Eric Chu',
+  author_email = 'eric.chu@investopedia.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
