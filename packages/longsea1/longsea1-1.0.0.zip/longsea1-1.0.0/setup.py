bc53cfdae5902f8fbc5306b8dd5a2e39f61da60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:39 +0000

    Add package longsea1/1.0.0/longsea1-1.0.0.zip

---
+++ packages/longsea1/longsea1-1.0.0.zip/longsea1-1.0.0/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+
+setup(
+        name        =  'longsea1',
+        version     =  '1.0.0',
+        py_modules  =  ['longsea1'],
+        author      =  'longsea',
+        author_email=  'hfpython@headfirstlabs.com',
+        url         =  'http://www.headfirstlabs.com',
+        description =  'A simple printer of nested lists',
+     )
