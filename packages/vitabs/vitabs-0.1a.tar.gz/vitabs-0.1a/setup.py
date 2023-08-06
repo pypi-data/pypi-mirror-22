3728bbd011d8c30476d7c91b0422bf64472dbc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:18 +0000

    Add package vitabs/0.1a/vitabs-0.1a.tar.gz

---
+++ packages/vitabs/vitabs-0.1a.tar.gz/vitabs-0.1a/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='vitabs',
+    version='0.1a',
+    author='Pawel Stiasny',
+    author_email='pawelstiasny@gmail.com',
+    packages=['vitabs'],
+    scripts=['bin/vitabs'],
+    url='http://github.com/pstiasny/VITABS',
+    license='GNU General Public License',
+    description='Vi-inspired guitar tab editor',
+    long_description=open('README').read(),
+)
