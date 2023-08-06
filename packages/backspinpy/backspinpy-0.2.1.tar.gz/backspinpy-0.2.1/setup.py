1b4c8f24d061cd8cfacb00a8402f927e81f2341e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:11 +0000

    Add package backspinpy/0.2.1/backspinpy-0.2.1.tar.gz

---
+++ packages/backspinpy/backspinpy-0.2.1.tar.gz/backspinpy-0.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.2.1"
+
+setup(
+    name="backspinpy",
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        'future',
+        'numpy',
+        'scipy',
+        'scikit-learn'
+    ],
+    # command
+    scripts=['backspinpy/backspin'],
+    # metadata
+    author="Linnarsson Lab",
+    author_email="gioelelamanno@gmail.com",
+    description="backSPIN clustering algorythm",
+    license="MIT",
+    url="https://github.com/linnarsson-lab/BackSPIN",
+)
