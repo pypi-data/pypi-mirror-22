a273fa7383c1160136885d3bdfada3c0cedb5947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:59 +0000

    Add package Webshot/0.6/Webshot-0.6.zip

---
+++ packages/Webshot/Webshot-0.6.zip/Webshot-0.6/README.txt
@@ -0,0 +1,8 @@
+A python wrapper for the NodeJS module Pagres CLI. Built and tested in Python 2.7.
+Requires the NodeJS package installed: https://github.com/sindresorhus/pageres-cli
+
+
+I had tried a few different python screenshot modules and could not find one that 'just worked'. 
+
+
+Install: pip install Webshot
---
+++ packages/Webshot/Webshot-0.6.zip/Webshot-0.6/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name='Webshot',
+    version='0.6',
+    packages=['webshot',],
+    license='MIT',
+    description='Python Wrapper for NodeJS Pagres CLI Website Schreenshot Utility',
+    long_description=open('README.txt').read(),
+    url="https://github.com/rpunnett/Webshot"
+)
