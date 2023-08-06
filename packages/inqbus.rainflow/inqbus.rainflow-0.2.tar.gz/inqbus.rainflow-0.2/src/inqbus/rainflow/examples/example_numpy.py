a34de39add4679a270a30cb4c524ec2ae7170207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:12 +0000

    Add package inqbus.rainflow/0.2/inqbus.rainflow-0.2.tar.gz

---
+++ packages/inqbus.rainflow/inqbus.rainflow-0.2.tar.gz/inqbus.rainflow-0.2/src/inqbus/rainflow/examples/example_numpy.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from inqbus.rainflow.run_rainflow import rainflow_on_numpy_array, \
+    classification_on_numpy_array
+
+# Example for numpy array
+
+# rainflow_on_numpy_array:
+# run base algorithm, return pairs and counted pairs
+
+# classification_on_numpy_array:
+# classify results from rainflow_on_numpy_array
+
+# example data
+N = 1000000
+
+data = np.random.random_integers(1, 1000, N)
+print(data)
+
+# main algorithm
+res, res_counted = rainflow_on_numpy_array(data)
+print(res, res_counted)
+
+# add some classifications afterwards
+res_32, res_counted_32 = classification_on_numpy_array(res, bin_count=32)
+print(res_32, res_counted_32)
