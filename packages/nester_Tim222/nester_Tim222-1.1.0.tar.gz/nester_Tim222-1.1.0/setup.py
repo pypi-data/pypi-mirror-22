5a425af0b1e17343cdc9319348a57fce43c10cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:45 +0000

    Add package nester_Tim222/1.1.0/nester_Tim222-1.1.0.tar.gz

---
+++ packages/nester_Tim222/nester_Tim222-1.1.0.tar.gz/nester_Tim222-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "nester_Tim222",    
+    version ='1.1.0',
+    py_modules = ['nester_Tim222'],
+    author = 'Tim',
+    author_email='m13817799245@163.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
