f0a105493266107aa3c19905ec4794503c111347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:47 +0000

    Add package ifrs/0.0.0.24/ifrs-0.0.0.24-py3-none-any.whl

---
+++ packages/ifrs/ifrs-0.0.0.24-py3-none-any.whl/ifrs/ifrs.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  8 05:03:19 2017
+
+@author: Young Ju Kim
+"""
+
+from ifrs.taxonomy import taxonomier
+
+
---
+++ packages/ifrs/ifrs-0.0.0.24-py3-none-any.whl/ifrs/taxonomy/taxonomier.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 04:14:19 2017
+
+@author: Young Ju Kim
+"""
+
+
+import zipfile
+import io
+import os
+from os.path import dirname, abspath
+
+
+class TaxonomySerializer:
+    
+    def __init__(self, locale):
+        
+        ifrsLabelFullStr = ""
+        filePath = dirname(abspath(__file__))
+        zipDirPath = filePath + r'/{lang_locale}/zip/'.format(lang_locale=locale)
+        zipList = os.listdir(zipDirPath)
+        
+        for file in zipList:
+            
+            zipFilePath = zipDirPath + file
+            with zipfile.ZipFile(zipFilePath, 'r') as archive:
+
+                fileList = archive.namelist()
+                ifrsLableFileList = [item for item in fileList if ('label' in item) &
+                                                      ('lab_' in item) &
+                                                      (item.endswith(r'.xml'))]
+                
+                for name in ifrsLableFileList:
+                    
+                    ifrsLabelFile = archive.open(name)
+                    ifrsLabelStr = io.TextIOWrapper(ifrsLabelFile).read()
+                    ifrsLabelFullStr += ifrsLabelStr
+                
+        self.parsed = ifrsLabelFullStr
+    
+    
+    def dump(self):
+        
+        with open('label.dump', 'w') as new:
+            new.write(ifrsLabelFullStr)
+        return ifrsLabelFullStr
+
+
+
+
