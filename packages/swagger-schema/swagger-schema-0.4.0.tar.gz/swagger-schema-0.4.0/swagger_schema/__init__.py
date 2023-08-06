5db890d2964e60ddb4291bfda1c8cd982a89ab74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:33 +0000

    Add package swagger-schema/0.4.0/swagger-schema-0.4.0.tar.gz

---
+++ packages/swagger-schema/swagger-schema-0.4.0.tar.gz/swagger-schema-0.4.0/swagger_schema/__init__.py
@@ -0,0 +1,15 @@
+from .external_documentation import ExternalDocumentation
+from .contact import Contact
+from .info import Info
+from .items import Items
+from .license import License
+from .schema import Schema
+from .swagger import Swagger
+from .operation import Operation
+from .path_item import PathItem
+from .parameter import (
+    QueryParameter, HeaderParameter,
+    FormDataParameter, PathParameter,
+    BodyParameter, Parameters
+)
+from .response import Response
