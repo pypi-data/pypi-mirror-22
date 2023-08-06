934c1834d74e907db33474590532676818e5e93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:41 +0000

    Add package smarturls/0.1.7/smarturls-0.1.7.tar.bz2

---
+++ packages/smarturls/smarturls-0.1.7.tar.bz2/smarturls-0.1.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="smarturls",
+    description="a url construction helper for django",
+    long_description=long_description,
+
+    version="0.1.7",
+    author='Amit Upadhyay',
+    author_email="upadhyay@gmail.com",
+
+    url='http://amitu.com/smarturls/',
+    license='BSD',
+
+    py_modules=["smarturls"],
+
+    zip_safe=True,
+
+    install_requires=["Django>=1.5"],
+)
