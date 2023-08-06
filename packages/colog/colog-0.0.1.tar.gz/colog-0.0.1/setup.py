1ecfb39d73e07b23e78923c20076b64ae83c10c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:07 +0000

    Add package colog/0.0.1/colog-0.0.1.tar.gz

---
+++ packages/colog/colog-0.0.1.tar.gz/colog-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'colog',
+    version = '0.0.1',
+    description = 'Color logging library',
+    license = 'MIT',
+    author = 'encry1024',
+    author_email = 'pgm3rdlinuxor1000@gmail.com',
+    packages = find_packages(),
+    install_requires = [],
+)
