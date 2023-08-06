e43a3f1c86d7da1eae65fc3d0e20b256ed7abdf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:15 +0000

    Add package pitargparse/0.1.1/pitargparse-0.1.1.tar.gz

---
+++ packages/pitargparse/pitargparse-0.1.1.tar.gz/pitargparse-0.1.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# vi: et sw=2 fileencoding=utf-8
+#============================================================================
+# pitargparse
+# Copyright (c) 2017 Pispalan Insinööritoimisto Oy (http://www.pispalanit.fi)
+#
+# Redistributions of files shall retain the above copyright notice.
+#
+# @created     26.05.2017
+# @author      Harry Karvonen <harry.karvonen@pispalanit.fi>
+# @copyright   Copyright (c) Pispalan Insinööritoimisto Oy
+# @license     MIT Licence
+#============================================================================
+
+from setuptools import setup, find_packages
+
+setup(name='pitargparse',
+    version='0.1.1',
+    description='extensions for python argparse',
+    url='https://git.pispalanit.fi/pit/pitargparse',
+    author='Harry Karvonen',
+    author_email='harry.karvonen@pispalanit.fi',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+
+         'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+zip_safe=False)
+
