beff78838efa31e73981c0f7343c636733aa9f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:22 +0000

    Add package more-ascii-art/1.3/more_ascii_art-1.3.zip

---
+++ packages/more-ascii-art/more_ascii_art-1.3.zip/more_ascii_art-1.3/more_ascii_art/cat.py
@@ -0,0 +1,13 @@
+# A cat in ASCII art
+
+def cat():
+  print(" _")
+  print("( \ ")
+  print(" ) )")
+  print("( (  .-''''-.  A.-.A")
+  print("\ \/         \/  , ,  \ ")
+  print(" \    \      =;   t   / = ")
+  print("  \    |''''.  ' ,-- ' ")
+  print("   /  //    | || ")
+  print("  /_ ,))    |_,)) ")
+        
