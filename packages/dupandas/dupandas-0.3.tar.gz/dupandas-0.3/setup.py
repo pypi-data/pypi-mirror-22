0897677239e291c6356e7ab2f5332b7cdbba4231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:41 +0000

    Add package dupandas/0.3/dupandas-0.3.tar.gz

---
+++ packages/dupandas/dupandas-0.3.tar.gz/dupandas-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+	name='dupandas',    
+	packages=[
+          'dupandas',
+      ],
+	version='0.3',                          
+	description = 'python package to deduplicate text data in pandas dataframe using flexible string matching and cleaning',
+  	author = 'Shivam Bansal',
+  	author_email = 'shivam5992@gmail.com',
+  	url = 'https://github.com/shivam5992/dupandas', 
+  	keywords = ['pandas', 'deduplication', 'text cleaning', 'flexible matching'],
+    long_description=open('README.md').read(),
+    license='MIT',
+    classifiers=(
+        "Programming Language :: Python"
+        ),
+)
