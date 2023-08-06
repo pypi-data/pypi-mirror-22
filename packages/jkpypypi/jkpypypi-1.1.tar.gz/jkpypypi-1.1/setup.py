bd454847dd22a683418bb8f61c9afc15ae0c5d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:50 +0000

    Add package jkpypypi/1.1/jkpypypi-1.1.tar.gz

---
+++ packages/jkpypypi/jkpypypi-1.1.tar.gz/jkpypypi-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jkpypypi',
+      version='1.1',
+      description='dj App',
+      author='Jelena',
+      author_email='kocicjelena@gmail.com',
+	  url='https://github.com/kocicjelena/py-pypi/'
+     )
