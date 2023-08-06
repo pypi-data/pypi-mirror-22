fcd2f8507b680a9f70d588d43c6937193a7e12df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:49 +0000

    Add package kalliope/0.4.4/kalliope-0.4.4.tar.gz

---
+++ packages/kalliope/kalliope-0.4.4.tar.gz/kalliope-0.4.4/kalliope/nico.py
@@ -0,0 +1,28 @@
+# coding: utf8
+import logging
+
+from kalliope import SettingLoader, SynapseLauncher
+from kalliope.core.ConfigurationManager import BrainLoader
+from kalliope.core.Players import Mplayer
+from kalliope.core.Players.PlaySoundPlayer import PlaySoundPlayer
+from kalliope.core.Players.PyAlsaAudioPlayer import PyAlsaAudioPlayer
+
+logging.basicConfig()
+logger = logging.getLogger("kalliope")
+logger.setLevel(logging.DEBUG)
+
+brain = BrainLoader().get_brain()
+settings = SettingLoader().settings
+
+import time
+
+
+# order = "bonjour"
+# SynapseLauncher.run_matching_synapse_from_order(order_to_process=order, brain=brain, settings=settings)
+if settings.rpi_settings:
+    print settings.rpi_settings.pin_mute_button
+    print settings.rpi_settings.pin_led_started
+    print settings.rpi_settings.pin_led_muted
+    print settings.rpi_settings.pin_led_talking
+    print settings.rpi_settings.pin_led_listening
+
