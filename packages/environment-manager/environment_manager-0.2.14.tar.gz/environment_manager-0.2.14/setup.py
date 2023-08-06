239b5e1bce45de5d0f724b50a6935a542141510d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:28 +0000

    Add package environment-manager/0.2.14/environment_manager-0.2.14.tar.gz

---
+++ packages/environment-manager/environment_manager-0.2.14.tar.gz/environment_manager-0.2.14/setup.py
@@ -0,0 +1,25 @@
+""" Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. """
+from setuptools import setup
+
+setup(name='environment_manager',
+      version='0.2.14',
+      description="A Client library for Environment Manager",
+      url="https://github.com/trainline/python-environment_manager",
+      author="Trainline Engineering",
+      author_email="platform.development@thetrainline.com",
+      install_requires=['requests', 'simplejson'],
+      license='Apache 2.0',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5'],
+      keywords='environment_manager client library development',
+      package_data={'': ['LICENSE.txt']},
+      packages=['environment_manager'],
+      zip_safe=True)
