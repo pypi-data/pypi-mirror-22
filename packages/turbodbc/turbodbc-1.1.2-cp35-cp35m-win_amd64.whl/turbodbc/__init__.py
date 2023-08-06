10a5c98681e203c3cd9905a755b5e21b6d92d1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:33 +0000

    Add package turbodbc/1.1.2/turbodbc-1.1.2-cp35-cp35m-win_amd64.whl

---
+++ packages/turbodbc/turbodbc-1.1.2-cp35-cp35m-win_amd64.whl/turbodbc/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+from .api_constants import apilevel, threadsafety, paramstyle
+from .connect import connect
+from .constructors import Date, Time, Timestamp
+from .exceptions import Error, InterfaceError, DatabaseError, ParameterError
+from .data_types import STRING, BINARY, NUMBER, DATETIME, ROWID
+from .options import make_options
+from turbodbc_intern import Rows, Megabytes
+
+
