a14f01ab76f20e491b1cf25ccb431b2c2f08332e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:51 +0000

    Add package rlagents/0.0.8/rlagents-0.0.8.tar.gz

---
+++ packages/rlagents/rlagents-0.0.8.tar.gz/rlagents-0.0.8/rlagents/__init__.py
@@ -0,0 +1,33 @@
+import random
+__version__ = (0, 0, 8)
+from .tools import benchmark, gymwrapper
+from .mc import MCAgent
+
+__all__ = ['benchmark', '__version__', 'RandomAgent', 'gymwrapper',
+           'MCAgent'
+           ]
+
+
+class RandomAgent:
+    def __init__(self, actions):
+        self.actions = actions
+        self.reset()
+
+    def reset(self):
+        pass
+
+    def copy(self):
+        d = RandomAgent(self.actions)
+        return d
+
+    def get_action(self, observation):
+        return random.choice(self.actions)
+
+    def start_episode(self):
+        pass
+
+    def end_episode(self):
+        pass
+
+    def observe_reward(self, rew):
+        pass
