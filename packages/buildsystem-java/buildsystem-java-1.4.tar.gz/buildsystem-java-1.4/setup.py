3e0f56d587a0ac3056d966526388903916acdc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:15 +0000

    Add package buildsystem-java/1.4/buildsystem-java-1.4.tar.gz

---
+++ packages/buildsystem-java/buildsystem-java-1.4.tar.gz/buildsystem-java-1.4/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='buildsystem-java',
+      version='1.4',
+      description='Zip downloader plugin for buildsystem package.',
+      author='Mirko Hecky',
+      url='https://github.com/mirhec/buildsystem-java',
+      download_url='https://github.com/mirhec/buildsystem-java/archive/1.4.tar.gz',
+      packages=['buildsystem'],
+      keywords=['buildsystem', 'build', 'automation', 'java', 'kotlin', 'jar']
+)
