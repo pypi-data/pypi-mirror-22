d1d78e82cb40da6e53cb7bdb38163123bb7ec985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:35 +0000

    Add package configmanager/1.11.0/configmanager-1.11.0.tar.gz

---
+++ packages/configmanager/configmanager-1.11.0.tar.gz/configmanager-1.11.0/configmanager/__init__.py
@@ -0,0 +1,19 @@
+__version__ = '1.11.0'
+
+from .managers import Config
+from .items import Item
+from .exceptions import ConfigError, RequiredValueMissing, NotFound
+from .base import ItemAttribute
+from .persistence import ConfigPersistenceAdapter
+from .item_types import Types
+
+all = [
+    'Item',
+    'Config',
+    'ItemAttribute',
+    'ConfigPersistenceAdapter',
+    'Types',
+    'ConfigError',
+    'RequiredValueMissing',
+    'NotFound',
+]
