ded9144ee2d78a44dab9977b3710a43deadd2bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:18 +0000

    Add package glwindow/2.0.3/GLWindow-2.0.3.tar.gz

---
+++ packages/glwindow/GLWindow-2.0.3.tar.gz/GLWindow-2.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, Extension
+
+GLWindow = Extension(
+	'GLWindow.glwnd',
+	libraries = ['User32', 'opengl32', 'gdi32'],
+	sources = [
+		'src/GLWindow.cpp',
+		'src/ModernContext.cpp',
+	]
+)
+
+setup(
+	name = 'GLWindow',
+	version = '2.0.3',
+	description = 'GLWindow',
+	url = 'https://github.com/cprogrammer1994/GLWindow',
+	author = 'Szabolcs Dombi',
+	author_email = 'cprogrammer1994@gmail.com',
+	license = 'MIT',
+	packages = ['GLWindow'],
+	ext_modules = [GLWindow],
+	platforms = ['win32', 'win64']
+)
