f36964ea53508929cf3d5adb60c35e49308a4feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:25 +0000

    Add package dicom2nifti/1.1.12/dicom2nifti-1.1.12.tar.gz

---
+++ packages/dicom2nifti/dicom2nifti-1.1.12.tar.gz/dicom2nifti-1.1.12/dicom2nifti/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+dicom2nifti
+
+@author: abrys
+"""
+
+# noinspection PyUnresolvedReferences
+from .settings import disable_validate_sliceincrement, \
+    disable_validate_orientation, \
+    disable_validate_orthogonal, \
+    disable_validate_slicecount, \
+    disable_validate_multiframe_implicit, \
+    enable_validate_orientation, \
+    enable_validate_orthogonal, \
+    enable_validate_slicecount, \
+    enable_validate_sliceincrement, \
+    enable_validate_multiframe_implicit
+# noinspection PyUnresolvedReferences
+from .convert_dicom import dicom_series_to_nifti
+# noinspection PyUnresolvedReferences
+from .convert_dir import convert_directory
