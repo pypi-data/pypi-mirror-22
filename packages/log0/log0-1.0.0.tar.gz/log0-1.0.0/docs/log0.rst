0ee3747c69811e929a4702b8c0478811155ba3d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:43 +0000

    Add package log0/1.0.0/log0-1.0.0.tar.gz

---
+++ packages/log0/log0-1.0.0.tar.gz/log0-1.0.0/docs/log0.rst
@@ -0,0 +1,22 @@
+log0 package
+============
+
+Submodules
+----------
+
+log0\.log0 module
+-----------------
+
+.. automodule:: log0.log0
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: log0
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ packages/log0/log0-1.0.0.tar.gz/log0-1.0.0/tests/test_log0.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_log0
+----------------------------------
+
+Tests for `log0` module.
+"""
+
+import pytest
+import tempfile
+
+from log0 import setup_logger
+
+
+def test_write_to_logfile():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    temp = tempfile.NamedTemporaryFile()
+    try:
+        logger = setup_logger(logfile=temp.name)
+        logger.info("test log output")
+
+        with open(temp.name) as f:
+            content = f.read()
+            assert content.endswith("test log output\n")
+
+    finally:
+        temp.close()
