bb6b2924099cf5ba6ade6872b8fc2494e2f827f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:30 +0000

    Add package nester_cjl/1.4.0/nester_cjl-1.4.0.zip

---
+++ packages/nester_cjl/nester_cjl-1.4.0.zip/nester_cjl-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from  distutils.core import setup
+setup(
+    name='nester_cjl',
+    version='1.4.0',
+    py_modules=['nester_cjl'],
+    author='Monster',
+    author_email='chengjinlang@126.com',
+    url='',
+    description='A simple printer of nested lists',
+)
