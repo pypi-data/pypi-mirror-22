d91936669e934cfdbacc5e484f47106a2d786e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:13 +0000

    Add package fastimage/2.0.0/fastimage-2.0.0.tar.gz

---
+++ packages/fastimage/fastimage-2.0.0.tar.gz/fastimage-2.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import fastimage
+
+setup(
+    name="fastimage",
+    version=fastimage.__version__,
+    description="Finds the size or type of an image given its uri by fetching as little as needed",
+    author="Brian Muller",
+    author_email="bamuller@gmail.com",
+    license="MIT",
+    url="http://github.com/bmuller/fastimage",
+    packages=find_packages(),
+    install_requires=["aiohttp>=2.1.0"],
+    scripts=['bin/fastimage']
+)
