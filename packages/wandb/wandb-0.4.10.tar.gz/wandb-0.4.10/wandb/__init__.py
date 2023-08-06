ee73ad5718b1f908793eccba8f0220f47e41643f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:43 +0000

    Add package wandb/0.4.10/wandb-0.4.10.tar.gz

---
+++ packages/wandb/wandb-0.4.10.tar.gz/wandb-0.4.10/wandb/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Chris Van Pelt"""
+__email__ = 'vanpelt@wandb.ai'
+__version__ = '0.4.10'
+
+import types, sys
+from .git_repo import GitRepo
+from .api import Api, Error
+from .sync import Sync
+from .config import Config
+
+__all__ = ["Api", "Error", "Config"]
