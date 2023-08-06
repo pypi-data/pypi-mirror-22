1c27b10a8b34d670c1435f53550d6c7900057596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:26 +0000

    Add package genderly/0.101/genderly-0.101.tar.gz

---
+++ packages/genderly/genderly-0.101.tar.gz/genderly-0.101/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='genderly',
+      version='0.101',
+      description='A library to classify an indian name as Male or Female. Mainly focused around making the user onboarding smooth for eCommerce merchants by pre-filling the gender for their users during a signup.',
+      url='https://github.com/rajdeltarobo/genderly',
+      author='Raj Vardhan Singh',
+      author_email='is.rajvardhan@gmail.com',
+      license='MIT',
+      packages=['genderly'],
+      include_package_data=True,
+      zip_safe=False)
