8fca4ebb4e38fb21767f761e7700fdcf189ca525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:28 +0000

    Add package funport/0.1/funport-0.1.tar.gz

---
+++ packages/funport/funport-0.1.tar.gz/funport-0.1/funport/__init__.py
@@ -0,0 +1,4 @@
+def joke():
+  return ("hello no jokes are here")
+
+
---
+++ packages/funport/funport-0.1.tar.gz/funport-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name="funport",
+	version="0.1",
+	description="the joke for funport",
+	author="flying man",
+	author_email="123@gmail.com",
+	license="MIT",
+	packages=["funport"],
+	zip_safe=False)
