4cda6f8c5138e6551ffaa2511301bc45e220ea7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:09 +0000

    Add package pyetsy/0.4.7/pyetsy-0.4.7.tar.gz

---
+++ packages/pyetsy/pyetsy-0.4.7.tar.gz/pyetsy-0.4.7/etsy/__init__.py
@@ -0,0 +1,9 @@
+from _v2 import EtsyV2 as Etsy  # noqa
+from etsy_env import EtsyEnvProduction  # noqa
+
+
+__version__ = '0.4.7'
+__author__ = 'Dan McKinley & Fulfil.IO Inc.'
+__copyright__ = 'Copyright 2010, Etsy Inc.'
+__license__ = 'GPL v3'
+__email__ = 'dan@etsy.com'
