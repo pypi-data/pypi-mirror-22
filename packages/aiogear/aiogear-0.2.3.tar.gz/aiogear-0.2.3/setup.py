33707945cc65ec81df9b3d66e09e3a6884bae581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:11 +0000

    Add package aiogear/0.2.3/aiogear-0.2.3.tar.gz

---
+++ packages/aiogear/aiogear-0.2.3.tar.gz/aiogear-0.2.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='aiogear',
+    version='0.2.3',
+    author='Sinan Nalkaya',
+    author_email='sardok@gmail.com',
+    url='https://github.com/sardok/aiogear',
+    description='Asynchronous gearman protocol based on asyncio',
+    packages=['aiogear'],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
